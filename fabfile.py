dff5d1beac92f61334995687e642cc81e2734879
Eishay Smith <eishay@gmail.com>
2012-07-15 21:32:47 -0700

    sync chrome files

---
+++ fabfile.py
@@ -0,0 +1,9 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.utils import *
+from fabric.contrib.console import confirm
+from fabric.operations import run, put
+from time import sleep
+
+def chrome():
+  local("rsync -vrc --delete chrome ~/Dropbox/keepit")
