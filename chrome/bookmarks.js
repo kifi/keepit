647a9948faea6eb3505398472f702ada9d9d8e94
yonatanm <yonatanm@gmail.com>
2012-08-20 01:29:20 +0300

    apply pull request 7 comments

--- chrome/bookmarks.js
+++ chrome/bookmarks.js
@@ -1,95 +1,91 @@
 // creates (if needed) bookmarks structure under bookmarks bar(if not exists) /keepIt/private /keepIt/public
 // returns all important bookmarks objects (root, bar, keepit, private and public)
 // since chrome.bookmarks api is asynchronious code is messi 
-function getBookMarks() {
 
-		var bookmarks={};
-		function getRoot() {
-			chrome.bookmarks.get("0", function(bm){ 
-				bookmarks.root=bm[0]; 
-				getBar();
-			});
-		}
+function getBookMarks(callback) {
+  $ = jQuery.noConflict()
 
-		function getBar() {
-			console.log("in getBar root.id is "+bookmarks.root.id);
-			chrome.bookmarks.getChildren(bookmarks.root.id, function(children) {
-				children.forEach(function(bm) { 
-					if (bm.title=="Bookmarks Bar")	{
-						bookmarks.bar=bm;
-						getKeepIt();
-						return;
-					}
-				});
-			});
-		}
+	function findRoot() {
+		chrome.bookmarks.get("0", function(bm){ 
+			bookmarks.root=bm[0]; 
+			findBar();
+		});
+	}
 
-		function getKeepIt() {
-			console.log("in getKeepIt bar.id is "+bookmarks.bar.id);
-			chrome.bookmarks.getChildren(bookmarks.bar.id, function(children) {
-				children.forEach(function(bm) { 
-					if (bm.title=="KeepIt")	{
-						bookmarks.keepIt=bm;
-						getPrivate();
-						return;
-					}
-				});
-				if (!bookmarks.keepIt) createKeepIt();
-			});
-		}
+	function findBar() {
+		chrome.bookmarks.getChildren(bookmarks.root.id, function(children) {
+			bookmarks.bar = $.grep(children, function (bm) { return bm.title=="Bookmarks Bar"; })[0];
+			internKeepIt();
+		});
+	}
 
-		function createKeepIt(){
-			console.log("in create keepIt");
-			chrome.bookmarks.create({'parentId': bookmarks.bar.id, 'title': 'KeepIt'}, function(bm) {
-				bookmarks.keepIt=bm;
-				createPrivate();
-				createPublic();
-			});
-		}
-
-		function createPrivate(){
-			chrome.bookmarks.create({'parentId': bookmarks.keepIt.id, 'title': 'private'}, function(bm) {
-				bookmarks.private=bm;
-			});
-		}
-
-		function createPublic(){
-			chrome.bookmarks.create({'parentId': bookmarks.keepIt.id, 'title': 'public'}, function(bm) {
-				bookmarks.public=bm;
-			});
-		}
+	function internKeepIt() {
+		chrome.bookmarks.getChildren(bookmarks.bar.id, function(children) {
+			var res = $.grep(children, function (bm) { return bm.title=="KeepIt"; })
+			if (res.length>0) {
+				bookmarks.keepIt=res[0];
+				internPrivate();
+			} else createKeepIt();
+		});
+	}
 
 
-		function getPrivate() {
-			chrome.bookmarks.getChildren(bookmarks.keepIt.id, function(children) {
-				children.forEach(function(bm) { 
-					if (bm.title=="private")	{
-						bookmarks.private=bm;
-						getPublic();
-						return;
-					}
-				});
-				if (!bookmarks.private){
-					createPrivate();
-					getPublic();
-				}
-			});
-		}
 
-	function getPublic() {
-			chrome.bookmarks.getChildren(bookmarks.keepIt.id, function(children) {
-				children.forEach(function(bm) { 
-					if (bm.title=="public")	{
-						bookmarks.public=bm;
-						return;
-					}
-				});
-				if (!bookmarks.public){
-					createPublic();
-				}
-			});
-		}
+	function internPrivate() {
+		chrome.bookmarks.getChildren(bookmarks.keepIt.id, function(children) {
+			var res = $.grep(children, function (bm) { return bm.title=="private"; })
+			if (res.length > 0) {
+				bookmarks.private=res[0];
+				internPublic();
+			} else createPrivate();
+		});
+	}
 
-	getRoot();
-	return  bookmarks;
+	function internPublic() {
+		chrome.bookmarks.getChildren(bookmarks.keepIt.id, function(children) {
+			var res = $.grep(children, function (bm) { return bm.title=="public"; })
+			if (res.length > 0) {
+				bookmarks.public=res[0];
+				done(bookmarks);
+			} else createPublic();
+		});
+	}
+
+	function createKeepIt(){
+		chrome.bookmarks.create({'parentId': bookmarks.bar.id, 'title': 'KeepIt'}, function(bm) {
+			bookmarks.keepIt=bm;
+			internPrivate();
+		});
+	}
+
+	function createPrivate(){
+		chrome.bookmarks.create({'parentId': bookmarks.keepIt.id, 'title': 'private'}, function(bm) {
+			bookmarks.private=bm;
+			internPublic();
+		});
+	}
+
+	function createPublic(){
+		chrome.bookmarks.create({'parentId': bookmarks.keepIt.id, 'title': 'public'}, function(bm) {
+			bookmarks.public=bm;
+			done(bookmarks);
+		});
+	}
+
+
+	function done(bookmarks){
+		console.log("finish bokmars traversing");
+		getBookMarks.prototype.cachedBookmarks = bookmarks; 
+		if (callback)
+			callback(bookmarks);
+	}
+
+	var bookmarks={};
+	console.log("looking for bookmarks");
+
+	if(getBookMarks.prototype.cachedBookmarks){
+		console.log("using cache: "+JSON.stringify(getBookMarks.prototype.cachedBookmarks));
+		callback(getBookMarks.prototype.cachedBookmarks);
+	}
+	findRoot();
 }
