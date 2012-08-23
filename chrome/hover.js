16392964cd606f5f677eff03728457bdf954d66d
Eishay Smith <eishay@gmail.com>
2012-08-23 00:16:09 -0700

    hover look

--- chrome/hover.js
+++ chrome/hover.js
@@ -6,40 +6,40 @@ console.log("injecting keep it hover div");
     setTimeout(fun, 1000);
   }
   
-  function showBookmarkHover(userInfo) {
+  function showBookmarkHover(user) {
     var existingElements = $('#keepit_hover').length;
     if (existingElements > 0) {
       console.warn("hover is already injected. There are " + existingElements + " existing elements")
       return;
     }
-    var hover = $("<div id='keepit_hover'></div>");
-    var button = $("<button id='keep_action' type='button'>Keep Bookmark</button>")
+    var hover = $("<div id='keepit_hover' class='keepit_hover'></div>");
+    var bar = $("<div class='keep_hover_bar'>" + 
+      "<a data-hover='tooltip' class='name_tooltip_link' href='http://www.facebook.com/" + user.facebook_id + "' target='_blank'><img src='https://graph.facebook.com/" + user.facebook_id + "/picture?type=square' width='24' height='24' alt=''></a>" +
+      "Keepit" + 
+      "</div>");
+    hover.append(bar);
+    var button = $("<div id='keep_action' class='keep_action' type='button'>Keep Bookmark</button>")
+    hover.append(button);
+    hover.append("<br/><input type='checkbox' id='keepit_private' value='private'>private</input>");
+
     button.click(function() {
       console.log("bookmarking page " + document.location.href);
       chrome.extension.sendRequest({type: "add_bookmarks", url: document.location.href, title: document.title, private: $("#keepit_private").is(":checked")}, function(response) {
         console.log("bookmark added! -> " + JSON.stringify(response));
-        hover.empty();
-        hover.append("<center>Share with facebook!<br/><br/>");
-        var img = "<img src='" + userInfo["picture"] + "'/><br/>";
-        hover.append(img);
-        hover.append('<br/><br/><a target="_top" href="https://www.facebook.com/dialog/oauth/?client_id=157864447681740&redirect_uri=https%3A%2F%2Fwww.keepit.com&scope=email">Connect with Facebook!</a></center>');
+        $('.keepit_hover').animate({
+            right: '-=207'
+          },
+        300);
       });
     });
-    hover.append(button);
-    hover.append("<br/><input type='checkbox' id='keepit_private' value='private'>private</input>")
-    hover.append("<br/>env: "+env)
     $("body").append(hover);
     setTimeout(function() {
-      //$(window).scroll(function () {
-        //if( $(this).scrollTop() > 10 ) {
-          $('#keepit_hover').animate({
-              right: '+=150'
-            },
-            300);
-        //}
-      //});
+      $('.keepit_hover').animate({
+          right: '+=207'
+        },
+        300);
     }, 1000);//1 seconds
-    $("#keepit_hover").onClick
+    //$(".keepit_hover").onClick
   }
 
   function getUserInfo(callback) {
