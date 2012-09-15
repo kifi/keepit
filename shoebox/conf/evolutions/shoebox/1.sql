444c946d5f3eb7ec02cddfa6c903983df697967d
Eishay Smith <eishay@gmail.com>
2012-09-14 17:21:01 -0700

    adding the normalized uri

--- shoebox/conf/evolutions/shoebox/1.sql
+++ shoebox/conf/evolutions/shoebox/1.sql
@@ -17,6 +17,25 @@ CREATE TABLE user (
     CONSTRAINT user_u_external_id UNIQUE (external_id) 
 );
 
+CREATE TABLE normalized_uri (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    created_at datetime NOT NULL,
+    updated_at datetime NOT NULL,
+    external_id varchar(36) NOT NULL,
+    state varchar(20) NOT NULL,
+
+    title varchar(2048) NOT NULL,
+    url varchar(2048) NOT NULL,
+    url_hash varchar(512) NOT NULL,
+    is_private boolean,
+    
+    KEY (url_hash),
+    PRIMARY KEY (id),
+    
+    CONSTRAINT normalized_uri_u_external_id UNIQUE (external_id) 
+);
+
+
 CREATE TABLE bookmark (
     id bigint(20) NOT NULL AUTO_INCREMENT,
     created_at datetime NOT NULL,
@@ -25,18 +44,18 @@ CREATE TABLE bookmark (
     state varchar(20) NOT NULL,
 
     user_id bigint(20) NOT NULL,
-    title varchar(1024) NOT NULL,
-    url varchar(1024) NOT NULL,
+    title varchar(2048) NOT NULL,
+    url varchar(2048) NOT NULL,
     uri_id bigint(20) NOT NULL,
     bookmark_path varchar(256),
     is_private boolean,
     
-    KEY (url_hash),
-    KEY (user_id, url_hash),
+    KEY (user_id, uri_id),
     PRIMARY KEY (id),
     
     CONSTRAINT bookmark_u_external_id UNIQUE (external_id),
-    CONSTRAINT bookmark_f_user FOREIGN KEY (user_id) REFERENCES user(id) 
+    CONSTRAINT bookmark_f_user FOREIGN KEY (user_id) REFERENCES user(id), 
+    CONSTRAINT bookmark_f_uri FOREIGN KEY (uri_id) REFERENCES normalized_uri(id) 
 );
 
 CREATE TABLE evolutions (
