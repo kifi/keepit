31c7d5c9c367c6a10f5c3af3972d8991c5a369b4
Eishay Smith <eishay@gmail.com>
2012-10-05 16:10:27 -0700

    keepitfindit -> ezkeep

--- chrome/hover.js
+++ chrome/hover.js
@@ -143,7 +143,7 @@ console.log("injecting keep it hover div");
           console.log(result);
         }
       }
-      xhr.open("POST", 'http://dev.keepitfindit.com:9000/chat/'+user.exuuid, true);
+      xhr.open("POST", 'http://dev.ezkeep.com:9000/chat/'+user.exuuid, true);
       xhr.setRequestHeader('Content-Type', 'application/json');
       xhr.send(JSON.stringify(  {"url": document.location.href, "message":message.val() } ));
       console.log("sent")
