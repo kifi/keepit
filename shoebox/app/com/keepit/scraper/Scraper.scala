4eec50857fb452f2b53bbb74050a37cd50713524
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2012-10-10 17:14:59 -0700

    eliminated null in Scraper, and now S3ArticleStore re-throws Exception

--- shoebox/app/com/keepit/scraper/Scraper.scala
+++ shoebox/app/com/keepit/scraper/Scraper.scala
@@ -15,40 +15,40 @@ class Scraper {
   val parser = new Parser(config);
   
   def fetchArticle(normalizedUri: NormalizedURI): Either[Article, ScraperError] = {
-    var fetchResult: PageFetchResult = null
+    var fetchResult: Option[PageFetchResult] = None
     val webURL = new WebURL
     webURL.setURL(normalizedUri.url)
     try {
-      val fetchResult = pageFetcher.fetchHeader(webURL);
-      val statusCode = fetchResult.getStatusCode()
-      if (statusCode != HttpStatus.SC_OK) {
-        if (statusCode == HttpStatus.SC_MOVED_PERMANENTLY || statusCode == HttpStatus.SC_MOVED_TEMPORARILY) {
-          // TODO: redirect?
-          // val movedToUrl = fetchResult.getMovedToUrl();
-          Right(ScraperError(normalizedUri, statusCode, "fetch failed: httpStatusCode=" + statusCode))
-        } else if (fetchResult.getStatusCode() == CustomFetchStatus.PageTooBig) {
-          // logger.info("Skipping a page which was bigger than max allowed size: " + curURL.getURL());
-          Right(ScraperError(normalizedUri, statusCode, "fetch failed: page size exceeded maximum. revisit crawler4j config"))
-        } else {
-          Right(ScraperError(normalizedUri, statusCode, "fetch failed: httpStatusCode=" + statusCode))
-        }
-      } else {
-        // the status is OK. now scrape it
+      val result = pageFetcher.fetchHeader(webURL)
+      fetchResult = Some(result)
+      val statusCode = result.getStatusCode()
+      
+      if (statusCode == HttpStatus.SC_OK) {
+      	// the status is OK. now scrape it.
         val page = new Page(webURL)
-        if (fetchResult.fetchContent(page) && parser.parse(page, normalizedUri.url)) {
+        if (result.fetchContent(page) && parser.parse(page, normalizedUri.url)) {
           page.getParseData() match {
-            case htmlData: HtmlParseData =>
-              val title = htmlData.getTitle()
+      	    case htmlData: HtmlParseData =>
+      	      val title = htmlData.getTitle()
               val content = htmlData.getText()
-              Left(Article(normalizedUri.copy(state = SCRAPED), title, content)) // return Article             
-            case _ => Right(ScraperError(normalizedUri.copy(state = SCRAPE_FAILED), statusCode, "not html"))
+      		  Left(Article(normalizedUri.copy(state = SCRAPED), title, content)) // return Article             
+      		case _ => Right(ScraperError(normalizedUri.copy(state = SCRAPE_FAILED), statusCode, "not html"))
           }
-        } else {
+      	} else {
           Right(ScraperError(normalizedUri.copy(state = SCRAPE_FAILED), statusCode, "parse failed"))
         }
+      } else if (statusCode == HttpStatus.SC_MOVED_PERMANENTLY || statusCode == HttpStatus.SC_MOVED_TEMPORARILY) {
+        // TODO: redirect?
+        // val movedToUrl = result.getMovedToUrl();
+        Right(ScraperError(normalizedUri, statusCode, "fetch failed: httpStatusCode=" + statusCode))
+      } else if (result.getStatusCode() == CustomFetchStatus.PageTooBig) {
+        // logger.info("Skipping a page which was bigger than max allowed size: " + curURL.getURL());
+        Right(ScraperError(normalizedUri, statusCode, "fetch failed: page size exceeded maximum. revisit crawler4j config"))
+      } else {
+        Right(ScraperError(normalizedUri, statusCode, "fetch failed: httpStatusCode=" + statusCode))
       }
     } finally {
-      if (fetchResult != null) fetchResult.discardContentIfNotConsumed();
+      fetchResult.foreach(_.discardContentIfNotConsumed())
     }
   }
 }
--- shoebox/app/com/keepit/search/S3ArticleStore.scala
+++ shoebox/app/com/keepit/search/S3ArticleStore.scala
@@ -1,5 +1,6 @@
 package com.keepit.search
 
+import com.keepit.common.logging.Logging
 import scala.collection.Map
 import com.amazonaws.auth._
 import com.amazonaws.services.s3._
@@ -9,14 +10,14 @@ import com.amazonaws.AmazonServiceException
 import java.io.{InputStream, ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
 import java.lang.UnsupportedOperationException
 
-class S3ArticleStore(bucketName: String, accessKey: String, secretKey: String) extends Map[Long, Article] {
+class S3ArticleStore(bucketName: String, accessKey: String, secretKey: String) extends Map[Long, Article] with Logging {
   val awsCredentials = new BasicAWSCredentials(accessKey, secretKey)
   val amazonS3Client = new AmazonS3Client(awsCredentials)
 
   def + [T >: Article](kv: (Long, T)):Map[Long, T] = {
     kv match {
       case (normalizedUrlId, article) =>
-        doWithS3Client{ s3Client =>
+        doWithS3Client("adding an item to S3ArticleStore"){ s3Client =>
           s3Client.putObject(bucketName, normalizedUrlId.toString, toInputStream(article.asInstanceOf[Article]), new ObjectMetadata)
         }
     }
@@ -24,14 +25,14 @@ class S3ArticleStore(bucketName: String, accessKey: String, secretKey: String) e
   }
   
   def - (normalizedUrlId: Long) = {
-    doWithS3Client{ s3Client =>
+    doWithS3Client("removing an item from S3ArticleStore"){ s3Client =>
       s3Client.deleteObject(bucketName, normalizedUrlId.toString)
     }
     this
   }
   
   def get(normalizedUrlId: Long): Option[Article] = {
-    doWithS3Client{ s3Client =>
+    doWithS3Client("getting an item from S3ArticleStore"){ s3Client =>
       val s3obj = s3Client.getObject(bucketName, normalizedUrlId.toString)
       val is = s3obj.getObjectContent
       val ois = new ObjectInputStream(is)
@@ -41,26 +42,21 @@ class S3ArticleStore(bucketName: String, accessKey: String, secretKey: String) e
         is.close
         ois.close
       }
-    }  
+    }
   }
   
   def iterator = throw new UnsupportedOperationException
   
   override def empty = throw new UnsupportedOperationException
   
-  private def doWithS3Client[T](body: AmazonS3Client=>T): Option[T] = {
+  private def doWithS3Client[T](what: =>String)(body: AmazonS3Client=>T): Option[T] = {
     var ret: Option[T] = None
     try {
       ret = Some(body(amazonS3Client))
     } catch {
-      case ase: AmazonServiceException =>
-        System.out.println("Error Message:    " + ase.getMessage())
-        System.out.println("HTTP Status Code: " + ase.getStatusCode())
-        System.out.println("AWS Error Code:   " + ase.getErrorCode())
-        System.out.println("Error Type:       " + ase.getErrorType())
-        System.out.println("Request ID:       " + ase.getRequestId())
-      case ace: AmazonClientException =>
-        System.out.println("Error Message: " + ace.getMessage())
+      case ex: Exception =>
+        log.error("failed: "+what, ex)
+        throw ex
     }
     ret
   }
