83361fd1c568fb2f7a287e6e4457c9c12f066c5a
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2012-10-11 11:51:08 -0700

    added a scraper test

--- shoebox/app/com/keepit/scraper/Scraper.scala
+++ shoebox/app/com/keepit/scraper/Scraper.scala
@@ -1,18 +1,55 @@
 package com.keepit.scraper
 
+import com.keepit.common.logging.Logging
+import com.keepit.common.db.Id
 import com.keepit.search.Article
 import com.keepit.model.NormalizedURI
 import com.keepit.model.NormalizedURI.States._
-import edu.uci.ics.crawler4j.crawler.{CrawlConfig, Page, WebCrawler}
+import com.keepit.common.db.CX
+import edu.uci.ics.crawler4j.crawler.{CrawlConfig, Page }
 import edu.uci.ics.crawler4j.fetcher.{CustomFetchStatus, PageFetcher, PageFetchResult}
 import edu.uci.ics.crawler4j.parser.{HtmlParseData, Parser}
 import edu.uci.ics.crawler4j.url.WebURL
 import org.apache.http.HttpStatus;
+import play.api.Play.current
 
-class Scraper {
+class Scraper(articleStore: scala.collection.mutable.Map[Id[NormalizedURI], Article]) extends Logging {
   val config = new CrawlConfig()
   val pageFetcher = new PageFetcher(config)
   val parser = new Parser(config);
+  
+  def run() {
+    try {
+      val uris = CX.withConnection { implicit c =>
+        NormalizedURI.getByState(ACTIVE)
+      }
+      processURIs(uris)
+    } catch {
+      case ex: Throwable => log.error("error in scaper run", ex) // log and eat the exception
+    }
+  }
+  
+  def processURIs(uris: Seq[NormalizedURI]) {
+    uris.map{ uri =>
+      fetchArticle(uri) match {
+        case Left(article) =>
+          // store article in a store map
+          try {
+            articleStore += (uri.id.get -> article)
+            // succeeded. update the state to SCRAPED and save
+            CX.withConnection { implicit c =>
+              uri.withState(NormalizedURI.States.SCRAPED).save
+            }
+          } catch {
+            case ex:Exception => log.error("failed to store an article: uri.id=" + uri.id.get.id)
+          }
+        case Right(error) =>
+          CX.withConnection { implicit c =>
+            uri.withState(NormalizedURI.States.SCRAPE_FAILED).save
+          }
+      }
+    }
+  }
   
   def fetchArticle(normalizedUri: NormalizedURI): Either[Article, ScraperError] = {
     var fetchResult: Option[PageFetchResult] = None
@@ -31,11 +68,11 @@ class Scraper {
       	    case htmlData: HtmlParseData =>
       	      val title = htmlData.getTitle()
               val content = htmlData.getText()
-      		  Left(Article(normalizedUri.copy(state = SCRAPED), title, content)) // return Article             
-      		case _ => Right(ScraperError(normalizedUri.copy(state = SCRAPE_FAILED), statusCode, "not html"))
+      		  Left(Article(normalizedUri, title, content)) // return Article             
+      		case _ => Right(ScraperError(normalizedUri, statusCode, "not html"))
           }
       	} else {
-          Right(ScraperError(normalizedUri.copy(state = SCRAPE_FAILED), statusCode, "parse failed"))
+          Right(ScraperError(normalizedUri, statusCode, "parse failed"))
         }
       } else if (statusCode == HttpStatus.SC_MOVED_PERMANENTLY || statusCode == HttpStatus.SC_MOVED_TEMPORARILY) {
         // TODO: redirect?
---
+++ shoebox/test/com/keepit/scraper/ScraperTest.scala
@@ -0,0 +1,87 @@
+package com.keepit.scraper
+
+import com.keepit.search.Article
+import com.keepit.model.NormalizedURI
+import com.keepit.common.db.CX
+import com.keepit.common.db.Id
+import com.keepit.model._
+import com.keepit.test.EmptyApplication
+import org.junit.runner.RunWith
+import org.specs2.mutable._
+import org.specs2.runner.JUnitRunner
+import play.api.Play.current
+import play.api.libs.json.Json
+import play.api.test._
+import play.api.test.Helpers._
+import edu.uci.ics.crawler4j.crawler.CrawlConfig
+import edu.uci.ics.crawler4j.fetcher.{PageFetcher, PageFetchResult}
+import edu.uci.ics.crawler4j.url.WebURL
+import org.apache.http.HttpStatus
+import scala.collection.mutable.{Map => MutableMap}
+
+@RunWith(classOf[JUnitRunner])
+class ScraperTest extends SpecificationWithJUnit {
+
+  def setup() = {
+  }
+
+  "Scraper" should {
+    "get a article from an existing website" in {
+      val store = MutableMap.empty[Id[NormalizedURI], Article]
+      val scraper = getMockScraper(store)
+      val url = "http://www.keepit.com/existing"
+      val uri = NormalizedURI(title = "title", url = url, state = NormalizedURI.States.ACTIVE)
+      val result = scraper.fetchArticle(uri)
+
+      result.isLeft === true // Left is Article
+      result.left.get.title === "foo"
+      result.left.get.content === "bar"        
+    }
+    
+    "throw an error from a non-existing website" in {
+      val store = scala.collection.mutable.Map.empty[Id[NormalizedURI], Article]
+      val scraper = getMockScraper(store)
+      val url = "http://www.keepit.com/missing"
+      val uri = NormalizedURI(title = "title", url = url, state = NormalizedURI.States.ACTIVE)
+      val result = scraper.fetchArticle(uri)
+      result.isRight === true // Right is ScraperError
+      result.right.get.httpStatusCode === HttpStatus.SC_NOT_FOUND
+    }
+    
+    "fetch ACTIVE uris and scrape them" in {
+      running(new EmptyApplication()) {
+        var uri1: NormalizedURI = null
+        var uri2: NormalizedURI = null
+        CX.withConnection { implicit c =>
+          val user1 = User(firstName = "Joe", lastName = "Smith").save
+          val user2 = User(firstName = "Moo", lastName = "Brown").save
+          uri1 = NormalizedURI(title = "existing", url = "http://www.keepit.com/existing").save
+          uri2 = NormalizedURI(title = "missing", url = "http://www.keepit.com/missing").save
+        }
+        val store = MutableMap.empty[Id[NormalizedURI], Article]
+        val scraper = getMockScraper(store)
+        scraper.run
+        store.size === 1
+      
+        // get URIs from db
+        CX.withConnection { implicit c =>
+          uri1 = NormalizedURI.get(uri1.id.get) 
+          uri2 = NormalizedURI.get(uri2.id.get)
+        }
+        uri1.state === NormalizedURI.States.SCRAPED 
+        uri2.state === NormalizedURI.States.SCRAPE_FAILED
+      }
+    }
+  }
+
+  def getMockScraper(articleStore: MutableMap[Id[NormalizedURI], Article]) = {
+	new Scraper(articleStore) {
+	  override def fetchArticle(uri: NormalizedURI): Either[Article, ScraperError]	 = {
+	    uri.url match {
+	      case "http://www.keepit.com/existing" => Left(Article(uri, "foo", "bar"))
+	      case "http://www.keepit.com/missing" => Right(ScraperError(uri, HttpStatus.SC_NOT_FOUND, "not found"))
+	    }
+	  } 
+	}
+  }
+}
