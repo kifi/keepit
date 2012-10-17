1e2e8c9b610fe72b5d4760751980190875647a0b
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2012-10-17 09:48:20 -0700

    added re-scraping and re-indexing admin

--- shoebox/app/com/keepit/controllers/admin/ArticleIndexerController.scala
+++ shoebox/app/com/keepit/controllers/admin/ArticleIndexerController.scala
@@ -5,15 +5,31 @@ import java.util.concurrent.TimeUnit
 import play.api._
 import play.api.Play.current
 import play.api.mvc._
+import com.keepit.common.db._
 import com.keepit.common.logging.Logging
 import com.keepit.controllers.CommonActions._
 import com.keepit.inject._
 import com.keepit.search.index.ArticleIndexerPlugin
 import com.keepit.search.index.ArticleIndexer
+import com.keepit.model.NormalizedURI
+import com.keepit.model.NormalizedURI.States._
 
 object ArticleIndexerController extends Controller with Logging {
 
   def index = Action { implicit request => 
+    val indexer = inject[ArticleIndexerPlugin]
+    val cnt = indexer.index()
+    Ok("indexed %d articles".format(cnt))
+  }
+
+  def indexByState(state: State[NormalizedURI]) = Action { implicit request =>
+    def updateStateFrom(oldState: State[NormalizedURI]) = CX.withConnection { implicit c =>
+      NormalizedURI.getByState(oldState).foreach{ uri => uri.withState(SCRAPED).save }
+    }
+    state match { // TODO: factor out valid state transitions
+      case INDEXED => updateStateFrom(state)
+      case _ => // ignore 
+    }
     val indexer = inject[ArticleIndexerPlugin]
     val cnt = indexer.index()
     Ok("indexed %d articles".format(cnt))
--- shoebox/app/com/keepit/controllers/admin/ScraperController.scala
+++ shoebox/app/com/keepit/controllers/admin/ScraperController.scala
@@ -11,12 +11,13 @@ import play.api.libs.json.JsObject
 import play.api.libs.json.JsString
 import play.api.libs.json.JsValue
 import play.api.libs.json.JsNumber
-import com.keepit.common.db.{Id, CX, ExternalId}
+import com.keepit.common.db._
 import com.keepit.common.logging.Logging
 import com.keepit.controllers.CommonActions._
 import com.keepit.inject._
 import com.keepit.scraper.ScraperPlugin
 import com.keepit.model.NormalizedURI
+import com.keepit.model.NormalizedURI.States._
 import com.keepit.search.ArticleStore
 
 object ScraperController extends Controller with Logging {
@@ -27,6 +28,21 @@ object ScraperController extends Controller with Logging {
     Ok(views.html.scrape(articles))
   }
   
+  def scrapeByState(state: State[NormalizedURI]) = Action { implicit request =>
+    def updateStateFrom(oldState: State[NormalizedURI]) = CX.withConnection { implicit c =>
+      NormalizedURI.getByState(oldState).foreach{ uri => uri.withState(ACTIVE).save }
+    }
+    state match { // TODO: factor out valid state transitions
+      case SCRAPED => updateStateFrom(state)
+      case SCRAPE_FAILED => updateStateFrom(state)
+      case INDEXED => updateStateFrom(state)
+      case _ => // ignore
+    }
+    val scraper = inject[ScraperPlugin]
+    val articles = scraper.scrape()
+    Ok(views.html.scrape(articles))
+  }
+
   def getScraped(id: Id[NormalizedURI]) = Action { implicit request => 
     val store = inject[ArticleStore]
     val article = store.get(id).get
