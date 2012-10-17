475746b9121e2a052c617aac15e6e7c85886bdef
Eishay Smith <eishay@gmail.com>
2012-10-17 16:17:52 -0700

    the basic store interface

---
+++ shoebox/app/com/keepit/common/store/ObjectStore.scala
@@ -0,0 +1,27 @@
+package com.keepit.common.store
+
+trait ObjectStore[A, B, +This <: ObjectStore[A, B, This]] {
+
+  /** Adds a new key/value pair to this map.
+   *  If the map already contains a
+   *  mapping for the key, it will be overridden by the new value.
+   *  @param    kv the key/value pair.
+   *  @return   the map itself
+   */
+  def += (kv: (A, B)): this.type
+  
+  /** Removes a key from this map.
+   *  @param    key the key to be removed
+   *  @return   the map itself.
+   */
+  def -= (key: A): this.type
+
+  /** Optionally returns the value associated with a key.
+   *
+   *  @param  key    the key value
+   *  @return an option value containing the value associated with `key` in this map,
+   *          or `None` if none exists.
+   */
+  def get(key: A): Option[B]
+  
+}
