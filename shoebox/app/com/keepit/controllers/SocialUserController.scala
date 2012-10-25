422daba760e24855e1b0225b2f0866d5d4800efb
Andrew Conner <git@andrewconner.org>
2012-10-25 08:42:50 -0700

    CSS moved to own file for maintainability. Added ability to refresh all user's social networks, and specific ones (on the social page).

--- shoebox/app/com/keepit/controllers/SocialUserController.scala
+++ shoebox/app/com/keepit/controllers/SocialUserController.scala
@@ -52,15 +52,11 @@ object SocialUserController extends Controller with Logging with SecureSocial {
     Ok(views.html.socialUsers(socialUsers))
   }
   
-  def refreshSocialInfo(userId: Id[User]) = SecuredAction(false) { implicit request => 
-    val socialUserInfos = CX.withConnection { implicit c => 
-      val user = User.get(userId)
-      SocialUserInfo.getByUser(user.id.get)
-    }
+  def refreshSocialInfo(socialUserInfoId: Id[SocialUserInfo]) = SecuredAction(false) { implicit request => 
     val graph = inject[SocialGraphPlugin]
-    socialUserInfos foreach { info =>
-      graph.asyncFetch(info)
-    }
-    Redirect(com.keepit.controllers.routes.UserController.userView(userId))
+    CX.withConnection { implicit conn =>
+      graph.asyncFetch(SocialUserInfo.get(socialUserInfoId)) 
+    }    
+    Redirect(com.keepit.controllers.routes.SocialUserController.socialUserView(socialUserInfoId))
   }
 }
