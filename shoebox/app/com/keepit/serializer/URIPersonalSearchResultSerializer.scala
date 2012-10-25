b5a75b53b54c5136e6ec8cf040bddee6f9359616
Eishay Smith <eishay@gmail.com>
2012-10-25 12:49:21 -0700

    exception logging

--- shoebox/app/com/keepit/serializer/URIPersonalSearchResultSerializer.scala
+++ shoebox/app/com/keepit/serializer/URIPersonalSearchResultSerializer.scala
@@ -1,22 +1,28 @@
 package com.keepit.serializer
 
+import com.keepit.common.logging.Logging
 import com.keepit.common.db.ExternalId
 import com.keepit.common.time._
 import com.keepit.controllers._
-
 import play.api.libs.json._
 
-class URIPersonalSearchResultSerializer extends Writes[PersonalSearchResult] {
+class URIPersonalSearchResultSerializer extends Writes[PersonalSearchResult] with Logging {
   //case class BookmarkPersonalSearchResult(bookmark: Bookmark, count: Int, users: Seq[User], score: Float)
   def writes(res: PersonalSearchResult): JsValue =
-    JsObject(List(
-      "count"  -> JsString(res.count.toString()),
-      "bookmark" -> NormalizedURISerializer.normalizedURISerializer.writes(res.uri),
-      "score" -> JsNumber(res.score),
-      "users" -> UserWithSocialSerializer.userWithSocialSerializer.writes(res.users),
-      "isMyBookmark" -> JsBoolean(res.isMyBookmark),
-      "isPrivate" -> JsBoolean(res.isPrivate)
-    ))
+    try {
+      JsObject(List(
+        "count"  -> JsString(res.count.toString()),
+        "bookmark" -> NormalizedURISerializer.normalizedURISerializer.writes(res.uri),
+        "users" -> UserWithSocialSerializer.userWithSocialSerializer.writes(res.users),
+        "score" -> JsNumber(res.score),
+        "isMyBookmark" -> JsBoolean(res.isMyBookmark),
+        "isPrivate" -> JsBoolean(res.isPrivate)
+      ))
+    } catch {
+      case e =>
+        log.error("can't serialize %s".format(res))
+        throw e
+    }
 
   def writes (ress: Seq[PersonalSearchResult]): JsValue = 
     JsArray(ress map { res => 
