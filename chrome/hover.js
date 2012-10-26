b2f8cd5e04d49cf5ad4af71259fedfc443ddfb37
Andrew Conner <git@andrewconner.org>
2012-10-26 12:08:11 -0700

    CSS normalization, UI improvements

--- chrome/hover.js
+++ chrome/hover.js
@@ -26,24 +26,79 @@ console.log("injecting keep it hover div");
 
             console.log('Rendering Mustache.js hover template...');
 
-            var tmpl = {
-              "logo": logo,
-              "arrow": arrow,
-              "profilepic": facebookImageLink
-            }
-            
-            tmpl.socialConnections = "Yo!"
+            $.get("http://" + config.server + "/users/keepurl?url=" + encodeURIComponent(document.location.href),
+                null,
+                function(users) {
 
+                  var tmpl = {
+                    "logo": logo,
+                    "arrow": arrow,
+                    "profilepic": facebookImageLink
+                  }
 
-            var tb = Mustache.to_html(
-                req.responseText,
-                tmpl
+                  console.log("got "+users.length+" result from /users/keepUrl");
+                  console.log(users);
+
+                  if (users.length>0) {
+                    var summary = "";
+                    if (users.length == 1) {
+                      summary="One of your friends";
+                    } else {
+                      summary = users.length+" other friends";
+                    }
+                    tmpl.socialConnections = {
+                      countText: summary,
+                      friends: users
+                    }
+                  }
+
+                  var tb = Mustache.to_html(
+                      req.responseText,
+                      tmpl
+                  );
+
+                  // Binders
+                  $('body').prepend(tb);
+                  $('.close').click(function() {
+                    slideOut();
+                  });
+                  $('.profilepic').click(function() { location=facebookProfileLink; });
+
+                  $('.keepitbtn').click(function() {
+                    console.log("bookmarking page: " + document.location.href);
+                    var request = {
+                      type: "add_bookmarks", 
+                      url: document.location.href, 
+                      title: document.title, 
+                      private: $("#keepit_private").is(":checked")
+                    }
+                    chrome.extension.sendRequest(request, function(response) {
+                      console.log("bookmark added! -> " + JSON.stringify(response));
+                      slideOut();
+                   });
+                  });
+
+                  $('.dropdownbtn').click(function() {
+                    $('.moreinnerbox').slideToggle(150);
+                  });
+
+                  /*$("#keep_summary").html("<span class='keep_summary_friends'>"+summary+"</span></br>choose to keep this bookmark");
+                  var faces = $("#keep_face");
+                  $(users).each(function(index, user){
+                    if(user.facebookId) {
+                      var img =  $("<a href='#'><img class='keep_face' src='https://graph.facebook.com/" + user.facebookId + "/picture?type=square'></a>");
+                      faces.append(img);
+                      img.click(function() {
+                        chatWith(user);
+                      });
+                    } else { //facebook id is missing for some reason!
+                      var img =  $("<img class='keep_face' src='/assets/images/missing_user.jpg'");
+                      faces.append(img);
+                    }
+                  });*/
+                },
+                "json"
             );
-            $('body').append(tb);
-            $('.close').click(function() {
-              slideOut();
-            });
-            $('.profilepic').click(function() { location=facebookProfileLink; });
         }
     };
     req.send(null);
@@ -102,7 +157,7 @@ console.log("injecting keep it hover div");
         opacity: 0,
         right: '-=100'
       },
-      500, function() {
+      300, function() {
         $('.kifi_hover').detach();
       });
   }
@@ -194,8 +249,9 @@ console.log("injecting keep it hover div");
 
   chrome.extension.sendRequest({"type": "get_conf"}, function(response) {
     config = response;
+    console.log("user config",response);
     getUserInfo(showBookmarkHover);
-    getUsersKeptThisUrl();
+
   });
 
 })();
