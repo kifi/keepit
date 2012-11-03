80390c35a0e9fe4526b814dff580e3170544a512
Eishay Smith <eishay@gmail.com>
2012-11-02 17:07:58 -0700

    some quotes

---
+++ shoebox/test/com/keepit/common/admin/DouglasAdamsQuotessTest.scala
@@ -0,0 +1,28 @@
+package com.keepit.common.admin
+
+import com.keepit.model.NormalizedURI.States._
+import com.keepit.common.time._
+import com.keepit.model._
+import org.junit.runner.RunWith
+import org.specs2.mutable._
+import org.specs2.runner.JUnitRunner
+import play.api.test._
+import play.api.test.Helpers._
+import scala.math._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import com.keepit.test.EmptyApplication
+
+@RunWith(classOf[JUnitRunner])
+class DouglasAdamsQuotesTest extends SpecificationWithJUnit {
+
+  "DouglasAdamsQuotes" should {
+    "load" in {
+      running(new EmptyApplication()) {
+        DouglasAdamsQuotes.qoutes.size === 503
+        DouglasAdamsQuotes.random.quote.size !== 0
+      }
+    }
+    
+  }
+}
