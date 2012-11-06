322ced2d9e7fbab1864d51a90a58a8f2218ff2a9
Eishay Smith <eishay@gmail.com>
2012-11-05 21:32:56 -0800

    fixing the sql patch

--- shoebox/conf/evolutions/shoebox/9.sql
+++ shoebox/conf/evolutions/shoebox/9.sql
@@ -1,25 +1,25 @@
 # --- !Ups
 
 alter TABLE article_search_result
-    create column last varchar(36) NOT NULL;
+    add column last varchar(36) NOT NULL;
     
 alter TABLE article_search_result
-    create column my_total INTEGER NOT NULL;
+    add column my_total INTEGER NOT NULL;
 
 alter TABLE article_search_result
-    create column friends_total INTEGER NOT NULL;
+    add column friends_total INTEGER NOT NULL;
 
 alter TABLE article_search_result
-    create column may_have_more_hits BOOLEAN NOT NULL;
+    add column may_have_more_hits BOOLEAN NOT NULL;
 
 alter TABLE article_search_result
-    create column millis_passed INTEGER NOT NULL;
+    add column millis_passed INTEGER NOT NULL;
 
 alter TABLE article_search_result
-    create column hit_count INTEGER NOT NULL;
+    add column hit_count INTEGER NOT NULL;
 
 alter TABLE article_search_result
-    create column page_number INTEGER NOT NULL;
+    add column page_number INTEGER NOT NULL;
     
 insert into evolutions (name, description) values('9.sql', 'adding more columns to the article search result table');
 
