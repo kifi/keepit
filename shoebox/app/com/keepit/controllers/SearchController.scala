abb387a4e1c4659efa46f5be64cfaa5778e540b8
Eishay Smith <eishay@gmail.com>
2012-11-06 12:43:36 -0800

    async persisting the search result

--- shoebox/app/com/keepit/controllers/SearchController.scala
+++ shoebox/app/com/keepit/controllers/SearchController.scala
@@ -40,6 +40,8 @@ import com.keepit.search.ArticleSearchResult
 import com.keepit.search.ArticleSearchResultRef
 import org.apache.commons.lang3.StringEscapeUtils
 import com.keepit.search.IdFilterCompressor
+import com.keepit.common.actor.ActorPlugin
+import com.keepit.search.ArticleSearchResultStore
 
 //note: users.size != count if some users has the bookmark marked as private
 case class PersonalSearchResult(uri: NormalizedURI, count: Int, isMyBookmark: Boolean, isPrivate: Boolean, users: Seq[UserWithSocial], score: Float)
@@ -78,8 +80,20 @@ object SearchController extends Controller with Logging {
     val searcher = new MainSearcher(userId, friendIds, filterOut, articleIndexer, uriGraph, config)
     val searchRes = searcher.search(term, maxHits, lastUUID)
     val res = toPersonalSearchResultPacket(searchRes)
+    reportArticleSearchResult(searchRes)
     Ok(RPS.resSerializer.writes(res)).as(ContentTypes.JSON)
   }
+  
+  private def reportArticleSearchResult(res: ArticleSearchResult) = try { 
+      inject[ActorPlugin].system.dispatcher.execute(new Runnable {
+        CX.withConnection { implicit c =>
+          ArticleSearchResultRef(res).save
+        }
+        inject[ArticleSearchResultStore] += (res.uuid -> res)
+      })
+    } catch {
+      case e => log.error("Could not report ArticleSearchResult %s".format(res))
+    }
   
   private[controllers] def toPersonalSearchResultPacket(res: ArticleSearchResult) = {
     val hits = CX.withConnection { implicit conn =>
