cb5b546e1d4802a148ece1bb04518c25777edc38
Eishay Smith <eishay@gmail.com>
2012-11-07 19:37:25 -0800

    display emails

--- shoebox/app/com/keepit/common/social/UserWithSocial.scala
+++ shoebox/app/com/keepit/common/social/UserWithSocial.scala
@@ -3,8 +3,9 @@ package com.keepit.common.social
 import java.sql.Connection
 import com.keepit.model.{User, SocialUserInfo}
 import com.keepit.model.Bookmark
+import com.keepit.model.EmailAddress
 
-case class UserWithSocial(user: User, socialUserInfo: SocialUserInfo, bookmarksCount: Long)
+case class UserWithSocial(user: User, socialUserInfo: SocialUserInfo, bookmarksCount: Long, emails: Seq[EmailAddress])
 
 object UserWithSocial {
   def toUserWithSocial(user: User)(implicit conn: Connection) = {
@@ -12,6 +13,7 @@ object UserWithSocial {
     if (socialInfos.size != 1) throw new Exception("Expected to have exactly one social info for user %s, got %s. All social infos are:".
         format(user, socialInfos, SocialUserInfo.all))
     val bookmarksCount = Bookmark.count(user)
-    UserWithSocial(user, socialInfos.head, bookmarksCount)
+    val emails = EmailAddress.getByUser(user.id.get)
+    UserWithSocial(user, socialInfos.head, bookmarksCount, emails)
   }
 }
