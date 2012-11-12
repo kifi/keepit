734ebd2155c602e0568f698c303cd7fe031315e9
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2012-11-12 10:51:30 -0800

    URI pattern matching, framework for normalizers/extractors extensibility

---
+++ shoebox/app/com/keepit/scraper/extractor/TikaBasedExtractor.scala
@@ -0,0 +1,50 @@
+package com.keepit.scraper.extractor
+
+import com.keepit.common.logging.Logging
+import com.keepit.common.net.URI
+import com.keepit.scraper.Scraper
+import org.apache.tika.detect.DefaultDetector
+import org.apache.tika.metadata.Metadata
+import org.apache.tika.parser.html.HtmlMapper
+import org.apache.tika.parser.AutoDetectParser
+import org.apache.tika.parser.ParseContext
+import org.apache.tika.parser.Parser
+import org.apache.tika.sax.ContentHandlerDecorator
+import org.apache.tika.sax.WriteOutContentHandler
+import org.xml.sax.Attributes
+import org.xml.sax.ContentHandler
+import org.xml.sax.SAXException
+import java.io.InputStream
+
+abstract class TikaBasedExtractor(url: String, maxContentChars: Int) extends Extractor with Logging {
+  
+  protected val output = new WriteOutContentHandler(maxContentChars)
+  protected val metadata = new Metadata()
+  
+  protected def getContentHandler: ContentHandler
+  protected def getParser: Parser = new AutoDetectParser(new DefaultDetector())
+  protected def getHtmlMapper: Option[HtmlMapper] = None
+  
+  def process(input: InputStream){
+    val context = new ParseContext()
+    var parser = getParser
+    val contentHandler = getContentHandler
+    context.set(classOf[Parser], parser)
+    getHtmlMapper.foreach(mapper => context.set(classOf[HtmlMapper], mapper))
+    
+    try {
+      parser.parse(input, contentHandler, metadata, context)
+    } catch {
+      case e =>
+        // check if we hit our content size limit (maxContentChars)
+        if (output.isWriteLimitReached(e))
+          log.warn("max number of characters reached: " + url)
+        else
+          log.error("extraction failed: ", e)
+    }
+  }
+  
+  def getContent() = output.toString
+  
+  def getMetadata(name: String) = Option(metadata.get(name))
+}
