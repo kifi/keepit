54e3759128b89899af5a1924c4c95b0ec5a7e884
Jared Jacobs <jared@42go.com>
2012-11-16 16:29:41 -0800

    fixing AdminDashboardControllerTest so it will not fail tomorrow

--- shoebox/app/com/keepit/controllers/admin/AdminDashboardController.scala
+++ shoebox/app/com/keepit/controllers/admin/AdminDashboardController.scala
@@ -39,7 +39,7 @@ object AdminDashboardController extends FortyTwoController {
     val dates = CX.withConnection { implicit conn => User.all }.map(_.createdAt.toLocalDateInZone)
     val day0 = dates.min
     val dayCounts = dates.foldLeft(Map[LocalDate,Int]().withDefaultValue(0)){(m, d) => m + (d -> (1 + m(d)))}
-    val userCounts = (0 to Days.daysBetween(day0, currentDate).getDays()) map {i => dayCounts(day0.plusDays(i))}
+    val userCounts = (0 to Days.daysBetween(day0, inject[LocalDate]).getDays()) map {i => dayCounts(day0.plusDays(i))}
     JsObject(List(
         "day0" -> day0.toJson,
         "userCounts" -> JsArray(userCounts map {i => JsNumber(i)})
--- shoebox/test/com/keepit/controllers/admin/AdminDashboardControllerTest.scala
+++ shoebox/test/com/keepit/controllers/admin/AdminDashboardControllerTest.scala
@@ -14,6 +14,7 @@ import play.api.libs.json.Json
 import play.api.libs.json.JsArray
 import play.api.libs.json.JsObject
 import play.api.libs.json.JsValue
+import com.keepit.inject._
 import com.keepit.common.social.SocialId
 import com.keepit.common.social.SocialNetworks.FACEBOOK
 import com.keepit.common.time._
@@ -21,6 +22,7 @@ import com.keepit.model.User
 import com.keepit.model.UserExperiment
 import com.keepit.model.UserExperiment.ExperimentTypes.ADMIN
 import com.keepit.model.SocialUserInfo
+import com.keepit.test.FakeClock
 import securesocial.core.SecureSocial
 import com.keepit.social.SecureSocialUserService
 import securesocial.core.UserService
@@ -28,21 +30,26 @@ import securesocial.core.OAuth2Info
 import securesocial.core.SocialUser
 import securesocial.core.UserId
 import securesocial.core.AuthenticationMethod
+import org.joda.time.LocalDate
+import org.joda.time.DateTime
 
 @RunWith(classOf[JUnitRunner])
 class AdminDashboardControllerTest extends SpecificationWithJUnit {
 
   "AdminDashboardController" should {
-
     "get users by date as JSON" in {
-      running(new EmptyApplication()) {
+      running(new EmptyApplication().withFakeClock()) {
         new SecureSocialUserService(current).onStart()
         UserService.delegate.isDefined === true
 
+        val now = new DateTime(2012, 5, 31, 4, 3, 2, 1, DEFAULT_DATE_TIME_ZONE)
+        val today = now.toDateTime
+        inject[FakeClock].push(today)
+
         val u1 = CX.withConnection { implicit c =>
-          val u1 = User(createdAt = currentDateTime.minusDays(3), firstName = "A", lastName = "1").save
-          val u2 = User(createdAt = currentDateTime.minusDays(1), firstName = "B", lastName = "2").save
-          val u3 = User(createdAt = currentDateTime.minusDays(1), firstName = "C", lastName = "3").save
+          val u1 = User(createdAt = now.minusDays(3), firstName = "A", lastName = "1").save
+          val u2 = User(createdAt = now.minusDays(1), firstName = "B", lastName = "2").save
+          val u3 = User(createdAt = now.minusDays(1), firstName = "C", lastName = "3").save
 
           val oAuth2Info = OAuth2Info(accessToken = "AAAHiW1ZC8SzYBAOtjXeZBivJ77eNZCIjXOkkZAZBjfLbaP4w0uPnj0XzXQUi6ib8m9eZBlHBBxmzzFbEn7jrZADmHQ1gO05AkSZBsZAA43RZC9dQZDZD",
             tokenType = None, expiresIn = None, refreshToken = None)
@@ -64,7 +71,7 @@ class AdminDashboardControllerTest extends SpecificationWithJUnit {
         status(result) must equalTo(OK)
         contentType(result) must beSome("application/json")
         charset(result) must beSome("utf-8")
-        contentAsString(result) === "{\"day0\":\"2012-11-13\",\"userCounts\":[1,0,2,0]}"
+        Json.parse(contentAsString(result)) === Json.parse("{\"day0\":\"2012-05-28\",\"userCounts\":[1,0,2,0]}")
       }
     }
   }
