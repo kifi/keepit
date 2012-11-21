505c3f24f968277548935996ad6812fd004e576f
Eishay Smith <eishay@gmail.com>
2012-11-20 18:18:13 -0800

    sending mails

--- shoebox/app/com/keepit/common/mail/MailSender.scala
+++ shoebox/app/com/keepit/common/mail/MailSender.scala
@@ -15,22 +15,26 @@ import com.keepit.common.db.CX
 import akka.actor.ActorSystem
 import akka.actor.Actor
 import akka.actor.Props
-import java.util.concurrent.TimeUnit
 import akka.actor.Props
 import akka.util.duration._
 import akka.actor.ActorRef
 import akka.actor.Cancellable
 import com.google.inject.Provider
-import com.keepit.common.net.HttpClient
 import play.api.libs.concurrent.Promise
 import com.keepit.common.net.ClientResponse
+import java.util.concurrent.TimeUnit
+import java.util.Properties
+import javax.mail._
+import javax.mail.internet._
+import javax.mail.event._
+import javax.mail.{Authenticator, PasswordAuthentication}
 
-class MailSender(system: ActorSystem, postmarkUrl: String, postmarkToken: String, healthcheck: Healthcheck, httpClient: HttpClient)
+class MailSender(system: ActorSystem, postmarkUrl: String, postmarkToken: String, healthcheck: Healthcheck)
   extends Logging with Plugin {
 
   private def processMail(mail: ElectronicMail) = actor ! ProcessMail(mail, this)
 
-  private val actor = system.actorOf(Props { new MailSenderActor(httpClient) })
+  private val actor = system.actorOf(Props { new MailSenderActor() })
   // plugin lifecycle methods
   private var _cancellables: Seq[Cancellable] = Nil
   override def enabled: Boolean = true
@@ -52,32 +56,86 @@ class MailSender(system: ActorSystem, postmarkUrl: String, postmarkToken: String
     }
   }
 
-  /**
-   * To test send from curl use the following command:
-   * $ curl -iv -H "Accept:application/json" -H "Content-Type:application/json" -H "X-Postmark-Server-Token:61311d22-d1cc-400b-865e-ffb95027251f" -d '{"From":"eng@keepit.com", "To":"eishay@gmail.com", "Subject": "Test from curl", "HtmlBody": "Some body here"}' https://api.postmarkapp.com/email; echo
-   */
-  private[mail] def sendMailToPostmark(mail: ElectronicMail, httpClient: HttpClient): ElectronicMail = try {
-    val json: JsValue = JsObject(Seq(
-          "From" -> JsString(mail.from.address),
-          "To" -> JsString(mail.to.address),
-          "Subject" -> JsString(mail.subject),
-          "HtmlBody" -> JsString(mail.htmlBody)
-         ))
-    val future: Promise[ElectronicMail] = httpClient.longTimeout().withHeaders(
-          "Accept" -> ContentTypes.JSON,
-          "Content-Type" -> ContentTypes.JSON,
-          "X-Postmark-Server-Token" -> postmarkToken
-        ).postPromise(postmarkUrl, json).map { response =>
-          parsePostmarkResponse(mail, response)
+  private class SMTPAuthenticator extends Authenticator {
+    override def getPasswordAuthentication(): PasswordAuthentication = {
+      val username = "fortytwo"//load from conf
+      val password = "keepemailsrunning"
+      return new PasswordAuthentication(username, password)
+    }
+  }
+
+  def sendMailToSendgrid(mail: ElectronicMail): Unit = {
+    val props = new Properties()
+    props.put("mail.transport.protocol", "smtp")
+    props.put("mail.smtp.host", "smtp.sendgrid.net")
+    props.put("mail.smtp.port", 587.asInstanceOf[AnyRef])
+    props.put("mail.smtp.auth", "true")
+
+    val auth = new SMTPAuthenticator()
+    val mailSession = Session.getDefaultInstance(props, auth)
+    // uncomment for debugging infos to stdout
+    mailSession.setDebug(true)
+
+    val transport = mailSession.getTransport()
+    val message = new MimeMessage(mailSession)
+    val multipart = new MimeMultipart("alternative")
+
+    val part1 = new MimeBodyPart()
+    part1.setText(mail.textBody.getOrElse(""))
+
+    val part2 = new MimeBodyPart()
+    part2.setContent(mail.htmlBody, ContentTypes.HTML)
+
+    multipart.addBodyPart(part1)
+    multipart.addBodyPart(part2)
+
+    message.setContent(multipart)
+    message.setFrom(new InternetAddress(mail.from.address))
+    message.setSubject(mail.subject)
+    message.addRecipient(Message.RecipientType.TO, new InternetAddress(mail.to.address))
+
+    transport.addTransportListener(new TransportListener() {
+      def messageDelivered(e: TransportEvent): Unit = {
+        log.info(e)
+        CX.withConnection { implicit c =>
+          mail.sent(e.toString()).save
         }
-    future.await(1, TimeUnit.MINUTES).get
-  } catch {
-    case e =>
-      val error = healthcheck.addError(HealthcheckError(error = Some(e), callType = Healthcheck.EMAIL,
-        errorMessage = Some("Can't send email from %s to %s: %s".format(mail.from, mail.to, mail.subject))))
-      CX.withConnection { implicit c =>
-        mail.errorSending("Error: %s".format(error)).save
       }
+      def messageNotDelivered(e: TransportEvent): Unit = {
+        log.error(e)
+        mailError(mail, e.toString())
+      }
+      def messagePartiallyDelivered(e: TransportEvent): Unit = {
+        log.error(e)
+        mailError(mail, e.toString())
+      }
+    })
+
+    transport.addConnectionListener(new ConnectionListener() {
+      def opened(e: ConnectionEvent)  { log.info(e) }
+      def closed(e: ConnectionEvent) { log.info(e) }
+      def disconnected(e: ConnectionEvent) { log.info(e) }
+    })
+
+    transport.connect()
+    try {
+      transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO))
+    } catch {
+      case e =>
+        log.error(e)
+        mailError(mail, e.toString())
+    } finally {
+      transport.close()
+    }
+
+  }
+
+  private def mailError(mail: ElectronicMail, message: String): ElectronicMail = {
+    val error = healthcheck.addError(HealthcheckError(callType = Healthcheck.EMAIL,
+      errorMessage = Some("Can't send email from %s to %s: %s. Error message: %s".format(mail.from, mail.to, mail.subject, message))))
+    CX.withConnection { implicit c =>
+      mail.errorSending("Error: %s".format(error)).save
+    }
   }
 
   /**
@@ -106,11 +164,11 @@ class MailSender(system: ActorSystem, postmarkUrl: String, postmarkToken: String
 private[mail] case class ProcessOutbox(sender: MailSender)
 private[mail] case class ProcessMail(mail: ElectronicMail, sender: MailSender)
 
-private[mail] class MailSenderActor(httpClient: HttpClient) extends Actor with Logging {
+private[mail] class MailSenderActor() extends Actor with Logging {
 
   def receive() = {
     case ProcessOutbox(sender) => sender.processOutbox()
-    case ProcessMail(mail, sender) => sender.sendMailToPostmark(mail, httpClient)
+    case ProcessMail(mail, sender) => sender.sendMailToSendgrid(mail)
     case unknown => throw new Exception("unknown message: %s".format(unknown))
   }
 }
