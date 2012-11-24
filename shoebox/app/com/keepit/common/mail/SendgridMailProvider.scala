af233743332a82d12aff994c90b6129056e76f8f
Eishay Smith <eishay@gmail.com>
2012-11-23 22:59:33 -0800

    keeping a live transport and checking for fault issues

--- shoebox/app/com/keepit/common/mail/SendgridMailProvider.scala
+++ shoebox/app/com/keepit/common/mail/SendgridMailProvider.scala
@@ -2,6 +2,7 @@ package com.keepit.common.mail
 
 import com.keepit.common.logging.Logging
 import com.keepit.common.db.CX
+import com.keepit.common.db.ExternalId
 import com.keepit.common.healthcheck.Healthcheck
 import com.keepit.common.healthcheck.HealthcheckError
 import com.keepit.inject._
@@ -12,13 +13,18 @@ import javax.mail.internet._
 import javax.mail.event._
 import javax.mail.{Authenticator, PasswordAuthentication}
 
-import com.google.inject.Inject
+import com.google.inject.{Inject, Singleton}
 
 import play.api.libs.json._
 
 import play.api.Play.current
 import play.api.http.ContentTypes
 
+object SendgridMailProvider {
+  val KIFI_MAIL_ID = "kifi-mail-id"
+}
+
+@Singleton
 class SendgridMailProvider @Inject() () extends Logging {
 
   private class SMTPAuthenticator extends Authenticator {
@@ -29,10 +35,7 @@ class SendgridMailProvider @Inject() () extends Logging {
     }
   }
 
-  /**
-   * Please see http://sendgrid.com/docs/API%20Reference/SMTP%20API/index.html for docs
-   */
-  def sendMailToSendgrid(mail: ElectronicMail): Unit = {
+  lazy val mailSession: Session = {
     val props = new Properties()
     props.put("mail.transport.protocol", "smtp")
     props.put("mail.smtp.host", "smtp.sendgrid.net")
@@ -41,10 +44,88 @@ class SendgridMailProvider @Inject() () extends Logging {
 
     val auth = new SMTPAuthenticator()
     val mailSession = Session.getDefaultInstance(props, auth)
-    // uncomment for debugging infos to stdout
+//    mailSession.setDebug(log.isDebugEnabled)
     mailSession.setDebug(true)
+    mailSession
+  }
 
+  def nullifyTransport(transport: Transport) = if (transportOpt.map(t => t == transport).getOrElse(false)) {
+    log.info("setting transportOpt to None since it contains a bad state transport")
+    if (transport.isConnected()) transport.close()
+    transportOpt = None
+  }
+
+  def createTransport(): Transport = {
     val transport = mailSession.getTransport()
+    def externalIdFromTransportEvent(e: TransportEvent) =
+      ExternalId[ElectronicMail](e.getMessage().getHeader(SendgridMailProvider.KIFI_MAIL_ID)(0))
+
+    transport.addTransportListener(new TransportListener() {
+      def messageDelivered(e: TransportEvent): Unit = {
+        log.info("messageDelivered: %s".format(e))
+        CX.withConnection { implicit c =>
+          ElectronicMail.get(externalIdFromTransportEvent(e)).sent("transport.messageDelivered").save
+        }
+      }
+      def messageNotDelivered(e: TransportEvent): Unit = {
+        mailError(externalIdFromTransportEvent(e), "transport.messageNotDelivered", transport)
+      }
+      def messagePartiallyDelivered(e: TransportEvent): Unit = {
+        mailError(externalIdFromTransportEvent(e), "transport.messagePartiallyDelivered", transport)
+      }
+    })
+
+    transport.addConnectionListener(new ConnectionListener() {
+      def opened(e: ConnectionEvent)  { log.info(e) }
+      def closed(e: ConnectionEvent) {
+        log.info("got event %s".format(e))
+        nullifyTransport(transport)
+      }
+      def disconnected(e: ConnectionEvent) {
+        closed(e)
+      }
+    })
+
+    transport.connect()
+    transport
+  }
+
+  var transportOpt: Option[Transport] = Some(createTransport())
+
+  def getLiveTransport(): Transport = transportOpt match {
+    case Some(transport) =>
+      if (transport.isConnected()) {
+        transport
+      } else {
+        nullifyTransport(transport)
+        transportOpt = Some(createTransport())
+        transportOpt.get
+      }
+    case _ =>
+      transportOpt = Some(createTransport())
+      transportOpt.get
+  }
+
+  /**
+   * Please see http://sendgrid.com/docs/API%20Reference/SMTP%20API/index.html for docs
+   */
+  def sendMailToSendgrid(mail: ElectronicMail): Unit = {
+    val message = createMessage(mail)
+    val transport = getLiveTransport()
+    try {
+      transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO))
+      CX.withConnection { implicit c =>
+        mail.sent("message sent").save
+      }
+      log.info("mail %s sent".format(mail.externalId))
+    } catch {
+      case e =>
+        log.error(e)
+        mailError(mail, e.toString(), transport)
+    }
+  }
+
+  private def createMessage(mail: ElectronicMail) = {
     val message = new MimeMessage(mailSession)
     val multipart = new MimeMultipart("alternative")
 
@@ -63,50 +144,21 @@ class SendgridMailProvider @Inject() () extends Logging {
     message.setFrom(new InternetAddress(mail.from.address))
     message.setSubject(mail.subject)
     message.addRecipient(Message.RecipientType.TO, new InternetAddress(mail.to.address))
-
-    transport.addTransportListener(new TransportListener() {
-      def messageDelivered(e: TransportEvent): Unit = {
-        log.info(e)
-        CX.withConnection { implicit c =>
-          mail.sent(e.toString()).save
-        }
-      }
-      def messageNotDelivered(e: TransportEvent): Unit = {
-        log.error(e)
-        mailError(mail, e.toString())
-      }
-      def messagePartiallyDelivered(e: TransportEvent): Unit = {
-        log.error(e)
-        mailError(mail, e.toString())
-      }
-    })
-
-    transport.addConnectionListener(new ConnectionListener() {
-      def opened(e: ConnectionEvent)  { log.info(e) }
-      def closed(e: ConnectionEvent) { log.info(e) }
-      def disconnected(e: ConnectionEvent) { log.info(e) }
-    })
-
-    transport.connect()
-    try {
-      transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO))
-      CX.withConnection { implicit c =>
-        mail.sent("message sent").save
-      }
-    } catch {
-      case e =>
-        log.error(e)
-        mailError(mail, e.toString())
-    } finally {
-      transport.close()
-      log.info("mail %s sent".format(mail.externalId))
-    }
-
+    message
   }
 
-  private def mailError(mail: ElectronicMail, message: String): ElectronicMail = {
+  private def mailError(mailId: ExternalId[ElectronicMail], message: String, transport: Transport): ElectronicMail = {
+    val mail = CX.withConnection { implicit c =>
+      ElectronicMail.get(mailId)
+    }
+    mailError(mail, message, transport)
+  }
+
+  private def mailError(mail: ElectronicMail, message: String, transport: Transport): ElectronicMail = {
+    nullifyTransport(transport)
     val error = inject[Healthcheck].addError(HealthcheckError(callType = Healthcheck.EMAIL,
       errorMessage = Some("Can't send email from %s to %s: %s. Error message: %s".format(mail.from, mail.to, mail.subject, message))))
+    log.error(error.errorMessage)
     CX.withConnection { implicit c =>
       mail.errorSending("Error: %s".format(error)).save
     }
