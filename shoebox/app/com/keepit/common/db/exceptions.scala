014820a8cbc8eb24d74f7003a0c5be623c8cbbbf
Jared Jacobs <jared@42go.com>
2012-11-26 15:51:30 -0800

    Follow model

--- shoebox/app/com/keepit/common/db/exceptions.scala
+++ shoebox/app/com/keepit/common/db/exceptions.scala
@@ -1,18 +1,23 @@
 package com.keepit.common.db
 
-class ElementWithExternalIdNotFoundException[T](id: ExternalId[T], m: Manifest[T])
-  extends NoSuchElementException("can't find external id %s for element of type %s".format(id.toString(), m.toString()))
+class ElementWithExternalIdNotFoundException[T](m: Manifest[T], id: ExternalId[T])
+  extends NoSuchElementException("can't find %s with external id %s".format(m, id))
 
-class ElementWithInternalIdNotFoundException[T](id: Id[T], m: Manifest[T])
-  extends NoSuchElementException("can't find internal id %s for element of type %s".format(id.toString(), m.toString()))
+class ElementWithInternalIdNotFoundException[T](m: Manifest[T], id: Id[T])
+  extends NoSuchElementException("can't find %s with internal id %s".format(m, id))
 
+class ElementWithInternalIdsNotFoundException[T,U,V](c: Class[T], m1: Manifest[U], id1: Id[U], m2: Manifest[V], id2: Id[V])
+  extends NoSuchElementException("can't find %s with internal %s id %s and %s id %s".format(c.getName(), m1, id1, m2, id2))
 
 object NotFoundException {
 
   def apply[T](id: ExternalId[T])(implicit m: Manifest[T]) =
-    new ElementWithExternalIdNotFoundException(id, m)
+    new ElementWithExternalIdNotFoundException(m, id)
 
   def apply[T](id: Id[T])(implicit m: Manifest[T]) =
-    new ElementWithInternalIdNotFoundException(id, m)
+    new ElementWithInternalIdNotFoundException(m, id)
+
+  def apply[T,U,V](c: Class[T], id1: Id[U], id2: Id[V])(implicit m1: Manifest[U], m2: Manifest[V]) =
+    new ElementWithInternalIdsNotFoundException(c, m1, id1, m2, id2)
 
 }
