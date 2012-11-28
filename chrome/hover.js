d9d478cee17d20a67e116004000e75d7fd16966f
Andrew Conner <git@andrewconner.org>
2012-11-27 16:32:46 -0800

    Needs to pull from server

--- chrome/hover.js
+++ chrome/hover.js
@@ -455,21 +455,9 @@ console.log("[" + new Date().getTime() + "] ", "injecting keep it hover div");
     });
 
     if(type == "message") {
-      $("#to-list").tokenInput([
-            {id: 7, name: "Ruby"},
-            {id: 'dd11', name: "Python"},
-            {id: 13, name: "JavaScript"},
-            {id: 17, name: "ActionScript"},
-            {id: 19, name: "Scheme"},
-            {id: 23, name: "Lisp"},
-            {id: 29, name: "C#"},
-            {id: 31, name: "Fortran"},
-            {id: 37, name: "Visual Basic"},
-            {id: 41, name: "C"},
-            {id: 43, name: "C++"},
-            {id: 47, name: "Java"}
-        ], {
-        theme: "facebook"
+      var friends = []; //TODO!
+      $("#to-list").tokenInput(friends, {
+        theme: "kifi"
       });
       $("#token-input-to-list").keypress(function(e) {
         return e.which !== 13;
