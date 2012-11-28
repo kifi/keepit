5e2d64f9aed5ac828ca91ca9f742b331676ce64b
Eishay Smith <eishay@gmail.com>
2012-11-28 00:02:14 -0800

    typo fix, it should have been title

--- shoebox/conf/evolutions/shoebox/16.sql
+++ shoebox/conf/evolutions/shoebox/16.sql
@@ -1,7 +1,7 @@
 # --- !Ups
 
 alter table normalized_uri
-  alter column normalized_uri normalized_uri varchar(2048);
+  alter column title title varchar(2048);
 
 insert into evolutions (name, description) values('16.sql', 'title may be null in normalized_uri table');
 
