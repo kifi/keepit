81d292b0f1ad73c3bdc41fab4950904fe8dc2aed
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2012-11-29 11:09:01 -0800

    use bookmark's title for my bookmark hit

--- shoebox/app/com/keepit/controllers/HomeController.scala
+++ shoebox/app/com/keepit/controllers/HomeController.scala
@@ -7,25 +7,9 @@ import play.api.data.Forms._
 import play.api.data.validation.Constraints._
 import play.api.libs.ws.WS
 import play.api.mvc._
-import play.api.libs.json.JsArray
-import play.api.libs.json.Json
-import play.api.libs.json.JsObject
-import play.api.libs.json.JsString
-import play.api.libs.json.JsValue
-import play.api.libs.json.JsNumber
-import play.api.libs.json.JsArray
 import play.api.http.ContentTypes
 import com.keepit.controllers.CommonActions._
-import com.keepit.common.db.CX
-import com.keepit.common.db._
-import com.keepit.model._
-import com.keepit.serializer.BookmarkSerializer
-import com.keepit.serializer.{URIPersonalSearchResultSerializer => BPSRS}
 import com.keepit.common.db.ExternalId
-import java.util.concurrent.TimeUnit
-import java.sql.Connection
-import play.api.http.ContentTypes
-import play.api.libs.json.JsString
 import com.keepit.common.logging.Logging
 import com.keepit.common.controller.FortyTwoController
 
