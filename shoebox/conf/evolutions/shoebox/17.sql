925713a774e588e88a730262a87811f5ae9e73c4
yonatanm <yonatanm@gmail.com>
2012-12-01 18:15:15 +0200

    conflicts with evolutions script

--- shoebox/conf/evolutions/shoebox/17.sql
+++ shoebox/conf/evolutions/shoebox/17.sql
@@ -1,8 +1,8 @@
 # --- !Ups
 
-alter table social_user_info 
-  add column lastGraphRefresh datetime NULL; 
-             
-insert into evolutions (name, description) values('17.sql', 'adding social_user_info lastGraphRefresh');
+alter table electronic_mail
+  add column from_name varchar(256);
+
+insert into evolutions (name, description) values('17.sql', 'adding from_name to emails');
 
 # --- !Downs
