4c9c0de96e9cf36a3ef695e3e3fea65c6cb0bac5
Andrew Conner <git@andrewconner.org>
2012-12-09 22:50:52 -0800

    No more Manifest

--- shoebox/app/com/keepit/inject/package.scala
+++ shoebox/app/com/keepit/inject/package.scala
@@ -6,17 +6,20 @@ import com.google.inject.Key
 import com.google.inject.Provider
 import com.google.inject.TypeLiteral
 import com.google.inject.util.Types
+import reflect.{ClassTag, classTag}
+import scala.language.implicitConversions
+
 
 package object inject {
   class RichInjector(val injector: Injector) {
-    def inject[A](implicit m: Manifest[A]): A = {
+    def inject[A](implicit m: ClassTag[A]): A = {
       m.typeArguments match {
         case Nil => injector.getInstance(m.runtimeClass.asInstanceOf[Class[A]])
         case _   => injector.getInstance(key(m))
       }
     }
 
-    private def key[A](implicit a: Manifest[A]): Key[A] = {
+    private def key[A](implicit a: ClassTag[A]): Key[A] = {
       val targs = a.typeArguments.map(_.runtimeClass)
       Key.get(Types.newParameterizedType(a.runtimeClass, targs:_*)).asInstanceOf[Key[A]]
     }
@@ -24,7 +27,7 @@ package object inject {
 
   implicit def richInjector(injector: Injector): RichInjector = new RichInjector(injector)
 
-  def inject[A](implicit m: Manifest[A], app: Application): A = app.global.asInstanceOf[FortyTwoGlobal].injector.inject[A]
+  def inject[A](implicit m: ClassTag[A], app: Application): A = app.global.asInstanceOf[FortyTwoGlobal].injector.inject[A]
 
   def provide[T](func: => T): Provider[T] = new Provider[T] { def get = func }
 }
