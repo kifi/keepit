b5da99792821ae86a608beb4637cbfa077179cc9
yonatanm <yonatanm@gmail.com>
2012-12-12 23:12:21 +0200

    adding test

--- shoebox/test/com/keepit/common/social/SocialUserCreateConnectionsTest.scala
+++ shoebox/test/com/keepit/common/social/SocialUserCreateConnectionsTest.scala
@@ -58,24 +58,23 @@ class SocialUserCreateConnectionsTest extends SpecificationWithJUnit {
     "disable non existing connecions" in {
       running(new EmptyApplication().withFakeStore) {
 
-        /*
-         * grab json
-         * import friends (create SocialUserInfo records)
-         * using json and one socialuserinfo, create connections
-         *
-         */
-        val json = Json.parse(io.Source.fromFile(new File("test/com/keepit/common/social/facebook_graph_eishay.json")).mkString)
+        val json1 = Json.parse(io.Source.fromFile(new File("test/com/keepit/common/social/facebook_graph_eishay.json")).mkString)
 
-        val result = inject[SocialUserImportFriends].importFriends(Seq(json))
+        inject[SocialUserImportFriends].importFriends(Seq(json1))
 
         val socialUserInfo = CX.withConnection { implicit conn =>
           SocialUserInfo(fullName = "Bob Smith", socialId = SocialId("bsmith"), networkType = SocialNetworks.FACEBOOK).withUser(User(firstName = "fn1", lastName = "ln1").save).save
         }
 
 
-        val connections = inject[SocialUserCreateConnections].createConnections(socialUserInfo, Seq(json))
+        val connections = inject[SocialUserCreateConnections].createConnections(socialUserInfo, Seq(json1))
 
         connections.size === 199
+        
+        val json2 = Json.parse(io.Source.fromFile(new File("test/com/keepit/common/social/facebook_graph_eishay_min.json")).mkString)
+
+        val connectionsAfter = inject[SocialUserCreateConnections].createConnections(socialUserInfo, Seq(json2))
+        connectionsAfter.size === 12
       }
     }
     
