f30a0826c7ab7d6d89f6d04f4a4b23d506b05e89
Andrew Conner <git@andrewconner.org>
2012-12-12 18:45:54 -0800

    Reversing Eishay :(

--- chrome/background.js
+++ chrome/background.js
@@ -410,6 +410,7 @@ function searchOnServer(request, sendResponse, tab) {
   xhr.open("GET",
    'http://' + userConfigs.server + '/search' +
       '?term=' + encodeURIComponent(request.query) +
+      '&externalId=' + userConfigs.user.keepit_external_id +
       '&maxHits=' + userConfigs.max_res * 2 +
       '&lastUUI=' + (request.lastUUID || "") +
       '&context=' + encodeURIComponent(request.context || "") +
@@ -528,6 +529,7 @@ function postBookmarks(supplyBookmarks, bookmarkSource) {
     xhr.open("POST", 'http://' + userConfigs.server + '/bookmarks/add', true);
     xhr.send(JSON.stringify({
       "bookmarks": bookmarks,
+      "user_info": userConfigs.user,
       "bookmark_source": bookmarkSource}));
     log("posted bookmarks");
   });
--- shoebox/app/com/keepit/controllers/BookmarksController.scala
+++ shoebox/app/com/keepit/controllers/BookmarksController.scala
@@ -150,7 +150,7 @@ object BookmarksController extends FortyTwoController {
   }
 
   def addBookmarks() = AuthenticatedJsonAction { request =>
-    val json = Json.parse(request.body.asText.get)
+    val json = request.body.asJson.get
     val bookmarkSource = (json \ "bookmark_source").asOpt[String]
     val user = CX.withConnection { implicit conn => User.get(request.userId) }
     log.info("adding bookmarks of user %s".format(user))
