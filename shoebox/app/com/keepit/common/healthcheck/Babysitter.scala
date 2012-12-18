e1f1496d0ebe3193aa3af72bfb74594eb3601d72
Eishay Smith <eishay@gmail.com>
2012-12-17 16:08:07 -0800

    log the time it took to complete the task and the health check it associated with it

--- shoebox/app/com/keepit/common/healthcheck/Babysitter.scala
+++ shoebox/app/com/keepit/common/healthcheck/Babysitter.scala
@@ -3,12 +3,13 @@ package com.keepit.common.healthcheck
 import play.api._
 import play.api.libs.concurrent.Akka
 import com.keepit.common.logging.Logging
-import play.api.libs.concurrent.Akka
-import akka.util.duration._
+import com.keepit.common.db.ExternalId
 import com.keepit.inject._
-import org.joda.time.DateTime
 import com.google.inject._
+import akka.util.duration._
 import akka.actor.Scheduler
+import org.joda.time.DateTime
+import java.util.concurrent.atomic.AtomicReference
 
 @ImplementedBy(classOf[BabysitterImpl])
 trait Babysitter {
@@ -21,9 +22,11 @@ class BabysitterImpl extends Babysitter with Logging {
   def watch[A](timeout: BabysitterTimeout)(block: => A)(implicit app: Application): A = {
     val startTime = inject[DateTime]
     val e = new Exception("Babysitter error timeout after %s millis".format(timeout.errorTimeout.toMillis))
+    val pointer = new AtomicReference[ExternalId[HealthcheckError]]()
     val babysitter = inject[Scheduler].scheduleOnce(timeout.errorTimeout) {
       log.error(e.getStackTrace() mkString "\n  ")
-      inject[HealthcheckPlugin].addError(HealthcheckError(Some(e), None, None, Healthcheck.INTERNAL, Some(e.getMessage())))
+      val error = inject[HealthcheckPlugin].addError(HealthcheckError(Some(e), None, None, Healthcheck.INTERNAL, Some(e.getMessage())))
+      pointer.set(error.id)
     }
     val result = try {
       block
@@ -31,8 +34,10 @@ class BabysitterImpl extends Babysitter with Logging {
     finally {
       val endTime = inject[DateTime]
       val difference = endTime.getMillis - endTime.getMillis
+      val healthcheckError = Option(pointer.get) map {he => "HealthcheckError %s".format(he)} getOrElse "No Healthcheck Error"
       if(difference > timeout.warnTimeout.toMillis) {
-        val e = new Exception("Babysitter error timeout after %s millis".format(timeout.errorTimeout.toMillis))
+        val e = new Exception("Babysitter timeout [%s]. Process took %s millis, timeout was set to %s millis".
+                                format(healthcheckError, difference, timeout.warnTimeout.toMillis))
         log.warn(e.getStackTrace() mkString "\n  ")
       }
       babysitter.cancel
