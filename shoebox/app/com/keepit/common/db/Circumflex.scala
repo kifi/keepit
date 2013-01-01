410446e8b08c1910f02e256bf4ce955e0ec4991c
Eishay Smith <eishay@gmail.com>
2012-12-31 22:17:33 -0800

    extracting id and external id from Circumflex file

--- shoebox/app/com/keepit/common/db/Circumflex.scala
+++ shoebox/app/com/keepit/common/db/Circumflex.scala
@@ -2,7 +2,6 @@ package com.keepit.common.db
 
 import java.io.BufferedReader
 import java.sql.{Clob, Connection, PreparedStatement, ResultSet, Timestamp, SQLException}
-import java.util.UUID
 import scala.util.control.ControlThrowable
 import org.joda.time.{DateTime, DateTimeZone}
 import com.keepit.common.logging.Logging
@@ -27,33 +26,6 @@ class CustomTypeConverter extends TypeConverter {
       case dt: DateTime => super.write(st, dt.toDate, paramIndex)
       case _ => super.write(st, parameter, paramIndex)
     }
-  }
-}
-
-case class Id[T](id: Long) {
-  override def toString = id.toString
-}
-
-object Id {
-  implicit def queryStringBinder[T](implicit longBinder: QueryStringBindable[Long]) = new QueryStringBindable[Id[T]] {
-    override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, Id[T]]] = {
-      longBinder.bind(key, params) map {
-        case Right(id) => Right(Id(id))
-        case _ => Left("Unable to bind an Id")
-      }
-    }
-    override def unbind(key: String, id: Id[T]): String = {
-      longBinder.unbind(key, id.id)
-    }
-  }
-
-  implicit def pathBinder[T](implicit longBinder: PathBindable[Long]) = new PathBindable[Id[T]] {
-    override def bind(key: String, value: String): Either[String, Id[T]] =
-      longBinder.bind(key, value) match {
-        case Right(id) => Right(Id(id))
-        case _ => Left("Unable to bind an Id")
-      }
-    override def unbind(key: String, id: Id[T]): String = id.id.toString
   }
 }
 
@@ -91,41 +63,6 @@ trait IdFields[R <: Record[_, R]] { self: R =>
   implicit def str2IdHelper(str: String): IdDefinitionHelper[R] =
     new IdDefinitionHelper(str, this)
 }
-
-
-case class ExternalId[T](id: String) {
-  if (!ExternalId.UUID_PATTERN.pattern.matcher(id).matches()) {
-    throw new Exception("external id [%s] does not match uuid pattern".format(id))
-  }
-  override def toString = id
-}
-
-object ExternalId {
-
-  val UUID_PATTERN = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$".r
-
-  implicit def queryStringBinder[T](implicit stringBinder: QueryStringBindable[String]) = new QueryStringBindable[ExternalId[T]] {
-    override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, ExternalId[T]]] = {
-      stringBinder.bind(key, params) map {
-        case Right(id) => Right(ExternalId(id))
-        case _ => Left("Unable to bind an ExternalId")
-      }
-    }
-    override def unbind(key: String, id: ExternalId[T]): String = {
-      stringBinder.unbind(key, id.id)
-    }
-  }
-
-  implicit def pathBinder[T] = new PathBindable[ExternalId[T]] {
-    override def bind(key: String, value: String): Either[String, ExternalId[T]] =
-      Right(ExternalId(value)) // TODO: handle errors if value is malformed
-
-    override def unbind(key: String, id: ExternalId[T]): String = id.toString
-  }
-
-  def apply[T](): ExternalId[T] = ExternalId(UUID.randomUUID.toString)
-}
-
 
 /** Field to store a type-safe string database identifier.
  * T is the referenced type
