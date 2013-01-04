98dad33373efbcc51f8dc2def9b16137a02a2ce0
Eishay Smith <eishay@gmail.com>
2013-01-04 01:05:05 -0800

    new db abstraction that is using slick/scala-query and using injectable components

--- shoebox/app/com/keepit/model/Follow.scala
+++ shoebox/app/com/keepit/model/Follow.scala
@@ -1,6 +1,5 @@
 package com.keepit.model
 
-import org.scalaquery.ql.extended.ExtendedTable
 import com.keepit.common.db.{CX, Id, Entity, EntityTable, ExternalId, State}
 import com.keepit.common.db.NotFoundException
 import com.keepit.common.db.StateException
@@ -15,14 +14,6 @@ import java.net.URI
 import java.security.MessageDigest
 import scala.collection.mutable
 import com.keepit.common.logging.Logging
-import com.keepit.common.db.slick.FortyTwoTypeMappers
-import com.keepit.common.db.slick.FortyTwoTypeMappers._
-import org.scalaquery.ql.SimpleExpression
-
-object FollowStates {
-  val ACTIVE = State[Follow]("active")
-  val INACTIVE = State[Follow]("inactive")
-}
 
 case class Follow (
   id: Option[Id[Follow]] = None,
@@ -30,56 +21,81 @@ case class Follow (
   updatedAt: DateTime = currentDateTime,
   userId: Id[User],
   uriId: Id[NormalizedURI],
-  state: State[Follow] = FollowStates.ACTIVE
-){
+  state: State[Follow] = Follow.States.ACTIVE
+) extends Logging {
 
-//  def save(implicit conn: Connection): Follow = throw new Exception
-//  def save(implicit conn: Connection): Follow = {
-//    log.info("saving new follow [user: %s, uri: %s]".format(userId, uriId))
-//    val entity = FollowEntity(this.copy(updatedAt = currentDateTime))
-//    assert(1 == entity.save())
-//    entity.view
-//  }
+  def save(implicit conn: Connection): Follow = {
+    log.info("saving new follow [user: %s, uri: %s]".format(userId, uriId))
+    val entity = FollowEntity(this.copy(updatedAt = currentDateTime))
+    assert(1 == entity.save())
+    entity.view
+  }
 
-  def activate = copy(state = FollowStates.ACTIVE)
-  def deactivate = copy(state = FollowStates.INACTIVE)
-  def isActive = state == FollowStates.ACTIVE
+  def activate = copy(state = Follow.States.ACTIVE)
+  def deactivate = copy(state = Follow.States.INACTIVE)
+  def isActive = state == Follow.States.ACTIVE
 }
 
-private[model] object FollowEntity extends ExtendedTable[Follow]("follow") {
-  def id =        column[Id[Follow]]("id", O.PrimaryKey, O.AutoInc)
-  def createdAt = column[DateTime]("created_at")
-  def updatedAt = column[DateTime]("updated_at")
-  def userId =    column[Id[User]]("user_id")
-  def uriId =     column[Id[NormalizedURI]]("uri_id")
-  def state =     column[State[Follow]]("state")
-
-  def * = id.? ~ createdAt ~ updatedAt ~ userId ~ uriId ~ state <> (Follow, Follow.unapply _)
-  def lastInsertedId: Id[Follow] = SimpleExpression.nullary[Id[Follow]]("scope_identity") // or LAST_INSERT_ID for mysql
-}
-
-class FollowRepo {
+object Follow {
 
   def all(implicit conn: Connection): Seq[Follow] =
-    throw new Exception //FollowEntity.all.map(_.view)
+    FollowEntity.all.map(_.view)
 
   def all(userId: Id[User])(implicit conn: Connection): Seq[Follow] =
-    throw new Exception //(FollowEntity AS "f").map { f => SELECT (f.*) FROM f WHERE (f.userId EQ userId) list }.map(_.view)
+    (FollowEntity AS "f").map { f => SELECT (f.*) FROM f WHERE (f.userId EQ userId) list }.map(_.view)
 
   def get(uriId: Id[NormalizedURI])(implicit conn: Connection): Seq[Follow] =
-    throw new Exception //(FollowEntity AS "f").map { f => SELECT (f.*) FROM f WHERE (f.uriId EQ uriId AND (f.state EQ FollowStates.ACTIVE)) list }.map(_.view)
+    (FollowEntity AS "f").map { f => SELECT (f.*) FROM f WHERE (f.uriId EQ uriId AND (f.state EQ Follow.States.ACTIVE)) list }.map(_.view)
 
   def get(id: Id[Follow])(implicit conn: Connection): Follow =
-    throw new Exception //FollowEntity.get(id).map(_.view).getOrElse(throw NotFoundException(id))
+    FollowEntity.get(id).map(_.view).getOrElse(throw NotFoundException(id))
 
   def get(userId: Id[User], uri: NormalizedURI)(implicit conn: Connection): Option[Follow] =
-    throw new Exception //get(userId, uri.id.get)
+    get(userId, uri.id.get)
 
   def get(userId: Id[User], uriId: Id[NormalizedURI])(implicit conn: Connection): Option[Follow] =
-    throw new Exception //(FollowEntity AS "f").map { f => SELECT (f.*) FROM f WHERE (f.userId EQ userId AND (f.uriId EQ uriId)) unique }.map(_.view)
+    (FollowEntity AS "f").map { f => SELECT (f.*) FROM f WHERE (f.userId EQ userId AND (f.uriId EQ uriId)) unique }.map(_.view)
 
   def getOrThrow(userId: Id[User], uriId: Id[NormalizedURI])(implicit conn: Connection): Follow =
-    throw new Exception //get(userId, uriId).getOrElse(throw NotFoundException(classOf[Follow], userId, uriId))
+    get(userId, uriId).getOrElse(throw NotFoundException(classOf[Follow], userId, uriId))
+
+  object States {
+    val ACTIVE = State[Follow]("active")
+    val INACTIVE = State[Follow]("inactive")
+  }
 
 }
 
+private[model] class FollowEntity extends Entity[Follow, FollowEntity] {
+  val createdAt = "created_at".JODA_TIMESTAMP.NOT_NULL(currentDateTime)
+  val updatedAt = "updated_at".JODA_TIMESTAMP.NOT_NULL(currentDateTime)
+  val userId = "user_id".ID[User].NOT_NULL
+  val uriId = "uri_id".ID[NormalizedURI].NOT_NULL
+  val state = "state".STATE[Follow].NOT_NULL(Follow.States.ACTIVE)
+
+  def relation = FollowEntity
+
+  def view(implicit conn: Connection): Follow = Follow(
+    id = id.value,
+    createdAt = createdAt(),
+    updatedAt = updatedAt(),
+    userId = userId(),
+    uriId = uriId(),
+    state = state()
+  )
+}
+
+private[model] object FollowEntity extends FollowEntity with EntityTable[Follow, FollowEntity] {
+  override def relationName = "follow"
+
+  def apply(view: Follow): FollowEntity = {
+    val uri = new FollowEntity
+    uri.id.set(view.id)
+    uri.createdAt := view.createdAt
+    uri.updatedAt := view.updatedAt
+    uri.userId := view.userId
+    uri.uriId := view.uriId
+    uri.state := view.state
+    uri
+  }
+}
