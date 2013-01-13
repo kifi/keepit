aa4aa4eb1debb4aa7fa2fd0eac2ce0811fa38a43
Eishay Smith <eishay@gmail.com>
2013-01-13 14:21:02 -0800

    small refactoring, simplify the api

--- shoebox/test/com/keepit/common/db/SlickTest.scala
+++ shoebox/test/com/keepit/common/db/SlickTest.scala
@@ -53,7 +53,7 @@ class SlickTest extends SpecificationWithJUnit {
             def apply(profile: BasicProfile) = new IdMapperDelegate[Bar]
           }
 
-          override lazy val table = new RepoTable[Bar]("foo")(db) {
+          override lazy val table = new RepoTable[Bar](db, "foo") {
             def name = column[String]("name")
             def * = id.? ~ name <> (Bar, Bar.unapply _)
           }
@@ -113,7 +113,7 @@ class SlickTest extends SpecificationWithJUnit {
             def apply(profile: BasicProfile) = new IdMapperDelegate[Bar]
           }
 
-          override lazy val table = new RepoTable[Bar]("foo")(db) with ExternalIdColumn[Bar] {
+          override lazy val table = new RepoTable[Bar](db, "foo") with ExternalIdColumn[Bar] {
             def name = column[String]("name")
             def * = id.? ~ externalId ~ name <> (Bar, Bar.unapply _)
           }
