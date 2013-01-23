e1a7218f8e870ba68cdf04be9812ec9be9333f59
Eishay Smith <eishay@gmail.com>
2013-01-22 21:46:18 -0800

    slicking the comment recipient

--- shoebox/app/com/keepit/model/CommentRecipient.scala
+++ shoebox/app/com/keepit/model/CommentRecipient.scala
@@ -43,6 +43,48 @@ case class CommentRecipient(
   }
 }
 
+@ImplementedBy(classOf[CommentRecipientRepoImpl])
+trait CommentRecipientRepo extends Repo[CommentRecipient] {
+  def getByComment(commentId: Id[Comment])(implicit session: RSession): Seq[CommentRecipient]
+  def getBySocialUser(socialUserId: Id[SocialUserInfo])(implicit session: RSession): Seq[CommentRecipient]
+  def getByUser(userId: Id[User])(implicit session: RSession): Seq[CommentRecipient]
+  def getByEmail(email: String)(implicit session: RSession): Seq[CommentRecipient]
+}
+
+@Singleton
+class CommentRecipientRepoImpl @Inject() (val db: DataBaseComponent) extends DbRepo[CommentRecipient] with CommentRecipientRepo {
+  import FortyTwoTypeMappers._
+  import org.scalaquery.ql._
+  import org.scalaquery.ql.ColumnOps._
+  import org.scalaquery.ql.basic.BasicProfile
+  import org.scalaquery.ql.extended.ExtendedTable
+  import db.Driver.Implicit._
+  import DBSession._
+
+  override lazy val table = new RepoTable[CommentRecipient](db, "comment_recipient") {
+    def commentId = column[Id[Comment]]("comment_id", O.NotNull)
+    def userId = column[Id[User]]("user_id", O.Nullable)
+    def socialUserId = column[Id[SocialUserInfo]]("social_user_id", O.Nullable)
+    def email = column[String]("email", O.Nullable)
+    def * = id.? ~ createdAt ~ updatedAt ~ commentId ~ userId.? ~ socialUserId.? ~ email.? ~ state <> (CommentRecipient, CommentRecipient.unapply _)
+  }
+
+  def getByComment(commentId: Id[Comment])(implicit session: RSession): Seq[CommentRecipient] =
+    (for(f <- table if f.commentId === commentId && f.state === CommentRecipientStates.ACTIVE) yield f).list
+
+  def getBySocialUser(socialUserId: Id[SocialUserInfo])(implicit session: RSession): Seq[CommentRecipient] =
+    (for(f <- table if f.socialUserId === socialUserId && f.state === CommentRecipientStates.ACTIVE) yield f).list
+
+  def getByUser(userId: Id[User])(implicit session: RSession): Seq[CommentRecipient] =
+    (for(f <- table if f.userId === userId && f.state === CommentRecipientStates.ACTIVE) yield f).list
+
+  def getByEmail(email: String)(implicit session: RSession): Seq[CommentRecipient] =
+    (for(f <- table if f.email === email && f.state === CommentRecipientStates.ACTIVE) yield f).list
+}
+
+
+
+
 object CommentRecipientCxRepo {
 
   def all(implicit conn: Connection): Seq[CommentRecipient] =
