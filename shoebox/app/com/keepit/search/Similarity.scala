9e12210e3d917d09fc31c12411ff88512b3ad9a9
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-01-28 14:50:22 -0800

    capitalized trait names

--- shoebox/app/com/keepit/search/Similarity.scala
+++ shoebox/app/com/keepit/search/Similarity.scala
@@ -5,35 +5,35 @@ import org.apache.lucene.search.{Similarity => LuceneSimilarity}
 import org.apache.lucene.search.DefaultSimilarity
 
 object Similarity extends Logging {
-  trait noTF { this: LuceneSimilarity =>
+  trait NoTF { this: LuceneSimilarity =>
     override def tf(freq: Int) = 1.0f
   }
-  trait noCoord { this: LuceneSimilarity =>
+  trait NoCoord { this: LuceneSimilarity =>
     override def coord(overlap: Int, maxOverlap: Int) = 1.0f
   }
-  trait proportionalCoord { this: LuceneSimilarity =>
+  trait ProportionalCoord { this: LuceneSimilarity =>
     override def coord(overlap: Int, maxOverlap: Int) = (overlap.toFloat) / (maxOverlap.toFloat)
   }
-  trait squaredCoord { this: LuceneSimilarity =>
+  trait SquaredCoord { this: LuceneSimilarity =>
     override def coord(overlap: Int, maxOverlap: Int) = {
       val coord = (overlap.toFloat) / (maxOverlap.toFloat)
       coord * coord
     }
   }
-  trait reciprocalCoord { this: LuceneSimilarity =>
+  trait ReciprocalCoord { this: LuceneSimilarity =>
     override def coord(overlap: Int, maxOverlap: Int) = 1.0f / ((1 + maxOverlap - overlap).toFloat)
   }
 
   private[this] val similarities = {
     var m = Map.empty[String, LuceneSimilarity]
-    m += ("default" -> new DefaultSimilarity with noCoord)
-    m += ("propotionalCoord" -> new DefaultSimilarity with proportionalCoord)
-    m += ("squaredCoord" -> new DefaultSimilarity with squaredCoord)
-    m += ("reciprocalCoord" -> new DefaultSimilarity with reciprocalCoord)
-    m += ("noTF" -> new DefaultSimilarity with noTF with noCoord)
-    m += ("noTF-propotionalCoord" -> new DefaultSimilarity with proportionalCoord)
-    m += ("noTF-squaredCoord" -> new DefaultSimilarity with noTF with squaredCoord)
-    m += ("noTF-reciprocalCoord" -> new DefaultSimilarity with noTF with reciprocalCoord)
+    m += ("default" -> new DefaultSimilarity with NoCoord)
+    m += ("propotionalCoord" -> new DefaultSimilarity with ProportionalCoord)
+    m += ("squaredCoord" -> new DefaultSimilarity with SquaredCoord)
+    m += ("reciprocalCoord" -> new DefaultSimilarity with ReciprocalCoord)
+    m += ("noTF" -> new DefaultSimilarity with NoTF with NoCoord)
+    m += ("noTF-propotionalCoord" -> new DefaultSimilarity with ProportionalCoord)
+    m += ("noTF-squaredCoord" -> new DefaultSimilarity with NoTF with SquaredCoord)
+    m += ("noTF-reciprocalCoord" -> new DefaultSimilarity with NoTF with ReciprocalCoord)
     m
   }
 
