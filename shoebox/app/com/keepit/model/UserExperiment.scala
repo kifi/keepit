d89ffdf18d969ca5c0c7d2998c944ab1e9ceddf7
Eishay Smith <eishay@gmail.com>
2013-02-04 22:37:32 -0800

    killing more circumflex entities

--- shoebox/app/com/keepit/model/UserExperiment.scala
+++ shoebox/app/com/keepit/model/UserExperiment.scala
@@ -19,11 +19,6 @@ case class UserExperiment (
 ) extends Model[UserExperiment] {
   def withId(id: Id[UserExperiment]) = this.copy(id = Some(id))
   def withUpdateTime(now: DateTime) = this.copy(updatedAt = now)
-  def save(implicit conn: Connection): UserExperiment = {
-    val entity = UserExperimentEntity(this.copy(updatedAt = currentDateTime))
-    assert(1 == entity.save())
-    entity.view
-  }
 }
 
 sealed case class ExperimentType(val value: String)
@@ -85,52 +80,4 @@ class UserExperimentRepoImpl @Inject() (val db: DataBaseComponent) extends DbRep
     q.list
   }
 
-}
-
-//slicked
-object UserExperimentCxRepo {
-
-  def get(id: Id[UserExperiment])(implicit conn: Connection): UserExperiment = UserExperimentEntity.get(id).get.view
-
-  def getByUser(userId: Id[User])(implicit conn: Connection): Seq[UserExperiment] =
-    (UserExperimentEntity AS "e").map { e => SELECT (e.*) FROM e WHERE ((e.userId EQ userId) AND (e.state EQ UserExperimentStates.ACTIVE))}.list.map(_.view)
-
-  def getExperiment(userId: Id[User], experiment: State[ExperimentType])(implicit conn: Connection): Option[UserExperiment] =
-    (UserExperimentEntity AS "e").map { e => SELECT (e.*) FROM e WHERE ((e.userId EQ userId) AND
-        (e.state EQ UserExperimentStates.ACTIVE) AND
-        (e.experimentType EQ experiment))}.unique.map(_.view)
-}
-
-private[model] class UserExperimentEntity extends Entity[UserExperiment, UserExperimentEntity] {
-  val createdAt = "created_at".JODA_TIMESTAMP.NOT_NULL(currentDateTime)
-  val updatedAt = "updated_at".JODA_TIMESTAMP.NOT_NULL(currentDateTime)
-  val experimentType = "experiment_Type".STATE[ExperimentType].NOT_NULL
-  val userId = "user_id".ID[User]
-  val state = "state".STATE.NOT_NULL(UserExperimentStates.ACTIVE)
-
-  def relation = UserExperimentEntity
-
-  def view = UserExperiment(
-    id = id.value,
-    createdAt = createdAt(),
-    updatedAt = updatedAt(),
-    userId = userId(),
-    experimentType = experimentType(),
-    state = state()
-  )
-}
-
-private[model] object UserExperimentEntity extends UserExperimentEntity with EntityTable[UserExperiment, UserExperimentEntity] {
-  override def relationName = "user_experiment"
-
-  def apply(view: UserExperiment): UserExperimentEntity = {
-    val entity = new UserExperimentEntity
-    entity.id.set(view.id)
-    entity.createdAt := view.createdAt
-    entity.updatedAt := view.updatedAt
-    entity.userId := view.userId
-    entity.experimentType := view.experimentType
-    entity.state := view.state
-    entity
-  }
 }
