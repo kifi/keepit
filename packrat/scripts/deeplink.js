a6ba1ab38274ef45238f96f99d1594ca06d4ce56
Jared Jacobs <jared@42go.com>
2013-02-05 05:58:23 -0800

    finishing porting deep link listener to browser-agnostic api, release 2.1.40

--- packrat/scripts/deeplink.js
+++ packrat/scripts/deeplink.js
@@ -3,7 +3,7 @@
 !function() {
   document.querySelector(".kifi-deep-link-no-extension").style.display = "none";
   var deepLink = JSON.parse(document.documentElement.dataset.kifiDeepLink);
-  chrome.extension.sendMessage(["add_deep_link_listener", deepLink]);
+  api.port.emit("add_deep_link_listener", deepLink);
   if (deepLink.uri) {
     window.location = deepLink.uri;
   }
