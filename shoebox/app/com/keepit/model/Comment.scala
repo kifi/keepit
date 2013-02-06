d78bc7f34fbb353ce41d5b4d0b4525a2d0514385
Eishay Smith <eishay@gmail.com>
2013-02-05 23:11:08 -0800

    slickleaning

--- shoebox/app/com/keepit/model/Comment.scala
+++ shoebox/app/com/keepit/model/Comment.scala
@@ -42,16 +42,8 @@ case class Comment(
   def withId(id: Id[Comment]) = this.copy(id = Some(id))
   def withUpdateTime(now: DateTime) = this.copy(updatedAt = now)
   def withState(state: State[Comment]) = copy(state = state)
-
   def withUrlId(urlId: Id[URL]) = copy(urlId = Some(urlId))
-
   def withNormUriId(normUriId: Id[NormalizedURI]) = copy(uriId = normUriId)
-
-  def save(implicit conn: Connection): Comment = {
-    val entity = CommentEntity(this.copy(updatedAt = currentDateTime))
-    assert(1 == entity.save())
-    entity.view
-  }
 }
 
 @ImplementedBy(classOf[CommentRepoImpl])
@@ -203,123 +195,6 @@ class CommentRepoImpl @Inject() (val db: DataBaseComponent, val commentCountCach
     (for(b <- table if b.urlId === urlId && b.state === CommentStates.ACTIVE) yield b).list
 }
 
-object CommentCxRepo {
-
-  def all(implicit conn: Connection): Seq[Comment] =
-    CommentEntity.all.map(_.view)
-
-  def all(permissions: State[CommentPermission])(implicit conn: Connection): Seq[Comment] =
-    (CommentEntity AS "c").map {c => SELECT (c.*) FROM c WHERE (c.permissions EQ permissions) list} map (_.view)
-
-  def all(permissions: State[CommentPermission], userId: Id[User])(implicit conn: Connection): Seq[Comment] =
-    (CommentEntity AS "c").map {c => SELECT (c.*) FROM c WHERE ((c.userId EQ userId) AND (c.permissions EQ permissions)) list} map (_.view)
-
-  def get(id: Id[Comment])(implicit conn: Connection): Comment =
-    getOpt(id).getOrElse(throw NotFoundException(id))
-
-  def getOpt(id: Id[Comment])(implicit conn: Connection): Option[Comment] =
-    CommentEntity.get(id).map(_.view)
-
-  def get(id: ExternalId[Comment])(implicit conn: Connection): Comment =
-    (CommentEntity AS "c").map {c => SELECT (c.*) FROM c WHERE (c.externalId EQ id) unique}
-      .map(_.view).getOrElse(throw NotFoundException(id))
-
-  def getRecipients(commentId: Id[Comment])(implicit conn: Connection) =
-    CommentRecipientCxRepo.getByComment(commentId)
-
-  def getParticipantsUserIds(comment: Comment)(implicit conn: Connection): Set[Id[User]] = {
-    val head = comment.parent map (CommentCxRepo.get(_)) getOrElse(comment)
-    (CommentRecipientCxRepo.getByComment(head.id.get) map (_.userId)).flatten.toSet + head.userId
-  }
-  //slicked
-  def getPublicCount(uriId: Id[NormalizedURI])(implicit conn: Connection): Int =
-    selectPublic({c => COUNT(c.id)}, uriId).unique.get.intValue
-  //slicked
-  private def selectPublic[T](
-      project: RelationNode[Id[Comment],CommentEntity] => Projection[T],
-      uriId: Id[NormalizedURI])(implicit conn: Connection) = {
-    val c = CommentEntity AS "c"
-    SELECT (project(c)) FROM c WHERE (
-        (c.uriId EQ uriId) AND
-        (c.permissions EQ CommentPermissions.PUBLIC) AND
-        (c.state EQ CommentStates.ACTIVE) AND
-        (c.parent IS_NULL))
-  }
-  //slicked
-  def getPrivate(uriId: Id[NormalizedURI], userId: Id[User])(implicit conn: Connection): Seq[Comment] =
-    selectPrivate({c => c.*}, uriId, userId).list.map(_.view)
-  //slicked
-  def getPrivateCount(uriId: Id[NormalizedURI], userId: Id[User])(implicit conn: Connection): Long =
-    selectPrivate({c => COUNT(c.id)}, uriId, userId).unique.get
-  //slicked
-  private def selectPrivate[T](
-      project: RelationNode[Id[Comment],CommentEntity] => Projection[T],
-      uriId: Id[NormalizedURI],
-      userId: Id[User])(implicit conn: Connection) = {
-    val c = CommentEntity AS "c"
-    SELECT (project(c)) FROM c WHERE (
-        (c.uriId EQ uriId) AND
-        (c.userId EQ userId) AND
-        (c.permissions EQ CommentPermissions.PRIVATE))
-  }
-
-  def getMessages(uriId: Id[NormalizedURI], userId: Id[User])(implicit conn: Connection): Seq[Comment] =
-    selectMessages({c => c.*}, uriId, userId).list.map(_.view)
-
-  def getMessagesWithChildrenCount(uriId: Id[NormalizedURI], userId: Id[User])(implicit conn: Connection): Int = {
-    val comments = selectMessages({c => c.*}, uriId, userId).list.map(_.view).toSet
-    val childrenCounts: Seq[Int] = (comments.toList map {c => getChildCount(c.id.get).toInt})
-    childrenCounts.foldLeft(0)((sum, count) => sum + count) + comments.size
-  }
-
-  def getMessageCount(uriId: Id[NormalizedURI], userId: Id[User])(implicit conn: Connection): Int =
-    selectMessages({c => c.id}, uriId, userId).list.size
-
-  private def selectMessages[T](
-      project: RelationNode[Id[Comment],CommentEntity] => Projection[T],
-      uriId: Id[NormalizedURI],
-      userId: Id[User])(implicit conn: Connection) = {
-    val c = CommentEntity AS "c"
-    val cr = CommentRecipientEntity AS "cr"
-
-    // Get all messages *from* and/or *to* the user
-    // (side effect: User cannot be removed from own messages)
-    (SELECT (project(c)) FROM (c JOIN cr).ON("c.id = cr.comment_id") WHERE (
-        (c.uriId EQ uriId) AND
-        (cr.userId EQ userId) AND
-        (c.permissions EQ CommentPermissions.MESSAGE) AND
-        (c.parent IS_NULL)))
-    .UNION (SELECT (project(c)) FROM c WHERE (
-        (c.uriId EQ uriId) AND
-        (c.userId EQ userId) AND
-        (c.permissions EQ CommentPermissions.MESSAGE) AND
-        (c.parent IS_NULL)))
-  }
-
-  def getChildren(commentId: Id[Comment])(implicit conn: Connection): Seq[Comment] =
-    selectChildren({c => c.*}, commentId).list.map(_.view)
-
-  def getChildCount(commentId: Id[Comment])(implicit conn: Connection): Int =
-    selectChildren({c => COUNT(c.id)}, commentId).unique.get.toInt
-
-  private def selectChildren[T](
-      project: RelationNode[Id[Comment],CommentEntity] => Projection[T],
-      commentId: Id[Comment])(implicit conn: Connection) = {
-    val c = CommentEntity AS "c"
-    SELECT (project(c)) FROM c WHERE (c.parent EQ commentId)
-  }
-
-  def page(page: Int = 0, size: Int = 20, permissions: State[CommentPermission] = CommentPermissions.PUBLIC)(implicit conn: Connection): Seq[Comment] =
-    (CommentEntity AS "c").map { c => SELECT (c.*) FROM c  WHERE (c.permissions EQ permissions)  LIMIT size OFFSET (page * size) ORDER_BY (c.id DESC) list }.map(_.view)
-
-  def count( permissions: State[CommentPermission] = CommentPermissions.PUBLIC)(implicit conn: Connection): Int =
-    (CommentEntity AS "c").map(c => SELECT(COUNT(c.id)).FROM(c).WHERE (c.permissions EQ permissions).unique).get.toInt
-
-  def getByUrlId(urlId: Id[URL])(implicit conn: Connection): Seq[Comment] =
-    (CommentEntity AS "b").map { b => SELECT (b.*) FROM b WHERE (b.urlId EQ urlId) list() }.map(_.view)
-
-}
-
 object CommentStates {
   val ACTIVE = State[Comment]("active")
   val INACTIVE = State[Comment]("inactive")
@@ -331,56 +206,4 @@ object CommentPermissions {
   val PRIVATE = State[CommentPermission]("private")
   val MESSAGE = State[CommentPermission]("message")
   val PUBLIC  = State[CommentPermission]("public")
-}
-
-private[model] class CommentEntity extends Entity[Comment, CommentEntity] {
-  val createdAt = "created_at".JODA_TIMESTAMP.NOT_NULL(currentDateTime)
-  val updatedAt = "updated_at".JODA_TIMESTAMP.NOT_NULL(currentDateTime)
-  val externalId = "external_id".EXTERNAL_ID[Comment].NOT_NULL(ExternalId())
-  val uriId = "normalized_uri_id".ID[NormalizedURI].NOT_NULL
-  val urlId = "url_id".ID[URL]
-  val userId = "user_id".ID[User]
-  val pageTitle = "page_title".VARCHAR(1024).NOT_NULL
-  val text = "text".CLOB.NOT_NULL
-  val parent = "parent".ID[Comment]
-  val permissions = "permissions".STATE[CommentPermission].NOT_NULL(CommentPermissions.PUBLIC)
-  val state = "state".STATE[Comment].NOT_NULL(CommentStates.ACTIVE)
-
-  def relation = CommentEntity
-
-  def view(implicit conn: Connection): Comment = Comment(
-    id = id.value,
-    createdAt = createdAt(),
-    updatedAt = updatedAt(),
-    externalId = externalId(),
-    uriId = uriId(),
-    urlId = urlId.value,
-    userId = userId(),
-    pageTitle = pageTitle(),
-    text = LargeString(text()),
-    parent = parent.value,
-    permissions = permissions(),
-    state = state()
-  )
-}
-
-private[model] object CommentEntity extends CommentEntity with EntityTable[Comment, CommentEntity] {
-  override def relationName = "comment"
-
-  def apply(view: Comment): CommentEntity = {
-    val comment = new CommentEntity
-    comment.id.set(view.id)
-    comment.createdAt := view.createdAt
-    comment.updatedAt := view.updatedAt
-    comment.externalId := view.externalId
-    comment.uriId := view.uriId
-    comment.urlId.set(view.urlId)
-    comment.userId := view.userId
-    comment.pageTitle := view.pageTitle
-    comment.text := view.text
-    comment.parent.set(view.parent)
-    comment.permissions := view.permissions
-    comment.state := view.state
-    comment
-  }
 }
