bf1ee11748fb211553d56e5ec9d3d70a2e568319
Eishay Smith <eishay@gmail.com>
2013-02-06 23:05:49 -0800

    killing all the circumflex models!

--- shoebox/app/com/keepit/search/ArticleSearchResultRef.scala
+++ shoebox/app/com/keepit/search/ArticleSearchResultRef.scala
@@ -26,11 +26,6 @@ case class ArticleSearchResultRef (
 ) extends ModelWithExternalId[ArticleSearchResultRef] {
   def withId(id: Id[ArticleSearchResultRef]) = this.copy(id = Some(id))
   def withUpdateTime(now: DateTime) = this.copy(updatedAt = now)
-  def save(implicit conn: Connection): ArticleSearchResultRef = {
-    val entity = ArticleSearchResultRefEntity(this.copy(updatedAt = currentDateTime))
-    assert(1 == entity.save())
-    entity.view
-  }
 }
 
 @ImplementedBy(classOf[ArticleSearchResultRefRepoImpl])
@@ -69,56 +64,4 @@ object ArticleSearchResultFactory {
     ArticleSearchResultRef(externalId = res.uuid, createdAt = res.time, updatedAt = res.time,
         last = res.last, myTotal = res.myTotal, friendsTotal = res.friendsTotal, mayHaveMoreHits = res.mayHaveMoreHits, millisPassed = res.millisPassed,
         hitCount = res.hits.size, pageNumber = res.pageNumber)
-}
-
-private[search] class ArticleSearchResultRefEntity extends Entity[ArticleSearchResultRef, ArticleSearchResultRefEntity] {
-  val createdAt = "created_at".JODA_TIMESTAMP.NOT_NULL(currentDateTime)
-  val updatedAt = "updated_at".JODA_TIMESTAMP.NOT_NULL(currentDateTime)
-  val externalId = "external_id".EXTERNAL_ID[ArticleSearchResultRef].NOT_NULL(ExternalId())
-  val state = "state".STATE.NOT_NULL(ArticleSearchResultRefStates.ACTIVE)
-  val last = "last".EXTERNAL_ID[ArticleSearchResultRef]
-  val myTotal = "my_total".INTEGER.NOT_NULL
-  val friendsTotal = "friends_total".INTEGER.NOT_NULL
-  val mayHaveMoreHits = "may_have_more_hits".BOOLEAN.NOT_NULL
-  val millisPassed = "millis_passed".INTEGER.NOT_NULL
-  val hitCount = "hit_count".INTEGER.NOT_NULL
-  val pageNumber = "page_number".INTEGER.NOT_NULL
-
-  def relation = ArticleSearchResultRefEntity
-
-  def view = ArticleSearchResultRef(
-    id = id.value,
-    createdAt = createdAt(),
-    updatedAt = updatedAt(),
-    externalId = externalId(),
-    state = state(),
-    last = last.value,
-    myTotal = myTotal(),
-    friendsTotal = friendsTotal(),
-    mayHaveMoreHits = mayHaveMoreHits(),
-    millisPassed = millisPassed(),
-    hitCount = hitCount(),
-    pageNumber = pageNumber()
-  )
-}
-
-private[search] object ArticleSearchResultRefEntity extends ArticleSearchResultRefEntity with EntityTable[ArticleSearchResultRef, ArticleSearchResultRefEntity] {
-  override def relationName = "article_search_result"
-
-  def apply(view: ArticleSearchResultRef): ArticleSearchResultRefEntity = {
-    val entity = new ArticleSearchResultRefEntity
-    entity.id.set(view.id)
-    entity.createdAt := view.createdAt
-    entity.updatedAt := view.updatedAt
-    entity.externalId := view.externalId
-    entity.state := view.state
-    entity.last.set(view.last)
-    entity.myTotal := view.myTotal
-    entity.friendsTotal := view.friendsTotal
-    entity.mayHaveMoreHits := view.mayHaveMoreHits
-    entity.millisPassed := view.millisPassed
-    entity.hitCount := view.hitCount
-    entity.pageNumber := view.pageNumber
-    entity
-  }
 }
