54682f8aa8c4383fed11a9e8c24f829aedb80365
Eishay Smith <eishay@gmail.com>
2013-02-06 22:52:54 -0800

    last model to slick!

--- shoebox/app/com/keepit/search/ArticleSearchResultRef.scala
+++ shoebox/app/com/keepit/search/ArticleSearchResultRef.scala
@@ -1,7 +1,10 @@
 package com.keepit.search;
 
-import com.keepit.common.db.{CX, Entity, EntityTable, ExternalId, Id, State, NotFoundException}
+import com.keepit.common.db._
+import com.keepit.common.db.slick._
+import com.keepit.common.db.slick.DBSession._
 import com.keepit.common.time._
+import com.google.inject.{Inject, ImplementedBy, Singleton}
 import java.sql.Connection
 import org.joda.time.DateTime
 import ru.circumflex.orm._
@@ -20,11 +23,39 @@ case class ArticleSearchResultRef (
   millisPassed: Int,
   hitCount: Int,
   pageNumber: Int
-) {
+) extends ModelWithExternalId[ArticleSearchResultRef] {
+  def withId(id: Id[ArticleSearchResultRef]) = this.copy(id = Some(id))
+  def withUpdateTime(now: DateTime) = this.copy(updatedAt = now)
   def save(implicit conn: Connection): ArticleSearchResultRef = {
     val entity = ArticleSearchResultRefEntity(this.copy(updatedAt = currentDateTime))
     assert(1 == entity.save())
     entity.view
+  }
+}
+
+@ImplementedBy(classOf[ArticleSearchResultRefRepoImpl])
+trait ArticleSearchResultRefRepo extends Repo[ArticleSearchResultRef] with ExternalIdColumnFunction[ArticleSearchResultRef] {
+}
+
+@Singleton
+class ArticleSearchResultRefRepoImpl @Inject() (val db: DataBaseComponent) extends DbRepo[ArticleSearchResultRef] with ArticleSearchResultRefRepo with ExternalIdColumnDbFunction[ArticleSearchResultRef] {
+  import FortyTwoTypeMappers._
+  import org.scalaquery.ql._
+  import org.scalaquery.ql.ColumnOps._
+  import org.scalaquery.ql.basic.BasicProfile
+  import org.scalaquery.ql.extended.ExtendedTable
+  import db.Driver.Implicit._
+  import DBSession._
+
+  override lazy val table = new RepoTable[ArticleSearchResultRef](db, "article_search_result") with ExternalIdColumn[ArticleSearchResultRef] {
+    def last = column[ExternalId[ArticleSearchResultRef]]("last", O.Nullable)
+    def myTotal = column[Int]("my_total", O.NotNull)
+    def friendsTotal = column[Int]("friends_total", O.NotNull)
+    def mayHaveMoreHits = column[Boolean]("may_have_more_hits", O.NotNull)
+    def millisPassed = column[Int]("millis_passed", O.NotNull)
+    def hitCount = column[Int]("hit_count", O.NotNull)
+    def pageNumber = column[Int]("page_number", O.NotNull)
+    def * = id.? ~ createdAt ~ updatedAt ~ externalId ~ state ~ last.? ~ myTotal ~ friendsTotal ~ mayHaveMoreHits ~ millisPassed ~ hitCount ~ pageNumber <> (ArticleSearchResultRef, ArticleSearchResultRef.unapply _)
   }
 }
 
@@ -33,20 +64,11 @@ object ArticleSearchResultRefStates {
   val INACTIVE = State[ArticleSearchResultRef]("inactive")
 }
 
-object ArticleSearchResultRef {
-
+object ArticleSearchResultFactory {
   def apply(res: ArticleSearchResult): ArticleSearchResultRef =
     ArticleSearchResultRef(externalId = res.uuid, createdAt = res.time, updatedAt = res.time,
         last = res.last, myTotal = res.myTotal, friendsTotal = res.friendsTotal, mayHaveMoreHits = res.mayHaveMoreHits, millisPassed = res.millisPassed,
         hitCount = res.hits.size, pageNumber = res.pageNumber)
-
-  def get(id: Id[ArticleSearchResultRef])(implicit conn: Connection): ArticleSearchResultRef = ArticleSearchResultRefEntity.get(id).get.view
-
-  def get(externalId: ExternalId[ArticleSearchResultRef])(implicit conn: Connection): ArticleSearchResultRef = getOpt(externalId).getOrElse(throw NotFoundException(externalId))
-
-  def getOpt(externalId: ExternalId[ArticleSearchResultRef])(implicit conn: Connection): Option[ArticleSearchResultRef] =
-    (ArticleSearchResultRefEntity AS "u").map { u => SELECT (u.*) FROM u WHERE (u.externalId EQ externalId) unique }.map(_.view)
-
 }
 
 private[search] class ArticleSearchResultRefEntity extends Entity[ArticleSearchResultRef, ArticleSearchResultRefEntity] {
