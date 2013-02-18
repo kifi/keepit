bc1e73ddd2270c38e4f9d2756cb0471c8dfe9006
Eishay Smith <eishay@gmail.com>
2013-02-18 00:33:51 -0800

    remove akka test system

--- shoebox/test/com/keepit/common/db/SlickTest.scala
+++ shoebox/test/com/keepit/common/db/SlickTest.scala
@@ -3,7 +3,6 @@ package com.keepit.common.db
 import com.keepit.common.db.slick._
 import com.keepit.common.db.slick.DBSession._
 import com.keepit.test._
-import com.keepit.TestAkkaSystem
 import com.keepit.inject._
 import scala.slick.lifted._
 import scala.slick.driver.H2Driver.Implicit._
@@ -11,8 +10,6 @@ import scala.slick.driver._
 import play.api.Play.current
 import play.api.libs.json.JsValue
 import play.api.test.Helpers._
-import akka.actor.ActorRef
-import akka.testkit.ImplicitSender
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.mutable.SpecificationWithJUnit
