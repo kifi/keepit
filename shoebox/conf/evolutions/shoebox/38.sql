d772f093f1a7f6833b8266675c509fa991bc2673
Jared Jacobs <jared@42go.com>
2013-02-20 17:56:25 -0800

    dropping redundant indices

--- shoebox/conf/evolutions/shoebox/38.sql
+++ shoebox/conf/evolutions/shoebox/38.sql
@@ -1,7 +1,9 @@
 # --- !Ups
 
 ALTER TABLE comment_read
-  ADD UNIQUE INDEX comment_read__user_id_parent_id (user_id,parent_id);
+  ADD UNIQUE INDEX comment_read__user_id_parent_id (user_id, parent_id),
+  DROP INDEX comment_read_i_message,
+  DROP INDEX comment_read_i_comment;
 
 insert into evolutions (name, description) values('38.sql', 'adding comment_read unique index');
 
