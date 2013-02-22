1a272f7efaae48f79b6c060c6e390f89d5b418eb
Andrew Conner <git@andrewconner.org>
2013-02-22 14:59:17 -0800

    Imports not needed

--- shoebox/test/com/keepit/common/net/URITest.scala
+++ shoebox/test/com/keepit/common/net/URITest.scala
@@ -1,15 +1,7 @@
 package com.keepit.common.net
 
-import com.keepit.test._
-import com.keepit.inject._
-import play.api.Play.current
-import play.api.libs.json.JsValue
-import play.api.test.Helpers._
-import akka.actor.ActorRef
-import akka.testkit.ImplicitSender
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
-import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
