f7668e11f9c69c0e281403ad0e569d0d91b843d8
Jared Jacobs <jared@42go.com>
2013-02-26 15:23:18 -0800

    expanding URITest and demonstrating how URI.unapply is intended to be used

--- shoebox/test/com/keepit/common/net/URITest.scala
+++ shoebox/test/com/keepit/common/net/URITest.scala
@@ -6,19 +6,52 @@ import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class URITest extends Specification {
-
   "URI" should {
-
-    "parse urls" in {
-      URI.parse("http://google.com/").isSuccess === true
-      URI.parse("http://bing.com/?q=1").isSuccess === true
-      URI.parse("https://sub.domain.com").isSuccess === true
-      URI.unapply("http://google.com/").get._3.get.name === "google.com"
+    "parse URLs" in {
+      URI.parse("http://google.com/").get.host.get.name === "google.com"
+      URI.parse("https://sub.domain.com").get.host.get.name === "sub.domain.com"
+      URI.parse("http://bing.com/?q=1").get.query.get.params === Seq(Param("q", Some("1")))
     }
-    "handle edge cases on unapply" in {
-      URI.unapply("http://premium.nba.com/pr/leaguepass/app/2012/console.html?debug=false&type=lp&TinedSid=Gaa419b-25665208-1262918951531-1&nsfg=1355463185|billing.lpbchoice_LAL_LAC_NYK_MIA_OKC^billing.lpbchoice^giBJ5TL8HJT8eLc6&retryCount=3").isDefined == true
-      URI.unapply("http://finance.yahoo.com/q?s=^dji").isDefined == true
-      URI.unapply("http://somerandomtorrentsi.te/torrent/7998570/torrent_that_previously_failed_2560_X_1600_[Set_7]").isDefined == true
+    "parse URLs via unapply" in {
+      "http://premium.nba.com/pr/leaguepass/app/2012/console.html?debug=false&type=lp&TinedSid=Gaa419b-25665208-1262918951531-1&nsfg=1355463185|billing.lpbchoice_LAL_LAC_NYK_MIA_OKC^billing.lpbchoice^giBJ5TL8HJT8eLc6&retryCount=3" match {
+        case URI(scheme, userInfo, host, port, path, query, fragment) =>
+          scheme === Some("http")
+          userInfo === None
+          host.get.domain === Seq("com", "nba", "premium")
+          port === -1
+          path === Some("/pr/leaguepass/app/2012/console.html")
+          query.get.params === Seq(  // alphabetized by parameter name
+            Param("TinedSid", Some("Gaa419b-25665208-1262918951531-1")),
+            Param("debug", Some("false")),
+            Param("nsfg", Some("1355463185%7Cbilling.lpbchoice_LAL_LAC_NYK_MIA_OKC%5Ebilling.lpbchoice%5EgiBJ5TL8HJT8eLc6")),
+            Param("retryCount", Some("3")),
+            Param("type", Some("lp")))
+          fragment === None
+        case _ => failure
+      }
+      "http://finance.yahoo.com/q?s=^dji" match {
+        case URI(scheme, userInfo, host, port, path, query, fragment) =>
+          scheme === Some("http")
+          userInfo === None
+          host.get.domain === Seq("com", "yahoo", "finance")
+          port === -1
+          path === Some("/q")
+          query.get.params === Seq(Param("s", Some("%5Edji")))
+          fragment === None
+        case _ => failure
+      }
+      "http://somerandomtorrentsi.te/torrent/7998570/torrent_that_previously_failed_2560_X_1600_[Set_7]" match {
+        case URI(scheme, userInfo, host, port, path, query, fragment) =>
+          scheme === Some("http")
+          userInfo === None
+          host.get.domain === Seq("te", "somerandomtorrentsi")
+          port === -1
+          path === Some("/torrent/7998570/torrent_that_previously_failed_2560_X_1600_[Set_7]")
+          query === None
+          fragment === None
+        case _ => failure
+      }
+      success
     }
   }
 }
