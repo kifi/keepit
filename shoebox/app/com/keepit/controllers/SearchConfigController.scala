cb64175189ef85826b1c4cd96191e3862d243768
Greg Methvin <greg@methvin.net>
2013-02-26 21:38:04 -0800

    Add some charts for kifi vs google

--- shoebox/app/com/keepit/controllers/SearchConfigController.scala
+++ shoebox/app/com/keepit/controllers/SearchConfigController.scala
@@ -1,15 +1,19 @@
 package com.keepit.controllers
 
+import scala.math.BigDecimal.RoundingMode
+
+import com.keepit.common.analytics.reports.{DailyKifiResultClickedByExperiment, DailyGoogleResultClickedByExperiment}
 import com.keepit.common.controller.FortyTwoController
 import com.keepit.common.db._
 import com.keepit.common.db.slick._
 import com.keepit.common.social._
+import com.keepit.common.time._
 import com.keepit.inject._
 import com.keepit.model._
 import com.keepit.search._
 
 import play.api.Play.current
-import play.api.libs.json.JsObject
+import play.api.libs.json.{JsString, JsArray, JsNumber, JsObject}
 
 object SearchConfigController extends FortyTwoController {
   def showUserConfig(userId: Id[User]) = AdminHtmlAction { implicit request =>
@@ -47,6 +51,33 @@ object SearchConfigController extends FortyTwoController {
     val experiments = inject[SearchConfigManager].getExperiments
     val default = inject[SearchConfigManager].defaultConfig
     Ok(views.html.searchConfigExperiments(experiments, default.params))
+  }
+
+  def getKifiVsGoogle(experimentId: Id[SearchConfigExperiment]) = AuthenticatedJsonAction { implicit request =>
+    val e = Some(experimentId).filter(_.id != 0).map(inject[SearchConfigManager].getExperiment)
+    val daysAgo = 30
+    val cdt = currentDateTime
+    val googleReport = new DailyGoogleResultClickedByExperiment(e).get(cdt.minusDays(daysAgo), cdt)
+    val kifiReport = new DailyKifiResultClickedByExperiment(e).get(cdt.minusDays(daysAgo), cdt)
+    val googleMap = googleReport.list.map(row => row.date.toLocalDate -> row.fields.head._2.value.toInt).toMap
+    val kifiMap = kifiReport.list.map(row => row.date.toLocalDate -> row.fields.head._2.value.toInt).toMap
+    val now = currentDate
+    val (days, kvgDiffs) = (for (i <- daysAgo to 0 by -1) yield {
+      val day = now.minusDays(i)
+      val (kifiClicks, googleClicks) = (kifiMap.get(day).getOrElse(0), googleMap.get(day).getOrElse(0))
+      val total = 1.0*(kifiClicks + googleClicks)
+      (day, if (total > 0) kifiClicks / total else 0)
+    }) match {
+      case tuples =>
+        val minLength = 10
+        val toDrop = tuples.indexWhere(_._2 != 0.0) min (tuples.length - minLength)
+        val (days, kvg) = tuples.drop(toDrop).unzip
+        (days, kvg.zip(0.0 +: kvg).map { case (a, b) => a - b })
+    }
+    Ok(JsObject(List(
+      "day0" -> JsString(days.min.toString),
+      "counts" -> JsArray(kvgDiffs.map {i => JsNumber(BigDecimal(i).setScale(3, RoundingMode.HALF_EVEN))})
+    )))
   }
 
   def addNewExperiment = AdminHtmlAction { implicit request =>
