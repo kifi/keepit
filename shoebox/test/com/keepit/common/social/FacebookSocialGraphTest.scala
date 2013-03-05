c312b94431a254817af4b3b4b55e731ebbd4edbe
Eishay Smith <eishay@gmail.com>
2013-03-04 21:36:37 -0800

    using the smaller data sample

--- shoebox/test/com/keepit/common/social/FacebookSocialGraphTest.scala
+++ shoebox/test/com/keepit/common/social/FacebookSocialGraphTest.scala
@@ -44,14 +44,14 @@ class FacebookSocialGraphTest extends SpecificationWithJUnit with DbRepos {
         //val httpClient = HttpClientImpl(timeout = 1, timeoutUnit = TimeUnit.MINUTES)
         val httpClient = new FakeHttpClient(
             Some(Map("https://graph.facebook.com/eishay?access_token=AAAHiW1ZC8SzYBAOtjXeZBivJ77eNZCIjXOkkZAZBjfLbaP4w0uPnj0XzXQUi6ib8m9eZBlHBBxmzzFbEn7jrZADmHQ1gO05AkSZBsZAA43RZC9dQZDZD&fields=link,name,first_name,middle_name,last_name,location,locale,gender,username,languages,third_party_id,installed,timezone,updated_time,verified,bio,birthday,devices,education,email,picture,significant_other,website,work,friends.fields(link,name,first_name,middle_name,last_name,location,locale,gender,username,languages,third_party_id,installed,timezone,updated_time,verified,bio,birthday,devices,education,email,picture,significant_other,website,work)" ->
-            io.Source.fromFile(new File("test/com/keepit/common/social/facebook_graph_eishay.json")).mkString))
+            io.Source.fromFile(new File("test/com/keepit/common/social/facebook_graph_eishay_super_min.json")).mkString))
         )
         val oAuth2Info = OAuth2Info(accessToken = "AAAHiW1ZC8SzYBAOtjXeZBivJ77eNZCIjXOkkZAZBjfLbaP4w0uPnj0XzXQUi6ib8m9eZBlHBBxmzzFbEn7jrZADmHQ1gO05AkSZBsZAA43RZC9dQZDZD",
           tokenType = None, expiresIn = None, refreshToken = None)
         val socialUser = SocialUser(UserId("100004067535411", "facebook"), "Boaz Tal", Some("boaz.tal@gmail.com"),
           Some("http://www.fb.com/me"), AuthenticationMethod.OAuth2, true, None, Some(oAuth2Info), None)
 
-        val user = inject[Database].readWrite { implicit s => 
+        val user = inject[Database].readWrite { implicit s =>
           userRepo.save(User(firstName = "Eishay", lastName = "Smith"))
         }
         val unsaved = SocialUserInfo(userId = user.id, fullName = "Eishay Smith", socialId = SocialId("eishay"), networkType = SocialNetworks.FACEBOOK, credentials = Some(socialUser))
