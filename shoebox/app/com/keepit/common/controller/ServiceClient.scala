2540b5a391c64f98a7a40c26645f8a11d705adcc
Greg Methvin <greg@methvin.net>
2013-03-20 13:06:42 -0700

    Make search admin controllers use the search service client

--- shoebox/app/com/keepit/common/controller/ServiceClient.scala
+++ shoebox/app/com/keepit/common/controller/ServiceClient.scala
@@ -17,13 +17,7 @@ trait ServiceClient {
 
   protected def url(path: String): String = s"${protocol}://${host}:${port}${path}"
 
-  protected def call(call: Call, body: JsValue = JsNull): ClientResponse = call match {
-    case Call("GET", path) => httpClient.get(url(path))
-    case Call("POST", path) => httpClient.post(url(path), body)
-    case Call(m, _) => throw new UnsupportedOperationException(s"Unsupported method $m")
-  }
-
-  protected def callFuture(call: Call, body: JsValue = JsNull): Future[ClientResponse] = call match {
+  protected def call(call: Call, body: JsValue = JsNull): Future[ClientResponse] = call match {
     case Call("GET", path) => httpClient.getFuture(url(path))
     case Call("POST", path) => httpClient.postFuture(url(path), body)
     case Call(m, _) => throw new UnsupportedOperationException(s"Unsupported method $m")
