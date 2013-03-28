cd1be331fa639e0854538c87e063a26ccbcf0470
Jared Jacobs <jared@42go.com>
2013-03-28 16:17:54 -0700

    don't need this here

--- packrat/scripts/comments.js
+++ packrat/scripts/comments.js
@@ -1,7 +1,6 @@
 // @require styles/metro/comments.css
 // @require styles/metro/compose.css
 // @require scripts/lib/jquery.timeago.js
-// @require scripts/lib/jquery-tokeninput-1.6.1.min.js
 // @require scripts/api.js
 // @require scripts/formatting.js
 // @require scripts/look.js
