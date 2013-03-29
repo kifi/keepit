5a20dcaf27a0455270bbfc403835ad00d55300a9
Andrew Conner <git@andrewconner.org>
2013-03-29 15:37:54 -0700

    Formatting, english

--- shoebox/app/com/keepit/controllers/assets/UserPicture.scala
+++ shoebox/app/com/keepit/controllers/assets/UserPicture.scala
@@ -10,7 +10,7 @@ import play.api.mvc._
 import play.api._
 import com.keepit.model._
 import com.keepit.common.db.slick._
-import com.google.inject.{Inject, Singleton}
+import com.google.inject.{ Inject, Singleton }
 import com.keepit.common.db.ExternalId
 import com.keepit.controllers.core.UserHelper
 
@@ -18,14 +18,14 @@ import com.keepit.controllers.core.UserHelper
 class UserPicture @Inject() (
   db: Database,
   userHelper: UserHelper)
-    extends WebsiteController {
+  extends WebsiteController {
 
   /* Usage: /widthxheight/userExternalId
    * /200/9de9a8c4-74aa-43fb-bdd3-f329b4a1c0f6 for a 200x200 square
    * /200x250/9de9a8c4-74aa-43fb-bdd3-f329b4a1c0f6 for a 200x250 square
    */
   def getByExternalId(sizeStr: String, userExternalId: ExternalId[User]) = Action { request =>
-    val url = db.readOnly( implicit s => userHelper.getAvatarByUserExternalId(sizeStr, userExternalId))
+    val url = db.readOnly(implicit s => userHelper.getAvatarByUserExternalId(sizeStr, userExternalId))
     Redirect(url)
   }
 }
--- shoebox/app/com/keepit/controllers/core/UserHelper.scala
+++ shoebox/app/com/keepit/controllers/core/UserHelper.scala
@@ -10,8 +10,8 @@ import play.api.mvc._
 import play.api._
 import com.keepit.model._
 import com.keepit.common.db.slick._
-import com.google.inject.{Inject, Singleton}
-import com.keepit.common.db.{ExternalId, Id}
+import com.google.inject.{ Inject, Singleton }
+import com.keepit.common.db.{ ExternalId, Id }
 import com.keepit.common.db.slick.DBSession.RSession
 
 @Singleton
@@ -21,7 +21,7 @@ class UserHelper @Inject() (
 
   /* Usage: /widthxheight/userExternalId
    * /200/9de9a8c4-74aa-43fb-bdd3-f329b4a1c0f6 for a 200x200 square
-   * /200x250/9de9a8c4-74aa-43fb-bdd3-f329b4a1c0f6 for a 200x250 square
+   * /200x250/9de9a8c4-74aa-43fb-bdd3-f329b4a1c0f6 for a 200x250 rectangle
    */
 
   private val defaultAvatar = "https://graph.facebook.com//picture?type=square"
@@ -29,7 +29,7 @@ class UserHelper @Inject() (
   def getAvatarByUserId(sizeStr: String, userId: Id[User])(implicit session: RSession): String = {
     val (width, height) = try {
       val parts = sizeStr.toLowerCase.split("x").map(i => Math.max(Math.min(i.toInt, 500), 50))
-      if(parts.size == 1)
+      if (parts.size == 1)
         (parts(0), parts(0))
       else
         (parts(0), parts(1))
