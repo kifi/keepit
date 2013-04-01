5169029f4782d5b475fd0969892c65a5f8284900
Eishay Smith <eishay@gmail.com>
2013-04-01 07:09:52 -0700

    fixing tests

--- shoebox/test/com/keepit/shoebox/ShoeboxModuleTest.scala
+++ shoebox/test/com/keepit/shoebox/ShoeboxModuleTest.scala
@@ -18,7 +18,7 @@ class ShoeboxModuleTest extends Specification with Logging {
 
   "Module" should {
     "instantiate controllers" in {
-      running(new ShoeboxApplication().withFakeHealthcheck().withFakeMail()) {
+      running(new ShoeboxApplication().withFakeHealthcheck.withFakeMail.withFakePhraseIndexer) {
         val ClassRoute = "@(.+)@.+".r
         val classes = current.routes.map(_.documentation).reduce(_ ++ _).collect {
           case (_, _, ClassRoute(className)) => Class.forName(className)
