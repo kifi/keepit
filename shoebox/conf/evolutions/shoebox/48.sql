20f4968b61306870c94364a86105d794177e5124
Andrew Conner <git@andrewconner.org>
2013-04-01 13:09:11 -0700

    New commands

--- shoebox/conf/evolutions/shoebox/48.sql
+++ shoebox/conf/evolutions/shoebox/48.sql
@@ -1,6 +1,7 @@
 # --- !Ups
 
 ALTER TABLE user_experiment ALTER COLUMN experiment_type VARCHAR(32) NOT NULL;
+# ALTER TABLE user_experiment MODIFY COLUMN experiment_type VARCHAR(32) NOT NULL;
 
 insert into evolutions (name, description) values('48.sql', 'increase size of experiment_type column');
 
