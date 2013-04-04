3c72fa051b7d21c6b1b4c0df5a9cd49883f2b6c3
Jared Jacobs <jared@42go.com>
2013-04-04 12:13:13 -0700

    removing second init_slider_please call, don't think it helps

--- packrat/scripts/scout.js
+++ packrat/scripts/scout.js
@@ -67,7 +67,6 @@ var injected, t0 = +new Date;
         document.removeEventListener("scroll", onScrollMaybeShow);  // in case already registered
         document.addEventListener("scroll", onScrollMaybeShow);
       }
-      clearTimeout(initSliderTimeout);
     },
     open_slider_to: function(o) {
       if (o.metro && !info) {
@@ -90,7 +89,6 @@ var injected, t0 = +new Date;
     auto_show: autoShow.bind(null, "auto")});
 
   api.port.emit("init_slider_please");
-  var initSliderTimeout = setTimeout(api.port.emit.bind(api.port, "init_slider_please"), 1000);
 
   function autoShow(trigger) {
     var width;
