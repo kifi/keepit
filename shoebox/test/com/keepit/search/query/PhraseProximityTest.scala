18c89f5fd6ebdd3d56bd7af776c5ed0c399d7482
yingjieMiao <yingjie@42go.com>
2013-04-15 14:28:04 -0700

    tab to spaces

--- shoebox/test/com/keepit/search/query/PhraseProximityTest.scala
+++ shoebox/test/com/keepit/search/query/PhraseProximityTest.scala
@@ -18,21 +18,21 @@ import org.apache.lucene.index.Term
 import scala.collection.mutable.ArrayBuffer
 import org.apache.lucene.search.DocIdSetIterator
 
-class PhraseProximityTest extends Specification{
+class PhraseProximityTest extends Specification {
   val terms = Array("machine", "learning", "and", "machine", "translation")
-  val phrases = Set((0,2), (3,2))
+  val phrases = Set((0, 2), (3, 2))
   val phraseHelper = new PhraseHelper(terms, phrases)
   val gapPenalty = PhraseProximityQuery.gapPenalty
 
   "PhraseHelper" should {
     "correctly construct phraseMap" in {
-      phraseHelper.phraseMap === Map("machine learning" -> (0, 2), "and" -> (1, 1), "machine translation"-> (2, 2))			// (id, len)
+      phraseHelper.phraseMap === Map("machine learning" -> (0, 2), "and" -> (1, 1), "machine translation" -> (2, 2))            // (id, len)
     }
 
     "correctly detect phrase locations" in {
       var pos = Array(5, 6, 7, 8, 9)
       var tokens = Array("machine", "learning", "and", "machine", "translation")
-      phraseHelper.getMatchedPhrases(pos, tokens) === Array((0, 2, 6), (1, 1, 7), (2, 2, 9))								// (id, len, endingPos)
+      phraseHelper.getMatchedPhrases(pos, tokens) === Array((0, 2, 6), (1, 1, 7), (2, 2, 9))                                    // (id, len, endingPos)
 
       pos = Array(4, 6, 7, 8, 9)
       phraseHelper.getMatchedPhrases(pos, tokens) === Array((1, 1, 7), (2, 2, 9))
@@ -48,21 +48,20 @@ class PhraseProximityTest extends Specification{
 
     "correctly compute scores" in {
 
-      var matches = Array((0, 2, 1), (1, 1, 2), (2, 2, 4))												// (machine learning)(and)(machine translation), 3 consecutive phrases
+      var matches = Array((0, 2, 1), (1, 1, 2), (2, 2, 4))                                  // (machine learning)(and)(machine translation), 3 consecutive phrases
       var score = phraseHelper.getMatchedScore(matches)
-      var correct = 5*(5+1)/2.0f - 5*(5-1)*gapPenalty/2.0f												// should match max possible score
-      assert(math.abs(score - correct) < 1e-4 )
+      var correct = 5 * (5 + 1) / 2.0f - 5 * (5 - 1) * gapPenalty / 2.0f                    // should match max possible score
+      assert(math.abs(score - correct) < 1e-4)
 
-      matches = Array((1,1,2), (0, 2, 5), (0, 2, 12), (1, 1, 13), (2, 2, 15))							// add some confusion terms: (and)...(machine translation)......(machine learning)(and)(machine translation)
+      matches = Array((1, 1, 2), (0, 2, 5), (0, 2, 12), (1, 1, 13), (2, 2, 15))             // add some confusion terms: (and)...(machine translation)......(machine learning)(and)(machine translation)
       score = phraseHelper.getMatchedScore(matches)
-      assert(math.abs(score - correct) < 1e-4 )															// should still match max possible score
+      assert(math.abs(score - correct) < 1e-4)                                              // should still match max possible score
 
-      matches = Array.empty[(Int,Int,Int)]
+      matches = Array.empty[(Int, Int, Int)]
       score = phraseHelper.getMatchedScore(matches)
       assert(score == 0)
     }
   }
-
 
   val analyzer = new WhitespaceAnalyzer(Version.LUCENE_41)
   val config = new IndexWriterConfig(Version.LUCENE_41, analyzer)
@@ -71,15 +70,15 @@ class PhraseProximityTest extends Specification{
   val indexReader = {
     val writer = new IndexWriter(ramDir, config)
     val texts = List("A book on machine learning and machine translation",
-    				"A book on machine learning and a book on machine translation",
-    				"A book on statistics and machine translation",
-    				"A book on statistics and machine reading",
-    				"A book")
-   texts.foreach{ text =>
-     val doc = new Document()
-     doc.add(new Field("c", text, TextField.TYPE_NOT_STORED))
-     writer.addDocument(doc)
-   }
+      "A book on machine learning and a book on machine translation",
+      "A book on statistics and machine translation",
+      "A book on statistics and machine reading",
+      "A book")
+    texts.foreach { text =>
+      val doc = new Document()
+      doc.add(new Field("c", text, TextField.TYPE_NOT_STORED))
+      writer.addDocument(doc)
+    }
     writer.commit()
     writer.close()
     DirectoryReader.open(ramDir)
@@ -96,24 +95,24 @@ class PhraseProximityTest extends Specification{
 
     var weight = searcher.createNormalizedWeight(q)
 
-      var scorer = weight.scorer(readerContext, true, true, reader.getLiveDocs)
-      val buf = new ArrayBuffer[(Int, Float)]()
-      var doc = scorer.nextDoc()
-      while (doc < DocIdSetIterator.NO_MORE_DOCS) {
-        buf += ((doc, scorer.score()))
-        doc = scorer.nextDoc()
-      }
-      indexReader.numDocs() === 5
-      buf.size === 4
-      buf.sortBy(_._2).map(_._1) === Seq(3, 2, 1, 0)        // doc 0 is most relevant
+    var scorer = weight.scorer(readerContext, true, true, reader.getLiveDocs)
+    val buf = new ArrayBuffer[(Int, Float)]()
+    var doc = scorer.nextDoc()
+    while (doc < DocIdSetIterator.NO_MORE_DOCS) {
+      buf += ((doc, scorer.score()))
+      doc = scorer.nextDoc()
+    }
+    indexReader.numDocs() === 5
+    buf.size === 4
+    buf.sortBy(_._2).map(_._1) === Seq(3, 2, 1, 0)      // doc 0 is most relevant
   }
 
   "not return hits when no term and phrase" in {
-      val q = PhraseProximityQuery(Seq.empty[Term], Set.empty[(Int, Int)])
-      val weight = searcher.createNormalizedWeight(q)
-      (weight != null) === true
+    val q = PhraseProximityQuery(Seq.empty[Term], Set.empty[(Int, Int)])
+    val weight = searcher.createNormalizedWeight(q)
+    (weight != null) === true
 
-      val scorer = weight.scorer(readerContext, true, true, reader.getLiveDocs)
-      scorer === null
-    }
+    val scorer = weight.scorer(readerContext, true, true, reader.getLiveDocs)
+    scorer === null
+  }
 }
