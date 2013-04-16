4349186b5258ee4a035b0b2e079a74ca5b6fcabd
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-04-16 10:27:33 -0700

    more aggressive phrase/proximity/semantic boosting, increased damping

--- shoebox/app/com/keepit/search/SearchConfig.scala
+++ shoebox/app/com/keepit/search/SearchConfig.scala
@@ -14,7 +14,7 @@ import com.keepit.search.query.QueryHash
 object SearchConfig {
   private[search] val defaultParams =
     Map[String, String](
-      "phraseBoost" -> "0.3",
+      "phraseBoost" -> "0.5",
       "siteBoost" -> "2.0",
       "enableCoordinator" -> "true",
       "similarity" -> "default",
@@ -29,11 +29,11 @@ object SearchConfig {
       "percentMatch" -> "75",
       "halfDecayHours" -> "24",
       "recencyBoost" -> "1.0",
-      "tailCutting" -> "0.20",
-      "proximityBoost" -> "0.3",
-      "semanticBoost" -> "0.4",
-      "dampingHalfDecayMine" -> "8.0",
-      "dampingHalfDecayFriends" -> "6.0",
+      "tailCutting" -> "0.25",
+      "proximityBoost" -> "0.8",
+      "semanticBoost" -> "0.8",
+      "dampingHalfDecayMine" -> "7.0",
+      "dampingHalfDecayFriends" -> "5.0",
       "dampingHalfDecayOthers" -> "2.0"
     )
   private[this] val descriptions =
