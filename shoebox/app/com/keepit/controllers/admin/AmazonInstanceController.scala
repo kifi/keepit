7cca14134001ce2bd9ea5ba501b3cbf897b9f2cf
Eishay Smith <eishay@gmail.com>
2013-04-16 20:47:27 -0700

    some fixes and adding a dev mock instance

--- shoebox/app/com/keepit/controllers/admin/AmazonInstanceController.scala
+++ shoebox/app/com/keepit/controllers/admin/AmazonInstanceController.scala
@@ -1,18 +1,14 @@
 package com.keepit.controllers.admin
 
-import com.keepit.common.db._
-import com.keepit.common.db.slick._
-import com.keepit.model.NormalizedURIStates._
-import com.keepit.model._
-import com.keepit.search.SearchServiceClient
-import play.api.libs.json.{JsNumber, JsObject}
-import scala.concurrent.ExecutionContext.Implicits.global
-
+import com.keepit.common.amazon.AmazonInstanceInfo
 import com.keepit.common.controller.{AdminController, ActionAuthenticator}
 import com.google.inject.{Inject, Singleton}
 
+import views.html
+
 @Singleton
 class AmazonInstanceController @Inject()(
+    actionAuthenticator: ActionAuthenticator,
     amazonInstanceInfo: AmazonInstanceInfo
   ) extends AdminController(actionAuthenticator) {
 
