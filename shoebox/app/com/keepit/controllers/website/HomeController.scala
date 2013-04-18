bf7f922b441191c20c89c54687d929f583db8c88
Eishay Smith <eishay@gmail.com>
2013-04-18 16:03:25 -0700

    keepitfindit.com => www.kifi.com

--- shoebox/app/com/keepit/controllers/website/HomeController.scala
+++ shoebox/app/com/keepit/controllers/website/HomeController.scala
@@ -19,7 +19,7 @@ import play.api.libs.json._
 import com.keepit.common.db.ExternalId
 import com.keepit.common.db.slick.DBSession.RSession
 
-case class BasicUserInvitation(name: String, picture: String, state: State[Invitation]) 
+case class BasicUserInvitation(name: String, picture: String, state: State[Invitation])
 
 @Singleton
 class HomeController @Inject() (db: Database,
@@ -52,7 +52,7 @@ class HomeController @Inject() (db: Database,
             else None
           } flatten
         }
-        
+
         if(userIsAllowed(request.user, request.experimants))
           Ok(views.html.website.userHome(request.user, friendsOnKifi))
         else
@@ -63,7 +63,7 @@ class HomeController @Inject() (db: Database,
     if(Play.isDev) Ok(views.html.website.welcome())
     else Ok // disabled for now
   })
-  
+
   def pendingHome()(implicit request: AuthenticatedRequest[AnyContent]) = {
     val user = request.user
     val name = s"${user.firstName} ${user.lastName}"
@@ -74,7 +74,7 @@ class HomeController @Inject() (db: Database,
         if(user.state == UserStates.ACTIVE) Some(user.externalId)
         else None
       } flatten
-  
+
       (email, friendsOnKifi)
     }
     Ok(views.html.website.onboarding.userRequestReceived(user, email, friendsOnKifi))
@@ -88,10 +88,10 @@ class HomeController @Inject() (db: Database,
         else None
       } flatten
     }
-    
+
     val (invites, invitesLeft, invitesSent, invitesAccepted) = db.readOnly { implicit session =>
       val totalAllowedInvites = userValueRepo.getValue(request.user.id.get, "availableInvites").map(_.toInt).getOrElse(6)
-      val currentInvitations = invitationRepo.getByUser(request.user.id.get).map{ s => 
+      val currentInvitations = invitationRepo.getByUser(request.user.id.get).map{ s =>
         val socialUser = socialUserRepo.get(s.recipientSocialUserId)
         Some(BasicUserInvitation(
           name = socialUser.fullName,
@@ -103,18 +103,18 @@ class HomeController @Inject() (db: Database,
       val sent = currentInvitations.length
       val accepted = currentInvitations.count( s => if(s.isDefined && s.get.state == InvitationStates.JOINED) true else false)
       val invites = currentInvitations ++ Seq.fill(left)(None)
-      
+
       (invites, left, sent, accepted)
     }
-    
+
     Ok(views.html.website.inviteFriends(request.user, friendsOnKifi, invites, invitesLeft, invitesSent, invitesAccepted))
   }
-  
+
   private def fbInviteUrl(invite: Invitation)(implicit session: RSession) = {
     val identity = socialUserInfoRepo.get(invite.recipientSocialUserId)
     s"https://www.facebook.com/dialog/send?app_id=104629159695560&name=You're%20invited%20to%20try%20KiFi!&picture=http://keepitfindit.com/assets/images/logo2.png&link=https://www.keepitfindit.com/invite/${invite.externalId.id}&description=Hey%20${identity.fullName}!%20You're%20invited%20to%20join%20KiFi.%20Click%20here%20to%20sign%20up&redirect_uri=https://www.keepitfindit.com/invite/confirm/${invite.externalId}&to=${identity.socialId.id}"
   }
-  
+
   def inviteConnection = AuthenticatedHtmlAction { implicit request =>
     val fullSocialId = request.request.body.asFormUrlEncoded match {
       case Some(form) =>
@@ -130,7 +130,7 @@ class HomeController @Inject() (db: Database,
           case Some(alreadyInvited) => BadRequest(Json.obj("invitation" -> "Already Invited"))
           case None =>
             val totalAllowedInvites = userValueRepo.getValue(request.user.id.get, "availableInvites").map(_.toInt).getOrElse(6)
-            val currentInvitations = invitationRepo.getByUser(request.user.id.get).map{ s => 
+            val currentInvitations = invitationRepo.getByUser(request.user.id.get).map{ s =>
               val socialUser = socialUserRepo.get(s.recipientSocialUserId)
               Some(BasicUserInvitation(
                 name = socialUser.fullName,
@@ -140,7 +140,7 @@ class HomeController @Inject() (db: Database,
             }
             val left = totalAllowedInvites - currentInvitations.length
             val invites = currentInvitations ++ Seq.fill(left)(None)
-            
+
             if(left > 0) {
               val invite = invitationRepo.save(Invitation(senderUserId = request.user.id.get, recipientSocialUserId = socialUserInfo.id.get, state = InvitationStates.INACTIVE))
               Redirect(fbInviteUrl(invite))
@@ -151,7 +151,7 @@ class HomeController @Inject() (db: Database,
       }
     }
   }
-  
+
   def acceptInvite(id: ExternalId[Invitation]) = Action {
     db.readOnly { implicit session =>
       val invitation = invitationRepo.getOpt(id)
@@ -164,7 +164,7 @@ class HomeController @Inject() (db: Database,
       }
     }
   }
-  
+
   def confirmInvite(id: ExternalId[Invitation]) = Action {
     val invite = db.readWrite { implicit session =>
       invitationRepo.save(invitationRepo.get(id).copy(state = InvitationStates.ACTIVE))
@@ -173,17 +173,17 @@ class HomeController @Inject() (db: Database,
   }
 
   def gettingStarted = AuthenticatedHtmlAction { implicit request =>
-    
+
     Ok(views.html.website.gettingStarted(request.user))
   }
-  
+
   // temporary during development:
   def userIsAllowed(user: User, experiments: Seq[State[ExperimentType]]) = {
     Play.isDev || experiments.contains(ExperimentTypes.ADMIN)
   }
 
   private def inviteFacebookUser(id: String) = {
-    
+
   }
 
 
