aeb1c5789d7cb873121acd150f5adf6e0e1b7b30
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-04-19 14:57:56 -0700

    ignoring drop down menu (option tag) when scrape

--- shoebox/app/com/keepit/scraper/extractor/DefaultExtractor.scala
+++ shoebox/app/com/keepit/scraper/extractor/DefaultExtractor.scala
@@ -14,13 +14,13 @@ object DefaultExtractorFactory extends ExtractorFactory {
 }
 
 class DefaultExtractor(url: String, maxContentChars: Int) extends TikaBasedExtractor(url, maxContentChars) with Logging {
-  protected def getContentHandler = new BoilerSafeContentHandler(new BoilerpipeContentHandler(output))
+  protected def getContentHandler = new DefaultContentHandler(new BoilerpipeContentHandler(output))
 }
 
-// To prevent Boilerpipe blowing up we need this
-class BoilerSafeContentHandler(handler: ContentHandler) extends ContentHandlerDecorator(handler) {
+class DefaultContentHandler(handler: ContentHandler) extends ContentHandlerDecorator(handler) {
 
   var inAnchor = false
+  var inOption = false
 
   override def startElement(uri: String, localName: String, qName: String, atts: Attributes) {
     localName.toLowerCase() match {
@@ -28,6 +28,8 @@ class BoilerSafeContentHandler(handler: ContentHandler) extends ContentHandlerDe
         if (inAnchor) endElement(uri, localName, qName)
         super.startElement(uri, localName, qName, atts)
         inAnchor = true
+      case "option" => //we will ignore text in options (drop down menu, etc.)
+        inOption = true
       case _ => super.startElement(uri, localName, qName, atts)
     }
   }
@@ -38,7 +40,16 @@ class BoilerSafeContentHandler(handler: ContentHandler) extends ContentHandlerDe
         super.endElement(uri, localName, qName)
         inAnchor = false
       }
+      case "option" =>
+        inOption = false
       case _ => super.endElement(uri, localName, qName)
+    }
+  }
+
+  override def characters(ch: Array[Char], start: Int, length: Int) {
+    //ignore text options (drop down menu, etc.)
+    if (!inOption) {
+      handler.characters(ch, start, length)
     }
   }
 }
