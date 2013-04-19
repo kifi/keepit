763608ce3ee6bd21b21f04fe07b6d41f75c7e2d7
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-04-18 17:40:08 -0700

    added google result click to result click boost

--- shoebox/test/com/keepit/test/TestApplication.scala
+++ shoebox/test/com/keepit/test/TestApplication.scala
@@ -100,7 +100,7 @@ case class TestModule() extends ScalaModule {
     bind[SocialGraphPlugin].to[FakeSocialGraphPlugin]
 
     val listenerBinder = Multibinder.newSetBinder(binder(), classOf[EventListenerPlugin])
-    listenerBinder.addBinding().to(classOf[KifiResultClickedListener])
+    listenerBinder.addBinding().to(classOf[ResultClickedListener])
     listenerBinder.addBinding().to(classOf[UsefulPageListener])
     listenerBinder.addBinding().to(classOf[SliderShownListener])
   }
