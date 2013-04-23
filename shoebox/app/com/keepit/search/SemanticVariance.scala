1753181cae8a7cb2c71d212711f65c18bd44c248
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-04-23 16:49:23 -0700

    complementing SVs for personal title only matches in svVariance computation

--- shoebox/app/com/keepit/search/SemanticVariance.scala
+++ shoebox/app/com/keepit/search/SemanticVariance.scala
@@ -4,6 +4,8 @@ import com.keepit.search.query.QueryUtil
 import com.keepit.search.index.Searcher
 import com.keepit.search.graph.URIGraphSearcher
 import org.apache.lucene.search.Query
+import com.keepit.search.index.PersonalizedSearcher
+import org.apache.lucene.index.Term
 
 object SemanticVariance {
   /**
@@ -43,24 +45,32 @@ object SemanticVariance {
   /**
    * Given a hitList, find the variance of the semantic vectors.
    */
-  def svVariance(query: Option[Query], hitList: List[MutableArticleHit], articleSearcher: Searcher, uriGraphSearcher: URIGraphSearcher) = {
-    val svSearcher = new SemanticVectorSearcher(articleSearcher, uriGraphSearcher)
+  def svVariance(query: Option[Query], hitList: List[MutableArticleHit], personalizedSearcher: Option[PersonalizedSearcher]) = {
     val uriIds = hitList.map(_.id).toSet
     val hitSize = uriIds.size
     var existCnt = List.empty[Int] // for each sv, we count how many docs contain it
-    val variance = query.map { q =>
-      val terms = QueryUtil.getTerms("sv", q)
-      var s = 0.0f
-      var cnt = 0
-      for (term <- terms) {
-        val sv = svSearcher.getSemanticVectors(term, uriIds).values
-        existCnt = sv.size::existCnt
-        // semantic vector v of terms will be concatenated from semantic vector v_i from each term
-        // avg bit variance of v is the avg of avgBitVariance of each v_i
-        s += avgBitVariance(sv, hitSize - sv.size)
-        cnt += 1
+    val variance = query.flatMap{ q =>
+      personalizedSearcher.map{ searcher =>
+        val terms = QueryUtil.getTerms("sv", q)
+        var s = 0.0f
+        var cnt = 0
+        for (term <- terms) {
+          val svMap = searcher.getSemanticVectors(term, uriIds)
+          val missing = uriIds -- svMap.keySet
+          var sv = svMap.values
+          if (!missing.isEmpty) {
+            val defaultSV = searcher.getSemanticVector(term)
+            sv ++= searcher.filterByTerm(missing, new Term("title_stemmed", term.text)).map{ id => defaultSV }
+          }
+
+          existCnt = sv.size::existCnt
+          // semantic vector v of terms will be concatenated from semantic vector v_i from each term
+          // avg bit variance of v is the avg of avgBitVariance of each v_i
+          s += avgBitVariance(sv, numMissing = (hitSize - sv.size))
+          cnt += 1
+        }
+        if (hitSize == 1) 0.0f else if (cnt > 0) s / cnt.toFloat else -1.0f
       }
-      if (hitSize == 1) 0.0f else if (cnt > 0) s / cnt.toFloat else -1.0f
     }
     val existVar =  existenceVariance(existCnt, uriIds.size)
     (variance.getOrElse(-1.0f), existVar)
