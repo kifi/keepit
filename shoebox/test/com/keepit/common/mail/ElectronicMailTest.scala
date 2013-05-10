47f9a50f5eff4f097ddd599ff82dfcbe6010a9ef
Eishay Smith <eishay@gmail.com>
2013-05-09 21:51:09 -0700

    add CC to electronic emails

--- shoebox/test/com/keepit/common/mail/ElectronicMailTest.scala
+++ shoebox/test/com/keepit/common/mail/ElectronicMailTest.scala
@@ -13,22 +13,26 @@ import com.keepit.common.db.slick._
 import com.keepit.common.db.slick.DBSession._
 
 
-class ElectronicMailTest extends Specification {
+class ElectronicMailTest extends Specification with TestDBRunner {
 
   "ElectronicMail" should {
     "user filters" in {
-      running(new ShoeboxApplication().withFakeMail()) {
-        val repo = inject[ElectronicMailRepo]
-        inject[Database].readWrite { implicit s =>
-          repo.save(ElectronicMail(from = EmailAddresses.TEAM, to = EmailAddresses.ENG, subject = "foo 1", htmlBody = "body", category = PostOffice.Categories.HEALTHCHECK))
-          repo.save(ElectronicMail(from = EmailAddresses.TEAM, to = EmailAddresses.TEAM, subject = "foo 2", htmlBody = "body", category = PostOffice.Categories.HEALTHCHECK))
-          repo.save(ElectronicMail(from = EmailAddresses.TEAM, to = EmailAddresses.EISHAY, subject = "foo 3", htmlBody = "body", category = PostOffice.Categories.HEALTHCHECK))
+      withDB(FakeMailModule()) { implicit injector =>
+        val mails = db.readWrite { implicit s =>
+          val mails = ElectronicMail(from = EmailAddresses.TEAM, to = EmailAddresses.ENG, subject = "foo 1", htmlBody = "body", category = PostOffice.Categories.HEALTHCHECK) ::
+                      ElectronicMail(from = EmailAddresses.TEAM, to = EmailAddresses.TEAM, subject = "foo 2", htmlBody = "body", category = PostOffice.Categories.HEALTHCHECK) ::
+                      ElectronicMail(from = EmailAddresses.TEAM, to = EmailAddresses.EISHAY, subject = "foo 3", htmlBody = "body", category = PostOffice.Categories.HEALTHCHECK) ::
+                      Nil
+          mails map {mail => electronicMailRepo.save(mail) }
         }
-        inject[Database].readOnly { implicit s =>
-          repo.page(0, 10, EmailAddresses.ENG).size == 2
-          repo.page(0, 2, EmailAddresses.ENG).size == 2
-          repo.count(EmailAddresses.ANDREW) === 3
-          repo.count(EmailAddresses.ENG) === 2
+        db.readOnly { implicit s =>
+          electronicMailRepo.page(0, 10, EmailAddresses.ENG).size == 2
+          electronicMailRepo.page(0, 2, EmailAddresses.ENG).size == 2
+          electronicMailRepo.count(EmailAddresses.ANDREW) === 3
+          electronicMailRepo.count(EmailAddresses.ENG) === 2
+          mails foreach { mail =>
+            electronicMailRepo.get(mail.id.get) === mail
+          }
         }
       }
     }
