5c59566b9e4e3f13ba28a154d8a65da6e72c82ff
Greg Methvin <greg@methvin.net>
2013-05-28 15:57:07 -0700

    add read-only website api for collections

--- shoebox/app/com/keepit/controllers/website/BookmarksController.scala
+++ shoebox/app/com/keepit/controllers/website/BookmarksController.scala
@@ -1,12 +1,11 @@
 package com.keepit.controllers.website
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 import com.google.inject.{Inject, Singleton}
 import com.keepit.common.controller.ActionAuthenticator
 import com.keepit.common.controller.WebsiteController
-import com.keepit.common.db.ExternalId
+import com.keepit.common.db.{Id, ExternalId}
 import com.keepit.common.db.slick.Database
 import com.keepit.common.social.{BasicUser, BasicUserRepo}
 import com.keepit.common.time._
@@ -15,11 +14,21 @@ import com.keepit.search.SearchServiceClient
 
 import play.api.libs.json._
 
+private case class BasicCollection(id: ExternalId[Collection], name: String)
+
+private object BasicCollection {
+  private implicit val externalIdFormat = ExternalId.format[Collection]
+  implicit val format = Json.format[BasicCollection]
+
+  def fromCollection(c: Collection): BasicCollection = BasicCollection(c.externalId, c.name)
+}
+
 @Singleton
 class BookmarksController @Inject() (
     db: Database,
     userRepo: UserRepo,
     bookmarkRepo: BookmarkRepo,
+    collectionRepo: CollectionRepo,
     basicUserRepo: BasicUserRepo,
     searchClient: SearchServiceClient,
     actionAuthenticator: ActionAuthenticator
@@ -41,13 +50,22 @@ class BookmarksController @Inject() (
     db.readOnly { implicit s => ExternalId.asOpt[Bookmark](id).flatMap(bookmarkRepo.getOpt) } map (_.externalId)
   }
 
-  def allKeeps(before: Option[String], after: Option[String], count: Int) = AuthenticatedJsonAction { request =>
-    (before map getBookmarkExternalId, after map getBookmarkExternalId) match {
-      case (Some(None), _) => BadRequest(s"Invalid id for before: ${before.get}")
-      case (_, Some(None)) => BadRequest(s"Invalid id for after: ${after.get}")
-      case (beforeId, afterId) => Async {
+  private def getCollectionByExternalId(userId: Id[User], id: String): Option[Collection] = {
+    db.readOnly { implicit s =>
+      ExternalId.asOpt[Collection](id).flatMap(collectionRepo.getByUserAndExternalId(userId, _))
+    }
+  }
+
+  def allKeeps(before: Option[String], after: Option[String], collection: Option[String], count: Int) =
+      AuthenticatedJsonAction { request =>
+    (before map getBookmarkExternalId, after map getBookmarkExternalId,
+        collection map { getCollectionByExternalId(request.userId, _) }) match {
+      case (Some(None), _, _) => BadRequest(s"Invalid id for before: ${before.get}")
+      case (_, Some(None), _) => BadRequest(s"Invalid id for after: ${after.get}")
+      case (_, _, Some(None)) => BadRequest(s"Invalid id for collection: ${collection.get}")
+      case (beforeId, afterId, coll) => Async {
         val keeps = db.readOnly { implicit s =>
-          bookmarkRepo.getByUser(request.userId, beforeId.flatten, afterId.flatten, count)
+          bookmarkRepo.getByUser(request.userId, beforeId.flatten, afterId.flatten, coll.flatten.map(_.id.get), count)
         }
         searchClient.sharingUserInfo(request.userId, keeps.map(_.uriId)) map { infos =>
           val idToBasicUser = db.readOnly { implicit s =>
@@ -56,6 +74,7 @@ class BookmarksController @Inject() (
           (keeps zip infos).map { case (keep, info) => (keep, info.sharingUserIds map idToBasicUser) }
         } map { keepsWithKeepers =>
           Ok(Json.obj(
+            "collection" -> coll.flatten.map(BasicCollection fromCollection _),
             "before" -> before,
             "after" -> after,
             "keeps" -> keepsWithKeepers
@@ -63,6 +82,14 @@ class BookmarksController @Inject() (
         }
       }
     }
+  }
+
+  def allCollections() = AuthenticatedJsonAction { request =>
+    Ok(Json.obj(
+      "collections" -> db.readOnly { implicit s =>
+        collectionRepo.getByUser(request.userId).map(BasicCollection fromCollection _)
+      }
+    ))
   }
 
   def numKeeps() = AuthenticatedJsonAction { request =>
