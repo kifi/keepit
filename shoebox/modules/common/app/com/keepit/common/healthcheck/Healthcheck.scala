d37b8162e2bab6327845ad923b3a2b991211b9e7
Léo <leo@42go.com>
2013-06-07 12:21:15 -0700

    Fixes deployment records (hopefully)

--- shoebox/modules/common/app/com/keepit/common/healthcheck/Healthcheck.scala
+++ shoebox/modules/common/app/com/keepit/common/healthcheck/Healthcheck.scala
@@ -25,7 +25,6 @@ import akka.util.Timeout
 import play.api.Plugin
 import play.api.templates.Html
 import com.keepit.common.mail.RemotePostOffice
-import play.modules.statsd.api.Statsd
 
 object Healthcheck {
 
@@ -184,7 +183,6 @@ class HealthcheckPluginImpl @Inject() (
   }
 
   override def reportStart() = {
-    Statsd.gauge("deploys", 1)
     val subject = s"Service ${services.currentService} started"
     val message = Html(s"Service version ${services.currentVersion} started at ${currentDateTime} on $host. Service compiled at ${services.compilationTime}")
     val email = (ElectronicMail(from = EmailAddresses.ENG, to = List(EmailAddresses.ENG),
