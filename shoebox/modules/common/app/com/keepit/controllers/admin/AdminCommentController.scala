8224b15d1727e42a89e77f79bd3149e9a94a3286
Greg Methvin <greg@methvin.net>
2013-06-06 17:15:35 -0700

    Remove UserWithSocial

--- shoebox/modules/common/app/com/keepit/controllers/admin/AdminCommentController.scala
+++ shoebox/modules/common/app/com/keepit/controllers/admin/AdminCommentController.scala
@@ -1,34 +1,12 @@
 package com.keepit.controllers.admin
 
-import java.sql.Connection
-import scala.Option.option2Iterable
-import scala.math.BigDecimal.long2bigDecimal
-import play.api.Play.current
-import com.google.inject.{Inject, ImplementedBy, Singleton}
-import com.keepit.common.db._
-import com.keepit.common.db.slick._
-import com.keepit.common.db.slick.DBSession._
+import com.google.inject.{Inject, Singleton}
 import com.keepit.common.controller.{AdminController, ActionAuthenticator}
-import com.keepit.common.mail.{ElectronicMail, EmailAddresses, PostOffice}
-import com.keepit.common.social._
+import com.keepit.common.db.slick._
 import com.keepit.model._
-import com.keepit.search.graph.URIGraph
-import com.keepit.search.index.ArticleIndexer
-import play.api.http.ContentTypes
-import play.api.libs.concurrent.Akka
-import play.api.libs.json.{JsArray, JsBoolean, JsNumber, JsObject, JsString}
-import play.api.mvc.Action
-import play.api.mvc.Controller
-import securesocial.core.SecureSocial
-import securesocial.core.java.SecureSocial.SecuredAction
-import com.keepit.common.healthcheck.BabysitterTimeout
-import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json._
-import com.keepit.common.time._
-import org.joda.time.DateTime
-import com.keepit.common.analytics.ActivityStream
-import views.html
 import com.keepit.realtime.UserNotifier
+
+import views.html
 
 @Singleton
 class AdminCommentController @Inject() (
@@ -37,7 +15,6 @@ class AdminCommentController @Inject() (
   commentRepo: CommentRepo,
   commentRecipientRepo: CommentRecipientRepo,
   normalizedURIRepo: NormalizedURIRepo,
-  userWithSocialRepo: UserWithSocialRepo,
   followRepo: FollowRepo,
   userRepo: UserRepo,
   userNotifier: UserNotifier)
@@ -45,8 +22,7 @@ class AdminCommentController @Inject() (
 
   def followsView = AdminHtmlAction { implicit request =>
     val uriAndUsers = db.readOnly { implicit s =>
-      followRepo.all() map {f =>
-        (userWithSocialRepo.toUserWithSocial(userRepo.get(f.userId)), f, normalizedURIRepo.get(f.uriId))
+      followRepo.all() map {f => (userRepo.get(f.userId), f, normalizedURIRepo.get(f.uriId))
       }
     }
     Ok(html.admin.follows(uriAndUsers))
@@ -59,9 +35,7 @@ class AdminCommentController @Inject() (
     val (count, uriAndUsers) = db.readOnly { implicit s =>
       val comments = commentRepo.page(page, PAGE_SIZE)
       val count = commentRepo.count(CommentPermissions.PUBLIC)
-      (count, (comments map {
-        co => (userWithSocialRepo.toUserWithSocial(userRepo.get(co.userId)), co, normalizedURIRepo.get(co.uriId))
-      }))
+      (count, (comments map { co => (userRepo.get(co.userId), co, normalizedURIRepo.get(co.uriId)) }))
     }
     val pageCount: Int = (count / PAGE_SIZE + 1).toInt
     Ok(html.admin.comments(uriAndUsers, page, count, pageCount))
@@ -75,15 +49,15 @@ class AdminCommentController @Inject() (
       val messages = commentRepo.page(page, PAGE_SIZE, CommentPermissions.MESSAGE)
       val count = commentRepo.count(CommentPermissions.MESSAGE)
       (count, (messages map {co =>
-        (userWithSocialRepo.toUserWithSocial(userRepo.get(co.userId)), co, normalizedURIRepo.get(co.uriId), commentRecipientRepo.getByComment(co.id.get) map { r =>
-          userWithSocialRepo.toUserWithSocial(userRepo.get(r.userId.get))
+        (userRepo.get(co.userId), co, normalizedURIRepo.get(co.uriId), commentRecipientRepo.getByComment(co.id.get) map { r =>
+          userRepo.get(r.userId.get)
         })
       }))
     }
     val pageCount: Int = (count / PAGE_SIZE + 1).toInt
     Ok(html.admin.messages(uriAndUsers, page, count, pageCount))
   }
-  
+
   def recreateNotificationDetails(safeMode: Boolean) = AdminHtmlAction { request =>
     db.readWrite { implicit session =>
       userNotifier.recreateAllActiveDetails(safeMode)
