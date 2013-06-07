a73c1c9a45fcd3f5469717fa2d9d58b6e3c74d38
Andrew Conner <git@andrewconner.org>
2013-06-06 17:15:29 -0700

    Removing unused conf files.

--- shoebox/conf/evolutions/shoebox/34.sql
+++
@@ -1,7 +0,0 @@
-# --- !Ups
-
-CREATE INDEX manual_sensitive_index ON domain (state, manual_sensitive);
-
-INSERT INTO evolutions (name, description) VALUES ('34.sql', 'index on manual_sensitive');
-
-# --- !Downs
