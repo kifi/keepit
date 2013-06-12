cb72db345a562a68756159fec917a696bbb40768
Andrew Conner <git@andrewconner.org>
2013-06-12 15:10:14 -0700

    Controllers in shoebox

--- kifi-backend/modules/common/app/com/keepit/controllers/ext/ExtAuthController.scala
+++ kifi-backend/modules/common/app/com/keepit/controllers/ext/ExtAuthController.scala
@@ -89,7 +89,7 @@ class ExtAuthController @Inject() (
   // where SecureSocial sends users if it can't figure out the right place (see securesocial.conf)
   def welcome = AuthenticatedJsonAction { implicit request =>
     log.debug("in welcome. with user : [ %s ]".format(request.identity))
-    Redirect(com.keepit.controllers.website.routes.HomeController.home())
+    Redirect("/")
   }
 
   // TODO: Fix logOut. ActionAuthenticator currently sets a new session cookie after this action clears it.
