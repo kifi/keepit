420985e95506fdcfd19c1e511f3f740b95c3000f
Andrew Conner <git@andrewconner.org>
2013-06-14 11:53:16 -0700

    New ServiceRouter pattern

--- kifi-backend/modules/common/app/com/keepit/common/routes/Router.scala
+++ kifi-backend/modules/common/app/com/keepit/common/routes/Router.scala
@@ -1,11 +1,34 @@
+package com.keepit.common.routes
 
-object ServiceRouter {
-  // A static object so that we can use this easily anywhere inside of Play
 
-  def apply(key: String): String = {
-
-    key match {
-      case "logout" => "/logout"
-    }
-  }
+trait Service
+case class ServiceRoute(method: Method, path: String, params: Param*) {
+  override def toString = path + (if(params.nonEmpty) params.map({ p =>
+    p.key + (if(p.value.value != "") "=" + p.value.value else "")
+  }).mkString("&") else "")
 }
+
+case class Param(key: String, value: ParamValue = ParamValue(""))
+case class ParamValue(value: String)
+object ParamValue {
+  implicit def stringToParam(i: String) = ParamValue(i)
+  implicit def longToParam(i: Long) = ParamValue(i.toString)
+}
+
+abstract class Method(name: String)
+case object GET extends Method("GET")
+case object POST extends Method("POST")
+case object PUT extends Method("PUT")
+
+object shoebox extends Service {
+  object service {
+    def getNormalizedURI(id: Long) = ServiceRoute(GET, "internal/shoebox/database/getNormalizedURI", Param("id", id))
+  }
+
+
+}
+
+object search extends Service {
+
+}
+
