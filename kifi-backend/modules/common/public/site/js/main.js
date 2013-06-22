c4fdce7d29b5a916bb6a8d36b3354dd5ab57f263
Jared Jacobs <jared@42go.com>
2013-06-21 17:37:16 -0700

    only putting focus on search query input if key pressed is number or letter

--- kifi-backend/modules/common/public/site/js/main.js
+++ kifi-backend/modules/common/public/site/js/main.js
@@ -454,7 +454,7 @@ $(function() {
 	});
 
 	$(document).keydown(function(e) {  // auto focus on search field when starting to type anywhere on the document
-		if (!$(e.target).is('input,textarea')) {
+		if (!$(e.target).is('input,textarea') && e.which >= 48 && e.which <= 90 && !e.ctrlKey && !e.metaKey && !e.altKey) {
 			$query.focus();
 		}
 	});
