3b628dcf01126a787739fe9efc17a9e36828a58d
yingjieMiao <yingjie@42go.com>
2013-06-25 16:32:28 -0700

    thresholds

--- kifi-backend/modules/shoebox/app/com/keepit/learning/topicmodel/TopicModelGlobal.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/learning/topicmodel/TopicModelGlobal.scala
@@ -3,5 +3,5 @@ package com.keepit.learning.topicmodel
 object TopicModelGlobal {
   val numTopics = 100
   val primaryTopicThreshold = 0.1       // need to tune this as numTopics varies
-  val topicTailcut = 0.6
+  val topicTailcut = 0.5
 }
