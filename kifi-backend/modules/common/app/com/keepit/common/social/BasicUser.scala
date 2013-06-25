3ffe0709161cd1d56af2b42ad1f21aa9539411a1
Greg Methvin <greg@methvin.net>
2013-06-24 20:35:11 -0700

    Remove networkIds from BasicUser and add new friend tooltip request

--- kifi-backend/modules/common/app/com/keepit/common/social/BasicUser.scala
+++ kifi-backend/modules/common/app/com/keepit/common/social/BasicUser.scala
@@ -1,19 +1,19 @@
 package com.keepit.common.social
 
 
+import scala.concurrent.duration.Duration
+
+import com.keepit.common.cache.{JsonCacheImpl, FortyTwoCachePlugin, Key}
 import com.keepit.common.db._
 import com.keepit.model._
 
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
-import com.keepit.common.cache.{JsonCacheImpl, FortyTwoCachePlugin, Key}
-import scala.concurrent.duration.Duration
 
 case class BasicUser(
   externalId: ExternalId[User],
   firstName: String,
   lastName: String,
-  networkIds: Map[SocialNetworkType, SocialId],
   pictureName: String)
 
 object BasicUser {
@@ -22,12 +22,17 @@ object BasicUser {
       (__ \ 'id).format[ExternalId[User]] and
       (__ \ 'firstName).format[String] and
       (__ \ 'lastName).format[String] and
-      (__ \ 'networkIds).format[Map[String, String]].inmap[Map[SocialNetworkType,SocialId]](
-        _.map { case (netStr, idStr) => SocialNetworkType(netStr) -> SocialId(idStr) }.toMap,
-        _.map { case (network, id) => network.name -> id.id }.toMap
-      ) and
       (__ \ 'pictureName).format[String]
   )(BasicUser.apply, unlift(BasicUser.unapply))
+
+  def fromUser(user: User): BasicUser = {
+    BasicUser(
+      externalId = user.externalId,
+      firstName = user.firstName,
+      lastName = user.lastName,
+      pictureName = "0.jpg" // TODO: when we have multiple picture IDs make sure we change this
+    )
+  }
 }
 
 case class BasicUserUserIdKey(userId: Id[User]) extends Key[BasicUser] {
