d1a33a663825bb188d2766dc717b2bdb3f3830b4
yingjieMiao <yingjie@42go.com>
2013-07-05 15:24:27 -0700

    switchable topic model accessor

--- kifi-backend/modules/shoebox/app/com/keepit/learning/topicmodel/TopicModelAccessor.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/learning/topicmodel/TopicModelAccessor.scala
@@ -19,7 +19,7 @@ class TopicModelAccessorA @Inject()(
   val userTopicRepo: UserTopicRepoA,
   val uriTopicRepo: UriTopicRepoA,
   val topicSeqInfoRepo: TopicSeqNumInfoRepoA,
-  val topicNameRepoA: TopicNameRepoA,
+  val topicNameRepo: TopicNameRepoA,
   val documentTopicModel: DocumentTopicModel
 ) extends TopicModelAccessor
 
@@ -28,6 +28,38 @@ class TopicModelAccessorB @Inject()(
   val userTopicRepo: UserTopicRepoB,
   val uriTopicRepo: UriTopicRepoB,
   val topicSeqInfoRepo: TopicSeqNumInfoRepoB,
-  val topicNameRepoB: TopicNameRepoB,
+  val topicNameRepo: TopicNameRepoB,
   val documentTopicModel: DocumentTopicModel
 ) extends TopicModelAccessor
+
+object TopicModelAccessorFlag {
+  val A = "a"
+  val B = "b"
+}
+
+@Singleton
+class SwitchableTopicModelAccessor @Inject()(
+  val accessorA: TopicModelAccessorA,
+  val accessorB: TopicModelAccessorB
+) {
+  var currentAccessor = TopicModelAccessorFlag.A       // will read this from configuration
+
+  // this is the one in use
+  def getActiveAccessor = currentAccessor match {
+    case TopicModelAccessorFlag.A  => accessorA
+    case TopicModelAccessorFlag.B  => accessorB
+  }
+
+  // this one will only be used during re-model procedure, when a new topic model is available
+  def getInactiveAccessor = currentAccessor match {
+    case TopicModelAccessorFlag.A  => accessorB
+    case TopicModelAccessorFlag.B  => accessorA
+  }
+
+  def switchAccessor() = {
+    currentAccessor match {
+      case TopicModelAccessorFlag.A  => currentAccessor = TopicModelAccessorFlag.B
+      case TopicModelAccessorFlag.B  => currentAccessor = TopicModelAccessorFlag.A
+    }
+  }
+}
