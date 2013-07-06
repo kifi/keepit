00d26153ddb16b67cd23982b3719b35cbcca6ac1
Léo <leo@42go.com>
2013-07-05 20:05:14 -0700

    Moves DomainClassifier to Shoebox Module

--- kifi-backend/modules/common/app/com/keepit/classify/DomainTag.scala
+++ kifi-backend/modules/common/app/com/keepit/classify/DomainTag.scala
@@ -39,34 +39,4 @@ object DomainTagName {
   def isBlacklisted(dt: DomainTagName): Boolean = blacklist contains dt
 }
 
-@ImplementedBy(classOf[DomainTagRepoImpl])
-trait DomainTagRepo extends Repo[DomainTag] {
-  def getTags(tagIds: Seq[Id[DomainTag]], excludeState: Option[State[DomainTag]] = Some(DomainTagStates.INACTIVE))
-      (implicit session: RSession): Seq[DomainTag]
-  def get(tag: DomainTagName, excludeState: Option[State[DomainTag]] = Some(DomainTagStates.INACTIVE))
-      (implicit session: RSession): Option[DomainTag]
-}
-
-@Singleton
-class DomainTagRepoImpl @Inject()(val db: DataBaseComponent, val clock: Clock) extends DbRepo[DomainTag] with DomainTagRepo {
-
-  import DBSession._
-  import FortyTwoTypeMappers._
-  import db.Driver.Implicit._
-
-  override val table = new RepoTable[DomainTag](db, "domain_tag") {
-    def name = column[DomainTagName]("name", O.NotNull)
-    def sensitive = column[Option[Boolean]]("sensitive", O.Nullable)
-    def * = id.? ~ name ~ sensitive ~ state ~ createdAt ~ updatedAt <> (DomainTag, DomainTag.unapply _)
-  }
-
-  def getTags(tagIds: Seq[Id[DomainTag]], excludeState: Option[State[DomainTag]] = Some(DomainTagStates.INACTIVE))
-      (implicit session: RSession): Seq[DomainTag] =
-    (for (t <- table if t.state =!= excludeState.getOrElse(null) && t.id.inSet(tagIds)) yield t).list
-
-  def get(tag: DomainTagName, excludeState: Option[State[DomainTag]] = Some(DomainTagStates.INACTIVE))
-      (implicit session: RSession): Option[DomainTag] =
-    (for (t <- table if t.state =!= excludeState.getOrElse(null) && t.name === tag) yield t).firstOption
-}
-
 object DomainTagStates extends States[DomainTag]
--- kifi-backend/modules/common/app/com/keepit/classify/DomainToTag.scala
+++ kifi-backend/modules/common/app/com/keepit/classify/DomainToTag.scala
@@ -22,48 +22,4 @@ case class DomainToTag(
   def withState(state: State[DomainToTag]) = this.copy(state = state)
 }
 
-@ImplementedBy(classOf[DomainToTagRepoImpl])
-trait DomainToTagRepo extends Repo[DomainToTag] {
-  def getByTag(tagId: Id[DomainTag], excludeState: Option[State[DomainToTag]] = Some(DomainToTagStates.INACTIVE))
-      (implicit session: RSession): Seq[DomainToTag]
-  def getByDomain(domainId: Id[Domain], excludeState: Option[State[DomainToTag]] = Some(DomainToTagStates.INACTIVE))
-      (implicit session: RSession): Seq[DomainToTag]
-  def insertAll(domainToToTags: Seq[DomainToTag])(implicit session: RWSession): Option[Int]
-  def getDomainsChangedSince(dateTime: DateTime)(implicit session: RSession): Set[Id[Domain]]
-  def setState(domainToTagIds: Seq[Id[DomainToTag]], state: State[DomainToTag])(implicit session: RWSession): Int
-}
-
-@Singleton
-class DomainToTagRepoImpl @Inject()(val db: DataBaseComponent, val clock: Clock)
-  extends DbRepo[DomainToTag] with DomainToTagRepo {
-
-  import DBSession._
-  import FortyTwoTypeMappers._
-  import db.Driver.Implicit._
-
-  override val table = new RepoTable[DomainToTag](db, "domain_to_tag") {
-    def domainId = column[Id[Domain]]("domain_id", O.NotNull)
-    def tagId = column[Id[DomainTag]]("tag_id", O.NotNull)
-    def * = id.? ~ domainId ~ tagId ~ state ~ createdAt ~ updatedAt <> (DomainToTag, DomainToTag.unapply _)
-  }
-
-  def getByDomain(domainId: Id[Domain], excludeState: Option[State[DomainToTag]] = Some(DomainToTagStates.INACTIVE))
-      (implicit session: RSession): Seq[DomainToTag] =
-    (for (t <- table if t.domainId === domainId && t.state =!= excludeState.getOrElse(null)) yield t).list
-
-  def getByTag(tagId: Id[DomainTag], excludeState: Option[State[DomainToTag]] = Some(DomainToTagStates.INACTIVE))
-      (implicit session: RSession): Seq[DomainToTag] =
-    (for (t <- table if t.tagId === tagId && t.state =!= excludeState.getOrElse(null)) yield t).list
-
-  def insertAll(domainToTags: Seq[DomainToTag])(implicit session: RWSession): Option[Int] =
-    table.insertAll(domainToTags: _*)
-
-  def setState(domainToTagIds: Seq[Id[DomainToTag]], state: State[DomainToTag])
-      (implicit session: RWSession): Int =
-    (for (t <- table if t.id inSet domainToTagIds) yield t.state).update(state)
-
-  def getDomainsChangedSince(dateTime: DateTime)(implicit session: RSession): Set[Id[Domain]] =
-    (for (t <- table if t.updatedAt >= dateTime) yield t.domainId).elements.toSet
-}
-
 object DomainToTagStates extends States[DomainToTag]
