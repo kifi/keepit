c08bb76931483fc2132ed03511454d093510960b
yingjieMiao <yingjie@42go.com>
2013-07-10 10:23:44 -0700

    cleaner code

--- kifi-backend/modules/common/app/com/keepit/learning/topicmodel/WordTopicStore.scala
+++ kifi-backend/modules/common/app/com/keepit/learning/topicmodel/WordTopicStore.scala
@@ -9,6 +9,7 @@ import java.io.InputStream
 import com.keepit.common.strings._
 import com.keepit.common.store.InMemoryObjectStore
 import com.keepit.common.store.ObjectStore
+import scala.io.Source
 
 // a plain txt file store, fileName -> fileContent
 trait WordTopicStore extends ObjectStore[String, String]
@@ -19,7 +20,7 @@ class S3WordTopicStoreImpl(val bucketName: S3Bucket, val amazonS3Client: AmazonS
   def unpackValue(s3Obj : S3Object) : String = {
     val is = s3Obj.getObjectContent
     try {
-      val jsonString = scala.io.Source.fromInputStream(is, UTF8).getLines().mkString("\n")
+      val jsonString = Source.fromInputStream(is, UTF8).getLines().mkString("\n")
       jsonString                         // will convert the string to model elsewhere
     } finally {
       is.close
