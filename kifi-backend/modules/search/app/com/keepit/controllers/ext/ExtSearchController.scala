e4a8d02857b7da12097af748b32b3e90911b27aa
yingjieMiao <yingjie@42go.com>
2013-07-19 10:01:03 -0700

    don't suggest expert if only 1 user

--- kifi-backend/modules/search/app/com/keepit/controllers/ext/ExtSearchController.scala
+++ kifi-backend/modules/search/app/com/keepit/controllers/ext/ExtSearchController.scala
@@ -198,7 +198,11 @@ class ExtSearchController @Inject() (
     val urisAndUsers = searchRes.hits.map{ hit =>
       (hit.uriId, hit.users)
     }
-    shoeboxClient.suggestExperts(urisAndUsers)
+    if (urisAndUsers.map{_._2}.flatten.size < 2){
+      Promise.successful(List.empty[Id[User]]).future
+    } else{
+      shoeboxClient.suggestExperts(urisAndUsers)
+    }
   }
 
 }
