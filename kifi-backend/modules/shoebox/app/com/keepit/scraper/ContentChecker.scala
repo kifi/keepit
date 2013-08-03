3938cb1a6ed5d5bb4406a5dceeed9f13b765a06a
yingjieMiao <yingjie@42go.com>
2013-08-02 17:27:26 -0700

    content checker

---
+++ kifi-backend/modules/shoebox/app/com/keepit/scraper/ContentChecker.scala
@@ -0,0 +1,100 @@
+package com.keepit.scraper
+
+import com.google.inject.Inject
+import com.keepit.scraper.extractor.Extractor
+import com.keepit.scraper.extractor.DefaultExtractorProvider
+import scala.util.{Success, Failure}
+import com.keepit.common.net.URI
+import com.keepit.common.logging.Logging
+import com.keepit.model.UnscrapableRepo
+import com.keepit.common.db.slick._
+import org.apache.http.HttpStatus
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+
+trait ContentChecker {
+  def check(urls: List[String]): Either[ContentCheckFail, ContentCheckSuccess]
+}
+
+case class ContentCheckSuccess(finalUrl: String)
+case class ContentCheckFail(urls: List[String])
+
+class ContentCheckerImpl @Inject()(
+  db: Database,
+  httpFetcher: HttpFetcher,
+  unscrapableRepo: UnscrapableRepo
+) extends ContentChecker with Logging {
+
+  override def check(urls: List[String]): Either[ContentCheckFail, ContentCheckSuccess] = {
+    val resultsFuture = Future.sequence(urls.map{ url => future{getHashCode(url)}})
+    val rv = resultsFuture.map{ results =>
+      results.forall(x => x.isRight) match {
+        case false => Left[ContentCheckFail, ContentCheckSuccess](ContentCheckFail(urls))
+        case true => {
+          val hashes = results.flatMap{
+            case Right(hash) => Some(hash)
+            case Left(msg) => None
+          }
+          hashes.distinct.size == hashes.size match {
+            case true => Right[ContentCheckFail, ContentCheckSuccess](ContentCheckSuccess(urls.head))     // lack of total order yet.
+            case false => Left[ContentCheckFail, ContentCheckSuccess](ContentCheckFail(urls))
+          }
+        }
+      }
+    }
+    Await.result(rv, 5 minutes)
+  }
+
+  private def getHashCode(url: String): Either[String, Int] = {
+    val extractor = getExtractor(url)
+    try {
+      val fetchStatus = httpFetcher.fetch(url) { input => extractor.process(input) }
+
+      fetchStatus.statusCode match {
+        case HttpStatus.SC_OK =>
+          if (isUnscrapable(url, fetchStatus.destinationUrl)) {
+            Left("not_scrapable")
+          } else {
+            val total = extractor.getContent + getTitle(extractor) + getDescription(extractor)
+            Right(total.hashCode)
+          }
+        case _ => Left("fetch_failed")
+      }
+    } catch {
+      case e: Throwable => Left("fetch_failed")
+    }
+  }
+
+  private def getExtractor(url: String): Extractor = {
+    try {
+      URI.parse(url) match {
+        case Success(uri) =>
+          Extractor.factories.find(_.isDefinedAt(uri)).map{ f =>
+            f.apply(uri)
+          }.getOrElse(throw new Exception("failed to find an extractor factory"))
+        case Failure(_) =>
+          log.warn("uri parsing failed: [%s]".format(url))
+          DefaultExtractorProvider(url)
+      }
+    } catch {
+      case e: Throwable =>
+          log.warn("uri parsing failed: [%s][%s]".format(url, e.toString))
+          DefaultExtractorProvider(url)
+    }
+  }
+
+  protected def isUnscrapable(url: String, destinationUrl: Option[String]) = {
+    db.readOnly { implicit s =>
+      (unscrapableRepo.contains(url) || (destinationUrl.isDefined && unscrapableRepo.contains(destinationUrl.get)))
+    }
+  }
+
+  private[this] def getTitle(x: Extractor): String = {
+    x.getMetadata("title").getOrElse("")
+  }
+  private[this] def getDescription(x: Extractor): Option[String] = {
+    x.getMetadata("description").orElse(x.getMetadata("Description")).orElse(x.getMetadata("DESCRIPTION"))
+  }
+}
