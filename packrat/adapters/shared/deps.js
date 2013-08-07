eb07344f83eb5e5ea8621f9d7bba142c6a42f578
Jared Jacobs <jared@42go.com>
2013-08-07 09:58:44 -0700

    finding transitive closure of script deps again

--- packrat/adapters/shared/deps.js
+++ packrat/adapters/shared/deps.js
@@ -1,18 +1,36 @@
-var meta = meta || require("./meta");
+!function(exports, meta) {
+	exports.deps = function(path, injected) {
+		injected = injected || {};
+		function notYetInjected(path) {
+			return !injected[path];
+		}
 
-(this.exports || this).deps = function(path, injected) {
-  injected = injected || {};
-  function notYetInjected(path) {
-    return !injected[path];
-  }
+		if (path.substr(-4) === ".css") {
+			return {scripts: [], styles: injected[path] ? [] : [path]};
+		}
 
-  if (path.substr(-4) === ".css") {
-    return {scripts: [], styles: injected[path] ? [] : [path]};
-  }
+		var scripts = Object.keys([path].reduce(transitiveScriptDeps, {})).filter(notYetInjected);
+		return {
+			scripts: scripts,
+			styles: Object.keys(scripts.reduce(styleDeps, {})).filter(notYetInjected)};
+	};
 
-  var styles = meta.styleDeps[path];
-  var scripts = meta.scriptDeps[path];
-  return {
-    styles: styles ? styles.filter(notYetInjected) : [],
-    scripts: scripts ? scripts.concat([path]).filter(notYetInjected) : [path]};
-};
+	function transitiveScriptDeps(o, path) {
+		var deps = meta.scriptDeps[path];
+		if (deps) {
+			deps.reduce(transitiveScriptDeps, o);
+		}
+		o[path] = true;
+		return o;
+	}
+
+	function styleDeps(o, path) {
+		var deps = meta.styleDeps[path];
+		if (deps) {
+			for (var i = 0; i < deps.length; i++) {
+				o[deps[i]] = true;
+			}
+		}
+		return o;
+	}
+}(this.exports || this, this.meta || require("./meta"));
