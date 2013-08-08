3142a0db26ef7177a335b0e71296bc6e70447566
Léo Grimaldi <leo@42go.com>
2013-08-08 10:20:34 -0700

    Implements NormalizationService.normalize

--- kifi-backend/modules/shoebox/test/com/keepit/normalizer/StaticNormalizationServiceModule.scala
+++
@@ -1,9 +0,0 @@
-package com.keepit.normalizer
-
-import net.codingwell.scalaguice.ScalaModule
-
-case class StaticNormalizationServiceModule() extends ScalaModule {
-  def configure() = {
-    bind[NormalizationService].toInstance(StaticNormalizationService)
-  }
-}
