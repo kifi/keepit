e1ff5e1e2a470d3005f0feecafa0df5744c24009
Eishay Smith <eishay@gmail.com>
2013-08-09 21:49:15 -0700

    renaming actor holder

---
+++ kifi-backend/modules/common/app/com/keepit/common/actor/ActorInstance.scala
@@ -0,0 +1,26 @@
+package com.keepit.common.actor
+
+import akka.actor._
+import akka.testkit.TestActorRef
+import com.google.inject.{ImplementedBy, Provider, Inject, Singleton}
+import com.keepit.common.akka.AlertingActor
+
+@Singleton
+class ActorInstance[T <: Actor] @Inject() (
+    systemProvider: Provider[ActorSystem],
+    builder: ActorBuilder,
+    provider: Provider[T]) {
+
+  lazy val system: ActorSystem = systemProvider.get
+  lazy val ref: ActorRef = builder(system, provider)
+}
+
+@ImplementedBy(classOf[ActorBuilderImpl])
+trait ActorBuilder {
+  def apply(system: ActorSystem, provider: Provider[_ <: Actor]): ActorRef
+}
+
+class ActorBuilderImpl extends ActorBuilder {
+  def apply(system: ActorSystem, provider: Provider[_ <: Actor]): ActorRef =
+    system.actorOf(Props { provider.get })
+}
