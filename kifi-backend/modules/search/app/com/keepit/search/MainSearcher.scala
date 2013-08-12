73a00ddeaf73c76d8cc75104e5feb3b7be23ae1e
yingjieMiao <yingjie@42go.com>
2013-08-12 14:34:05 -0700

    tune classifier

--- kifi-backend/modules/search/app/com/keepit/search/MainSearcher.scala
+++ kifi-backend/modules/search/app/com/keepit/search/MainSearcher.scala
@@ -418,8 +418,7 @@ class MainSearcher(
   private def classify(hitList: List[MutableArticleHit], personalizedSearcher: PersonalizedSearcher) = {
     val semanticScoreThreshold = (1.0f - semanticBoost) + semanticBoost * 0.2f
     def classify(hit: MutableArticleHit) = {
-      hit.isMyBookmark ||
-      (hit.clickBoost) > 1.25f ||
+      (hit.clickBoost) > 1.05f ||
       hit.scoring.recencyScore > 0.25f ||
       hit.scoring.textScore > 0.7f ||
       (hit.scoring.textScore >= 0.04f && hit.semanticScore >= semanticScoreThreshold)
