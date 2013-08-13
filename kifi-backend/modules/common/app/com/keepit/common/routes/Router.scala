8c4a059fe8dc859d744f9c379b3d4bd88e99e549
Stephen Kemmerling <stephen@42go.com>
2013-08-13 13:38:45 -0700

    Send user broadcasts form shoebox through eliza

--- kifi-backend/modules/common/app/com/keepit/common/routes/Router.scala
+++ kifi-backend/modules/common/app/com/keepit/common/routes/Router.scala
@@ -137,6 +137,7 @@ object Eliza extends Service {
   object internal {
     def sendToUserNoBroadcast() = ServiceRoute(POST, "/internal/eliza/sendToUserNoBroadcast")
     def sendToUser() = ServiceRoute(POST, "/internal/eliza/sendToUser")
+    def sendToAllUsers() = ServiceRoute(POST, "/internal/eliza/sendToAllUsers")
     def importThread() = ServiceRoute(POST, "/internal/eliza/importThread")
   }
 }
