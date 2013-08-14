7316debe2a9986e5496e60145584b0f0d2e6d733
Jared Jacobs <jared@42go.com>
2013-08-14 15:40:15 -0700

    fixing status check more better

--- packrat/adapters/chrome/api.js
+++ packrat/adapters/chrome/api.js
@@ -451,7 +451,7 @@ api = function() {
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() {
         if (this.readyState == 4) {
-          if (this.status % 100 === 2) {
+          if (this.status >= 200 && this.status < 300) {
             done && done(/^application\/json/.test(this.getResponseHeader("Content-Type")) ? JSON.parse(this.responseText) : this);
           } else if (fail) {
             fail(this);
