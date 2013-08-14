e5e47269a5aaab31631cf99b705156ee81c68654
Andrew Conner <git@andrewconner.org>
2013-08-14 16:10:58 -0700

    Nonworking MonitoredExecutionContext

--- kifi-backend/modules/common/app/com/keepit/common/akka/SlowRunningExecutionContext.scala
+++
@@ -1,9 +0,0 @@
-package com.keepit.common.akka
-
-import scala.concurrent.ExecutionContext
-import play.api.libs.concurrent.Akka
-import play.api.Play.current
-
-object SlowRunningExecutionContext {
-  implicit val ec: ExecutionContext = Akka.system.dispatchers.lookup("slow-running-execution-context")
-}
