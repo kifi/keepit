ed14f15a9feba93ac0bd9aff3d97430ea82a9f9b
Andrew Conner <git@andrewconner.org>
2013-08-14 19:04:17 -0700

    Slow running cleanup

--- kifi-backend/modules/common/app/com/keepit/common/akka/SlowRunningExecutionContext.scala
+++ kifi-backend/modules/common/app/com/keepit/common/akka/SlowRunningExecutionContext.scala
@@ -1,13 +1,8 @@
 package com.keepit.common.akka
 
-import scala.concurrent.{ExecutionContextExecutor, ExecutionContext}
+import scala.concurrent.ExecutionContext
 import play.api.libs.concurrent.Akka
 import play.api.Play.current
-import java.util.concurrent.Executor
-import play.api.Play.current
-import play.api.Application
-import com.keepit.FortyTwoGlobal
-import com.keepit.common.healthcheck.{Healthcheck, HealthcheckError, HealthcheckPlugin}
 
 object SlowRunningExecutionContext {
   implicit val ec: ExecutionContext = Akka.system.dispatchers.lookup("slow-running-execution-context")
