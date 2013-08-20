dbb24be1fdb32dd95a82f0e5ed6469c62d8a44eb
Andrew Conner <git@andrewconner.org>
2013-08-20 14:33:17 -0700

    Untapping

--- kifi-backend/modules/common/app/com/keepit/common/akka/SafeFuture.scala
+++ kifi-backend/modules/common/app/com/keepit/common/akka/SafeFuture.scala
@@ -6,29 +6,27 @@ import scala.util.Try
 import play.api.{Logger}
 import com.keepit.FortyTwoGlobal
 import com.keepit.common.healthcheck.{Healthcheck, HealthcheckError, HealthcheckPlugin}
-import com.keepit.common.KestrelCombinator
 import play.api.Play.current
 
 class SafeFuture[+T](future: Future[T], name: Option[String] = None)(implicit executor: ExecutionContext) extends Future[T] {
-  private val safeFuture = future tap { f =>
-    f match {
-      case _: SafeFuture[_] =>
-      case dangerousFuture =>
-        dangerousFuture.onFailure {
-          case cause: Throwable =>
-            cause.printStackTrace() // should always work, to stdout
-            try {
-              // Needs a running Play application. May fail.
-              Logger(getClass).error(s"[SafeFuture] Failure of future${name.map(": " + _).getOrElse("")}", cause)
-              val fortyTwoInjector = current.global.asInstanceOf[FortyTwoGlobal].injector
-              fortyTwoInjector.getInstance(classOf[HealthcheckPlugin]).addError(
-                HealthcheckError(Some(cause), None, None, Healthcheck.INTERNAL, Some(s"[SafeFuture]: ${cause.getMessage}"))
-              )
-            } catch {
-              case _: Throwable => // tried our best.
-            }
-        }
-    }
+
+  future match {
+    case _: SafeFuture[_] =>
+    case dangerousFuture =>
+      dangerousFuture.onFailure {
+        case cause: Throwable =>
+          cause.printStackTrace() // should always work, to stdout
+          try {
+            // Needs a running Play application. May fail.
+            Logger(getClass).error(s"[SafeFuture] Failure of future${name.map(": " + _).getOrElse("")}", cause)
+            val fortyTwoInjector = current.global.asInstanceOf[FortyTwoGlobal].injector
+            fortyTwoInjector.getInstance(classOf[HealthcheckPlugin]).addError(
+              HealthcheckError(Some(cause), None, None, Healthcheck.INTERNAL, Some(s"[SafeFuture]: ${cause.getMessage}"))
+            )
+          } catch {
+            case _: Throwable => // tried our best.
+          }
+      }
   }
 
   // Just a wrapper around Future. Wakka Wakka.
