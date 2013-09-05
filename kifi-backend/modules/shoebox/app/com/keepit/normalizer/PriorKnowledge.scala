947c56d6611dbd60e56ad7cfb5a3b475ba001e6d
Léo Grimaldi <leo@42go.com>
2013-09-05 13:32:38 -0700

    Fixes normalization recursion issues

--- kifi-backend/modules/shoebox/app/com/keepit/normalizer/PriorKnowledge.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/normalizer/PriorKnowledge.scala
@@ -8,8 +8,6 @@ case class PriorKnowledge(currentReference: NormalizedURI)(implicit normalizedUR
   lazy val contentChecks = PriorKnowledge.getContentChecks(currentReference.url)
 
   def apply(candidate: NormalizationCandidate): PriorKnowledge.Action = candidate match {
-    case _ => PriorKnowledge.REJECT
-    //case candidate if !PriorKnowledge.getDomain(candidate.url).map(domain => domain.endsWith("linkedin.com") || domain.endsWith("kifi.com")).getOrElse(false) => PriorKnowledge.REJECT
     case _: TrustedCandidate => PriorKnowledge.ACCEPT
     case _: UntrustedCandidate => contentChecks.find(_.isDefinedAt(candidate)).map(PriorKnowledge.Check).getOrElse(PriorKnowledge.REJECT)
   }
