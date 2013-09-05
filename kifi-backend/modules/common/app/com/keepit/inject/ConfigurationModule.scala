63dc8bc81b83af6740a9cf7416cfd22b05c7a451
Andrew Conner <git@andrewconner.org>
2013-09-05 14:14:58 -0700

    Saving state. Broken :(

--- kifi-backend/modules/common/app/com/keepit/inject/ConfigurationModule.scala
+++ kifi-backend/modules/common/app/com/keepit/inject/ConfigurationModule.scala
@@ -3,12 +3,19 @@ package com.keepit.inject
 import _root_.net.codingwell.scalaguice.ScalaModule
 import com.keepit.common.logging.Logging
 
-abstract class ConfigurationModule extends ScalaModule with Logging { self =>
+abstract class AbstractModuleAccessor extends ScalaModule {
+  protected def install0(module: ScalaModule) = install(module)
+}
+
+abstract class ConfigurationModule extends ScalaModule with Logging {
+
   final def configure() {
     log.info(s"Configuring ${this}")
-    for (field <- self.getClass.getDeclaredFields) yield {
+
+    for (field <- this.getClass.getDeclaredFields) yield {
+      log.info("\nGot " + field.getName)
       field.setAccessible(true)
-      field.get(self) match {
+      field.get(this) match {
         case module: ScalaModule =>  
           log.info(s"Install ${module}")
           install(module)
