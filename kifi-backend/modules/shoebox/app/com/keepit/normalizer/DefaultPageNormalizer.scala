342ad5adc8bff55da69fb5a475575579cefc3a93
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-09-05 09:13:43 -0700

    using case insensitive matching

--- kifi-backend/modules/shoebox/app/com/keepit/normalizer/DefaultPageNormalizer.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/normalizer/DefaultPageNormalizer.scala
@@ -6,21 +6,14 @@ import com.keepit.common.net.URI
 object DefaultPageNormalizer extends StaticNormalizer with Logging {
 
   // default page normalization (moved from URI class)
-  private[this] val defaultPage = """/(index|default)\.(html|htm|asp|aspx|php|php3|php4|phtml|cfm|cgi|jsp|jsf|jspx|jspa)$""".r
+  private[this] val defaultPage = """(?i)/(index|default)\.(html|htm|asp|aspx|php|php3|php4|phtml|cfm|cgi|jsp|jsf|jspx|jspa)$""".r
 
   private def normalizePath(path: Option[String]): Option[String] = {
-    path.map{ path =>
-      defaultPage.findFirstMatchIn(path.toLowerCase) match {
-        case Some(m) =>
-          val delta = path.length - path.toLowerCase.length // in case the case conversion changed the length
-          path.substring(0, m.start + delta) + "/"
-        case _ => path
-      }
-    }
+    path.map{ defaultPage.replaceFirstIn(_, "/") }
   }
 
   def isDefinedAt(uri: URI) = {
-    uri.path.exists{ path => defaultPage.findFirstMatchIn(path.toLowerCase).isDefined }
+    uri.path.exists{ path => defaultPage.findFirstMatchIn(path).isDefined }
   }
 
   def apply(uri: URI) = {
