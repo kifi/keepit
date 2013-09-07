267ae981d4662a8de2c7341142c297dbadfcdac0
Léo Grimaldi <leo@42go.com>
2013-09-06 20:11:34 -0700

    Tools to renormalize the KiFi website

--- kifi-backend/modules/shoebox/app/com/keepit/model/NormalizedURIRepo.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/model/NormalizedURIRepo.scala
@@ -20,6 +20,7 @@ trait NormalizedURIRepo extends DbRepo[NormalizedURI] with ExternalIdColumnDbFun
   def internByUri(url: String, candidates: NormalizationCandidate*)(implicit session: RWSession): NormalizedURI
   def getByNormalizedUrl(normalizedUrl: String)(implicit session: RSession): Option[NormalizedURI]
   def getByRedirection(redirect: Id[NormalizedURI])(implicit session: RWSession): Seq[NormalizedURI]
+  def getOldSite()(implicit session: RSession): Seq[NormalizedURI]
 }
 
 @Singleton
@@ -145,4 +146,7 @@ class NormalizedURIRepoImpl @Inject() (
   def getByRedirection(redirect: Id[NormalizedURI])(implicit session: RWSession): Seq[NormalizedURI] = {
     (for(t <- table if t.state === NormalizedURIStates.REDIRECTED && t.redirect === redirect) yield t).list
   }
+
+  def getOldSite()(implicit session: RSession): Seq[NormalizedURI] =
+    (for { u <- table if u.url like "https://www.kifi.com/site%" } yield u).list
 }
