016a8ee7f40c870aa6921d7a4fc8600e8ecad168
Stephen Kemmerling <stephen@42go.com>
2013-09-16 10:29:03 -0700

    Better code organization

--- kifi-backend/modules/heimdal/app/com/keepit/heimdal/EventTrackingController.scala
+++ kifi-backend/modules/heimdal/app/com/keepit/heimdal/EventTrackingController.scala
@@ -1,14 +1,14 @@
-package com.keepit.heimdal
+package com.keepit.heimdal.controllers
 
 import com.keepit.common.controller.HeimdalServiceController
 import com.keepit.common.time._
+import com.keepit.heimdal.{UserEventLoggingRepo, UserEvent}
 
 import play.api.mvc.Action
 import play.api.libs.json.{JsValue}
 
 import com.google.inject.Inject
 
-//remember user block!!
 
 class EventTrackingController @Inject() (userEventLoggingRepo: UserEventLoggingRepo) extends HeimdalServiceController {
 
