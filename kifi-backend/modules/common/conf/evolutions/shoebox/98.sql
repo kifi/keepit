cae0de8d6321c800d4a2094a8e486acc339a869f
yingjieMiao <yingjie@42go.com>
2013-09-17 16:40:58 -0700

    sql evolution

--- kifi-backend/modules/common/conf/evolutions/shoebox/98.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/98.sql
@@ -17,7 +17,7 @@ CREATE TABLE renormalized_url (
 CREATE SEQUENCE renormalized_url_sequence;
 CREATE INDEX renormalized_url_seq_index on renormalized_url(seq);
 
-insert into evolutions (name, description) values('97.sql', 'adding renormalized_url');
+insert into evolutions (name, description) values('98.sql', 'adding renormalized_url');
 
 
 # --- !Downs
