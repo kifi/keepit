4f3661575e2ac0e8efeb8263ab490824f2ae4e4f
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-09-19 12:23:50 -0700

    clean up

--- kifi-backend/modules/common/app/com/keepit/common/net/URI.scala
+++ kifi-backend/modules/common/app/com/keepit/common/net/URI.scala
@@ -6,7 +6,6 @@ import com.keepit.common.logging.Logging
 import com.keepit.common.strings.UTF8
 
 object URI extends Logging {
-
   def parse(uriString: String): Try[URI] = {
     unapplyTry(uriString).map { case (scheme, userInfo, host, port, path, query, fragment) =>
       URI(Some(uriString), scheme, userInfo, host, port, path, query, fragment)
