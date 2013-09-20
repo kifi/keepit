053652f5715319b2f44552550835b32b9b7d0603
yingjieMiao <yingjie@42go.com>
2013-09-19 17:51:42 -0700

    better fragment handling

--- kifi-backend/modules/shoebox/app/com/keepit/normalizer/DefaultNormalizer.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/normalizer/DefaultNormalizer.scala
@@ -13,13 +13,16 @@ object DefaultNormalizer extends StaticNormalizer with Logging {
   def isDefinedAt(uri: URI) = true // default normalizer should always be applicable
   def apply(uri: URI) = {
     uri match {
-      case URI(scheme, userInfo, host, port, path, query, _) =>
+      case URI(scheme, userInfo, host, port, path, query, fragment) =>
         try {
           val newQuery = query.flatMap{ query =>
             val newParams = query.params.filter{ param => !stopParams.contains(param.name) }
             if (newParams.isEmpty) None else Some(Query(newParams))
           }
-          URI(scheme, userInfo, host, port, path, newQuery, None)
+          if (fragment.isDefined && fragment.get != "/" && (fragment.get.contains("/") || fragment.get.contains("%2F"))){
+            URI(scheme, userInfo, host, port, path, newQuery, fragment)
+          }
+          else URI(scheme, userInfo, host, port, path, newQuery, None)
         } catch {
           case e: Exception =>
             log.warn("uri normalization failed: [%s] caused by [%s]".format(uri.raw, e.getMessage))
