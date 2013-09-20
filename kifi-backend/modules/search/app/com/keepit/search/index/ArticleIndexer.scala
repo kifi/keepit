8c61f7e2a81a9f9e568f9980f09bfce85e3e26db
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-09-20 09:38:37 -0700

    actually indexing visibility

--- kifi-backend/modules/search/app/com/keepit/search/index/ArticleIndexer.scala
+++ kifi-backend/modules/search/app/com/keepit/search/index/ArticleIndexer.scala
@@ -118,10 +118,9 @@ class ArticleIndexer @Inject() (
       val doc = super.buildDocument
       getArticle(id = uri.id.get, maxRetry = 5, minSleepTime = 1000) match {
         case Some(article) =>
-          // TODO make URIs with invisible if there is a reason
-          //article.restriction.map{ reason =>
-          //  doc.add(buildKeywordField(ArticleVisibility.redirectTerm.field(), ArticleVisibility.redirectTerm.text()))
-          //}
+          uri.restriction.map{ reason =>
+            doc.add(buildKeywordField(ArticleVisibility.redirectTerm.field(), ArticleVisibility.redirectTerm.text()))
+          }
           val titleLang = article.titleLang.getOrElse(Lang("en"))
           val contentLang = article.contentLang.getOrElse(Lang("en"))
           doc.add(buildKeywordField("cl", contentLang.lang))
