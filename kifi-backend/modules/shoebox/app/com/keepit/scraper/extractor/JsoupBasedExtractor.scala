ffa743a1f0e70521afc1cb649ebec537f5b01d68
Léo Grimaldi <leo@42go.com>
2013-10-03 10:09:47 -0700

    Ability to pass a specific parser (e.g. XML) to a JSoupBasedExtractor

--- kifi-backend/modules/shoebox/app/com/keepit/scraper/extractor/JsoupBasedExtractor.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/scraper/extractor/JsoupBasedExtractor.scala
@@ -5,21 +5,26 @@ import com.keepit.scraper.HttpInputStream
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.parser.Parser
 
-abstract class JsoupBasedExtractor(url: String, maxContentChars: Int) extends Extractor with Logging {
+abstract class JsoupBasedExtractor(url: String, maxContentChars: Int, parser: Option[Parser] = None) extends Extractor with Logging {
   protected var doc: Document = null
 
   def parse(doc: Document): String
 
   def process(input: HttpInputStream){
     try {
-      doc = Jsoup.parse(input, null, url) // null charset autodetects based on `http-equiv` meta tag and default to UTF-8
+      doc = parser.map(Jsoup.parse(input, null, url, _)).getOrElse(Jsoup.parse(input, null, url)) // null charset autodetects based on `http-equiv` meta tag and default to UTF-8, Parser defaults to HTML
     } catch {
       case e: Throwable => log.error("Jsoup extraction failed: ", e)
     }
   }
 
-  def getContent() = parse(doc).take(maxContentChars)
+  def getContent() = {
+    val content = parse(doc)
+    if (content.length > maxContentChars) log.warn(s"max number of characters reached: ${url}")
+    content.take(maxContentChars)
+  }
 
   def getMetadata(name: String) = Option(doc.select("meta[name=" + name + "]").select("content").first()) map (_.toString)
 }
