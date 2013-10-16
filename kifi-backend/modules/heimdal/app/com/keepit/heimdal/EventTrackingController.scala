276123e629227fd3fcc221c1d21424e88da7feb6
Stephen Kemmerling <stephen@42go.com>
2013-10-16 09:47:09 -0700

    Reducing parallelism and responding quicker to http calls in heimdal

--- kifi-backend/modules/heimdal/app/com/keepit/heimdal/EventTrackingController.scala
+++ kifi-backend/modules/heimdal/app/com/keepit/heimdal/EventTrackingController.scala
@@ -3,6 +3,9 @@ package com.keepit.heimdal.controllers
 import com.keepit.common.controller.HeimdalServiceController
 import com.keepit.common.time._
 import com.keepit.heimdal.{UserEventLoggingRepo, UserEvent}
+import com.keepit.common.akka.SafeFuture
+import com.keepit.common.akka.SlowRunningExecutionContext
+
 
 import play.api.mvc.Action
 import play.api.libs.json.{JsValue}
@@ -14,8 +17,10 @@ class EventTrackingController @Inject() (userEventLoggingRepo: UserEventLoggingR
 
 
   def trackInternalEvent = Action(parse.json) { request =>
-    val event: UserEvent = request.body.as[UserEvent]
-    userEventLoggingRepo.insert(event)
+    SafeFuture{
+      val event: UserEvent = request.body.as[UserEvent]
+      userEventLoggingRepo.insert(event)
+    }(SlowRunningExecutionContext.ec)
     Status(202)
   }
 
