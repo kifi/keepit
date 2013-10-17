c4c002fac347495feb761a23cc2dfc20dd79234d
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-10-17 09:23:23 -0700

    added user cache to showboxclient

--- kifi-backend/modules/abook/app/com/keepit/common/cache/ABookCacheModule.scala
+++ kifi-backend/modules/abook/app/com/keepit/common/cache/ABookCacheModule.scala
@@ -57,6 +57,11 @@ case class ABookCacheModule(cachePluginModules: CachePluginModule*) extends Cach
 
   @Singleton
   @Provides
+  def userIdCache(stats: CacheStatistics, accessLog: AccessLog, innerRepo: InMemoryCachePlugin, outerRepo: FortyTwoCachePlugin) =
+    new UserIdCache(stats, accessLog, (innerRepo, 10 minutes), (outerRepo, 24 hours))
+
+  @Singleton
+  @Provides
   def userExperimentCache(stats: CacheStatistics, accessLog: AccessLog, outerRepo: FortyTwoCachePlugin) =
     new UserExperimentCache(stats, accessLog, (outerRepo, 7 days))
 
