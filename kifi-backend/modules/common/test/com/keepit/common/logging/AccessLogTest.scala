dbd4ba42e150eb111095ecb357894c4d1eae0f48
Eishay Smith <eishay@gmail.com>
2013-10-22 21:30:29 -0700

    better, shorter tracking info

--- kifi-backend/modules/common/test/com/keepit/common/logging/AccessLogTest.scala
+++ kifi-backend/modules/common/test/com/keepit/common/logging/AccessLogTest.scala
@@ -15,9 +15,9 @@ class AccessLogTest extends Specification with TestInjector {
         push(new DateTime(2013, 5, 31, 4, 3, 2, 1, DEFAULT_DATE_TIME_ZONE)))
       val access = accessLog.timer(Access.HTTP_OUT)
       //do something
-      val line = accessLog.format(access.done(remoteHost = "host42", method = "POST"))
+      val line = accessLog.format(access.done(remoteServiceType = "host42", method = "POST"))
       println(line)
-      line === "t:2013-05-31 04:03:02.004\ttype:HTTP_OUT\tduration:3\tmethod:POST\tremoteHost:host42"
+      line === "t:2013-05-31 04:03:02.004\ttype:HTTP_OUT\tduration:3\tmethod:POST\tremoteServiceType:host42"
     }
   }
 
