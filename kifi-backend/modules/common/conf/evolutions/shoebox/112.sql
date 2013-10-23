68806f193b5306645da5aad2ef5609afc9f8bb8a
Léo Grimaldi <leo@42go.com>
2013-10-23 15:12:12 -0700

    Bumps up migration number

--- kifi-backend/modules/common/conf/evolutions/shoebox/112.sql
+++
@@ -1,9 +0,0 @@
-#SHOEBOX
-
-# --- !Ups
-
-DROP TABLE click_history ;
-DROP TABLE browsing_history ;
-insert into evolutions (name, description) values('112.sql', 'drop tables click_history and browsing_history');
-
-# --- !Downs
