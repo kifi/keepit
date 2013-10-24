a423648f9fafdc73131b3028903fa40fcbd15ecc
Stephen Kemmerling <stephen@42go.com>
2013-10-24 12:26:50 -0700

    Don't send an airbrake on duplicate key errors if dropDups is set

--- kifi-backend/modules/heimdal/app/com/keepit/heimdal/MongoRepo.scala
+++ kifi-backend/modules/heimdal/app/com/keepit/heimdal/MongoRepo.scala
@@ -6,9 +6,9 @@ import com.keepit.common.akka.SafeFuture
 
 import reactivemongo.bson.BSONDocument
 import reactivemongo.api.collections.default.BSONCollection
-import reactivemongo.core.commands.{PipelineOperator, Aggregate}
+import reactivemongo.core.commands.{PipelineOperator, Aggregate, LastError}
 
-import scala.concurrent.{Promise, Future}
+import scala.concurrent.{Promise, Future, future}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext //Might want to change this to a custom play one
 import java.util.concurrent.atomic.{AtomicLong, AtomicBoolean}
 
@@ -23,17 +23,29 @@ trait MongoRepo[T] {
   def toBSON(obj: T): BSONDocument
   def fromBSON(bson: BSONDocument) : T
 
+  private def handleError(doc: BSONDocument, lastError: LastError, dropDups: Boolean = false) : Unit = {
+    if (lastError.ok==false && (!dropDups || (lastError.code.isDefined && lastError.code.get!=11000))) {
+      airbrake.notify(AirbrakeError(
+        exception = lastError.fillInStackTrace,
+        message = Some(s"Error inserting $doc into MongoDB")
+      ))
+    }
+  }
+
   protected def safeInsert(doc: BSONDocument, dropDups: Boolean = false) = {
-    val insertionFuture = new SafeFuture(collection.insert(doc))
+    val insertionFuture = collection.insert(doc) //Non safe future on purpose! (-Stephen)
+    insertionFuture.onFailure{ 
+      case lastError : LastError => handleError(doc, lastError, dropDups)
+      case ex: Throwable => airbrake.notify(AirbrakeError(
+        exception = ex,
+        message = Some(s"Error inserting $doc into MongoDB")
+      ))
+    }
     insertionFuture.map{ lastError =>
-      if (lastError.ok==false && (!dropDups || (lastError.code.isDefined && lastError.code.get!=11000))) {
-        airbrake.notify(AirbrakeError(
-          exception = lastError.fillInStackTrace,
-          message = Some(s"Error inserting $doc into MongoDB")
-        ))
-      }
+      handleError(doc, lastError, dropDups)
       lastError
     }
+
   }
 
   def insert(obj: T, dropDups: Boolean = false) : Unit = {
