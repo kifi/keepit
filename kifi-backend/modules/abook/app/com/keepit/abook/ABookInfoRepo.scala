1cb41635e4a792f0faee53bf461026f7672dd085
Raymond Ng <raymond.ng@gmail.com>
2013-10-25 15:23:07 -0700

    gmail_contacts_import_10

--- kifi-backend/modules/abook/app/com/keepit/abook/ABookInfoRepo.scala
+++ kifi-backend/modules/abook/app/com/keepit/abook/ABookInfoRepo.scala
@@ -10,7 +10,8 @@ import com.keepit.common.db.slick.DBSession.RSession
 
 @ImplementedBy(classOf[ABookInfoRepoImpl])
 trait ABookInfoRepo extends Repo[ABookInfo] {
-  def findByUserIdAndOriginOpt(userId:Id[User], origin:ABookOriginType)(implicit session:RSession):Option[ABookInfo]
+  def findByUserIdOriginAndOwnerId(userId:Id[User], origin:ABookOriginType, ownerId:Option[String])(implicit session:RSession):Option[ABookInfo]
+  def findByUserIdAndOrigin(userId:Id[User], origin:ABookOriginType)(implicit session:RSession):Seq[ABookInfo]
   def findByUserId(userId:Id[User])(implicit session:RSession):Seq[ABookInfo]
 }
 
@@ -23,13 +24,20 @@ class ABookInfoRepoImpl @Inject() (val db:DataBaseComponent, val clock:Clock) ex
   override val table = new RepoTable[ABookInfo](db, "abook_info") {
     def userId = column[Id[User]]("user_id", O.NotNull)
     def origin = column[ABookOriginType]("origin", O.NotNull)
+    def ownerId = column[String]("owner_id")
+    def ownerEmail = column[String]("owner_email")
     def rawInfoLoc = column[String]("raw_info_loc")
-    def * = id.? ~ createdAt ~ updatedAt ~ state ~ userId ~ origin ~ rawInfoLoc.? <> (ABookInfo.apply _, ABookInfo.unapply _)
+    def * = id.? ~ createdAt ~ updatedAt ~ state ~ userId ~ origin ~ ownerId.? ~ ownerEmail.? ~ rawInfoLoc.? <> (ABookInfo.apply _, ABookInfo.unapply _)
   }
 
-  def findByUserIdAndOriginOpt(userId: Id[User], origin: ABookOriginType)(implicit session:RSession):Option[ABookInfo] = {
-    val q = for { c <- table if c.userId === userId && c.origin === origin } yield c
+  def findByUserIdOriginAndOwnerId(userId: Id[User], origin: ABookOriginType, ownerId:Option[String])(implicit session:RSession): Option[ABookInfo] = {
+    val q = for { c <- table if c.userId === userId && c.origin === origin && c.ownerId === ownerId} yield c // assumption: NULL === None
     q.firstOption
+  }
+
+  def findByUserIdAndOrigin(userId: Id[User], origin: ABookOriginType)(implicit session:RSession): Seq[ABookInfo] = {
+    val q = for { c <- table if c.userId === userId && c.origin === origin } yield c
+    q.list
   }
 
   def findByUserId(userId: Id[User])(implicit session: RSession): Seq[ABookInfo] = {
