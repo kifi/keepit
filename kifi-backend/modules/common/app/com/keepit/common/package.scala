1f59cce1c37a2d518310d93e3fd671bac30ee47c
Léo Grimaldi <leo@42go.com>
2013-10-29 11:22:47 -0700

    Some IO helpers

--- kifi-backend/modules/common/app/com/keepit/common/package.scala
+++ kifi-backend/modules/common/app/com/keepit/common/package.scala
@@ -1,5 +1,9 @@
 package com.keepit
 
+import org.apache.commons.compress.archivers.tar.{TarArchiveInputStream, TarArchiveEntry, TarArchiveOutputStream}
+import java.io.{FileOutputStream, File}
+import java.nio.file.{StandardCopyOption, CopyOption, Files}
+
 package object common {
 
   /** Useful when wanting to side-effect (log, stats, etc) and return the original value.
@@ -34,6 +38,41 @@ package object common {
       try { f }
       catch {
         case t: Throwable => g(t)
+      }
+    }
+  }
+
+  object IO {
+    def copyFile(source: File, destination: String): Unit = {
+      val target = new File(destination)
+      if (target.exists()) deleteFile(target)
+      Files.copy(source.toPath, target.toPath)
+      if (source.isDirectory) source.listFiles().foreach(file => copyFile(file, source.getAbsolutePath + "/" + file.getName))
+    }
+
+    def deleteFile(file: File): Unit = {
+      if (file.isDirectory) file.listFiles().foreach(deleteFile)
+      file.delete()
+    }
+
+    def addToArchive(tarArchive: TarArchiveOutputStream, file: File, base: String = ""): Unit = {
+      val entryName = base + file.getName
+      val entry = new TarArchiveEntry(file, entryName)
+      tarArchive.putArchiveEntry(entry)
+      if (file.isFile) Files.copy(file.toPath, tarArchive)
+      tarArchive.closeArchiveEntry()
+      if (file.isDirectory) file.listFiles().foreach(addToArchive(tarArchive, _, entryName + "/"))
+    }
+
+    def extractArchive(tarArchive: TarArchiveInputStream, destination: String): Unit = {
+      var entryOption = Option(tarArchive.getNextTarEntry)
+      while (entryOption.isDefined) {
+        entryOption.foreach { entry =>
+          val file = new File(destination + "/" + entry.getName)
+          if (entry.isDirectory) file.mkdirs()
+          else Files.copy(entry.getFile.toPath, file.toPath)
+        }
+        entryOption = Option(tarArchive.getNextTarEntry)
       }
     }
   }
