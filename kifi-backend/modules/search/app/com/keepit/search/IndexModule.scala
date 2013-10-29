173e4f2ba240263cc1e1fadbc7b095975e260e4d
Léo Grimaldi <leo@42go.com>
2013-10-29 11:27:27 -0700

    Configuration and dependency injection for new IndexDirectory

--- kifi-backend/modules/search/app/com/keepit/search/IndexModule.scala
+++ kifi-backend/modules/search/app/com/keepit/search/IndexModule.scala
@@ -6,7 +6,7 @@ import com.google.inject.{Provides, Singleton}
 import com.keepit.search.graph._
 import com.keepit.common.healthcheck.AirbrakeNotifier
 import com.keepit.shoebox.ShoeboxServiceClient
-import com.keepit.search.index.{ArticleIndexerPluginImpl, ArticleIndexerPlugin, DefaultAnalyzer, ArticleIndexer}
+import com.keepit.search.index._
 import play.api.Play._
 import org.apache.lucene.index.IndexWriterConfig
 import org.apache.lucene.util.Version
@@ -17,9 +17,26 @@ import com.keepit.inject.AppScoped
 import java.io.File
 import com.keepit.common.logging.Logging
 
-trait IndexModule extends ScalaModule
+trait IndexModule extends ScalaModule with Logging {
 
-case class ProdIndexModule() extends IndexModule with Logging {
+  protected def getPersistentIndexDirectory(maybeDir: Option[String], indexStore: IndexStore): Option[IndexDirectory] = {
+    maybeDir.map { d =>
+      val dir = new File(d).getCanonicalFile
+      val indexDirectory = new IndexDirectoryImpl( dir, indexStore)
+      if (!dir.exists()) {
+        try { indexDirectory.restoreFromBackup() }
+        catch { case e: Exception => {
+          log.error(s"Could not restore $dir from backup", e)
+          if (!dir.mkdirs()) {
+            throw new Exception(s"Could not create directory $dir")
+          }
+        }}
+      }
+      indexDirectory
+    }
+  }
+
+  protected def getIndexDirectory(maybeDir: Option[String], indexStore: IndexStore): IndexDirectory
 
   def configure() {
     bind[ArticleIndexerPlugin].to[ArticleIndexerPluginImpl].in[AppScoped]
@@ -27,22 +44,10 @@ case class ProdIndexModule() extends IndexModule with Logging {
     bind[CommentIndexerPlugin].to[CommentIndexerPluginImpl].in[AppScoped]
   }
 
-  private def getDirectory(maybeDir: Option[String]): Directory = {
-    maybeDir.map { d =>
-      val dir = new File(d).getCanonicalFile
-      if (!dir.exists()) {
-        if (!dir.mkdirs()) {
-          throw new Exception(s"could not create dir $dir")
-        }
-      }
-      new MMapDirectory(dir)
-    }.get
-  }
-
   @Singleton
   @Provides
-  def articleIndexer(articleStore: ArticleStore, uriGraph: URIGraph, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): ArticleIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.article.directory"))
+  def articleIndexer(articleStore: ArticleStore, uriGraph: URIGraph, backup: IndexStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): ArticleIndexer = {
+    val dir = getIndexDirectory(current.configuration.getString("index.article.directory"), backup)
     log.info(s"storing search index in $dir")
     val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
     new ArticleIndexer(dir, config, articleStore, airbrake, shoeboxClient)
@@ -50,8 +55,8 @@ case class ProdIndexModule() extends IndexModule with Logging {
 
   @Singleton
   @Provides
-  def bookmarkStore(airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): BookmarkStore = {
-    val dir = getDirectory(current.configuration.getString("index.bookmarkStore.directory"))
+  def bookmarkStore(backup: IndexStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): BookmarkStore = {
+    val dir = getIndexDirectory(current.configuration.getString("index.bookmarkStore.directory"), backup)
     log.info(s"storing BookmarkStore in $dir")
     val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
     new BookmarkStore(dir, config, airbrake, shoeboxClient)
@@ -59,8 +64,8 @@ case class ProdIndexModule() extends IndexModule with Logging {
 
   @Singleton
   @Provides
-  def uriGraphIndexer(bookmarkStore: BookmarkStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): URIGraphIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.urigraph.directory"))
+  def uriGraphIndexer(bookmarkStore: BookmarkStore, backup: IndexStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): URIGraphIndexer = {
+    val dir = getIndexDirectory(current.configuration.getString("index.urigraph.directory"), backup)
     log.info(s"storing URIGraph in $dir")
     val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
     new URIGraphIndexer(dir, config, bookmarkStore, airbrake, shoeboxClient)
@@ -68,8 +73,8 @@ case class ProdIndexModule() extends IndexModule with Logging {
 
   @Singleton
   @Provides
-  def collectionIndexer(airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): CollectionIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.collection.directory"))
+  def collectionIndexer(backup: IndexStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): CollectionIndexer = {
+    val dir = getIndexDirectory(current.configuration.getString("index.collection.directory"), backup)
     log.info(s"storing collection index in $dir")
     val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
     new CollectionIndexer(dir, config, airbrake, shoeboxClient)
@@ -77,8 +82,8 @@ case class ProdIndexModule() extends IndexModule with Logging {
 
   @Singleton
   @Provides
-  def commentStore(airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): CommentStore = {
-    val dir = getDirectory(current.configuration.getString("index.commentStore.directory"))
+  def commentStore(backup: IndexStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): CommentStore = {
+    val dir = getIndexDirectory(current.configuration.getString("index.commentStore.directory"), backup)
     log.info(s"storing CommentStore in $dir")
     val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
     new CommentStore(dir, config, airbrake, shoeboxClient)
@@ -86,8 +91,8 @@ case class ProdIndexModule() extends IndexModule with Logging {
 
   @Singleton
   @Provides
-  def commentIndexer(commentStore: CommentStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): CommentIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.comment.directory"))
+  def commentIndexer(commentStore: CommentStore, backup: IndexStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): CommentIndexer = {
+    val dir = getIndexDirectory(current.configuration.getString("index.comment.directory"), backup)
     log.info(s"storing comment index in $dir")
     val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
     new CommentIndexer(dir, config, commentStore, airbrake, shoeboxClient)
@@ -95,8 +100,8 @@ case class ProdIndexModule() extends IndexModule with Logging {
 
   @Singleton
   @Provides
-  def phraseIndexer(airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): PhraseIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.phrase.directory"))
+  def phraseIndexer(backup: IndexStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): PhraseIndexer = {
+    val dir = getIndexDirectory(current.configuration.getString("index.phrase.directory"), backup)
     val dataDir = current.configuration.getString("index.config").map{ path =>
       val configDir = new File(path).getCanonicalFile()
       new File(configDir, "phrase")
@@ -104,117 +109,36 @@ case class ProdIndexModule() extends IndexModule with Logging {
     val analyzer = DefaultAnalyzer.forIndexing
     val config = new IndexWriterConfig(Version.LUCENE_41, analyzer)
     new PhraseIndexerImpl(dir, config, airbrake, shoeboxClient)
-
   }
+}
+
+case class ProdIndexModule() extends IndexModule {
+
+  protected def getIndexDirectory(maybeDir: Option[String], indexStore: IndexStore): IndexDirectory =
+    getPersistentIndexDirectory(maybeDir, indexStore).get
 
   @Singleton
   @Provides
-  def spellCorrector: SpellCorrector = {
-    val spellDir = getDirectory(current.configuration.getString("index.spell.directory"))
-    val articleDir = getDirectory(current.configuration.getString("index.article.directory"))
+  def spellCorrector(backup: IndexStore): SpellCorrector = {
+    val spellDir = getIndexDirectory(current.configuration.getString("index.spell.directory"), backup)
+    val articleDir = getIndexDirectory(current.configuration.getString("index.article.directory"), backup)
     SpellCorrector(spellDir, articleDir)
   }
 }
 
-case class DevIndexModule() extends IndexModule with Logging {
+case class DevIndexModule() extends IndexModule {
 
-  def configure {
-    bind[ArticleIndexerPlugin].to[ArticleIndexerPluginImpl].in[AppScoped]
-    bind[URIGraphPlugin].to[URIGraphPluginImpl].in[AppScoped]
-    bind[CommentIndexerPlugin].to[CommentIndexerPluginImpl].in[AppScoped]
-  }
-
-  private def getDirectory(maybeDir: Option[String]): Directory = {
-    maybeDir.map { d =>
-      val dir = new File(d).getCanonicalFile
-      if (!dir.exists()) {
-        if (!dir.mkdirs()) {
-          throw new Exception(s"could not create dir $dir")
-        }
-      }
-      new MMapDirectory(dir)
-    }.getOrElse {
-      new RAMDirectory()
-    }
-  }
+  protected def getIndexDirectory(maybeDir: Option[String], indexStore: IndexStore): IndexDirectory =
+    getPersistentIndexDirectory(maybeDir, indexStore).getOrElse(new VolatileIndexDirectoryImpl())
 
   @Singleton
   @Provides
-  def articleIndexer(articleStore: ArticleStore, uriGraph: URIGraph, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): ArticleIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.article.directory"))
-    log.info(s"storing search index in $dir")
+  def spellCorrector(backup: IndexStore): SpellCorrector = {
+    val spellDirOpt = getPersistentIndexDirectory(current.configuration.getString("index.spell.directory"), backup)
+    val articleDirOpt = getPersistentIndexDirectory(current.configuration.getString("index.article.directory"), backup)
 
-    val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
-    new ArticleIndexer(dir, config, articleStore, airbrake, shoeboxClient)
-  }
-
-  @Singleton
-  @Provides
-  def bookmarkStore(airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): BookmarkStore = {
-    val dir = getDirectory(current.configuration.getString("index.bookmarkStore.directory"))
-    log.info(s"storing BookmarkStore in $dir")
-    val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
-    new BookmarkStore(dir, config, airbrake, shoeboxClient)
-  }
-
-  @Singleton
-  @Provides
-  def uriGraphIndexer(bookmarkStore: BookmarkStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): URIGraphIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.urigraph.directory"))
-    log.info(s"storing URIGraph in $dir")
-    val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
-    new URIGraphIndexer(dir, config, bookmarkStore, airbrake, shoeboxClient)
-  }
-
-  @Singleton
-  @Provides
-  def collectionIndexer(airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): CollectionIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.collection.directory"))
-    log.info(s"storing collection index in $dir")
-    val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
-    new CollectionIndexer(dir, config, airbrake, shoeboxClient)
-  }
-
-  @Singleton
-  @Provides
-  def commentStore(airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): CommentStore = {
-    val dir = getDirectory(current.configuration.getString("index.commentStore.directory"))
-    log.info(s"storing CommentStore in $dir")
-    val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
-    new CommentStore(dir, config, airbrake, shoeboxClient)
-  }
-
-  @Singleton
-  @Provides
-  def commentIndexer(commentStore: CommentStore, airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): CommentIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.comment.directory"))
-    log.info(s"storing comment index in $dir")
-    val config = new IndexWriterConfig(Version.LUCENE_41, DefaultAnalyzer.forIndexing)
-    new CommentIndexer(dir, config, commentStore, airbrake, shoeboxClient)
-  }
-
-  @Singleton
-  @Provides
-  def phraseIndexer(airbrake: AirbrakeNotifier, shoeboxClient: ShoeboxServiceClient): PhraseIndexer = {
-    val dir = getDirectory(current.configuration.getString("index.phrase.directory"))
-    val dataDir = current.configuration.getString("index.config").map{ path =>
-      val configDir = new File(path).getCanonicalFile()
-      new File(configDir, "phrase")
-    }
-    val analyzer = DefaultAnalyzer.forIndexing
-    val config = new IndexWriterConfig(Version.LUCENE_41, analyzer)
-    new PhraseIndexerImpl(dir, config, airbrake, shoeboxClient)
-
-  }
-
-  @Singleton
-  @Provides
-  def spellCorrector : SpellCorrector = {
-    val spellDir = getDirectory(current.configuration.getString("index.spell.directory"))
-    val articleDir = getDirectory(current.configuration.getString("index.article.directory"))
-
-    (spellDir, articleDir) match {
-      case (sDir: MMapDirectory, aDir: MMapDirectory) => SpellCorrector(sDir, aDir)
+    (spellDirOpt, articleDirOpt) match {
+      case (Some(sDir), Some(aDir)) => SpellCorrector(sDir, aDir)
       case _ => new FakeSpellCorrector
     }
   }
