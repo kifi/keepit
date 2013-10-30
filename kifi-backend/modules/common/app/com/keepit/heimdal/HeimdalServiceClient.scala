f468cd8d1743b716374a1df8f9614cda35c6286d
Eishay Smith <eishay@gmail.com>
2013-10-29 22:14:17 -0700

    making heimdal event tracking async on the client side and having a single thread dealing with the posting to the server. next phase would be to batch the events in the actor

--- kifi-backend/modules/common/app/com/keepit/heimdal/HeimdalServiceClient.scala
+++ kifi-backend/modules/common/app/com/keepit/heimdal/HeimdalServiceClient.scala
@@ -8,6 +8,9 @@ import com.keepit.common.routes.Heimdal
 import com.keepit.common.healthcheck.AirbrakeNotifier
 import com.keepit.common.net.HttpClient
 import com.keepit.common.zookeeper.ServiceCluster
+import com.keepit.common.akka.{FortyTwoActor, UnsupportedActorMessage}
+import com.keepit.common.actor.ActorInstance
+import com.keepit.common.zookeeper.ServiceDiscovery
 
 import scala.concurrent.{Future, Promise}
 
@@ -26,16 +29,29 @@ trait HeimdalServiceClient extends ServiceClient {
   def updateMetrics(): Unit
 }
 
+class HeimdalClientActor @Inject() (
+    val airbrakeNotifier: AirbrakeNotifier,
+    val httpClient: HttpClient,
+    serviceDiscovery: ServiceDiscovery
+  ) extends FortyTwoActor(airbrakeNotifier) with ServiceClient with Logging {
+
+  final val serviceType = ServiceType.HEIMDAL
+  val serviceCluster = serviceDiscovery.serviceCluster(serviceType)
+
+  def receive = {
+    case event: UserEvent => call(Heimdal.internal.trackEvent, Json.toJson(event))
+    case m => throw new UnsupportedActorMessage(m)
+  }
+}
+
 class HeimdalServiceClientImpl @Inject() (
     val airbrakeNotifier: AirbrakeNotifier,
     val httpClient: HttpClient,
-    val serviceCluster: ServiceCluster
-  )
+    val serviceCluster: ServiceCluster,
+    actor: ActorInstance[HeimdalClientActor])
   extends HeimdalServiceClient with Logging {
 
-  def trackEvent(event: UserEvent) : Unit = {
-    call(Heimdal.internal.trackEvent, Json.toJson(event))
-  }
+  def trackEvent(event: UserEvent) : Unit = actor.ref ! event
 
   def getMetricData(name: String): Future[JsObject] = {
     call(Heimdal.internal.getMetricData(name)).map{ response =>
