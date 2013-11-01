aecbdb50e4ba3b045ee405792365946cc7de8d9d
Jared Jacobs <jared@42go.com>
2013-11-01 09:55:30 -0700

    typo

--- kifi-backend/modules/common/conf/evolutions/shoebox/109.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/109.sql
@@ -4,6 +4,6 @@
 
 ALTER TABLE email_address
   ADD COLUMN verification_code VARCHAR(32),
-  ADD INDEX email_address_i_verification_code ON (verification_code);
+  ADD INDEX email_address_i_verification_code (verification_code);
 
 # --- !Downs
