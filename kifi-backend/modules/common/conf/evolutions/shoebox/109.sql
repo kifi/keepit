ea3b331731b8f959aa1a638a238bd37bdf291289
Jared Jacobs <jared@42go.com>
2013-11-01 10:31:19 -0700

    making this index compatible with HSQL too

--- kifi-backend/modules/common/conf/evolutions/shoebox/109.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/109.sql
@@ -3,7 +3,8 @@
 # --- !Ups
 
 ALTER TABLE email_address
-  ADD COLUMN verification_code VARCHAR(32),
-  ADD INDEX email_address_i_verification_code (verification_code);
+  ADD COLUMN verification_code VARCHAR(32);
+
+CREATE INDEX email_address_i_verification_code ON email_address(verification_code);
 
 # --- !Downs
