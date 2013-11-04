e133910e17c1ae66d461fc24bc9233f697f3762e
yingjieMiao <yingjie@42go.com>
2013-11-04 08:34:01 -0800

    log

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/shoebox/ShoeboxController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/shoebox/ShoeboxController.scala
@@ -325,6 +325,7 @@ class ShoeboxController @Inject() (
   def getEmailsForUsers() = Action(parse.json) { request =>
     val jsArr = request.body.as[JsArray]
     log.info("request js array: " + jsArr)
+    log.info("request js array value: " + jsArr.value)
     val userIds = jsArr.value.map{x => Id[User](x.as[Long])}
     val emails = db.readOnly{ implicit s =>
       userIds.map{userId => userId.id.toString -> emailAddressRepo.getByUser(userId).map{_.address}}.toMap
