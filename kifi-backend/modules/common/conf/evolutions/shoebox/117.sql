fc6038cdca94dfc81aa29d4a17f16542cceebf4a
Léo Grimaldi <leo@42go.com>
2013-11-04 15:25:43 -0800

    no message

--- kifi-backend/modules/common/conf/evolutions/shoebox/117.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/117.sql
@@ -5,11 +5,13 @@
 -- MySQL:
 -- CREATE TABLE phrase_sequence (id INT NOT NULL);
 -- INSERT INTO phrase_sequence (SELECT MAX(id) FROM phrase);
+-- ALTER TABLE phrase ADD seq INT NOT NULL DEFAULT id;
+-- CREATE INDEX phrase_seq_index ON phrase(seq);
+--insert into evolutions (name, description) values('117.sql', 'add sequence number to phrase');
+
 -- H2:
 CREATE SEQUENCE phrase_sequence;
----
-
-ALTER TABLE phrase ADD seq INT NOT NULL DEFAULT id;
+ALTER TABLE phrase ADD seq INT NOT NULL DEFAULT 0;
 CREATE INDEX phrase_seq_index ON phrase(seq);
 
 insert into evolutions (name, description) values('117.sql', 'add sequence number to phrase');
