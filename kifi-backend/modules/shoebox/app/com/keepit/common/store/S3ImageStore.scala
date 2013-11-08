2bc3bd2258fb2ee7a4e3a948d7e5fec92e21fbcf
Andrew Conner <git@andrewconner.org>
2013-11-08 14:13:12 -0800

    New ghost image

--- kifi-backend/modules/shoebox/app/com/keepit/common/store/S3ImageStore.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/common/store/S3ImageStore.scala
@@ -33,7 +33,7 @@ object S3UserPictureConfig {
   val ImageSizes = Seq(100, 200)
   val sizes = ImageSizes.map(s => ImageSize(s, s))
   val OriginalImageSize = "original"
-  val defaultImage = "http://s.c.lnkd.licdn.com/scds/common/u/images/themes/katy/ghosts/person/ghost_person_200x200_v1.png"
+  val defaultImage = "https://www.kifi.com/assets/img/ghost.200.png"
 }
 
 @ImplementedBy(classOf[S3ImageStoreImpl])
