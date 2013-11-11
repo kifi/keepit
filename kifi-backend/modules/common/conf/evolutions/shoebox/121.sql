4b477eee5a7eec1c3550e5b01d4a85c74031031a
Raymond Ng <raymond.ng@gmail.com>
2013-11-11 14:26:45 -0800

    contacts_invite_1

--- kifi-backend/modules/common/conf/evolutions/shoebox/121.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/121.sql
@@ -1,7 +1,7 @@
 # SHOEBOX
 
-ALTER TABLE invitation DROP INDEX invitation_i_recipient_social_user_id;
-ALTER TABLE invitation DROP FOREIGN KEY invitation_recipient_social_user_id;
+-- ALTER TABLE invitation DROP INDEX invitation_i_recipient_social_user_id;
+-- ALTER TABLE invitation DROP FOREIGN KEY invitation_recipient_social_user_id;
 ALTER TABLE invitation MODIFY recipient_social_user_id bigint(20);
 
 INSERT INTO evolutions (name, description) VALUES ('121.sql', 'make recipient_social_user_id optional');
