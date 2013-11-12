47361941fe1d1ed2a03b6efdbd68097f1d6e4236
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-11-12 13:36:55 -0800

    decrease the result click boosts when the licked hit has no normalized uri

--- kifi-backend/modules/search/app/com/keepit/search/ResultClickTracker.scala
+++ kifi-backend/modules/search/app/com/keepit/search/ResultClickTracker.scala
@@ -3,10 +3,10 @@ package com.keepit.search
 import com.keepit.common.db.Id
 import com.keepit.search.index.DefaultAnalyzer
 import com.keepit.search.query.QueryHash
-import scala.math._
 import com.keepit.model.NormalizedURI
 import com.keepit.model.User
-
+import scala.math._
+import scala.util.Random
 
 
 abstract class ResultClickBoosts {
@@ -14,20 +14,29 @@ abstract class ResultClickBoosts {
 }
 
 class ResultClickTracker(lru: ProbablisticLRU) {
-  
+
+  private[this] val rnd = new Random
+
   private[this] val analyzer = DefaultAnalyzer.defaultAnalyzer
 
-  def add(userId: Id[User], query: String, uriId: Id[NormalizedURI], rank: Int, isUserKeep: Boolean) = {
-    val hash = QueryHash(userId, query, analyzer)
+  def add(userId: Id[User], query: String, uriId: Id[NormalizedURI], rank: Int, isUserKeep: Boolean): Unit = {
     val updateStrength = if (isUserKeep) {
       min(0.1d * (rank.toDouble + 3.0d), 0.7d)
     } else {
       0.20d
     }
-    lru.put(hash, uriId.id, updateStrength)
+    add(userId, query, uriId.id, updateStrength)
   }
 
-  def getBoosts(userId: Id[User], query: String, maxBoost: Float, useSlaveAsPrimary: Boolean = false) = {
+  def moderate(userId: Id[User], query: String): Unit = {
+    add(userId, query, rnd.nextLong(), 0.1d) // slowly making lru to forget by adding a random id
+  }
+
+  private[this] def add(userId: Id[User], query: String, uriId: Long, updateStrength: Double): Unit = {
+    lru.put(QueryHash(userId, query, analyzer), uriId, updateStrength)
+  }
+
+  def getBoosts(userId: Id[User], query: String, maxBoost: Float, useSlaveAsPrimary: Boolean = false): ResultClickBoosts = {
     val hash = QueryHash(userId, query, analyzer)
     val likeliness = lru.get(hash, useSlaveAsPrimary)
     new ResultClickBoosts {
