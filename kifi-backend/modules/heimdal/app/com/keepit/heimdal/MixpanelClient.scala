9987d3ef68cb5bd817ab663e6384d87fd3fc3c8d
Léo Grimaldi <leo@42go.com>
2013-11-13 15:32:31 -0800

    Finer context unpacking for Mixpanel

--- kifi-backend/modules/heimdal/app/com/keepit/heimdal/MixpanelClient.scala
+++ kifi-backend/modules/heimdal/app/com/keepit/heimdal/MixpanelClient.scala
@@ -1,21 +1,32 @@
 package com.keepit.heimdal
+
 import com.keepit.serializer.TypeCode
 import play.api.libs.json._
 import org.apache.commons.codec.binary.Base64
 import play.api.libs.ws.WS
 
-
 class MixpanelClient(projectToken: String) {
+
   def send[E <: HeimdalEvent: TypeCode](event: E) = {
     val eventCode = implicitly[TypeCode[E]]
-    val eventName = JsString(s"${eventCode}_${event.eventType.name}")
-    var properties = Json.toJson(event.context).as[JsObject] + ("token", JsString(projectToken)) + ("time", JsNumber(event.time.getMillis))
-    properties.value.get("remoteAddress").foreach { ip => properties = properties + ("ip", ip) - "remoteAddress" }
-    event match {
-      case e: UserEvent => properties = properties + ("distinct_id", JsString(s"${eventCode}_${e.userId.toString}"))
-      case _ =>
+    val eventName = s"${eventCode}_${event.eventType.name}"
+    val distinctId = event match {
+      case e: UserEvent => s"${eventCode}_${e.userId.toString}"
+      case _ => eventCode.toString
     }
-    val data = Base64.encodeBase64String(Json.stringify(Json.obj("event" -> eventName, "properties" -> properties)).getBytes)
+    val properties = getProperties(event.context) :+ ("distinct_id" -> JsString(distinctId)) :+ ("token", JsString(projectToken)) :+ ("time", JsNumber(event.time.getMillis))
+
+    val data = Base64.encodeBase64String(Json.stringify(Json.obj("event" -> JsString(eventName), "properties" -> JsObject(properties))).getBytes)
     WS.url("http://api.mixpanel.com/track").withQueryString(("data", data)).get()
   }
+
+  private def getProperties(context: EventContext) : Seq[(String, JsValue)] = context.data.map {
+      case ("remoteAddress", Seq(ContextStringData(ip))) => "ip" -> JsString(ip)
+      case (key, Seq(ContextStringData(s))) => key -> JsString(s)
+      case (key, Seq(ContextDoubleData(x))) => key -> JsNumber(x)
+      case (key, seq) => key -> JsArray(seq.map{ _ match {
+        case ContextStringData(s) => JsString(s)
+        case ContextDoubleData(x) => JsNumber(x)
+      }})
+  }.toSeq
 }
