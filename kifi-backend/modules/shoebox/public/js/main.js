492195a284ec6af0bf90d5d43d3bdfefaeb56b95
Andrew Conner <git@andrewconner.org>
2013-11-14 14:52:56 -0800

    [don’t merge] Opening invites for all.

--- kifi-backend/modules/shoebox/public/js/main.js
+++ kifi-backend/modules/shoebox/public/js/main.js
@@ -1661,11 +1661,7 @@ $(function() {
 				break;
 			case 'friends':
 				$.when(promise.me).done(function() {
-					if (parts[1] === 'invites' && !canInvite()) {
-						navigate('friends', {replace: true});
-					} else {
-						showFriends(hash);
-					}
+					showFriends(hash);
 				});
 				break;
 			case 'blog':
@@ -2279,15 +2275,9 @@ $(function() {
 		$(".my-pic").css("background-image", "url(" + formatPicUrl(data.id, data.pictureName, 200) + ")");
 		$(".my-name").text(data.firstName + ' ' + data.lastName);
 		$(".my-description").text(data.description || '\u00A0'); // nbsp
-		$friendsTabs.filter('[data-href="friends/invite"]').toggle(canInvite());
+		$friendsTabs.filter('[data-href="friends/invite"]').toggle(true);
 		updateGmailTab();
 		updateConnectTab();
-	}
-
-	function canInvite() {
-		//TODO dev
-		return me.experiments.indexOf('admin') >= 0 ||
-			me.experiments.indexOf('can invite') >= 0;
 	}
 
 	function canInviteViaGmail() {
@@ -2344,7 +2334,7 @@ $(function() {
 			}
 		}).promise()};
 	$.when(promise.me).done(function () {
-		$('#invite-friends-link').toggle(canInvite());
+		$('#invite-friends-link').toggle(true);
 		updateGmailTab();
 		updateConnectTab();
 	});
