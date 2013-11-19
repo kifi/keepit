8df52dc74825d751a1e50206b6feefb9130fce73
yingjieMiao <yingjie@42go.com>
2013-11-19 10:43:58 -0800

    adjacency score

--- kifi-backend/modules/search/app/com/keepit/search/spellcheck/SpellCorrector.scala
+++ kifi-backend/modules/search/app/com/keepit/search/spellcheck/SpellCorrector.scala
@@ -2,6 +2,8 @@ package com.keepit.search.spellcheck
 
 import com.google.inject.{ImplementedBy, Inject, Singleton}
 import org.apache.lucene.analysis.standard.StandardAnalyzer
+import scala.util.Random
+import scala.math.exp
 
 @ImplementedBy(classOf[SpellCorrectorImpl])
 trait SpellCorrector {
@@ -11,7 +13,7 @@ trait SpellCorrector {
 }
 
 @Singleton
-class SpellCorrectorImpl @Inject()(spellIndexer: SpellIndexer) extends SpellCorrector{
+class SpellCorrectorImpl @Inject()(spellIndexer: SpellIndexer, enableAdjScore: Boolean = true) extends SpellCorrector{
   val spellChecker = spellIndexer.getSpellChecker
   val stopwords = StandardAnalyzer.STOP_WORDS_SET
 
@@ -29,7 +31,7 @@ class SpellCorrectorImpl @Inject()(spellIndexer: SpellIndexer) extends SpellCorr
 
   override def getScoredSuggestions(input: String, numSug: Int, enableBoost: Boolean): Array[ScoredSuggest] = {
     val suggestions = getSuggestions(input, numSug.min(10)).map{Suggest(_)}
-    val scorer = new SuggestionScorer(spellIndexer.getTermStatsReader)
+    val scorer = new SuggestionScorer(spellIndexer.getTermStatsReader, enableAdjScore)
     scorer.rank(input, suggestions, enableBoost)
   }
 
@@ -68,10 +70,18 @@ class ScoreDecorator(originalQuery: String) {
   }
 }
 
-class SuggestionScorer(statsReader: TermStatsReader) {
+class SuggestionScorer(statsReader: TermStatsReader, enableAdjScore: Boolean) {
 
   private var statsMap = Map.empty[String, SimpleTermStats]
-  private var jointMap = Map.empty[(String, String), Int]
+  private var jointMap = Map.empty[(String, String), Set[Int]]
+  private var adjScoreMap = Map.empty[(String, String), Float]
+
+  val rand = new Random()
+  val SIGMA = 5
+
+  def gaussianScore(x: Float) = {
+    exp(-x*x/(2*SIGMA*SIGMA)) max 0.05f      // with sigma = 5, this close to 0 when x > 10. Add a smoother
+  }
 
   def warmUpStatsMap(words: Set[String]) = words.foreach{getOrUpdateStats(_)}
 
@@ -82,11 +92,29 @@ class SuggestionScorer(statsReader: TermStatsReader) {
   private def getOrUpdateJoint(key: (String, String)) = jointMap.getOrElse(key, {
       val (a, b) = key
       val (aStat, bStat) = (getOrUpdateStats(a), getOrUpdateStats(b))
-      val inter = (aStat.docIds intersect bStat.docIds).size
+      val inter = aStat.docIds intersect bStat.docIds
       jointMap += key -> inter
       inter
     }
   )
+
+  private def getOrUpdateAdjScore(key: (String, String), inter: Set[Int]) = adjScoreMap.getOrElse(key, {
+    val (a, b) = key
+    val score = adjacencyScore(a, b, inter)
+    adjScoreMap += key -> score
+    score
+  })
+
+  private def adjacencyScore(a: String, b: String, inter: Set[Int]): Float = {
+    val subset = if (inter.size <= 10 ) inter else rand.shuffle(inter).take(10)     // sample
+    val liveDocs = TermStatsReader.genBits(subset)
+    val (aMap, bMap) = (statsReader.getDocsAndPositions(a, liveDocs), statsReader.getDocsAndPositions(b, liveDocs))
+    assume (aMap.keySet == bMap.keySet)
+    val scorer = new AdjacencyScorer
+    val dists = aMap.keySet.map{k => scorer.distance(aMap(k), bMap(k))}
+    val avgDist = dists.foldLeft(0)(_+_)/(dists.size).toFloat       // take average ( or median? )
+    gaussianScore(avgDist).toFloat
+  }
 
   def score(suggest: Suggest): ScoredSuggest = {
     if (suggest.value.trim == "") return ScoredSuggest("", 0)
@@ -99,8 +127,12 @@ class SuggestionScorer(statsReader: TermStatsReader) {
       var score = 1f
       pairs.foreach{ case Array(a, b) =>
         val key = if (a <= b) (a,b) else (b, a)
-        val pairScore = getOrUpdateJoint(key)
-        score *= pairScore.max(1)     // smooth
+        val inter = getOrUpdateJoint(key)
+        val pairScore = inter.size.max(1)   // smooth
+        val adjBoost = if (enableAdjScore) {
+          getOrUpdateAdjScore(key, inter)
+        } else 1f
+        score *= pairScore * adjBoost
       }
       ScoredSuggest(suggest.value, score)
     }
