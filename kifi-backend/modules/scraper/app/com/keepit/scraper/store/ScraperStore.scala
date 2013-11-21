e4f3733e1b6ee6b11a2ff4e0c1c94ddbb3d47051
Stephen Kemmerling <stephen@42go.com>
2013-11-21 14:13:03 -0800

    Fixing S3 screenshot store module issue

--- kifi-backend/modules/scraper/app/com/keepit/scraper/store/ScraperStore.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/store/ScraperStore.scala
@@ -1,6 +1,11 @@
 package com.keepit.common.store
 
 import com.google.inject.{Provides, Singleton}
+import com.amazonaws.services.s3.{AmazonS3Client, AmazonS3}
+import com.keepit.common.healthcheck.AirbrakeNotifier
+import com.keepit.shoebox.ShoeboxServiceClient
+import com.keepit.common.time.Clock
+
 import play.api.Play._
 
 case class ScraperProdStoreModule() extends ProdStoreModule {
@@ -15,6 +20,14 @@ case class ScraperProdStoreModule() extends ProdStoreModule {
     S3ImageConfig(bucket.get, base.get)
   }
 
+  @Singleton
+  @Provides
+  def screenshotStore(amazonS3Client: AmazonS3, shoeboxServiceClient: ShoeboxServiceClient,
+      airbrake: AirbrakeNotifier, clock: Clock, config: S3ImageConfig): S3ScreenshotStore = {
+    new S3ScreenshotStoreImpl(amazonS3Client, shoeboxServiceClient: ShoeboxServiceClient, airbrake, clock, config)
+  }
+
+
 }
 
 case class ScraperDevStoreModule() extends DevStoreModule(ScraperProdStoreModule()) {
@@ -25,5 +38,12 @@ case class ScraperDevStoreModule() extends DevStoreModule(ScraperProdStoreModule
   @Provides
   def s3ImageConfig: S3ImageConfig =
     whenConfigured("cdn.bucket")(prodStoreModule.s3ImageConfig).getOrElse(S3ImageConfig("", "http://dev.ezkeep.com:9000", true))
+
+  @Singleton
+  @Provides
+  def screenshotStore(amazonS3Client: AmazonS3, shoeboxServiceClient: ShoeboxServiceClient,
+      airbrake: AirbrakeNotifier, clock: Clock, config: S3ImageConfig): S3ScreenshotStore = {
+    new S3ScreenshotStoreImpl(amazonS3Client, shoeboxServiceClient: ShoeboxServiceClient, airbrake, clock, config)
+  }
 
 }
