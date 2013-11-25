432de4d796cb0163dd725eedb98f2a40c79f9f8f
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-11-25 15:00:15 -0800

    macro based MonitoredAwait

---
+++ kifi-backend/modules/macros/app/com/keepit/macros/MonitoredAwaitMacro.scala
@@ -0,0 +1,78 @@
+package com.keepit.macros
+
+import scala.concurrent.Await
+import scala.concurrent.Awaitable
+import scala.concurrent.duration._
+import scala.reflect.macros.Context
+import play.api.Logger
+
+trait MonitoredAwait {
+
+  def result[T](awaitable: Awaitable[T], atMost: Duration, errorMessage: String, valueFailureHandler: T): T  = macro MonitoredAwaitMacro.resultWithDefault[T]
+  def result[T](awaitable: Awaitable[T], atMost: Duration, errorMessage: String): T  = macro MonitoredAwaitMacro.result[T]
+
+  def result[T](awaitable: Awaitable[T], atMost: Duration, errorMessage: String, valueFailureHandler: T, position: String): T = {
+    val startTime = System.nanoTime()
+    try {
+      Await.result(awaitable, atMost)
+    } catch {
+      case ex: Throwable =>
+        onError(position, ex, errorMessage)
+        valueFailureHandler
+    } finally {
+      logTime(position, (System.nanoTime() - startTime))
+    }
+  }
+
+  def result[T](awaitable: Awaitable[T], atMost: Duration, errorMessage: String, position: String): T = {
+    val startTime = System.nanoTime()
+    try {
+      Await.result(awaitable, atMost)
+    } catch {
+      case ex: Throwable =>
+        onError(position, ex, errorMessage)
+        throw ex
+    } finally {
+      logTime(position, (System.nanoTime() - startTime))
+    }
+  }
+
+  def onError(position: String, ex: Throwable, msg: String): Unit
+
+  def logTime(tag: String, elapsedTimeNano: Long): Unit
+}
+
+object MonitoredAwaitMacro {
+
+  type MonitoredAwaitContext = Context { type PrefixType =  MonitoredAwait }
+
+  private[this] def getPosition(x: MonitoredAwaitContext) = {
+    val className = Option(x.enclosingClass).map(_.symbol.toString).getOrElse("")
+    val methodName = Option(x.enclosingMethod).map(_.symbol.toString).getOrElse("")
+    val line = x.enclosingPosition.line
+
+    s"Await[${className}][${methodName}]:${line}"
+  }
+
+  def resultWithDefault[T](x: MonitoredAwaitContext)(
+      awaitable: x.Expr[Awaitable[T]],
+      atMost: x.Expr[Duration],
+      errorMessage: x.Expr[String],
+      valueFailureHandler: x.Expr[T]
+  ):  x.Expr[T] = {
+    import x.universe._
+    val position = getPosition(x)
+    reify(x.prefix.splice.result(awaitable.splice, atMost.splice, errorMessage.splice, valueFailureHandler.splice, x.literal(position).splice))
+  }
+
+  def result[T](x: MonitoredAwaitContext)(
+      awaitable: x.Expr[Awaitable[T]],
+      atMost: x.Expr[Duration],
+      errorMessage: x.Expr[String]
+  ):  x.Expr[T] = {
+    import x.universe._
+    val position = getPosition(x)
+    reify(x.prefix.splice.result(awaitable.splice, atMost.splice, errorMessage.splice, x.literal(position).splice))
+  }
+}
+
