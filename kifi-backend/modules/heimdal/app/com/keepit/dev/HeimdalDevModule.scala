cc9603e4596462808ceae3c7230a61d5c5f7349c
Léo Grimaldi <leo@42go.com>
2013-11-25 11:21:58 -0800

    Fix build

--- kifi-backend/modules/heimdal/app/com/keepit/dev/HeimdalDevModule.scala
+++ kifi-backend/modules/heimdal/app/com/keepit/dev/HeimdalDevModule.scala
@@ -1,14 +1,14 @@
 package com.keepit.dev
 
 import com.keepit.common.cache.HeimdalCacheModule
-import com.keepit.heimdal.{ProdMongoModule, HeimdalModule, DevMongoModule}
+import com.keepit.heimdal.{HeimdalModule, DevMongoModule}
 import com.keepit.common.cache.HashMapMemoryCacheModule
 import com.keepit.inject.CommonDevModule
 
 
 case class HeimdalDevModule() extends HeimdalModule(
   cacheModule = HeimdalCacheModule(HashMapMemoryCacheModule()),
-  mongoModule = ProdMongoModule()
+  mongoModule = DevMongoModule()
 ) with CommonDevModule {
 
 }
