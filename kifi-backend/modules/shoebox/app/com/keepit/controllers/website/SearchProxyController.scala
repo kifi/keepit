acccf9bbbf78641c7123a3adb01d9da67fe7e454
Stephen Kemmerling <stephen@42go.com>
2013-11-25 17:05:06 -0800

    Pass through raw query stirng when T-ing search

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/SearchProxyController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/SearchProxyController.scala
@@ -11,20 +11,9 @@ import com.google.inject.Inject
 
 class SearchProxyController @Inject() (actionAuthenticator: ActionAuthenticator, searchServiceClient: SearchServiceClient) extends BrowserExtensionController(actionAuthenticator) with ShoeboxServiceController {
 
-  def search(
-    query: String,
-    filter: Option[String],
-    maxHits: Int,
-    lastUUIDStr: Option[String],
-    context: Option[String],
-    kifiVersion: Option[KifiVersion] = None,
-    start: Option[String] = None,
-    end: Option[String] = None,
-    tz: Option[String] = None,
-    coll: Option[String] = None) = AuthenticatedJsonAction { request =>
-
+  def search() = AuthenticatedJsonAction { request =>
     Async(
-      searchServiceClient.search(request.userId, request.experiments.contains(NO_SEARCH_EXPERIMENTS), request.request.acceptLanguages.map(_.code), query, filter, maxHits, lastUUIDStr, context, kifiVersion, start, end, tz, coll).map{ res =>
+      searchServiceClient.search(request.userId, request.experiments.contains(NO_SEARCH_EXPERIMENTS), request.request.acceptLanguages.map(_.code), request.rawQueryString).map{ res =>
         Ok(res).withHeaders("Content-Type" -> "application/json")
       }
     )
