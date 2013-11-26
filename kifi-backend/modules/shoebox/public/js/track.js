9b15510920e1a61759046776fa50c95140331756
Stephen Kemmerling <stephen@42go.com>
2013-11-26 12:22:04 -0800

    First commit event tracking for the website

---
+++ kifi-backend/modules/shoebox/public/js/track.js
@@ -0,0 +1,66 @@
+/**
+ * ---------------
+ *      Track
+ * ---------------
+ *
+ * Contains functionality to track user interactions on the Site.
+ * It only requires the mixpanel tag to be on the page (+jquery),
+ * Everything else is stand alone (i.e. it attaches itself to the elements)
+ */
+
+(function() {
+  'use strict';
+  var thingsToTrack = {
+    yourKeeps: {
+      selector: ".my-keeps",
+      events: ['click']
+    },
+    yourFriends: {
+      selector: ".my-friends",
+      events: ['click']
+    },
+    installExtension: {
+      selector: ".install-kifi",
+      events: ['click']
+    },
+    tagResults: {
+      selector: ".collection",
+      events: ['click']
+    },
+    kifiBlog: {
+      selector: ".updates-features",
+      events: ['click']
+    }
+  }
+
+  function trackEvent(properties) {
+    mixpanel.track("beta_clicked_internal_page", properties);
+  }
+
+  function defaultEventHandler(tpe, event) {
+    var properties = {
+      'type' : tpe,
+      'where': window.location.pathname.slice(1).split("/"),
+      'what' : [event.target.className, event.target.id]
+    };
+    trackEvent(properties);
+  }
+
+  function attachEventHandlers() {
+    var spec, handler;
+    for (var type in thingsToTrack){
+      spec = thingsToTrack[type];
+      handler = spec.handler || defaultEventHandler;
+      $(spec.selector).each(function(elem) {
+        spec.events.forEach(function(eventType) {
+          elem.addEventListener(eventType, function(evnt) {
+            handler(type, evnt);
+          }, true);
+        });
+      });
+    };
+  }
+
+  attachEventHandlers();
+
+})();
