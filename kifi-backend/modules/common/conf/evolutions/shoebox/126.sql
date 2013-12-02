57a378a72d269dc211e4463c2378c0e792fdb1ae
Jared Jacobs <jared@42go.com>
2013-12-02 09:41:56 -0800

    dropping redundant index

--- kifi-backend/modules/common/conf/evolutions/shoebox/126.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/126.sql
@@ -3,6 +3,7 @@
 # --- !Ups
 
 CREATE INDEX message_i_thread_id_created_at ON message (thread_id, created_at);
+DROP INDEX message_i_my_thread ON message;
 
 INSERT INTO evolutions (name, description) VALUES ('126.sql', 'add index on thread id and creation time in message table');
 
