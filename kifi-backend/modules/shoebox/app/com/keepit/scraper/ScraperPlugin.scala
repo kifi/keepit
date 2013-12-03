ead6e4228967779ffa55cf05ed472821a4a8ff4c
Andrew Conner <git@andrewconner.org>
2013-12-03 11:51:59 -0800

    Bookmark import improvements

--- kifi-backend/modules/shoebox/app/com/keepit/scraper/ScraperPlugin.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/scraper/ScraperPlugin.scala
@@ -5,9 +5,10 @@ import scala.concurrent.Future
 import com.keepit.model.NormalizedURI
 import com.keepit.search.Article
 import com.keepit.scraper.extractor.Extractor
+import com.keepit.common.db.slick.DBSession.RWSession
 
 trait ScraperPlugin extends Plugin {
   def scrapePending(): Future[Seq[(NormalizedURI, Option[Article])]]
-  def scheduleScrape(uri: NormalizedURI): Unit
+  def scheduleScrape(uri: NormalizedURI)(implicit session: RWSession): Unit
   def scrapeBasicArticle(url: String, customExtractor: Option[Extractor] = None): Future[Option[BasicArticle]]
 }
