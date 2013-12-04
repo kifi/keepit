79525a4bf61e9512ac7d39edbbc0c6e8fe4269de
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2013-12-03 16:09:18 -0800

    s3 accesslog to use ltsv format

--- kifi-backend/modules/search/app/com/keepit/common/store/SearchProdStoreModule.scala
+++ kifi-backend/modules/search/app/com/keepit/common/store/SearchProdStoreModule.scala
@@ -3,6 +3,7 @@ package com.keepit.common.store
 import com.google.inject.{Provides, Singleton}
 import com.amazonaws.services.s3.AmazonS3
 import play.api.Play._
+import com.keepit.common.logging.AccessLog
 import com.keepit.search._
 import com.keepit.search.BrowsingHistoryBuilder
 import com.keepit.search.index.{InMemoryIndexStoreImpl, IndexStore, S3IndexStoreImpl}
@@ -12,21 +13,21 @@ case class SearchProdStoreModule() extends ProdStoreModule {
   def configure {}
 
   @Provides @Singleton
-  def browsingHistoryStore(amazonS3Client: AmazonS3, cache: BrowsingHistoryUserIdCache, builder: BrowsingHistoryBuilder): BrowsingHistoryStore = {
+  def browsingHistoryStore(amazonS3Client: AmazonS3, accessLog: AccessLog, cache: BrowsingHistoryUserIdCache, builder: BrowsingHistoryBuilder): BrowsingHistoryStore = {
     val bucketName = S3Bucket(current.configuration.getString("amazon.s3.browsingHistory.bucket").get)
-    new S3BrowsingHistoryStoreImpl(bucketName, amazonS3Client, cache, builder)
+    new S3BrowsingHistoryStoreImpl(bucketName, amazonS3Client, accessLog, cache, builder)
   }
 
   @Provides @Singleton
-  def clickHistoryStore(amazonS3Client: AmazonS3, cache: ClickHistoryUserIdCache, builder: ClickHistoryBuilder): ClickHistoryStore = {
+  def clickHistoryStore(amazonS3Client: AmazonS3, accessLog: AccessLog, cache: ClickHistoryUserIdCache, builder: ClickHistoryBuilder): ClickHistoryStore = {
     val bucketName = S3Bucket(current.configuration.getString("amazon.s3.clickHistory.bucket").get)
-    new S3ClickHistoryStoreImpl(bucketName, amazonS3Client, cache, builder)
+    new S3ClickHistoryStoreImpl(bucketName, amazonS3Client, accessLog, cache, builder)
   }
 
   @Provides @Singleton
-  def indexStore(amazonS3Client: AmazonS3): IndexStore = {
+  def indexStore(amazonS3Client: AmazonS3, accessLog: AccessLog): IndexStore = {
     val bucketName = S3Bucket(current.configuration.getString("amazon.s3.index.bucket").get)
-    new S3IndexStoreImpl(bucketName, amazonS3Client)
+    new S3IndexStoreImpl(bucketName, amazonS3Client, accessLog)
   }
 }
 
@@ -34,23 +35,23 @@ case class SearchDevStoreModule() extends DevStoreModule(SearchProdStoreModule()
   def configure() {}
 
   @Provides @Singleton
-  def browsingHistoryStore(amazonS3Client: AmazonS3, cache: BrowsingHistoryUserIdCache, builder: BrowsingHistoryBuilder): BrowsingHistoryStore = {
+  def browsingHistoryStore(amazonS3Client: AmazonS3, accessLog: AccessLog, cache: BrowsingHistoryUserIdCache, builder: BrowsingHistoryBuilder): BrowsingHistoryStore = {
     whenConfigured("amazon.s3.browsingHistory.bucket")(
-      prodStoreModule.browsingHistoryStore(amazonS3Client, cache, builder)
+      prodStoreModule.browsingHistoryStore(amazonS3Client, accessLog, cache, builder)
     ).getOrElse(new InMemoryBrowsingHistoryStoreImpl())
   }
 
   @Provides @Singleton
-  def clickHistoryStore(amazonS3Client: AmazonS3, cache: ClickHistoryUserIdCache, builder: ClickHistoryBuilder): ClickHistoryStore = {
+  def clickHistoryStore(amazonS3Client: AmazonS3, accessLog: AccessLog, cache: ClickHistoryUserIdCache, builder: ClickHistoryBuilder): ClickHistoryStore = {
     whenConfigured("amazon.s3.clickHistory.bucket")(
-      prodStoreModule.clickHistoryStore(amazonS3Client, cache, builder)
+      prodStoreModule.clickHistoryStore(amazonS3Client, accessLog, cache, builder)
     ).getOrElse(new InMemoryClickHistoryStoreImpl())
   }
 
   @Provides @Singleton
-  def indexStore(amazonS3Client: AmazonS3): IndexStore = {
+  def indexStore(amazonS3Client: AmazonS3, accessLog: AccessLog): IndexStore = {
     whenConfigured("amazon.s3.index.bucket")(
-      prodStoreModule.indexStore(amazonS3Client)
+      prodStoreModule.indexStore(amazonS3Client, accessLog)
     ).getOrElse(new InMemoryIndexStoreImpl())
   }
 }
