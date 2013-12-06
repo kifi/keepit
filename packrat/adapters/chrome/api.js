33b08338abac4a58127bd6dbd97372878af1b791
Jared Jacobs <jared@42go.com>
2013-12-06 13:50:38 -0800

    2.6.61: fixing Firefox

--- packrat/adapters/chrome/api.js
+++ packrat/adapters/chrome/api.js
@@ -332,7 +332,7 @@ var api = function() {
 
     var scripts = meta.contentScripts.filter(function(cs) { return !cs[2] && cs[1].test(page.url) });
 
-    var js = api.log ? '' : 'function log() {return log}', injected;
+    var js = api.logging ? '' : 'function log() {return log}', injected;
     chrome.tabs.executeScript(page.id, {code: js + "this.api&&api.injected", runAt: "document_start"}, function(arr) {
       injected = arr[0] || {};
       done(0);
