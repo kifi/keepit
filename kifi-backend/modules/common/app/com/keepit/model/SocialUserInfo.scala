6910b971aa3366b4217e8b83adc7623fab55d9bb
Andrew Conner <git@andrewconner.org>
2013-12-16 19:39:29 -0800

    Use https for Facebook pictures

--- kifi-backend/modules/common/app/com/keepit/model/SocialUserInfo.scala
+++ kifi-backend/modules/common/app/com/keepit/model/SocialUserInfo.scala
@@ -37,7 +37,7 @@ case class SocialUserInfo(
   def withLastGraphRefresh(lastGraphRefresh : Option[DateTime] = Some(currentDateTime)) = copy(lastGraphRefresh = lastGraphRefresh)
   def getPictureUrl(preferredWidth: Int = 50, preferredHeight: Int = 50): Option[String] = networkType match {
     case SocialNetworks.FACEBOOK =>
-      Some(s"http://graph.facebook.com/$socialId/picture?width=$preferredWidth&height=$preferredHeight")
+      Some(s"https://graph.facebook.com/$socialId/picture?width=$preferredWidth&height=$preferredHeight")
     case _ => pictureUrl
   }
   def getProfileUrl: Option[String] = profileUrl orElse (networkType match {
