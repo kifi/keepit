c8d3dd89d0c01e819a43248a5158f3443a5e182a
Joon Ho Cho <joon@42go.com>
2013-12-16 19:06:07 -0800

    markup and css complete for landing page

--- kifi-backend/modules/shoebox/public/Gruntfile.js
+++ kifi-backend/modules/shoebox/public/Gruntfile.js
@@ -191,7 +191,7 @@ module.exports = function (grunt) {
 				tasks: 'default'
 			},
 			statics: {
-				files: ['static/css/landing.less'],
+				files: ['static/css/*.less'],
 				tasks: 'less:statics'
 			}
 		}
