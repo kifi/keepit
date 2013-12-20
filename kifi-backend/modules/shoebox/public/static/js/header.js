25a16dce6fbb466f2952a4cf9c1aaddc43d629b0
Joon Ho Cho <joon@42go.com>
2013-12-20 13:29:37 -0800

    fixed urls and integrated trackings

--- kifi-backend/modules/shoebox/public/static/js/header.js
+++ kifi-backend/modules/shoebox/public/static/js/header.js
@@ -1,3 +1,15 @@
-$(window).scroll(function () {
-	$('html').toggleClass('scroll', $(this).scrollTop() > 0);
-});
+(function (win) {
+	'use strict';
+
+	var $ = win.jQuery;
+
+	$(win).scroll(function () {
+		var scrolling = $(this).scrollTop() > 0;
+		$('html').toggleClass('scroll', scrolling);
+
+		// for tracking purpose
+		var source = scrolling ? 'scrollingHeader' : 'header';
+		$('.kifi-header').data('trackSource', source);
+	});
+
+})(this);
---
+++ kifi-backend/modules/shoebox/public/static/js/track-ext.js
@@ -0,0 +1,66 @@
+/* global mixpanel */
+(function (win) {
+	'use strict';
+
+	var $ = win.jQuery,
+		document = win.document;
+
+	var EVENT_EXT_VIEW = 'viewed_external_page',
+		EVENT_EXT_CLICK = 'clicked_external_page';
+
+	// on load, track view event
+	$(document).ready(function () {
+		track(EVENT_EXT_VIEW);
+	});
+
+	var DEFAULT_SOURCE_VALUE = 'body';
+
+	// on clicking on links, track click event
+	$(document).on('click', 'a[href]', function (e) {
+		var $el = $(this),
+			data = $el.data(),
+			trackAction = data.trackAction;
+		if (trackAction) {
+			track(EVENT_EXT_CLICK, {
+				action: trackAction,
+				source: getDataFromAncestors($el, 'trackSource', DEFAULT_SOURCE_VALUE)
+			});
+		}
+		e.preventDefault();
+	});
+
+	function getDataFromAncestors($el, name, defaultValue) {
+		var value;
+		while ($el && $el.length) {
+			value = $el.data(name);
+			if (value) {
+				return value;
+			}
+			$el = $el.parent();
+		}
+
+		return defaultValue;
+	}
+
+	function getType() {
+		return $('html').data('trackType');
+	}
+
+	function addDefaultValues(data) {
+		data = data || {};
+		if (data.type == null) {
+			data.type = getType();
+		}
+		if (data.origin == null) {
+			data.origin = win.location.origin;
+		}
+		return data;
+	}
+
+	function track(name, data) {
+		data = addDefaultValues(data);
+		win.console.log('track', name, data);
+		return mixpanel.track(name, data);
+	}
+
+})(this);
