2fbf019e041d65d8fd0a4aa08501e1fd78f4cbc6
eishay <eishay@gmail.com>
2014-01-07 07:01:46 -0800

    some documentation

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/SendgridWebhook.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/SendgridWebhook.scala
@@ -1,12 +1,16 @@
 package com.keepit.controllers
 
 import com.google.inject.Inject
-import com.keepit.common.db.Id
-import com.keepit.model.User
 import play.api.mvc.Action
 import com.keepit.common.logging.Logging
 import com.keepit.common.controller.{WebsiteController, ActionAuthenticator}
 
+/**
+ * see:
+ * http://sendgrid.com/docs/API_Reference/Webhooks/event.html
+ * https://sendgrid.com/app/appSettings/type/eventnotify/id/15
+ * http://sendgrid.com/docs/API_Reference/Webhooks/parse.html
+ */
 class SendgridWebhook  @Inject() (
   actionAuthenticator:ActionAuthenticator)
     extends WebsiteController(actionAuthenticator) with Logging {
