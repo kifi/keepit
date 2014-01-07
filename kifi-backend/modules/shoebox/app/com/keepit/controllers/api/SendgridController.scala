8b1f2cd1f516b6342772cb908e10c79fc9378105
eishay <eishay@gmail.com>
2014-01-07 15:55:58 -0800

    parsing sendgrid events

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/api/SendgridController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/api/SendgridController.scala
@@ -4,11 +4,13 @@ import com.google.inject.Inject
 import play.api.mvc.Action
 import com.keepit.common.logging.Logging
 import com.keepit.common.controller.{WebsiteController, ActionAuthenticator}
+import play.api.libs.json.Json
+import com.keepit.commanders.SendgridEvent
 
 /**
  * see:
- * http://sendgrid.com/docs/API_Reference/Webhooks/event.html
  * https://sendgrid.com/app/appSettings/type/eventnotify/id/15
+ * http://sendgrid.com/docs/API_Reference/Webhooks/event.html
  * http://sendgrid.com/docs/API_Reference/Webhooks/parse.html
  */
 class SendgridController  @Inject() (
@@ -16,7 +18,9 @@ class SendgridController  @Inject() (
     extends WebsiteController(actionAuthenticator) with Logging {
 
   def parseEvent() = Action(parse.json) { request =>
+    val events: Seq[SendgridEvent] = Json.fromJson[Seq[SendgridEvent]](request.body).get
     log.info(s"got a new event from sendgrid: ${request.body.toString()} with headers: ${request.headers.toMap}")
+    log.info(s"there are ${events.size} events in the batch")
     Ok
   }
 }
