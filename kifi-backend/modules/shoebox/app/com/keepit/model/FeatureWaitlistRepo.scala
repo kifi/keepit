03f368353d59a3fcaa910b828d5752ccc101fb0c
Stephen Kemmerling <stephen@42go.com>
2014-01-08 19:11:58 -0800

    Very simple more generic feature wait list

---
+++ kifi-backend/modules/shoebox/app/com/keepit/model/FeatureWaitlistRepo.scala
@@ -0,0 +1,37 @@
+package com.keepit.model
+
+
+import com.google.inject.{Inject, Singleton}
+
+import com.keepit.common.db.slick.{DbRepo, DataBaseComponent, ExternalIdColumnDbFunction}
+import com.keepit.common.time.Clock
+import com.keepit.common.db.{ModelWithExternalId, Id, ExternalId}
+import com.keepit.common.time._
+
+import org.joda.time.DateTime
+
+case class FeatureWaitlistEntry(
+  id: Option[Id[FeatureWaitlistEntry]] = None,
+  createdAt: DateTime = currentDateTime,
+  updatedAt: DateTime = currentDateTime,
+  externalId: ExternalId[FeatureWaitlistEntry] = ExternalId(),
+  email: String,
+  feature: String,
+  userAgent: String
+) extends ModelWithExternalId[FeatureWaitlistEntry] {
+
+  def withId(id: Id[FeatureWaitlistEntry]): FeatureWaitlistEntry = this.copy(id = Some(id))
+  def withUpdateTime(updateTime: DateTime) = this.copy(updatedAt=updateTime)
+}
+
+@Singleton
+class FeatureWaitlistRepo @Inject() (val db: DataBaseComponent, val clock: Clock) extends DbRepo[FeatureWaitlistEntry] with ExternalIdColumnDbFunction[FeatureWaitlistEntry] {
+
+  override val table = new RepoTable[FeatureWaitlistEntry](db, "feature_waitlist") with ExternalIdColumn[FeatureWaitlistEntry] {
+    def email = column[String]("email", O.NotNull)
+    def feature = column[String]("feature", O.NotNull)
+    def userAgent = column[String]("userAgent", O.NotNull)
+    def * = id.? ~ createdAt ~ updatedAt ~ externalId ~ email ~ feature ~ userAgent <> (FeatureWaitlistEntry.apply _, FeatureWaitlistEntry.unapply _)
+  }
+
+}
