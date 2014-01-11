c9c383a3efe2a48468faf9e3f529bf08ff11d9ef
Léo Grimaldi <leo@42go.com>
2014-01-10 18:13:51 -0800

    Updated default keeps

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/UserCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/UserCommander.scala
@@ -581,17 +581,17 @@ object DefaultKeeps {
     val Seq(recipe, shopping, travel, later, funny, example, support) = orderedTags
     Seq(
       // Example keeps
-      (KeepInfo(title = None, url = "http://www.simplyrecipes.com/recipes/bruschetta_with_tomato_and_basil/", isPrivate = true), Seq(example, recipe)),
-      (KeepInfo(title = None, url = "http://www.apple.com/ipad/", isPrivate = true), Seq(example, shopping)),
-      (KeepInfo(title = None, url = "http://www.fourseasons.com/borabora/", isPrivate = true), Seq(example, travel)),
+      (KeepInfo(title = None, url = "http://joythebaker.com/2013/12/curry-hummus-with-currants-and-olive-oil/", isPrivate = true), Seq(example, recipe)),
+      (KeepInfo(title = None, url = "http://www.kickstarter.com/projects/1046165765/egg-the-intelligent-cat-companion", isPrivate = true), Seq(example, shopping)),
+      (KeepInfo(title = None, url = "https://www.airbnb.com/locations/san-francisco/mission-district", isPrivate = true), Seq(example, travel)),
       (KeepInfo(title = None, url = "http://twistedsifter.com/2013/01/50-life-hacks-to-simplify-your-world/", isPrivate = true), Seq(example, later)),
       (KeepInfo(title = None, url = "http://www.youtube.com/watch?v=_OBlgSz8sSM", isPrivate = true), Seq(example, funny)),
 
       // Support Keeps
-      (KeepInfo(title = Some("Install Kifi"), url = "https://www.kifi.com/install", isPrivate = true), Seq(support)),
-      (KeepInfo(title = Some("How to Use Kifi"), url = "http://support.kifi.com/", isPrivate = true), Seq(support)),
-      (KeepInfo(title = Some("Contact Us"), url = "http://support.kifi.com/customer/portal/emails/new", isPrivate = true), Seq(support)),
-      (KeepInfo(title = Some("Kifi is better with more friends"), url = "https://www.kifi.com/friends/invite", isPrivate = true), Seq(support))
+      (KeepInfo(title = Some("kifi • Install Kifi on Firefox and Chrome"), url = "https://www.kifi.com/install", isPrivate = true), Seq(support)),
+      (KeepInfo(title = Some("kifi • How to Use Kifi"), url = "http://support.kifi.com/customer/portal/articles/1397866-introduction-to-kifi-", isPrivate = true), Seq(support)),
+      (KeepInfo(title = Some("kifi • Contact Us"), url = "http://support.kifi.com/customer/portal/emails/new", isPrivate = true), Seq(support)),
+      (KeepInfo(title = Some("kifi • Find friends your friends on Kifi"), url = "https://www.kifi.com/friends/invite", isPrivate = true), Seq(support))
     )
   }
 }
