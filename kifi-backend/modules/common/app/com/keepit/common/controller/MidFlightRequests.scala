e3d703d69ea1e7de1cc632a65e10550b9efbdf88
eishay <eishay@gmail.com>
2014-01-12 00:33:59 -0800

    alerting if there are more then 100 concurrent requests processed at the same time

--- kifi-backend/modules/common/app/com/keepit/common/controller/MidFlightRequests.scala
+++ kifi-backend/modules/common/app/com/keepit/common/controller/MidFlightRequests.scala
@@ -2,15 +2,20 @@ package com.keepit.common.controller
 
 import java.util.concurrent.atomic.{AtomicLong, AtomicInteger}
 import com.google.inject.{Inject, Singleton}
+import com.keepit.common.healthcheck.AirbrakeNotifier
 
 @Singleton
-class MidFlightRequests @Inject() () {
+class MidFlightRequests @Inject() (airbrake: AirbrakeNotifier) {
   private val currentCount = new AtomicInteger(0)
   private val totalRequests = new AtomicLong(0L)
   def totalRequestsSoFar: Long = totalRequests.get()
   def comingIn(): Int = {
     totalRequests.incrementAndGet()
-    currentCount.incrementAndGet()
+    val count = currentCount.incrementAndGet()
+    if (count > 100) { //say that more then 100 concurrent request is an issue
+      airbrake.notify(s"There are $count concurrent requests on this service, $totalRequestsSoFar since start")
+    }
+    count
   }
   def goingOut(): Int = {
     currentCount.decrementAndGet()
