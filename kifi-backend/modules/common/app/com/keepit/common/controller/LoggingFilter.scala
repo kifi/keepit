7e747ddcd8acbed9bffb36b60fc03d5f9d44d22e
eishay <eishay@gmail.com>
2014-01-13 18:34:42 -0800

    formatting

--- kifi-backend/modules/common/app/com/keepit/common/controller/LoggingFilter.scala
+++ kifi-backend/modules/common/app/com/keepit/common/controller/LoggingFilter.scala
@@ -26,7 +26,7 @@ class LoggingFilter() extends EssentialFilter {
 
   def apply(next: EssentialAction) = new EssentialAction {
     def apply(rh: RequestHeader): Iteratee[Array[Byte],play.api.mvc.Result] = {
-        if (!discovery.amIUp && (discovery.timeSinceLastStatusChange > 20000L)) {
+      if (!discovery.amIUp && (discovery.timeSinceLastStatusChange > 20000L)) {
         val message = s"Current status of service ${discovery.thisInstance.map(_.instanceInfo.localIp).getOrElse("UNREGISTERED")} ${discovery.myStatus.getOrElse("UNKNOWN")}, last changed ${discovery.timeSinceLastStatusChange}ms ago"
         //system is going down, maybe the logger, emails, airbrake are gone already
         println(message)
