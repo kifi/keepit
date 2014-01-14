ec66fe4811e2c64950bb5ab1a1051eba17cece19
eishay <eishay@gmail.com>
2014-01-14 11:48:04 -0800

    mid flight

--- kifi-backend/modules/common/app/com/keepit/common/controller/MidFlightRequests.scala
+++ kifi-backend/modules/common/app/com/keepit/common/controller/MidFlightRequests.scala
@@ -3,13 +3,14 @@ package com.keepit.common.controller
 import java.util.concurrent.atomic.{AtomicLong, AtomicInteger}
 import com.google.inject.{Inject, Singleton}
 import com.keepit.common.healthcheck.AirbrakeNotifier
+import play.api.mvc.RequestHeader
 
 @Singleton
 class MidFlightRequests @Inject() (airbrake: AirbrakeNotifier) {
   private val currentCount = new AtomicInteger(0)
   private val totalRequests = new AtomicLong(0L)
   def totalRequestsSoFar: Long = totalRequests.get()
-  def comingIn(): Int = {
+  def comingIn(rh: RequestHeader): Int = {
     totalRequests.incrementAndGet()
     val count = currentCount.incrementAndGet()
     if (count > 100) { //say that more then 100 concurrent request is an issue
@@ -17,7 +18,7 @@ class MidFlightRequests @Inject() (airbrake: AirbrakeNotifier) {
     }
     count
   }
-  def goingOut(): Int = {
+  def goingOut(rh: RequestHeader): Int = {
     currentCount.decrementAndGet()
   }
 }
