42eb4fdf8638ee561ac3ccb1e16a69ffe45a4a1e
eishay <eishay@gmail.com>
2014-01-14 16:37:03 -0800

    have the MidFlightRequests alert with the top path/requests that come in and * make sure we’re not over alerting, no more then one per 10min * better counting of requests and careful about removing ones

--- kifi-backend/modules/common/app/com/keepit/common/controller/MidFlightRequests.scala
+++ kifi-backend/modules/common/app/com/keepit/common/controller/MidFlightRequests.scala
@@ -1,24 +1,46 @@
 package com.keepit.common.controller
 
-import java.util.concurrent.atomic.{AtomicLong, AtomicInteger}
+import com.google.common.collect.MapMaker
 import com.google.inject.{Inject, Singleton}
 import com.keepit.common.healthcheck.AirbrakeNotifier
 import play.api.mvc.RequestHeader
+import java.util.concurrent.ConcurrentMap
+import scala.collection.JavaConversions._
+
 
 @Singleton
 class MidFlightRequests @Inject() (airbrake: AirbrakeNotifier) {
-  private val currentCount = new AtomicInteger(0)
-  private val totalRequests = new AtomicLong(0L)
-  def totalRequestsSoFar: Long = totalRequests.get()
+  private val currentRequests: ConcurrentMap[RequestHeader, Long] = new MapMaker().concurrencyLevel(4).weakKeys().makeMap()
+
+  def count: Int = currentRequests.size()
+  @volatile private var lastAlert: Long = -1
+
   def comingIn(rh: RequestHeader): Int = {
-    totalRequests.incrementAndGet()
-    val count = currentCount.incrementAndGet()
-    if (count > 100) { //say that more then 100 concurrent request is an issue
-      airbrake.notify(s"There are $count concurrent requests on this service, $totalRequestsSoFar since start")
+    currentRequests.put(rh, System.currentTimeMillis())
+    val count = currentRequests.size() //may not be accurate since we're not synchronizing this block, but good enough
+    if (count > 30) { //say that more then 30 concurrent request is an issue
+      alert(count)
     }
     count
   }
-  def goingOut(rh: RequestHeader): Int = {
-    currentCount.decrementAndGet()
+
+  private def alert(count: Long): Unit = {
+    val now = System.currentTimeMillis()
+    if (now - lastAlert > 600000) { //10 minutes
+      synchronized {
+        if (now - lastAlert > 600000) { //10 minutes - double check after getting into the synchronized block
+          airbrake.notify(s"$count concurrent requests: $topRequests")
+          lastAlert = System.currentTimeMillis()
+        }
+      }
+    }
   }
+
+  def topRequests: String = {
+    val paths = currentRequests.keySet().toList map {rh => rh.path}
+    val countedPaths = paths.foldLeft(Map.empty[String, Int]) { (m, x) => m + ((x, m.getOrElse(x, 0) + 1)) }
+    countedPaths.toList.sortWith{case t => t._1._2 > t._2._2}.map{t => s"${t._2}:${t._1}"}.mkString(",")
+  }
+
+  def goingOut(rh: RequestHeader): Long = currentRequests.remove(rh)
 }
