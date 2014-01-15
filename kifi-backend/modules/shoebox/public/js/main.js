2d3326dbe237805bae27ac1db74807ed3a4475ec
Joon Ho Cho <joon@42go.com>
2014-01-15 11:37:36 -0800

    remove invite count related functions

--- kifi-backend/modules/shoebox/public/js/main.js
+++ kifi-backend/modules/shoebox/public/js/main.js
@@ -2243,16 +2243,10 @@ $(function () {
 		friendsTimeout = setTimeout(prepInviteTab, 200);
 	}
 
-	var invitesUpdatedAt;
-	function updateInviteCache() {
-		invitesUpdatedAt = Date.now();
-	}
-	updateInviteCache();
 
 	var friendsToShow = FETCH_SIZE;
 	var friendsShowing = [];
 	var moreFriends = true;
-	var invitesLeft;
 	var inviteEmailTemplate = Handlebars.compile($('#invite-email-tpl').html());
 
 	function alignInviteFriends() {
@@ -2284,8 +2278,7 @@ $(function () {
 			limit: limit,
 			after: moreToShow ? lastValue : void 0,
 			search: search,
-			network: network,
-			updatedAt: invitesUpdatedAt
+			network: network
 		};
 
 		log('[prepInviteTab]', opts);
@@ -2363,10 +2356,6 @@ $(function () {
 		})
 		.always(function () {
 			$nwFriendsLoading.hide();
-		});
-		$.getJSON(xhrBase + '/user/inviteCounts', { updatedAt: invitesUpdatedAt }, function (invites) {
-			invitesLeft = invites.left;
-			$('.num-invites').text(invitesLeft).parent().show();
 		});
 	}
 
@@ -2452,15 +2441,6 @@ $(function () {
 		});
 	}
 
-	var $noInvitesDialog = $('.no-invites-dialog').detach().show()
-	.on('click', '.more-invites', function (e) {
-		e.preventDefault();
-		$noInvitesDialog.dialog('hide');
-		$.postJson('/site/user/needMoreInvites', {}, function (data) {
-			$noInvitesDialog.dialog('hide');
-		});
-	});
-
 	var $inviteMessageDialog = $('.invite-message-dialog').detach().show()
 	.on('submit', 'form', function (e) {
 		e.preventDefault();
@@ -2471,7 +2451,6 @@ $(function () {
 				log('sent invite');
 				$inviteMessageDialog.dialog('hide');
 			}
-			updateInviteCache();
 			prepInviteTab();
 		});
 	}).on('click', '.invite-cancel', function (e) {
@@ -2482,10 +2461,6 @@ $(function () {
 	var inviteMessageDialogTmpl = Tempo.prepare($inviteMessageDialog);
 	$('.invite-filter').on('input', filterFriends);
 	$('.invite-friends').on('click', '.invite-button', function () {
-		if (!invitesLeft) {
-			$noInvitesDialog.dialog('show');
-			return;
-		}
 		var $friend = $(this).closest('.invite-friend'), fullSocialId = $friend.data('value');
 		// TODO(greg): figure out why this doesn't work cross-domain
 		if (/^facebook/.test(fullSocialId)) {
