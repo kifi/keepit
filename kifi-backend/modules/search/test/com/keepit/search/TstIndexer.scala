8cd5f2ecd11b92c8abd70a2404d1007127189b53
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-01-14 18:27:30 -0800

    factored out a common test indexer + typo

---
+++ kifi-backend/modules/search/test/com/keepit/search/TstIndexer.scala
@@ -0,0 +1,50 @@
+package com.keepit.search
+
+import com.keepit.common.db.Id
+import com.keepit.common.db.SequenceNumber
+import com.keepit.search.index.IndexDirectory
+import com.keepit.search.index.Indexer
+import com.keepit.search.index.Indexable
+import org.apache.lucene.index.IndexWriterConfig
+import com.keepit.search.semantic.SemanticVectorBuilder
+import org.apache.lucene.analysis.Analyzer
+import java.io.StringReader
+
+class Tst(val id: Id[Tst], val text: String, val personalText: String)
+
+class TstIndexer(indexDirectory: IndexDirectory, indexWriterConfig: IndexWriterConfig) extends Indexer[Tst](indexDirectory, indexWriterConfig) {
+  def buildIndexable(id: Id[Tst]): Indexable[Tst] = throw new UnsupportedOperationException()
+  def buildIndexable(data: Tst): Indexable[Tst] = new TstIndexable(data.id, data.text, data.personalText, indexWriterConfig.getAnalyzer)
+
+  def index(id: Id[Tst], text: String, personalText: String) = {
+    indexDocuments(Some(buildIndexable(new Tst(id, text, personalText))).iterator, 100)
+  }
+
+  def getPersonalizedSearcher(ids: Set[Long]) = PersonalizedSearcher(searcher, ids)
+
+  def update(): Int = ???
+}
+
+class TstIndexable[Tst](override val id: Id[Tst], val text: String, val personalText: String, analyzer: Analyzer) extends Indexable[Tst] {
+
+  implicit def toReader(text: String) = new StringReader(text)
+
+  override val sequenceNumber = SequenceNumber.ZERO
+  override val isDeleted = false
+
+  override def buildDocument = {
+    val doc = super.buildDocument
+    val content = buildTextField("c", text)
+    val personal = buildTextField("p", personalText)
+    val builder = new SemanticVectorBuilder(60)
+    builder.load( analyzer.tokenStream("c", text) )
+    val semanticVector = buildSemanticVectorField("sv", builder)
+    val docSemanticVector = buildDocSemanticVectorField("docSV", builder)
+    doc.add(content)
+    doc.add(personal)
+    doc.add(semanticVector)
+    doc.add(docSemanticVector)
+    doc
+  }
+}
+
--- kifi-backend/modules/search/test/com/keepit/search/query/SemanticVectorQueryTest.scala
+++ kifi-backend/modules/search/test/com/keepit/search/query/SemanticVectorQueryTest.scala
@@ -9,51 +9,10 @@ import com.keepit.common.db.SequenceNumber
 import com.keepit.search.index._
 import com.keepit.search.PersonalizedSearcher
 import com.keepit.search.semantic.SemanticVectorBuilder
-import java.io.StringReader
-import scala.Some
+import com.keepit.search.Tst
+import com.keepit.search.TstIndexer
 
 class SemanticVectorQueryTest extends Specification {
-
-  class Tst(val id: Id[Tst], val text: String, val fallbackText: String)
-
-  class TstIndexer(indexDirectory: VolatileIndexDirectoryImpl, indexWriterConfig: IndexWriterConfig)
-    extends Indexer[Tst](indexDirectory, indexWriterConfig) {
-
-    class TstIndexable[Tst](override val id: Id[Tst], val text: String, val fallbackText: String) extends Indexable[Tst] {
-
-      implicit def toReader(text: String) = new StringReader(text)
-
-      override val sequenceNumber = SequenceNumber.ZERO
-      override val isDeleted = false
-
-      override def buildDocument = {
-        val doc = super.buildDocument
-        val analyzer = indexWriterConfig.getAnalyzer
-        val content = buildTextField("c", text)
-        val fallback = buildTextField("fallback", fallbackText)
-        val builder = new SemanticVectorBuilder(60)
-        builder.load( analyzer.tokenStream("c", text) )
-        val semanticVector = buildSemanticVectorField("sv", builder)
-        val docSemanticVector = buildDocSemanticVectorField("docSV", builder)
-        doc.add(content)
-        doc.add(fallback)
-        doc.add(semanticVector)
-        doc.add(docSemanticVector)
-        doc
-      }
-    }
-
-    def buildIndexable(id: Id[Tst]): Indexable[Tst] = throw new UnsupportedOperationException()
-    def buildIndexable(data: Tst): Indexable[Tst] = new TstIndexable(data.id, data.text, data.fallbackText)
-
-    def index(id: Id[Tst], text: String, fallbackText: String) = {
-      indexDocuments(Some(buildIndexable(new Tst(id, text, fallbackText))).iterator, 100)
-    }
-
-    def getPersonalizedSeacher(ids: Set[Long]) = PersonalizedSearcher(searcher, ids)
-
-    def update(): Int = ???
-  }
 
   val indexingAnalyzer = DefaultAnalyzer.forIndexing
   val config = new IndexWriterConfig(Version.LUCENE_41, indexingAnalyzer)
@@ -67,10 +26,10 @@ class SemanticVectorQueryTest extends Specification {
     "score using a personalized vector" in {
       var q = SemanticVectorQuery(new Term("sv", "abc"))
 
-      val searcher0 = indexer.getPersonalizedSeacher(Set(0L))
-      val searcher1 = indexer.getPersonalizedSeacher(Set(1L))
-      val searcher2 = indexer.getPersonalizedSeacher(Set(2L))
-      val searcher3 = indexer.getPersonalizedSeacher(Set(3L))
+      val searcher0 = indexer.getPersonalizedSearcher(Set(0L))
+      val searcher1 = indexer.getPersonalizedSearcher(Set(1L))
+      val searcher2 = indexer.getPersonalizedSearcher(Set(2L))
+      val searcher3 = indexer.getPersonalizedSearcher(Set(3L))
       searcher0.search(q).head.id === 0
       searcher1.search(q).head.id === 1
       searcher2.search(q).head.id === 2
