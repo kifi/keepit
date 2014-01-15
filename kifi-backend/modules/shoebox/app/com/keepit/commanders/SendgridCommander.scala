648a0116c38ec7e58629acbee0378af1cdadfce0
Martin Raison <martin@42go.com>
2014-01-14 19:58:30 -0800

    Instrumented send grid email alert sending, using Slave db to fetch info

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/SendgridCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/SendgridCommander.scala
@@ -5,6 +5,7 @@ import com.keepit.common.mail.{LocalPostOffice, PostOffice, EmailAddresses, Elec
 import com.keepit.common.db.slick.Database
 import com.keepit.heimdal.{UserEventTypes, HeimdalContext, UserEvent, HeimdalServiceClient, HeimdalContextBuilderFactory}
 import com.keepit.model.{EmailAddress, EmailAddressRepo}
+import com.keepit.common.performance.timing
 
 class SendgridCommander @Inject() (
   db: Database,
@@ -21,7 +22,8 @@ class SendgridCommander @Inject() (
 
   private val alertEventTypes = Set("dropped", "bounce", "spamreport")
 
-  private def emailAlert(event: SendgridEvent, emailOpt: Option[ElectronicMail]): Unit = {
+  private def emailAlert(event: SendgridEvent, emailOpt: Option[ElectronicMail]): Unit =
+    timing(s"sendgrid emailAlert eventType(${event.event}}) mailId(${event.mailId}}) ") {
     event.event foreach { eventType =>
       if (alertEventTypes.contains(eventType)) {
         val htmlBody = emailOpt match {
@@ -49,14 +51,15 @@ class SendgridCommander @Inject() (
     }
   }
 
-  private def heimdalEvent(event: SendgridEvent, emailOpt: Option[ElectronicMail]): Unit = {
+  private def heimdalEvent(event: SendgridEvent, emailOpt: Option[ElectronicMail]): Unit =
+    timing(s"sendgrid heimdalEvent eventType(${event.event}}) mailId(${event.mailId}}) ") {
     for {
       eventType <- event.event
       address <- event.email
       email <- emailOpt
     } yield {
       if (PostOffice.Categories.User.all.contains(email.category)) {
-        val emailAddresses = db.readOnly{ implicit s => emailAddressRepo.getByAddress(address).toSet }
+        val emailAddresses = db.readOnly{ implicit s => emailAddressRepo.getByAddress(address).toSet }(Database.Slave)
         emailAddresses foreach { emailAddress =>
           val contextBuilder =  heimdalContextBuilder()
           contextBuilder += ("action", eventType)
@@ -69,10 +72,12 @@ class SendgridCommander @Inject() (
   }
 
   private def report(event: SendgridEvent): Unit = {
-    val emailOpt = for {
-      mailId <- event.mailId
-      mail <- db.readOnly{ implicit s => electronicMailRepo.getOpt(mailId) }
-    } yield mail
+    val emailOpt = timing(s"sendgrid (fetch email for alert) eventType(${event.event}}) mailId(${event.mailId}}) ") {
+      for {
+        mailId <- event.mailId
+        mail <- db.readOnly{ implicit s => electronicMailRepo.getOpt(mailId) }(Database.Slave)
+      } yield mail
+    }
     emailAlert(event, emailOpt)
     heimdalEvent(event, emailOpt)
   }
