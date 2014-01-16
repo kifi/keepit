14460644b3b4797f9abbfcf311068b55503e9366
Martin Raison <martin@42go.com>
2014-01-16 11:52:44 -0800

    Admin panels for showing only real users or only fake users

--- kifi-backend/modules/shoebox/app/com/keepit/admin/AdminUserController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/admin/AdminUserController.scala
@@ -223,6 +223,8 @@ class AdminUserController @Inject() (
   }
 
   def allUsersView = usersView(0)
+  def allRealUsersView = realUsersView(0)
+  def allFakeUsersView = fakeUsersView(0)
 
   private def userStatistics(user: User)(implicit s: RSession): UserStatistics = {
     val kifiInstallations = kifiInstallationRepo.all(user.id.get).sortWith((a,b) => b.updatedAt.isBefore(a.updatedAt)).take(3)
@@ -240,6 +242,26 @@ class AdminUserController @Inject() (
       userRepo.pageExcluding(UserStates.PENDING, UserStates.INACTIVE, UserStates.BLOCKED)(page, PAGE_SIZE) map userStatistics
     }
     val userCount = db.readOnly { implicit s => userRepo.countExcluding(UserStates.PENDING, UserStates.INACTIVE, UserStates.BLOCKED) }
+    Ok(html.admin.users(users, page, userCount, Math.ceil(userCount.toFloat / PAGE_SIZE.toFloat).toInt, None))
+  }
+
+  def realUsersView(page: Int = 0) = AdminHtmlAction { implicit request =>
+    val PAGE_SIZE = 50
+
+    val users = db.readOnly { implicit s =>
+      userRepo.pageExcludingWithoutExp(UserStates.PENDING, UserStates.INACTIVE, UserStates.BLOCKED)(ExperimentType.FAKE)(page, PAGE_SIZE) map userStatistics
+    }
+    val userCount = db.readOnly { implicit s => userRepo.countExcludingWithoutExp(UserStates.PENDING, UserStates.INACTIVE, UserStates.BLOCKED)(ExperimentType.FAKE) }
+    Ok(html.admin.users(users, page, userCount, Math.ceil(userCount.toFloat / PAGE_SIZE.toFloat).toInt, None))
+  }
+
+  def fakeUsersView(page: Int = 0) = AdminHtmlAction { implicit request =>
+    val PAGE_SIZE = 50
+
+    val users = db.readOnly { implicit s =>
+      userRepo.pageExcludingWithExp(UserStates.PENDING, UserStates.INACTIVE, UserStates.BLOCKED)(ExperimentType.FAKE)(page, PAGE_SIZE) map userStatistics
+    }
+    val userCount = db.readOnly { implicit s => userRepo.countExcludingWithExp(UserStates.PENDING, UserStates.INACTIVE, UserStates.BLOCKED)(ExperimentType.FAKE) }
     Ok(html.admin.users(users, page, userCount, Math.ceil(userCount.toFloat / PAGE_SIZE.toFloat).toInt, None))
   }
 
