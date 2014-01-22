fe8d61745e20bbc428d59a028050b586e1dfc67d
Léo Grimaldi <leo@42go.com>
2014-01-21 16:17:41 -0800

    Removes AUTO_GEN from user status

--- kifi-backend/modules/common/app/com/keepit/model/UserExperiment.scala
+++ kifi-backend/modules/common/app/com/keepit/model/UserExperiment.scala
@@ -47,9 +47,6 @@ object ExperimentType {
   val EXTENSION_LOGGING = ExperimentType("extension_logging")
   val SHOW_HIT_SCORES = ExperimentType("show_hit_scores")
 
-  val DONT_SHOW_IN_ANALYTICS = List(ADMIN, FAKE)
-  val DONT_SHOW_IN_ANALYTICS_STR = DONT_SHOW_IN_ANALYTICS map {s => s"'$s'"} mkString ","
-
   def get(str: String): ExperimentType = str.toLowerCase.trim match {
     case ADMIN.value => ADMIN
     case AUTO_GEN.value => AUTO_GEN
@@ -66,8 +63,7 @@ object ExperimentType {
   }
 
   def getUserStatus(experiments: Set[ExperimentType]): String =
-    if (experiments.contains(AUTO_GEN)) AUTO_GEN.value
-    else if (experiments.contains(FAKE)) FAKE.value
+    if (experiments.contains(FAKE)) FAKE.value
     else if (experiments.contains(ADMIN)) ADMIN.value
     else "standard"
 
