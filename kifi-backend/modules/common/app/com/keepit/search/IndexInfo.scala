c022f7eabbf7834b25d57ad5669246e3ac0f9736
yingjieMiao <yingjie@42go.com>
2014-01-24 15:04:59 -0800

    readable index size

--- kifi-backend/modules/common/app/com/keepit/search/IndexInfo.scala
+++ kifi-backend/modules/common/app/com/keepit/search/IndexInfo.scala
@@ -11,8 +11,26 @@ case class IndexInfo(
   indexSize: Option[Long]
 )
 
+case class ReadableIndexInfo(
+  name: String,
+  sequenceNumber: SequenceNumber,
+  numDocs: Int,
+  committedAt: Option[String],
+  indexSize: Option[String]
+)
+
 object IndexInfo {
   implicit val indexInfoFormat = Json.format[IndexInfo]
+  def toReadableIndexInfo(info: IndexInfo): ReadableIndexInfo = {
+    val indexSize = info.indexSize.map{ n =>
+      (n/1e3, n/1e6, n/1e9) match {
+        case (_, _, gb) if gb.toInt > 0 => "%.2f".format(gb.toFloat) + "GB"
+        case (_, mb, _) if mb.toInt > 0 => "%.2f".format(mb.toFloat) + "MB"
+        case (kb, _, _) => "%.2f".format(kb.toFloat) + "KB"
+      }
+    }
+    ReadableIndexInfo(info.name, info.sequenceNumber, info.numDocs, info.committedAt, indexSize)
+  }
 }
 
 case class SharingUserInfo(
