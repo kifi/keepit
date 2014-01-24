f2cdf3a637dea1d72922bde7c8b1535280cf1fac
Andrew Conner <git@andrewconner.org>
2014-01-24 11:20:41 -0800

    Letting clients know when auth is expired.
    For the extension, mobile, and the site, the endpoint that gives user data now will include a `”notAuthed”: [array, of, social, networks, that, are, expired]`

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobileUserController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobileUserController.scala
@@ -64,8 +64,12 @@ class MobileUserController @Inject() (
 
   private def getUserInfo[T](request: AuthenticatedRequest[T]) = {
     val user = userCommander.getUserInfo(request.user)
+    val notAuthed = db.readOnly { implicit session =>
+      socialUserRepo.getNotAuthorizedByUser(userId).map(_.networkType.name)
+    }
     Ok(toJson(user.basicUser).as[JsObject] ++
        toJson(user.info).as[JsObject] ++
+      Json.obj("notAuthed" -> notAuthed) ++
        Json.obj("experiments" -> request.experiments.map(_.value)))
   }
 
