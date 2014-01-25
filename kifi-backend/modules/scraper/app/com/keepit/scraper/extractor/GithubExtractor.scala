e45e72cd9bc59aa2e27ab6b5930b506f401cc8bf
eishay <eishay@gmail.com>
2014-01-24 16:25:54 -0800

    adding tests and custom github behavior

--- kifi-backend/modules/scraper/app/com/keepit/scraper/extractor/GithubExtractor.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/extractor/GithubExtractor.scala
@@ -22,9 +22,7 @@ object GithubExtractorProvider extends ExtractorProvider {
 
 class GithubExtractor(url: String, maxContentChars: Int) extends JsoupBasedExtractor(url, maxContentChars) with Logging {
 
-  override def getMetadata(name: String): Option[String] = None //We don't trust their data
-
-  override def getLink(name: String): Option[String] = None //We don't trust their data
+  override def getCanonicalUrl(): Option[String] = None //we don't trust github's canonical urls
 
   def parse(doc: Document) = {
     // Determine which kind of page we're on
