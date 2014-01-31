fd745d044757b32563708ccf02770da4a306694e
Martin Raison <martin@42go.com>
2014-01-30 18:23:13 -0800

    Renaming val

--- kifi-backend/modules/shoebox/app/com/keepit/scraper/ScrapeSchedulerPlugin.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/scraper/ScrapeSchedulerPlugin.scala
@@ -7,7 +7,7 @@ import com.keepit.scraper.extractor.{ExtractorProviderType}
 import com.keepit.common.db.slick.DBSession.RWSession
 
 trait ScrapeSchedulerPlugin extends Plugin {
-  def scheduleScrape(uri: NormalizedURI, randomDelayMillis: Int = 0)(implicit session: RWSession): Unit
+  def scheduleScrape(uri: NormalizedURI, delayMillis: Int = 0)(implicit session: RWSession): Unit
   def scrapeBasicArticle(url: String, extractorProviderType:Option[ExtractorProviderType]): Future[Option[BasicArticle]] // todo: move out
   def getSignature(url: String, extractorProviderType: Option[ExtractorProviderType]): Future[Option[Signature]]
 }
