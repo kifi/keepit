8200299c4f498adbee8d22fcfcea8026935fd01f
yingjieMiao <yingjie@42go.com>
2014-01-31 12:37:31 -0800

    provide a default value 0 to seq column

--- kifi-backend/modules/common/conf/evolutions/shoebox/135.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/135.sql
@@ -9,7 +9,7 @@
 create sequence user_connection_sequence;
 ---
 
-alter table user_connection add seq INT;
+alter table user_connection add seq INT NOT NULL DEFAULT 0;
 create index user_connection_seq_index on user_connection(seq);
 
 
@@ -19,7 +19,7 @@ create index user_connection_seq_index on user_connection(seq);
 -- H2:
 create sequence search_friend_sequence;
 
-alter table search_friend add seq INT;
+alter table search_friend add seq INT NOT NULL DEFAULT 0;
 create index search_friend_seq_index on search_friend(seq);
 
 insert into evolutions (name, description) values ('135.sql', 'add seq number to user_connection and search_friend');
