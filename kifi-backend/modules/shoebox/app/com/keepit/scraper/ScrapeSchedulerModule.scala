26c5d0043fad53dbfb7cd0a0ece08be8c666a25b
Raymond Ng <ray@42go.com>
2014-01-31 16:09:52 -0800

    scraper module: clean-up

--- kifi-backend/modules/shoebox/app/com/keepit/scraper/ScrapeSchedulerModule.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/scraper/ScrapeSchedulerModule.scala
@@ -16,18 +16,6 @@ case class ProdScrapeSchedulerModule() extends ScrapeSchedulerModule {
   @Singleton
   @Provides
   def scraperConfig: ScraperConfig = ScraperConfig()
-
-//  @Singleton
-//  @Provides
-//  def httpFetcher(airbrake:AirbrakeNotifier): HttpFetcher = {
-//    new HttpFetcherImpl(
-//      airbrake,
-//      userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
-//      connectionTimeout = 30000,
-//      soTimeOut = 30000,
-//      trustBlindly = true
-//    )
-//  }
 }
 
 case class DevScrapeSchedulerModule() extends ScrapeSchedulerModule {
@@ -39,17 +27,5 @@ case class DevScrapeSchedulerModule() extends ScrapeSchedulerModule {
   @Singleton
   @Provides
   def scraperConfig: ScraperConfig = ScraperConfig().copy(scrapePendingFrequency = 5, pendingOverdueThreshold = 5, pendingSkipThreshold = 50)
-
-//  @Singleton
-//  @Provides
-//  def httpFetcher(airbrake:AirbrakeNotifier): HttpFetcher = {
-//    new HttpFetcherImpl(
-//      airbrake,
-//      userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
-//      connectionTimeout = 3000,
-//      soTimeOut = 3000,
-//      trustBlindly = true
-//    )
-//  }
 }
 
