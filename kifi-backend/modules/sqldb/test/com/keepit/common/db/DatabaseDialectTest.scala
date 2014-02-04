1e0a7a721acd44358916c078eb136069a3703ba8
Andrew Conner <git@andrewconner.org>
2014-02-03 19:38:15 -0800

    Fixing production bookmark ordering. More to do.

--- kifi-backend/modules/sqldb/test/com/keepit/common/db/DatabaseDialectTest.scala
+++ kifi-backend/modules/sqldb/test/com/keepit/common/db/DatabaseDialectTest.scala
@@ -9,7 +9,7 @@ import com.keepit.test._
 
 class DatabaseDialectTest extends Specification with DbTestInjector {
 
-  val dec_20_2013 = new DateTime(2013, 12, 20, 0, 0, 0, DEFAULT_DATE_TIME_ZONE)
+  val dec_20_2013 = new DateTime(2013, 12, 20, 10, 1, 30, DEFAULT_DATE_TIME_ZONE)
 
   "MySqlDatabaseDialect" should {
 
@@ -30,6 +30,25 @@ class DatabaseDialectTest extends Specification with DbTestInjector {
           val st = s.conn.createStatement()
           val sql = s"""select DATEADD('MONTH', 1, ${H2DatabaseDialect.day(dec_20_2013)}) as day from dual"""
           sql === """select DATEADD('MONTH', 1, PARSEDATETIME('2013-12-20', 'y-M-d')) as day from dual"""
+          val rs = st.executeQuery(sql)
+          rs.next
+          rs.getString("day").split(' ')(0) === "2014-01-20"
+        }
+      }
+    }
+
+
+    // H2 uses SimpleDateFormat which is *not* ISO-8601 compatible!
+    "stringToDate to string" in {
+      H2DatabaseDialect.dateTime(dec_20_2013) === """PARSEDATETIME('2013-12-20 10:01:30.000', 'yyyy-MM-d hh:mm:ss.SSS')"""
+    }
+
+    "stringToDay to db" in {
+      withDb() { implicit injector: Injector =>
+        inject[Database].readWrite { implicit s =>
+          val st = s.conn.createStatement()
+          val sql = s"""select DATEADD('MONTH', 1, ${H2DatabaseDialect.dateTime(dec_20_2013)}) as day from dual"""
+          sql === """select DATEADD('MONTH', 1, PARSEDATETIME('2013-12-20 10:01:30.000', 'yyyy-MM-d hh:mm:ss.SSS')) as day from dual"""
           val rs = st.executeQuery(sql)
           rs.next
           rs.getString("day").split(' ')(0) === "2014-01-20"
