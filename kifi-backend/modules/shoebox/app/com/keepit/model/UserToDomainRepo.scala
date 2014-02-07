79e2777ef84e1aedea6a4e1ea78c8fc310d401bf
Raymond Ng <raymond.ng@gmail.com>
2014-02-07 11:04:23 -0800

    userToDomain -> slick2 (cont)

--- kifi-backend/modules/shoebox/app/com/keepit/model/UserToDomainRepo.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/model/UserToDomainRepo.scala
@@ -24,25 +24,29 @@ trait UserToDomainRepo extends Repo[UserToDomain] {
 class UserToDomainRepoImpl @Inject()(val db: DataBaseComponent, val clock: Clock) extends DbRepo[UserToDomain] with UserToDomainRepo {
 
   import db.Driver.simple._
+  import FortyTwoTypeMappers._
 
   type RepoImpl = UserToDomainTable
   class UserToDomainTable(tag:Tag) extends RepoTable[UserToDomain](db, tag, "user_to_domain") {
     def userId = column[Id[User]]("user_id", O.NotNull)
     def domainId = column[Id[Domain]]("domain_id", O.NotNull)
-    def kind = column[State[UserToDomainKind]]("kind", O.NotNull)
+    def kind = column[UserToDomainKind]("kind", O.NotNull)
     def value = column[Option[JsValue]]("value", O.Nullable)
     def * = (id.?,userId,domainId,kind,value,state,createdAt,updatedAt) <> ((UserToDomain.apply _).tupled, UserToDomain.unapply _)
   }
 
+  def table(tag:Tag) = new UserToDomainTable(tag)
+  initTable()
+
   override def deleteCache(model: UserToDomain)(implicit session: RSession): Unit = {}
   override def invalidateCache(model: UserToDomain)(implicit session: RSession): Unit = {}
 
-  def get(userId: Id[User], domainId: Id[Domain], kind: State[UserToDomainKind],
+  def get(userId: Id[User], domainId: Id[Domain], kind: UserToDomainKind,
           excludeState: Option[State[UserToDomain]] = Some(UserToDomainStates.INACTIVE))
          (implicit session: RSession): Option[UserToDomain] =
     (for (t <- rows if t.userId === userId && t.domainId === domainId && t.kind === kind && t.state =!= excludeState.orNull) yield t).firstOption
 
-  def exists(userId: Id[User], domainId: Id[Domain], kind: State[UserToDomainKind],
+  def exists(userId: Id[User], domainId: Id[Domain], kind: UserToDomainKind,
              excludeState: Option[State[UserToDomain]] = Some(UserToDomainStates.INACTIVE))
             (implicit session: RSession): Boolean =
     (for (t <- rows if t.userId === userId && t.domainId === domainId && t.kind === kind && t.state =!= excludeState.orNull) yield t.id).firstOption.isDefined
