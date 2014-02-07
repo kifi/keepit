57598b43081af7409551c9c2e32a93a83ffa02b4
Stephen Kemmerling <stephen@42go.com>
2014-02-07 14:10:00 -0800

    Singletons!

--- kifi-backend/modules/common/app/com/keepit/commanders/RemoteUserExperimentCommander.scala
+++ kifi-backend/modules/common/app/com/keepit/commanders/RemoteUserExperimentCommander.scala
@@ -8,13 +8,13 @@ import com.keepit.common.akka.MonitoredAwait
 import com.keepit.common.healthcheck.AirbrakeNotifier
 
 
-import com.google.inject.Inject
+import com.google.inject.{Inject, Singleton}
 
 import scala.concurrent.Future
 
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-
+@Singleton //The Singleton is very importatnt here. There is a cache on the object.
 class RemoteUserExperimentCommander @Inject() (
     shoebox: ShoeboxServiceClient,
     protected val generatorCache: ProbabilisticExperimentGeneratorAllCache,
