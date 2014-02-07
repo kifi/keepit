f3efdf94a0f54aea22fbbbe2ae5cb0bc2cdcb541
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-02-07 10:26:26 -0800

    SocialUserInfoRepo

--- kifi-backend/modules/sqldb/app/com/keepit/common/db/slick/FortyTwoTypeMappers.scala
+++ kifi-backend/modules/sqldb/app/com/keepit/common/db/slick/FortyTwoTypeMappers.scala
@@ -8,7 +8,16 @@ import org.joda.time.{DateTime, LocalDate}
 import scala.slick.jdbc.{PositionedParameters, SetParameter}
 import java.sql.Timestamp
 import play.api.libs.json.{JsValue, JsArray, Json}
-import com.keepit.common.mail.{GenericEmailAddress, EmailAddressHolder}
+import com.keepit.common.mail._
+import com.keepit.common.net.UserAgent
+import com.keepit.model.UrlHash
+import play.api.libs.json.JsArray
+import com.keepit.common.db.slick.InvalidDatabaseEncodingException
+import com.keepit.common.mail.GenericEmailAddress
+import com.keepit.model.DeepLocator
+import com.keepit.social.{SocialId, SocialNetworkType}
+import securesocial.core.SocialUser
+import com.keepit.serializer.SocialUserSerializer
 
 case class InvalidDatabaseEncodingException(msg: String) extends java.lang.Throwable
 
@@ -23,7 +32,19 @@ trait FortyTwoGenericTypeMappers { self: {val db: DataBaseComponent} =>
   implicit val sequenceNumberTypeMapper = MappedColumnType.base[SequenceNumber, Long](_.value, SequenceNumber.apply)
   implicit val abookOriginMapper = MappedColumnType.base[ABookOriginType, String](_.name, ABookOriginType.apply)
   implicit val issuerMapper = MappedColumnType.base[OAuth2TokenIssuer, String](_.name, OAuth2TokenIssuer.apply)
+  implicit val electronicMailCategoryMapper = MappedColumnType.base[ElectronicMailCategory, String](_.category, ElectronicMailCategory.apply)
+  implicit val userAgentMapper = MappedColumnType.base[UserAgent, String](_.userAgent, UserAgent.fromString)
   implicit val emailAddressHolderMapper = MappedColumnType.base[EmailAddressHolder, String](_.address, GenericEmailAddress.apply)
+  implicit val kifiVersionMapper = MappedColumnType.base[KifiVersion, String](_.toString, KifiVersion.apply)
+  implicit val urlHashMapper = MappedColumnType.base[UrlHash, String](_.hash, UrlHash.apply)
+  implicit val deepLocatorMapper = MappedColumnType.base[DeepLocator, String](_.value, DeepLocator.apply)
+  implicit val deepLinkTokenMapper = MappedColumnType.base[DeepLinkToken, String](_.value, DeepLinkToken.apply)
+  implicit val bookmarkSourceMapper = MappedColumnType.base[BookmarkSource, String](_.value, BookmarkSource.apply)
+  implicit val systemEmailAddressMapper = MappedColumnType.base[SystemEmailAddress, String](_.address, EmailAddresses.apply)
+  implicit val socialIdMapper = MappedColumnType.base[SocialId, String](_.id, SocialId.apply)
+  implicit val socialNetworkTypeMapper = MappedColumnType.base[SocialNetworkType, String](SocialNetworkType.unapply(_).get, SocialNetworkType.apply)
+  implicit val socialUserMapper = MappedColumnType.base[SocialUser, String](SocialUserSerializer.userSerializer.writes(_).toString, s => SocialUserSerializer.userSerializer.reads(Json.parse(s)).get)
+
 
   implicit val jsArrayMapper = MappedColumnType.base[JsArray, String]({ json =>
     Json.stringify(json)
