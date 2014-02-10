ecbc21c353d3ebc585ccf6be7fe48928c05c5809
Raymond Ng <ray@42go.com>
2014-02-09 23:55:49 -0800

    normalizationUpdateJobQueue: clean-up

--- kifi-backend/modules/shoebox/test/com/keepit/queue/FakeNormalizationUpdateJobQueueModule.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/queue/FakeNormalizationUpdateJobQueueModule.scala
@@ -5,16 +5,19 @@ import com.keepit.common.queue.SimpleQueueMessage
 
 case class FakeNormalizationUpdateJobQueueModule() extends NormalizationUpdateJobQueueModule {
 
+  val queueName = "barQ"
+
   override def configure(): Unit = {
   }
 
   @Singleton
   @Provides
   def normalizationUpdateTaskQ():NormalizationUpdateJobQueue = new NormalizationUpdateJobQueue {
-    override def queueUrl: String = "http://foo.com/barQ"
-    override def receive(): Seq[SimpleQueueMessage] = Seq.empty[SimpleQueueMessage]
-    override def send(s: String): Unit = {}
-    override def delete(msgHandle: String): Unit = {}
+    def name = queueName
+    def queueUrl = "http://foo.com/barQ"
+    def receive(): Seq[SimpleQueueMessage] = Seq.empty[SimpleQueueMessage]
+    def send(s: String): Unit = {}
+    def delete(msgHandle: String): Unit = {}
   }
 
 }
