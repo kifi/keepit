8d7890985bff1241d1bf7320b4a146d1f8de83e5
Andrew Conner <git@andrewconner.org>
2014-02-12 13:43:14 -0800

    An attempt to fix routing

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/AngularAssets.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/AngularAssets.scala
@@ -1,7 +1,18 @@
 package com.keepit.controllers.website
 
 import controllers.AssetsBuilder
+import play.api.mvc.{Action, Controller}
+import play.api.Play
+import play.api.libs.iteratee.Enumerator
+import play.api.Play.current
+import play.api.libs.concurrent.Execution.Implicits._
 
-object AngularAssets extends AssetsBuilder {
+
+object AngularDistAssets extends AssetsBuilder with Controller {
+  def home = Action {
+    Status(200).chunked(Enumerator.fromStream(Play.resourceAsStream("angular/index.html").get)) as HTML
+  }
+}
+object AngularImgAssets extends AssetsBuilder {
 
 }
