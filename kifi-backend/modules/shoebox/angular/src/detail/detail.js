bd7c8fd1157e7c58917a491275d5e5476261cc3a
Andrew Conner <git@andrewconner.org>
2014-02-21 16:14:42 -0800

    Typeahead

--- kifi-backend/modules/shoebox/angular/src/detail/detail.js
+++ kifi-backend/modules/shoebox/angular/src/detail/detail.js
@@ -6,6 +6,13 @@ angular.module('kifi.detail', ['kifi.keepService', 'kifi.keepWhoPics', 'kifi.kee
 	'keepService', 'tagService', '$filter', '$sce',
 	function (keepService, tagService, $filter, $sce) {
 		var isAddingTag = false;
+		var KEY_UP = 38,
+			KEY_DOWN = 40,
+			KEY_ENTER = 13,
+			KEY_ESC = 27,
+			//KEY_TAB = 9,
+			KEY_DEL = 46,
+			KEY_F2 = 113;
 
 		return {
 			replace: true,
@@ -32,7 +39,14 @@ angular.module('kifi.detail', ['kifi.keepService', 'kifi.keepWhoPics', 'kifi.kee
 
 				scope.tagFilter = {name: ''};
 
-				scope.tagTypeAheadResults = {};
+				scope.tagTypeAheadResults = [];
+
+				function indexOfTag(tag) {
+					if (tag) {
+						return scope.tagTypeAheadResults.indexOf(tag);
+					}
+					return -1;
+				}
 
 				var filterTags = function (tagFilterTerm) {
 					function keepHasTag(tagId) {
@@ -63,6 +77,10 @@ angular.module('kifi.detail', ['kifi.keepService', 'kifi.keepWhoPics', 'kifi.kee
 						scope.tagTypeAheadResults = allTagsExceptPreexisting();
 					}
 
+					if (scope.tagTypeAheadResults.length > 0) {
+						scope.highlightFirst();
+					}
+
 					scope.tagTypeAheadResults.forEach(function (tag) {
 						var safe = tag.name.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
 						// todo: highlight matching terms
@@ -78,8 +96,113 @@ angular.module('kifi.detail', ['kifi.keepService', 'kifi.keepWhoPics', 'kifi.kee
 					return isAddingTag = false;
 				};
 
+				scope.createAndAddTag = function (keep) {
+					tagService.create(scope.tagFilter.name).then(function (tag) {
+						scope.addTag(tag, keep);
+					});
+				};
+
 				scope.isAddingTag = function () {
 					return isAddingTag;
+				};
+
+				scope.isTagHighlighted = function (tag) {
+					return scope.highlightedTag === tag;
+				};
+
+				scope.highlightNext = function () {
+					var index = indexOfTag(scope.highlightedTag);
+					if (index === -1) {
+						// no highlight
+						// highlight the first
+						return scope.highlightFirst();
+					}
+
+					if (index === scope.tagTypeAheadResults.length - 1) {
+						// last item on the list
+
+						if (scope.isAddTagShown) {
+							// highlight the new tag if available
+							return scope.highlightNewSuggestion();
+						}
+
+						// the first, otherwise
+						return scope.highlightFirst();
+					}
+
+					// highlight the next item
+					return scope.highlightAt(index + 1);
+				};
+
+				scope.highlightPrev = function () {
+					var index = indexOfTag(scope.highlightedTag);
+					console.log(scope.highlightedTag, index);
+					if (index === -1) {
+						// no highlight
+						// highlight the first
+						return scope.highlightLast();
+					}
+
+					if (index === 0) {
+						// last item on the list
+
+						if (scope.isAddTagShown) {
+							// highlight the new tag if available
+							return scope.highlightNewSuggestion();
+						}
+
+						// the first, otherwise
+						return scope.highlightLast();
+					}
+
+					// highlight the next item
+					return scope.highlightAt(index - 1);
+				};
+
+				scope.isAddTagShown = function () {
+					return scope.tagFilter.name.length > 0 && scope.tagTypeAheadResults.find(function (tag) {
+						return tag.name === scope.tagFilter.name;
+					}) === undefined;
+				}
+
+				scope.highlightAt = function (index) {
+					console.log("highlighting: ", index, scope.tagTypeAheadResults.length);
+					if (index == null) {
+						return scope.highlightNewSuggestion();
+					}
+					var tags = scope.tagTypeAheadResults,
+						len = tags.length;
+					if (!len) {
+						return scope.highlightNewSuggestion();
+					}
+
+					index = ((index % len) + len) % len;
+					var tag = tags[index];
+					scope.highlightedTag = tag;
+					//dom.scrollIntoViewLazy(element.find('.kf-tag')[index]);
+					return tag;
+				};
+
+				scope.highlightFirst = function () {
+					scope.highlightAt(0);
+				};
+
+				scope.highlightLast = function () {
+					return scope.highlightAt(-1);
+				};
+
+				scope.highlightNewSuggestion = function () {
+					if (scope.isAddTagShown()) {
+						return scope.highlightedTag = null;
+					}
+					return scope.highlightFirst();
+				};
+
+				scope.selectTag = function () {
+					if (scope.highlightedTag) {
+						return scope.addTag(scope.highlightedTag, scope.keep);
+					}
+					return scope.createAndAddTag(scope.keep);
 				};
 
 				scope.hasTags = function () {
@@ -98,6 +221,34 @@ angular.module('kifi.detail', ['kifi.keepService', 'kifi.keepWhoPics', 'kifi.kee
 
 				scope.getTitleText = function () {
 					return keepService.getSelectedLength() + ' Keeps selected';
+				};
+
+				scope.onKeydown = function (e) {
+					switch (e.keyCode) {
+					case KEY_UP:
+						scope.highlightPrev();
+						break;
+					case KEY_DOWN:
+						scope.highlightNext();
+						break;
+					case KEY_ENTER:
+						scope.selectTag();
+						break;
+					case KEY_ESC:
+						// if (scope.highlight) {
+						// 	scope.highlightNewSuggestion();
+						// }
+						// else {
+						// 	scope.clearFilter();
+						// }
+						break;
+					case KEY_DEL:
+						// scope.remove(scope.highlight);
+						break;
+					case KEY_F2:
+						// scope.rename(scope.highlight);
+						break;
+					}
 				};
 
 				scope.howKept = null;
