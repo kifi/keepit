54d02373104e056f2e74daecd657bd2acba2502d
Andrew Conner <git@andrewconner.org>
2014-02-27 15:21:48 -0800

    Modal work

--- kifi-backend/modules/shoebox/angular/src/common/modal/modal.js
+++ kifi-backend/modules/shoebox/angular/src/common/modal/modal.js
@@ -1,14 +1,54 @@
 'use strict';
 
-angular.module('kifi.modal', ['ui.bootstrap'])
+angular.module('kifi.modal', [])
 
-.factory('kfModal', [
-  '$modal',
-  function ($modal) {
+.directive('kfPlainModal', [
+  '$document',
+  function ($document) {
     return {
-      open: function (opts) {
+      restrict: 'A',
+      replace: true,
+      scope: {
+        show: '=',
+      },
+      templateUrl: 'common/modal/plainModal.tpl.html',
+      transclude: true,
+      link: function (scope, element, attrs) {
+        scope.dialogStyle = {};
+        scope.backdropStyle = {};
 
-        $modal.open(opts);
+        if (attrs.kfWidth) {
+          scope.dialogStyle.width = attrs.kfWidth;
+        }
+        if (attrs.kfHeight) {
+          scope.dialogStyle.height = attrs.kfHeight;
+        }
+
+        scope.backdropStyle.opacity = attrs.kfOpacity || .3;
+        scope.backdropStyle.backgroundColor = attrs.kfBackdropColor || 'rgba(0, 40, 90, 1)';
+
+        scope.hideModal = function($event) {
+          scope.show = false;
+        };
+
+        function exitModal(evt) {
+          var modal;
+          if (evt.which === 27) {
+            scope.hideModal(evt);
+            scope.$apply();
+          }
+        }
+
+        scope.$watch(function () {
+          return scope.show;
+        }, function () {
+          if (scope.show) {
+            $document.on('keydown', exitModal);
+          } else {
+            $document.off('keydown', exitModal);
+          }
+        });
+
       }
     };
   }
