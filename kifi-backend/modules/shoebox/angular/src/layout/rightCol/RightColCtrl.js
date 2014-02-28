c77c0c82c8c2f80961f4b433c88df9539aba7cf0
Andrew Conner <git@andrewconner.org>
2014-02-27 17:05:33 -0800

    Onboarding works.

--- kifi-backend/modules/shoebox/angular/src/layout/rightCol/RightColCtrl.js
+++ kifi-backend/modules/shoebox/angular/src/layout/rightCol/RightColCtrl.js
@@ -3,26 +3,23 @@
 angular.module('kifi.layout.rightCol', ['kifi.modal'])
 
 .controller('RightColCtrl', [
-  '$scope', '$window',
-  function ($scope, $window) {
+  '$scope', '$window', 'profileService', '$q',
+  function ($scope, $window, profileService, $q) {
     $scope.data = {};
 
     // onboarding.js are using this functions
     $window.getMe = function() {
-      return promise.me.done(function (me) {
-        me.pic200 = formatPicUrl(me.id, me.pictureName, 200);
-        return me;
-      });
+      return profileService.me ? $q.when(profileService.me) : profileService.fetchMe();
     };
 
     $window.exitOnboarding = function () {
-      $('.kifi-onboarding-iframe').remove();
-      $.postJson(xhrBase + '/user/prefs', {
-        onboarding_seen: 'true'
-      }, function (data) {
-        log('[prefs]', data);
-      });
-      initBookmarkImport();
+      data.showGettingStarted = false;
+      // $.postJson(xhrBase + '/user/prefs', {
+      //   onboarding_seen: 'true'
+      // }, function (data) {
+      //   log('[prefs]', data);
+      // });
+      //initBookmarkImport();
     };
 
   }
