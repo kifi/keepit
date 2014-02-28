51dfb8f32bdd646f5c0d21c9123ed8019cf05728
Andrew Conner <git@andrewconner.org>
2014-02-27 16:31:40 -0800

    Onboarding modal

--- kifi-backend/modules/shoebox/angular/src/layout/rightCol/RightColCtrl.js
+++ kifi-backend/modules/shoebox/angular/src/layout/rightCol/RightColCtrl.js
@@ -7,5 +7,23 @@ angular.module('kifi.layout.rightCol', ['kifi.modal'])
   function ($scope, $window) {
     $scope.data = {};
 
+    // onboarding.js are using this functions
+    $window.getMe = function() {
+      return promise.me.done(function (me) {
+        me.pic200 = formatPicUrl(me.id, me.pictureName, 200);
+        return me;
+      });
+    };
+
+    $window.exitOnboarding = function () {
+      $('.kifi-onboarding-iframe').remove();
+      $.postJson(xhrBase + '/user/prefs', {
+        onboarding_seen: 'true'
+      }, function (data) {
+        log('[prefs]', data);
+      });
+      initBookmarkImport();
+    };
+
   }
 ]);
