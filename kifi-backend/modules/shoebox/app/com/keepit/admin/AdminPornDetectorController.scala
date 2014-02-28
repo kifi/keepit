04c6480ee00022bf4e3f963a9d5e5d979db41e75
yingjieMiao <yingjie@42go.com>
2014-02-28 10:49:01 -0800

    better info on porn detector admin page

--- kifi-backend/modules/shoebox/app/com/keepit/admin/AdminPornDetectorController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/admin/AdminPornDetectorController.scala
@@ -12,6 +12,11 @@ class AdminPornDetectorController @Inject()(
   scraper: ScraperServiceClient,
   actionAuthenticator: ActionAuthenticator
 ) extends AdminController(actionAuthenticator) {
+
+  private def tokenize(query: String): Array[String] = {
+    query.split("[^a-zA-Z0-9]").filter(!_.isEmpty).map{_.toLowerCase}
+  }
+
   def index() = AdminHtmlAction.authenticated{ implicit request =>
     Ok(html.admin.pornDetector())
   }
@@ -19,9 +24,10 @@ class AdminPornDetectorController @Inject()(
   def detect() = AdminHtmlAction.authenticated{ implicit request =>
     val body = request.body.asFormUrlEncoded.get.mapValues(_.head)
     val text = body.get("query").get
+    val numBlocks = tokenize(text).sliding(10, 5).size
     val badTexts = Await.result(scraper.detectPorn(text), 15 seconds)
     val badInfo = badTexts.map{ x => x._1 + " ---> " + x._2}.mkString("\n")
-    val msg = if (badTexts.size == 0) "input text is clean" else "The following blocks look suspicious:\n" + badInfo
+    val msg = if (badTexts.size == 0) "input text is clean" else s"${badTexts.size} out of ${numBlocks} blocks look suspicious:\n" + badInfo
     Ok(msg.replaceAll("\n","\n<br>"))
   }
 }
