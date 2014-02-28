3861cdabbee3d51b6874707bccc97c244da89de8
Martin Raison <martin@42go.com>
2014-02-27 16:54:08 -0800

    tooltip code cleaning

--- kifi-backend/modules/shoebox/angular/src/common/directives/keepWho/keepWhoPics.js
+++ kifi-backend/modules/shoebox/angular/src/common/directives/keepWho/keepWhoPics.js
@@ -25,21 +25,22 @@ angular.module('kifi.keepWhoPics', ['kifi.keepWhoService'])
             $rootElement.append(tooltip);
             $compile(tooltip)(scope);
           }
-          $timeout(function () {
-            // Set position
-            var triangleOffset = 42;
-            var triangleWidth = 1;
-            var triangle = tooltip.find('.kifi-fr-kcard-tri');
-            var left = element.offset().left + element.width() / 2 - triangleOffset;
-            var top = element.offset().top - 91;
-            var triangleLeft = triangleOffset - triangleWidth;
-            if ($window.innerWidth - left - tooltip.width() < 3) {
-              left += 2 * triangleOffset - tooltip.width();
-              triangleLeft = tooltip.width() - triangleOffset - triangleWidth;
-            }
-            tooltip.css({left: left + 'px', top: top + 'px', width: tooltip.width() + 'px'});
-            triangle.css({left: triangleLeft});
 
+          // Set position
+          var triangleOffset = 42;
+          var triangleWidth = 1;
+          var triangle = tooltip.find('.kifi-fr-kcard-tri');
+          var left = element.offset().left + element.width() / 2 - triangleOffset;
+          var top = element.offset().top - 91;
+          var triangleLeft = triangleOffset - triangleWidth;
+          if ($window.innerWidth - left - tooltip.width() < 3) {
+            left += 2 * triangleOffset - tooltip.width();
+            triangleLeft = tooltip.width() - triangleOffset - triangleWidth;
+          }
+          tooltip.css({left: left + 'px', top: top + 'px', width: tooltip.width() + 'px'});
+          triangle.css({left: triangleLeft});
+
+          $timeout(function () {
             scope.tooltipEnabled = true;
           });
         };
