22fe6a7b5b5657e763227c33d658da88a56c85f4
Andrew Conner <git@andrewconner.org>
2014-03-03 15:59:32 -0800

    File rename

--- kifi-backend/modules/shoebox/angular/src/common/modal/modal.js
+++ kifi-backend/modules/shoebox/angular/src/common/modal/modal.js
@@ -2,7 +2,59 @@
 
 angular.module('kifi.modal', [])
 
-.directive('kfPlainModal', [
+.directive('kfModal', [
+  '$document',
+  function ($document) {
+    return {
+      restrict: 'A',
+      replace: true,
+      scope: {
+        show: '='
+      },
+      templateUrl: 'common/modal/modal.tpl.html',
+      transclude: true,
+      link: function (scope, element, attrs) {
+        scope.dialogStyle = {};
+        scope.backdropStyle = {};
+
+        if (attrs.kfWidth) {
+          scope.dialogStyle.width = attrs.kfWidth;
+        }
+        if (attrs.kfHeight) {
+          scope.dialogStyle.height = attrs.kfHeight;
+        }
+
+        scope.backdropStyle.opacity = attrs.kfOpacity || .3;
+        scope.backdropStyle.backgroundColor = attrs.kfBackdropColor || 'rgba(0, 40, 90, 1)';
+
+        scope.hideModal = function($event) {
+          scope.show = false;
+        };
+
+        function exitModal(evt) {
+          var modal;
+          if (evt.which === 27) {
+            scope.hideModal(evt);
+            scope.$apply();
+          }
+        }
+
+        scope.$watch(function () {
+          return scope.show;
+        }, function () {
+          if (scope.show) {
+            $document.on('keydown', exitModal);
+          } else {
+            $document.off('keydown', exitModal);
+          }
+        });
+
+      }
+    };
+  }
+])
+
+.directive('kfBasicModal', [
   '$document',
   function ($document) {
     return {
