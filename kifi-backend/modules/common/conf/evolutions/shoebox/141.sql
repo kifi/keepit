589f8919c42f26d4fc1bd306e88d901445ec98eb
Stephen Kemmerling <stephen@42go.com>
2014-03-03 13:07:26 -0800

    merge conflict

--- kifi-backend/modules/common/conf/evolutions/shoebox/141.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/141.sql
@@ -12,6 +12,6 @@ CREATE INDEX social_user_info_i_seq ON social_user_info(seq);
 ALTER TABLE invitation add seq Int NOT NULL DEFAULT 0;
 CREATE INDEX invitation_i_seq ON invitation(seq);
 
-INSERT INTO evolutions (name, description) VALUES ('140.sql', 'add sequence number to social_connection, social_user_info and invitation');
+INSERT INTO evolutions (name, description) VALUES ('141.sql', 'add sequence number to social_connection, social_user_info and invitation');
 
 # --- !Downs
