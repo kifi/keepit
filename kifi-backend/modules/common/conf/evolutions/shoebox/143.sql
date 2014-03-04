0c59318bffc6338f59a10f2260c223062d61e0e4
Léo Grimaldi <leo@42go.com>
2014-03-04 13:49:00 -0800

    Fix evolution

--- kifi-backend/modules/common/conf/evolutions/shoebox/143.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/143.sql
@@ -2,7 +2,7 @@
 
 # --- !Ups
 
-
+CREATE SEQUENCE email_address_sequence;
 ALTER TABLE email_address add seq Int NOT NULL DEFAULT 0;
 CREATE INDEX email_address_i_seq ON email_address(seq);
 
@@ -10,10 +10,6 @@ CREATE INDEX email_address_i_seq ON email_address(seq);
 -- CREATE TABLE email_address_sequence (id INT NOT NULL);
 -- INSERT INTO email_address_sequence VALUES (0);
 -- H2:
-
-CREATE SEQUENCE email_address_sequence;
-UPDATE email_address SET seq=id;
-UPDATE email_address_sequence SET id = (SELECT MAX(id) FROM email_address);
 
 INSERT INTO evolutions (name, description) VALUES ('143.sql', 'add sequence number to email_address');
 
