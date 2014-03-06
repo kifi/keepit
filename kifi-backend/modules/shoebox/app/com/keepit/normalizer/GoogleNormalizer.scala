8ec55c85819372d7ac95bef66be31613a46ea374
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-03-06 11:36:44 -0800

    google search url normalizaiton: preserve ie and safe

--- kifi-backend/modules/shoebox/app/com/keepit/normalizer/GoogleNormalizer.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/normalizer/GoogleNormalizer.scala
@@ -1,12 +1,12 @@
 package com.keepit.normalizer
 
-import com.keepit.common.net.{Query, Param, Host, URI}
+import com.keepit.common.net.{Query, Param, Host, URI, URIParserUtil}
 
 object GoogleNormalizer extends StaticNormalizer {
 
   def isDefinedAt(uri: URI) = {
     uri.host match {
-      case Some(Host("com", "google", name)) =>
+      case Some(Host("com", "google", name)) => //todo: international domains
         name match {
           case "mail" => true
           case "groups" => true
@@ -24,7 +24,8 @@ object GoogleNormalizer extends StaticNormalizer {
   val gmail = """(/mail/)(.*)""".r
   val driveTabs = Set[String]("my-drive", "shared-with-me", "starred", "recent", "activity", "offline", "all", "trash")
 
-  val searchDropParams = Set[String]("aqs", "sourceid", "espv", "ie", "oq", "es_sm")
+  val searchParamsToDrop = Set[String]("aqs", "sourceid", "espv", "oq", "es_sm")
+  val searchParamsToTake = Set[String]("ie", "safe")
 
   def apply(uri: URI) = {
     uri match {
@@ -52,8 +53,17 @@ object GoogleNormalizer extends StaticNormalizer {
         }
       case URI(scheme, userInfo, host @ Some(Host("com", "google", "www")), port, path @ Some("/search"), query, fragment) =>
         val q = fragment match {
-          case Some(f) => Some(Query.parse(f))
-          case _ => query.map{ qry => Query(qry.params.filterNot{ case Param(name, _) => searchDropParams.contains(name) }) }
+          case Some(f) =>
+            val newParams = Query.parse(f).params
+            val finalParams = query match {
+              case Some(qry) =>
+                URIParserUtil.normalizeParams(qry.params.filter{ case Param(name, _) => searchParamsToTake.contains(name) } ++ newParams)
+              case None =>
+                URIParserUtil.normalizeParams(newParams)
+            }
+            Some(Query(finalParams))
+          case _ =>
+            query.map{ qry => Query(qry.params.filterNot{ case Param(name, _) => searchParamsToDrop.contains(name) }) }
         }
         URI(scheme, userInfo, host, port, path, q, None)
       case _ => uri
