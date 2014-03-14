38aadc2c337e6bd64116c03d861acd0c85442c21
Léo Grimaldi <leo@42go.com>
2014-03-13 18:31:52 -0700

    Escaping “others” characters and fixing URITest

--- kifi-backend/modules/common/test/com/keepit/common/net/URITest.scala
+++ kifi-backend/modules/common/test/com/keepit/common/net/URITest.scala
@@ -9,19 +9,19 @@ class URITest extends Specification {
       URI.parse("https://sub.domain.com").get.host.get.name === "sub.domain.com"
       URI.parse("http://bing.com/?q=1").get.query.get.params === Seq(Param("q", Some("1")))
       URI.parse("http://cr.com/SchedPdf.aspx?locIds={99D3-41B6-9852}&dir=asc").get.query.get.params ===
-        Seq(Param("dir", Some("asc")), Param("locIds", Some("{99D3-41B6-9852}")))
+        Seq(Param("dir", Some("asc")), Param("locIds", Some("%7B99D3-41B6-9852%7D")))
       URI.parse("http://da.seek.com/d/PERK_[PerkinElmer_Optoelec]_SU405-2.html").get.path ===
         Some("/d/PERK_%5BPerkinElmer_Optoelec%5D_SU405-2.html")
       URI.parse("http://scala.org/api/index.html#scala.reflect.api.Universe@Type>:Null<:Types.this.TypeApi").get.fragment ===
-        Some("scala.reflect.api.Universe@Type>:Null<:Types.this.TypeApi")
+        Some("scala.reflect.api.Universe@Type%3E:Null%3C:Types.this.TypeApi")
       URI.parse("http://www.walmart.com/browse/tvs/3_1/?facet=tv_screen_size_range%3A60``+%26+Larger").get.query.get.params ===
-        Seq(Param("facet", Some("tv_screen_size_range:60``+%26+Larger")))
+        Seq(Param("facet", Some("tv_screen_size_range:60%60%60+&+Larger")))
       URI.parse("""https://mint.com/inv?accountId=42#location%3A%7B"accountId"%3A"0",+"tab"%3A0%7D""").get.fragment ===
-        Some("location:{\"accountId\":\"0\",+\"tab\":0}")
+        Some("location:%7B%22accountId%22:%220%22,+%22tab%22:0%7D")
       URI.parse("http://4945457844119005844_911a106f1584b002d8018a27243b8aa2829655c4.blogspot.com").get.host.get.domain ===
         Seq("com", "blogspot", "4945457844119005844_911a106f1584b002d8018a27243b8aa2829655c4")
       URI.parse("http://foo+bar").get.host.get.domain === Seq("foo+bar")
-      URI.parse("http://www.liveleak.com/view?comments=1\\").get.query.get.params === Seq(Param("comments", Some("1\\")))
+      URI.parse("http://www.liveleak.com/view?comments=1\\").get.query.get.params === Seq(Param("comments", Some("1%5C")))
     }
     "parse URLs via unapply" in {
       "http://premium.nba.com/pr/leaguepass/app/2012/console.html?debug=false&type=lp&TinedSid=Gaa419b-25665208-1262918951531-1&nsfg=1355463185|billing.lpbchoice_LAL_LAC_NYK_MIA_OKC^billing.lpbchoice^giBJ5TL8HJT8eLc6&retryCount=3" match {
@@ -34,7 +34,7 @@ class URITest extends Specification {
           query.get.params === Seq(  // alphabetized by parameter name
             Param("TinedSid", Some("Gaa419b-25665208-1262918951531-1")),
             Param("debug", Some("false")),
-            Param("nsfg", Some("1355463185|billing.lpbchoice_LAL_LAC_NYK_MIA_OKC^billing.lpbchoice^giBJ5TL8HJT8eLc6")),
+            Param("nsfg", Some("1355463185%7Cbilling.lpbchoice_LAL_LAC_NYK_MIA_OKC%5Ebilling.lpbchoice%5EgiBJ5TL8HJT8eLc6")),
             Param("retryCount", Some("3")),
             Param("type", Some("lp")))
           fragment === None
@@ -47,7 +47,7 @@ class URITest extends Specification {
           host.get.domain === Seq("com", "yahoo", "finance")
           port === -1
           path === Some("/q")
-          query.get.params === Seq(Param("s", Some("^dji")))
+          query.get.params === Seq(Param("s", Some("%5Edji")))
           fragment === None
         case _ => failure
       }
