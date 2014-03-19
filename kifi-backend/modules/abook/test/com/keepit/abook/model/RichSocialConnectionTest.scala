9735a9db800d33f8f8f907410b40224cfe5cac80
Léo Grimaldi <leo@42go.com>
2014-03-18 23:31:10 -0700

    Fix test

--- kifi-backend/modules/abook/test/com/keepit/abook/model/RichSocialConnectionTest.scala
+++ kifi-backend/modules/abook/test/com/keepit/abook/model/RichSocialConnectionTest.scala
@@ -57,7 +57,7 @@ class RichSocialConnectionTest extends Specification with ABookTestInjector  {
       léoToMarvin.connectionType === SocialNetworks.FACEBOOK
       léoToMarvin.kifiFriendsCount === 1
       léoToMarvin.commonKifiFriendsCount === 0
-      léoToMarvin.invitation === None
+      léoToMarvin.invitationSent === 0
       léoToMarvin.invitationCount === 0
 
       db.readWrite { implicit session =>
@@ -126,7 +126,7 @@ class RichSocialConnectionTest extends Specification with ABookTestInjector  {
       léoToGrassfed42.connectionType === SocialNetworks.EMAIL
       léoToGrassfed42.kifiFriendsCount === 1
       léoToGrassfed42.commonKifiFriendsCount === 0
-      léoToGrassfed42.invitation === None
+      léoToGrassfed42.invitationSent === 0
       léoToGrassfed42.invitationCount === 0
 
       db.readWrite { implicit session =>
@@ -136,42 +136,39 @@ class RichSocialConnectionTest extends Specification with ABookTestInjector  {
     }
 
     "keep track of invitations" in {
-      val léoToMarvinInvitation = Id[Invitation](12)
-      db.readWrite { implicit session => richConnectionRepo.recordInvitation(kifiLéo, léoToMarvinInvitation, Left(facebookMarvin.id.get)) }
+      db.readWrite { implicit session => richConnectionRepo.recordInvitation(kifiLéo, Left(facebookMarvin.id.get)) }
 
       db.readOnly { implicit session =>
         val stephenToMarvin = richConnectionRepo.getByUserAndSocialFriend(kifiStephen, Left(facebookMarvin.id.get)).get
-        stephenToMarvin.invitation === None
+        stephenToMarvin.invitationSent === 0
         stephenToMarvin.invitationCount === 1
 
         val léoToMarvin = richConnectionRepo.getByUserAndSocialFriend(kifiLéo, Left(facebookMarvin.id.get)).get
-        léoToMarvin.invitation === Some(léoToMarvinInvitation)
+        léoToMarvin.invitationSent === 1
         léoToMarvin.invitationCount === 1
       }
 
-      val stephenToMarvinInvitation = Id[Invitation](13)
-      db.readWrite { implicit session => richConnectionRepo.recordInvitation(kifiStephen, stephenToMarvinInvitation, Left(facebookMarvin.id.get)) }
+      db.readWrite { implicit session => richConnectionRepo.recordInvitation(kifiStephen, Left(facebookMarvin.id.get)) }
 
       db.readOnly { implicit session =>
         val stephenToMarvin = richConnectionRepo.getByUserAndSocialFriend(kifiStephen, Left(facebookMarvin.id.get)).get
-        stephenToMarvin.invitation === Some(stephenToMarvinInvitation)
+        stephenToMarvin.invitationSent === 1
         stephenToMarvin.invitationCount === 2
 
         val léoToMarvin = richConnectionRepo.getByUserAndSocialFriend(kifiLéo, Left(facebookMarvin.id.get)).get
-        léoToMarvin.invitation === Some(léoToMarvinInvitation)
+        léoToMarvin.invitationSent === 1
         léoToMarvin.invitationCount === 2
       }
 
-      val léoToGrassfed42Invitation = Id[Invitation](14)
-      db.readWrite { implicit session => richConnectionRepo.recordInvitation(kifiLéo, léoToGrassfed42Invitation, Right(contact42.email)) }
+      db.readWrite { implicit session => richConnectionRepo.recordInvitation(kifiLéo, Right(contact42.email)) }
 
       db.readOnly { implicit session =>
         val léoToGrassfed42 = richConnectionRepo.getByUserAndSocialFriend(kifiLéo, Right(contact42.email)).get
-        léoToGrassfed42.invitation === Some(léoToGrassfed42Invitation)
+        léoToGrassfed42.invitationSent === 1
         léoToGrassfed42.invitationCount === 1
 
         val léoToMarvin = richConnectionRepo.getByUserAndSocialFriend(kifiLéo, Left(facebookMarvin.id.get)).get
-        léoToMarvin.invitation === Some(léoToMarvinInvitation)
+        léoToMarvin.invitationSent === 1
         léoToMarvin.invitationCount === 2
       }
     }
--- kifi-backend/modules/common/conf/evolutions/shoebox/150.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/150.sql
@@ -2,8 +2,8 @@
 
 # --- !Ups
 
-ALTER TABLE rich_social_connection add invitation_sent Int NOT NULL DEFAULT 0;
-ALTER TABLE rich_social_connection drop invitation;
+ALTER TABLE rich_social_connection drop column if exists invitation;
+ALTER TABLE rich_social_connection add column if not exists invitation_sent Int NOT NULL DEFAULT 0;
 
 insert into evolutions (name, description) values('150.sql', 'add column invitation_sent to rich_social_connection and drop column invitation');
 
