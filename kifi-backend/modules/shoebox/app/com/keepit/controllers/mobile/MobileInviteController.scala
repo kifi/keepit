a9cf7a9ea6a14da0168d13ba01d57f3233337a9b
Léo Grimaldi <leo@42go.com>
2014-03-20 16:20:23 -0700

    Refactor invite controllers

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobileInviteController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobileInviteController.scala
@@ -2,64 +2,31 @@ package com.keepit.controllers.mobile
 
 import com.google.inject.Inject
 import com.keepit.common.controller.{ShoeboxServiceController, MobileController, ActionAuthenticator}
-import com.keepit.commanders.{InviteInfo, InviteCommander}
+import com.keepit.commanders.{InviteStatus, FullSocialId, InviteCommander}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
-import com.keepit.abook.ABookServiceClient
-import scala.util.{Failure, Success}
-import play.api.Play._
-import scala.util.Success
-import scala.util.Failure
-import com.keepit.common.db.Id
-import com.keepit.model.{InvitationStates, Invitation, SocialUserInfo, User}
-import play.api.mvc.Result
-import com.keepit.common.db.slick.DBSession.RWSession
-import com.keepit.social.{SocialNetworkType, SocialId, SocialNetworks}
-import com.keepit.inject.FortyTwoConfig
-
+import com.keepit.model.Invitation
+import com.keepit.social.SocialNetworks
+import scala.concurrent.Future
 
 class MobileInviteController @Inject()(
   actionAuthenticator:ActionAuthenticator,
-  inviteCommander:InviteCommander,
-  abookServiceClient:ABookServiceClient,
-  fortytwoConfig: FortyTwoConfig
+  inviteCommander:InviteCommander
 ) extends MobileController(actionAuthenticator) with ShoeboxServiceController {
 
   def inviteConnection = JsonAction.authenticatedParseJsonAsync { implicit request =>
-    val inviteInfoOpt = Json.fromJson[InviteInfo](request.body).asOpt
-    log.info(s"[inviteConnection(${request.userId})] invite=$inviteInfoOpt")
-    inviteInfoOpt map { inviteInfo =>
-      val userId = request.userId
-      val user = request.user
-      if (inviteInfo.fullSocialId.network == "email") {
-        abookServiceClient.getOrCreateEContact(userId, inviteInfo.fullSocialId.id) map { econtactTr =>
-          econtactTr match {
-            case Success(c) =>
-              inviteCommander.sendInvitationForContact(userId, c, user, inviteInfo)
-              log.info(s"[inviteConnection-email(${inviteInfo.fullSocialId.id}, $userId)] invite sent successfully")
-              Ok(Json.obj("code" -> "invitation_sent"))
-            case Failure(e) =>
-              log.warn(s"[inviteConnection-email(${inviteInfo.fullSocialId.id}, $userId)] cannot locate or create econtact entry; Error: $e; Cause: ${e.getCause}")
-              BadRequest(Json.obj("code" -> "invalid_arguments"))
-          }
+    (request.body \ "fullSocialId").asOpt[String].flatMap(FullSocialId.fromString) match {
+      case None => Future.successful(BadRequest(Json.obj("code" -> "invalid_arguments")))
+      case Some(fullSocialId) => {
+        val subject = (request.body \ "subject").asOpt[String]
+        val message = (request.body \ "message").asOpt[String]
+        inviteCommander.invite(request.userId, fullSocialId, subject, message).map {
+          case inviteStatus if inviteStatus.sent => Ok(Json.obj("code" -> "invitation_sent"))
+          case InviteStatus(false, Some(facebookInvite), code @ "client_handle") if fullSocialId.network == SocialNetworks.FACEBOOK =>
+            Ok(Json.obj("code" -> code, "invite" -> Json.toJson[Invitation](facebookInvite)))
+          case _ => Status(INTERNAL_SERVER_ERROR)(Json.obj("code" -> "internal_error"))
         }
-      } else {
-        val inviteStatus = inviteCommander.processSocialInvite(userId, inviteInfo)
-        log.info(s"[inviteConnection(${request.userId})] inviteStatus=$inviteStatus")
-        if (inviteStatus.sent) resolve(Ok(Json.obj("code" -> "invitation_sent")))
-        else if (inviteInfo.fullSocialId.network.equalsIgnoreCase("facebook") && inviteStatus.code == "client_handle") { // special handling
-          inviteStatus.savedInvite match {
-            case Some(saved) =>
-              resolve(Ok(Json.obj("code" -> "client_handle", "invite" -> Json.toJson[Invitation](saved))))
-            case None => { // shouldn't happen
-              log.error(s"[processInvite($userId,$user,$inviteInfo)] Could not send Facebook invite")
-              resolve(Status(INTERNAL_SERVER_ERROR)(Json.obj("code" -> "internal_error")))
-            }
-          }
-        } else resolve(BadRequest(Json.obj("code" -> "invite_not_sent")))
       }
-    } getOrElse resolve(BadRequest(Json.obj("code" -> "invalid_arguments")))
+    }
   }
-
 }
--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/InviteController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/InviteController.scala
@@ -4,37 +4,27 @@ import scala.concurrent.{Future, Promise}
 import scala.concurrent.duration._
 
 import com.google.inject.Inject
-import com.keepit.common.controller.{ShoeboxServiceController, AuthenticatedRequest, ActionAuthenticator, WebsiteController}
-import com.keepit.common.db.slick.DBSession.{RWSession, RSession}
+import com.keepit.common.controller.{ShoeboxServiceController, ActionAuthenticator, WebsiteController}
 import com.keepit.common.db.slick._
-import com.keepit.common.db.{Id, ExternalId, State}
-import com.keepit.common.net.HttpClient
-import com.keepit.common.social._
-import com.keepit.inject.FortyTwoConfig
+import com.keepit.common.db.{ExternalId, State}
 import com.keepit.model._
 import com.keepit.social._
-import com.keepit.common.akka.{TimeoutFuture, SafeFuture}
+import com.keepit.common.akka.TimeoutFuture
 import com.keepit.heimdal._
 import com.keepit.common.controller.ActionAuthenticator.MaybeAuthenticatedRequest
-
-import play.api._
-import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 import play.api.templates.Html
-import com.keepit.common.mail._
 import com.keepit.abook.ABookServiceClient
 import play.api.mvc.Cookie
 import com.keepit.model.Invitation
-import scala.util.{Failure, Try, Success}
-import com.keepit.commanders.{FullSocialId, InviteInfo, InviteCommander}
+import com.keepit.commanders.{InviteStatus, FullSocialId, InviteCommander}
 import com.keepit.inject.FortyTwoConfig
 
 case class BasicUserInvitation(name: String, picture: Option[String], state: State[Invitation])
 
 class InviteController @Inject() (db: Database,
   userRepo: UserRepo,
-  userValueRepo: UserValueRepo,
   socialUserRepo: SocialUserInfoRepo,
   emailRepo: EmailAddressRepo,
   userConnectionRepo: UserConnectionRepo,
@@ -42,11 +32,7 @@ class InviteController @Inject() (db: Database,
   socialUserInfoRepo: SocialUserInfoRepo,
   socialGraphPlugin: SocialGraphPlugin,
   actionAuthenticator: ActionAuthenticator,
-  httpClient: HttpClient,
-  eventContextBuilder: HeimdalContextBuilderFactory,
-  heimdal: HeimdalServiceClient,
   abookServiceClient: ABookServiceClient,
-  postOffice: LocalPostOffice,
   inviteCommander: InviteCommander,
   fortytwoConfig: FortyTwoConfig
 ) extends WebsiteController(actionAuthenticator) with ShoeboxServiceController {
@@ -57,46 +43,27 @@ class InviteController @Inject() (db: Database,
 
   private def CloseWindow() = Ok(Html("<script>window.close()</script>"))
 
-  def inviteConnection = HtmlAction.authenticated { implicit request =>
-    val (fullSocialId, subject, message) = request.request.body.asFormUrlEncoded match {
-      case Some(form) =>
-        (form.get("fullSocialId").map(_.head).getOrElse(""),
-          form.get("subject").map(_.head),
-          form.get("message").map(_.head))
-      case None => ("", None, None)
+  def inviteConnection = HtmlAction.authenticatedAsync { implicit request =>
+    val resultOption = for {
+      form <- request.body.asFormUrlEncoded
+      fullSocialIdString <- form.get("fullSocialId").map(_.head)
+      fullSocialId <- FullSocialId.fromString(fullSocialIdString)
+    } yield {
+      val subject = form.get("subject").map(_.head)
+      val message = form.get("message").map(_.head)
+      inviteCommander.invite(request.userId, fullSocialId, subject, message).map {
+        case inviteStatus if inviteStatus.sent => CloseWindow()
+        case InviteStatus(false, Some(facebookInvite), "client_handle") if fullSocialId.network == SocialNetworks.FACEBOOK =>
+          Redirect(inviteCommander.fbInviteUrl(facebookInvite, fullSocialId.identifier.left.get))
+        case _ => InternalServerError("0")
+      }
     }
-
-    if (fullSocialId.split("/").length != 2) CloseWindow() else {
-      val inviteInfo = InviteInfo(FullSocialId(fullSocialId), subject, message)
-      processInvite(request.userId, request.user, inviteInfo)
-    }
+    resultOption getOrElse Future.successful(CloseWindow())
   }
 
-  def processInvite(userId:Id[User], user:User, inviteInfo:InviteInfo): SimpleResult = {
-    if (inviteInfo.fullSocialId.network == "email") {
-      abookServiceClient.getOrCreateEContact(userId, inviteInfo.fullSocialId.id) map { econtactTr =>
-        econtactTr match {
-          case Success(c) =>
-            inviteCommander.sendInvitationForContact(userId, c, user, inviteInfo)
-            log.info(s"[inviteConnection-email(${inviteInfo.fullSocialId.id}, $userId)] invite sent successfully")
-          case Failure(e) =>
-            log.warn(s"[inviteConnection-email(${inviteInfo.fullSocialId.id}, $userId)] cannot locate or create econtact entry; Error: $e; Cause: ${e.getCause}")
-        }
-      }
-      CloseWindow()
-    } else {
-      val inviteStatus = inviteCommander.processSocialInvite(userId, inviteInfo)
-      if (!inviteStatus.sent && inviteInfo.fullSocialId.network.equalsIgnoreCase("facebook") && inviteStatus.code == "client_handle") {
-        inviteStatus.savedInvite match {
-          case Some(invitation) =>
-            Redirect(inviteCommander.fbInviteUrl(invitation, inviteInfo.fullSocialId.socialId))
-          case None => { // shouldn't happen
-            log.error(s"[processInvite($userId,$user,$inviteInfo)] Could not send Facebook invite")
-            InternalServerError("0")
-          }
-        }
-      } else CloseWindow()
-    }
+  def confirmInvite(id: ExternalId[Invitation], errorMsg: Option[String], errorCode: Option[Int]) = Action {
+    if (inviteCommander.confirmFacebookInvite(id: ExternalId[Invitation], errorMsg: Option[String], errorCode: Option[Int]).sent) { CloseWindow() }
+    else { Redirect(routes.HomeController.home) }
   }
 
   def refreshAllSocialInfo() = HtmlAction.authenticatedAsync { implicit request =>
@@ -152,21 +119,4 @@ class InviteController @Inject() (db: Database,
           resolve(Redirect(com.keepit.controllers.website.routes.HomeController.home))
       }
   })
-
-  def confirmInvite(id: ExternalId[Invitation], errorMsg: Option[String], errorCode: Option[Int]) = Action {
-    db.readWrite { implicit session =>
-      val invitation = invitationRepo.getOpt(id)
-      invitation match {
-        case Some(invite) =>
-          if (errorCode.isEmpty) {
-            invitationRepo.save(invite.copy(state = InvitationStates.ACTIVE))
-            inviteCommander.reportSentInvitation(invite, SocialNetworks.FACEBOOK)
-          }
-          CloseWindow()
-        case None =>
-          Redirect(routes.HomeController.home)
-      }
-    }
-  }
-
 }
