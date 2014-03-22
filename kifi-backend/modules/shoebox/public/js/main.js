1bfa1a1c1f36e13b9bbf52fea8a794d164d416f2
eishay <eishay@gmail.com>
2014-03-21 20:19:39 -0700

    update the right file

--- kifi-backend/modules/shoebox/public/js/main.js
+++ kifi-backend/modules/shoebox/public/js/main.js
@@ -3760,9 +3760,11 @@ $(function () {
 			collectionId: collId,
 			keeps: $keeps.map(function () {
 				var a = this.querySelector('.keep-title>a');
+				var isPrivate = this.querySelector('.keep-private.on') != null;
 				return {
 					title: a.title,
-					url: a.href
+					url: a.href,
+          isPrivate: isPrivate
 				};
 			}).get()
 		})
