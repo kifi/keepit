00c0d53b9e30527fe14d6c1b92154381e66adea8
Andrew Conner <git@andrewconner.org>
2014-03-25 11:33:51 -0700

    Invite stuffs

--- kifi-backend/modules/shoebox/angular/src/friends/friends.js
+++ kifi-backend/modules/shoebox/angular/src/friends/friends.js
@@ -3,7 +3,8 @@
 angular.module('kifi.friends', [
   'util',
   'kifi.profileService',
-  'kifi.routeService'
+  'kifi.routeService',
+  'kifi.inviteService' // for kfSocialInviteSearch
 ])
 
 .config([
@@ -23,4 +24,45 @@ angular.module('kifi.friends', [
 
     // plenty to do!
   }
+])
+
+.directive('kfSocialInviteSearch', [ // move to /invite/
+  'inviteService',
+  function (inviteService) {
+    return {
+      scope: {
+        'friend': '&'
+      },
+      replace: true,
+      restrict: 'A',
+      templateUrl: 'friends/inviteSearch.tpl.html',
+      link: function (scope/*, element, attrs*/) {
+        scope.search = {};
+        scope.search.showDropdown = false;
+
+        scope.results = inviteService.inviteList;
+        scope.selected = inviteService.socialSelected;
+
+        scope.change = function (e) {
+          inviteService.socialSearch(scope.search.name).then(function (res) {
+            if (!res || res.length === 0) {
+              scope.search.showDropdown = false;
+            } else {
+              scope.search.showDropdown = true;
+            }
+          });
+        };
+
+        // scope.focus = function (e) {
+        //   console.log('focus', e);
+        // };
+
+        scope.blur = function (e) {
+          console.log('blur', e);
+        };
+
+      }
+    };
+  }
 ]);
+
