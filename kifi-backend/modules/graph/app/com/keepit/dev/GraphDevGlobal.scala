51be1897ff33d6153e931b0e0ebf7ed27181842c
Léo Grimaldi <leo@42go.com>
2014-03-25 14:15:00 -0700

    Graph project file structure

---
+++ kifi-backend/modules/graph/app/com/keepit/dev/GraphDevGlobal.scala
@@ -0,0 +1,15 @@
+package com.keepit.dev
+
+import com.keepit.FortyTwoGlobal
+import play.api.Mode._
+import play.api.Application
+import com.keepit.graph.GraphServices
+
+object GraphDevGlobal extends FortyTwoGlobal(Dev) with GraphServices {
+  override val module = GraphDevModule()
+
+  override def onStart(app: Application) {
+    startGraphServices()
+    super.onStart(app)
+  }
+}
---
+++ kifi-backend/modules/graph/app/com/keepit/graph/GraphModule.scala
@@ -0,0 +1,8 @@
+package com.keepit.graph
+
+import com.keepit.inject.{CommonServiceModule, ConfigurationModule}
+
+
+abstract class GraphModule() extends ConfigurationModule with CommonServiceModule {
+
+}
