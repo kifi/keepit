baace3636f1fd346dbd54ebda30c03d5b39ba9b5
Léo Grimaldi <leo@42go.com>
2014-03-27 11:28:35 -0700

    TypeHeader contra variance no longer needed, less boilerplate

--- kifi-backend/modules/common/app/com/keepit/common/db/ModelWithGlobalId.scala
+++ kifi-backend/modules/common/app/com/keepit/common/db/ModelWithGlobalId.scala
@@ -2,7 +2,7 @@ package com.keepit.common.db
 
 import com.keepit.model.{User, Reflect}
 
-case class TypeHeader[-G <: ModelWithGlobalId](code: Short) {
+case class TypeHeader[M <: ModelWithGlobalId[M]](code: Short) {
   require(code > 0, "Type header must be positive")
   def shifted: Long = code.toLong << 48
 }
@@ -10,7 +10,7 @@ case class TypeHeader[-G <: ModelWithGlobalId](code: Short) {
 case class GlobalId(id: Long) extends AnyVal {
   def code: Short = (id >> 48).toShort
   def value: Long = id & GlobalId.maxId
-  def toId[M <: ModelWithGlobalId: TypeHeader]: Id[M] = {
+  def toId[M <: ModelWithGlobalId[M]: TypeHeader]: Id[M] = {
     val header = implicitly[TypeHeader[M]]
     require((code == header.code), "Invalid GlobalId")
     Id[M](value)
@@ -20,20 +20,21 @@ case class GlobalId(id: Long) extends AnyVal {
 
 object GlobalId {
   val maxId: Long = (1.toLong << 48) - 1
-  def apply[G <: ModelWithGlobalId: TypeHeader, M <: G](id: Id[M]): GlobalId = {
-    val header = implicitly[TypeHeader[G]]
+  def apply[M <: ModelWithGlobalId[M]: TypeHeader](id: Id[M]): GlobalId = {
+    val header = implicitly[TypeHeader[M]]
     require(id.id <= maxId, "Id too large to be globalized")
     GlobalId((header.shifted | id.id))
   }
 }
 
 trait GlobalIdCompanion {
-  type G <: ModelWithGlobalId
-  implicit def header: TypeHeader[G]
+  type M <: ModelWithGlobalId[M]
+  type G <: ModelWithGlobalId[M]
+  implicit def header: TypeHeader[M]
 }
 
 object ModelWithGlobalId {
-  val all: Set[GlobalIdCompanion] = Reflect.getCompanionTypeSystem[ModelWithGlobalId, GlobalIdCompanion]("G")
+  val all: Set[GlobalIdCompanion] = Reflect.getCompanionTypeSystem[ModelWithGlobalId[_], GlobalIdCompanion]("G")
   private val byHeaderCode = {
     require(all.size == all.map(_.header).size, "Duplicate GlobalId headers")
     all.map { companion => companion.header.code -> companion }.toMap
@@ -41,17 +42,16 @@ object ModelWithGlobalId {
   def apply(headerCode: Short): GlobalIdCompanion = byHeaderCode(headerCode)
 }
 
-
-sealed trait ModelWithGlobalId {
-  type G <: ModelWithGlobalId
-  type M <: G
+sealed trait ModelWithGlobalId[M <: ModelWithGlobalId[M]] { self: Model[M] =>
+  type G <: ModelWithGlobalId[M]
   def id: Option[Id[M]]
-  def globalId(implicit header: TypeHeader[G]): Option[GlobalId] = id.map(GlobalId[G, M](_))
+  def globalId(implicit header: TypeHeader[M]): Option[GlobalId] = id.map(GlobalId[M](_))
 }
 
-trait UserWithGlobalId extends ModelWithGlobalId { self: User => type G = UserWithGlobalId; type M = User }
+trait UserWithGlobalId extends ModelWithGlobalId[User] { self: User => type G = UserWithGlobalId; }
 
 case object UserWithGlobalId extends GlobalIdCompanion {
   type G = UserWithGlobalId
-  val header = TypeHeader[G](1)
+  type M = User
+  val header = TypeHeader[M](1)
 }
