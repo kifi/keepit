f324600ab1719797477f5204e1dc5a15ddbdbf31
Andrew Conner <git@andrewconner.org>
2014-04-04 17:40:37 -0700

    Ability to tell backend to refresh networks

--- kifi-backend/modules/shoebox/angular/src/common/directives/keepWho/keepWhoText.js
+++ kifi-backend/modules/shoebox/angular/src/common/directives/keepWho/keepWhoText.js
@@ -47,7 +47,7 @@ angular.module('kifi.keepWhoText', [])
           if (others === 1) {
             text = '1 other';
           } else {
-            text = others + 'others';
+            text = others + ' others';
           }
           if (scope.keep.isMyBookmark || scope.keep.keepers.length > 0) {
             text = '+ ' + text
