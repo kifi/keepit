64dbeea523dec6a58f1b4c0717a40c14ef82a63a
Léo Grimaldi <leo@42go.com>
2014-04-07 08:54:24 -0700

    Better naming in Companion type system

--- kifi-backend/modules/common/app/com/keepit/common/reflection/CompanionTypeSystem.scala
+++ kifi-backend/modules/common/app/com/keepit/common/reflection/CompanionTypeSystem.scala
@@ -5,49 +5,47 @@ import scala.util.{Failure, Success, Try}
 
 object CompanionTypeSystem {
 
-  def apply[SealedClass: TypeTag, Companion: TypeTag](forwardTypeReference: String): Set[Companion] = {
+  def apply[SealedClass: TypeTag, Companion: TypeTag](fBoundedType: String): Set[Companion] = {
     val sealedClassType = typeOf[SealedClass]
     val companionType = typeOf[Companion]
-    val forwardTypeUpperBound = sealedClassType
-    val refineCompanionWithForwardTypeParameter = getTypeWithForwardTypeParameterOrElseCheckTypeMember(companionType, forwardTypeReference, forwardTypeUpperBound)
-    val refineSealedClassWithForwardTypeParameter = getTypeWithForwardTypeParameterOrElseCheckTypeMember(sealedClassType, forwardTypeReference, forwardTypeUpperBound)
+    val upperBound = sealedClassType
+    val refineCompanionWithTypeParameter = getTypeWithTypeParameterOrElseCheckTypeMember(companionType, fBoundedType, upperBound)
+    val refineSealedClassWithTypeParameter = getTypeWithTypeParameterOrElseCheckTypeMember(sealedClassType, fBoundedType, upperBound)
 
     getSubclasses(sealedClassType).map { subclass =>
       val subclassType = subclass.toType
 
-      refineSealedClassWithForwardTypeParameter match {
+      refineSealedClassWithTypeParameter match {
         case Some(refineSealedClassTypeWith) =>
           val expectedSubclassType = refineSealedClassTypeWith(subclassType)
           require(subclassType <:< expectedSubclassType, s"Class $subclassType must extend $expectedSubclassType")
         case None =>
           require(subclassType <:< sealedClassType, s"Class $subclassType must extend $sealedClassType")
-          val expectedForwardType = subclassType
-          checkTypeMember(subclassType, forwardTypeReference, expectedForwardType, strict = true)
+          checkTypeMember(subclassType, fBoundedType, subclassType)
       }
 
       val subclassCompanionType = subclass.companionSymbol.typeSignature
 
-      refineCompanionWithForwardTypeParameter match {
+      refineCompanionWithTypeParameter match {
         case Some(refineCompanionTypeWith) =>
           val expectedSubclassCompanionType = refineCompanionTypeWith(subclassType)
           require(subclassCompanionType <:< expectedSubclassCompanionType, s"Companion object $subclassCompanionType of $subclass must extend $expectedSubclassCompanionType")
         case None =>
           require(subclassCompanionType <:< companionType, s"Companion object $subclassCompanionType of $subclass must extend $companionType")
-          val expectedForwardType = subclassType
-          checkTypeMember(subclassCompanionType, forwardTypeReference, expectedForwardType, strict = true)
+          checkTypeMember(subclassCompanionType, fBoundedType, subclassType)
       }
       getCompanion(subclass).asInstanceOf[Companion]
    }
   }
 
-  private def getTypeWithForwardTypeParameterOrElseCheckTypeMember(owner: Type, forwardTypeReference: TypeName, forwardTypeUpperBound: Type): Option[Type => Type] = {
-    Try { refineExistentialTypeParameter(owner, forwardTypeReference, forwardTypeUpperBound) } match {
-      case Success(typeWithForwardTypeParameter) => Some(typeWithForwardTypeParameter)
-      case Failure(forwardTypeParameterException) => {
-        Try(checkTypeMember(owner, forwardTypeReference, forwardTypeUpperBound, strict = false)) match {
+  private def getTypeWithTypeParameterOrElseCheckTypeMember(owner: Type, fBoundedType: TypeName, upperBound: Type): Option[Type => Type] = {
+    Try { refineExistentialTypeParameter(owner, fBoundedType, upperBound) } match {
+      case Success(typeWithTypeParameter) => Some(typeWithTypeParameter)
+      case Failure(fBoundedTypeParameterException) => {
+        Try(checkExistentialTypeMember(owner, fBoundedType, upperBound)) match {
           case Success(_) => None
-          case Failure(forwardTypeMemberException) =>
-            throw new IllegalArgumentException(s"Could not find a valid forward type reference $forwardTypeReference in $owner: \n No valid type parameter (${forwardTypeParameterException.getMessage}) \n No valid type member (${forwardTypeMemberException.getMessage})")
+          case Failure(fBoundedTypeMemberException) =>
+            throw new IllegalArgumentException(s"Could not find a valid F-bounded type $fBoundedType in $owner: \n No valid type parameter (${fBoundedTypeParameterException.getMessage}) \n No valid type member (${fBoundedTypeMemberException.getMessage})")
         }
       }
     }
@@ -64,36 +62,38 @@ object CompanionTypeSystem {
     clazz.knownDirectSubclasses.map(_.asClass)
   }
 
-   private def checkTypeMember(owner: Type, name: TypeName, expectedType: Type, strict: Boolean): Unit = {
+   private def checkTypeMember(owner: Type, name: TypeName, expectedType: Type): Unit = {
      val typeMemberType = owner.member(name).typeSignature
-     if (strict) require(typeMemberType =:= expectedType, s"Type member $name in $owner is $typeMemberType, must be $expectedType")
-     else {
-       require(typeMemberType.isInstanceOf[TypeBoundsApi], s"$typeMemberType is not a type bounds.")
-       val TypeBounds(_, hi) = typeMemberType
-       require(hi =:= expectedType, s"Type member $name in $owner is $typeMemberType, upper type bound must be $expectedType")
-     }
+     require(typeMemberType =:= expectedType, s"Type member $name in $owner is $typeMemberType, must be $expectedType")
    }
 
-    private def refineExistentialTypeParameter(existentialType: Type, parameterName: TypeName, expectedUpperType: Type): Type => Type = {
-      require(existentialType.isInstanceOf[ExistentialTypeApi], s"$existentialType is not existential.")
+    private def checkExistentialTypeMember(owner: Type, name: TypeName, upperBound: Type): Unit = {
+      val typeMemberType = owner.member(name).typeSignature
+      require(typeMemberType.isInstanceOf[TypeBoundsApi], s"$typeMemberType is not a type bounds.")
+      val TypeBounds(_, hi) = typeMemberType
+      require(hi =:= upperBound, s"Type member $name in $owner is $typeMemberType, upper type bound must be $upperBound")
+    }
 
-      val clazz = existentialType.typeSymbol.asClass
+    private def refineExistentialTypeParameter(owner: Type, parameterName: TypeName, upperBound: Type): Type => Type = {
+      require(owner.isInstanceOf[ExistentialTypeApi], s"$owner is not existential.")
+
+      val clazz = owner.typeSymbol.asClass
       val paramPosition = clazz.typeParams.indexWhere(_.name == parameterName)
-      require(paramPosition > -1, s"Type parameter $parameterName not found in $clazz of $existentialType")
+      require(paramPosition > -1, s"Type parameter $parameterName not found in $clazz of $owner")
 
-      val ExistentialType(existentialArgs, underlying) = existentialType
+      val ExistentialType(existentialArgs, underlying) = owner
       val TypeRef(_, _, args) = underlying
       val existentialArg = args(paramPosition).typeSymbol
       val existentialArgSymbol = existentialArg
-      require(existentialArgs.contains(existentialArgSymbol), s"Argument $existentialArg for type parameter $parameterName in $existentialType is not existential.")
+      require(existentialArgs.contains(existentialArgSymbol), s"Argument $existentialArg for type parameter $parameterName in $owner is not existential.")
 
       val existentialArgBounds = existentialArgSymbol.typeSignature
       val TypeBounds(lo, hi) = existentialArgBounds
-      require(hi =:= expectedUpperType, s"Existential argument for parameter $parameterName in $existentialType is $existentialArg: $existentialArgBounds, upper type bound must be $expectedUpperType.")
+      require(hi =:= upperBound, s"Existential argument for parameter $parameterName in $owner is $existentialArg: $existentialArgBounds, upper type bound must be $upperBound.")
 
       { case refinedArgument: Type =>
-        require(refinedArgument <:< hi, s"Argument $refinedArgument for parameter $parameterName in $existentialType must be a subtype of $hi from $existentialArg: $existentialArgBounds")
-        require(lo <:< refinedArgument, s"Argument $refinedArgument for parameter $parameterName in $existentialType must be a supertype of $lo from $existentialArg: $existentialArgBounds")
+        require(refinedArgument <:< hi, s"Argument $refinedArgument for parameter $parameterName in $owner must be a subtype of $hi from $existentialArg: $existentialArgBounds")
+        require(lo <:< refinedArgument, s"Argument $refinedArgument for parameter $parameterName in $owner must be a supertype of $lo from $existentialArg: $existentialArgBounds")
         underlying.substituteTypes(existentialArgSymbol::Nil, refinedArgument::Nil)
       }
     }
