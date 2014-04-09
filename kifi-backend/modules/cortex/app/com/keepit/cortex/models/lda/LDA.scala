a6fb3f9486b4dc779ade3024f7b7063e8a1a4d0d
yingjieMiao <yingjie@42go.com>
2014-04-08 22:39:56 -0700

    clean imports

--- kifi-backend/modules/cortex/app/com/keepit/cortex/models/lda/LDA.scala
+++ kifi-backend/modules/cortex/app/com/keepit/cortex/models/lda/LDA.scala
@@ -1,24 +1,7 @@
 package com.keepit.cortex.models.lda
 
-import com.amazonaws.services.s3.AmazonS3
-import com.google.inject.Inject
-import com.google.inject.Singleton
-import com.keepit.common.db.Id
-import com.keepit.common.db.SequenceNumber
-import com.keepit.common.logging.AccessLog
-import com.keepit.common.store.S3Bucket
-import com.keepit.cortex.core._
-import com.keepit.cortex.features._
-import com.keepit.cortex.plugins.FeatureUpdater
-import com.keepit.cortex.plugins.URIPuller
-import com.keepit.cortex.store._
-import com.keepit.model.NormalizedURI
-import com.keepit.search.Article
-import com.keepit.search.ArticleStore
-import com.keepit.search.Lang
-import com.keepit.cortex.plugins.URIFeatureUpdater
-import com.keepit.cortex.plugins.DataPuller
-import com.keepit.cortex._
+import com.keepit.cortex.core.{BinaryFormatter, StatModel}
+import com.keepit.cortex.store.StoreUtil
 
 trait LDA extends StatModel
 
