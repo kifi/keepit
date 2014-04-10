72c3add3763d178d932780ec26b3fecc5e6cfe47
Andrew Conner <git@andrewconner.org>
2014-04-10 14:22:38 -0700

    Removing amateur alignment

--- kifi-backend/modules/shoebox/angular/src/app.js
+++ kifi-backend/modules/shoebox/angular/src/app.js
@@ -145,13 +145,13 @@ angular.module('kifi', [
 ])
 
 .run([
-  'profileService', '$rootScope',
-  function (profileService, $rootScope) {
+  'profileService', '$rootScope', '$window',
+  function (profileService, $rootScope, $window) {
     // Initial data loading:
 
     profileService.fetchPrefs().then(function (res) {
       // handle onboarding / imports
-      if (!res['onboarding_seen']) {
+      if (!res.onboarding_seen) {
         $rootScope.$emit('showGettingStarted');
       } else {
         $window.postMessage('get_bookmark_count_if_should_import', '*'); // may get {bookmarkCount: N} reply message
