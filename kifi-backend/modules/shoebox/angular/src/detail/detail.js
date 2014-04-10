b06148a6c8c8f1dc0616272ce50fbf08bcbc4af7
Andrew Conner <git@andrewconner.org>
2014-04-10 08:38:40 -0700

    Small fixes, mobile launch date change

--- kifi-backend/modules/shoebox/angular/src/detail/detail.js
+++ kifi-backend/modules/shoebox/angular/src/detail/detail.js
@@ -68,7 +68,7 @@ angular.module('kifi.detail',
         };
 
         scope.isPublic = function () {
-          return scope.howKept === 'public';
+          return scope.howKept === 'public' && scope.keep.isMyBookmark;
         };
 
         scope.toggleKeep = function () {
