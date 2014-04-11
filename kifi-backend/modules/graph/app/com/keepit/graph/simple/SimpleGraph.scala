563718171f9c2a60feb6f00ee4c902aac9736c20
Léo Grimaldi <leo@42go.com>
2014-04-11 13:49:05 -0700

    Locking SimpleGraph on commits

--- kifi-backend/modules/graph/app/com/keepit/graph/simple/SimpleGraph.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/simple/SimpleGraph.scala
@@ -3,12 +3,14 @@ package com.keepit.graph.simple
 import com.keepit.graph.model._
 import scala.collection.concurrent.{Map => ConcurrentMap, TrieMap}
 
-class SimpleGraph(vertices: ConcurrentMap[VertexId, MutableVertex]) extends GraphReaderImpl(vertices) with GraphManager {
+class SimpleGraph(vertices: ConcurrentMap[VertexId, MutableVertex]) extends GraphReaderImpl(vertices) with GraphManager { self =>
   def write(f: GraphWriter => Unit): Unit = {
     val bufferedVertices = new BufferedMap(vertices)
     val graphWriter = new GraphWriterImpl(bufferedVertices)
     f(graphWriter)
-    graphWriter.commit()
+    self.synchronized {
+      graphWriter.commit()
+    }
   }
 }
 
