cc2d9406b4247b8cd96ec5ba6f4691005aa0e113
Léo Grimaldi <leo@42go.com>
2014-04-15 09:57:14 -0700

    Drafts GraphUpdateFetcher implementation

--- kifi-backend/modules/graph/app/com/keepit/graph/ingestion/GraphUpdateFetcher.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/ingestion/GraphUpdateFetcher.scala
@@ -4,13 +4,23 @@ import com.kifi.franz.{SQSMessage, SQSQueue}
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import com.google.inject.Inject
+import com.keepit.shoebox.ShoeboxServiceClient
+import com.keepit.eliza.ElizaServiceClient
 
 trait GraphUpdateFetcher {
-  val lockTimeout: FiniteDuration
-  def lockDuration
-  def queue: SQSQueue[GraphUpdate]
-  def nextBatch(maxBatchSize: Int): Future[Seq[SQSMessage[GraphUpdate]]] = {
-    queue.nextBatchWithLock(maxBatchSize, lockTimeout)
-  }
+  def nextBatch(maxBatchSize: Int): Future[Seq[SQSMessage[GraphUpdate]]]
   def fetch(currentState: GraphUpdaterState): Unit
+}
+
+class GraphUpdateFetcherImpl @Inject() (
+  queue: SQSQueue[GraphUpdate],
+  shoebox: ShoeboxServiceClient,
+  eliza: ElizaServiceClient
+) extends GraphUpdateFetcher {
+  val lockTimeout: FiniteDuration = ???
+  def nextBatch(maxBatchSize: Int): Future[Seq[SQSMessage[GraphUpdate]]] = queue.nextBatchWithLock(maxBatchSize, lockTimeout)
+  def fetch(currentState: GraphUpdaterState): Unit = GraphUpdateKind.all.foreach {
+    case _ => ???
+  }
 }
