711d3c53e0cebb626a8ecdc3f3d442973bdde53a
Léo Grimaldi <leo@42go.com>
2014-04-14 20:41:30 -0700

    Simpler sealed GraphUpdate

--- kifi-backend/modules/graph/app/com/keepit/graph/ingestion/GraphUpdate.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/ingestion/GraphUpdate.scala
@@ -1,17 +1,15 @@
 package com.keepit.graph.ingestion
 
-import com.keepit.graph.model._
 import com.keepit.common.db.SequenceNumber
 import com.keepit.common.reflection.CompanionTypeSystem
 
-trait GraphUpdate { self =>
+sealed trait GraphUpdate { self =>
   type U >: self.type <: GraphUpdate
-  def ingest()(implicit writer: GraphWriter): Unit
   def seq: SequenceNumber[U]
   def kind: GraphUpdateKind[U]
 }
 
-trait GraphUpdateKind[U <: GraphUpdate] {
+sealed trait GraphUpdateKind[U <: GraphUpdate] {
   def code: String
   def seq(value: Long): SequenceNumber[U] = SequenceNumber(value)
 }
