c37207478af1098969d37ddee41c00ababfc0041
Léo Grimaldi <leo@42go.com>
2014-04-14 23:04:25 -0700

    With mutable GraphUpdaterState

--- kifi-backend/modules/graph/app/com/keepit/graph/ingestion/GraphUpdaterState.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/ingestion/GraphUpdaterState.scala
@@ -4,22 +4,21 @@ import com.keepit.common.db.SequenceNumber
 import play.api.libs.json._
 import play.api.libs.json.JsObject
 
-case class GraphUpdaterState(state: Map[GraphUpdateKind[_ <: GraphUpdate], Long]) extends AnyVal {
-  def withUpdates(updates: Seq[GraphUpdate]): GraphUpdaterState = {
-    val newState = state ++ updates.groupBy(_.kind).mapValues(_.map(_.seq.value).max)
-    GraphUpdaterState(newState)
-  }
+class GraphUpdaterState(var state: Map[GraphUpdateKind[_ <: GraphUpdate], Long]) {
+  def updateWith(updates: Seq[GraphUpdate]): Unit = { state ++= updates.groupBy(_.kind).mapValues(_.map(_.seq.value).max) }
   def getCurrentSequenceNumber[U <: GraphUpdate](implicit kind: GraphUpdateKind[U]): SequenceNumber[U] = SequenceNumber[U](state.getOrElse(kind, 0))
 }
 
 object GraphUpdaterState {
   val format: Format[GraphUpdaterState] = new Format[GraphUpdaterState] {
     def reads(json: JsValue): JsResult[GraphUpdaterState] = json.validate[JsObject].map { case obj =>
-      val state = obj.value.map { case (kindName, seqValue) => GraphUpdateKind(kindName) -> seqValue.as[Long] }.toMap
-      GraphUpdaterState(state)
+      val state = obj.value.map { case (kindName, seqValue) =>
+        GraphUpdateKind(kindName) -> seqValue.as[Long]
+      }.toMap[GraphUpdateKind[_ <: GraphUpdate], Long]
+      new GraphUpdaterState(state)
     }
     def writes(state: GraphUpdaterState): JsValue = JsObject(state.state.map { case (kind, seq) => kind.code -> JsNumber(seq) }.toSeq)
   }
 
-  val empty = GraphUpdaterState(Map.empty)
+  val empty = new GraphUpdaterState(Map.empty)
 }
