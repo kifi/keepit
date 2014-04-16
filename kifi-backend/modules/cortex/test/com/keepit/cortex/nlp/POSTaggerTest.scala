cb9b1e065471ac4e2690d567c608c49ca2bf969a
yingjieMiao <yingjie@42go.com>
2014-04-15 17:11:58 -0700

    fix indentation

--- kifi-backend/modules/cortex/test/com/keepit/cortex/nlp/POSTaggerTest.scala
+++ kifi-backend/modules/cortex/test/com/keepit/cortex/nlp/POSTaggerTest.scala
@@ -6,7 +6,7 @@ class POSTaggerTest extends Specification {
   "basic pos tagger" should {
     "work" in {
       POSTagger.tagOneWord("apple").value() === "NN"
-        POSTagger.tagOneWord("apples").value() === "NNS"
+      POSTagger.tagOneWord("apples").value() === "NNS"
       POSTagger.tagOneWord("speak").value() === "VB"
     }
   }
