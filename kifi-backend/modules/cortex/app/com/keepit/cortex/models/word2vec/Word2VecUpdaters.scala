5e4cafa79c1afcc45cf4ad85f37ef398d3e1e489
yingjieMiao <yingjie@42go.com>
2014-04-18 12:36:03 -0700

    let cpu burn

--- kifi-backend/modules/cortex/app/com/keepit/cortex/models/word2vec/Word2VecUpdaters.scala
+++ kifi-backend/modules/cortex/app/com/keepit/cortex/models/word2vec/Word2VecUpdaters.scala
@@ -16,7 +16,7 @@ class Word2VecURIFeatureUpdater @Inject()(
   commitStore: Word2VecURIFeatureCommitStore,
   uriPuller: URIPuller
 ) extends URIFeatureUpdater[Word2Vec](representer, featureStore, commitStore, uriPuller){
-  override val pullSize = 100
+  override val pullSize = 150
 }
 
 class Word2VecURIFeatureUpdateActor @Inject()(
