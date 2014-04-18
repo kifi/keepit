f7e909f53f7746b0b2e0cc3f8f48e0911f3a4648
Léo Grimaldi <leo@42go.com>
2014-04-18 11:42:25 -0700

    Instantiate GraphManagerPlugin

--- kifi-backend/modules/graph/app/com/keepit/graph/controllers/internal/GraphController.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/controllers/internal/GraphController.scala
@@ -3,10 +3,10 @@ package com.keepit.graph.controllers.internal
 import com.google.inject.Inject
 import com.keepit.common.controller.GraphServiceController
 import com.keepit.common.logging.Logging
-import com.keepit.graph.manager.GraphManager
+import com.keepit.graph.manager.GraphManagerPlugin
 
 class GraphController @Inject() (
-  graphManager: GraphManager
+  graphPlugin: GraphManagerPlugin
 ) extends GraphServiceController with Logging {
 
 }
