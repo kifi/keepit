d425eb56ab8b5e139c2756436fab533c649bd0e4
Martin Raison <martin@42go.com>
2014-04-18 16:51:03 -0700

    [ext-msg] handling future failure

--- kifi-backend/modules/eliza/app/com/keepit/eliza/commanders/EmailMessageProcessingCommander.scala
+++ kifi-backend/modules/eliza/app/com/keepit/eliza/commanders/EmailMessageProcessingCommander.scala
@@ -11,7 +11,7 @@ import scala.Some
 import com.keepit.eliza.model.NonUserThread
 import com.keepit.common.healthcheck.AirbrakeNotifier
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import scala.util.Success
+import scala.util.{Success, Failure}
 import com.keepit.common.db.slick.Database
 import com.keepit.heimdal.HeimdalContextBuilderFactory
 import scala.concurrent.duration._
@@ -27,28 +27,34 @@ class EmailMessageProcessingCommander @Inject() (
 
   def readIncomingMessages(): Unit = {
     import NonUserThread._
-    mailNotificationReplyQueue.nextWithLock(1 minute).map{ result =>
-      try {
-        result.map { sqsMessage =>
-          val message = sqsMessage.body
-          ModelWithPublicId.decode[NonUserThread](message.publicId) match {
-            case Success(id: Id[NonUserThread]) => {
-              message.content match {
-                case Some(content: String) => {
-                  val contextBuilder = heimdalContextBuilder()
-                  contextBuilder += ("source", "email")
-                  messagingCommander.sendMessageWithNonUserThread(id, content, None)(contextBuilder.build)
+    mailNotificationReplyQueue.nextWithLock(1 minute).onComplete{
+      case Success(result) => {
+        try {
+          result.map { sqsMessage =>
+            val message = sqsMessage.body
+            ModelWithPublicId.decode[NonUserThread](message.publicId) match {
+              case Success(id: Id[NonUserThread]) => {
+                message.content match {
+                  case Some(content: String) => {
+                    val contextBuilder = heimdalContextBuilder()
+                    contextBuilder += ("source", "email")
+                    messagingCommander.sendMessageWithNonUserThread(id, content, None)(contextBuilder.build)
+                  }
+                  case None => airbrake.notify(s"Could not extract contents of email: publicId = ${id} and timestamp = ${message.timestamp}")
                 }
-                case None => airbrake.notify(s"Could not extract contents of email: publicId = ${id} and timestamp = ${message.timestamp}")
               }
+              case _ => log.info(s"Email with invalid public id ${message.publicId}")
             }
-            case _ => log.info(s"Email with invalid public id ${message.publicId}")
           }
+        } catch {
+          case e:Throwable => log.warn(s"Failed to read messages: ${e.getMessage()}")
         }
-      } catch {
-        case e:Throwable => log.warn(s"Failed to read messages ${e.getMessage()}")
+        readIncomingMessages()
       }
-      readIncomingMessages()
+      case Failure(t) => {
+        log.info("RConn: Queue call failed")
+        airbrake.notify("Failed reading incoming messages from queue", t)
+      }
     }
   }
 }
