ee49b4077b596d1509a72b98ed412df619ff4778
yingjieMiao <yingjie@42go.com>
2014-04-18 17:54:52 -0700

    admin endpoint to query uri similarity

--- kifi-backend/modules/common/test/com/keepit/cortex/FakeCortexServiceClientImpl.scala
+++ kifi-backend/modules/common/test/com/keepit/cortex/FakeCortexServiceClientImpl.scala
@@ -1,8 +1,11 @@
 package com.keepit.cortex
 
 import scala.concurrent.Future
+import com.keepit.common.db.Id
+import com.keepit.model.NormalizedURI
 
 class FakeCortexServiceClientImpl extends CortexServiceClientImpl(null, null, null){
   override def word2vecWordSimilarity(word1: String, word2: String): Future[Option[Float]] = ???
   override def word2vecKeywordsAndBOW(text: String): Future[Map[String, String]] = ???
+  override def word2vecURISimilairty(uri1: Id[NormalizedURI], uri2: Id[NormalizedURI]): Future[Option[Float]] = ???
 }
