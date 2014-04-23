454f13a6ab345dcf48421ecc3bc1790ae702b867
Léo Grimaldi <leo@42go.com>
2014-04-23 11:17:16 -0700

    Defines SocialUserInfoGraphUpdate processing in GraphUpdater

--- kifi-backend/modules/graph/app/com/keepit/graph/manager/GraphUpdater.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/manager/GraphUpdater.scala
@@ -1,8 +1,11 @@
 package com.keepit.graph.manager
 
-import com.keepit.graph.model.{EmptyEdgeData, UserData, GraphWriter}
+import com.keepit.graph.model._
 import com.google.inject.Inject
 import com.keepit.model.UserConnectionStates
+import com.keepit.social.SocialNetworks
+import com.keepit.graph.model.UserData
+import com.keepit.graph.model.FacebookAccountData
 
 trait GraphUpdater {
   def apply(update: GraphUpdate)(implicit writer: GraphWriter): Unit
@@ -12,6 +15,7 @@ class GraphUpdaterImpl @Inject() () extends GraphUpdater {
   def apply(update: GraphUpdate)(implicit writer: GraphWriter): Unit = update match {
     case userGraphUpdate: UserGraphUpdate => writer.saveVertex(UserData(userGraphUpdate.userId))
     case userConnectionGraphUpdate: UserConnectionGraphUpdate => processUserConnectionGraphUpdate(userConnectionGraphUpdate)
+    case socialUserInfoGraphUpdate: SocialUserInfoGraphUpdate => processSocialUserInfoGraphUpdate(socialUserInfoGraphUpdate)
   }
 
   private def processUserConnectionGraphUpdate(update: UserConnectionGraphUpdate)(implicit writer: GraphWriter) = update.state match {
@@ -23,5 +27,11 @@ class GraphUpdaterImpl @Inject() () extends GraphUpdater {
       writer.saveVertex(UserData(update.secondUserId))
       writer.saveEdge(update.firstUserId, update.secondUserId, EmptyEdgeData)
       writer.saveEdge(update.secondUserId, update.firstUserId, EmptyEdgeData)
+  }
+
+  private def processSocialUserInfoGraphUpdate(update: SocialUserInfoGraphUpdate)(implicit writer: GraphWriter) = update.network match {
+    case SocialNetworks.FACEBOOK => writer.saveVertex(FacebookAccountData(update.socialUserId))
+    case SocialNetworks.LINKEDIN => writer.saveVertex(LinkedInAccountData(update.socialUserId))
+    case _ => // ignore
   }
 }
