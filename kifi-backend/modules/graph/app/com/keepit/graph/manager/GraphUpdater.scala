a473e0c327244c5b8f90d79dc308d61e3f1ba4f9
Léo Grimaldi <leo@42go.com>
2014-04-22 17:03:44 -0700

    Adds processing case statement for UserConnectionGraphUpdate

--- kifi-backend/modules/graph/app/com/keepit/graph/manager/GraphUpdater.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/manager/GraphUpdater.scala
@@ -1,7 +1,8 @@
 package com.keepit.graph.manager
 
-import com.keepit.graph.model.{UserData, GraphWriter}
+import com.keepit.graph.model.{EmptyEdgeData, UserData, GraphWriter}
 import com.google.inject.Inject
+import com.keepit.model.UserConnectionStates
 
 trait GraphUpdater {
   def apply(update: GraphUpdate)(implicit writer: GraphWriter): Unit
@@ -10,5 +11,17 @@ trait GraphUpdater {
 class GraphUpdaterImpl @Inject() () extends GraphUpdater {
   def apply(update: GraphUpdate)(implicit writer: GraphWriter): Unit = update match {
     case userGraphUpdate: UserGraphUpdate => writer.saveVertex(UserData(userGraphUpdate.userId))
+    case userConnectionGraphUpdate: UserConnectionGraphUpdate => processUserConnectionGraphUpdate(userConnectionGraphUpdate)
+  }
+
+  private def processUserConnectionGraphUpdate(update: UserConnectionGraphUpdate)(implicit writer: GraphWriter) = update.state match {
+    case UserConnectionStates.UNFRIENDED | UserConnectionStates.INACTIVE =>
+      writer.removeEdge(update.firstUserId, update.secondUserId)
+      writer.removeEdge(update.secondUserId, update.firstUserId)
+    case UserConnectionStates.ACTIVE =>
+      writer.saveVertex(UserData(update.firstUserId))
+      writer.saveVertex(UserData(update.secondUserId))
+      writer.saveEdge(update.firstUserId, update.secondUserId, EmptyEdgeData)
+      writer.saveEdge(update.secondUserId, update.firstUserId, EmptyEdgeData)
   }
 }
