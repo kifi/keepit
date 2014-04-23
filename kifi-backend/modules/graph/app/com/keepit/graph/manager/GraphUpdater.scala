29ee9c07886206fce4b08c2fa6d78e4b67170aba
Léo Grimaldi <leo@42go.com>
2014-04-23 16:56:19 -0700

    GraphUpdater methods processing SocialUserInfo and SocialConnection updates

--- kifi-backend/modules/graph/app/com/keepit/graph/manager/GraphUpdater.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/manager/GraphUpdater.scala
@@ -2,7 +2,7 @@ package com.keepit.graph.manager
 
 import com.keepit.graph.model._
 import com.google.inject.Inject
-import com.keepit.model.UserConnectionStates
+import com.keepit.model.{SocialConnectionStates, UserConnectionStates}
 import com.keepit.social.SocialNetworks
 import com.keepit.graph.model.UserData
 import com.keepit.graph.model.FacebookAccountData
@@ -13,15 +13,20 @@ trait GraphUpdater {
 
 class GraphUpdaterImpl @Inject() () extends GraphUpdater {
   def apply(update: GraphUpdate)(implicit writer: GraphWriter): Unit = update match {
-    case userGraphUpdate: UserGraphUpdate => writer.saveVertex(UserData(userGraphUpdate.userId))
+    case userGraphUpdate: UserGraphUpdate => processUserGraphUpdate(userGraphUpdate)
     case userConnectionGraphUpdate: UserConnectionGraphUpdate => processUserConnectionGraphUpdate(userConnectionGraphUpdate)
     case socialUserInfoGraphUpdate: SocialUserInfoGraphUpdate => processSocialUserInfoGraphUpdate(socialUserInfoGraphUpdate)
+    case socialConnectionGraphUpdate: SocialConnectionGraphUpdate => processSocialConnectionGraphUpdate(socialConnectionGraphUpdate)
+  }
+
+  private def processUserGraphUpdate(update: UserGraphUpdate)(implicit writer: GraphWriter) = {
+    writer.saveVertex(UserData(update.userId))
   }
 
   private def processUserConnectionGraphUpdate(update: UserConnectionGraphUpdate)(implicit writer: GraphWriter) = update.state match {
     case UserConnectionStates.UNFRIENDED | UserConnectionStates.INACTIVE =>
-      writer.removeEdge(update.firstUserId, update.secondUserId)
-      writer.removeEdge(update.secondUserId, update.firstUserId)
+      writer.removeEdgeIfExists(update.firstUserId, update.secondUserId)
+      writer.removeEdgeIfExists(update.secondUserId, update.firstUserId)
     case UserConnectionStates.ACTIVE =>
       writer.saveVertex(UserData(update.firstUserId))
       writer.saveVertex(UserData(update.secondUserId))
@@ -30,8 +35,57 @@ class GraphUpdaterImpl @Inject() () extends GraphUpdater {
   }
 
   private def processSocialUserInfoGraphUpdate(update: SocialUserInfoGraphUpdate)(implicit writer: GraphWriter) = update.network match {
-    case SocialNetworks.FACEBOOK => writer.saveVertex(FacebookAccountData(update.socialUserId))
-    case SocialNetworks.LINKEDIN => writer.saveVertex(LinkedInAccountData(update.socialUserId))
+    case SocialNetworks.FACEBOOK =>
+      val facebookAccountVertex = FacebookAccountData(update.socialUserId)
+      writer.saveVertex(facebookAccountVertex)
+      update.userId.foreach { userId =>
+        val userVertex = UserData(userId)
+        writer.saveVertex(userVertex)
+        writer.saveEdge(facebookAccountVertex.id, userVertex.id, EmptyEdgeData)
+        writer.saveEdge(userVertex.id, facebookAccountVertex.id, EmptyEdgeData)
+      }
+
+    case SocialNetworks.LINKEDIN =>
+      val linkedInAccountVertex = LinkedInAccountData(update.socialUserId)
+      writer.saveVertex(linkedInAccountVertex)
+      update.userId.foreach { userId =>
+        val userVertex = UserData(userId)
+        writer.saveVertex(userVertex)
+        writer.saveEdge(linkedInAccountVertex.id, userVertex.id, EmptyEdgeData)
+        writer.saveEdge(userVertex.id, linkedInAccountVertex.id, EmptyEdgeData)
+      }
+    case _ => // ignore
+  }
+
+  private def processSocialConnectionGraphUpdate(update: SocialConnectionGraphUpdate)(implicit writer: GraphWriter) = update.network match {
+    case SocialNetworks.FACEBOOK =>
+      val firstFacebookUserVertexId: VertexDataId[FacebookAccountReader] = update.firstSocialUserId
+      val secondFacebookUserVertexId: VertexDataId[FacebookAccountReader] = update.secondSocialUserId
+      update.state match {
+        case SocialConnectionStates.INACTIVE =>
+          writer.removeEdgeIfExists(firstFacebookUserVertexId, secondFacebookUserVertexId)
+          writer.removeEdgeIfExists(secondFacebookUserVertexId, firstFacebookUserVertexId)
+        case SocialConnectionStates.ACTIVE =>
+          writer.saveVertex(FacebookAccountData(firstFacebookUserVertexId))
+          writer.saveVertex(FacebookAccountData(secondFacebookUserVertexId))
+          writer.saveEdge(firstFacebookUserVertexId, secondFacebookUserVertexId, EmptyEdgeData)
+          writer.saveEdge(secondFacebookUserVertexId, firstFacebookUserVertexId, EmptyEdgeData)
+      }
+
+    case SocialNetworks.LINKEDIN =>
+      val firstLinkedInUserVertexId: VertexDataId[LinkedInAccountReader] = update.firstSocialUserId
+      val secondLinkedInUserVertexId: VertexDataId[LinkedInAccountReader] = update.secondSocialUserId
+      update.state match {
+        case SocialConnectionStates.INACTIVE =>
+          writer.removeEdgeIfExists(firstLinkedInUserVertexId, secondLinkedInUserVertexId)
+          writer.removeEdgeIfExists(secondLinkedInUserVertexId, firstLinkedInUserVertexId)
+        case SocialConnectionStates.ACTIVE =>
+          writer.saveVertex(LinkedInAccountData(firstLinkedInUserVertexId))
+          writer.saveVertex(LinkedInAccountData(secondLinkedInUserVertexId))
+          writer.saveEdge(firstLinkedInUserVertexId, secondLinkedInUserVertexId, EmptyEdgeData)
+          writer.saveEdge(secondLinkedInUserVertexId, firstLinkedInUserVertexId, EmptyEdgeData)
+      }
+
     case _ => // ignore
   }
 }
