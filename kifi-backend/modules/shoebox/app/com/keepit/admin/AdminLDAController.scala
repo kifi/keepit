a12be42a66b9143a01a664749a3426946bed3219
yingjieMiao <yingjie@42go.com>
2014-04-24 15:30:28 -0700

    better formatting

--- kifi-backend/modules/shoebox/app/com/keepit/admin/AdminLDAController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/admin/AdminLDAController.scala
@@ -23,12 +23,24 @@ class AdminLDAController @Inject()(
     Ok(html.admin.lda(n))
   }
 
+  private def getFormatted(words: Map[String, Float]): String = {
+    val width = words.keys.map{_.length}.foldLeft(0)(_ max _) + 1
+    words.toArray.sortBy(-1f * _._2).grouped(8).map{ gp =>
+      gp.map{ case (w, s) => s"%${width}s".format(w) + "  " +  "%.4f".format(s) }.mkString("  ")
+    }.mkString("\n")
+  }
+
   def showTopics() = AdminHtmlAction.authenticated { implicit request =>
     val body = request.body.asFormUrlEncoded.get.mapValues(_.head)
     val fromId = body.get("fromId").get.toInt
     val toId = body.get("toId").get.toInt
     val topN = body.get("topN").get.toInt
     val res = Await.result(cortex.ldaShowTopics(fromId, toId, topN), 5 seconds)
-    Ok(Json.toJson(res))
+
+    val topics = res.map{ case (id, map) =>
+      getFormatted(map)
+    }
+
+    Ok(Json.toJson(topics))
   }
 }
