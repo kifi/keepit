9d04dac726bb51b3020d8229d9db30b98b12a6a1
Jared Jacobs <jared@42go.com>
2014-04-28 10:03:59 -0700

    never suggesting anyone to invite from old extensions

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/ext/ExtNonUserSearchController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/ext/ExtNonUserSearchController.scala
@@ -2,66 +2,16 @@ package com.keepit.controllers.ext
 
 import com.google.inject.Inject
 
-import com.keepit.common.akka.SafeFuture
 import com.keepit.common.controller.{ShoeboxServiceController, BrowserExtensionController, ActionAuthenticator}
-import com.keepit.common.db.Id
-import com.keepit.common.db.slick.DBSession.RSession
-import com.keepit.common.db.slick.Database
-import com.keepit.model.{EContact, SocialUserInfo, SocialUserBasicInfo, User, InvitationRepo}
-import com.keepit.typeahead.TypeaheadHit
-import com.keepit.typeahead.abook.EContactTypeahead
-import com.keepit.typeahead.socialusers.SocialUserTypeahead
 
-import org.joda.time.DateTime
-
-import play.api.Play.current
-import play.api.libs.concurrent.Akka
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json.{Json, JsArray, JsNumber, JsObject, JsString, JsValue}
+import play.api.libs.json.JsArray
 
 class ExtNonUserSearchController @Inject() (
-  db: Database,
-  actionAuthenticator: ActionAuthenticator,
-  econtactTypeahead: EContactTypeahead,
-  socialUserTypeahead: SocialUserTypeahead,
-  invitationRepo: InvitationRepo)
+  actionAuthenticator: ActionAuthenticator)
     extends BrowserExtensionController(actionAuthenticator) with ShoeboxServiceController {
 
-  def findNonUsers(q: String, n: Int) = JsonAction.authenticatedAsync { request =>
-    val socialF = new SafeFuture({
-      socialUserTypeahead.asyncSearch(request.userId, q)(TypeaheadHit.defaultOrdering[SocialUserBasicInfo])
-    }) map { _ getOrElse Seq.empty[SocialUserBasicInfo] }
-    val contactF = new SafeFuture ({
-      econtactTypeahead.asyncSearch(request.userId, q)(TypeaheadHit.defaultOrdering[EContact])
-    }) map { _ getOrElse Seq.empty[EContact] }
-    for {
-      socialRes <- socialF
-      contactRes <- contactF
-    } yield {
-      val socialResN = socialRes.filterNot(_.userId.isDefined).take(n);
-      val contactResN = contactRes.filterNot(_.contactUserId.isDefined).take(n - socialResN.size)
-      val (sInvitedAt, cInvitedAt) = db.readOnly { implicit session =>
-        (invitationRepo.getLastInvitedAtBySenderIdAndRecipientSocialUserIds(request.userId, socialResN.map(_.id)),
-         invitationRepo.getLastInvitedAtBySenderIdAndRecipientEContactIds(request.userId, contactResN.map(_.id.get)))
-      }
-      Ok(serializeResults(socialResN, contactResN, sInvitedAt, cInvitedAt))
-    }
-  }
-
-  def serializeResults(
-      infos: Seq[SocialUserBasicInfo],
-      contacts: Seq[EContact],
-      infosInvitedAt: Map[Id[SocialUserInfo], DateTime],
-      contactsInvitedAt: Map[Id[EContact], DateTime]): JsArray = {
-    JsArray(infos.map {i => JsObject(Seq[(String, JsValue)](
-      "name" -> JsString(i.fullName),
-      "id" -> JsString(s"${i.networkType}/${i.socialId.id}")) ++
-      infosInvitedAt.get(i.id).map {t => "invited" -> JsNumber(t.getMillis)} ++
-      i.getPictureUrl(56, 56).map {url => "pic" -> JsString(url)})}) ++
-    JsArray(contacts.map {c => JsObject(Seq[(String, JsValue)](
-      "email" -> JsString(c.email)) ++
-      c.name.map {name => "name" -> JsString(name)} ++
-      contactsInvitedAt.get(c.id.get).map {t => "invited" -> JsNumber(t.getMillis)})})
+  def findNonUsersToInvite(q: String, n: Int) = JsonAction.authenticated { request =>
+    Ok(JsArray())
   }
 
 }
