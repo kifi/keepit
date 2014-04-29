f50319f89b762b4f35700b92c1292acea70b3c57
Martin Raison <martin@42go.com>
2014-04-29 16:48:22 -0700

    Adding title to URISummary (+ endpoint fix)

--- kifi-backend/modules/common/app/com/keepit/model/PageInfo.scala
+++ kifi-backend/modules/common/app/com/keepit/model/PageInfo.scala
@@ -197,10 +197,11 @@ object URISummaryRequest {
     )(URISummaryRequest.apply _, unlift(URISummaryRequest.unapply))
 }
 
-case class URISummary(imageUrl: Option[String] = None, description: Option[String] = None)
+case class URISummary(imageUrl: Option[String] = None, title: Option[String] = None, description: Option[String] = None)
 object URISummary {
   implicit val format: Format[URISummary] = (
     (__ \ 'imageUrl).formatNullable[String] and
+    (__ \ 'title).formatNullable[String] and
     (__ \ 'description).formatNullable[String]
     )(URISummary.apply _, unlift(URISummary.unapply))
 }
