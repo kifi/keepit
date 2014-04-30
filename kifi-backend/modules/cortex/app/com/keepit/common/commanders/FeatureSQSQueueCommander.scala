74c7807e0d22a592b99b332417d563e5ed3588f1
yingjieMiao <yingjie@42go.com>
2014-04-29 18:58:11 -0700

    fix if else

--- kifi-backend/modules/cortex/app/com/keepit/common/commanders/FeatureSQSQueueCommander.scala
+++ kifi-backend/modules/cortex/app/com/keepit/common/commanders/FeatureSQSQueueCommander.scala
@@ -13,14 +13,15 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import com.keepit.graph.manager.LDAURITopicGraphUpdate
 import com.keepit.cortex.CortexVersionedSequenceNumber
 import com.keepit.cortex._
+import com.keepit.common.logging.Logging
 
 
 @Singleton
 class FeatureSQSQueueCommander(
   basicAWSCreds: BasicAWSCredentials,
   featureCommander: FeatureRetrievalCommander
-){
-  val DEFAULT_PUSH_SIZE = 500
+) extends Logging {
+  val DEFAULT_PUSH_SIZE = 100
   private val sqsClient = SimpleSQSClient(basicAWSCreds, Regions.US_WEST_1, buffered = false)
 
   private def getLDAURIUpateMessgages(lowSeq: SequenceNumber[NormalizedURI], version: ModelVersion[DenseLDA]): Seq[LDAURITopicGraphUpdate] = {
@@ -34,12 +35,18 @@ class FeatureSQSQueueCommander(
   def graphLDAURIFeatureUpdate(lowSeq: CortexVersionedSequenceNumber[NormalizedURI], queueName: QueueName): Unit = {
     val queue = sqsClient.formatted[LDAURITopicGraphUpdate](queueName)
     val (seq, version) = (SequenceNumber[NormalizedURI](lowSeq.seq), ModelVersion[DenseLDA](lowSeq.version))
-    val msgs = getLDAURIUpateMessgages(seq, version)
 
-    if (msgs.isEmpty && version.version < GraphUpdateConfigs.updateUpToLDAVersion.version){
-      // old version consumed up. If cortex is ready to push next version, do it.
-      val msgs2 = getLDAURIUpateMessgages(SequenceNumber[NormalizedURI](-1L), GraphUpdateConfigs.updateUpToLDAVersion)
-      msgs2.foreach{queue.send(_)}
+    log.info(s"start pulling features from seq = ${seq}, version = ${version}")
+    val t = System.currentTimeMillis()
+    val msgs = getLDAURIUpateMessgages(seq, version)
+    log.info(s"get ${msgs.size} msgs in ${(System.currentTimeMillis - t)/1000f} seconds")
+
+    if (msgs.isEmpty){
+      if (version.version < GraphUpdateConfigs.updateUpToLDAVersion.version){
+        log.info(s"old version ${version.version} feature has exhausted. fetching new version ${GraphUpdateConfigs.updateUpToLDAVersion.version}")
+        val msgs2 = getLDAURIUpateMessgages(SequenceNumber[NormalizedURI](-1L), GraphUpdateConfigs.updateUpToLDAVersion)
+        msgs2.foreach{queue.send(_)}
+      }
     } else {
       msgs.foreach{queue.send(_)}
     }
