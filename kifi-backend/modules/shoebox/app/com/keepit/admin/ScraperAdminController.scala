7daba0446492487176ea2c460197f69f3f2cb008
Léo Grimaldi <leo@42go.com>
2014-04-29 21:32:44 -0700

    Display article page even if scrape has failed

--- kifi-backend/modules/shoebox/app/com/keepit/admin/ScraperAdminController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/admin/ScraperAdminController.scala
@@ -66,24 +66,9 @@ class ScraperAdminController @Inject() (
   }
 
   def getScraped(id: Id[NormalizedURI]) = AdminHtmlAction.authenticated { implicit request =>
-    def errorMsg(id: Id[NormalizedURI]) = {
-      val uri = db.readOnly{ implicit s =>
-        normalizedURIRepo.get(id)
-      }
-      Ok(s"Oops, this page was not scraped.\ntitle = ${uri.title.getOrElse("N/A")}\nurl = ${uri.url}")
-    }
-
-    articleStore.get(id) match {
-      case Some(article) => {
-        db.readOnly { implicit s =>
-          (normalizedURIRepo.get(article.id), scrapeInfoRepo.getByUriId(article.id))
-        } match {
-          case (uri, Some(info)) => Ok(html.admin.article(article, uri, info))
-          case (uri, None) => errorMsg(id)
-        }
-      }
-      case None => errorMsg(id)
-    }
+    val articleOption = articleStore.get(id)
+    val (uri, scrapeInfoOption) = db.readOnly { implicit s => (normalizedURIRepo.get(id), scrapeInfoRepo.getByUriId(id)) }
+    Ok(html.admin.article(articleOption, uri, scrapeInfoOption))
   }
 
   def getProxies = AdminHtmlAction.authenticated { implicit request =>
