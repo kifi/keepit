b31509f862bb0a089e73bd170c58c21afd35a723
Raymond Ng <ray@42go.com>
2014-04-29 18:14:35 -0700

    helprank/data -- stop tracking clicks for now

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/internal/ShoeboxController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/internal/ShoeboxController.scala
@@ -613,19 +613,9 @@ class ShoeboxController @Inject() (
     db.readWrite { implicit session =>
       if (keepers.isEmpty) userBookmarkClicksRepo.increaseCounts(clicker, uriId, true)
       else {
-        val randomUUID = ExternalId[ArticleSearchResult]()
         keepers.foreach { extId =>
           val keeperId: Id[User] = userRepo.get(extId).id.get
           userBookmarkClicksRepo.increaseCounts(keeperId, uriId, false)
-          keepRepo.getByUriAndUser(uriId, keeperId) match {
-            case None =>
-              log.warn(s"[clickAttribution($clicker, $uriId, ${keepers.mkString(",")})] keep not found for keeperId=${keeperId}")
-              // moving on
-            case Some(keep) =>
-              val keepClicks = KeepClick(searchUUID = randomUUID, numKeepers = keepers.length, keeperId = keeperId, keepId = keep.id.get, uriId = uriId, clickerId = clicker)
-              log.info(s"[clickAttribution($clicker, $uriId, ${keepers.mkString(",")})] saving $keepClicks")
-              keepClicksRepo.save(keepClicks)
-          }
         }
       }
     }
