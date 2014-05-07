969e960ab59c9eab0e76879f685525ab0b887792
Léo Grimaldi <leo@42go.com>
2014-05-06 19:02:44 -0700

    Provides a FakeGraphServiceClientImpl in TestGraphServiceClientModule

---
+++ kifi-backend/modules/common/test/com/keepit/graph/FakeGraphServiceClientImpl.scala
@@ -0,0 +1,17 @@
+package com.keepit.graph
+
+import scala.concurrent.Future
+import com.keepit.common.amazon.AmazonInstanceId
+import com.keepit.graph.manager.{GraphUpdaterState, PrettyGraphStatistics}
+import com.keepit.common.zookeeper.ServiceCluster
+import com.keepit.common.net.HttpClient
+import com.keepit.common.healthcheck.AirbrakeNotifier
+
+class FakeGraphServiceClientImpl(
+  override val serviceCluster: ServiceCluster,
+  override val httpClient: HttpClient,
+  val airbrakeNotifier: AirbrakeNotifier
+) extends GraphServiceClient {
+  def getGraphStatistics(): Future[Map[AmazonInstanceId, PrettyGraphStatistics]] = Future.successful(Map.empty)
+  def getGraphUpdaterStates(): Future[Map[AmazonInstanceId, GraphUpdaterState]] = Future.successful(Map.empty)
+}
