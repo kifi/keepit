eb1434ca7719bb303a22a6e63eab4fb96bc794a8
Martin Raison <martin@42go.com>
2014-05-12 11:47:18 -0700

    addressing feedback + fixes

--- packrat/scripts/iframes/view_email.js
+++ packrat/scripts/iframes/view_email.js
@@ -8,19 +8,18 @@ if (/^Mac/.test(navigator.platform)) {
   var msgId = location.hash.match(/#(.+&)*msgId=([^&]+)(&|$)/)[2];
 
   var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (request.readyState == XMLHttpRequest.DONE) {
-      if (request.status == 200) {
-        document.open();
-        document.write(request.responseText);
-        document.close();
-      } else {
-        document.querySelector('.kifi-waiting').style.display = 'none';
-        document.querySelector('.kifi-error').style.display = 'block';
-      }
+  request.onloadend = function() {
+    if (request.status == 200) {
+      document.open();
+      document.write(request.responseText);
+      document.close();
+    } else {
+      document.querySelector('.kifi-waiting').style.display = 'none';
+      document.querySelector('.kifi-error').style.display = 'block';
     }
   }
+  request.withCredentials = true;
 
-  request.open('GET', '/eliza/emailPreview/' + msgId, true);
+  request.open('GET', 'https://eliza.kifi.com/eliza/emailPreview/' + msgId, true);
   request.send();
 }());
--- packrat/scripts/thread.js
+++ packrat/scripts/thread.js
@@ -225,16 +225,14 @@ panes.thread = function () {
       messageTip: 'message_tip'
     };
     if (m.auxData && m.auxData.length) {
-      var $rendered = $(render('html/keeper/message_aux', m, templates));
-      $rendered.find('.kifi-message-email-view').each(function() {
-        $(this).on('click', function () {
+      var $rendered = $(render('html/keeper/message_aux', m, templates))
+        .on('click', '.kifi-message-email-view', function() {
           api.require('scripts/iframe_dialog.js', function() {
             api.port.emit('auth_info', function (info) {
               iframeDialog.toggle('viewEmail', info.origin, {msgId: m.id});
             });
           });
         });
-      });
     } else {
       var $rendered = $(render('html/keeper/message_discussion', m, templates));
     }
