770b66acac401ed81d185a80792940a1c6737051
Léo Grimaldi <leo@42go.com>
2014-05-13 13:53:43 -0700

    Have GraphStatistics about degrees

--- kifi-backend/modules/common/app/com/keepit/graph/manager/PrettyGraphStatistics.scala
+++ kifi-backend/modules/common/app/com/keepit/graph/manager/PrettyGraphStatistics.scala
@@ -5,7 +5,7 @@ import play.api.libs.json.JsArray
 import play.api.libs.json.JsNumber
 import play.api.libs.json.JsString
 
-case class PrettyGraphStatistics(vertexStatistics: Map[String, Long], edgeStatistics: Map[(String, String, String), Long])
+case class PrettyGraphStatistics(vertexStatistics: Map[String, (Long, Double, Double)], edgeStatistics: Map[(String, String, String), (Long, Double, Double)])
 
 object PrettyGraphStatistics {
 
@@ -14,11 +14,13 @@ object PrettyGraphStatistics {
     def reads(json: JsValue): JsResult[PrettyGraphStatistics] = for {
 
       vertexStatistics <- (json \ "vertexStatistics").validate[JsArray].map(_.value.sliding(2,2).map {
-        case Seq(JsString(vertexKind), JsNumber(count)) => (vertexKind -> count.toLong)
+        case Seq(JsString(vertexKind), JsNumber(count), JsNumber(outgoingDegree), JsNumber(incomingDegree)) =>
+          (vertexKind -> (count.toLong, outgoingDegree.toDouble, incomingDegree.toDouble))
       }.toMap)
 
       edgeStatistics <- (json \ "edgeStatistics").validate[JsArray].map(_.value.sliding(4,4).map {
-        case Seq(JsString(sourceKind), JsString(destinationKind), JsString(edgeKind), JsNumber(count)) => ((sourceKind, destinationKind, edgeKind) -> count.toLong)
+        case Seq(JsString(sourceKind), JsString(destinationKind), JsString(edgeKind), JsNumber(count), JsNumber(outgoingDegree), JsNumber(incomingDegree)) =>
+          ((sourceKind, destinationKind, edgeKind) -> (count.toLong, outgoingDegree.toDouble, incomingDegree.toDouble))
       }.toMap)
 
     } yield PrettyGraphStatistics(vertexStatistics, edgeStatistics)
@@ -26,12 +28,12 @@ object PrettyGraphStatistics {
 
     def writes(statistics: PrettyGraphStatistics): JsValue = {
 
-      val vertexStatistics = JsArray(statistics.vertexStatistics.flatMap { case (vertexKind, count) =>
-        Seq(JsString(vertexKind), JsNumber(count))
+      val vertexStatistics = JsArray(statistics.vertexStatistics.flatMap { case (vertexKind, (count, degree)) =>
+        Seq(JsString(vertexKind), JsNumber(count), JsNumber(degree))
       }.toSeq)
 
-      val edgeStatistics = JsArray(statistics.edgeStatistics.flatMap { case ((sourceKind, destinationKind, edgeKind), count) =>
-        Seq(JsString(sourceKind), JsString(destinationKind), JsString(edgeKind), JsNumber(count))
+      val edgeStatistics = JsArray(statistics.edgeStatistics.flatMap { case ((sourceKind, destinationKind, edgeKind), (count, sourceDegree, destinationDegree)) =>
+        Seq(JsString(sourceKind), JsString(destinationKind), JsString(edgeKind), JsNumber(count), JsNumber(sourceDegree), JsNumber(destinationDegree))
       }.toSeq)
 
       Json.obj(
