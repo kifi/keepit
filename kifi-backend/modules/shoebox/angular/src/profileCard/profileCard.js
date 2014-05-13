4c3ba5b001d49f7fbcb4b019d81da4218c058cb5
Jared Jacobs <jared@42go.com>
2014-05-12 17:21:56 -0700

    removing user name+desc, moving help rank stuff to right column

--- kifi-backend/modules/shoebox/angular/src/profileCard/profileCard.js
+++ kifi-backend/modules/shoebox/angular/src/profileCard/profileCard.js
@@ -3,40 +3,16 @@
 angular.module('kifi.profileCard', ['kifi.profileService'])
 
 .directive('kfProfileCard', [
-  'profileService', '$analytics',
-  function (profileService, $analytics) {
+  'profileService',
+  function (profileService) {
     return {
       replace: true,
       restrict: 'A',
       templateUrl: 'profileCard/profileCard.tpl.html',
-      link: function (scope /*, element, attrs*/ ) {
+      link: function (scope) {
         scope.me = profileService.me;
 
         profileService.getMe();
-
-        scope.data = scope.data || {};
-        scope.openHelpRankHelp = function () {
-          scope.data.showHelpRankHelp = true;
-          $analytics.eventTrack('user_viewed_page', {
-            'type': 'HelpRankHelp'
-          });
-        };
-
-        scope.yesLikeHelpRank = function () {
-          scope.data.showHelpRankHelp = false;
-          $analytics.eventTrack('user_clicked_page', {
-            'action': 'yesLikeHelpRank'
-          });
-        };
-
-        scope.noLikeHelpRank = function () {
-          scope.data.showHelpRankHelp = false;
-          $analytics.eventTrack('user_clicked_page', {
-            'action': 'noLikeHelpRank'
-          });
-        };
-
-
       }
     };
   }
