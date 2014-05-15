3842b202c4efe5858830f9842ee6188f111808e5
eishay <eishay@gmail.com>
2014-05-14 17:26:26 -0700

    tmp work

--- kifi-backend/modules/common/app/com/keepit/common/cache/Key.scala
+++ kifi-backend/modules/common/app/com/keepit/common/cache/Key.scala
@@ -4,6 +4,7 @@ trait Key[T] {
   val namespace: String
   val version: Int = 1
   def toKey(): String
-  override final def toString: String = namespace + "%" + version + "#" + toKey()
+  private def escapedKey()
+  override final def toString: String = namespace + "%" + version + "#" + escapedKey()
 }
 
