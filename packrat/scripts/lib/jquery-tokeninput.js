8c5b3d2f912fba243edbc65538398893169c19b5
Martin Raison <martin@42go.com>
2014-05-16 15:06:19 -0700

    enter to send

--- packrat/scripts/lib/jquery-tokeninput.js
+++ packrat/scripts/lib/jquery-tokeninput.js
@@ -255,11 +255,13 @@
           case KEY.NUMPAD_ENTER:
             if (selectedDropdownItem) {
               handleItemChosen(selectedDropdownItem);
+              return false;
             } else if (this.value) {
               this.value = '';
               handleQueryChange();
+              return false;
             }
-            return false;
+            return true;
 
           case KEY.ESC:
             hideDropdown();
