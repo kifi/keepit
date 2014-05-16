3d0ffb2feb045ee732bb584288c854c28d1473ef
Léo Grimaldi <leo@42go.com>
2014-05-16 14:19:36 -0700

    Fix test

--- kifi-backend/modules/common/conf/evolutions/shoebox/173.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/173.sql
@@ -3,7 +3,7 @@
 # --- !Ups
 
 CREATE INDEX non_user_thread_i_last_notified_at_thread_updated_at ON non_user_thread (last_notified_at, thread_updated_at);
-DROP INDEX non_user_thread_i_last_notified_at ON non_user_thread
+-- DROP INDEX non_user_thread_i_last_notified_at ON non_user_thread
 
 INSERT INTO evolutions (name, description) VALUES ('173.sql', 'add index on last notified at and thread updated at in non_user_thread table');
 
