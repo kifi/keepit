2c6d6c31425ad875dafa08a472f1a524ded74bc8
eishay <eishay@gmail.com>
2014-05-18 10:56:34 -0700

    no nulls

--- kifi-backend/modules/common/app/com/keepit/model/serialize/MsgPackSequenceNumberTemplate.scala
+++ kifi-backend/modules/common/app/com/keepit/model/serialize/MsgPackSequenceNumberTemplate.scala
@@ -5,25 +5,14 @@ import com.keepit.common.db.SequenceNumber
 import org.msgpack.template._
 import org.msgpack.unpacker.Unpacker
 import org.msgpack.packer.Packer
-import org.msgpack.MessageTypeException
 
 class MsgPackSequenceNumberTemplate[T] extends AbstractTemplate[SequenceNumber[T]]{
   def write(packer: Packer, seq: SequenceNumber[T], required: Boolean): Unit = {
-    if(seq == null){
-      if(required){
-        throw new MessageTypeException("Attempted to write null")
-      }
-      packer.writeNil()
-    }else{
-      packer.write(seq.value)
-    }
+    if(seq == null) throw new NullPointerException("can't write a null seq, Only Option[Seq[T]] may have a not required value")
+    packer.write(seq.value)
   }
 
-
   def read(unpacker: Unpacker, to: SequenceNumber[T], required: Boolean): SequenceNumber[T] = {
-    if(!required && unpacker.trySkipNil){
-      return null
-    }
     SequenceNumber[T](unpacker.readLong())
   }
 }
