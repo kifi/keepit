415eb070a76d3c1aa8bacf4952b3a14dbd3f50f8
eishay <eishay@gmail.com>
2014-05-18 10:34:52 -0700

    no to nulls!

--- kifi-backend/modules/common/app/com/keepit/common/db/Id.scala
+++ kifi-backend/modules/common/app/com/keepit/common/db/Id.scala
@@ -2,8 +2,8 @@ package com.keepit.common.db
 
 import play.api.libs.json._
 import play.api.mvc.{PathBindable, QueryStringBindable}
-import org.msgpack.{MessagePack, ScalaMessagePack}
-import com.keepit.model.serialize.MsgPackIdTemplate
+import org.msgpack.ScalaMessagePack
+import com.keepit.model.serialize.{MsgPackOptIdTemplate, MsgPackIdTemplate}
 
 case class Id[T](id: Long) { //use "extends AnyVal" at Scala 2.11.0 https://issues.scala-lang.org/browse/SI-6260
   override def toString = id.toString
@@ -12,6 +12,7 @@ case class Id[T](id: Long) { //use "extends AnyVal" at Scala 2.11.0 https://issu
 object Id {
 
   ScalaMessagePack.messagePack.register(classOf[Id[Any]], new MsgPackIdTemplate[Any]())
+  ScalaMessagePack.messagePack.register(classOf[Option[Id[Any]]], new MsgPackOptIdTemplate[Any]())
 
   implicit def format[T]: Format[Id[T]] =
     Format(__.read[Long].map(Id(_)), new Writes[Id[T]]{ def writes(o: Id[T]) = JsNumber(o.id) })
--- kifi-backend/modules/common/app/com/keepit/model/serialize/MsgPackIdTemplate.scala
+++ kifi-backend/modules/common/app/com/keepit/model/serialize/MsgPackIdTemplate.scala
@@ -9,21 +9,27 @@ import org.msgpack.MessageTypeException
 
 class MsgPackIdTemplate[T] extends AbstractTemplate[Id[T]]{
   def write(packer: Packer, id: Id[T], required: Boolean): Unit = {
-    if(id == null){
-      if(required){
-        throw new MessageTypeException("Attempted to write null")
-      }
-      packer.writeNil()
-    }else{
-      packer.write(id.id)
-    }
+    if(id == null) throw new NullPointerException("can't write a null id, Only Option[Id[T]] may have a not required value")
+    packer.write(id.id)
   }
 
 
   def read(unpacker: Unpacker, to: Id[T], required: Boolean): Id[T] = {
-    if(!required && unpacker.trySkipNil){
-      return null
-    }
     Id[T](unpacker.readLong())
+  }
+}
+
+class MsgPackOptIdTemplate[T] extends AbstractTemplate[Option[Id[T]]] {
+  def write(packer: Packer, idOpt: Option[Id[T]], required: Boolean): Unit = {
+    if(idOpt == null || idOpt.isEmpty){
+      if (required) throw new MessageTypeException("Attempted to write null")
+      packer.writeNil()
+    } else {
+      packer.write(idOpt.get.id)
+    }
+  }
+
+  def read(unpacker: Unpacker, to: Option[Id[T]], required: Boolean): Option[Id[T]] = {
+    if (!required && unpacker.trySkipNil) None else Some(Id[T](unpacker.readLong()))
   }
 }
