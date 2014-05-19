de5cca66d378f7748fa3792d344547ba69ae51cf
Léo Grimaldi <leo@42go.com>
2014-05-19 16:33:49 -0700

    Use a TravelJournal instead of doing specific bookkeeping in Wanderer

--- kifi-backend/modules/graph/app/com/keepit/graph/wander/Wanderer.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/wander/Wanderer.scala
@@ -1,46 +1,56 @@
 package com.keepit.graph.wander
 
-import com.keepit.graph.model.{VertexId, GlobalVertexReader}
+import com.keepit.graph.model._
 import scala.collection.mutable
 import com.keepit.common.math.ProbabilityDensity
+import scala.Some
 
 trait Wanderer {
-  def wander(steps: Int, teleport: Teleporter, resolve: EdgeWeightResolver): Map[VertexId, Int]
+  def wander(steps: Int, teleport: Teleporter, resolve: EdgeWeightResolver, journal: TravelJournal): Unit
 }
 
 class ScoutingWanderer(wanderer: GlobalVertexReader, scout: GlobalVertexReader) {
-  def wander(steps: Int, teleport: Teleporter, resolve: EdgeWeightResolver): Map[VertexId, Int] = {
-    wanderer.moveTo(teleport.surely)
-    val reached = mutable.Map[VertexId, Int]().withDefaultValue(0)
+  def wander(steps: Int, teleporter: Teleporter, resolver: EdgeWeightResolver, journal: TravelJournal): Unit = {
+
+    def teleportTo(destination: VertexId): Unit = {
+      scout.moveTo(destination)
+      journal.onTeleportation(wanderer, scout)
+      wanderer.moveTo(destination)
+    }
+
+    def traverseTo(destination: VertexId, edgeKind: EdgeKind[_ <: EdgeDataReader]): Unit = {
+      scout.moveTo(destination)
+      journal.onEdgeTraversal(wanderer, scout, edgeKind)
+      wanderer.moveTo(destination)
+    }
+
+    wanderer.moveTo(teleporter.surely)
     var step = 0
     while (step < steps) {
-      teleport.maybe(wanderer) match {
-        case Some(newStart) =>
-          reached(wanderer.id) = reached(wanderer.id) + 1
-          wanderer.moveTo(newStart)
+      teleporter.maybe(wanderer) match {
+        case Some(newStart) => teleportTo(newStart)
         case None => {
-          var weights = mutable.MutableList[(VertexId, Double)]()
+          var weights = mutable.MutableList[((VertexId, EdgeKind[_ <:EdgeDataReader]), Double)]()
 
           while (weights.length < wanderer.edgeReader.degree) {
             wanderer.edgeReader.moveToNextEdge()
             val destination = wanderer.edgeReader.destination
             scout.moveTo(destination)
-            val weight = resolve.weight(wanderer, scout, wanderer.edgeReader)
-            weights += (destination -> weight)
+            val weight = resolver.weight(wanderer, scout, wanderer.edgeReader)
+            weights += ((destination, wanderer.edgeReader.kind) -> weight)
           }
 
           val destinationProbability = ProbabilityDensity.normalized(weights)
           destinationProbability.sample(Math.random()) match {
-            case Some(nextDestination) => wanderer.moveTo(nextDestination)
+            case Some((nextDestination, edgeKind)) => traverseTo(nextDestination, edgeKind)
             case None => {  // failed to move, force teleportation
-              reached(wanderer.id) = reached(wanderer.id) + 1
-              wanderer.moveTo(teleport.surely)
+              teleportTo(teleporter.surely)
             }
           }
         }
       }
       step += 1
     }
-    reached.toMap
   }
+
 }
