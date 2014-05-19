e5662faa4ad657e0e7533c9adbfeece6f5c34846
yingjieMiao <yingjie@42go.com>
2014-05-19 14:49:51 -0700

    update wordCount cache in scheduled scraping jobs

--- kifi-backend/modules/scraper/app/com/keepit/common/cache/ScraperCacheModule.scala
+++ kifi-backend/modules/scraper/app/com/keepit/common/cache/ScraperCacheModule.scala
@@ -142,4 +142,8 @@ case class ScraperCacheModule(cachePluginModules: CachePluginModule*) extends Ca
   @Provides @Singleton
   def verifiedEmailUserIdCache(stats: CacheStatistics, accessLog: AccessLog, innerRepo: InMemoryCachePlugin, outerRepo: FortyTwoCachePlugin) =
     new VerifiedEmailUserIdCache(stats, accessLog, (outerRepo, 7 days))
+
+  @Provides @Singleton
+  def uriWordCountCache(stats: CacheStatistics, accessLog: AccessLog, innerRepo: InMemoryCachePlugin, outerRepo: FortyTwoCachePlugin) =
+    new NormalizedURIWordCountCache(stats, accessLog, (innerRepo, 5 minutes), (outerRepo, 7 days))
 }
