938c3803e5e5bb0b022d2da1ff4a6c3ac9fa7e76
Léo Grimaldi <leo@42go.com>
2014-05-20 16:48:14 -0700

    Add a wandering internal route

--- kifi-backend/modules/graph/app/com/keepit/graph/controllers/internal/GraphController.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/controllers/internal/GraphController.scala
@@ -6,13 +6,19 @@ import com.keepit.common.logging.Logging
 import com.keepit.graph.manager.{GraphUpdaterState, GraphStatistics, GraphManager}
 import play.api.mvc.Action
 import play.api.libs.json._
+import com.keepit.graph.wander.{Wanderlust, WanderingCommander}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 class GraphController @Inject() (
-  graphManager: GraphManager
+  graphManager: GraphManager,
+  wanderingCommander: WanderingCommander
 ) extends GraphServiceController with Logging {
 
-  def discover() = Action { request =>
-    Ok
+  def wander() = SafeAsyncAction(parse.json) { request =>
+    val wanderlust = request.body.as[Wanderlust]
+    val collisions = wanderingCommander.wander(wanderlust)
+    val json = Json.toJson(collisions)
+    Ok(json)
   }
 
   def getGraphStatistics() = Action { request =>
