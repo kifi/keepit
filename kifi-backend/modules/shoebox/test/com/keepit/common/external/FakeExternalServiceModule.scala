f4f650b513742e2318abc3362d8046fd1913b132
yingjieMiao <yingjie@42go.com>
2014-05-21 14:14:14 -0700

    completely remove embedly from shoebox

--- kifi-backend/modules/shoebox/test/com/keepit/common/external/FakeExternalServiceModule.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/common/external/FakeExternalServiceModule.scala
@@ -1,16 +1,11 @@
 package com.keepit.common.external
 
-import com.keepit.common.embedly.{DevEmbedlyClient, EmbedlyClient}
 import com.keepit.common.pagepeeker.{DevPagePeekerClient, PagePeekerClient}
 import com.google.inject.{Provides, Singleton}
 
 case class FakeExternalServiceModule() extends ExternalServiceModule {
 
   def configure() {}
-
-  @Provides
-  @Singleton
-  def embedlyClient: EmbedlyClient = new DevEmbedlyClient()
 
   @Provides
   @Singleton
