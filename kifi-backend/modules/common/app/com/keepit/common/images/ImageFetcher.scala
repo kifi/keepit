e405aa140fff1a60a62a01bb00690593ef119549
eishay <eishay@gmail.com>
2014-05-21 21:00:08 -0700

    property catching CertificateExpiredException, ignoring site and not reporting airbrake on it (log is enough since its a user error)

--- kifi-backend/modules/common/app/com/keepit/common/images/ImageFetcher.scala
+++ kifi-backend/modules/common/app/com/keepit/common/images/ImageFetcher.scala
@@ -11,7 +11,8 @@ import play.api.http.Status
 import com.keepit.common.logging.Logging
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import com.google.inject.{Inject, Singleton, ImplementedBy}
-import com.keepit.common.healthcheck.AirbrakeNotifier
+import com.keepit.common.healthcheck.{StackTrace, AirbrakeNotifier}
+import java.security.cert.CertificateExpiredException
 
 @ImplementedBy(classOf[ImageFetcherImpl])
 trait ImageFetcher {
@@ -32,26 +33,31 @@ class ImageFetcherImpl @Inject() (airbrake: AirbrakeNotifier) extends ImageFetch
   private def getBufferedImage(is:InputStream) = Try { ImageUtils.forceRGB(ImageIO.read(is)) }
 
   override def fetchRawImage(url: String): Future[Option[BufferedImage]] = {
+    val trace = new StackTrace()
     WS.url(url).withRequestTimeout(120000).get map { resp =>
-      log.info(s"[fetchRawImage(${url})] resp=${resp.statusText}")
+      log.info(s"[fetchRawImage($url)] resp=${resp.statusText}")
       resp.status match {
         case Status.OK =>
           withInputStream(resp.getAHCResponse.getResponseBodyAsStream) { is =>
             getBufferedImage(is) match {
               case Failure(ex) =>
-                log.error(s"Failed to process image: (${url})")
+                log.error(s"Failed to process image: ($url)")
                 None
               case Success(rawImage) =>
                 Some(rawImage)
             }
           }
         case _ =>
-          log.error(s"[fetchRawImage(${url})] Failed to retrieve image. Response: ${resp.statusText}")
+          log.error(s"[fetchRawImage($url)] Failed to retrieve image. Response: ${resp.statusText}")
           None
       }
     } recover {
+      case cee: CertificateExpiredException => {
+        log.warn(s"Security concern when fetching image with url $url, since we don't trust the site we'll ignore its content", trace.withCause(cee))
+        None
+      }
       case t: Throwable => {
-        airbrake.notify(s"Error fetching image with url $url", t)
+        airbrake.notify(s"Error fetching image with url $url", trace.withCause(t))
         None
       }
     }
