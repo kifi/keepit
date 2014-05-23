b768cfe1e940cc172094a2c16b27aad485287d63
Léo Grimaldi <leo@42go.com>
2014-05-23 15:31:26 -0700

    Return formatted social users in the admin page and allow for exclusion of first degree results.

--- kifi-backend/modules/common/app/com/keepit/graph/wander/Wanderlust.scala
+++ kifi-backend/modules/common/app/com/keepit/graph/wander/Wanderlust.scala
@@ -1,13 +1,14 @@
 package com.keepit.graph.wander
 
 import com.keepit.common.db.Id
-import com.keepit.model.{NormalizedURI, User}
+import com.keepit.model.{SocialUserInfo, NormalizedURI, User}
 import play.api.libs.json._
 
 case class Wanderlust(
   startingVertexKind: String,
   startingVertexDataId: Long,
   preferredCollisions: Set[String] = Set.empty,
+  allowFirstDegreeCollisions: Boolean = false,
   steps: Int = 10000,
   restartProbability: Double = 0.15
 )
@@ -16,7 +17,7 @@ object Wanderlust {
   implicit val format = Json.format[Wanderlust]
 }
 
-case class Collisions(users: Map[Id[User], Int], uris: Map[Id[NormalizedURI], Int], extra: Map[String, Int])
+case class Collisions(users: Map[Id[User], Int], socialUsers: Map[Id[SocialUserInfo], Int], uris: Map[Id[NormalizedURI], Int], extra: Map[String, Int])
 
 object Collisions {
   private implicit def idMapFormat[T] = new Format[Map[Id[T], Int]] {
@@ -26,5 +27,5 @@ object Collisions {
 
   implicit val format = Json.format[Collisions]
 
-  val empty = Collisions(Map.empty, Map.empty, Map.empty)
+  val empty = Collisions(Map.empty, Map.empty, Map.empty, Map.empty)
 }
--- kifi-backend/modules/graph/app/com/keepit/graph/wander/WanderingCommander.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/wander/WanderingCommander.scala
@@ -3,7 +3,7 @@ package com.keepit.graph.wander
 import com.google.inject.{Singleton, Inject}
 import com.keepit.graph.manager.GraphManager
 import com.keepit.graph.model._
-import com.keepit.model.{NormalizedURI, User}
+import com.keepit.model.{SocialUserInfo, NormalizedURI, User}
 import com.keepit.common.db.Id
 import scala.collection.mutable
 import com.keepit.graph.model.VertexKind.VertexType
@@ -12,17 +12,14 @@ import com.keepit.graph.model.VertexKind.VertexType
 class WanderingCommander @Inject() (graph: GraphManager) {
 
   def wander(wanderlust: Wanderlust): Collisions = {
+
     val startingVertexKind = VertexKind.apply(wanderlust.startingVertexKind)
     val startingVertexId = VertexId(startingVertexKind)(wanderlust.startingVertexDataId)
     val preferredCollisions = wanderlust.preferredCollisions.map(VertexKind(_))
-    val rawCollisions = doWander(startingVertexId, preferredCollisions, wanderlust.steps, wanderlust.restartProbability)
-    getCollisions(rawCollisions)
-  }
-
-  private def doWander(startingVertexId: VertexId, preferredCollisions: Set[VertexType], steps: Int, restartProbability: Double): TeleportationJournal = {
+    val forbiddenCollisions = if (wanderlust.allowFirstDegreeCollisions) Set(startingVertexId) else getFirstDegreeConnections(startingVertexId) + startingVertexId
     val journal = new TeleportationJournal()
-    val teleporter = UniformTeleporter(Set(startingVertexId), restartProbability) { wanderer =>
-      wanderer.id != startingVertexId && (preferredCollisions.isEmpty || preferredCollisions.contains(wanderer.kind))
+    val teleporter = UniformTeleporter(Set(startingVertexId), wanderlust.restartProbability) { wanderer =>
+      !forbiddenCollisions.contains(wanderer.id) && (preferredCollisions.isEmpty || preferredCollisions.contains(wanderer.kind))
     }
     val resolver = RestrictedDestinationResolver { case (source, destination, edge) =>
       !journal.getLastVisited().exists(_ == destination.id)
@@ -32,20 +29,34 @@ class WanderingCommander @Inject() (graph: GraphManager) {
       val wanderer = reader.getNewVertexReader()
       val scout = reader.getNewVertexReader()
       val scoutingWanderer = new ScoutingWanderer(wanderer, scout)
-      scoutingWanderer.wander(steps, teleporter, resolver, journal)
+      scoutingWanderer.wander(wanderlust.steps, teleporter, resolver, journal)
     }
-    journal
+    getCollisions(journal)
   }
 
   private def getCollisions(journal: TeleportationJournal): Collisions = {
     val users = mutable.Map[Id[User], Int]()
+    val socialUsers = mutable.Map[Id[SocialUserInfo], Int]()
     val uris =  mutable.Map[Id[NormalizedURI], Int]()
     val extra = mutable.Map[String, Int]()
+
     journal.getTeleportations().foreach {
       case (vertexId, count) if vertexId.kind == UserReader => users += VertexDataId.toUserId(vertexId.asId[UserReader]) -> count
       case (vertexId, count) if vertexId.kind == UriReader => uris += VertexDataId.toNormalizedUriId(vertexId.asId[UriReader]) -> count
+      case (vertexId, count) if vertexId.kind == FacebookAccountReader => socialUsers += VertexDataId.fromFacebookAccountIdtoSocialUserId(vertexId.asId[FacebookAccountReader]) -> count
+      case (vertexId, count) if vertexId.kind == LinkedInAccountReader => socialUsers += VertexDataId.fromLinkedInAccountIdtoSocialUserId(vertexId.asId[LinkedInAccountReader]) -> count
       case (vertexId, count) => extra += vertexId.toString() -> count
     }
-    Collisions(users.toMap, uris.toMap, extra.toMap)
+    Collisions(users.toMap, socialUsers.toMap, uris.toMap, extra.toMap)
+  }
+
+  private def getFirstDegreeConnections(vertexId: VertexId): Set[VertexId] = graph.readOnly { reader =>
+    val vertexReader = reader.getNewVertexReader()
+    vertexReader.moveTo(vertexId)
+    val firstDegreeDestinations = mutable.Set[VertexId]()
+    while (vertexReader.edgeReader.moveToNextEdge()) {
+      firstDegreeDestinations += vertexReader.edgeReader.destination
+    }
+    firstDegreeDestinations.toSet
   }
 }
