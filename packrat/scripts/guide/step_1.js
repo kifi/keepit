3a490f3b3fba5a368451ccde4f1a5ba0cf909b1d
Jared Jacobs <jared@42go.com>
2014-05-23 17:31:06 -0700

    working on supporting all of the sub-steps in keeping and tagging

--- packrat/scripts/guide/step_1.js
+++ packrat/scripts/guide/step_1.js
@@ -1,6 +1,7 @@
 // @require styles/guide/step_1.css
 // @require styles/guide/steps.css
 // @require scripts/lib/jquery.js
+// @require scripts/lib/underscore.js
 // @require scripts/keeper.js
 // @require scripts/render.js
 // @require scripts/guide/spotlight.js
@@ -9,55 +10,116 @@
 
 var guide = guide || {};
 guide.step1 = guide.step1 || function () {
-  var $stage, /*$pages,*/ $steps, spotlight, observer;
+  var $stage, $steps, spotlight, tileObserver, tagboxObserver, completed;
+  var steps = [
+    {lit: '.kifi-tile-card', pad: [20, 40]},
+    {lit: '.kifi-keep-card', pad: [0, 44], req: [0]},
+    {lit: '.kifi-keep-card', pad: [0, 44], req: [0], arrow: '.kifi-kept-tag,.kifi-keep-tag'},
+    {lit: '.kifi-tagbox', pad: [0, 10, 20], req: [0, 2]}];
+  var handlers = {
+    kept: complete.bind(null, 2),
+    add_tag: complete.bind(null, 4)
+  };
+
   return show;
 
   function show() {
     if (!$stage) {
-      spotlight = new Spotlight(wholeWindow(), {opacity: 0});
-      var ms = updateSpotlightPosition();
-      observer = new MutationObserver(updateSpotlightPosition);
-      observer.observe(tile, {childList: true});
+      completed = 0;
+      tileObserver = new MutationObserver(onTileChildChange);
+      tileObserver.observe(tile, {childList: true});
+      api.port.on(handlers);
 
-      $stage = $(render('html/guide/step_1', me))
-        .css('transition-delay', Math.max(0, ms - 200) + 'ms')
-        .appendTo('body').layout().addClass('kifi-open');
-      $steps = $(render('html/guide/steps', {showing: true}))
-        .appendTo('body');
-      // $pages = $stage.find('.kifi-guide-pages');
-      // $pages.find('.kifi-guide-next').click(next);
+      spotlight = new Spotlight(wholeWindow(), {opacity: 0});
+      $stage = $(render('html/guide/step_1', me));
+      $steps = $(render('html/guide/steps', {showing: true}));
+      var ms = updateSpotlightPosition();
+      $stage.css('transition-delay', Math.max(0, ms - 200) + 'ms');
+      $steps.appendTo('body');
+      $stage.appendTo('body').layout().addClass('kifi-open');
       $steps.find('.kifi-guide-steps-x').click(hide);
       $(document).data('esc').add(hide);
       $(window).on('resize', updateSpotlightPosition);
     }
   }
 
-  // function next() {
-  //   $pages.attr('kifi-p', '2');
-  //   $steps.addClass('kifi-showing');
-  // }
-
   function hide() {
     if ($stage) {
-      $stage.one('transitionend', remove).removeClass('kifi-open');
-      $steps.one('transitionend', remove).css('transition-delay', '').removeClass('kifi-showing');
-      spotlight.animateTo(wholeWindow(), {opacity: 0, detach: true});
+      $stage.one('transitionend', remove).css('transition-delay', '').removeClass('kifi-open');
+      $steps.one('transitionend', remove).removeClass('kifi-showing');
+      var ms = spotlight.animateTo(wholeWindow(), {opacity: 0, detach: true});
+      tileObserver.disconnect();
+      if (tagboxObserver) tagboxObserver.disconnect();
+      api.port.off(handlers);
 
-      $stage = /*$pages =*/ $steps = spotlight = observer = null;
+      $stage = $steps = spotlight = tileObserver = tagboxObserver = null;
       $(document).data('esc').remove(hide);
       $(window).off('resize', updateSpotlightPosition);
+      return ms;
+    } else {
+      return 0;
     }
   }
 
-  function updateSpotlightPosition() {
-    var btn = $('.kifi-keep-card')[0];
-    var elRect = (btn || tile).getBoundingClientRect(), hPad = btn ? 60 : 40, vPad = btn ? 30 : 20;
-    return spotlight.animateTo({
-        x: elRect.left - hPad,
-        y: elRect.top - vPad,
-        w: elRect.width + hPad * 2,
-        h: elRect.height + vPad * 2
-      }, {opacity: 1});
+  function complete(n) {
+    if (completed < n) {
+      completed = n;
+      updateSpotlightPosition();
+    }
+  }
+
+  function updateSpotlightPosition(el, r) {
+    var stepIdx = completed, pIdx = stepIdx;
+    var step = steps[stepIdx], p = step;
+    var lit = document.querySelector(step.lit);
+    if (!lit && step.req) {
+      for (var i = step.req.length; !lit && i--;) {
+        pIdx = step.req[i];
+        p = steps[pIdx];
+        lit = document.querySelector(p.lit);
+      }
+    }
+    if (lit) {
+      $stage.attr({'kifi-step': stepIdx, 'kifi-p': pIdx});
+      if (!r || lit !== el) {
+        r = lit.getBoundingClientRect();
+      }
+      console.log('animating to:', r);
+      return spotlight.animateTo({
+          x: r.left - p.pad[1],
+          y: r.top - p.pad[0],
+          w: r.width + p.pad[1] * 2,
+          h: r.height + p.pad[0] + p.pad[p.pad.length > 2 ? 2 : 0]
+        }, {opacity: 1});
+    } else {
+      return hide();
+    }
+  }
+
+  function onTileChildChange() {
+    if (completed < 1) {
+      if (tile.querySelector(steps[1].lit)) {
+        completed = 1;
+      }
+    } else if (completed < 3) {
+      var tagbox = tile.querySelector(steps[3].lit);
+      if (tagbox) {
+        completed = 3;
+        var onChange = _.debounce(onTagboxChange.bind(tagbox), 5, true);
+        tagboxObserver = new MutationObserver(onChange);
+        tagboxObserver.observe(tagbox, {attributes: true});
+        $(tagbox).on('transitionend', onChange);
+        return;
+      }
+    }
+    updateSpotlightPosition();
+  }
+
+  function onTagboxChange() {
+    var r = this.getBoundingClientRect(), cs = window.getComputedStyle(this);
+    var w = parseFloat(cs.width) + parseFloat(cs.borderLeftWidth) + parseFloat(cs.borderRightWidth);
+    var h = parseFloat(cs.height) + parseFloat(cs.borderTopWidth) + parseFloat(cs.borderBottomWidth);
+    updateSpotlightPosition(this, {left: r.right - w, top: r.bottom - h, width: w, height: h});
   }
 
   function wholeWindow() {
