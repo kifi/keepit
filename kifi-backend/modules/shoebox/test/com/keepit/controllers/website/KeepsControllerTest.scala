14d8e2cfdeebda62aad0142e032ecb75ea469fd1
Raymond Ng <raymond.ng@gmail.com>
2014-05-27 14:35:16 -0700

    test adjustment

--- kifi-backend/modules/shoebox/test/com/keepit/controllers/website/KeepsControllerTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/controllers/website/KeepsControllerTest.scala
@@ -150,8 +150,8 @@ class KeepsControllerTest extends Specification with ApplicationInjector {
               "createdAt":"${bookmark2.createdAt.toStandardTimeString}",
               "others":1,
               "keepers":[{"id":"${user2.externalId.toString}","firstName":"Eishay","lastName":"S","pictureName":"0.jpg"}],
-              "clickCount":0,
-              "rekeepCount":0,
+              "clickCount":-1,
+              "rekeepCount":-1,
               "collections":[]},
             {
               "id":"${bookmark1.externalId.toString}",
@@ -161,8 +161,8 @@ class KeepsControllerTest extends Specification with ApplicationInjector {
               "createdAt":"${bookmark1.createdAt.toStandardTimeString}",
               "others":-1,
               "keepers":[],
-              "clickCount":0,
-              "rekeepCount":0,
+              "clickCount":-1,
+              "rekeepCount":-1,
               "collections":[]}
           ]}
         """)
@@ -231,8 +231,8 @@ class KeepsControllerTest extends Specification with ApplicationInjector {
                 "createdAt":"2013-02-16T23:59:00.000Z",
                 "others":-1,
                 "keepers":[],
-                "clickCount":0,
-                "rekeepCount":0,
+                "clickCount":-1,
+                "rekeepCount":-1,
                 "collections":[]
               }
             ]
