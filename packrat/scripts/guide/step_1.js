58c903f6f6e8b317793ffb65b616cdbd0ca73c59
Jared Jacobs <jared@42go.com>
2014-05-29 21:32:59 -0700

    fading instructions between each step

--- packrat/scripts/guide/step_1.js
+++ packrat/scripts/guide/step_1.js
@@ -1,6 +1,7 @@
 // @require styles/guide/step_1.css
 // @require styles/guide/steps.css
 // @require scripts/lib/jquery.js
+// @require scripts/lib/q.min.js
 // @require scripts/lib/underscore.js
 // @require scripts/keeper.js
 // @require scripts/render.js
@@ -13,12 +14,13 @@ guide.step1 = guide.step1 || function () {
   var $stage, $p, $steps, spotlight, tileObserver, keeperObserver, tagboxObserver, stepIdx, pIdx;
   var steps = [
     {lit: '.kifi-tile-card', pad: [20, 40], arrow: {}},
-    {lit: '.kifi-keep-card', pad: [10, 44], arrow: {}, req: [0]},
-    {lit: '.kifi-keep-card', pad: [10, 44], arrow: {sel: '.kifi-kept-tag,.kifi-keep-tag', rot: 45}, req: [0]},
-    {lit: '.kifi-tagbox.kifi-in', pad: [0, 10, 20], arrow: {sel: '.kifi-tagbox-suggestion', rot: 90}, req: [0, 2]}];
+    {lit: '.kifi-keep-card', pad: [10, 44], arrow: {}},
+    {lit: '.kifi-keep-card', pad: [10, 44], arrow: {sel: '.kifi-kept-tag,.kifi-keep-tag', rot: 45}},
+    {lit: '.kifi-tagbox.kifi-in', pad: [0, 10, 20], arrow: {sel: '.kifi-tagbox-suggestion', rot: 90}},
+    {lit: '.kifi-tagbox.kifi-in', pad: [0, 10, 20]}];
   var handlers = {
-    kept: animateIntoPlace.bind(null, 2),
-    add_tag: animateIntoPlace.bind(null, 4)
+    kept: showStep.bind(null, 2),
+    add_tag: showStep.bind(null, 4)
   };
 
   return show;
@@ -30,16 +32,13 @@ guide.step1 = guide.step1 || function () {
       api.port.on(handlers);
 
       spotlight = new Spotlight(wholeWindow(), {opacity: 0});
-      $stage = $(render('html/guide/step_1', me));
+      $stage = $(render('html/guide/step_1', me)).appendTo('body');
       $p = $stage.find('.kifi-guide-p');
-      $steps = $(render('html/guide/steps', {showing: true}));
-      var ms = animateIntoPlace(0);
-      $stage.css('transition-delay', Math.max(0, ms - 200) + 'ms');
-      $steps.appendTo('body');
-      $stage.appendTo('body').layout().addClass('kifi-open');
+      $steps = $(render('html/guide/steps', {showing: true})).appendTo('body');
+      showStep(0);
       $steps.find('.kifi-guide-steps-x').click(hide);
       $(document).data('esc').add(hide);
-      $(window).on('resize.guideStep1', animateIntoPlace.bind(null, null, null, null, 1));
+      $(window).on('resize.guideStep1', showStep.bind(null, null, null, null, 1));
     }
   }
 
@@ -62,35 +61,72 @@ guide.step1 = guide.step1 || function () {
     }
   }
 
-  function animateIntoPlace(idx, el, r, ms) {
-    if (pIdx !== idx || idx == null) {
-      if (idx != null) {
-        if (stepIdx == null || stepIdx < idx) {
-          stepIdx = idx;
-        }
-        pIdx = idx;
-      }
-      var step = steps[stepIdx], p = steps[pIdx];
-      var lit = document.querySelector(p.lit);
-      if (lit) {
-        $stage.attr({'kifi-step': stepIdx, 'kifi-p': pIdx});
-        if (!r || lit !== el) {
-          r = lit.getBoundingClientRect();
-        }
-        log('[animateIntoPlace]', stepIdx, pIdx, r);
-        ms = spotlight.animateTo({
-            x: r.left - p.pad[1],
-            y: r.top - p.pad[0],
-            w: r.width + p.pad[1] * 2,
-            h: r.height + p.pad[0] + p.pad[p.pad.length > 2 ? 2 : 0]
-          }, {opacity: 1, ms: ms});
+  function showStep(idx, el, r, ms) {
+    if (pIdx !== idx) {
+      var pIdxOrig = pIdx;
+      var stepIdxOrig = stepIdx;
 
-        return ms;
-      } else {
-        log('[animateIntoPlace]', stepIdx, pIdx, 'hiding');
-        return hide();
+      var stepIdxNew = stepIdx == null || idx > stepIdx ? idx : stepIdx;
+      var pIdxNew = idx != null ? idx : pIdx;
+      var step = steps[stepIdxNew];
+      var p = steps[pIdxNew];
+      var lit = document.querySelector(p.lit);
+      if (!r || lit !== el) {
+        r = lit.getBoundingClientRect();
       }
+      log('[showStep] step:', stepIdx, '=>', stepIdxNew, 'p:', pIdx, '=>', pIdxNew, r);
+      var t0 = Date.now();
+      ms = spotlight.animateTo({
+          x: r.left - p.pad[1],
+          y: r.top - p.pad[0],
+          w: r.width + p.pad[1] * 2,
+          h: r.height + p.pad[0] + p.pad[p.pad.length > 2 ? 2 : 0]
+        }, {opacity: 1, ms: ms});
+
+      var hidePromise = stepIdxNew !== stepIdxOrig && stepIdxOrig ? hideStep() : null;
+
+      if (stepIdxOrig == null || hidePromise) {
+        Q.when(hidePromise, function () {
+          showNewStep(stepIdxNew, ms - (Date.now() - t0));
+        });
+      } else if (pIdxNew !== pIdxOrig) {
+        switchP(pIdxNew, ms);
+      }
+
+      return ms;
+      // } else {
+      //   log('[showStep]', stepIdx, pIdx, 'hiding');
+      //   return hide();
+      // }
     }
+  }
+
+  function showNewStep(idx, ms) {
+    stepIdx = pIdx = idx;
+    $stage
+      .attr({'kifi-step': stepIdx, 'kifi-p': pIdx})
+      .css('transition-delay', Math.max(0, ms - 200) + 'ms')
+      .layout().addClass('kifi-open');
+  }
+
+  function switchP(idx, ms) {
+    stepIdx = Math.max(stepIdx, idx);
+    pIdx = idx;
+    $stage
+      .attr({'kifi-step': stepIdx, 'kifi-p': pIdx})
+      // .css('transition-delay', Math.max(0, ms - 200) + 'ms')
+      // .layout().addClass('kifi-open');
+  }
+
+  function hideStep() {
+    var deferred = Q.defer();
+    $stage.on('transitionend', function end(e) {
+      if (e.target === this) {
+        $stage.off('transitionend', end).removeClass('kifi-open kifi-done');
+        deferred.resolve();
+      }
+    }).addClass('kifi-done');
+    return deferred.promise;
   }
 
   function onTileChildChange(records) {
@@ -106,7 +142,7 @@ guide.step1 = guide.step1 || function () {
       keeperObserver = new MutationObserver(onKeeperChangeDebounced);
       keeperObserver.observe(keeper, {attributes: true});
       // onKeeperChangeDebounced();
-      animateIntoPlace(1);
+      showStep(stepIdx > 1 ? 2 : 1);
     }
     // TODO: disconnect and release keeperObserver if .kifi-keeper element removed
     // TODO: disconnect and release tagboxObserver if .kifi-tagbox element removed
@@ -114,7 +150,9 @@ guide.step1 = guide.step1 || function () {
 
   function onTagboxChange(records) {
     log('[onTagboxChange]', records);
-    if (this.classList.contains('kifi-in')) {
+    if (this.classList.contains('kifi-tagged')) {
+      // TODO: inspect .kifi-tagbox-tagged-wrapper’s transition to compute new height and its transition duration
+    } else if (this.classList.contains('kifi-in')) {
       var cs = window.getComputedStyle(this);
       var w = parseFloat(cs.width) + parseFloat(cs.borderLeftWidth) + parseFloat(cs.borderRightWidth);
       var h = parseFloat(cs.height) + parseFloat(cs.borderTopWidth) + parseFloat(cs.borderBottomWidth);
@@ -122,19 +160,19 @@ guide.step1 = guide.step1 || function () {
       var ms = (~dur.indexOf('ms') ? 1 : 1000) * parseFloat(dur);  // TODO: check whether a transition is in effect before using ms
       var r = this.getBoundingClientRect();
       log('[onTagboxChange]', records, w, 'x', h, 'right:', r.right, 'bottom:', r.bottom, 'ms:', ms);
-      animateIntoPlace(3, this, {left: r.right - w, top: r.bottom - h, width: w, height: h}, ms);
+      showStep(Math.max(3, stepIdx), this, {left: r.right - w, top: r.bottom - h, width: w, height: h}, ms);
     } else {
       log('[onTagboxChange]', records);
-      animateIntoPlace(2);
+      showStep(2);
     }
   }
 
   function onKeeperChange(records) {
     log('[onKeeperChange]', records);
     if (this.classList.contains('kifi-hiding')) {
-      animateIntoPlace(0);
+      showStep(0);
     } else {
-      // animateIntoPlace();
+      // showStep();
     }
   }
 
---
+++ packrat/scripts/guide/svg_arrow.js
@@ -0,0 +1,188 @@
+
+var SvgArrow = SvgArrow || (function (window, document) {
+  'use strict';
+
+  function SvgArrow(..., opts) {
+    var ri = this.ri = rectToCircumscribe;
+    var ro = this.ro = opts.outer || {x: 0, y: 0, w: 9000, h: 9000};
+    var el = this.el = [0,0,0,0].map(div.bind(null, 'kifi-spotlight kifi-root'));
+    if ('opacity' in opts) {
+      setOpacity(el, this.opacity = sanitizeOpacity(opts.opacity));
+    }
+    position(el, ro, ri);
+    this.attach();
+  }
+
+  SvgArrow.prototype = {
+    attached: false,
+    attach: function () {
+      if (!this.attached) {
+        var f = document.createDocumentFragment();
+        this.el.forEach(f.appendChild.bind(f));
+        document.body.appendChild(f);
+        this.attached = true;
+      }
+      return this;
+    },
+    detach: function () {
+      if (this.attached) {
+        this.el.forEach(function (el) {el.remove()});
+        this.attached = false;
+      }
+      return this;
+    },
+    animateTo: function (rectToCircumscribe, opts) {
+      var o0 = this.opacity;
+      var oN = 'opacity' in opts ? sanitizeOpacity(opts.opacity) : null;
+      var ease = opts.ease || swing;
+      var r0 = this.ri;
+      var rN = rectToCircumscribe;
+      var ms = opts.ms || calcDurationMs(r0, rN);
+      var ms_1 = 1 / ms;
+      var t0 = window.performance.now();
+      var tN = t0 + ms;
+      var tick = this.tick = (function (t) {
+        if (this.tick === tick) {
+          if (t < tN) {
+            window.requestAnimationFrame(tick);
+            var alpha = t > t0 ? (t - t0) * ms_1 : 0;
+            position(this.el, this.ro, this.ri = interpolateRect(ease(alpha), r0, rN));
+            if (oN != null) {
+              setOpacity(this.el, this.opacity = o0 + (oN - o0) * alpha);
+            }
+          } else {
+            this.tick = null;
+            if (opts.detach) {
+              this.detach();
+            } else {
+              position(this.el, this.ro, this.ri = rN);
+            }
+            if (oN != null) {
+              setOpacity(this.el, this.opacity = oN);
+            }
+          }
+        }
+      }).bind(this);
+      window.requestAnimationFrame(tick);
+      return ms;
+    }
+  };
+
+  function div(className) {
+    var el = document.createElement('div');
+    el.className = className;
+    return el;
+  }
+
+  function position(el, ro, ri) {
+    var r = Math.round(Math.sqrt(ri.w * ri.w + ri.h * ri.h) / 2);
+    var cx = Math.round(ri.x + ri.w / 2);
+    var cy = Math.round(ri.y + ri.h / 2);
+    positionOne(el[0], r, cx, cy, ro.x, ro.y, ro.w, ri.y - ro.y);
+    positionOne(el[1], r, cx, cy, ro.x, ri.y, ri.x - ro.x, ri.h);
+    positionOne(el[2], r, cx, cy, ri.x + ri.w, ri.y, ro.x + ro.w - ri.x - ri.w, ri.h);
+    positionOne(el[3], r, cx, cy, ro.x, ri.y + ri.h, ro.w, ro.y + ro.h - ri.y - ri.h);
+  }
+
+  function positionOne(el, r, cx, cy, x, y, w, h) {
+    var s = el.style;
+    if (w > 0 && h > 0) {
+      s.display = '';
+      s.left = Math.round(x) + 'px';
+      s.top = Math.round(y) + 'px';
+      s.width = Math.round(w) + 'px';
+      s.height = Math.round(h) + 'px';
+      s.backgroundImage = 'radial-gradient(circle ' + (r + 9) + 'px at ' + (cx - x) + 'px ' + (cy - y) + 'px, rgba(0,0,0,0) ' + r + 'px, rgba(0,0,0,.85) ' + (r + 4) + 'px)';
+    } else {
+      s.display = 'none';
+    }
+  }
+
+  function sanitizeOpacity(val) {
+    return typeof val === 'number' ? (val < 0 ? 0 : (val > 1 ? 1 : val)) : 1;
+  }
+
+  function setOpacity(el, val) {
+    for (var i = 0; i < el.length; i++) {
+      el[i].style.opacity = val;
+    }
+  }
+
+  function calcDurationMs(r1, r2) {
+    var px = Math.max.apply(null, [r1.x - r2.x, r1.y - r2.y, r1.x + r1.w - r2.x - r2.w, r1.y + r1.h - r2.y - r2.h].map(Math.abs));
+    return 200 * Math.log((px + 80) / 60) | 0;
+  }
+
+  function interpolateRect(alpha, r1, r2) {
+    return {
+      x: r1.x + (r2.x - r1.x) * alpha,
+      y: r1.y + (r2.y - r1.y) * alpha,
+      w: r1.w + (r2.w - r1.w) * alpha,
+      h: r1.h + (r2.h - r1.h) * alpha
+    };
+  }
+
+  function swing(p) {
+    return .5 - Math.cos(p * Math.PI) / 2;
+  }
+
+  return Spotlight;
+}(window, document));
+
+
+// github.com/mbostock/d3
+// Copyright (c) 2010-2014, Michael Bostock
+// All rights reserved.
+
+function d3_interpolateNumber(a, b) {
+  b -= a = +a;
+  return function(t) { return a + b * t; };
+}
+
+function d3_interpolateString(a, b) {
+  var bi = d3_interpolate_numberA.lastIndex = d3_interpolate_numberB.lastIndex = 0, // scan index for next number in b
+      am, // current match in a
+      bm, // current match in b
+      bs, // string preceding current number in b, if any
+      i = -1, // index in s
+      s = [], // string constants and placeholders
+      q = []; // number interpolators
+
+  // Coerce inputs to strings.
+  a = a + '', b = b + '';
+
+  // Interpolate pairs of numbers in a & b.
+  while ((am = d3_interpolate_numberA.exec(a))
+      && (bm = d3_interpolate_numberB.exec(b))) {
+    if ((bs = bm.index) > bi) { // a string precedes the next number in b
+      bs = b.substring(bi, bs);
+      if (s[i]) s[i] += bs; // coalesce with previous string
+      else s[++i] = bs;
+    }
+    if ((am = am[0]) === (bm = bm[0])) { // numbers in a & b match
+      if (s[i]) s[i] += bm; // coalesce with previous string
+      else s[++i] = bm;
+    } else { // interpolate non-matching numbers
+      s[++i] = null;
+      q.push({i: i, x: d3_interpolateNumber(am, bm)});
+    }
+    bi = d3_interpolate_numberB.lastIndex;
+  }
+
+  if (bi < b.length) {
+    bs = b.substring(bi);
+    if (s[i]) s[i] += bs; // coalesce with previous string
+    else s[++i] = bs;
+  }
+
+  return s.length < 2
+      ? (q[0] ? (b = q[0].x, function(t) { return b(t) + ''; })
+      : function() { return b; })
+      : (b = q.length, function(t) {
+          for (var i = 0, o; i < b; ++i) s[(o = q[i]).i] = o.x(t);
+          return s.join('');
+        });
+}
+
+var d3_interpolate_numberA = /[-+]?(?:\d+\.?\d*|\.?\d+)(?:[eE][-+]?\d+)?/g,
+    d3_interpolate_numberB = new RegExp(d3_interpolate_numberA.source, 'g');
