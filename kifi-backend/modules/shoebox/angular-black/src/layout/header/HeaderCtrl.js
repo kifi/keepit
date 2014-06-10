5c2ac116b43d574e075745b700f23f319151d89b
Andrew Conner <git@andrewconner.org>
2014-06-10 11:56:43 -0700

    Sprite tweaks

--- kifi-backend/modules/shoebox/angular-black/src/layout/header/HeaderCtrl.js
+++ kifi-backend/modules/shoebox/angular-black/src/layout/header/HeaderCtrl.js
@@ -3,15 +3,20 @@
 angular.module('kifi.layout.header', ['kifi.profileService'])
 
 .controller('HeaderCtrl', [
-  '$scope', '$rootElement', 'profileService',
-  function ($scope, $rootElement, profileService) {
-    
+  '$scope', '$rootElement', 'profileService', '$location', 'util',
+  function ($scope, $rootElement, profileService, $location, util) {
+
     $scope.toggleMenu = function () {
       $rootElement.toggleClass('kf-sidebar-active');
     };
 
     $scope.me = profileService.me;
     profileService.getMe();
+
+    $scope.isActive = function (path) {
+      var loc = $location.path();
+      return loc === path || util.startsWith(loc, path + '/');
+    };
 
     $scope.logout = function () {
       profileService.logout();
