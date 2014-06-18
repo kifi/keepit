ceaeb04c554a521bc530f5dc4f60b78788b22cb8
Léo Grimaldi <leo@42go.com>
2014-06-18 12:44:50 +0200

    Correct evolution number

--- kifi-backend/modules/common/conf/evolutions/shoebox/176.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/176.sql
@@ -4,6 +4,6 @@
 
 ALTER TABLE user ADD COLUMN if not exists primary_email varchar(512) NULL;
 
-insert into evolutions (name, description) values('175.sql', 'add primary_email to user table');
+insert into evolutions (name, description) values('176.sql', 'add primary_email to user table');
 
 # --- !Downs
