a8f0d4ef88f2584e8ad132e84b8ac3a2e41ca2c7
Raymond Ng <ray@42go.com>
2014-06-19 13:39:14 -0700

    wire-up TestScrapeSchedulerConfigModule

--- kifi-backend/modules/shoebox/test/com/keepit/controllers/shoebox/ShoeboxControllerTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/controllers/shoebox/ShoeboxControllerTest.scala
@@ -23,7 +23,7 @@ import com.keepit.common.actor.TestActorSystemModule
 import com.keepit.common.healthcheck.FakeAirbrakeModule
 import scala.concurrent.ExecutionContext.Implicits.global
 import com.keepit.abook.TestABookServiceClientModule
-import com.keepit.scraper.{TestScraperServiceClientModule, FakeScrapeSchedulerModule}
+import com.keepit.scraper.{TestScrapeSchedulerConfigModule, TestScraperServiceClientModule, FakeScrapeSchedulerModule}
 import com.keepit.common.db.SequenceNumber
 import com.keepit.common.external.FakeExternalServiceModule
 import com.keepit.cortex.FakeCortexServiceClientModule
@@ -47,7 +47,8 @@ class ShoeboxControllerTest extends Specification with ShoeboxApplicationInjecto
     FakeScrapeSchedulerModule(),
     FakeExternalServiceModule(),
     FakeCortexServiceClientModule(),
-    TestScraperServiceClientModule()
+    TestScraperServiceClientModule(),
+    TestScrapeSchedulerConfigModule()
   )
 
   def setupSomeUsers()(implicit injector: Injector) = {
