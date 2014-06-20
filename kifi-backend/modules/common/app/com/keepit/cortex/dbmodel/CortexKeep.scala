c39a9172ec6d2eea1928aa407f0ffcbba36c7495
yingjieMiao <yingjie@42go.com>
2014-06-19 18:14:40 -0700

    cortex keep

---
+++ kifi-backend/modules/common/app/com/keepit/cortex/dbmodel/CortexKeep.scala
@@ -0,0 +1,43 @@
+package com.keepit.cortex.dbmodel
+
+import org.joda.time.DateTime
+import com.keepit.common.db._
+import com.keepit.common.time._
+import com.keepit.model.{Keep, KeepSource, NormalizedURI, User}
+import com.keepit.model.KeepStates
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+
+
+case class CortexKeep(
+  id: Option[Id[CortexKeep]],
+  createdAt: DateTime = currentDateTime,
+  updatedAt: DateTime = currentDateTime,
+  keepId: Id[Keep],
+  userId: Id[User],
+  uriId: Id[NormalizedURI],
+  isPrivate: Boolean,
+  state: State[CortexKeep],
+  source: KeepSource,
+  seq: SequenceNumber[CortexKeep]
+) extends ModelWithState[CortexKeep] with ModelWithSeqNumber[CortexKeep] {
+  def withId(id: Id[CortexKeep]): CortexKeep = copy(id = Some(id))
+  def withUpdateTime(now: DateTime): CortexKeep = copy(updatedAt = now)
+}
+
+object CortexKeep {
+  implicit def fromKeepState(state: State[Keep]): State[CortexKeep] = State[CortexKeep](state.value)
+  implicit def fromKeepSeq(seq: SequenceNumber[Keep]): SequenceNumber[CortexKeep] = SequenceNumber[CortexKeep](seq.value)
+  implicit val format = (
+    (__ \ 'id).formatNullable(Id.format[CortexKeep]) and
+    (__ \ 'createdAt).format(DateTimeJsonFormat) and
+    (__ \ 'updatedAt).format(DateTimeJsonFormat) and
+    (__ \ 'keepId).format(Id.format[Keep]) and
+    (__ \ 'userId).format(Id.format[User]) and
+    (__ \ 'uriId).format(Id.format[NormalizedURI]) and
+    (__ \ 'isPrivate).format[Boolean] and
+    (__ \ 'state).format(State.format[CortexKeep]) and
+    (__ \ 'source).format(Json.format[KeepSource]) and
+    (__ \ 'seq).format(SequenceNumber.format[CortexKeep])
+  )(CortexKeep.apply, unlift(CortexKeep.unapply))
+}
---
+++ kifi-backend/modules/cortex/app/com/keepit/cortex/dbmodel/CortexKeepRepo.scala
@@ -0,0 +1,47 @@
+package com.keepit.cortex.dbmodel
+
+import com.google.inject.{ImplementedBy, Provider, Inject, Singleton}
+import com.keepit.common.db.slick._
+import com.keepit.common.time._
+import com.keepit.common.healthcheck.AirbrakeNotifier
+import com.keepit.common.db.Id
+import com.keepit.model.NormalizedURI
+import com.keepit.common.db.slick.DBSession.RSession
+import com.keepit.common.db.State
+import com.keepit.model.Keep
+import com.keepit.model.KeepSource
+import com.keepit.model.URL
+import com.keepit.model.User
+import com.keepit.common.db.SequenceNumber
+
+@ImplementedBy(classOf[CortexKeepRepoImpl])
+trait CortexKeepRepo extends DbRepo[CortexKeep] with SeqNumberFunction[CortexKeep]
+
+@Singleton
+class CortexKeepRepoImpl @Inject()(
+  val db: DataBaseComponent,
+  val clock: Clock,
+  airbrake: AirbrakeNotifier
+
+) extends DbRepo[CortexKeep] with CortexKeepRepo with SeqNumberDbFunction[CortexKeep]{
+
+  import db.Driver.simple._
+
+  type RepoImpl = CortexKeepTable
+
+  class CortexKeepTable(tag: Tag) extends RepoTable[CortexKeep](db, tag, "cortex_keep") with SeqNumberColumn[CortexKeep]{
+    def keepId = column[Id[Keep]]("keep_id", O.NotNull)
+    def userId = column[Id[User]]("user_id", O.NotNull)
+    def uriId = column[Id[NormalizedURI]]("uri_id", O.NotNull)
+    def isPrivate = column[Boolean]("is_private", O.NotNull)
+    def source = column[KeepSource]("source", O.NotNull)
+    def * = (id.?, createdAt, updatedAt, keepId, userId, uriId, isPrivate, state, source, seq) <> ((CortexKeep.apply _).tupled, CortexKeep.unapply _)
+  }
+
+  def table(tag:Tag) = new CortexKeepTable(tag)
+  initTable()
+
+  override def invalidateCache(keep: CortexKeep)(implicit session: RSession): Unit = {}
+
+  override def deleteCache(uri: CortexKeep)(implicit session: RSession): Unit = {}
+}
