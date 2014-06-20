7ac783f83b768d9f37ed57d6a50803a60ffcdcf9
yingjieMiao <yingjie@42go.com>
2014-06-19 17:10:16 -0700

    cortex uri

---
+++ kifi-backend/modules/common/app/com/keepit/cortex/dbmodel/CortexURI.scala
@@ -0,0 +1,44 @@
+package com.keepit.cortex.dbmodel
+
+import com.keepit.common.db.Id
+import com.keepit.model.NormalizedURI
+import com.keepit.common.db.State
+import com.keepit.common.db.SequenceNumber
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import com.keepit.model.IndexableUri
+import com.keepit.common.db.ModelWithSeqNumber
+import com.keepit.common.db.ModelWithState
+import org.joda.time.DateTime
+import com.keepit.common.time._
+
+
+case class CortexURI(
+  id: Option[Id[CortexURI]],
+  createdAt: DateTime = currentDateTime,
+  updatedAt: DateTime = currentDateTime,
+  uriId: Id[NormalizedURI],
+  title: Option[String] = None,
+  url: String,
+  state: State[CortexURI],
+  seq: SequenceNumber[CortexURI]
+) extends ModelWithState[CortexURI] with ModelWithSeqNumber[CortexURI] {
+  def withId(id: Id[CortexURI]): CortexURI = copy(id = Some(id))
+  def withUpdateTime(now: DateTime): CortexURI = copy(updatedAt = now)
+}
+
+object CortexURI {
+  implicit def fromURIState(state: State[NormalizedURI]): State[CortexURI] = State[CortexURI](state.value)
+  implicit def fromURISeq(seq: SequenceNumber[NormalizedURI]): SequenceNumber[CortexURI] = SequenceNumber[CortexURI](seq.value)
+
+  implicit def format = (
+    (__ \ 'id).formatNullable(Id.format[CortexURI]) and
+    (__ \ 'createdAt).format(DateTimeJsonFormat) and
+    (__ \ 'updatedAt).format(DateTimeJsonFormat) and
+    (__ \ 'uriId).format(Id.format[NormalizedURI]) and
+    (__ \ 'title).formatNullable[String] and
+    (__ \ 'url).format[String] and
+    (__ \ 'state).format(State.format[CortexURI]) and
+    (__ \ 'seq).format(SequenceNumber.format[CortexURI])
+  )(CortexURI.apply, unlift(CortexURI.unapply))
+}
---
+++ kifi-backend/modules/cortex/app/com/keepit/cortex/dbmodel/CortexURIRepo.scala
@@ -0,0 +1,43 @@
+package com.keepit.cortex.dbmodel
+
+import com.google.inject.{ImplementedBy, Provider, Inject, Singleton}
+import com.keepit.common.db.slick._
+import com.keepit.common.time._
+import com.keepit.common.healthcheck.AirbrakeNotifier
+import com.keepit.common.db.Id
+import com.keepit.model.NormalizedURI
+import com.keepit.common.db.slick.DBSession.RSession
+
+
+@ImplementedBy(classOf[CortexURIRepoImpl])
+trait CortexURIRepo extends DbRepo[CortexURI] with SeqNumberFunction[CortexURI]{
+
+}
+
+@Singleton
+class CortexURIRepoImpl @Inject()(
+  val db: DataBaseComponent,
+  val clock: Clock,
+  airbrake: AirbrakeNotifier
+) extends DbRepo[CortexURI] with CortexURIRepo with SeqNumberDbFunction[CortexURI] {
+
+  import db.Driver.simple._
+
+  type RepoImpl = CortexURITable
+
+  class CortexURITable(tag: Tag) extends RepoTable[CortexURI](db, tag, "cortex_uri") with SeqNumberColumn[CortexURI] {
+    def uriId = column[Id[NormalizedURI]]("uri_id")
+    def title = column[String]("title")
+    def url = column[String]("url", O.NotNull)
+    def * = (id.?, createdAt,updatedAt, uriId, title.?, url, state, seq)  <> ((CortexURI.apply _).tupled, CortexURI.unapply _)
+  }
+
+  def table(tag:Tag) = new CortexURITable(tag)
+  initTable()
+
+  override def invalidateCache(uri: CortexURI)(implicit session: RSession): Unit = {}
+
+  override def deleteCache(uri: CortexURI)(implicit session: RSession): Unit = {}
+
+}
+
