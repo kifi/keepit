3b2e6924de50c1c030aff499081b43b3bbd1b59a
Jared Jacobs <jared@42go.com>
2014-06-24 09:47:07 -0700

    adding "bag" to query to avoid getting google definition

--- kifi-backend/modules/shoebox/angular-black/src/layout/rightCol/RightColCtrl.js
+++ kifi-backend/modules/shoebox/angular-black/src/layout/rightCol/RightColCtrl.js
@@ -99,9 +99,9 @@ angular.module('kifi.layout.rightCol', ['kifi.modal'])
           thumb: '/img/guide/leather_tote.jpg',
           noun: 'tote',
           tag: 'Wishlist',
-          query: 'tote',
+          query: 'tote+bag',
           title: 'Large Leather Tote - Everyday tote bag',
-          matches: {title: [[14,4],[30,4]], url: [[53,4],[67,4]]}
+          matches: {title: [[14,4],[30,4],[35,3]], url: [[53,4],[67,4],[72,3]]}
         }, {
           url: 'http://www.lifehack.org/articles/communication/10-things-people-who-truly-love-their-lives-differently.html',
           name: ['10 Things','People Who Truly','Love Their Lives','Do Differently'],
