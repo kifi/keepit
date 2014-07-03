b9f6079a13c08d4f6db4d4ebd11bcbfeeab6a7a0
yingjieMiao <yingjie@42go.com>
2014-07-02 18:53:15 -0700

    add back

--- kifi-backend/modules/common/test/com/keepit/common/commanders/TimeToReadCommanderTest.scala
+++ kifi-backend/modules/common/test/com/keepit/common/commanders/TimeToReadCommanderTest.scala
@@ -1,6 +1,7 @@
 package com.keepit.common.commanders
 
 import org.specs2.mutable.Specification
+import com.keepit.commanders.TimeToReadCommander
 
 
 class TimeToReadCommanderTest extends Specification {
