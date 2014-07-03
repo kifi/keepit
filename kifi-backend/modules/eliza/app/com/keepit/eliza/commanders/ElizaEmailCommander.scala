1dbf9c63c82e8f7879233f243d2f7d7c6c603697
yingjieMiao <yingjie@42go.com>
2014-07-03 08:29:46 -0700

    longer cache ttl

--- kifi-backend/modules/eliza/app/com/keepit/eliza/commanders/ElizaEmailCommander.scala
+++ kifi-backend/modules/eliza/app/com/keepit/eliza/commanders/ElizaEmailCommander.scala
@@ -144,7 +144,7 @@ class ElizaEmailCommander @Inject() (
       nUrlId <- thread.uriId
       url <- thread.url
     } yield {
-      scraper.getURIWordCount(nUrlId, url) map { cnt => TimeToReadCommander.wordCountToReadTimeMinutes(cnt)}
+      scraper.getURIWordCount(nUrlId, Some(url)) map { cnt => TimeToReadCommander.wordCountToReadTimeMinutes(cnt)}
     }) getOrElse Future.successful(None)
   }
 
