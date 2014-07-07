f2d4ba728699d6371391aada330857f226b7592a
Léo Grimaldi <leo@42go.com>
2014-07-07 16:05:02 -0700

    Fix test

--- kifi-backend/modules/abook/app/com/keepit/commanders/ContactInterner.scala
+++ kifi-backend/modules/abook/app/com/keepit/commanders/ContactInterner.scala
@@ -7,15 +7,15 @@ import com.keepit.common.db.Id
 import com.keepit.model.{EContactStates, ABookInfo, EContact, User}
 import com.keepit.common.mail.BasicContact
 import com.keepit.common.db.slick.DBSession.RWSession
-import com.keepit.typeahead.abook.EContactTypeahead
 import com.keepit.common.db.slick.Database
 import com.keepit.common.logging.Logging
+import com.keepit.abook.typeahead.EContactABookTypeahead
 
 @Singleton
 class ContactInterner @Inject() (
   emailAccountRepo: EmailAccountRepo,
   econtactRepo: EContactRepo,
-  econtactTypeahead: EContactTypeahead,
+  econtactTypeahead: EContactABookTypeahead,
   db: Database
 ) extends Logging {
 
@@ -52,7 +52,7 @@ class ContactInterner @Inject() (
     }
 
     if (inserted + updated > 0) { econtactTypeahead.refresh(userId) }
-    log.info(s"Inserted $inserted new contacts and updated $updated existing contacts for ABook $abookId of user $userId)")
+    log.info(s"Inserted $inserted new contacts and updated $updated existing contacts for ABook $abookId of user $userId")
     (inserted, updated)
   }
 
