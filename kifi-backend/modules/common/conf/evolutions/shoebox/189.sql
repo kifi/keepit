3b9864da7e69d3b4a94633f8619cb3ffa505753a
Léo Grimaldi <leo@42go.com>
2014-07-07 09:10:07 -0700

    Change unique constraint on econtact table

--- kifi-backend/modules/common/conf/evolutions/shoebox/189.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/189.sql
@@ -2,6 +2,8 @@
 
 # --- !Ups
 ALTER TABLE econtact ADD COLUMN abook_id;
+ALTER TABLE econtact DROP INDEX econtact_i_user_id_email ON econtact;
+ALTER TABLE econtact ADD CONSTRAINT econtact_i_abook_id_email UNIQUE (abook_id, email)
 insert into evolutions (name, description) values('189.sql', 'add abook_id to econtact');
 
 # --- !Downs
