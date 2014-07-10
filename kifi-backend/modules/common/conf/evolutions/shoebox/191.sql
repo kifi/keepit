88613be3f246a3b60a8eb514f88cb5853e2c985b
Stephen Kemmerling <stephen@42go.com>
2014-07-10 14:40:16 -0700

    fixing another evolution merge conflict

--- kifi-backend/modules/common/conf/evolutions/shoebox/191.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/191.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # MAVEN
 
 # --- !Ups
@@ -47,23 +46,5 @@ CREATE TABLE maven_keep_info (
 insert into evolutions (name, description) values('190.sql', 'adding new tables for keep ingestion in maven');
 
 
-=======
-# HEIMDAL
-
-# --- !Ups
-
-CREATE TABLE IF NOT EXISTS system_value (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
-    name varchar(64) NOT NULL,
-    value TEXT NOT NULL,
-    state VARCHAR(20) NOT NULL,
-    created_at datetime NOT NULL,
-    updated_at datetime NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE INDEX system_value_name (name)
-);
-
-insert into evolutions (name, description) values('190.sql', 'adding system_value table to heimdal');
->>>>>>> parent/master
 
 # --- !Downs
