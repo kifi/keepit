d00898747acca5a4db0d01473d65960ee5fb8f0c
Jared Jacobs <jared@42go.com>
2014-07-10 14:30:28 -0700

    canonicalizing (lowercasing domain) after validating

--- kifi-backend/modules/common/test/com/keepit/common/mail/EmailAddressTest.scala
+++ kifi-backend/modules/common/test/com/keepit/common/mail/EmailAddressTest.scala
@@ -20,11 +20,11 @@ class EmailAddressTest extends Specification {
       EmailAddress("ninja_turtle_babe2126@hotmail.com").address === "ninja_turtle_babe2126@hotmail.com"
     }
     "reject invalid email addresses" in {
-      EmailAddress.validate("a").get must throwAn[IllegalArgumentException]
-      EmailAddress.validate("@").get must throwAn[IllegalArgumentException]
-      EmailAddress.validate("@a").get must throwAn[IllegalArgumentException]
-      EmailAddress.validate("a@").get must throwAn[IllegalArgumentException]
-      EmailAddress.validate("\"a\"@b").get must throwAn[IllegalArgumentException]
+      EmailAddress.validate("a") must beFailedTry.withThrowable[IllegalArgumentException]
+      EmailAddress.validate("@") must beFailedTry.withThrowable[IllegalArgumentException]
+      EmailAddress.validate("@a") must beFailedTry.withThrowable[IllegalArgumentException]
+      EmailAddress.validate("a@") must beFailedTry.withThrowable[IllegalArgumentException]
+      EmailAddress.validate("\"a\"@b") must beFailedTry.withThrowable[IllegalArgumentException]
     }
     "canonicalize (lowercase) domains when reading from JSON" in {
       readFromJson("a@b") === JsSuccess(EmailAddress("a@b"))
