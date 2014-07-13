ccc0f6970e02032f8cb0f121d0db4c307df1d79d
Léo Grimaldi <leo@42go.com>
2014-07-13 13:44:28 -0700

    Fix evolution

--- kifi-backend/modules/common/conf/evolutions/shoebox/194.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/194.sql
@@ -6,7 +6,7 @@
 -- CREATE TABLE econtact_sequence (id bigint(20) NOT NULL);
 -- INSERT INTO econtact_sequence VALUES (0);
 -- H2:
-CREATE SEQUENCE email_account_sequence;
+CREATE SEQUENCE econtact_sequence;
 
 ALTER TABLE econtact ADD COLUMN seq bigint(20) NOT NULL;
 CREATE INDEX econtact_i_seq on econtact(seq);
