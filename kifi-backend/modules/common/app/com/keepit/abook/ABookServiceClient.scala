12a96b39b1f42a800f02e827397ffbb9ce989d2e
Léo Grimaldi <leo@42go.com>
2014-07-13 17:07:43 -0700

    Add controller and endpoints to fetch email account and contact updates

--- kifi-backend/modules/common/app/com/keepit/abook/ABookServiceClient.scala
+++ kifi-backend/modules/common/app/com/keepit/abook/ABookServiceClient.scala
@@ -2,7 +2,7 @@ package com.keepit.abook
 
 // import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import com.keepit.model._
-import com.keepit.common.db.{ ExternalId, Id }
+import com.keepit.common.db.{ SequenceNumber, ExternalId, Id }
 import com.keepit.common.service.{ ServiceClient, ServiceType }
 import com.keepit.common.logging.Logging
 import com.keepit.common.healthcheck.AirbrakeNotifier
@@ -22,7 +22,7 @@ import com.google.inject.util.Providers
 import com.keepit.common.routes.ABook
 import scala.util.{ Success, Failure, Try }
 import play.api.http.Status
-import com.keepit.abook.model.{ RichContact, RichSocialConnection }
+import com.keepit.abook.model.{ IngestableContact, IngestableEmailAccount, RichContact, RichSocialConnection }
 import com.keepit.common.mail.{ EmailAddress, BasicContact }
 import com.keepit.typeahead.TypeaheadHit
 
@@ -56,6 +56,8 @@ trait ABookServiceClient extends ServiceClient {
   def internKifiContact(userId: Id[User], contact: BasicContact): Future[RichContact]
   def prefixQuery(userId: Id[User], query: String, maxHits: Option[Int] = None): Future[Seq[TypeaheadHit[RichContact]]]
   def getContactsByUser(userId: Id[User], page: Int = 0, pageSize: Option[Int] = None): Future[Seq[RichContact]]
+  def getEmailAccountsChanged(seqNum: SequenceNumber[IngestableEmailAccount], fetchSize: Int): Future[Seq[IngestableEmailAccount]]
+  def getContactsChanged(seqNum: SequenceNumber[IngestableContact], fetchSize: Int): Future[Seq[IngestableContact]]
 }
 
 class ABookServiceClientImpl @Inject() (
@@ -224,6 +226,13 @@ class ABookServiceClientImpl @Inject() (
     call(ABook.internal.hideEmailFromUser(userId, email)).map(_.json.as[Boolean])
   }
 
+  def getEmailAccountsChanged(seqNum: SequenceNumber[IngestableEmailAccount], fetchSize: Int): Future[Seq[IngestableEmailAccount]] = {
+    call(ABook.internal.getEmailAccountsChanged(seqNum, fetchSize)).map(_.json.as[Seq[IngestableEmailAccount]])
+  }
+  def getContactsChanged(seqNum: SequenceNumber[IngestableContact], fetchSize: Int): Future[Seq[IngestableContact]] = {
+    call(ABook.internal.getContactsChanged(seqNum, fetchSize)).map(_.json.as[Seq[IngestableContact]])
+  }
+
 }
 
 class FakeABookServiceClientImpl(val airbrakeNotifier: AirbrakeNotifier, scheduler: Scheduler) extends ABookServiceClient {
@@ -281,4 +290,9 @@ class FakeABookServiceClientImpl(val airbrakeNotifier: AirbrakeNotifier, schedul
   def prefixQuery(userId: Id[User], query: String, maxHits: Option[Int]): Future[Seq[TypeaheadHit[RichContact]]] = Future.successful(Seq.empty)
 
   def getContactsByUser(userId: Id[User], page: Int, pageSize: Option[Int]): Future[Seq[RichContact]] = Future.successful(Seq.empty)
+
+  def getEmailAccountsChanged(seqNum: SequenceNumber[IngestableEmailAccount], fetchSize: Int): Future[Seq[IngestableEmailAccount]] = Future.successful(Seq.empty)
+
+  def getContactsChanged(seqNum: SequenceNumber[IngestableContact], fetchSize: Int): Future[Seq[IngestableContact]] = Future.successful(Seq.empty)
+
 }
