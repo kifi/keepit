baac45e1c4388bc52fee038db9734df56f12a0fb
Aaron Hsu <aaron@kifi.com>
2014-07-15 13:39:23 -0700

    added some error handling for addLibrary() ??

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryCommander.scala
@@ -27,8 +27,11 @@ class LibraryCommander @Inject() (
     scheduler: Scheduler,
     clock: Clock) extends Logging {
 
-  def addLibrary(libInfo: LibraryAddRequest, ownerId: Id[User]) = {
+  def addLibrary(libInfo: LibraryAddRequest, ownerId: Id[User]) : Either [LibraryFail, FullLibraryInfo] = {
 
+    if (!libInfo.collaborators.intersect(libInfo.followers).isEmpty) {
+      return Left(LibraryFail("collaborators & followers overlap!"))
+    }
     val (collaboratorIds, collaboratorUsers, followerIds, followerUsers, ownerExtId) = db.readOnlyReplica { implicit s =>
       val collabs = libInfo.collaborators.map { x =>
         val inviteeIdOpt = userRepo.getOpt(x) collect { case user => user.id.get }
@@ -47,7 +50,7 @@ class LibraryCommander @Inject() (
     val (libId, libExtId) = db.readWrite { implicit s =>
       val lib = libraryRepo.save(Library(ownerId = ownerId, name = libInfo.name, description = libInfo.description,
         visibility = libInfo.visibility, slug = libInfo.slug))
-      val libId = lib.id.get
+      val libId = lib.id.getOrElse { return Left(LibraryFail("Library Id not found")) }
       val libMem = libraryMembershipRepo.save(LibraryMembership(libraryId = libId, userId = ownerId, access = LibraryAccess.OWNER))
       (libId, lib.externalId)
     }
@@ -62,9 +65,9 @@ class LibraryCommander @Inject() (
 
     val groupCollabs = GroupHolder(count = collaboratorIds.length, users = collaboratorUsers, isMore = false)
     val groupFollowers = GroupHolder(count = followerIds.length, users = followerUsers, isMore = false)
-    FullLibraryInfo(id = libExtId, ownerId = ownerExtId, name = libInfo.name, slug = libInfo.slug,
+    Right(FullLibraryInfo(id = libExtId, ownerId = ownerExtId, name = libInfo.name, slug = libInfo.slug,
       visibility = libInfo.visibility, description = libInfo.description, keepCount = 0,
-      collaborators = groupCollabs, followers = groupFollowers)
+      collaborators = groupCollabs, followers = groupFollowers))
   }
 
   private def inviteBulkUsers(invites: Seq[LibraryInvite]) {
@@ -73,6 +76,8 @@ class LibraryCommander @Inject() (
     }
   }
 }
+
+case class LibraryFail(message: String) extends AnyVal
 
 case class LibraryAddRequest(
   name: String,
