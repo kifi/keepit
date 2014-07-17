9f738642c2c6a4eea7e10b60b04ae44fc612dcad
Jared Jacobs <jared@42go.com>
2014-07-17 14:21:57 -0700

    using base32 encoding to avoid underscores and hyphens

--- kifi-backend/modules/common/app/com/keepit/common/crypto/ModelWithPublicId.scala
+++ kifi-backend/modules/common/app/com/keepit/common/crypto/ModelWithPublicId.scala
@@ -1,11 +1,10 @@
 package com.keepit.common.crypto
 
 import com.keepit.common.db.Id
+import com.google.common.io.BaseEncoding
 
 import play.api.libs.json._
 import play.api.mvc.{ PathBindable, QueryStringBindable }
-
-import org.apache.commons.codec.binary.Base64
 
 import scala.util.{ Failure, Success, Try }
 
@@ -37,6 +36,8 @@ object PublicId {
 
     override def unbind(key: String, id: PublicId[T]): String = id.toString
   }
+
+  private[crypto] val coder = BaseEncoding.base32().lowerCase().omitPadding()
 }
 
 // TODO: Cipher must be a singleton, not re-created for every encode/decode.
@@ -48,7 +49,7 @@ trait ModelWithPublicId[T <: ModelWithPublicId[T]] {
 
   def publicId(implicit config: PublicIdConfiguration): Try[PublicId[T]] = {
     id.map { someId =>
-      Success(PublicId[T](prefix + Base64.encodeBase64URLSafeString(Aes64BitCipher(config.key).encrypt(someId.id))))
+      Success(PublicId[T](prefix + PublicId.coder.encode(Aes64BitCipher(config.key).encrypt(someId.id))))
     }.getOrElse(Failure(new IllegalStateException("model has no id")))
   }
 
@@ -59,11 +60,11 @@ trait ModelWithPublicIdCompanion[T <: ModelWithPublicId[T]] {
   val prefix: String
 
   def decode(publicId: String)(implicit config: PublicIdConfiguration): Try[Id[T]] = {
-    val reg = raw"^$prefix(.*)$$".r
-    Try {
-      reg.findFirstMatchIn(publicId).map(_.group(1)).map { identifier =>
-        Id[T](Aes64BitCipher(config.key).decrypt(Base64.decodeBase64(identifier)))
-      }.get
+    if (publicId.startsWith(prefix)) {
+      Try(Id[T](Aes64BitCipher(config.key).decrypt(PublicId.coder.decode(publicId.substring(prefix.length)))))
+    } else {
+      Failure(new IllegalArgumentException(s"Expected $publicId to start with $prefix"))
     }
   }
+
 }
