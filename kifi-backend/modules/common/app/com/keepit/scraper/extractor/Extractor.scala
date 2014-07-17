7cf969a548275c8f321bd9e4b54a401aa02a9371
Raymond Ng <raymond.ng@gmail.com>
2014-07-17 15:15:55 -0700

    ScrapeWorker: clean-up #1 (reduce code size)

--- kifi-backend/modules/common/app/com/keepit/scraper/extractor/Extractor.scala
+++ kifi-backend/modules/common/app/com/keepit/scraper/extractor/Extractor.scala
@@ -2,8 +2,9 @@ package com.keepit.scraper.extractor
 
 import com.keepit.scraper.HttpInputStream
 import com.keepit.common.net.URI
+import com.keepit.scraper.mediatypes.MediaTypes
 
-trait Extractor {
+trait Extractor { // todo(ray): move to scraper
   def process(input: HttpInputStream): Unit
   def getContent(): String
   def getMetadata(name: String): Option[String]
@@ -15,7 +16,12 @@ trait Extractor {
       case _ => None
     }
   }
+
+  // helper methods
   def getAlternateUrls(): Set[String] = getLinks("alternate")
+  def getTitle(): String = getMetadata("title").getOrElse("")
+  def getDescription(): Option[String] = getMetadata("description")
+  def getMediaTypeString(): Option[String] = MediaTypes(this).getMediaTypeString(this)
 }
 
 trait ExtractorFactory extends Function[String, Extractor]
