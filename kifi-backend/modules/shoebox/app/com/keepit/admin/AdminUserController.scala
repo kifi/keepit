b0311aeed04ed939e85df081ec3c7b01a2eb0a79
Andrew Conner <git@andrewconner.org>
2014-07-21 14:51:25 -0700

    Needs a wrapper

--- kifi-backend/modules/shoebox/app/com/keepit/admin/AdminUserController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/admin/AdminUserController.scala
@@ -821,7 +821,7 @@ class AdminUserController @Inject() (
   }
 
   def setUsername(userId: Id[User], username: String, overrideRestrictions: Boolean = false) = AdminHtmlAction.authenticated { request =>
-    val res = userCommander.setUsername(userId, username, overrideRestrictions)
+    val res = userCommander.setUsername(userId, Username(username), overrideRestrictions)
 
     Ok(res)
   }
