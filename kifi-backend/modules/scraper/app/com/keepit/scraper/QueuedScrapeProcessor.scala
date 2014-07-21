d3500274e5921519385432d7402586dbc40075e7
Raymond Ng <ray@42go.com>
2014-07-21 15:31:21 -0700

    - basicArticle from ScrapeWorker

--- kifi-backend/modules/scraper/app/com/keepit/scraper/QueuedScrapeProcessor.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/QueuedScrapeProcessor.scala
@@ -236,7 +236,7 @@ class QueuedScrapeProcessor @Inject() (
         if (URI.parse(url).get.host.isEmpty) throw new IllegalArgumentException(s"url $url has no host!")
         val fetchStatus = httpFetcher.fetch(url, proxy = proxyOpt)(input => extractor.process(input))
         val res = fetchStatus.statusCode match {
-          case HttpStatus.SC_OK if !helper.syncIsUnscrapableP(url, fetchStatus.destinationUrl) => Some(worker.basicArticle(fetchStatus.destinationUrl getOrElse url, extractor))
+          case HttpStatus.SC_OK if !helper.syncIsUnscrapableP(url, fetchStatus.destinationUrl) => Some(extractor.basicArticle(fetchStatus.destinationUrl getOrElse url))
           case _ => None
         }
         res
