ba9ce8ba0ace17456703ed0b803d3427b79b54ed
Léo Grimaldi <leo@42go.com>
2014-07-22 11:28:33 -0700

    Define LibraryIndexer + LibraryIndexerActor + LibraryIndexerPlugin

--- kifi-backend/modules/search/app/com/keepit/search/graph/library/LibraryIndexer.scala
+++ kifi-backend/modules/search/app/com/keepit/search/graph/library/LibraryIndexer.scala
@@ -1,9 +1,12 @@
 package com.keepit.search.graph.library
 
-import com.keepit.model.{User, Library}
-import com.keepit.common.db.Id
-import com.keepit.search.index.{DefaultAnalyzer, Indexable}
+import com.keepit.model.{ User, Library }
+import com.keepit.common.db.{ SequenceNumber, Id }
+import com.keepit.search.index._
 import com.keepit.search.LangDetector
+import com.keepit.common.healthcheck.AirbrakeNotifier
+import scala.concurrent.Future
+import com.keepit.shoebox.ShoeboxServiceClient
 
 object LibraryFields {
   val nameField = "n"
@@ -13,6 +16,8 @@ object LibraryFields {
   val visibilityField = "v"
   val usersField = "u"
   val recordField = "rec"
+
+  val decoders: Map[String, FieldDecoder] = Map.empty
 }
 
 class LibraryIndexable(library: Library, users: Seq[Id[User]]) extends Indexable[Library, Library] {
@@ -42,5 +47,30 @@ class LibraryIndexable(library: Library, users: Seq[Id[User]]) extends Indexable
     doc.add(buildBinaryDocValuesField(recordField, record))
 
     doc
+  }
+}
+
+class LibraryIndexer(indexDirectory: IndexDirectory, shoebox: ShoeboxServiceClient, val airbrake: AirbrakeNotifier) extends Indexer[Library, Library, LibraryIndexer](indexDirectory, LibraryFields.decoders) {
+
+  def update(): Int = throw new UnsupportedOperationException()
+
+  import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+  def fetchUpdates(seq: SequenceNumber[Library], fetchSize: Int): Future[Seq[Indexable[Library, Library]]] = {
+    shoebox.getLibrariesWithMembersChanged(seq, fetchSize).map {
+      case libraries =>
+        libraries.map {
+          case (library, memberships) =>
+            val users = memberships.flatMap { membership =>
+              if (membership.libraryId != library.id.get) { throw new IllegalArgumentException(s"Inconsistent membership for library ${library.id.get}: $membership") }
+              Some(membership.userId) //todo(Léo): filter this once LibraryMembership tracks searchability
+            }
+            new LibraryIndexable(library, users)
+        }
+    }
+  }
+
+  def processUpdates(updates: Seq[Indexable[Library, Library]]): Int = updateLock.synchronized {
+    doUpdate("LibraryIndex")(updates.iterator)
   }
 }
