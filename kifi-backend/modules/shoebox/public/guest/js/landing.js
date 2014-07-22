96de71f46c04818b606f2f254a429d717a87c33a
Jared Jacobs <jared@42go.com>
2014-07-22 12:25:06 -0700

    making JS document scrolling work in more browsers

--- kifi-backend/modules/shoebox/public/guest/js/landing.js
+++ kifi-backend/modules/shoebox/public/guest/js/landing.js
@@ -1,10 +1,10 @@
 $(document).on('click', '.more-arrow', function () {
-  var body = document.body;
-  var top0 = body.scrollTop;
+  var $html = $('html');
+  var top0 = $html.prop('scrollTop');
   var topN = this.offsetTop + this.offsetHeight;
   var px = Math.abs(topN - top0);
   var ms = 320 * Math.log((px + 80) / 60) | 0;
-  $(body).animate({scrollTop: topN}, ms);
+  $html.animate({scrollTop: topN}, ms);
 });
 
 window.addEventListener('message', function (e) {
