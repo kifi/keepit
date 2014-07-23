6c4c3c624695a209c45946b86dcda76ad44bb3b9
Andrew Conner <git@andrewconner.org>
2014-07-23 07:32:50 -0700

    Progress towards better tests!

--- kifi-backend/modules/shoebox/test/com/keepit/controllers/website/LibraryControllerTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/controllers/website/LibraryControllerTest.scala
@@ -1,32 +1,51 @@
 package com.keepit.controllers.website
 
+import com.keepit.abook.TestABookServiceClientModule
 import com.keepit.commanders.{ FullLibraryInfo, LibraryInfo }
-import com.keepit.common.crypto.{ PublicIdConfiguration, ShoeboxCryptoModule }
-import com.keepit.common.social.FakeShoeboxSecureSocialModule
+import com.keepit.common.crypto.{ TestCryptoModule, PublicIdConfiguration, ShoeboxCryptoModule }
+import com.keepit.common.external.FakeExternalServiceModule
+import com.keepit.common.mail.TestMailModule
+import com.keepit.common.social.{ FakeShoeboxSecureSocialModule, FakeSocialGraphModule }
 import com.keepit.common.store.ShoeboxFakeStoreModule
 import com.keepit.common.time._
-import com.keepit.inject.ApplicationInjector
+import com.keepit.cortex.FakeCortexServiceClientModule
 import com.keepit.model._
-import com.keepit.shoebox.FakeShoeboxServiceModule
-import com.keepit.test.{ ShoeboxApplication, TestApplication, ShoeboxApplicationInjector, ShoeboxTestInjector }
+import com.keepit.scraper.{ FakeScrapeSchedulerModule, TestScrapeSchedulerConfigModule }
+import com.keepit.search.FakeSearchServiceClientModule
+import com.keepit.shoebox.{ FakeShoeboxServiceModule, FakeKeepImportsModule }
+import com.keepit.test.ShoeboxTestInjector
 import org.joda.time.DateTime
 import org.specs2.mutable.Specification
-import play.api.libs.json.{ JsString, Json }
-import play.api.test.FakeRequest
+import play.api.libs.json.Json
+import play.api.mvc.SimpleResult
 import play.api.test.Helpers._
+import play.api.test._
 
-import scala.util.Success
+import scala.concurrent.Future
 
-class LibraryControllerTest extends Specification with ShoeboxApplicationInjector {
-  val controllerTestModules = Seq(
-    ShoeboxCryptoModule()
+class LibraryControllerTest extends Specification with ShoeboxTestInjector {
+  val modules = Seq(
+    TestCryptoModule(),
+    FakeShoeboxSecureSocialModule(),
+    ShoeboxFakeStoreModule(),
+    TestABookServiceClientModule(),
+    FakeKeepImportsModule(),
+    TestMailModule(),
+    FakeExternalServiceModule(),
+    FakeCortexServiceClientModule(),
+    FakeSearchServiceClientModule(),
+    TestScrapeSchedulerConfigModule(),
+    FakeSocialGraphModule(),
+    FakeScrapeSchedulerModule(),
+    FakeShoeboxServiceModule()
   )
 
   "LibraryController" should {
 
     "create library" in {
-      running(new ShoeboxApplication(controllerTestModules: _*)) {
+      withDb(modules: _*) { implicit injector =>
         val t1 = new DateTime(2014, 7, 21, 6, 59, 0, 0, DEFAULT_DATE_TIME_ZONE)
+        val libraryController = inject[LibraryController]
         val testPath = com.keepit.controllers.website.routes.LibraryController.addLibrary().url
         val user = db.readWrite { implicit s =>
           userRepo.save(User(firstName = "Aaron", lastName = "Hsu", createdAt = t1))
@@ -38,8 +57,8 @@ class LibraryControllerTest extends Specification with ShoeboxApplicationInjecto
           "collaborators" -> Json.arr(),
           "followers" -> Json.arr()
         )
-        val request1 = FakeRequest("POST", testPath).withJsonBody(inputJson1)
-        val result1 = route(request1).get
+        val request1 = FakeRequest("POST", testPath).withBody(inputJson1).withHeaders("userId" -> "1")
+        val result1 = libraryController.addLibrary()(request1)
         status(result1) must equalTo(OK)
         contentType(result1) must beSome("application/json")
 
@@ -57,14 +76,14 @@ class LibraryControllerTest extends Specification with ShoeboxApplicationInjecto
           "collaborators" -> Json.arr(),
           "followers" -> Json.arr()
         )
-        val request2 = FakeRequest("POST", testPath).withJsonBody(inputJson2)
-        val result2 = route(request2).get
+        val request2 = FakeRequest("POST", testPath).withBody(inputJson2).withHeaders("userId" -> "1")
+        val result2: Future[SimpleResult] = libraryController.addLibrary()(request2)
         status(result2) must equalTo(BAD_REQUEST)
         contentType(result2) must beSome("application/json")
 
         // Re-add Library 1
-        val request3 = FakeRequest("POST", com.keepit.controllers.website.routes.LibraryController.addLibrary().url).withJsonBody(inputJson1)
-        val result3 = route(request3).get
+        val request3 = FakeRequest("POST", com.keepit.controllers.website.routes.LibraryController.addLibrary().url).withBody(inputJson1).withHeaders("userId" -> "1")
+        val result3: Future[SimpleResult] = libraryController.addLibrary()(request3)
         status(result3) must equalTo(BAD_REQUEST)
         contentType(result3) must beSome("application/json")
 
@@ -75,17 +94,19 @@ class LibraryControllerTest extends Specification with ShoeboxApplicationInjecto
           "collaborators" -> Json.arr(),
           "followers" -> Json.arr()
         )
-        val request4 = FakeRequest("POST", com.keepit.controllers.website.routes.LibraryController.addLibrary().url).withJsonBody(inputJson4)
-        val result4 = route(request4).get
+        val request4 = FakeRequest("POST", com.keepit.controllers.website.routes.LibraryController.addLibrary().url).withBody(inputJson4).withHeaders("userId" -> "1")
+        val result4: Future[SimpleResult] = libraryController.addLibrary()(request4)
         status(result4) must equalTo(BAD_REQUEST)
         contentType(result4) must beSome("application/json")
       }
     }
 
     "modify library" in {
-      running(new ShoeboxApplication(controllerTestModules: _*)) {
+      withDb(modules: _*) { implicit injector =>
         implicit val config = inject[PublicIdConfiguration]
         val t1 = new DateTime(2014, 7, 21, 6, 59, 0, 0, DEFAULT_DATE_TIME_ZONE)
+        val libraryController = inject[LibraryController]
+
         val (user1, lib1) = db.readWrite { implicit s =>
           val user = userRepo.save(User(firstName = "Aaron", lastName = "Hsu", createdAt = t1))
           val library = libraryRepo.save(Library(name = "Library1", ownerId = user.id.get, slug = LibrarySlug("lib1"), visibility = LibraryVisibility.SECRET))
@@ -97,16 +118,16 @@ class LibraryControllerTest extends Specification with ShoeboxApplicationInjecto
         val testPath = com.keepit.controllers.website.routes.LibraryController.modifyLibrary(pubId).url
 
         val inputJson1 = Json.obj("name" -> "Library2")
-        val request1 = FakeRequest("POST", testPath).withJsonBody(inputJson1)
-        val result1 = route(request1).get
+        val request1 = FakeRequest("POST", testPath).withBody(inputJson1).withHeaders("userId" -> "1")
+        val result1: Future[SimpleResult] = libraryController.modifyLibrary(pubId)(request1)
         status(result1) must equalTo(OK)
         contentType(result1) must beSome("application/json")
 
         Json.parse(contentAsString(result1)).as[LibraryInfo].name === "Library2"
 
         val inputJson2 = Json.obj("slug" -> "lib2", "description" -> "asdf", "visibility" -> LibraryVisibility.ANYONE.value)
-        val request2 = FakeRequest("POST", testPath).withJsonBody(inputJson2)
-        val result2 = route(request2).get
+        val request2 = FakeRequest("POST", testPath).withBody(inputJson2).withHeaders("userId" -> "1")
+        val result2: Future[SimpleResult] = libraryController.modifyLibrary(pubId)(request2)
         status(result2) must equalTo(OK)
         contentType(result2) must beSome("application/json")
 
@@ -126,7 +147,7 @@ class LibraryControllerTest extends Specification with ShoeboxApplicationInjecto
     }
 
     "remove library" in {
-      running(new ShoeboxApplication(controllerTestModules: _*)) {
+      withDb(modules: _*) { implicit injector =>
         implicit val config = inject[PublicIdConfiguration]
         val t1 = new DateTime(2014, 7, 21, 6, 59, 0, 0, DEFAULT_DATE_TIME_ZONE)
         val (user1, lib1, lib2) = db.readWrite { implicit s =>
@@ -156,7 +177,7 @@ class LibraryControllerTest extends Specification with ShoeboxApplicationInjecto
     }
 
     "get library by public id" in {
-      running(new ShoeboxApplication(controllerTestModules: _*)) {
+      withDb(modules: _*) { implicit injector =>
         implicit val config = inject[PublicIdConfiguration]
         val t1 = new DateTime(2014, 7, 21, 6, 59, 0, 0, DEFAULT_DATE_TIME_ZONE)
         val (user1, lib1) = db.readWrite { implicit s =>
@@ -190,7 +211,7 @@ class LibraryControllerTest extends Specification with ShoeboxApplicationInjecto
     }
 
     "get library by user id" in {
-      running(new ShoeboxApplication(controllerTestModules: _*)) {
+      withDb(modules: _*) { implicit injector =>
         implicit val config = inject[PublicIdConfiguration]
         val t1 = new DateTime(2014, 7, 21, 6, 59, 0, 0, DEFAULT_DATE_TIME_ZONE)
         val (user1, user2, lib1, lib2) = db.readWrite { implicit s =>
