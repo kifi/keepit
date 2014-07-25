3620a9c716784c7c559fdca5fec5ab924f35b7e5
Raymond Ng <ray@42go.com>
2014-07-24 23:45:39 -0700

    back to QueuedProcessor (for now)

--- kifi-backend/modules/scraper/app/com/keepit/scraper/ScrapeProcessorModule.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/ScrapeProcessorModule.scala
@@ -1,14 +1,8 @@
 package com.keepit.scraper
 
-import com.keepit.scraper.actor.ScrapeProcessorActorImpl
-import com.keepit.scraper.fetcher.{ ProdHttpFetcherModule, HttpFetcher }
-import com.keepit.scraper.fetcher.apache.ApacheHttpFetcher
-import net.codingwell.scalaguice.ScalaModule
 import com.keepit.inject.AppScoped
-import com.google.inject.{ Provides, Singleton }
-import com.keepit.scraper.extractor.{ ExtractorFactoryImpl, ExtractorFactory }
-import com.keepit.common.healthcheck.AirbrakeNotifier
-import com.keepit.common.plugin.SchedulingProperties
+import com.keepit.scraper.extractor.{ ExtractorFactory, ExtractorFactoryImpl }
+import net.codingwell.scalaguice.ScalaModule
 
 trait ScrapeProcessorModule extends ScalaModule
 
@@ -19,7 +13,7 @@ case class ProdScraperProcessorModule() extends ScrapeProcessorModule {
     bind[ShoeboxDbCallbacks].to[ShoeboxDbCallbackHelper].in[AppScoped]
     bind[SyncShoeboxDbCallbacks].to[ShoeboxDbCallbackHelper].in[AppScoped]
     bind[PullerPlugin].to[PullerPluginImpl].in[AppScoped]
-    bind[ScrapeProcessor].to[ScrapeProcessorActorImpl]
+    bind[ScrapeProcessor].to[QueuedScrapeProcessor]
     install(ProdScraperConfigModule())
     install(ProdScrapeSchedulerConfigModule())
   }
