301582f0100076348fecc5b0a924c8f7a75177e8
eishay <eishay@gmail.com>
2014-07-25 21:52:42 -0700

    updating miss ratio to take in account cases of sparse, high updates caches

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/internal/ShoeboxController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/internal/ShoeboxController.scala
@@ -234,7 +234,7 @@ class ShoeboxController @Inject() (
         // todo(Léo): What follows is dangerous. Someone could mess up with our data by reporting wrong alternate Urls on its website. We need to do a specific content check.
         bestReference.normalization.map(ScrapedCandidate(scrapedUri.url, _)).foreach { bestCandidate =>
           alternateUrls.foreach { alternateUrl =>
-            val uri = db.readOnlyReplica { implicit session =>
+            val uri = db.readOnlyMaster { implicit session =>
               normalizedURIInterner.getByUri(alternateUrl)
             }
             uri match {
