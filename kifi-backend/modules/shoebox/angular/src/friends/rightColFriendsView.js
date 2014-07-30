b754903659713a32abc4571bce0576eca8e577ce
Yiping Liao <yiping@kifi.com>
2014-07-30 11:33:52 -0700

    Add rotating connection badge to right column.
    https://app.asana.com/0/15014070792041/15100933478436
    https://app.asana.com/0/15014070792041/15100933478440

--- kifi-backend/modules/shoebox/angular/src/friends/rightColFriendsView.js
+++ kifi-backend/modules/shoebox/angular/src/friends/rightColFriendsView.js
@@ -2,8 +2,15 @@
 
 angular.module('kifi.friends.rightColFriendsView', [])
 
+.directive('kfRightColFriendsView', [function () {
+  return {
+    replace: true,
+    restrict: 'A',
+    templateUrl: 'friends/rightColFriendsView.tpl.html'
+  };
+}])
 
-.directive('kfCompactFriendsView', ['$log', 'friendService', function ($log, friendService) {
+.directive('kfCompactFriendsView', ['friendService', function (friendService) {
   return {
     replace: true,
     restrict: 'A',
@@ -41,6 +48,74 @@ angular.module('kifi.friends.rightColFriendsView', [])
   };
 }])
 
+.directive('kfRightColConnectView', ['friendService', 'socialService', 'util', function (friendService, socialService, util) {
+  return {
+    replace: true,
+    restrict: 'A',
+    templateUrl: 'friends/rightColConnectView.tpl.html',
+    link: function (scope/*, element, attrs*/) {
+      socialService.refresh();
+      scope.friendCount = friendService.totalFriends;
+      scope.showConnect = false;
+
+      var connectionStatus = {
+        'Facebook': socialService.facebook && !!socialService.facebook.profileUrl,
+        'LinkedIn': socialService.linkedin && !!socialService.linkedin.profileUrl,
+        'Gmail': socialService.gmail.length > 0
+      };
+
+      // The connection badge randomly rotates among networks that have not
+      // been connected yet and is not shown if the user has connected to all
+      // networks or has more than 20 friends.
+      var updateConnectDisplayStatus = function () {
+        // Array of unconnected networks.
+        var connectOptions = [];
+
+        _.forEach(connectionStatus, function (status, name) {
+          if (!status) {
+            connectOptions.push(name);
+          }
+        });
+
+        if ((connectOptions.length === 0) || scope.friendCount >= 20) {
+          scope.showConnect = false;
+        } else {
+          scope.showConnect = true;
+        }
+
+        var randomConnectOption = util.getRandomInt(0, connectOptions.length - 1);
+
+        scope.showFacebookConnect = connectOptions[randomConnectOption] === 'Facebook';
+        scope.showLinkedInConnect = connectOptions[randomConnectOption] === 'LinkedIn';
+        scope.showGmailImport = connectOptions[randomConnectOption] === 'Gmail';
+      };
+
+      scope.$watch(function () {
+        return socialService.facebook && !!socialService.facebook.profileUrl;
+      }, function () {
+        connectionStatus.Facebook = socialService.facebook && !!socialService.facebook.profileUrl;
+        updateConnectDisplayStatus();
+      });
+
+      scope.$watch(function () {
+        return socialService.linkedin && !!socialService.linkedin.profileUrl;
+      }, function () {
+        connectionStatus.LinkedIn = socialService.linkedin && !!socialService.linkedin.profileUrl;
+        updateConnectDisplayStatus();
+      });
+
+      scope.$watch(function () {
+        return socialService.gmail && socialService.gmail.length > 0;
+      }, function () {
+        connectionStatus.Gmail = socialService.gmail && socialService.gmail.length > 0;
+        updateConnectDisplayStatus();
+      });
+
+      updateConnectDisplayStatus();
+    }
+  };
+}])
+
 .directive('kfNoFriendsOrConnectionsView', ['socialService', function (socialService) {
   return {
     replace: true,
@@ -48,6 +123,8 @@ angular.module('kifi.friends.rightColFriendsView', [])
     templateUrl: 'friends/noFriendsOrConnectionsView.tpl.html',
     link: function (scope) {
       scope.connectFacebook = socialService.connectFacebook;
+      scope.connectLinkedIn = socialService.connectLinkedIn;
+      scope.importGmail = socialService.importGmail;
     }
   };
 }]);
