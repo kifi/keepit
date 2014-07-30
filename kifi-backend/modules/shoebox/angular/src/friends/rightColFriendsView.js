2e76542b90e625db37b220a48bafe4e18af9416f
Yiping Liao <yiping@kifi.com>
2014-07-30 11:59:42 -0700

    Make changes per code review comments.

--- kifi-backend/modules/shoebox/angular/src/friends/rightColFriendsView.js
+++ kifi-backend/modules/shoebox/angular/src/friends/rightColFriendsView.js
@@ -9,37 +9,33 @@ angular.module('kifi.friends.rightColFriendsView', [])
     restrict: 'A',
     templateUrl: 'friends/compactFriendsView.tpl.html',
     link: function (scope/*, element, attrs*/) {
-      // Configuration.
-      scope.friendsDisplayLimit = 4;
-
-      // Get number of friends.
       scope.friendCount = friendService.totalFriends;
 
-      // Populate local friends list.
       friendService.getKifiFriends().then(function (data) {
         var actualFriends = _.filter(data, function (friend) {
-          // Attach friend's picture url to local friends list.
-          friend.pictureUrl = friendService.getPictureUrlForUser(friend);
-
-          // Filter out friends who have been unfriended.
           return !friend.unfriended;
         });
 
-        // Reorder local friends list to list friends with pics first.
-        var hasPicFriends = [];
-        var noPicFriends = [];
-
         actualFriends.forEach(function (friend) {
-          // TODO(yiping): figure out when we can remove the '0.jpg.jpg' check.
-          if ((friend.pictureName === '0.jpg') || (friend.pictureName === '0.jpg.jpg')) {
-            noPicFriends.push(friend);
-          } else {
-            hasPicFriends.push(friend);
-          }
+          friend.pictureUrl = friendService.getPictureUrlForUser(friend);
         });
 
-        // Expose local friends list on scope.
-        scope.friends = hasPicFriends.concat(noPicFriends);
+        var hasPicture = function (friend) {
+          return (friend.pictureName !== '0.jpg') && (friend.pictureName !== '0.jpg.jpg');
+        };
+        actualFriends.sort(function (friendA, friendB) {
+          if (hasPicture(friendA) && !hasPicture(friendB)) {
+            return -1;
+          }
+
+          if (!hasPicture(friendA) && hasPicture(friendB)) {
+            return 1;
+          }
+
+          return 0;
+        });
+
+        scope.friends = actualFriends;
       });
 
       scope.friendsLink = function () {
