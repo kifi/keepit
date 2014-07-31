e5fdf912da926aa0301a8c45ea6172f1f4cd0bb7
Aaron Hsu <aaron@kifi.com>
2014-07-30 18:12:50 -0700

    last one

--- kifi-backend/modules/common/conf/evolutions/shoebox/212.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/212.sql
@@ -5,7 +5,7 @@
 ALTER TABLE bookmark
 ADD CONSTRAINT bookmark_f_library_id
 FOREIGN KEY (library_id)
-REFERENCES library(id)
+REFERENCES library(id);
 
 insert into evolutions (name, description) values('212.sql', 'adding index to libraryId');
 
