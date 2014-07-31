c6d0174f6e45c94650e2ba31168fca1c8c12337c
Andrew Conner <git@andrewconner.org>
2014-07-31 12:42:47 -0700

    index_cdn support, with reactive streaming support

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/AngularAssets.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/AngularAssets.scala
@@ -1,16 +1,55 @@
 package com.keepit.controllers.website
 
+import java.io.StringWriter
+
 import controllers.AssetsBuilder
+import org.apache.commons.io.IOUtils
 import play.api.mvc.{ AnyContent, Action, Controller }
-import play.api.Play
+import play.api.{ Mode, Play }
 import play.api.libs.iteratee.Enumerator
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits._
 import com.keepit.common.logging.Logging
 
+import scala.concurrent.{ Promise, ExecutionContext, Future }
+import scala.util.{ Success, Try }
+
 object AngularDistAssets extends AssetsBuilder with Controller with Logging {
-  def home = Action {
-    Status(200).chunked(Enumerator.fromStream(Play.resourceAsStream("angular/index.html").get)) as HTML
+  private def index() = {
+    val fileStream = Play.resourceAsStream("angular/index_cdn.html").orElse(Play.resourceAsStream("angular/index.html")).get
+    val writer = new StringWriter()
+    IOUtils.copy(fileStream, writer, "UTF-8")
+    val fileStr = writer.toString
+    val closingBody = fileStr.lastIndexOf("</body>")
+    val beginning = fileStr.substring(0, closingBody)
+    val end = fileStr.substring(closingBody)
+    (Enumerator(beginning), Enumerator(end))
+  }
+  private lazy val cachedIndex = index()
+  private def maybeCachedIndex = {
+    if (Play.maybeApplication.exists(_.mode == Mode.Prod)) {
+      cachedIndex
+    } else {
+      index()
+    }
+  }
+
+  @inline
+  private def reactiveEnumerator(futureSeq: Seq[Future[String]]) = {
+    // Returns successful results of Futures in the order they are completed, reactively
+    Enumerator.interleave(futureSeq.map { future =>
+      Enumerator.flatten(future.map(r => Enumerator(r)))
+    })
+  }
+
+  @inline
+  private def augmentPage(splice: => Seq[Future[String]]) = {
+    val idx = maybeCachedIndex
+    idx._1.andThen(reactiveEnumerator(splice)).andThen(idx._2).andThen(Enumerator.eof)
+  }
+
+  def angularApp(splice: => Seq[Future[String]] = Seq()) = {
+    Ok.chunked(augmentPage(splice)).as(HTML)
   }
 }
 
