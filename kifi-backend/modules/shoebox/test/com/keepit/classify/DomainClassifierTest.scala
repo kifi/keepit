8c5b63e72b12abab4cb27f60cefc838a6ac5ad3a
eishay <eishay@gmail.com>
2014-08-01 09:30:42 -0700

    updating test to use db only injector Before: Finished in 4 seconds, 502 ms After: Finished in 2 seconds, 429 ms

--- kifi-backend/modules/shoebox/test/com/keepit/classify/DomainClassifierTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/classify/DomainClassifierTest.scala
@@ -1,26 +1,23 @@
 package com.keepit.classify
 
-import org.specs2.mutable.SpecificationLike
-
-import com.keepit.common.db.slick.Database
-import com.keepit.common.net.FakeHttpClientModule
-import com.keepit.inject._
-import com.keepit.test.{ ShoeboxApplicationInjector, ShoeboxApplication }
-
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
-import play.api.test.Helpers.running
-import com.keepit.common.mail.FakeMailModule
-import com.keepit.common.analytics.TestAnalyticsModule
 import com.keepit.common.actor.TestActorSystemModule
-import com.keepit.common.store.ShoeboxFakeStoreModule
-import com.keepit.shoebox.FakeShoeboxServiceModule
-import com.keepit.search.TestSearchServiceClientModule
-import com.keepit.akka.TestKitScope
+import com.keepit.common.analytics.TestAnalyticsModule
+import com.keepit.common.cache.{ HashMapMemoryCacheModule, ShoeboxCacheModule }
+import com.keepit.common.db.{ TestDbInfo, TestSlickModule }
+import com.keepit.common.db.slick.Database
 import com.keepit.common.healthcheck.FakeAirbrakeModule
+import com.keepit.common.mail.FakeMailModule
+import com.keepit.common.net.FakeHttpClientModule
+import com.keepit.common.store.ShoeboxFakeStoreModule
 import com.keepit.heimdal.TestHeimdalServiceClientModule
+import com.keepit.search.TestSearchServiceClientModule
+import com.keepit.shoebox.FakeShoeboxServiceModule
+import com.keepit.test.{ CommonTestInjector, DbInjectionHelper }
+import org.specs2.mutable.SpecificationLike
 
-class DomainClassifierTest extends TestKit(ActorSystem()) with SpecificationLike with ShoeboxApplicationInjector {
+class DomainClassifierTest extends TestKit(ActorSystem()) with SpecificationLike with CommonTestInjector with DbInjectionHelper {
 
   val domainClassifierTestModules = Seq(
     FakeMailModule(),
@@ -31,12 +28,14 @@ class DomainClassifierTest extends TestKit(ActorSystem()) with SpecificationLike
     TestActorSystemModule(Some(system)),
     FakeShoeboxServiceModule(),
     TestSearchServiceClientModule(),
+    TestSlickModule(TestDbInfo.dbInfo),
+    ShoeboxCacheModule(HashMapMemoryCacheModule()),
     FakeAirbrakeModule()
   )
 
   "The domain classifier" should {
     "use imported classifications and not fetch for known domains" in {
-      running(new ShoeboxApplication(domainClassifierTestModules :+ FakeHttpClientModule(): _*)) {
+      withDb(domainClassifierTestModules :+ FakeHttpClientModule(): _*) { implicit injector =>
         val classifier = inject[DomainClassifierImpl]
         val tagRepo = inject[DomainTagRepo]
         val importer = inject[DomainTagImporterImpl]
@@ -58,14 +57,14 @@ class DomainClassifierTest extends TestKit(ActorSystem()) with SpecificationLike
       }
     }
     "fetch if necessary" in {
-      running(new ShoeboxApplication(domainClassifierTestModules :+ FakeHttpClientModule {
+      withDb(domainClassifierTestModules :+ FakeHttpClientModule {
         case s if s.url.contains("yahoo.com") => "FR~Search engines"
         case s if s.url.contains("zdnet.com") => "FM~Technology and computers,News and magazines"
         case s if s.url.contains("schwab.com") => "FM~Business and services,Finance (Banks, Real estate, Insurance)"
         case s if s.url.contains("hover.com") => "FM~Business and services,Web hosting"
         case s if s.url.contains("42go.com") || s.url.contains("addepar.com") => "FM~Technology and computers"
         case s if s.url.contains("playboy.com") || s.url.contains("porn.com") => "FM~Porn"
-      }: _*)) {
+      }: _*) { implicit injector =>
         val classifier = inject[DomainClassifierImpl]
         val domainRepo = inject[DomainRepo]
         val tagRepo = inject[DomainTagRepo]
