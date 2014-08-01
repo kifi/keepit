a8dd29d347b8ac16c6530e26f216389cc33c20a6
eishay <eishay@gmail.com>
2014-07-31 21:07:07 -0700

    breaking shoebox-scraper client out of shoebox general (kitchen sink) client. both client and controllers are very large and its hard to understand who is using what and deal with timeouts in these scopes. next would be to break the huge controller and extract few calls like with the ShoeboxDataPipeController. the new service client has higher timeouts to reduce noise and be more tolerant for these calls

--- kifi-backend/modules/scraper/app/com/keepit/scraper/ScraperServiceModule.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/ScraperServiceModule.scala
@@ -1,13 +1,13 @@
 package com.keepit.scraper
 
 import com.keepit.common.cache.CacheModule
-import com.keepit.scraper.fetcher.HttpFetcherModule
-import com.keepit.social.RemoteSecureSocialModule
-import com.keepit.inject.{ CommonServiceModule, ConfigurationModule }
-import com.keepit.shoebox.ProdShoeboxServiceClientModule
-import com.keepit.common.store.StoreModule
 import com.keepit.common.concurrent.ForkJoinContextMonitorModule
+import com.keepit.common.store.StoreModule
+import com.keepit.inject.{ CommonServiceModule, ConfigurationModule }
 import com.keepit.scraper.embedly.EmbedlyModule
+import com.keepit.scraper.fetcher.HttpFetcherModule
+import com.keepit.shoebox.{ ShoeboxScraperClientModule, ShoeboxServiceClientModule }
+import com.keepit.social.RemoteSecureSocialModule
 
 abstract class ScraperServiceModule(
     val cacheModule: CacheModule,
@@ -16,7 +16,8 @@ abstract class ScraperServiceModule(
     val scrapeProcessorModule: ScrapeProcessorModule,
     val embedlyModule: EmbedlyModule) extends ConfigurationModule with CommonServiceModule {
   // Service clients
-  val shoeboxServiceClientModule = ProdShoeboxServiceClientModule()
+  val shoeboxServiceClientModule: ShoeboxServiceClientModule
+  val shoeboxScraperClientModule: ShoeboxScraperClientModule
   val secureSocialModule = RemoteSecureSocialModule()
 
   val fetcherModule: HttpFetcherModule
