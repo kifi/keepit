45094f64249b4fcb18a687adda84c677305a4d47
Stephen Kemmerling <stephen@42go.com>
2014-08-01 09:56:16 -0700

    ServiceDiscoveryLiveTest needs neither application nor db

--- kifi-backend/modules/common/test/com/keepit/common/zookeeper/ServiceDiscoveryLiveTest.scala
+++ kifi-backend/modules/common/test/com/keepit/common/zookeeper/ServiceDiscoveryLiveTest.scala
@@ -17,9 +17,9 @@ import com.google.inject.util._
 import com.google.inject.Injector
 import akka.actor.Scheduler
 
-class ServiceDiscoveryLiveTest extends Specification with ApplicationInjector {
+class ServiceDiscoveryLiveTest extends Specification with CommonTestInjector {
 
-  args(skipAll = true)
+  //args(skipAll = true)
 
   implicit val amazonInstanceInfoFormat = AmazonInstanceInfo.format
 
@@ -43,7 +43,7 @@ class ServiceDiscoveryLiveTest extends Specification with ApplicationInjector {
   "discovery" should {
 
     "register" in {
-      running(new CommonTestApplication(TestActorSystemModule())) {
+      withInjector(TestActorSystemModule()) { implicit injector =>
         val services = new FortyTwoServices(inject[Clock], Mode.Test, None, None, inject[FortyTwoConfig]) {
           override lazy val currentService: ServiceType = ServiceType.SHOEBOX
         }
