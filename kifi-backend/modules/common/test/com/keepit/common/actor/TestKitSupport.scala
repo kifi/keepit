e3db07eb42c72ee7ffd2de7bfebebe629ce90236
Raymond Ng <raymond.ng@gmail.com>
2014-08-01 11:24:04 -0700

    NormalizationServiceTest <- TestKitSupport

--- kifi-backend/modules/common/test/com/keepit/common/actor/TestKitSupport.scala
+++ kifi-backend/modules/common/test/com/keepit/common/actor/TestKitSupport.scala
@@ -3,8 +3,9 @@ package com.keepit.common.actor
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
 import org.specs2.specification.After
+import org.specs2.time.NoTimeConversions
 
 // shutdown() is required to ensure there's no memory leak
-abstract class TestKitSupport extends TestKit(ActorSystem()) with After {
+abstract class TestKitSupport extends TestKit(ActorSystem()) with After with NoTimeConversions {
   def after = system.shutdown
 }
