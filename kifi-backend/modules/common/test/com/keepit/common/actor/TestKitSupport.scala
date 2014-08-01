66014b680020047b1b34c6c013b0dc6e13aa3ad3
Raymond Ng <raymond.ng@gmail.com>
2014-08-01 16:09:12 -0700

    pass along system

--- kifi-backend/modules/common/test/com/keepit/common/actor/TestKitSupport.scala
+++ kifi-backend/modules/common/test/com/keepit/common/actor/TestKitSupport.scala
@@ -7,7 +7,7 @@ import org.specs2.time.NoTimeConversions
 import play.api.Play
 
 // shutdown() is required to ensure there's no memory leak
-abstract class TestKitSupport extends TestKit(ActorTestSupport.getActorSystem()) with After with NoTimeConversions {
+abstract class TestKitSupport(testSystem: ActorSystem = ActorTestSupport.getActorSystem()) extends TestKit(testSystem) with After with NoTimeConversions {
   def after = system.shutdown
 }
 
