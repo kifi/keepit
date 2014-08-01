afbb8bf564d4c97e8762a64fe045744d3cc41dc1
Martin Raison <martin@42go.com>
2014-07-31 18:22:15 -0700

    mapKeys for Map and convert for Format[T]

--- kifi-backend/modules/common/app/com/keepit/common/collection/package.scala
+++ kifi-backend/modules/common/app/com/keepit/common/collection/package.scala
@@ -2,13 +2,15 @@ package com.keepit.common
 
 package object collection {
 
-  implicit class PimpedMap[A, B](x: Map[A, B]) {
+  implicit class PimpedMap[A, B](val m: Map[A, B]) {
     /**
      * Adds given (key, value) pairs to the map, where each value is wrapped in an Option.
      * For each value, if the Option is None, the pair is dropped.
      */
     def withOpt(optElems: (A, Option[B])*) = {
-      this.x ++ optElems.collect { case (a, b) if b.nonEmpty => (a, b.get) }.toMap
+      m ++ optElems.collect { case (a, b) if b.nonEmpty => (a, b.get) }.toMap
     }
+
+    def mapKeys[C](f: A => C) = m.map { case (k, v) => (f(k), v) }
   }
 }
