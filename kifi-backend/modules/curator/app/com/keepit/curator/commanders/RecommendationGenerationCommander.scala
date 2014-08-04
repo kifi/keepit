87ba1c2a78152613dd646d7140f319cf351290ba
Stephen Kemmerling <stephen@42go.com>
2014-08-04 15:07:06 -0700

    Batch Recommandation Generation - Storage

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/RecommendationGenerationCommander.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/RecommendationGenerationCommander.scala
@@ -1,10 +1,12 @@
 package com.keepit.curator.commanders
 
-import com.keepit.curator.model.Recommendation
+import com.keepit.curator.model.{ Recommendation, UserRecommendationGenerationStateRepo, UserRecommendationGenerationState, Keepers, ScoredSeedItem }
 import com.keepit.common.db.Id
 import com.keepit.model.ScoreType._
 import com.keepit.model.{ ScoreType, User }
 import com.keepit.shoebox.ShoeboxServiceClient
+import com.keepit.common.concurrent.ReactiveLock
+import com.keepit.common.db.slick.Database
 
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json.Json
@@ -17,9 +19,41 @@ import com.google.inject.{ Inject, Singleton }
 class RecommendationGenerationCommander @Inject() (
     seedCommander: SeedIngestionCommander,
     shoebox: ShoeboxServiceClient,
-    scoringHelper: UriScoringHelper) {
+    scoringHelper: UriScoringHelper,
+    db: Database,
+    genStateRepo: UserRecommendationGenerationStateRepo) {
 
   val defaultScore = 0.0f
+
+  val recommendationGenerationLock = new ReactiveLock(3)
+
+  val FEED_PRECOMPUTATION_WHITELIST: Seq[Id[User]] = Seq(
+    1, //Eishay
+    3, //Andrew
+    7, //Yasu
+    9, //Danny
+    48, //Jared
+    61, //Jen
+    100, //Tamila
+    115, //Yingjie
+    134, //Léo
+    243, //Stephen
+    460, //Ray
+    1114, //Martin
+    2538, //Mark
+    3466, //JP
+    6498, //Tan
+    6622, //David
+    7100, //Aaron
+    7456, //Josh
+    7589, //Lydia
+    8465, //Yiping
+    8476 //Tommy
+  ).map(Id[User](_)) //will go away once we release, just saving some computation/time for now
+
+  private def computeMasterScore(scoredItem: ScoredSeedItem): Float = {
+    0.3f * scoredItem.uriScores.socialScore + 2 * scoredItem.uriScores.overallInterestScore + 0.5f * scoredItem.uriScores.priorScore
+  }
 
   def getAdHocRecommendations(userId: Id[User], howManyMax: Int, scoreCoefficients: Map[ScoreType.Value, Float]): Future[Seq[Recommendation]] = {
     val seedsFuture = for {
@@ -36,7 +70,7 @@ class RecommendationGenerationCommander @Inject() (
           userId = scoredItem.userId,
           uriId = scoredItem.uriId,
           score = if (scoreCoefficients.isEmpty)
-            0.3f * scoredItem.uriScores.socialScore + 2 * scoredItem.uriScores.overallInterestScore + 0.5f * scoredItem.uriScores.priorScore
+            computeMasterScore(scoredItem)
           else
             scoreCoefficients.getOrElse(ScoreType.recencyScore, defaultScore) * scoredItem.uriScores.recencyScore
               + scoreCoefficients.getOrElse(ScoreType.overallInterestScore, defaultScore) * scoredItem.uriScores.overallInterestScore
@@ -49,6 +83,49 @@ class RecommendationGenerationCommander @Inject() (
       }.sortBy(-1 * _.score).take(howManyMax)
     }
 
+  }
+
+  private def precomputeRecommendationsForUser(userId: Id[User]): Unit = recommendationGenerationLock.withLockFuture {
+    val state = db.readOnlyMaster { implicit session =>
+      genStateRepo.getByUserId(userId)
+    } getOrElse {
+      UserRecommendationGenerationState(userId = userId)
+    }
+    val seedsFuture = for {
+      seeds <- seedCommander.getBySeqNumAndUser(state.seq, userId, 200)
+      restrictions <- shoebox.getAdultRestrictionOfURIs(seeds.map { _.uriId })
+    } yield {
+      (seeds zip restrictions) filterNot (_._2) map (_._1)
+    }
+
+    val res: Future[Boolean] = seedsFuture.flatMap { seedItems =>
+      val newState = state.copy(seq = seedItems.map(_.seq).max)
+      val cleanedItems = seedItems.filter { seedItem => //discard super popular items and the users own keeps
+        seedItem.keepers match {
+          case Keepers.ReasonableNumber(users) => !users.contains(userId)
+          case _ => false
+        }
+      }
+      scoringHelper(cleanedItems).map { scoredItems =>
+        val toBeSavedItems = scoredItems.filter(computeMasterScore(_) > 0.5)
+        db.readWrite { implicit session =>
+          //actually saving the recommendation when Tans storage code is merged
+          genStateRepo.save(newState)
+        }
+        if (!cleanedItems.isEmpty) {
+          precomputeRecommendationsForUser(userId)
+          true
+        } else false
+      }
+
+    }
+    res
+  }
+
+  def precomputeRecommendations(): Unit = {
+    if (recommendationGenerationLock.waiting < FEED_PRECOMPUTATION_WHITELIST.length) {
+      FEED_PRECOMPUTATION_WHITELIST.map(precomputeRecommendationsForUser)
+    }
   }
 
 }
