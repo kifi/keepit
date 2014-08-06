bf6c7e9b411f634bd48fa06960d6bff939b1b30f
Yiping Liao <yiping@kifi.com>
2014-08-06 09:24:02 -0700

    Use minimal template for friends unit tests.

--- kifi-backend/modules/shoebox/angular/test/unit/friends/rightColFriends.spec.js
+++ kifi-backend/modules/shoebox/angular/test/unit/friends/rightColFriends.spec.js
@@ -10,23 +10,16 @@ describe('kifi.friends.rightColFriendsView', function () {
       scope,
       friendService;
 
-  tpl = 
-    '<div ng-if="friends.length > 0">' +
-      '<div class="kf-rightcol-friends-links">' +
-        '<div><ng-pluralize count="friendCount()" when="{\'one\': \'1 friend\', \'other\': \'{} friends\'}"></ng-pluralize></div>' +
-        '<a ng-href="{{friendsLink()}}">See all &#0187;</a>' +
-      '</div>' +
-      '<div class="kf-compact-friends-image-row">' +
-        '<a href="/friends" ng-repeat="friend in friends | limitTo: 4">' +
-          '<img class="kf-compact-friend-image" title="{{friend.firstName}}" ng-src="{{friend.pictureUrl}}">' +
-        '</a>' +
-      '</div>' +
-    '</div>';
+  tpl = '<div kf-compact-friends-view></div>';
 
   function compile() {
     elem = angular.element(tpl);
     scope = $rootScope.$new();
     $compile(elem)(scope);
+  }
+
+  function mockPromise() {
+    return $injector.get('$q').defer().promise;
   }
 
   beforeEach(module('kifi.friends.rightColFriendsView'));
@@ -46,9 +39,12 @@ describe('kifi.friends.rightColFriendsView', function () {
 
     it('should have the correct friend count text', function () {
       spyOn(friendService, 'totalFriends').andReturn(2);
-      scope.friendCount = friendService.totalFriends;
+      spyOn(friendService, 'getKifiFriends').andReturn(mockPromise());
+
+      // Hardcode this to be a positive number so ng-if will be true
+      // for this test.
       scope.friends = [1, 2];
-      
+
       scope.$digest();
 
       // With a combination of replace being true and ng-if, the
