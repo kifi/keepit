b8bfb0aac0ca9a4e1ce143309fe29ba4f92e0dcd
Tan Lin <tan@kifi.com>
2014-08-06 18:26:01 -0700

    Updating

--- kifi-backend/modules/common/app/com/keepit/common/util/MapFormatUtil.scala
+++ kifi-backend/modules/common/app/com/keepit/common/util/MapFormatUtil.scala
@@ -7,10 +7,6 @@ import com.keepit.common.json._
 
 object MapFormatUtil {
 
-  //  Example of how to convert customized key to json when using a map
-  //  implicit def scoreTypeMapFormat(implicit f: Format[Map[String, Float]]): Format[Map[ScoreType.Value, Float]] =
-  //    mapKeyFormat(ScoreType.withName _, _.toString)
-
   def mapKeyFormat[A, B, C](aToC: A => C, cToA: C => A)(implicit f: Format[Map[A, B]]): Format[Map[C, B]] =
     f.convert(_.mapKeys(aToC), _.mapKeys(cToA))
 
