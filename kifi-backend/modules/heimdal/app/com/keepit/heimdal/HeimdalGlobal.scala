969e8fec388e19dcc1c5441ecc814ba0359fb297
Raymond Ng <ray@42go.com>
2014-08-07 17:49:32 -0700

    AttributionCommander & ReKeepStatsUpdater (heimdal)

--- kifi-backend/modules/heimdal/app/com/keepit/heimdal/HeimdalGlobal.scala
+++ kifi-backend/modules/heimdal/app/com/keepit/heimdal/HeimdalGlobal.scala
@@ -3,6 +3,7 @@ package com.keepit.heimdal
 import com.keepit.FortyTwoGlobal
 import com.keepit.common.cache.{ InMemoryCachePlugin, FortyTwoCachePlugin }
 import com.keepit.common.healthcheck._
+import com.keepit.helprank.ReKeepStatsUpdaterPlugin
 import play.api.Mode._
 import play.api._
 import com.keepit.controllers.EventTrackingController
@@ -26,5 +27,6 @@ trait HeimdalServices { self: FortyTwoGlobal =>
     require(injector.instance[FortyTwoCachePlugin] != null) //make sure its not lazy loaded
     require(injector.instance[InMemoryCachePlugin] != null) //make sure its not lazy loaded
     require(injector.instance[DelightedPlugin] != null) //make sure its not lazy loaded
+    require(injector.instance[ReKeepStatsUpdaterPlugin] != null) //make sure its not lazy loaded
   }
 }
