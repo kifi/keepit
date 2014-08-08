646a3baaefb46889ad526f63f4cb61d93838c2cd
Tan Lin <tan@kifi.com>
2014-08-08 16:11:48 -0700

    Adding correct evolution file

---
+++ kifi-backend/modules/common/conf/evolutions/shoebox/223.sql
@@ -0,0 +1,13 @@
+# CURATOR
+
+# --- !Ups
+
+ALTER TABLE curator_keep_info
+  ADD COLUMN discoverable boolean NOT NULL DEFAULT false;
+
+ALTER TABLE raw_seed_item
+  ADD COLUMN discoverable boolean NOT NULL DEFAULT false;
+
+INSERT INTO evolutions (name, description) VALUES('223.sql', 'add discoverable columns to curator_keep_info & raw_seed_item_sequence table');
+
+# --- !Downs
