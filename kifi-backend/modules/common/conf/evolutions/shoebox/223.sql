f53a4f067d82351643f6e082b576fbad59ba9a27
Tan Lin <tan@kifi.com>
2014-08-08 12:46:44 -0700

    Updating

--- kifi-backend/modules/common/conf/evolutions/shoebox/223.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/223.sql
@@ -8,4 +8,4 @@ ALTER TABLE curator_keep_info
 ALTER TABLE raw_seed_item_sequence
   ADD COLUMN discoverable boolean NOT NULL DEFAULT false;
 
-INSERT INTO evolutions (name, description) VALUES('222.sql', 'add discoverable columns to curator_keep_info & raw_seed_item_sequence table');
+INSERT INTO evolutions (name, description) VALUES('223.sql', 'add discoverable columns to curator_keep_info & raw_seed_item_sequence table');
