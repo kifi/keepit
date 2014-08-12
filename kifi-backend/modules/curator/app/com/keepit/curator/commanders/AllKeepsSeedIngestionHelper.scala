3052983c733828810077abaa7ca5ba9e17c4390e
Tan Lin <tan@kifi.com>
2014-08-12 15:03:59 -0700

    Adding check state condition for check discoverability

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/AllKeepsSeedIngestionHelper.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/AllKeepsSeedIngestionHelper.scala
@@ -105,7 +105,7 @@ class AllKeepSeedIngestionHelper @Inject() (
 
       val discoverable = {
         {
-          if (keepInfo.discoverable && keep.isPrivate) keepInfoRepo.checkDiscoverableByUriId(keep.uriId)
+          if (keepInfo.discoverable && (keep.isPrivate && keep.state == KeepStates.ACTIVE)) keepInfoRepo.checkDiscoverableByUriId(keep.uriId)
           else keepInfo.discoverable || (!keep.isPrivate && keep.state == KeepStates.ACTIVE)
         } && {
           if (keepInfo.state == CuratorKeepInfoStates.ACTIVE && keep.state != KeepStates.ACTIVE)
