cce1f5bfcb2b254c958dbce40a5ae549374ef7d9
Tan Lin <tan@kifi.com>
2014-08-14 10:14:29 -0700

    Simplify UriWeightingHelper

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/UriWeightingHelper.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/UriWeightingHelper.scala
@@ -4,32 +4,26 @@ import com.keepit.curator.model.{ WeightedSeedItem, SeedItem }
 
 class UriWeightingHelper() {
 
-  val penalizeScore = Seq(
+  val weightScore = Seq(
+    //----------------------------------Penalize---------------------------------------------------------------------------
     ("""(?i)\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*twitter.com[-A-Za-z0-9+&@#/%=~_|]""".r, 0.01f, "Twitter"),
     ("""(?i)\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*linkedin.com[-A-Za-z0-9+&@#/%=~_|]""".r, 0.01f, "LinkedIn"),
     ("""(?i)\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*mail.google.com[-A-Za-z0-9+&@#/%=~_|]""".r, 0.001f, "Google Mail"),
     ("""(?i)\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*wikipedia.org[-A-Za-z0-9+&@#/%=~_|]""".r, 0.1f, "Wikipedia"),
-    ("""(?i)\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*facebook.com[-A-Za-z0-9+&@#/%=~_|]""".r, 0.001f, "Facebook")
-  )
+    ("""(?i)\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*facebook.com[-A-Za-z0-9+&@#/%=~_|]""".r, 0.001f, "Facebook"),
 
-  val boostScore = Seq(
+    //----------------------------------Boost--------------------------------------------------------------------------------
     ("""(?i)\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*techcrunch.com[-A-Za-z0-9+&@#/%=~_|]""".r, 1.2f, "Techcrunch")
   )
 
   def apply(items: Seq[SeedItem]): Seq[WeightedSeedItem] = items.map { item =>
     var weight = 1.0f
-    penalizeScore.foreach { reg =>
+    weightScore.foreach(reg =>
       weight = reg._1.findFirstIn(item.url) match {
         case Some(data) => reg._2
         case None => weight
       }
-    }
-    boostScore.foreach { reg =>
-      weight = reg._1.findFirstIn(item.url) match {
-        case Some(data) => reg._2
-        case None => weight
-      }
-    }
+    )
 
     WeightedSeedItem(
       weightMultiplier = weight,
