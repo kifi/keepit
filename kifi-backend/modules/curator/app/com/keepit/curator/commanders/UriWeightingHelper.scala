440063a0ee869bb54802d1cf7e8ab82c020ccd26
Tan Lin <tan@kifi.com>
2014-08-14 13:21:18 -0700

    Updating per cr

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/UriWeightingHelper.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/UriWeightingHelper.scala
@@ -1,6 +1,7 @@
 package com.keepit.curator.commanders
 
 import com.keepit.curator.model.{ WeightedSeedItem, SeedItem }
+import com.google.inject.{ Singleton }
 
 import scala.util.matching.Regex
 
@@ -9,15 +10,16 @@ case class UrlPattern(
   weight: Float,
   description: String)
 
+@Singleton
 class UriWeightingHelper() {
 
   val scoringMultiplier = Seq(
     //----------------------------------Penalize---------------------------------------------------------------------------
-    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*twitter.com[-A-Za-z0-9+&@#/%=~_|]""".r, 0.01f, "Twitter"),
-    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*linkedin.com[-A-Za-z0-9+&@#/%=~_|]""".r, 0.01f, "LinkedIn"),
-    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*mail.google.com[-A-Za-z0-9+&@#/%=~_|]""".r, 0.001f, "Google Mail"),
-    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*wikipedia.org[-A-Za-z0-9+&@#/%=~_|]""".r, 0.1f, "Wikipedia"),
-    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*facebook.com[-A-Za-z0-9+&@#/%=~_|]""".r, 0.001f, "Facebook"),
+    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*twitter.com*""".r, 0.01f, "Twitter"),
+    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*linkedin.com*""".r, 0.01f, "LinkedIn"),
+    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*mail.google.com*""".r, 0.001f, "Google Mail"),
+    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*wikipedia.org*""".r, 0.1f, "Wikipedia"),
+    UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*facebook.com*""".r, 0.001f, "Facebook"),
 
     //----------------------------------Boost--------------------------------------------------------------------------------
     UrlPattern("""^https?://[-A-Za-z0-9+&@#/%?=~_|.]*techcrunch.com[-A-Za-z0-9+&@#/%=~_|]""".r, 1.2f, "Techcrunch")
