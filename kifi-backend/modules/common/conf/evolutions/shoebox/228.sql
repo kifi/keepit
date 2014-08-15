9860d3d451e0ce88ec3079b6375d65ecef4a2d5b
yingjieMiao <yingjie@42go.com>
2014-08-15 16:37:12 -0700

    rename evolution

--- kifi-backend/modules/common/conf/evolutions/shoebox/228.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/228.sql
@@ -8,6 +8,6 @@ alter table uri_lda_topic
 alter table uri_lda_topic
     add column times_first_topic_changed smallint unsigned DEFAULT 0;
 
-insert into evolutions (name, description) values('227.sql', 'add first_topic_score to uri_lda_topic');
+insert into evolutions (name, description) values('228.sql', 'add first_topic_score to uri_lda_topic');
 
 # --- !Downs
