1b4e9c2c757bdaeb9acc3d5018a31db5d345689a
Stephen Kemmerling <stephen@42go.com>
2014-08-15 11:20:00 -0700

    Making multiplier optional since otherwise all current data in the db fails to deserialize

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/UriScoringHelper.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/UriScoringHelper.scala
@@ -120,7 +120,7 @@ class UriScoringHelper @Inject() (
             priorScore = priorScores(i),
             rekeepScore = rekeepScores(i),
             discoveryScore = discoveryScores(i),
-            multiplier = items(i).multiplier
+            multiplier = Some(items(i).multiplier)
           )
           ScoredSeedItem(items(i).userId, items(i).uriId, scores)
         }
