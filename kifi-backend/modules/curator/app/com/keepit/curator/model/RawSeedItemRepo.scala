67ce2b2f690efb51e1f9fdb8ea55f7b8a67b5bdb
Stephen Kemmerling <stephen@42go.com>
2014-08-15 11:55:20 -0700

    changing order in the object as well to make tests work

--- kifi-backend/modules/curator/app/com/keepit/curator/model/RawSeedItemRepo.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/model/RawSeedItemRepo.scala
@@ -49,7 +49,7 @@ class RawSeedItemRepoImpl @Inject() (
     def priorScore = column[Float]("prior_score", O.Nullable)
     def timesKept = column[Int]("times_kept", O.NotNull)
     def discoverable = column[Boolean]("discoverable", O.NotNull)
-    def * = (id.?, createdAt, updatedAt, seq, uriId, url, userId.?, firstKept, lastKept, lastSeen, priorScore.?, timesKept, discoverable) <> ((RawSeedItem.apply _).tupled, RawSeedItem.unapply _)
+    def * = (id.?, createdAt, updatedAt, seq, uriId, userId.?, firstKept, lastKept, lastSeen, priorScore.?, timesKept, discoverable, url) <> ((RawSeedItem.apply _).tupled, RawSeedItem.unapply _)
   }
 
   def table(tag: Tag) = new RawSeedItemTable(tag)
