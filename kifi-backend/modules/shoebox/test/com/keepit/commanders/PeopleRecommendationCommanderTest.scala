dccf852413497077d3a2bfae6c89aa2c05d1b0ce
Josh McDade <josh@kifi.com>
2014-08-15 11:25:22 -0700

    mobile endpoint and tests for PYMK
    introduced ShoeboxTestFactory as a way to make the setup code in our tests more DRY.
    In many tests, setup code is often duplicated and takes up the much more LOC than the actual testing code.

--- kifi-backend/modules/shoebox/test/com/keepit/commanders/PeopleRecommendationCommanderTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/commanders/PeopleRecommendationCommanderTest.scala
@@ -1,7 +1,6 @@
 package com.keepit.commanders
 
 import com.keepit.abook.{ FakeABookServiceClientModule, FakeABookServiceClientImpl, ABookServiceClient }
-import com.keepit.model.{ UserConnection, User, UserRepo, UserConnectionRepo }
 import com.keepit.test.ShoeboxTestInjector
 import org.specs2.mutable.Specification
 
@@ -14,44 +13,28 @@ class PeopleRecommendationCommanderTest extends Specification with ShoeboxTestIn
     FakeABookServiceClientModule()
   )
 
-  "PeopleRecommendationCommanderTest" should {
+  "PeopleRecommendationCommander" should {
     "getFriendRecommendations" should {
       "return users and mutual users friend counts" in {
         withDb(modules: _*) { implicit injector =>
-          val userConnRepo = inject[UserConnectionRepo]
-          val userRepo = inject[UserRepo]
-          val (user1, user2, user3, user4) = db.readWrite { implicit rw =>
-            val user1 = userRepo.save(User(firstName = "Aaron", lastName = "Paul"))
-            val user2 = userRepo.save(User(firstName = "Bryan", lastName = "Cranston"))
-            val user3 = userRepo.save(User(firstName = "Anna", lastName = "Gunn"))
-            val user4 = userRepo.save(User(firstName = "Dean", lastName = "Norris"))
-
-            userConnRepo.save(UserConnection(user1 = user2.id.get, user2 = user3.id.get))
-            userConnRepo.save(UserConnection(user1 = user2.id.get, user2 = user4.id.get))
-            userConnRepo.save(UserConnection(user1 = user3.id.get, user2 = user4.id.get))
-            userConnRepo.save(UserConnection(user1 = user1.id.get, user2 = user2.id.get))
-            userConnRepo.save(UserConnection(user1 = user1.id.get, user2 = user3.id.get))
-
-            (user1, user2, user3, user4)
-          }
-
+          val users = db.readWrite { implicit rw => testFactory.createUsersWithConnections() }
           val abook = inject[ABookServiceClient].asInstanceOf[FakeABookServiceClientImpl]
-          abook.addFriendRecommendationsExpectations(user1.id.get, Seq(user2.id.get, user3.id.get, user4.id.get))
+          abook.addFriendRecommendationsExpectations(users(0).id.get, Seq(users(1).id.get, users(2).id.get, users(3).id.get))
 
-          val friendRecoDataF = inject[PeopleRecommendationCommander].getFriendRecommendations(user1.id.get, 2, 25)
+          val friendRecoDataF = inject[PeopleRecommendationCommander].getFriendRecommendations(users(0).id.get, 2, 25)
           val friendRecoData = Await.result(friendRecoDataF, Duration(5, "seconds"))
 
           friendRecoData.basicUsers === Map(
-            user2.id.get -> com.keepit.social.BasicUser.fromUser(user2),
-            user3.id.get -> com.keepit.social.BasicUser.fromUser(user3),
-            user4.id.get -> com.keepit.social.BasicUser.fromUser(user4)
+            users(1).id.get -> com.keepit.social.BasicUser.fromUser(users(1)),
+            users(2).id.get -> com.keepit.social.BasicUser.fromUser(users(2)),
+            users(3).id.get -> com.keepit.social.BasicUser.fromUser(users(3))
           )
-          friendRecoData.recommendedUsers === Seq(user2.id.get, user3.id.get, user4.id.get)
-          friendRecoData.mutualFriendConnectionCounts === Map(user2.id.get -> 3, user3.id.get -> 3)
+          friendRecoData.recommendedUsers === Seq(users(1).id.get, users(2).id.get, users(3).id.get)
+          friendRecoData.mutualFriendConnectionCounts === Map(users(1).id.get -> 3, users(2).id.get -> 3)
           friendRecoData.mutualFriends === Map(
-            user2.id.get -> Set(user3.id.get),
-            user3.id.get -> Set(user2.id.get),
-            user4.id.get -> Set(user2.id.get, user3.id.get)
+            users(1).id.get -> Set(users(2).id.get),
+            users(2).id.get -> Set(users(1).id.get),
+            users(3).id.get -> Set(users(1).id.get, users(2).id.get)
           )
         }
       }
