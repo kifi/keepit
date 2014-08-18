738d63e5f6a9c4d3f3593fa090e8b891ddb5cb07
Raymond Ng <ray@42go.com>
2014-08-18 11:06:49 -0700

    "move" tables to heimdal

--- kifi-backend/modules/common/conf/evolutions/shoebox/160.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/160.sql
@@ -1,15 +1,12 @@
-# SHOEBOX
+# HEIMDAL
 
 # --- !Ups
 
 -- MySQL:
--- ALTER TABLE keep_click DROP FOREIGN KEY keep_click_clicker_id
-ALTER TABLE keep_click DROP CONSTRAINT keep_click_clicker_id;
 ALTER TABLE keep_click DROP COLUMN clicker_id;
 
 -- MySQL:
 -- ALTER TABLE keep_click DROP INDEX keep_click_uuid;
-ALTER TABLE keep_click DROP CONSTRAINT IF EXISTS keep_click_uuid;
 ALTER TABLE keep_click DROP search_uuid;
 
 ALTER TABLE keep_click ADD COLUMN hit_uuid varchar(36) NOT NULL;
