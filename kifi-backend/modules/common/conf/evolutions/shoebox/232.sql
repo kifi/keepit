749a9524c94cac7f62ae5bc8676298a7f06b8ba8
Aaron Hsu <aaron@kifi.com>
2014-08-18 10:29:49 -0700

    indexes on library_invite keys

---
+++ kifi-backend/modules/common/conf/evolutions/shoebox/232.sql
@@ -0,0 +1,13 @@
+# SHOEBOX
+
+# --- !Ups
+
+CREATE INDEX library_invite_i_email_address
+ON library_invite (email_address);
+CREATE INDEX library_invite_i_auth_token
+ON library_invite (auth_token);
+
+
+INSERT INTO evolutions (name, description) VALUES('232.sql', 'add indexes on email addresses & authentication tokens');
+
+# --- !Downs
