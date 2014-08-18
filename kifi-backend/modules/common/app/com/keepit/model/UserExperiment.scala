1d1d0390d2ef72b1d2cfe2985799304635c29ce5
Andrew Conner <git@andrewconner.org>
2014-08-18 16:34:56 -0700

    wip ng libs

--- kifi-backend/modules/common/app/com/keepit/model/UserExperiment.scala
+++ kifi-backend/modules/common/app/com/keepit/model/UserExperiment.scala
@@ -62,13 +62,14 @@ object ExperimentType {
   val DIGEST_EMAIl = ExperimentType("digest_email")
   val RECOS_BETA = ExperimentType("recos_beta")
   val SPECIAL_CURATOR = ExperimentType("special_curator")
+  val LIBRARIES = ExperimentType("libraries")
 
   val _ALL = ADMIN :: AUTO_GEN :: FAKE :: NO_SEARCH_EXPERIMENTS :: NOT_SENSITIVE ::
     CAN_MESSAGE_ALL_USERS :: DEMO :: EXTENSION_LOGGING :: SHOW_HIT_SCORES :: SHOW_DISCUSSIONS ::
     MOBILE_REDIRECT :: GUIDE :: DELIGHTED_SURVEY_PERMANENT ::
-    DIGEST_EMAIl :: RECOS_BETA :: SPECIAL_CURATOR :: Nil
+    DIGEST_EMAIl :: RECOS_BETA :: SPECIAL_CURATOR :: LIBRARIES :: Nil
 
-  private val _ALL_MAP: Map[String, ExperimentType] = _ALL map { e => e.value -> e } toMap
+  private val _ALL_MAP: Map[String, ExperimentType] = _ALL.map(e => e.value -> e).toMap
 
   def get(str: String): ExperimentType = _ALL_MAP(str.toLowerCase.trim)
 
