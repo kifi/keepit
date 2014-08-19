01eedc356f89c03fbaf77ee0e3a0369861936ec6
Yiping Liao <yiping@kifi.com>
2014-08-19 14:23:51 -0700

    Fix build (increase Protractor page sync timeout).

--- kifi-backend/modules/shoebox/angular/test/protractor.conf.js
+++ kifi-backend/modules/shoebox/angular/test/protractor.conf.js
@@ -4,8 +4,12 @@ module.exports.config = {
     browserName: 'chrome'
   },
 
-
   // Locally, set protractor.kifi.com to point to where the local server
   // is running (usually localhost).
-  baseUrl: 'http://protractor.kifi.com:9080/'
+  baseUrl: 'http://protractor.kifi.com:9080/',
+
+  // Increase protractor timeout for page synchronization to 15 seconds.
+  // (default is 11 seconds)
+  // See: https://github.com/angular/protractor/blob/master/docs/timeouts.md
+  allScriptsTimeout: 15000
 };
