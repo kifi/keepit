2d13162be4266f7bd1cd21a4b686f841d4e20bd3
yingjieMiao <yingjie@42go.com>
2014-08-20 12:48:31 -0700

    remove unused file

--- kifi-backend/modules/curator/app/com/keepit/controllers/curator/CuratorController.scala
+++
@@ -1,9 +0,0 @@
-package com.keepit.controllers.curator
-
-import com.google.inject.Inject
-import com.keepit.common.controller.CuratorServiceController
-import play.api.libs.concurrent.Execution.Implicits._
-
-class CuratorController @Inject() () extends CuratorServiceController {
-
-}
