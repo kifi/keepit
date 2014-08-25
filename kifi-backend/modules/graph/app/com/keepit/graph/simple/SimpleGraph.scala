c4bd0b9a919ebf98ce4a0e2db2ac1d1ffb7ea79b
Léo Grimaldi <leo@42go.com>
2014-08-25 11:17:34 -0700

    More logging of graph serialization exceptions

--- kifi-backend/modules/graph/app/com/keepit/graph/simple/SimpleGraph.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/simple/SimpleGraph.scala
@@ -7,6 +7,7 @@ import com.keepit.graph.manager.GraphStatistics
 import java.io.File
 import org.apache.commons.io.{ LineIterator, FileUtils }
 import scala.collection.JavaConversions._
+import com.keepit.common.logging.Logging
 
 case class SimpleGraph(vertices: ConcurrentMap[VertexId, MutableVertex] = TrieMap()) {
 
@@ -34,7 +35,7 @@ case class SimpleGraph(vertices: ConcurrentMap[VertexId, MutableVertex] = TrieMa
   def readOnly[T](f: GraphReader => T): T = f(reusableGraphReader)
 }
 
-object SimpleGraph {
+object SimpleGraph extends Logging {
   implicit val vertexFormat = MutableVertex.lossyFormat
 
   def write(graph: SimpleGraph, graphFile: File): Unit = {
@@ -69,28 +70,38 @@ object SimpleGraph {
 
   def checkVertexIntegrity(graph: SimpleGraph, vertexId: VertexId, vertex: Vertex): Unit = {
 
+    var errors: List[Throwable] = Nil
+
     if (vertex.data.kind != vertexId.kind || vertex.data.id != vertexId.asId(vertexId.kind)) {
-      throw new IllegalStateException(s"Invalid vertex id $vertexId for vertex of kind ${vertex.data.kind} with id ${vertex.data.id}")
+      errors :+= new IllegalStateException(s"Invalid vertex id $vertexId for vertex of kind ${vertex.data.kind} with id ${vertex.data.id}")
     }
 
     vertex.outgoingEdges.edges.foreach {
       case (component, destinationIds) =>
-        if (vertexId.kind != component._1) { throw new IllegalStateException(s"Invalid source kind for outgoing component $component in vertex $vertexId") }
+        if (vertexId.kind != component._1) { errors :+= new IllegalStateException(s"Invalid source kind for outgoing component $component in vertex $vertexId") }
         destinationIds.foreach {
           case (destinationId, edgeData) =>
-            if (!graph.vertices.contains(destinationId)) { throw new IllegalStateException(s"Could not find destination vertex of outgoing edge ${(vertexId, destinationId, edgeData.kind)}") }
-            if (destinationId.kind != component._2) { throw new IllegalStateException(s"Invalid destination kind for outgoing edge ${(vertexId, destinationId, edgeData.kind)} in component $component") }
-            if (edgeData.kind != component._3) { throw new IllegalStateException(s"Invalid edge data kind for outgoing edge ${(vertexId, destinationId, edgeData.kind)} in component $component") }
+            if (!graph.vertices.contains(destinationId)) { errors :+= new IllegalStateException(s"Could not find destination vertex of outgoing edge ${(vertexId, destinationId, edgeData.kind)}") }
+            if (destinationId.kind != component._2) { errors :+= new IllegalStateException(s"Invalid destination kind for outgoing edge ${(vertexId, destinationId, edgeData.kind)} in component $component") }
+            if (edgeData.kind != component._3) { errors :+= new IllegalStateException(s"Invalid edge data kind for outgoing edge ${(vertexId, destinationId, edgeData.kind)} in component $component") }
         }
     }
 
     vertex.incomingEdges.edges.foreach {
       case (component, sourcesIds) =>
-        if (vertexId.kind != component._2) { throw new IllegalStateException(s"Invalid destination kind for incoming component $component in vertex $vertexId") }
+        if (vertexId.kind != component._2) { errors :+= new IllegalStateException(s"Invalid destination kind for incoming component $component in vertex $vertexId") }
         sourcesIds.foreach { sourceId =>
-          if (!graph.vertices.contains(sourceId)) { throw new IllegalStateException(s"Could not find source vertex of incoming edge ${(sourceId, vertexId, component._3)}") }
-          if (sourceId.kind != component._1) { throw new IllegalStateException(s"Invalid source kind for incoming edge ${(sourceId, vertexId, component._3)} in component $component") }
+          if (!graph.vertices.contains(sourceId)) { errors :+= new IllegalStateException(s"Could not find source vertex of incoming edge ${(sourceId, vertexId, component._3)}") }
+          if (sourceId.kind != component._1) { errors :+= new IllegalStateException(s"Invalid source kind for incoming edge ${(sourceId, vertexId, component._3)} in component $component") }
         }
+    }
+
+    if (errors.nonEmpty) {
+      val message = s"${errors.length} problems found with vertex $vertexId:"
+      val errorMessages = errors.map(_.toString)
+      log.error(message)
+      errorMessages.foreach(log.error)
+      throw new IllegalStateException((message :: errors).mkString("\n"))
     }
   }
 
