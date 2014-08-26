ada5112763506faf2ad319805f71deef1ef1c992
Tan Lin <tan@kifi.com>
2014-08-25 18:35:35 -0700

    Add url regex patterns

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/UriWeightingHelper.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/UriWeightingHelper.scala
@@ -13,16 +13,38 @@ case class UrlPattern(
 object UrlPatterns {
   val scoringMultiplier = Seq(
     //----------------------------------Penalize---------------------------------------------------------------------------
-    UrlPattern("""^https?://[-A-Za-z0-9.]*twitter.com[./?\#]""".r, 0.01f, "Twitter"),
-    UrlPattern("""^https?://[-A-Za-z0-9.]*linkedin.com[./?\#]""".r, 0.01f, "LinkedIn"),
-    UrlPattern("""^https?://[-A-Za-z0-9.]*mail.google.com[./?\#]""".r, 0.001f, "Google Mail"),
-    UrlPattern("""^https?://[-A-Za-z0-9.]*wikipedia.org[./?\#]""".r, 0.1f, "Wikipedia"),
-    UrlPattern("""^https?://[-A-Za-z0-9.]*facebook.com[./?\#]""".r, 0.001f, "Facebook"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*kifi.com[./?\#]""".r, 0.0f, "Anything about kifi.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*42go.com[./?\#]""".r, 0.0f, "Anything about 42go.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*wikipedia.org[./?\#]""".r, 0.0f, "Anything about wikipedia.org"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*amazon.com[./?\#]""".r, 0.0f, "Anything about amazon.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*ebay.*[./?\#]""".r, 0.0f, "Anything about ebay"),//mark
+    UrlPattern("""^https?://[-A-Za-z0-9.]*walmart.com[./?\#]""".r, 0.0f, "Anything about walmart.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*etsy.com[./?\#]""".r, 0.0f, "Anything about etsy.com"),
+    UrlPattern("""^https?://google.com/calendar*""".r, 0.0f, "Google calendar"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*fedex.com[./?\#]""".r, 0.0f, "Anything about fedex.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*ups.com[./?\#]""".r, 0.0f, "Anything about ups.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*usps.gov[./?\#]""".r, 0.0f, "Anything about usps.gov"),
+    UrlPattern("""^https?://mail.google.com[./?\#]""".r, 0.0f, "Anything about Gmail"),
+    UrlPattern("""^https?://gmail.com[./?\#]""".r, 0.0f, "Anything about Gmail"),
 
+    UrlPattern("""^https?://[-A-Za-z0-9.]*groupon.com[./?\#]""".r, 0.5f, "Anything about groupon.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*linkedin.com/profile[./?\#]""".r, 0.5f, "Linkedin profile pages"),
+    UrlPattern("""^https?://[www?].twitter.com[./?\#]""".r, 0.5f, "Twitter feeds"),
+    UrlPattern("""^https?://[www?].facebook.com[./?\#]""".r, 0.5f, "Facebook feeds"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*dropbox.com[./?\#]""".r, 0.5f, "dropbox.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*craigslist.com[./?\#]""".r, 0.5f, "craigslist.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*dropbox.com[./?\#]""".r, 0.5f, "Dropbox.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*[-A-Za-z0-9.]**[-A-Za-z0-9.][./?\#]support*""".r, 0.5f, "Anything with support"),//not sure
+    UrlPattern("""^https?://(drive|docs|plus).google.com[./?\#]""".r, 0.5f, "google drive, docs and plus"),
+    UrlPattern("""^https?://(images?|videos?|maps?|search|[www?]*).google.com[./?\#]""".r, 0.5f, "google search, images, videos and maps"),
+    UrlPattern("""^https?://(images?|videos?|maps?|search|[www?]*).bing.com[./?\#]""".r, 0.5f, "bing search, images, videos and maps"),
+    UrlPattern("""^https?://([images?|videos?|screen|maps?|search|answers|].|[www?.]?)yahoo.com[.\/?\#]?""".r, 0.5f, "yahoo search, images, videos, answer and maps"),
+    UrlPattern("""^https?://[www?.]? """.r, 0.0f, ""),
     //----------------------------------Boost------------------------------------------------------------------------------
-    UrlPattern("""^https?://[-A-Za-z0-9.]*techcrunch.com[./?\#]""".r, 1.2f, "Techcrunch"),
-    UrlPattern("""^https?://[-A-Za-z0-9.]*.[-A-Za-z0-9.]*.[./?\#]kifi[-A-Za-z0-9.]*""".r, 1.2f, "Public page relate to Kifi"),
-    UrlPattern("""^https?://(code|blog|engineering).[-A-Za-z0-9.]*.[./?\#][-A-Za-z0-9.]*""".r, 1.1f, "Tech")
+
+    UrlPattern("""^https?://[-A-Za-z0-9.]*medium.com[./?\#]""".r, 1.1f, "Anything about medium.com"),
+    UrlPattern("""^https?://[-A-Za-z0-9.]*longreads.com[./?\#]""".r, 1.1f, "Anything about longreads.com"),
+    UrlPattern("""^https?://[www?.]?youtube.com[./?\#]watch""".r, 1.2f, "Videos on Youtube")
   )
 }
 
