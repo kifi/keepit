832fed065fc030e5285ca435ef21c4e9ca5354bd
Tan Lin <tan@kifi.com>
2014-08-27 16:18:20 -0700

    add low score reaper for public feeds

--- kifi-backend/modules/curator/app/com/keepit/curator/model/PublicFeedRepo.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/model/PublicFeedRepo.scala
@@ -1,18 +1,24 @@
 package com.keepit.curator.model
 
 import com.google.inject.{ ImplementedBy, Singleton, Inject }
-import com.keepit.common.db.slick.DBSession.RSession
+import com.keepit.common.db.slick.DBSession.{ RWSession, RSession }
 import com.keepit.common.db.{ State, Id }
 import com.keepit.common.db.slick.{ SeqNumberFunction, SeqNumberDbFunction, DBSession, DbRepo, DataBaseComponent }
 import com.keepit.common.logging.Logging
-import com.keepit.common.time.Clock
+import com.keepit.common.time.{ currentDateTime, Clock }
 import com.keepit.model.{ NormalizedURI }
+import org.joda.time.DateTime
 import play.api.libs.json.Json
+import com.keepit.common.time._
+import org.joda.time.DateTime
+
+import scala.slick.jdbc.StaticQuery
 
 @ImplementedBy(classOf[PublicFeedRepoImpl])
 trait PublicFeedRepo extends DbRepo[PublicFeed] {
   def getByUri(uriId: Id[NormalizedURI], publicFeedState: Option[State[PublicFeed]])(implicit session: RSession): Option[PublicFeed]
   def getByTopMasterScore(maxBatchSize: Int, publicFeedState: Option[State[PublicFeed]] = Some(PublicFeedStates.ACTIVE))(implicit session: RSession): Seq[PublicFeed]
+  def cleanupLowMasterScoreFeeds(limitNumFeeds: Int, before: DateTime)(implicit session: RWSession): Boolean
 }
 
 @Singleton
@@ -51,6 +57,24 @@ class PublicFeedRepoImpl @Inject() (
   def getByTopMasterScore(maxBatchSize: Int, publicFeedState: Option[State[PublicFeed]])(implicit session: RSession): Seq[PublicFeed] = {
     (for (row <- rows if row.state === publicFeedState) yield row).
       sortBy(_.publicMasterScore.desc).take(maxBatchSize).list
+  }
+
+  def cleanupLowMasterScoreFeeds(limitNumFeeds: Int, before: DateTime)(implicit session: RWSession): Boolean = {
+    import StaticQuery.interpolation
+    val limitScore =
+      sql"""SELECT MIN(master_score)
+              FROM (
+	              SELECT master_score
+	              FROM public_feed
+	              WHERE state=${PublicFeedStates.ACTIVE}
+	              ORDER BY master_score DESC LIMIT $limitNumFeeds
+              ) AS mScoreTable""".as[Float].first
+
+    (for (
+      row <- rows if row.updatedAt < before && row.publicMasterScore < limitScore &&
+        row.state === PublicFeedStates.ACTIVE
+    ) yield (row.state, row.updatedAt)).
+      update((PublicFeedStates.INACTIVE, currentDateTime)) > 0
   }
 
 }
