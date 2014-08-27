ee3d0555e14304233e5120ae7eb45f878634c74b
Tan Lin <tan@kifi.com>
2014-08-27 15:42:13 -0700

    format

--- kifi-backend/modules/curator/app/com/keepit/curator/model/SeedItem.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/model/SeedItem.scala
@@ -32,11 +32,6 @@ case class PublicSeedItem(
   keepers: Keepers,
   discoverable: Boolean)
 
-@json case class RecomputedScores(
-  socialScore: Float,
-  overallInterestScore: Float,
-  recentInterestScore: Float)
-
 @json case class PublicUriScores(
   popularityScore: Float,
   recencyScore: Float,
