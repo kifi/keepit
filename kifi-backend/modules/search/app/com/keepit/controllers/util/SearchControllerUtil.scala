f35f105531939f640d971dcc192bdde9e803cdab
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-09-02 15:40:25 -0700

    add /site/search2

---
+++ kifi-backend/modules/search/app/com/keepit/controllers/util/SearchControllerUtil.scala
@@ -0,0 +1,63 @@
+package com.keepit.controllers.util
+
+import com.keepit.common.concurrent.ExecutionContext._
+import com.keepit.common.db.Id
+import com.keepit.model.NormalizedURI
+import com.keepit.search.engine.result.KifiPlainResult
+import com.keepit.search.result.{ ResultUtil, DecoratedResult, KifiSearchResult }
+import com.keepit.search.util.IdFilterCompressor
+import com.keepit.shoebox.ShoeboxServiceClient
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.json.JsObject
+
+import scala.concurrent.Future
+
+trait SearchControllerUtil {
+
+  @inline
+  def reactiveEnumerator[T](futureSeq: Seq[Future[T]]) = {
+    // Returns successful results of Futures in the order they are completed, reactively
+    Enumerator.interleave(futureSeq.map { future =>
+      Enumerator.flatten(future.map(r => Enumerator(r))(immediate))
+    })
+  }
+
+  def uriSummaryInfoFuture(shoeboxClient: ShoeboxServiceClient, plainResultFuture: Future[KifiPlainResult]): Future[String] = {
+    plainResultFuture.flatMap { r =>
+      val uriIds = r.hits.map(h => Id[NormalizedURI](h.id))
+      shoeboxClient.getUriSummaries(uriIds).map { uriSummaries =>
+        KifiSearchResult.uriSummaryInfoV2(uriIds.map { uriId => uriSummaries.get(uriId) }).toString
+      }
+    }
+  }
+
+  def toKifiSearchResultV2(kifiPlainResult: KifiPlainResult): JsObject = {
+    KifiSearchResult.v2(
+      kifiPlainResult.uuid,
+      kifiPlainResult.query,
+      kifiPlainResult.hits,
+      kifiPlainResult.myTotal,
+      kifiPlainResult.friendsTotal,
+      kifiPlainResult.mayHaveMoreHits,
+      kifiPlainResult.show,
+      kifiPlainResult.searchExperimentId,
+      IdFilterCompressor.fromSetToBase64(kifiPlainResult.idFilter)).json
+  }
+
+  def toKifiSearchResultV1(decoratedResult: DecoratedResult): JsObject = {
+    KifiSearchResult.v1(
+      decoratedResult.uuid,
+      decoratedResult.query,
+      ResultUtil.toKifiSearchHits(decoratedResult.hits),
+      decoratedResult.myTotal,
+      decoratedResult.friendsTotal,
+      decoratedResult.othersTotal,
+      decoratedResult.mayHaveMoreHits,
+      decoratedResult.show,
+      decoratedResult.searchExperimentId,
+      IdFilterCompressor.fromSetToBase64(decoratedResult.idFilter),
+      Nil,
+      decoratedResult.experts).json
+  }
+}
