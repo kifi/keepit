91990b12cea54493aff09a2ef3c1d043f0384ce0
Aaron Hsu <aaron@kifi.com>
2014-09-03 10:45:19 -0700

    newline

--- kifi-backend/modules/common/conf/evolutions/shoebox/238.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/238.sql
@@ -3,7 +3,7 @@
 # --- !Ups
 
 ALTER TABLE curator_keep_info
-  ADD COLUMN library_id BIGINT(20) DEFAULT NULL;
+  ADD COLUMN library_id BIGINT(20) NULL;
 
 insert into evolutions (name, description) values('238.sql', 'add library id column in curator_keep_info');
 
