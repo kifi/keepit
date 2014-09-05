802116fe7621bf9be8bea3924e8148bb62e26179
Raymond Ng <raymond.ng@gmail.com>
2014-09-04 17:56:48 -0700

    tests passed!

--- kifi-backend/modules/common/test/com/keepit/common/actor/TestActorBuilderImpl.scala
+++ kifi-backend/modules/common/test/com/keepit/common/actor/TestActorBuilderImpl.scala
@@ -1,14 +1,12 @@
 package com.keepit.common.actor
 
-import akka.actor.{ ActorRef, ActorSystem }
+import akka.actor.{ Props, ActorRef, ActorSystem, Actor }
 import com.google.inject.Provider
-import com.keepit.common.akka.AlertingActor
 import akka.testkit.TestActorRef
-import akka.actor.Actor
 
 class TestActorBuilderImpl extends ActorBuilder {
   def apply(system: ActorSystem, provider: Provider[_ <: Actor]): ActorRef = {
-    TestActorRef(provider.get)(system)
+    TestActorRef(Props { provider.get })(system)
   }
 
 }
