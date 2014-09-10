594a84e19d00b0b8a53fbbc305f0f09f79b520de
Josh McDade <josh@kifi.com>
2014-09-10 13:58:01 -0700

    Kifi black reset password email
    This does not update the production email.  I will test it with the new emailSenderTest endpoint first, then update the production template in another PR.
    
    I made no changes to AuthController.forgotPassword, but I went ahead and added a unit/regression test for it, which should continue to pass when I change it to use ResetPasswordEmailSender

---
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/emails/ResetPasswordEmailSender.scala
@@ -0,0 +1,41 @@
+package com.keepit.commanders.emails
+
+import com.google.inject.Inject
+import com.keepit.common.db.Id
+import com.keepit.common.db.slick.Database
+import com.keepit.common.healthcheck.AirbrakeNotifier
+import com.keepit.common.logging.Logging
+import com.keepit.common.mail.{ EmailAddress, ElectronicMail, SystemEmailAddress }
+import com.keepit.common.mail.template.EmailToSend
+import com.keepit.controllers.core.routes
+import com.keepit.inject.FortyTwoConfig
+import com.keepit.model.{ PasswordResetRepo, NotificationCategory, User }
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import scala.concurrent.Future
+
+class ResetPasswordEmailSender @Inject() (
+    db: Database,
+    emailTemplateSender: EmailTemplateSender,
+    passwordResetRepo: PasswordResetRepo,
+    config: FortyTwoConfig,
+    protected val airbrake: AirbrakeNotifier) extends Logging {
+
+  def sendToUser(userId: Id[User], resetEmailAddress: EmailAddress): Future[ElectronicMail] = {
+    val reset = db.readWrite { implicit rw =>
+      passwordResetRepo.createNewResetToken(userId, resetEmailAddress)
+    }
+
+    val resetUrl = config.applicationBaseUrl + routes.AuthController.setPasswordPage(reset.token)
+    val emailToSend = EmailToSend(
+      fromName = Some("Kifi Support"),
+      from = SystemEmailAddress.SUPPORT,
+      subject = "Kifi.com | Password reset requested",
+      to = Right(resetEmailAddress),
+      category = NotificationCategory.User.RESET_PASSWORD,
+      htmlTemplate = views.html.email.resetPasswordBlack(userId, resetUrl),
+      campaign = Some("passwordReset")
+    )
+    emailTemplateSender.send(emailToSend)
+  }
+}
