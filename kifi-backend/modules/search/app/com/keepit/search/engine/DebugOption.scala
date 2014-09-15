0b20489cef61ceb9d48203712646b378028a94b1
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-09-15 15:06:44 -0700

    debug option test

--- kifi-backend/modules/search/app/com/keepit/search/engine/DebugOption.scala
+++ kifi-backend/modules/search/app/com/keepit/search/engine/DebugOption.scala
@@ -1,5 +1,7 @@
 package com.keepit.search.engine
 
+import com.keepit.common.logging.Logging
+
 object DebugOption {
 
   object DumpBuf {
@@ -10,4 +12,25 @@ object DebugOption {
     }
   }
 
+}
+
+trait DebugOption { self: Logging =>
+  protected var debugFlags: Int = 0
+  protected var debugDumpBufIds: Set[Long] = null
+
+  // debug flags
+  def debug(debugMode: String): Unit = {
+    import DebugOption._
+    debugFlags = debugMode.split(",").map(_.toLowerCase.trim).foldLeft(0) { (flags, str) =>
+      str match {
+        case DumpBuf(ids) =>
+          debugDumpBufIds = ids
+          flags | DumpBuf.flag
+        case _ =>
+          log.warn(s"debug mode ignored: $str")
+          flags
+      }
+    }
+    log.info(s"debug flags: $debugFlags")
+  }
 }
