ab2d79c38a34cfc6c584e022456d288f9931c7ce
Stephen Kemmerling <stephen@42go.com>
2014-09-15 11:01:53 -0700

    adding library membership ingestion test

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/SeedIngestionCommander.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/SeedIngestionCommander.scala
@@ -21,7 +21,7 @@ import scala.concurrent.Future
 class SeedIngestionCommander @Inject() (
     allKeepIngestor: AllKeepSeedIngestionHelper,
     topUrisIngestor: TopUriSeedIngestionHelper,
-    libraryIngestor: LibraryMembershipIngestionHelper,
+    libraryMembershipIngestor: LibraryMembershipIngestionHelper,
     airbrake: AirbrakeNotifier,
     rawSeedsRepo: RawSeedItemRepo,
     keepInfoRepo: CuratorKeepInfoRepo,
@@ -38,7 +38,7 @@ class SeedIngestionCommander @Inject() (
 
   def ingestAll(): Future[Boolean] = ingestionLock.withLockFuture {
     val fut = ingestAllKeeps().flatMap { _ =>
-      ingestLibraries.flatMap { _ =>
+      ingestLibraryMemberships.flatMap { _ =>
         usersToIngestGraphDataFor().flatMap { userIds =>
           FutureHelpers.sequentialExec(userIds)(ingestTopUris)
         }
@@ -58,7 +58,7 @@ class SeedIngestionCommander @Inject() (
     log.info("Ingested one batch of keeps.")
   }
 
-  def ingestLibraries(): Future[Unit] = FutureHelpers.whilef(libraryIngestor(INGESTION_BATCH_SIZE)) {
+  def ingestLibraryMemberships(): Future[Unit] = FutureHelpers.whilef(libraryMembershipIngestor(INGESTION_BATCH_SIZE)) {
     log.info("Ingested one batch of libraries.")
   }
 
