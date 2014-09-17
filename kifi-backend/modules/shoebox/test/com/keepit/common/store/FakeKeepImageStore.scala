9822c69d1333339657250d760a3b5c2591d24282
Andrew Conner <git@andrewconner.org>
2014-09-16 17:49:57 -0700

    Being a better filesystem citizen

--- kifi-backend/modules/shoebox/test/com/keepit/common/store/FakeKeepImageStore.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/common/store/FakeKeepImageStore.scala
@@ -16,13 +16,14 @@ class FakeKeepImageStore(s3ImageConfig: S3ImageConfig) extends KeepImageStore {
 
   def put(key: String, is: InputStream, contentLength: Int, mimeType: String): Future[UploadResult] = {
     val tf = TemporaryFile(prefix = "test-file", suffix = ".png")
-    try {
-      val result = new UploadResult()
-      result.setBucketName(s3ImageConfig.bucketName)
-      result.setKey(key)
-      cache.put(key, tf)
-      Future.successful(result)
-    }
+    tf.file.deleteOnExit()
+    // Intentionally does not write the data to the actual file. If this is needed, please add a mutable flag in this class.
+    val result = new UploadResult()
+    result.setBucketName(s3ImageConfig.bucketName)
+    result.setKey(key)
+    cache.put(key, tf)
+    is.close()
+    Future.successful(result)
   }
   def get(key: String): Future[TemporaryFile] = {
     cache.get(key) match {
