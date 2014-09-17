c84ece938ecfdf33dbb34da60c002a9f5aeecc52
Raymond Ng <raymond.ng@gmail.com>
2014-09-17 11:07:41 -0700

    review feedback from andrew

--- kifi-backend/modules/common/app/com/keepit/common/controller/UserActions.scala
+++ kifi-backend/modules/common/app/com/keepit/common/controller/UserActions.scala
@@ -4,32 +4,20 @@ import com.keepit.common.db.{ ExternalId, Id }
 import com.keepit.model.{ ExperimentType, KifiInstallation, User }
 import play.api.mvc._
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import scala.concurrent.duration._
 import scala.concurrent.{ Await, Future }
 
-sealed trait MaybeUserRequest[T]
-trait NonUserRequest[T] extends MaybeUserRequest[T]
-trait PartialNonUserRequest[T] extends NonUserRequest[T] // OAuth successful but no User (sign-up)
-trait UserRequest[T] extends MaybeUserRequest[T] {
-  def userId: Id[User]
-  def userF: Future[User]
-  def user: User
-  def experimentsF: Future[Set[ExperimentType]]
-  def experiments: Set[ExperimentType]
-  def kifiInstallationId: Option[ExternalId[KifiInstallation]]
-  def adminUserId: Option[Id[User]]
-}
-
-case class SimpleNonUserRequest[T](request: Request[T]) extends WrappedRequest(request) with NonUserRequest[T]
-case class SimplePartialNonUserRequest[T](request: Request[T]) extends WrappedRequest(request) with PartialNonUserRequest[T]
-case class SimpleUserRequest[T](
+sealed trait MaybeUserRequest[T] extends Request[T]
+case class NonUserRequest[T](request: Request[T]) extends WrappedRequest(request) with MaybeUserRequest[T]
+// class PartialNonUserRequest[T](override val request: Request[T]) extends NonUserRequest(request) // todo(ray)
+case class UserRequest[T](
     request: Request[T],
     userId: Id[User],
     userF: Future[User],
     experimentsF: Future[Set[ExperimentType]],
     kifiInstallationId: Option[ExternalId[KifiInstallation]] = None,
-    adminUserId: Option[Id[User]] = None) extends WrappedRequest(request) with UserRequest[T] {
+    adminUserId: Option[Id[User]] = None) extends WrappedRequest(request) with MaybeUserRequest[T] {
   lazy val user = Await.result(userF, 5 seconds)
   lazy val experiments = Await.result(experimentsF, 5 seconds)
 }
@@ -51,7 +39,7 @@ trait UserActions { self: Controller =>
   protected def userActionsHelper: UserActionsHelper
 
   private def buildUserRequest[A](userId: Id[User])(implicit request: Request[A]) =
-    SimpleUserRequest(request, userId, userActionsHelper.getUserOpt.map(_.get), userActionsHelper.getUserExperiments)
+    UserRequest(request, userId, userActionsHelper.getUserOpt.map(_.get), userActionsHelper.getUserExperiments)
 
   object UserAction extends ActionBuilder[UserRequest] {
     def invokeBlock[A](request: Request[A], block: (UserRequest[A]) => Future[Result]): Future[Result] = {
@@ -68,7 +56,7 @@ trait UserActions { self: Controller =>
       implicit val req = request
       userActionsHelper.getUserIdOpt match {
         case Some(userId) => block(buildUserRequest(userId))
-        case None => block(SimpleNonUserRequest(request))
+        case None => block(NonUserRequest(request))
       }
     }
   }
