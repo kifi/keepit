723fbb588113c6f4fae755dcf57aca386ce5b8cb
Andrew Conner <git@andrewconner.org>
2014-09-17 17:57:04 -0700

    This makes for a better error message

--- kifi-backend/modules/sqldb/app/com/keepit/common/db/slick/DbRepo.scala
+++ kifi-backend/modules/sqldb/app/com/keepit/common/db/slick/DbRepo.scala
@@ -99,7 +99,7 @@ trait DbRepo[M <: Model[M]] extends Repo[M] with FortyTwoGenericTypeMappers with
   }
   def get(id: Id[M])(implicit session: RSession): M = {
     val startTime = System.currentTimeMillis()
-    val model = getCompiled(id).firstOption.getOrElse(throw new IllegalArgumentException("can't find $id in ${_taggedTable.tableName}"))
+    val model = getCompiled(id).firstOption.getOrElse(throw new IllegalArgumentException(s"can't find $id in ${_taggedTable.tableName}"))
     val time = System.currentTimeMillis - startTime
     checkTiming(time, "GET", model)
     model
