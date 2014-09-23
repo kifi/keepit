8b5443c78edf0ad5ddee298031695f34d7c22dfe
eishay <eishay@gmail.com>
2014-09-23 08:51:16 -0700

    increase ttl for user cache to one week so it would survive the weekend and few inactivity days

--- kifi-backend/modules/graph/app/com/keepit/graph/common/cache/GraphCacheModule.scala
+++ kifi-backend/modules/graph/app/com/keepit/graph/common/cache/GraphCacheModule.scala
@@ -71,7 +71,7 @@ case class GraphCacheModule(cachePluginModules: CachePluginModule*) extends Cach
   @Singleton
   @Provides
   def userIdCache(stats: CacheStatistics, accessLog: AccessLog, innerRepo: InMemoryCachePlugin, outerRepo: FortyTwoCachePlugin) =
-    new UserIdCache(stats, accessLog, (innerRepo, 10 minutes), (outerRepo, 24 hours))
+    new UserSessionViewExternalIdCache(stats, accessLog, (innerRepo, 10 minutes), (outerRepo, 7 days))
 
   @Singleton
   @Provides
