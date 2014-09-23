3e5a9d4056bb5f3d135a7464c58a8d488d788859
Stephen Kemmerling <stephen@42go.com>
2014-09-23 14:32:09 -0700

    adding a comment

--- kifi-backend/modules/shoebox/angular/src/recos/recos.js
+++ kifi-backend/modules/shoebox/angular/src/recos/recos.js
@@ -91,6 +91,12 @@ angular.module('kifi')
       $scope.initialCardClosed = true;
     };
 
+
+    /*
+    This is intended be called from the console only, for debugging.
+    Specifically, running `$(".recos-view").scope().toggleExplain(); $(".recos-view").scope().$digest();`
+    in the bowser console while on the recommendations page will toggle between showing the page description and the score breakdown in the card.
+    */
     $scope.toggleExplain = function () {
       $scope.recos.forEach(function (reco) {
         var temp = reco.recoKeep.summary.description;
