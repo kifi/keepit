0f95c4e96e17b1be4af208ee565e63239e320915
Yiping Liao <yiping@kifi.com>
2014-09-24 15:44:47 -0700

    cp15: create library functionality working.

--- kifi-backend/modules/shoebox/angular/src/libraries/manageLibrary.js
+++ kifi-backend/modules/shoebox/angular/src/libraries/manageLibrary.js
@@ -2,8 +2,8 @@
 
 angular.module('kifi')
 
-.directive('kfManageLibrary', ['libraryService', 'profileService',
-  function (libraryService, profileService) {
+.directive('kfManageLibrary', ['$location', 'libraryService', 'profileService',
+  function ($location, libraryService, profileService) {
     return {
       restrict: 'A',
       scope: {},
@@ -13,6 +13,8 @@ angular.module('kifi')
         //
         // Internal data.
         //
+        var returnAction = null;
+        var submitting = false;
 
 
         //
@@ -20,6 +22,7 @@ angular.module('kifi')
         //
         scope.username = profileService.me.username;
         scope.userHasEditedSlug = false;
+        scope.$error = {};
 
         scope.visibility = {
           'published': {
@@ -52,26 +55,91 @@ angular.module('kifi')
           kfModalCtrl.close();
         };
 
-        scope.$watch(function () {
-          return scope.library.name;
-        }, function (v) {
-          if (!scope.userHasEditedSlug) {
-            scope.library.slug = generateSlug(v);
-          }
-        });
-
         scope.editSlug = function () {
           scope.userHasEditedSlug = scope.library.slug? true : false;
           scope.library.slug = generateSlug(scope.library.slug);
         };
+
+        scope.saveLibrary = function () {
+          if (submitting) {
+            return;
+          }
+
+          if (scope.library.name.length < 3) {
+            scope.$error.name = 'Try a longer name';
+            return;
+          }
+
+          submitting = true;
+          var promise;
+          if (scope.modifyingExistingLibrary && scope.library.id) {
+            promise = libraryService.modifyLibrary(scope.library);
+          } else {
+            promise = libraryService.createLibrary(scope.library);
+          }
+
+          promise.then(function (resp) {
+              scope.$error = {};
+              submitting = false;
+              libraryService.fetchLibrarySummaries(true);
+
+              scope.close();
+
+              if (!returnAction) {
+                $location.path(resp.url);
+              } else {
+                returnAction(resp);
+              }
+          })['catch'](function (err) {
+            submitting = false;
+            var error = err.data && err.data.error;
+            switch (error) {
+              case 'library name already exists for user':  // deprecated
+              case 'library_name_exists':
+                scope.$error.general = 'You already have a library with this name. Pick another.';
+                break;
+              case 'invalid library name':  // deprecated
+              case 'invalid_name':
+                scope.$error.general = 'You already have a library with this name. Pick another.';
+                break;
+              case 'library_slug_exists':
+                scope.$error.general = 'You already have a library with the same URL. Pick another.';
+                break;
+              case 'invalid library slug':  // deprecated
+              case 'invalid_slug':
+                scope.$error.general = 'The URL you picked isn\'t valid. Try using only letters and numbers.';
+                break;
+            }
+          });
+        };
+
+
+        //
+        // Watches and listeners.
+        //
+        scope.$watch(function () {
+          return scope.library.name;
+        }, function (newVal, oldVal) {
+          scope.userHasEditedSlug = scope.library.name && (newVal !== oldVal) ? true : false;
+
+          if (newVal !== oldVal) {
+            scope.library.slug = generateSlug(newVal);
+          }
+        });
 
 
         //
         // On link.
         //
         if (libraryService.libraryState.library) {
-          scope.userHasEditedSlug = true;
+          scope.modifyingExistingLibrary = true;
+
           scope.library = _.cloneDeep(libraryService.libraryState.library);
+          returnAction = libraryService.libraryState.returnAction || null;
+
+          libraryService.libraryState = {};
+
+          scope.userHasEditedSlug = true;
           scope.modalTitle = scope.library.name;
         } else {
           scope.library = {
@@ -87,103 +155,103 @@ angular.module('kifi')
       }
     };
   }
-])
-
-
-.controller('ManageLibraryCtrl', [
-  '$scope', '$routeParams', 'libraryService', 'util', '$timeout', '$location', 'profileService',
-  function ($scope, $routeParams, libraryService, util, $timeout, $location, profileService) {
-    $scope.$error = {};
-    $scope.userHasEditedSlug = false;
-    $scope.username = profileService.me.username;
-    var returnAction;
-
-    if (libraryService.libraryState.library) {
-      $scope.modifyingExistingLibrary = true;
-      $scope.library = _.cloneDeep(libraryService.libraryState.library);
-      returnAction = libraryService.libraryState.returnAction || null;
-      libraryService.libraryState = {};
-      $scope.modalTitle = $scope.library.name;
-      $scope.userHasEditedSlug = true;
-    } else {
-      $scope.library = {
-        'name': '',
-        'visibility': 'discoverable',
-        'description': '',
-        'slug': ''
-      };
-      $scope.modalTitle = 'Create a library';
-    }
-
-    var generateSlug = function (name) {
-      return name.toLowerCase().replace(/[^\w\s]|_/g, '').replace(/\s+/g, '-').replace(/^-/, '');
-    };
-
-    $scope.$watch(function() {
-      return $scope.library.name;
-    }, function (v) {
-      if (!$scope.userHasEditedSlug) {
-        $scope.library.slug = generateSlug(v);
-      }
-    });
-
-    $scope.editSlug = function () {
-      $scope.userHasEditedSlug = true;
-      $scope.library.slug = generateSlug($scope.library.slug);
-    };
-
-
-    $scope.saveLibrary = function () {
-      var newError = false;
-      if ($scope.library.name.length < 3) {
-        $scope.$error.name = ' Try a longer name';
-        newError = true;
-      }
-      if (newError || $scope.submitting) {
-        return;
-      }
-      $scope.submitting = true;
-      var promise;
-      if ($scope.modifyingExistingLibrary && $scope.library.id) {
-        // Save existing library
-        promise = libraryService.modifyLibrary($scope.library);
-      } else {
-        promise = libraryService.createLibrary($scope.library);
-      }
-      promise.then(function (resp) {
-          $scope.$error = {};
-          $scope.submitting = false;
-          libraryService.fetchLibrarySummaries(true);
-          // There must be a better way:
-          $scope.hideModal();
-          $scope.modal = null;
-
-          if (!returnAction) {
-            $location.path(resp.url);
-          } else {
-            returnAction(resp);
-          }
-      })['catch'](function (err) {
-        $scope.submitting = false;
-        var error = err.data && err.data.error;
-        switch (error) {
-          case 'library name already exists for user': // deprecated
-          case 'library_name_exists':
-            $scope.$error.general = 'You already have a library with this name. Pick another.';
-            break;
-          case 'invalid library name': // deprecated
-          case 'invalid_name':
-            $scope.$error.general = 'You already have a library with this name. Pick another.';
-            break;
-          case 'library_slug_exists':
-            $scope.$error.general = 'You already have a library with the same URL. Pick another.';
-            break;
-          case 'invalid library slug': // deprecated
-          case 'invalid_slug':
-            $scope.$error.general = 'The URL you picked isn\'t valid. Try using only letters and numbers.';
-            break;
-        }
-      });
-    };
-  }
 ]);
+
+
+// .controller('ManageLibraryCtrl', [
+//   '$scope', '$routeParams', 'libraryService', 'util', '$timeout', '$location', 'profileService',
+//   function ($scope, $routeParams, libraryService, util, $timeout, $location, profileService) {
+//     $scope.$error = {};
+//     // $scope.userHasEditedSlug = false;
+//     // $scope.username = profileService.me.username;
+//     // var returnAction;
+
+//     // if (libraryService.libraryState.library) {
+//       // $scope.modifyingExistingLibrary = true;
+//       // $scope.library = _.cloneDeep(libraryService.libraryState.library);
+//       // returnAction = libraryService.libraryState.returnAction || null;
+//       // libraryService.libraryState = {};
+//       // $scope.modalTitle = $scope.library.name;
+//       // $scope.userHasEditedSlug = true;
+//     // } else {
+//       // $scope.library = {
+//       //   'name': '',
+//       //   'visibility': 'discoverable',
+//       //   'description': '',
+//       //   'slug': ''
+//       // };
+//       // $scope.modalTitle = 'Create a library';
+//     }
+
+//     // var generateSlug = function (name) {
+//     //   return name.toLowerCase().replace(/[^\w\s]|_/g, '').replace(/\s+/g, '-').replace(/^-/, '');
+//     // };
+
+//     // $scope.$watch(function() {
+//     //   return $scope.library.name;
+//     // }, function (v) {
+//     //   if (!$scope.userHasEditedSlug) {
+//     //     $scope.library.slug = generateSlug(v);
+//     //   }
+//     // });
+
+//     // $scope.editSlug = function () {
+//     //   $scope.userHasEditedSlug = true;
+//     //   $scope.library.slug = generateSlug($scope.library.slug);
+//     // };
+
+
+//     // $scope.saveLibrary = function () {
+//     //   var newError = false;
+//     //   if ($scope.library.name.length < 3) {
+//     //     $scope.$error.name = ' Try a longer name';
+//     //     newError = true;
+//     //   }
+//     //   if (newError || $scope.submitting) {
+//     //     return;
+//     //   }
+//     //   $scope.submitting = true;
+//     //   var promise;
+//     //   if ($scope.modifyingExistingLibrary && $scope.library.id) {
+//     //     // Save existing library
+//     //     promise = libraryService.modifyLibrary($scope.library);
+//     //   } else {
+//     //     promise = libraryService.createLibrary($scope.library);
+//     //   }
+//     //   promise.then(function (resp) {
+//     //       $scope.$error = {};
+//     //       $scope.submitting = false;
+//     //       libraryService.fetchLibrarySummaries(true);
+//     //       // There must be a better way:
+//     //       $scope.hideModal();
+//     //       $scope.modal = null;
+
+//     //       if (!returnAction) {
+//     //         $location.path(resp.url);
+//     //       } else {
+//     //         returnAction(resp);
+//     //       }
+//     //   })['catch'](function (err) {
+//     //     $scope.submitting = false;
+//     //     var error = err.data && err.data.error;
+//     //     switch (error) {
+//     //       case 'library name already exists for user': // deprecated
+//     //       case 'library_name_exists':
+//     //         $scope.$error.general = 'You already have a library with this name. Pick another.';
+//     //         break;
+//     //       case 'invalid library name': // deprecated
+//     //       case 'invalid_name':
+//     //         $scope.$error.general = 'You already have a library with this name. Pick another.';
+//     //         break;
+//     //       case 'library_slug_exists':
+//     //         $scope.$error.general = 'You already have a library with the same URL. Pick another.';
+//     //         break;
+//     //       case 'invalid library slug': // deprecated
+//     //       case 'invalid_slug':
+//     //         $scope.$error.general = 'The URL you picked isn\'t valid. Try using only letters and numbers.';
+//     //         break;
+//     //     }
+//     //   });
+//     // };
+//   }
+// ]);
