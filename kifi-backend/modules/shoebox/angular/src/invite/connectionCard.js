35016a5d5ef264c7adc46f17e900a5792a9b29a4
Yiping Liao <yiping@kifi.com>
2014-09-27 15:33:11 -0700

    Checkpoint M10: migrate LinkedIn hit rate limit and LinkedIn token expired modals.

--- kifi-backend/modules/shoebox/angular/src/invite/connectionCard.js
+++ kifi-backend/modules/shoebox/angular/src/invite/connectionCard.js
@@ -3,14 +3,12 @@
 angular.module('kifi')
 
 
-.directive('kfConnectionCard', ['$window', '$http', 'routeService', 'inviteService', 'modalService',
-  function ($window, $http, routeService, inviteService, modalService) {
+.directive('kfConnectionCard', ['$window', '$http', '$timeout', 'routeService', 'inviteService', 'modalService',
+  function ($window, $http, $timeout, routeService, inviteService, modalService) {
   return {
     scope: {
       'friend': '&',
-      'refreshScroll': '=',
-      'showLinkedinTokenExpiredModal': '=',
-      'showLinkedinHitRateLimitModal': '='
+      'showAddNetworksModal': '&'
     },
     replace: true,
     restrict: 'A',
@@ -36,6 +34,14 @@ angular.module('kifi')
       scope.linkedin = network === 'linkedin';
       scope.email    = network === 'email';
 
+      scope.reconnectLinkedIn = function () {
+        $timeout(function () {
+          modalService.open({
+            template: 'social/addNetworksModal.tpl.html'
+          });
+        }, 0);
+      };
+
       scope.action = function () {
         inviteService.invite(network, inNetworkId).then(function () {
           scope.invited = true;
@@ -49,9 +55,14 @@ angular.module('kifi')
           }
         }, function (err) {
           if (err === 'token_expired') {
-            scope.showLinkedinTokenExpiredModal = true;
+            modalService.open({
+              template: 'invite/linkedinTokenExpiredModal.tpl.html',
+              scope: scope
+            });
           } else if (err === 'hit_rate_limit_reached') {
-            scope.showLinkedinHitRateLimitModal = true;
+            modalService.open({
+              template: 'invite/linkedinHitRateLimitModal.tpl.html'
+            });
           } else {
             modalService.open({
               template: 'invite/genericInviteErrorModal.tpl.html'
@@ -59,6 +70,7 @@ angular.module('kifi')
           }
         });
       };
+
       scope.closeAction = function () {
         scope.hidden = true;
         var data = {
