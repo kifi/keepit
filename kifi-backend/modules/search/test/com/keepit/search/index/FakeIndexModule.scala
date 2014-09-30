e444ec31d532104eed6b984532d7855802729bed
yingjieMiao <yingjie@42go.com>
2014-09-30 12:49:22 -0700

    fix build?

--- kifi-backend/modules/search/test/com/keepit/search/index/FakeIndexModule.scala
+++ kifi-backend/modules/search/test/com/keepit/search/index/FakeIndexModule.scala
@@ -5,6 +5,7 @@ import com.keepit.common.util.Configuration
 
 case class FakeIndexModule() extends IndexModule {
 
-  protected def getIndexDirectory(configName: String, shard: Shard[_], version: IndexerVersion, indexStore: IndexStore, conf: Configuration): IndexDirectory = new VolatileIndexDirectory()
+  protected def removeOldIndexDirs(conf: Configuration, configName: String, shard: Shard[_], versionsToClean: Seq[IndexerVersion]): Unit = {}
+  protected def getIndexDirectory(configName: String, shard: Shard[_], version: IndexerVersion, indexStore: IndexStore, conf: Configuration, versionsToClean: Seq[IndexerVersion]): IndexDirectory = new VolatileIndexDirectory()
 
 }
