d5329743ab63f0727de1f728f160ae17b57dfbf1
Raymond Ng <raymond.ng@gmail.com>
2014-10-01 14:44:15 -0700

    comments

--- kifi-backend/modules/common/conf/evolutions/shoebox/253.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/253.sql
@@ -8,8 +8,8 @@ ALTER TABLE bookmark ADD CONSTRAINT user_disjoint_lib_uri UNIQUE (user_id, in_di
 ALTER TABLE bookmark DROP CONSTRAINT user_id_primary_uri_id;
 
 -- MySQL:
--- ALTER TABLE bookmark ADD UNIQUE lib_id_primary_uri_id (library_id, is_primary, uri_id);
--- ALTER TABLE bookmark ADD UNIQUE user_id_in_disjoint_lib_uri_id (user_id, in_disjoint_lib, uri_id);
+-- ALTER TABLE bookmark ADD UNIQUE lib_primary_uri (library_id, is_primary, uri_id);
+-- ALTER TABLE bookmark ADD UNIQUE user_disjoint_lib_uri (user_id, in_disjoint_lib, uri_id);
 -- ALTER TABLE bookmark DROP KEY user_id_primary_uri_id;
 
 insert into evolutions (name, description) values('253.sql', 'drop user_id_primary_uri_id AND add in_disjoint_lib to bookmark');
