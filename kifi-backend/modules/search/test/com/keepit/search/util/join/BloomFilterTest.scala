01f44a31771658ce5475ca68bc754ebbe3754633
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-10-01 12:31:25 -0700

    more test

--- kifi-backend/modules/search/test/com/keepit/search/util/join/BloomFilterTest.scala
+++ kifi-backend/modules/search/test/com/keepit/search/util/join/BloomFilterTest.scala
@@ -5,8 +5,9 @@ import scala.util.Random
 
 class BloomFilterTest extends Specification {
   private[this] val rnd = new Random
+  private[this] def genPositiveIds = (0 until 1000).map { _ => rnd.nextInt(Int.MaxValue).toLong }.toSet
   private[this] def genIds = {
-    val positiveIds = (0 until 1000).map { _ => rnd.nextInt(Int.MaxValue).toLong }.toSet
+    val positiveIds = genPositiveIds
     var negativeIds = Set.empty[Long]
     while (negativeIds.size < 1000) {
       val id = rnd.nextInt(Int.MaxValue).toLong
@@ -17,6 +18,18 @@ class BloomFilterTest extends Specification {
 
   "BloomFilter" should {
     "detect existence of ids with no false negative" in {
+      val buf = new DataBuffer()
+      val writer = new DataBufferWriter
+
+      val positiveIds = genPositiveIds
+      positiveIds.foreach { id => buf.alloc(writer, 1, 8).putLong(id) }
+
+      val bloomFilter = BloomFilter(buf)
+
+      positiveIds.forall(bloomFilter(_)) === true
+    }
+
+    "detect existence of ids with a low false positive ratio" in {
       (0 until 3).exists { _ =>
         val buf = new DataBuffer()
         val writer = new DataBufferWriter
@@ -25,14 +38,11 @@ class BloomFilterTest extends Specification {
         positiveIds.foreach { id => buf.alloc(writer, 1, 8).putLong(id) }
 
         val bloomFilter = BloomFilter(buf)
-
-        positiveIds.forall(bloomFilter(_)) === true
-
         val falsePositiveCount = negativeIds.count(bloomFilter(_))
 
         //println(s"\n\t\t falsePositiveCount = $falsePositiveCount (${falsePositiveCount.toDouble / negativeIds.size.toDouble})\n")
 
-        (falsePositiveCount < negativeIds.size / 20)
+        positiveIds.forall(bloomFilter(_)) && (falsePositiveCount < negativeIds.size / 20)
       } === true
     }
   }
