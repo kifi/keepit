f937855d83920ae2ec21c85ff26b15b57002f07a
eishay <eishay@gmail.com>
2014-10-01 12:26:09 -0700

    tmp

---
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/emails/EmailVerificationSender.scala
@@ -0,0 +1,34 @@
+package com.keepit.commanders.emails
+
+import com.google.inject.Inject
+import com.keepit.common.db.Id
+import com.keepit.common.healthcheck.AirbrakeNotifier
+import com.keepit.common.logging.Logging
+import com.keepit.common.mail.{ SystemEmailAddress, ElectronicMail }
+import com.keepit.common.mail.template.EmailToSend
+import com.keepit.common.mail.template.helpers.fullName
+import com.keepit.model.{ User, NotificationCategory }
+
+import scala.concurrent.Future
+
+class EmailVerificationSender @Inject() (
+    emailTemplateSender: EmailTemplateSender,
+    protected val airbrake: AirbrakeNotifier) extends Logging {
+
+  def apply(toUserId: Id[User], contactUserId: Id[User]): Future[ElectronicMail] =
+    sendToUser(toUserId, contactUserId)
+
+  def sendToUser(toUserId: Id[User], contactUserId: Id[User]): Future[ElectronicMail] = {
+    val emailToSend = EmailToSend(
+      fromName = Some(Left(contactUserId)),
+      from = SystemEmailAddress.NOTIFICATIONS,
+      subject = s"some title about verifying your email",
+      to = Left(toUserId),
+      category = NotificationCategory.User.CONTACT_JOINED,
+      htmlTemplate = views.html.email.black.contactJoined(toUserId, contactUserId),
+      textTemplate = Some(views.html.email.black.contactJoinedText(toUserId, contactUserId)),
+      campaign = Some("contactJoined")
+    )
+    emailTemplateSender.send(emailToSend)
+  }
+}
