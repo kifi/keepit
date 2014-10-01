29ccf28c407ec84b2956c17541ddeb1385642398
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-10-01 12:24:25 -0700

    improve bloomfilter

--- kifi-backend/modules/search/app/com/keepit/search/util/join/BloomFilter.scala
+++ kifi-backend/modules/search/app/com/keepit/search/util/join/BloomFilter.scala
@@ -1,9 +1,8 @@
 package com.keepit.search.util.join
 
 object BloomFilter {
-  private[this] val ITEMS_PER_ENTRY = 2
   def apply(dataBuffer: DataBuffer): BloomFilter = {
-    val bitMaps = new Array[Int]((dataBuffer.size + ITEMS_PER_ENTRY - 1) / ITEMS_PER_ENTRY) // each array entry is a bloom filter
+    val bitMaps = new Array[Long](dataBuffer.size / 4 + 1) // four items per entry (each array entry is a bloom filter)
     dataBuffer.scan(new DataBufferReader) { reader =>
       val id = reader.nextLong()
       bitMaps((id % bitMaps.length).toInt) |= genBits(id)
@@ -15,19 +14,20 @@ object BloomFilter {
     override def apply(id: Long): Boolean = true
   }
 
-  private[join] def genBits(id: Long): Int = {
+  @inline private[join] def genBits(value: Long): Long = {
     // unlike conventional BloomFilter, the number of bits turned on is not fixed
-    // it is expected that the average number of bits on is close to 8 bits (1/4 of 32 bits)
+    // it is expected that the average number of bits on is close to 8 bits (1/8 of 64 bits)
     // this makes computation very fast
     // this increases the false positive ratio a little, though
-    val v1 = ((id / ITEMS_PER_ENTRY) * 0x5DEECE66DL)
-    val v2 = ((v1 + 0x123456789L) * 0x5DEECE66DL)
+    val v1 = (value * 6364136223846793005L)
+    val v2 = ((v1 + 1442695040888963407L) * 6364136223846793005L)
+    val v3 = ((v2 + 1442695040888963407L) * 6364136223846793005L)
 
-    ((v1 & v2) >> 16).toInt
+    (v1 & v2 & v3)
   }
 }
 
-class BloomFilter(bitMaps: Array[Int]) {
+class BloomFilter(bitMaps: Array[Long]) {
   def apply(id: Long): Boolean = {
     val bits = BloomFilter.genBits(id)
     (bits & bitMaps((id % bitMaps.length).toInt)) == bits
--- kifi-backend/modules/search/test/com/keepit/search/util/join/BloomFilterTest.scala
+++ kifi-backend/modules/search/test/com/keepit/search/util/join/BloomFilterTest.scala
@@ -5,30 +5,35 @@ import scala.util.Random
 
 class BloomFilterTest extends Specification {
   private[this] val rnd = new Random
-  private[this] val positiveIds = (0 until 1000).map { _ => rnd.nextInt(Int.MaxValue).toLong }.toSet
-  private[this] val negativeIds = {
-    var negativeSet = Set.empty[Long]
-    while (negativeSet.size < 1000) {
+  private[this] def genIds = {
+    val positiveIds = (0 until 1000).map { _ => rnd.nextInt(Int.MaxValue).toLong }.toSet
+    var negativeIds = Set.empty[Long]
+    while (negativeIds.size < 1000) {
       val id = rnd.nextInt(Int.MaxValue).toLong
-      if (!positiveIds.contains(id)) negativeSet += id
+      if (!positiveIds.contains(id)) negativeIds += id
     }
-    negativeSet
+    (positiveIds, negativeIds)
   }
 
   "BloomFilter" should {
     "detect existence of ids with no false negative" in {
-      val buf = new DataBuffer()
-      val writer = new DataBufferWriter
+      (0 until 3).exists { _ =>
+        val buf = new DataBuffer()
+        val writer = new DataBufferWriter
 
-      positiveIds.foreach { id => buf.alloc(writer, 1, 8).putLong(id) }
+        val (positiveIds, negativeIds) = genIds
+        positiveIds.foreach { id => buf.alloc(writer, 1, 8).putLong(id) }
 
-      val bloomFilter = BloomFilter(buf)
+        val bloomFilter = BloomFilter(buf)
 
-      positiveIds.forall(bloomFilter(_)) === true
+        positiveIds.forall(bloomFilter(_)) === true
 
-      val falsePositiveCount = negativeIds.count(bloomFilter(_))
+        val falsePositiveCount = negativeIds.count(bloomFilter(_))
 
-      (falsePositiveCount < negativeIds.size / 10) === true
+        //println(s"\n\t\t falsePositiveCount = $falsePositiveCount (${falsePositiveCount.toDouble / negativeIds.size.toDouble})\n")
+
+        (falsePositiveCount < negativeIds.size / 20)
+      } === true
     }
   }
 }
