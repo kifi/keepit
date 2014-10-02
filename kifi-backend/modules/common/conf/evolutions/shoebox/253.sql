73e4b805ce48dd29a57dea44209d2186c7429bfd
Raymond Ng <raymond.ng@gmail.com>
2014-10-02 11:29:47 -0700

    make h2 happy

--- kifi-backend/modules/common/conf/evolutions/shoebox/253.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/253.sql
@@ -5,7 +5,6 @@ ALTER TABLE bookmark ADD COLUMN in_disjoint_lib BOOLEAN DEFAULT TRUE AFTER is_pr
 
 ALTER TABLE bookmark ADD CONSTRAINT lib_primary_uri UNIQUE (library_id, is_primary, uri_id);
 ALTER TABLE bookmark ADD CONSTRAINT user_disjoint_primary_uri UNIQUE (user_id, in_disjoint_lib, is_primary, uri_id);
-ALTER TABLE bookmark DROP CONSTRAINT user_id_primary_uri_id;
 
 -- MySQL:
 -- ALTER TABLE bookmark ADD UNIQUE lib_primary_uri (library_id, is_primary, uri_id);
