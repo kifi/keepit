6f5e031ab37499d4b486ec741183080d32e5916e
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-10-06 11:56:36 -0700

    disable click boost and sharing boost for tag only queries

--- kifi-backend/modules/search/app/com/keepit/search/engine/QueryEngineBuilder.scala
+++ kifi-backend/modules/search/app/com/keepit/search/engine/QueryEngineBuilder.scala
@@ -18,6 +18,7 @@ class QueryEngineBuilder(coreQuery: Query) {
   private[this] val _core = buildExpr(coreQuery) // (query, expr, size)
   private[this] lazy val _final = buildFinal()
   private[this] var built = false
+  private[this] var noClickBoostNoSharingBoost = false
 
   def addBoosterQuery(booster: Query, boostStrength: Float): QueryEngineBuilder = {
     if (built) throw new IllegalStateException("cannot modify the engine builder once an engine is built")
@@ -30,7 +31,7 @@ class QueryEngineBuilder(coreQuery: Query) {
   }
 
   def build(): QueryEngine = {
-    new QueryEngine(_final._1, _final._2, _final._3, _core._3)
+    new QueryEngine(_final._1, _final._2, _final._3, _core._3, noClickBoostNoSharingBoost)
   }
 
   private[this] def buildFinal(): (ScoreExpr, Query, Int) = {
@@ -79,7 +80,10 @@ class QueryEngineBuilder(coreQuery: Query) {
         (coreQuery, MaxWithTieBreakerExpr(0, _tieBreakerMultiplier), 1)
 
       case tagQuery: KFilterQuery =>
-        // this is a filter only query, use FixedScoreQuery and MaxExpr so that the recency boosting takes over ranking
+        // this is a filter only query, use FixedScoreQuery and MaxExpr, and disable click boost and sharing boost
+        // so that the recency boosting takes over ranking
+        // this is a special requirement for "tag:" only query on kifi.com
+        noClickBoostNoSharingBoost = true
         (new KWrapperQuery(new FixedScoreQuery(tagQuery.subQuery)), MaxExpr(0), 1)
 
       case q =>
