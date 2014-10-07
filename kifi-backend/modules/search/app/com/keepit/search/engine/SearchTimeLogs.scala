e39c2dc89fb357aa3e714203104119bc6ee511c4
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-10-07 12:31:57 -0700

    remove personalized searcher timing

--- kifi-backend/modules/search/app/com/keepit/search/engine/SearchTimeLogs.scala
+++ kifi-backend/modules/search/app/com/keepit/search/engine/SearchTimeLogs.scala
@@ -8,7 +8,6 @@ class SearchTimeLogs(startTime: Long = System.currentTimeMillis()) extends Loggi
   private[this] var _socialGraphInfo: Long = 0L
   private[this] var _clickBoost: Long = 0L
   private[this] var _queryParsing: Long = 0L
-  private[this] var _personalizedSearcher: Long = 0L
   private[this] var _search: Long = 0L
   private[this] var _processHits: Long = 0L
   private[this] var _endTime: Long = 0L
@@ -16,7 +15,6 @@ class SearchTimeLogs(startTime: Long = System.currentTimeMillis()) extends Loggi
   def socialGraphInfo(now: Long = System.currentTimeMillis()): Unit = { _socialGraphInfo = now }
   def clickBoost(now: Long = System.currentTimeMillis()): Unit = { _clickBoost = now }
   def queryParsing(now: Long = System.currentTimeMillis()): Unit = { _queryParsing = now }
-  def personalizedSearcher(now: Long = System.currentTimeMillis()): Unit = { _personalizedSearcher = now }
   def search(now: Long = System.currentTimeMillis()): Unit = { _search = now }
   def processHits(now: Long = System.currentTimeMillis()): Unit = { _processHits = now }
   def done(now: Long = System.currentTimeMillis()): Unit = { _endTime = now }
@@ -27,7 +25,6 @@ class SearchTimeLogs(startTime: Long = System.currentTimeMillis()) extends Loggi
     send("mainSearch.socialGraphInfo", _socialGraphInfo, ALWAYS)
     send("mainSearch.queryParsing", _queryParsing, ALWAYS)
     send("mainSearch.getClickboost", _clickBoost, ALWAYS)
-    send("mainSearch.personalizedSearcher", _personalizedSearcher, ALWAYS)
     send("mainSearch.LuceneSearch", _search, ALWAYS)
     send("mainSearch.processHits", _processHits, ALWAYS)
     send("mainSearch.total", _endTime, ALWAYS)
@@ -43,7 +40,6 @@ class SearchTimeLogs(startTime: Long = System.currentTimeMillis()) extends Loggi
       ("socialGraphInfo", _socialGraphInfo),
       ("queryParsing", _queryParsing),
       ("clickBoost", _clickBoost),
-      ("personalizedSearcher", _personalizedSearcher),
       ("luceneSearch", _search),
       ("processHits", _processHits)
     ).filter(_._2 > 0L).sortBy(_._2)
