e1b157ba247d660d605d19de34e0f78cb6acb52c
Yiping Liao <yiping@kifi.com>
2014-10-08 11:23:57 -0700

    Style and implement interactions for add-to-library widget. https://app.asana.com/0/15523651812135/17179575596212

--- kifi-backend/modules/shoebox/angular/src/keep/keepToLibraryWidget.js
+++ kifi-backend/modules/shoebox/angular/src/keep/keepToLibraryWidget.js
@@ -3,8 +3,8 @@
 angular.module('kifi')
 
 .directive('kfKeepToLibraryWidget', [
-  '$rootElement', '$compile', '$document', '$filter', '$templateCache', '$timeout',
-  function ($rootElement, $compile, $document, $filter, $templateCache, $timeout) {
+  '$rootElement', '$compile', '$document', '$filter', '$templateCache', '$timeout', 'keyIndices',
+  function ($rootElement, $compile, $document, $filter, $templateCache, $timeout, keyIndices) {
     return {
       restrict: 'A',
       /*
@@ -17,7 +17,12 @@ angular.module('kifi')
         // Internal data.
         //
         var widget = null;
-        var timeout = null;
+        var selectedIndex = 0;
+        var searchInput = null;
+        var libraryList = null;
+        var justScrolled = false;
+        var removeTimeout = null;
+        var resetJustScrolledTimeout = null;
 
 
         //
@@ -30,10 +35,18 @@ angular.module('kifi')
         // Internal methods.
         //
         function cancelTimeout() {
-          $timeout.cancel(timeout);
+          $timeout.cancel(removeTimeout);
+          $timeout.cancel(resetJustScrolledTimeout);
+        }
+
+        function clearSelection () {
+          scope.libraries.forEach(function (library) {
+            library.selected = false;
+          });
         }
 
         function removeWidget() {
+          clearSelection();
           $document.off('mousedown', onClick);
           cancelTimeout();
 
@@ -53,8 +66,47 @@ angular.module('kifi')
           // otherwise the widget disappears too quickly for the user to see a nice
           // highlight on their selection.
           if (angular.element(event.target).closest('.library-select-option').length) {
-            $timeout(removeWidget, 200);
+            removeTimeout = $timeout(removeWidget, 200);
             return;
+          }
+        }
+
+        /**
+         * If a user uses up-and-down arrows to select a library that is not visible,
+         * scroll the list of libraries so that the selected library is visible.
+         *
+         * @param {number} selectedIndex - the index of the selected library in the library list.
+         */
+        function adjustScroll(selectedIndex) {
+
+          /**
+           * After we finish scrolling, we set a flag that a scroll has just happened so that
+           * a mouseenter event on a library item that was triggered as a result of the scroll
+           * would not result in that library item being selected. After a short amount of time,
+           * set the flag to false so that mouseenter can function as normal.
+           */
+          function setJustScrolled() {
+            justScrolled = true;
+            resetJustScrolledTimeout = $timeout(function () {
+              justScrolled = false;
+            }, 200);
+          }
+
+          // Each library list item is 47px high, and we fit in 8 library list items within the
+          // visible area. For a library item to be visible, it should be entirely within the
+          // visible area (this means its top offset should be at least one library item height from
+          // the visible bottom).
+
+          var selectedLibraryTop = selectedIndex * 47;
+          var visibleTop = libraryList.scrollTop();
+          var visibleBottom = visibleTop + (8 * 47);
+
+          if (selectedLibraryTop < visibleTop) {
+            libraryList.scrollTop(selectedLibraryTop);
+            setJustScrolled();
+          } else if (selectedLibraryTop > (visibleBottom - 47)) {
+            libraryList.scrollTop(selectedLibraryTop - (7 * 47));
+            setJustScrolled();
           }
         }
 
@@ -69,19 +121,82 @@ angular.module('kifi')
           $compile(widget)(scope);
 
           // Set position.
-          var top = element.offset().top;
+          var top = element.offset().top - 100;
           var left = element.offset().left;
           widget.css({top: top + 'px', left: left + 'px'});
 
-          // Clear search test.
-          scope.search = {};
-
           // Set event handlers.
           $document.on('mousedown', onClick);
+
+          // Initialize state.
+          scope.search = {};
+          selectedIndex = 0;
+          scope.libraries[selectedIndex].selected = true;
+          libraryList = widget.find('.library-select-list');
+          searchInput = widget.find('.keep-to-library-search-input');
+
+          // Focus on search input.
+          searchInput.focus();
         };
 
-        scope.selectLibrary = function (library) {
-          scope.selection.library = library;
+        scope.onHover = function (library) {
+          if (justScrolled) {
+            justScrolled = false;
+          } else {
+            clearSelection();
+            library.selected = true;
+            scope.selection.library = library;
+            selectedIndex = _.indexOf(scope.libraries, library);
+          }
+        };
+
+        scope.onUnhover = function (library) {
+          library.selected = false;
+        };
+
+        scope.processKeyEvent = function ($event) {
+          function getNextIndex(index, direction) {
+            var nextIndex = index + direction;
+            return (nextIndex < 0 || nextIndex > scope.libraries.length - 1) ? index : nextIndex;
+          }
+
+          switch ($event.keyCode) {
+            case keyIndices.KEY_UP:
+              // Otherwise browser will move cursor to start of input.
+              $event.preventDefault();
+
+              clearSelection();
+              selectedIndex = getNextIndex(selectedIndex, -1);
+              scope.libraries[selectedIndex].selected = true;
+
+              adjustScroll(selectedIndex);
+
+              break;
+            case keyIndices.KEY_DOWN:
+              // Otherwise browser will move cursor to end of input.
+              $event.preventDefault();
+
+              clearSelection();
+              selectedIndex = getNextIndex(selectedIndex, 1);
+              scope.libraries[selectedIndex].selected = true;
+
+              adjustScroll(selectedIndex);
+
+              break;
+            case keyIndices.KEY_ENTER:
+              // Prevent any open modals from processing this.
+              $event.stopPropagation();
+
+              scope.selection.library = scope.libraries[selectedIndex];
+              removeWidget();
+              break;
+            case keyIndices.KEY_ESC:
+              // Prevent any open modals from processing this.
+              $event.stopPropagation();
+
+              removeWidget();
+              break;
+          }
         };
 
 
