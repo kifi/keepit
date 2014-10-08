b967b73da672a9463cc85841e3e60c95c33b6157
eishay <eishay@gmail.com>
2014-10-07 18:49:33 -0700

    more abstraction

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/email/FeedDigestEmailSender.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/email/FeedDigestEmailSender.scala
@@ -22,6 +22,7 @@ import com.keepit.shoebox.ShoeboxServiceClient
 import com.keepit.social.{ BasicUser, SocialNetworks }
 import com.kifi.franz.SQSQueue
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.twirl.api.Html
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -75,7 +76,7 @@ object RecentInterestRankStrategy extends RecoRankStrategy {
   }
 }
 
-case class AllDigestRecos(toUser: Id[User], recos: Seq[DigestReco], isFacebookConnected: Boolean = false)
+case class AllDigestItems(toUser: Id[User], items: Seq[DigestItem], isFacebookConnected: Boolean = false)
 
 case class DigestCandidate(uriId: Id[NormalizedURI], topic: Option[TopicAttribution], recommendationId: Id[UriRecommendation], userAttribution: Option[UserAttribution])
 
@@ -99,11 +100,13 @@ trait DigestItem {
   val viewPageUrl = if (isForQa) uri.url else s"${config.applicationBaseUrl}/r/e/1/recos/view?id=${uri.externalId}"
   val sendPageUrl = if (isForQa) uri.url else s"${config.applicationBaseUrl}/r/e/1/recos/send?id=${uri.externalId}"
   val keepUrl = if (isForQa) uri.url else s"${config.applicationBaseUrl}/r/e/1/recos/keep?id=${uri.externalId}"
+
+  val reasonHeader: Option[Html]
 }
 
 case class DigestReco(topicOpt: Option[TopicAttribution], recommendationId: Id[UriRecommendation], uri: NormalizedURI, uriSummary: URISummary,
     keepers: DigestItemKeepers, protected val config: FortyTwoConfig, protected val isForQa: Boolean = false) extends DigestItem {
-  val topic = topicOpt.map(_.topicName)
+  val reasonHeader = topicOpt.map(t => Html(s"Recommended because it’s trending in a topic you’re interested in: ${t.topicName}"))
 }
 
 case class DigestItemKeepers(friends: Seq[Id[User]] = Seq.empty, others: Int = 0, friendsToShow: Seq[Id[User]] = Seq.empty) {
@@ -210,7 +213,7 @@ class FeedDigestEmailSender @Inject() (
     socialInfos: Seq[SocialUserInfo]): Future[DigestRecoMail] = {
 
     val isFacebookConnected = socialInfos.find(_.networkType == SocialNetworks.FACEBOOK).exists(_.getProfileUrl.isDefined)
-    val emailData = AllDigestRecos(toUser = userId, recos = digestRecos, isFacebookConnected = isFacebookConnected)
+    val emailData = AllDigestItems(toUser = userId, items = digestRecos, isFacebookConnected = isFacebookConnected)
 
     val emailToSend = EmailToSend(
       category = NotificationCategory.User.DIGEST,
@@ -236,7 +239,7 @@ class FeedDigestEmailSender @Inject() (
     }
   }
 
-  private def sendAnonymoizedEmailToQa(module: EmailToSend, emailData: AllDigestRecos): Unit = {
+  private def sendAnonymoizedEmailToQa(module: EmailToSend, emailData: AllDigestItems): Unit = {
     // these hard-coded userIds are to replace the email references to the recipient user's friends;
     // with an attempt to maintain the same look and feel, of the original email w/o revealing the real users
 
@@ -248,22 +251,24 @@ class FeedDigestEmailSender @Inject() (
 
     val qaEmailData = emailData.copy(
       toUser = myFakeUserId,
-      recos = emailData.recos.map { reco =>
-        val qaFriends = otherUserIds.take(reco.keepers.friends.size)
-        val qaKeepers = qaFriends.take(reco.keepers.friendsToShow.size)
-        reco.copy(
-          isForQa = true,
-          keepers = reco.keepers.copy(
-            friends = qaFriends,
-            friendsToShow = qaKeepers
+      items = emailData.items.map { item =>
+        val qaFriends = otherUserIds.take(item.keepers.friends.size)
+        val qaKeepers = qaFriends.take(item.keepers.friendsToShow.size)
+        item match {
+          case reco: DigestReco => reco.copy(
+            isForQa = true,
+            keepers = item.keepers.copy(
+              friends = qaFriends,
+              friendsToShow = qaKeepers
+            )
           )
-        )
+        }
       }
     )
 
     val qaEmailToSend = EmailToSend(
       category = NotificationCategory.User.DIGEST_QA,
-      subject = s"Kifi Digest: ${emailData.recos.head.title}",
+      subject = s"Kifi Digest: ${emailData.items.head.title}",
       to = Right(SystemEmailAddress.FEED_QA),
       from = SystemEmailAddress.NOTIFICATIONS,
       htmlTemplate = views.html.email.feedDigest(qaEmailData),
