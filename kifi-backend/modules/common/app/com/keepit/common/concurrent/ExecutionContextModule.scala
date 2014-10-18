fb357e3750de07c35780728edd544ae50ab67c01
eishay <eishay@gmail.com>
2014-10-17 17:07:17 -0700

    out of bound fix

--- kifi-backend/modules/common/app/com/keepit/common/concurrent/ExecutionContextModule.scala
+++ kifi-backend/modules/common/app/com/keepit/common/concurrent/ExecutionContextModule.scala
@@ -3,7 +3,7 @@ package com.keepit.common.concurrent
 import com.google.inject.{ Provides, Singleton }
 import net.codingwell.scalaguice.ScalaModule
 
-import scala.concurrent.{ExecutionContext => ScalaExecutionContext}
+import scala.concurrent.{ ExecutionContext => ScalaExecutionContext }
 
 case class ExecutionContextModule() extends ScalaModule {
 
