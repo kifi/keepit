83cbfcfe5e11461e2c16e0c4a9a558adbb0cb626
Jared Jacobs <jared@kifi.com>
2014-10-23 12:00:31 -0700

    3.2.20: retiring forced FTUE experiment, removing x from guide steps widget

--- packrat/scripts/guide/step_2.js
+++ packrat/scripts/guide/step_2.js
@@ -24,7 +24,7 @@ guide.step2 = guide.step2 || function () {
     }
   };
 
-  function show($guide, page, pageIdx, allowEsc) {
+  function show($guide, page, pageIdx) {
     if (!step) {
       step = guide.step(steps, {
         $guide: $guide,
@@ -34,8 +34,7 @@ guide.step2 = guide.step2 || function () {
         done: .5,
         anchor: 'tl',
         opacity: .65,
-        hide: onHide,
-        esc: allowEsc
+        hide: onHide
       });
     }
   }
