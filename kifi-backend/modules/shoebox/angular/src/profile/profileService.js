f88dff1c3889e750012d60f6c3086e7849027952
Andrew Conner <git@andrewconner.org>
2014-10-27 11:03:18 -0700

    Default prefs.

--- kifi-backend/modules/shoebox/angular/src/profile/profileService.js
+++ kifi-backend/modules/shoebox/angular/src/profile/profileService.js
@@ -10,7 +10,11 @@ angular.module('kifi')
       picUrl: 'https://www.kifi.com/assets/img/ghost.200.png',
       seqNum: 0
     };
-    var prefs = {};
+    var prefs = {
+      "library_callout_shown": true,
+      "tag_callout_shown": true,
+      "guide_callout_shown": true
+    };
     var userLoggedIn; // undefined means we don't know the status yet
 
     $rootScope.$on('social.updated', function () {
