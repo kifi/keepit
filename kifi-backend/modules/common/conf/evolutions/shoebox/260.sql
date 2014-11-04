49077943fbfc4d73ee6d061818a43629605c16f9
Léo Grimaldi <leo@42go.com>
2014-11-04 09:30:27 -0800

    Add route and update evolution

--- kifi-backend/modules/common/conf/evolutions/shoebox/260.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/260.sql
@@ -2,7 +2,8 @@
 
 # --- !Ups
 
-ALTER TABLE collection ADD CONSTRAINT collection_u_user_id_name UNIQUE (user_id, name);
+ALTER TABLE collection ADD UNIQUE INDEX collection_u_user_id_name (user_id, name);
+DROP INDEX collection_i_user_id_name;
 
 insert into evolutions (name, description) values('260.sql', 'add unique constraint on (user_id, name) for collection');
 
