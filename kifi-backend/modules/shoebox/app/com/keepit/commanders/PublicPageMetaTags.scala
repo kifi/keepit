12c0f35cb2694335a924ca4d58ea4fb4162b9852
Andrew Conner <git@andrewconner.org>
2014-11-07 12:41:35 -0800

    Getting rid of HTML tag

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/PublicPageMetaTags.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/PublicPageMetaTags.scala
@@ -59,7 +59,7 @@ case class PublicPageMetaFullTags(unsafeTitle: String, url: String, urlPathOnly:
       s"""
          |<meta property="og:image" content="$image" />
          |<meta itemprop="image" content="$image">
-       """.stripMargin
+       """.stripMargin.trim
     } mkString ("\n")
 
     def twitterImageTags = images.headOption map { image =>
@@ -73,7 +73,6 @@ case class PublicPageMetaFullTags(unsafeTitle: String, url: String, urlPathOnly:
     } getOrElse ""
 
     s"""
-      |<html itemscope itemtype="http://schema.org/Product">
       |<title>${title}</title>
       |<meta name="apple-itunes-app" content="app-id=740232575, app-argument=kifi:$urlPathOnly"/>
       |<meta name="apple-mobile-web-app-capable" content="no"/>
