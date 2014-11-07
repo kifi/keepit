134eb01ec29d29453bd4dbe4b21a6c9e1909ce9a
eishay <eishay@gmail.com>
2014-11-07 15:49:31 -0800

    caching sitemap and serving it dynamically with last modification time

--- kifi-backend/modules/shoebox/app/com/keepit/common/seo/SiteMapGenerator.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/common/seo/SiteMapGenerator.scala
@@ -1,14 +1,16 @@
 package com.keepit.common.seo
 
+import com.keepit.common.cache.TransactionalCaching.Implicits.directCacheAccess
 import com.google.inject.{ Singleton, Inject }
 import com.keepit.commanders.LibraryCommander
 import com.keepit.common.CollectionHelpers
+import com.keepit.common.cache._
 import com.keepit.common.time._
 import com.keepit.common.actor.ActorInstance
 import com.keepit.common.akka.FortyTwoActor
 import com.keepit.common.db.slick.Database
 import com.keepit.common.healthcheck.AirbrakeNotifier
-import com.keepit.common.logging.Logging
+import com.keepit.common.logging.{ AccessLog, Logging }
 import com.keepit.common.plugin.{ SchedulerPlugin, SchedulingProperties }
 import com.keepit.inject.FortyTwoConfig
 import com.keepit.model.{ KeepRepo, UserRepo, Library, LibraryRepo }
@@ -17,7 +19,7 @@ import play.api.{ Play, Plugin }
 import Play.current
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.xml.Elem
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 trait SiteMapGeneratorPlugin extends Plugin {
   def generate()
@@ -59,6 +61,15 @@ class SiteMapGeneratorActor @Inject() (
 
 }
 
+class SiteMapCache(stats: CacheStatistics, accessLog: AccessLog, innermostPluginSettings: (FortyTwoCachePlugin, Duration), innerToOuterPluginSettings: (FortyTwoCachePlugin, Duration)*)
+  extends StringCacheImpl[SiteMapKey](stats, accessLog, innermostPluginSettings, innerToOuterPluginSettings: _*)
+
+case class SiteMapKey() extends Key[String] {
+  override val version = 1
+  val namespace = "sitemap"
+  def toKey(): String = ""
+}
+
 @Singleton
 class SiteMapGenerator @Inject() (
     airbrake: AirbrakeNotifier,
@@ -67,50 +78,63 @@ class SiteMapGenerator @Inject() (
     keepRepo: KeepRepo,
     fortyTwoConfig: FortyTwoConfig,
     libraryRepo: LibraryRepo,
+    siteMapCache: SiteMapCache,
     libraryCommander: LibraryCommander) extends Logging {
 
+  def intern(): Future[String] = {
+    siteMapCache.getOrElseFuture(SiteMapKey()) {
+      generate()
+    }
+  }
+
   // prototype/silver-bullet implementation -- will learn and improve
-  def generate(): Future[Elem] = Future.successful {
-    val libraries = db.readOnlyReplica { implicit ro => libraryRepo.getAllPublishedLibraries().take(50000) } // ahem does not scale
-    // may want to add warning when > 40K
+  def generate(): Future[String] = {
+    db.readOnlyReplicaAsync { implicit ro =>
+      // ahem does not scale
+      // may want to add warning when > 40K
+      libraryRepo.getAllPublishedLibraries().take(50000)
+    } map { libraries =>
 
-    // batch, optimize
-    val ownerIds = CollectionHelpers.dedupBy(libraries.map(_.ownerId))(id => id)
-    val owners = db.readOnlyMaster { implicit ro => userRepo.getUsers(ownerIds) } // cached
-    val libs = libraries.filter { lib =>
-      owners.get(lib.ownerId).isDefined && db.readOnlyMaster { implicit ro => keepRepo.getCountByLibrary(lib.id.get) > 3 } // proxy for quality; need bulk version
-    }
-
-    def path(lib: Library): String = {
-      val owner = owners(lib.ownerId)
-      s"${fortyTwoConfig.applicationBaseUrl}{Library.formatLibraryPath(owner.username, owner.externalId, lib.slug)}"
-    }
-
-    def lastMod(lib: Library): DateTime = {
-      db.readOnlyReplica { implicit s =>
-        keepRepo.latestKeepInLibrary(lib.id.get) match {
-          case Some(date) if date.isAfter(lib.updatedAt) => date
-          case _ => lib.updatedAt
-        }
+      // batch, optimize
+      val ownerIds = CollectionHelpers.dedupBy(libraries.map(_.ownerId))(id => id)
+      val owners = db.readOnlyMaster { implicit ro => userRepo.getUsers(ownerIds) } // cached
+      val libs = libraries.filter { lib =>
+        owners.get(lib.ownerId).isDefined && db.readOnlyMaster { implicit ro => keepRepo.getCountByLibrary(lib.id.get) > 3 } // proxy for quality; need bulk version
       }
-    }
 
-    // location only for now
-    val urlset =
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        {
-          libs.map { lib =>
-            <url>
-              <loc>
-                { path(lib) }
-              </loc>
-              <lastmod>{ ISO_8601_DAY_FORMAT.print(lastMod(lib)) }</lastmod>
-            </url>
+      def path(lib: Library): String = {
+        val owner = owners(lib.ownerId)
+        s"${fortyTwoConfig.applicationBaseUrl}{Library.formatLibraryPath(owner.username, owner.externalId, lib.slug)}"
+      }
+
+      def lastMod(lib: Library): DateTime = {
+        db.readOnlyReplica { implicit s =>
+          keepRepo.latestKeepInLibrary(lib.id.get) match {
+            case Some(date) if date.isAfter(lib.updatedAt) => date
+            case _ => lib.updatedAt
           }
         }
-      </urlset>
-    log.info(s"[generate] done with sitemap generation. #libraries=${libs.size}")
-    urlset
+      }
+
+      val urlset =
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          {
+            libs.map { lib =>
+              <url>
+                <loc>
+                  { path(lib) }
+                </loc>
+                <lastmod>{ ISO_8601_DAY_FORMAT.print(lastMod(lib)) }</lastmod>
+              </url>
+            }
+          }
+        </urlset>
+      log.info(s"[generate] done with sitemap generation. #libraries=${libs.size}")
+      s"""
+         |<?xml-stylesheet type='text/xsl' href='sitemap.xsl'?>
+         |${urlset.toString}
+       """.stripMargin
+    }
   }
 
 }
