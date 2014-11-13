2d6e09424fc55fae182a1cfce985b9c2e6f3da62
Stephen Kemmerling <stephen@42go.com>
2014-11-13 14:02:18 -0800

    reviving the old recommendation reaper

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/RecommendationCleanupCommander.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/RecommendationCleanupCommander.scala
@@ -6,16 +6,24 @@ import com.keepit.common.time.currentDateTime
 import com.keepit.curator.model.{ PublicFeedRepo, UriRecommendationRepo }
 import org.joda.time.DateTime
 import com.keepit.common.time._
+import scala.util.Failure
 
 class RecommendationCleanupCommander @Inject() (
     db: Database,
-    uriRecRepo: UriRecommendationRepo,
+    uriRecoRepo: UriRecommendationRepo,
     feedsRepo: PublicFeedRepo) {
 
-  private val defaultLimitNumRecosForUser = 1000
-  def cleanupLowMasterScoreRecos(limitNumRecosForUser: Option[Int] = Some(defaultLimitNumRecosForUser), before: Option[DateTime] = Some(currentDateTime.minusDays(14))): Boolean = {
-    db.readWrite { implicit session =>
-      uriRecRepo.cleanupLowMasterScoreRecos(limitNumRecosForUser.getOrElse(defaultLimitNumRecosForUser), before.getOrElse(currentDateTime.minusDays(14)))
+  private val defaultLimitNumRecosForUser = 500
+  def cleanupLowMasterScoreRecos(overrideLimit: Option[Int] = None, overrideTimeCutoff: Option[DateTime] = None): Unit = {
+    val userToClean = db.readOnlyReplica { implicit session => uriRecoRepo.getUsersWithRecommendations() }.toSeq
+    db.readWriteBatch(userToClean) { (session, userId) =>
+      uriRecoRepo.cleanupLowMasterScoreRecos(userId, overrideLimit.getOrElse(defaultLimitNumRecosForUser), overrideTimeCutoff.getOrElse(currentDateTime.minusDays(7)))(session)
+    }.foreach {
+      case (userId, res) =>
+        res match {
+          case Failure(ex) => throw ex
+          case _ =>
+        }
     }
   }
 
