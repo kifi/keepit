c77b64f7fd08165d2ecdf5c083a1f1f0af7554f0
Yiping Liao <yiping@kifi.com>
2014-11-12 17:40:05 -0800

    Implement library search for logged-out users; add some user_clicked_page and visitor_clicked_page tracking.

--- kifi-backend/modules/shoebox/angular/src/layout/header/PublicHeaderCtrl.js
+++ kifi-backend/modules/shoebox/angular/src/layout/header/PublicHeaderCtrl.js
@@ -2,31 +2,65 @@
 
 angular.module('kifi')
 
-.controller('PublicHeaderCtrl', ['$scope', 'env', 'signupService', 'platformService',
-  function ($scope, env, signupService, platformService) {
+.controller('PublicHeaderCtrl', ['$scope', '$rootScope', 'env', 'signupService', 'platformService', 'libraryService',
+  function ($scope, $rootScope, env, signupService, platformService, libraryService) {
     $scope.navBase = env.navBase;
+
+    $scope.clickLogo = function () {
+      $scope.$emit('getCurrentLibrary', { callback: function (lib) {
+        if (lib && lib.id) {
+          libraryService.trackEvent('visitor_clicked_page', lib, {
+            type: 'libraryLanding',
+            action: 'clickedLogo'
+          });
+        }
+      }});
+    };
 
     $scope.join = function ($event) {
       $event.preventDefault();
 
-      if (platformService.isSupportedMobilePlatform()) {
-        platformService.goToAppOrStore();
-      } else {
-        $scope.$emit('getCurrentLibrary', { callback: function (lib) {
+      $scope.$emit('getCurrentLibrary', {
+        callback: function (lib) {
           var userData;
+
           if (lib && lib.id) {
             userData = { libraryId: lib.id };
+
+            libraryService.trackEvent('visitor_clicked_page', lib, {
+              type: 'libraryLanding',
+              action: 'clickedSignupHeader'
+            });
           }
-          signupService.register(userData);
-        }});
-      }
+
+          if (platformService.isSupportedMobilePlatform()) {
+            platformService.goToAppOrStore();
+          } else {
+            signupService.register(userData);
+          }
+        }
+      });
     };
 
     $scope.login = function ($event) {
       if (platformService.isSupportedMobilePlatform()) {
         $event.preventDefault();
-        platformService.goToAppOrStore();
       }
+
+      $scope.$emit('getCurrentLibrary', {
+        callback: function (lib) {
+          if (lib && lib.id) {
+            libraryService.trackEvent('visitor_clicked_page', lib, {
+              type: 'libraryLanding',
+              action: 'clickedLoginHeader'
+            });
+          }
+
+          if (platformService.isSupportedMobilePlatform()) {
+            platformService.goToAppOrStore();
+          }
+        }
+      });
     };
   }
 ]);
