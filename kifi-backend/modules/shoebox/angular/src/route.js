851ca52ed53303d28595ea014efbe29e7233e837
Yiping Liao <yiping@kifi.com>
2014-12-09 16:10:01 -0800

    Migrate Angular router from ngRoute to AngularUI router (ui.router); skeletal user profiles.

--- kifi-backend/modules/shoebox/angular/src/route.js
+++ kifi-backend/modules/shoebox/angular/src/route.js
@@ -2,63 +2,145 @@
 
 angular.module('kifi')
 
-.config([
-  '$routeProvider', '$locationProvider', '$httpProvider',
-  function ($routeProvider, $locationProvider, $httpProvider) {
+.config(['$httpProvider', '$locationProvider', '$stateProvider', '$urlRouterProvider',
+  function($httpProvider, $locationProvider, $stateProvider, $urlRouterProvider) {
     $locationProvider
       .html5Mode(true)
       .hashPrefix('!');
 
-    $routeProvider.when('/friends', {
-      templateUrl: 'friends/friends.tpl.html'
-    }).when('/friends/requests', {
-      redirectTo: '/friends'
-    }).when('/friends/requests/:network', {
-      redirectTo: '/friends'
-    }).when('/helprank/:helprank', {
-      templateUrl: 'helprank/helprank.tpl.html',
-      controller: 'HelpRankCtrl'
-    }).when('/', {
-      templateUrl: 'recos/recosView.tpl.html'
-    }).when('/invite', {
-      templateUrl: 'invite/invite.tpl.html'
-    }).when('/friends/invite', {
-      redirectTo: '/invite'
-    }).when('/profile', {
-      templateUrl: 'profile/profile.tpl.html',
-      controller: 'ProfileCtrl'
-    }).when('/recommendations', {
-      redirectTo: '/'
-    }).when('/find', {
-      templateUrl: 'search/search.tpl.html',
-      controller: 'SearchCtrl',
-      reloadOnSearch: false
-    }).when('/keep/:keepId', {
-      templateUrl: 'keep/keepView.tpl.html',
-      controller: 'KeepViewCtrl'
-    }).when('/tags/manage', {
-      templateUrl: 'tagManage/tagManage.tpl.html',
-      controller: 'ManageTagCtrl'
-    })
-    // ↓↓↓↓↓ Important: This needs to be last! ↓↓↓↓↓
-    .when('/:username/:librarySlug/find', {
-      templateUrl: 'libraries/library.tpl.html',
-      controller: 'LibraryCtrl',
-      resolve: { librarySearch: function () { return true; } },
-      reloadOnSearch: false
-    })
-
-    .when('/:username/:librarySlug', {
-      templateUrl: 'libraries/library.tpl.html',
-      controller: 'LibraryCtrl',
-      resolve: { librarySearch: function () { return false; } }
-    });
-    // ↑↑↑↑↑ Important: This needs to be last! ↑↑↑↑↑
-
-    $routeProvider.otherwise({
-      redirectTo: '/'
-    });
-
     $httpProvider.defaults.withCredentials = true;
-  }
-]);
+
+    // URL redirects.
+    $urlRouterProvider
+      .when('/friends/invite', '/invite')
+      .when('/friends/requests', '/friends')
+      .when('/friends/requests/:network', 'friends')
+      .when('/recommendations', '/')
+
+      // For any unmatched url, redirect to '/'.
+      .otherwise('/');
+
+    // Set up the states.
+    $stateProvider
+      .state('home', {  // Home page.
+        url: '/',
+        templateUrl: 'recos/recosView.tpl.html'
+      })
+      .state('friends', {
+        url: '/friends',
+        templateUrl: 'friends/friends.tpl.html'
+      })
+      .state('helpRank', {
+        url: '/helprank/:helprank',
+        templateUrl: 'helprank/helprank.tpl.html',
+        controller: 'HelpRankCtrl'
+      })
+      .state('invite', {
+        url: '/invite',
+        templateUrl: 'invite/invite.tpl.html'
+      })
+      .state('keep', {
+        url: '/keep/:keepId',
+        templateUrl: 'keep/keepView.tpl.html',
+        controller: 'KeepViewCtrl'
+      })
+      .state('manageTags', {
+        url: '/tags/manage',
+        templateUrl: 'tagManage/tagManage.tpl.html',
+        controller: 'ManageTagCtrl'
+      })
+      .state('settings', {
+        url: '/profile',
+        templateUrl: 'profile/profile.tpl.html',
+        controller: 'ProfileCtrl'
+      })
+      .state('search', {
+        url: '/find?q&f',
+        templateUrl: 'search/search.tpl.html',
+        controller: 'SearchCtrl',
+
+        // SearchCtrl manually reloads search results on search input change in a debounced function.
+        reloadOnSearch: false
+      })
+      .state('userProfile', {
+        url: '/:username?upb',
+        templateUrl: 'userProfile/userProfile.tpl.html',
+        controller: 'UserProfileCtrl',
+        'abstract': true
+      })
+        .state('userProfile.libraries', {
+          url: '',
+          templateUrl: 'userProfile/userProfileLibraries.tpl.html',
+          controller: 'UserProfileLibrariesCtrl',
+          'abstract': true
+        })
+          .state('userProfile.libraries.my', {
+            url: '',
+            templateUrl: 'userProfile/userProfileLibrariesList.tpl.html',
+            controller: 'UserProfileLibrariesListCtrl',
+            data: {
+              libraryType: 'My'
+            }
+          })
+          .state('userProfile.libraries.following', {
+            url: '/libraries/following',
+            templateUrl: 'userProfile/userProfileLibrariesList.tpl.html',
+            controller: 'UserProfileLibrariesListCtrl',
+            data: {
+              libraryType: 'Following'
+            }
+          })
+          .state('userProfile.libraries.invited', {
+            url: '/libraries/invited',
+            templateUrl: 'userProfile/userProfileLibrariesList.tpl.html',
+            controller: 'UserProfileLibrariesListCtrl',
+            data: {
+              libraryType: 'Invited'
+            }
+          })
+        .state('userProfile.friends', {
+          url: '/friends',
+          templateUrl: 'userProfile/userProfilePeople.tpl.html',
+          controller: 'UserProfilePeopleCtrl',
+          data: {
+            peopleType: 'Friends'
+          }
+        })
+        .state('userProfile.followers', {
+          url: '/followers',
+          templateUrl: 'userProfile/userProfilePeople.tpl.html',
+          controller: 'UserProfilePeopleCtrl',
+          data: {
+            peopleType: 'Followers'
+          }
+        })
+        .state('userProfile.helped', {
+          url: '/helped',
+          templateUrl: 'userProfile/userProfileKeeps.tpl.html',
+          controller: 'UserProfileKeepsCtrl'
+        })
+
+      // ↓↓↓↓↓ Important: This needs to be last! ↓↓↓↓↓
+      .state('library', {
+        url: '/:username/:librarySlug',
+        templateUrl: 'libraries/library.tpl.html',
+        controller: 'LibraryCtrl',
+        data: {
+          librarySearch: false
+        },
+        'abstract': true
+      })
+        .state('library.keeps', {
+          url: '',
+          templateUrl: 'libraries/libraryKeeps.tpl.html'
+        })
+        .state('library.search', {
+          url: '/find?q&f',
+          templateUrl: 'search/search.tpl.html',
+          controller: 'SearchCtrl',
+          data: {
+            librarySearch: true
+          }
+        });
+      // ↑↑↑↑↑ Important: This needs to be last! ↑↑↑↑↑
+}]);
