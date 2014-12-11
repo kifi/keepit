ba00b308d625503f4f4029ae18eb77759acab549
Raymond Ng <raymond.ng@gmail.com>
2014-12-10 18:10:23 -0800

    Revert "Revert "OAuth2Test++ ""
    This reverts commit 018aadde43709734d5047cab25b8cb0255406599.

--- kifi-backend/modules/common/app/com/keepit/social/providers/FacebookProvider.scala
+++ kifi-backend/modules/common/app/com/keepit/social/providers/FacebookProvider.scala
@@ -3,26 +3,16 @@ package com.keepit.social.providers
 import com.keepit.FortyTwoGlobal
 import com.keepit.common.logging.Logging
 import com.keepit.common.oauth.FacebookOAuthProviderImpl
-import com.keepit.common.oauth.adaptor.SecureSocialProviderHelper
+import com.keepit.common.oauth.adaptor.SecureSocialOAuth2Adaptor
 import com.keepit.social.UserIdentityProvider
-
-import play.api.libs.ws.{ WSResponse }
-import play.api.{ Application }
-import securesocial.core._
 import net.codingwell.scalaguice.InjectorExtensions._
+import play.api.Application
 
 class FacebookProvider(app: Application)
-    extends securesocial.core.providers.FacebookProvider(app) with UserIdentityProvider with SecureSocialProviderHelper with Logging {
-
-  override protected def buildInfo(response: WSResponse): OAuth2Info = {
-    try super.buildInfo(response) catch {
-      case e: Throwable =>
-        log.info(s"[securesocial] Failed to build oauth2 info. Response was ${response.body}")
-        throw e
-    }
-  }
-
+    extends securesocial.core.providers.FacebookProvider(app)
+    with UserIdentityProvider
+    with SecureSocialOAuth2Adaptor
+    with Logging {
   lazy val global = app.global.asInstanceOf[FortyTwoGlobal] // fail hard
   lazy val provider = global.injector.instance[FacebookOAuthProviderImpl]
-
 }
---
+++ kifi-backend/modules/shoebox/test/com/keepit/common/oauth/OAuth2Test.scala
@@ -0,0 +1,105 @@
+package com.keepit.common.oauth
+
+import java.net.URLDecoder
+
+import com.keepit.abook.FakeABookServiceClientModule
+import com.keepit.common.actor.FakeActorSystemModule
+import com.keepit.common.concurrent.FakeExecutionContextModule
+import com.keepit.common.controller.FakeUserActionsModule
+import com.keepit.common.controller.KifiSession._
+import com.keepit.common.core._
+import com.keepit.common.external.FakeExternalServiceModule
+import com.keepit.common.healthcheck.FakeAirbrakeModule
+import com.keepit.common.mail.FakeMailModule
+import com.keepit.common.net.FakeHttpClientModule
+import com.keepit.common.social.{ FakeShoeboxAppSecureSocialModule, FakeSocialGraphModule }
+import com.keepit.common.store.FakeShoeboxStoreModule
+import com.keepit.controllers.core.AuthController
+import com.keepit.cortex.FakeCortexServiceClientModule
+import com.keepit.curator.FakeCuratorServiceClientModule
+import com.keepit.heimdal.{ FakeHeimdalServiceClientModule, HeimdalContext }
+import com.keepit.scraper.{ FakeScrapeSchedulerModule, FakeScraperServiceClientModule }
+import com.keepit.search.FakeSearchServiceClientModule
+import com.keepit.shoebox.{ FakeShoeboxServiceModule, KeepImportsModule }
+import com.keepit.test.{ ShoeboxApplication, ShoeboxApplicationInjector }
+import org.specs2.mutable.Specification
+import play.api.test.Helpers._
+import play.api.test._
+
+class OAuth2Test extends Specification with ShoeboxApplicationInjector {
+
+  implicit val context = HeimdalContext.empty
+
+  def modules = Seq(
+    FakeExecutionContextModule(),
+    FakeShoeboxServiceModule(),
+    FakeSearchServiceClientModule(),
+    FakeScrapeSchedulerModule(),
+    FakeShoeboxStoreModule(),
+    FakeActorSystemModule(),
+    FakeAirbrakeModule(),
+    FakeABookServiceClientModule(),
+    FakeMailModule(),
+    FakeHttpClientModule(),
+    FakeSocialGraphModule(),
+    FakeHeimdalServiceClientModule(),
+    FakeShoeboxAppSecureSocialModule(),
+    FakeUserActionsModule(),
+    FakeExternalServiceModule(),
+    FakeCortexServiceClientModule(),
+    FakeScraperServiceClientModule(),
+    KeepImportsModule(),
+    FakeCuratorServiceClientModule(),
+    FakeOAuth1ConfigurationModule(),
+    FakeOAuth2ConfigurationModule()
+  )
+
+  // only checks the initial redirect flow for now; more work required
+  def oauth2FlowCheck(providerConfig: OAuth2ProviderConfiguration)(implicit authController: AuthController) = {
+    val path = com.keepit.controllers.core.routes.AuthController.signup(providerConfig.name).toString()
+    path === s"/signup/${providerConfig.name}"
+
+    val request = FakeRequest("GET", path)
+    val result = authController.signup(providerConfig.name)(request)
+
+    status(result) === SEE_OTHER // 303
+    val sess = session(result)
+    sess.getUserId.isDefined === false // no user (yet)
+    contentAsString(result) === ""
+
+    val hdrs = headers(result)
+    val locURL = new java.net.URL(hdrs("location"))
+    locURL.getHost === providerConfig.authUrl.getHost
+    locURL.getPath === providerConfig.authUrl.getPath
+    val queryParams = locURL.getQuery.split('&').map { kv =>
+      kv.split('=') |> { arr =>
+        arr(0) -> arr(1)
+      }
+    }.toMap
+    queryParams.size === 5
+    queryParams.get("state").isDefined === true
+    queryParams.get("scope").exists { s =>
+      URLDecoder.decode(s, "UTF-8") == providerConfig.scope
+    } === true
+    queryParams.get("client_id").exists(_ == providerConfig.clientId) === true
+    queryParams.get("response_type").exists(_ == "code") === true
+    queryParams.get("redirect_uri").isDefined === true
+    val redirectUrl = new java.net.URL(URLDecoder.decode(queryParams.get("redirect_uri").get, "UTF-8"))
+    redirectUrl.getPath === s"/authenticate/${providerConfig.name}"
+  }
+
+  "AuthController" should {
+    "(signup) starts oauth2.1 redirect flow" in {
+      running(new ShoeboxApplication(modules: _*)) {
+        implicit val authController = inject[AuthController]
+
+        val oauth2Config = inject[OAuth2Configuration]
+        val fbConfig = oauth2Config.getProviderConfig("facebook").get
+        oauth2FlowCheck(fbConfig)
+        val lnkdConfig = oauth2Config.getProviderConfig("linkedin").get
+        oauth2FlowCheck(lnkdConfig)
+      }
+    }
+  }
+
+}
