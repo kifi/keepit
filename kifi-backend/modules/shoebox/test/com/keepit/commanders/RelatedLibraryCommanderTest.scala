3243ad7e27f3d4edff91f0f5f5aaeb1e713c3e5b
yingjieMiao <yingjie@42go.com>
2014-12-11 15:52:58 -0800

    full tests

--- kifi-backend/modules/shoebox/test/com/keepit/commanders/RelatedLibraryCommanderTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/commanders/RelatedLibraryCommanderTest.scala
@@ -1,8 +1,18 @@
 package com.keepit.commanders
 
+import com.keepit.abook.FakeABookServiceClientModule
+import com.keepit.common.concurrent.FakeExecutionContextModule
+import com.keepit.common.crypto.FakeCryptoModule
 import com.keepit.common.db.Id
+import com.keepit.common.mail.FakeMailModule
+import com.keepit.common.social.FakeSocialGraphModule
+import com.keepit.common.store.FakeShoeboxStoreModule
 import com.keepit.cortex.FakeCortexServiceClientImpl
+import com.keepit.eliza.FakeElizaServiceClientModule
+import com.keepit.heimdal.FakeHeimdalServiceClientModule
 import com.keepit.model._
+import com.keepit.scraper.FakeScrapeSchedulerModule
+import com.keepit.search.FakeSearchServiceClientModule
 import com.keepit.test.ShoeboxTestInjector
 import org.specs2.mutable.Specification
 
@@ -11,6 +21,19 @@ import scala.concurrent.duration._
 
 class RelatedLibraryCommanderTest extends Specification with ShoeboxTestInjector {
 
+  def modules = Seq(
+    FakeExecutionContextModule(),
+    FakeScrapeSchedulerModule(),
+    FakeSearchServiceClientModule(),
+    FakeMailModule(),
+    FakeShoeboxStoreModule(),
+    FakeCryptoModule(),
+    FakeSocialGraphModule(),
+    FakeABookServiceClientModule(),
+    FakeElizaServiceClientModule(),
+    FakeHeimdalServiceClientModule()
+  )
+
   val fakeCortex = new FakeCortexServiceClientImpl(null) {
     override def similarLibraries(libId: Id[Library], limit: Int)(implicit version: LDAVersionOpt): Future[Seq[Id[Library]]] = {
       if (libId == Id[Library](1)) return Future.successful((2 to 5).map { Id[Library](_) })
@@ -18,23 +41,28 @@ class RelatedLibraryCommanderTest extends Specification with ShoeboxTestInjector
     }
   }
 
-  "RelatedLibraryCommander" should withDb() { implicit injector =>
+  "RelatedLibraryCommander" should withDb(modules: _*) { implicit injector =>
 
     def setup(): Unit = {
       val libRepo = inject[LibraryRepo]
+      val userRepo = inject[UserRepo]
       val libMemRepo = inject[LibraryMembershipRepo]
 
       db.readWrite { implicit s =>
+        (1 to 10).foreach { i => userRepo.save(User(firstName = "test" + i, lastName = "foo", username = Username("whatever"), normalizedUsername = "whatever")) }
+
         (1 to 10).foreach { i =>
           val lib = Library(name = s"Library ${i}", ownerId = Id[User](i), visibility = LibraryVisibility.PUBLISHED, slug = LibrarySlug("slug"), memberCount = i)
           libRepo.save(lib)
-        }
-      }
 
-      db.readWrite { implicit s =>
-        libMemRepo.save(LibraryMembership(libraryId = Id[Library](2), userId = Id[User](1), access = LibraryAccess.READ_WRITE, showInSearch = true))
-        libMemRepo.save(LibraryMembership(libraryId = Id[Library](3), userId = Id[User](2), access = LibraryAccess.READ_WRITE, showInSearch = true))
-        libMemRepo.save(LibraryMembership(libraryId = Id[Library](3), userId = Id[User](3), access = LibraryAccess.READ_ONLY, showInSearch = true))
+          // user i owns library i
+          libMemRepo.save(LibraryMembership(libraryId = Id[Library](i), userId = Id[User](i), access = LibraryAccess.READ_WRITE, showInSearch = true))
+
+          // a few followers for library i
+          (1 until i).foreach { j =>
+            libMemRepo.save(LibraryMembership(libraryId = Id[Library](i), userId = Id[User](j), access = LibraryAccess.READ_ONLY, showInSearch = true))
+          }
+        }
       }
     }
 
@@ -42,7 +70,7 @@ class RelatedLibraryCommanderTest extends Specification with ShoeboxTestInjector
 
     "query related libraries" in {
 
-      val commander = new RelatedLibraryCommanderImpl(db, inject[LibraryRepo], null, null, fakeCortex)
+      val commander = new RelatedLibraryCommanderImpl(db, inject[LibraryRepo], inject[LibraryMembershipRepo], inject[LibraryCommander], fakeCortex)
 
       val libsF = commander.relatedLibraries(Id[Library](1))
       Await.result(libsF, FiniteDuration(5, SECONDS)).sortBy(_.id.get).map { _.id.get.id } === List(2, 3, 4, 5)
@@ -72,6 +100,28 @@ class RelatedLibraryCommanderTest extends Specification with ShoeboxTestInjector
       val libsF3 = commander.suggestedLibraries(Id[Library](2))
       Await.result(libsF3, FiniteDuration(5, SECONDS)).sortBy(_.id.get).map { _.id.get.id }.toList === List(6, 7, 8, 9, 10)
 
+    }
+
+    "get full library info for non-user" in {
+      val commander = new RelatedLibraryCommanderImpl(db, inject[LibraryRepo], inject[LibraryMembershipRepo], inject[LibraryCommander], fakeCortex)
+      val resF = commander.suggestedLibrariesInfo(Id[Library](1), None)
+      val res = Await.result(resF, FiniteDuration(5, SECONDS))
+      res.seq.sortBy(_.numFollowers).map { _.numFollowers } === List(1, 2, 3, 4)
+      res.seq.sortBy(_.numFollowers).map { _.owner.firstName } === List(2, 3, 4, 5).map { i => "test" + i }
+    }
+
+    "do not show libraries user already know" in {
+      val commander = new RelatedLibraryCommanderImpl(db, inject[LibraryRepo], inject[LibraryMembershipRepo], inject[LibraryCommander], fakeCortex)
+      val resF = commander.suggestedLibrariesInfo(Id[Library](1), Some(Id[User](1)))
+      Await.result(resF, FiniteDuration(5, SECONDS)).size === 0
+
+      val resF2 = commander.suggestedLibrariesInfo(Id[Library](2), Some(Id[User](8)))
+      val res2 = Await.result(resF2, FiniteDuration(5, SECONDS))
+      res2.seq.sortBy(_.numFollowers).map { _.owner.firstName } === List(6, 7).map { i => "test" + i }
+
+      val resF3 = commander.suggestedLibrariesInfo(Id[Library](6), Some(Id[User](8)))
+      val res3 = Await.result(resF3, FiniteDuration(5, SECONDS))
+      res3.seq.sortBy(_.numFollowers).map { _.owner.firstName } === List(7).map { i => "test" + i }
     }
   }
 
