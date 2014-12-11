bd0edd987a24ceefe992040f35505660402b847d
Léo Grimaldi <leo@42go.com>
2014-12-11 12:09:52 -0800

    Move controllers to the search package

---
+++ kifi-backend/modules/search/test/com/keepit/search/controllers/mobile/MobileSearchControllerTest.scala
@@ -0,0 +1,146 @@
+package com.keepit.search.controllers.mobile
+
+import com.keepit.common.actor.FakeActorSystemModule
+import com.keepit.common.controller.{ FakeUserActionsHelper, FakeUserActionsModule }
+import com.keepit.common.db.{ ExternalId, Id }
+import com.keepit.common.net.FakeHttpClientModule
+import com.keepit.model._
+import com.keepit.search._
+import com.keepit.search.result.{ DecoratedResult, _ }
+import com.keepit.social.BasicUser
+import com.keepit.test.SearchTestInjector
+import com.keepit.common.util.PlayAppConfigurationModule
+import org.specs2.mutable._
+import play.api.libs.json._
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import com.keepit.common.crypto.FakeCryptoModule
+import com.keepit.search.controllers.{ FixedResultSearchCommander, FixedResultIndexModule }
+
+class MobileSearchControllerTest extends SpecificationLike with SearchTestInjector {
+
+  def modules = Seq(
+    FakeActorSystemModule(),
+    FakeUserActionsModule(),
+    FixedResultIndexModule(),
+    FakeHttpClientModule(),
+    PlayAppConfigurationModule(),
+    FakeCryptoModule()
+  )
+
+  "MobileSearchController" should {
+    "search keeps (V1)" in {
+      withInjector(modules: _*) { implicit injector =>
+        val mobileSearchController = inject[MobileSearchController]
+        val path = com.keepit.search.controllers.mobile.routes.MobileSearchController.searchV1("test", None, 7, None, None, None, None, None, None, None).toString
+        path === "/m/1/search?q=test&maxHits=7"
+
+        inject[SearchCommander].asInstanceOf[FixedResultSearchCommander].setDecoratedResults(MobileSearchControllerTest.decoratedTestResults)
+        val user = User(Some(Id[User](1)), firstName = "prénom", lastName = "nom", username = Username("test"), normalizedUsername = "test")
+        inject[FakeUserActionsHelper].setUser(user)
+        val request = FakeRequest("GET", path)
+        val result = mobileSearchController.searchV1("test", None, 7, None, None, None, None, None, None, None)(request)
+        status(result) must equalTo(OK)
+        contentType(result) must beSome("application/json")
+
+        val expected = Json.parse(s"""
+          {
+            "uuid":"21eb7aa7-97ba-466f-a357-c3511e4c8b29",
+            "query":"test",
+            "hits":
+              [
+                {
+                  "count":2,
+                  "bookmark":
+                    {
+                      "title":"this is a test",
+                      "url":"http://kifi.com/%5B%E3%83%86%E3%82%B9%E3%83%88%5D",
+                      "id":"604754fb-182d-4c39-a314-2d1994b24159",
+                      "matches":
+                        {
+                          "title":[[9,4]]
+                        },
+                      "tags":
+                        [
+                          "c17da7ce-64bb-4c91-8832-1f1a6a88b7be",
+                          "19ccb3db-4e18-4ade-91bd-1a98ef33aa63"
+                        ]
+                      },
+                    "users":
+                      [
+                        {
+                          "id":"4e5f7b8c-951b-4497-8661-a1001885b2ec",
+                          "firstName":"Vorname",
+                          "lastName":"Nachname",
+                          "pictureName":"1.jpg",
+                          "username":"vorname"
+                        }
+                      ],
+                    "score":0.9990000128746033,
+                    "isMyBookmark":true,
+                    "isPrivate":false
+                }
+              ],
+            "myTotal":1,
+            "friendsTotal":12,
+            "othersTotal":123,
+            "mayHaveMore":false,
+            "show":true,
+            "experimentId":10,
+            "context":"AgFJAN8CZHg=",
+            "experts":[]
+          }
+        """)
+        // println(Json.parse(contentAsString(result)).toString) // can be removed?
+        Json.parse(contentAsString(result)) === expected
+      }
+    }
+  }
+}
+
+object MobileSearchControllerTest {
+  val decoratedTestResults: Map[String, DecoratedResult] = Map(
+    "test" -> DecoratedResult(
+      ExternalId[ArticleSearchResult]("21eb7aa7-97ba-466f-a357-c3511e4c8b29"), // uuid
+      Seq[DetailedSearchHit]( // hits
+        DetailedSearchHit(
+          1000, // uriId
+          2, // bookmarkCount
+          BasicSearchHit(
+            Some("this is a test"), // title
+            "http://kifi.com/[テスト]", // url, '[' and ']' should be percent-encoded and the result json
+            Some(Seq( // collections
+              ExternalId[Collection]("c17da7ce-64bb-4c91-8832-1f1a6a88b7be"),
+              ExternalId[Collection]("19ccb3db-4e18-4ade-91bd-1a98ef33aa63")
+            )),
+            Some(ExternalId[Keep]("604754fb-182d-4c39-a314-2d1994b24159")), // bookmarkId
+            Some(Seq((9, 13))), // title matches
+            None // url matches
+          ),
+          true, // isMyBookmark
+          true, // isFriendsBookmark
+          false, // isPrivate
+          Seq(Id[User](999)), // users
+          0.999f, // score
+          10.0f, // textScore
+          new Scoring( // scoring
+            1.3f, // textScore
+            1.0f, // normalizedTextScore,
+            1.0f, // bookmarkScore
+            0.5f, // recencyScore
+            false // usefulPage
+          )
+        ).set("basicUsers", JsArray(Seq(Json.toJson(BasicUser(ExternalId[User]("4e5f7b8c-951b-4497-8661-a1001885b2ec"), "Vorname", "Nachname", "1.jpg", Username("vorname"))))))
+      ),
+      1, // myTotal
+      12, // friendsTotal
+      123, // othersTotal
+      "test", // query
+      Id[User](99), // userId
+      Set(100, 220), // idFilter
+      false, // mayHaveMoreHits
+      true, //show
+      Some(Id[SearchConfigExperiment](10)) //searchExperimentId
+    )
+  )
+}
---
+++ kifi-backend/modules/search/test/com/keepit/search/controllers/mobile/MobileUserSearchControllerTest.scala
@@ -0,0 +1,197 @@
+package com.keepit.search.controllers.mobile
+
+import com.keepit.common.net.FakeHttpClientModule
+import com.keepit.test.{ SearchApplication, SearchTestInjector }
+import org.specs2.mutable._
+
+import com.keepit.model._
+import com.keepit.common.db.{ Id, ExternalId }
+import com.keepit.common.controller.{ FakeSecureSocialClientIdModule, FakeUserActionsHelper, FakeUserActionsModule }
+import com.keepit.common.actor.FakeActorSystemModule
+
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.api.libs.json.Json
+
+import akka.actor.ActorSystem
+
+import com.keepit.shoebox.FakeShoeboxServiceClientImpl
+import com.keepit.shoebox.FakeShoeboxServiceModule
+
+import com.keepit.shoebox.ShoeboxServiceClient
+import com.keepit.search.index.user.UserIndexer
+import com.keepit.search.index.user.UserSearchFilterFactory
+import com.keepit.common.mail.EmailAddress
+import com.keepit.common.util.PlayAppConfigurationModule
+
+import com.google.inject.Injector
+
+class MobileUserSearchControllerTest extends Specification with SearchTestInjector {
+
+  private def setup(client: FakeShoeboxServiceClientImpl) = {
+    val extIds = (0 until 5).map { i => "4e5f7b8c-951b-4497-8661-12345678900" + i.toString }.map { ExternalId[User] }
+    val users = (0 until 4).map { i =>
+      User(externalId = extIds(i), firstName = s"firstName${i}", lastName = s"lastName${i}", pictureName = Some(s"picName${i}"), username = Username("test"), normalizedUsername = "test")
+    } :+ User(externalId = extIds(4), firstName = "Woody", lastName = "Allen", pictureName = Some("face"), username = Username("test"), normalizedUsername = "test")
+
+    val usersWithId = client.saveUsers(users: _*)
+
+    val emails = (0 until 4).map { i => usersWithId(i).id.get -> EmailAddress(s"user${i}@42go.com") } ++ Seq(
+      usersWithId(4).id.get -> EmailAddress("woody@fox.com"),
+      usersWithId(4).id.get -> EmailAddress("Woody.Allen@GMAIL.com")
+    )
+
+    client.addEmails(emails: _*)
+
+    val friendRequests = Seq(Id[User](1) -> Id[User](2))
+    client.saveFriendRequests(friendRequests: _*)
+
+    val connections = Map(Id[User](1) -> Set(Id[User](3)))
+    client.saveConnections(connections)
+
+    usersWithId
+  }
+
+  def filterFactory(implicit injector: Injector) = inject[UserSearchFilterFactory]
+
+  def modules = {
+    Seq(
+      FakeActorSystemModule(),
+      FakeUserActionsModule(),
+      FakeHttpClientModule(),
+      FakeSecureSocialClientIdModule(),
+      FakeShoeboxServiceModule(),
+      PlayAppConfigurationModule()
+    )
+  }
+
+  "MobileUserSearchController" should {
+    "search user" in {
+      withInjector(modules: _*) { implicit injector =>
+        val client = inject[ShoeboxServiceClient].asInstanceOf[FakeShoeboxServiceClientImpl]
+        val users = setup(client)
+        val indexer = inject[UserIndexer]
+        indexer.update()
+
+        val path = com.keepit.search.controllers.mobile.routes.MobileUserSearchController.searchV1("woody", None, None, 3).toString
+        path === "/m/1/search/users/search?query=woody&maxHits=3"
+
+        inject[FakeUserActionsHelper].setUser(users.head)
+        val request = FakeRequest("GET", path)
+
+        val controller = inject[MobileUserSearchController]
+        val result = controller.searchV1("woody", None, None, 3)(request)
+        status(result) must equalTo(OK)
+        contentType(result) must beSome("application/json")
+
+        val expected = Json.parse(s"""
+          {
+            "hits":
+              [
+                {
+                  "userId":5,
+                  "basicUser":
+                    {
+                      "id":"4e5f7b8c-951b-4497-8661-123456789004",
+                      "firstName":"Woody",
+                      "lastName":"Allen",
+                      "pictureName":"face.jpg","username":"test"
+                    },
+                  "isFriend":false
+                }
+              ],
+            "context":"AgAJAAcBBQ=="
+            }
+          """)
+        Json.parse(contentAsString(result)) === expected
+      }
+    }
+
+    "page user by name" in {
+      withInjector(modules: _*) { implicit injector =>
+        val client = inject[ShoeboxServiceClient].asInstanceOf[FakeShoeboxServiceClientImpl]
+        val users = setup(client)
+        val indexer = inject[UserIndexer]
+        indexer.update()
+
+        val path = com.keepit.search.controllers.mobile.routes.MobileUserSearchController.pageV1("firstNa", None, 0, 10).toString
+        path === "/m/1/search/users/page?query=firstNa&pageNum=0&pageSize=10"
+
+        inject[FakeUserActionsHelper].setUser(users.head)
+        val request = FakeRequest("GET", path)
+        val controller = inject[MobileUserSearchController]
+        val result = controller.pageV1("firstNa", None, 0, 10)(request)
+        status(result) must equalTo(OK)
+        contentType(result) must beSome("application/json")
+
+        val expected = Json.parse(s"""
+          [
+            {
+              "user":{
+                "id":"4e5f7b8c-951b-4497-8661-123456789001",
+                "firstName":"firstName1",
+                "lastName":"lastName1",
+                "pictureName":"picName1.jpg","username":"test"
+              },
+              "status":"requested"
+            },
+
+            {
+              "user":{
+                "id":"4e5f7b8c-951b-4497-8661-123456789002",
+                "firstName":"firstName2",
+                "lastName":"lastName2",
+                "pictureName":"picName2.jpg","username":"test"
+              },
+              "status":"friend"
+            },
+            {
+              "user":{
+                "id":"4e5f7b8c-951b-4497-8661-123456789003",
+                "firstName":"firstName3",
+                "lastName":"lastName3",
+                "pictureName":"picName3.jpg","username":"test"
+              },
+              "status":""
+            }
+          ]
+          """)
+        Json.parse(contentAsString(result)) === expected
+      }
+    }
+
+    "page user by email" in {
+      withInjector(modules: _*) { implicit injector =>
+        val client = inject[ShoeboxServiceClient].asInstanceOf[FakeShoeboxServiceClientImpl]
+        val users = setup(client)
+        val indexer = inject[UserIndexer]
+        indexer.update()
+
+        val path = com.keepit.search.controllers.mobile.routes.MobileUserSearchController.pageV1("woody@fox.com", None, 0, 10).toString
+        path === "/m/1/search/users/page?query=woody%40fox.com&pageNum=0&pageSize=10"
+
+        inject[FakeUserActionsHelper].setUser(users.head)
+        val request = FakeRequest("GET", path)
+        val controller = inject[MobileUserSearchController]
+        val result = controller.pageV1("woody@fox.com", None, 0, 10)(request)
+        status(result) must equalTo(OK)
+        contentType(result) must beSome("application/json")
+
+        val expected = Json.parse(s"""
+          [
+            {
+              "user":{
+                "id":"4e5f7b8c-951b-4497-8661-123456789004",
+                "firstName":"Woody",
+                "lastName":"Allen",
+                "pictureName":"face.jpg","username":"test"
+              },
+              "status":""
+            }
+          ]
+         """)
+        Json.parse(contentAsString(result)) === expected
+      }
+    }
+  }
+}
