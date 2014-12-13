049a4bd29aba87bbefe75a6cae6c8f8dc18bf2f4
Josh McDade <josh.ncsu@gmail.com>
2014-12-12 16:58:32 -0800

    created smallLibraryCard directive for (hopefully) easier reuse and template that is wrapped by a relatedLibaryCard

--- kifi-backend/modules/shoebox/angular/src/libraries/relatedLibraries.js
+++ kifi-backend/modules/shoebox/angular/src/libraries/relatedLibraries.js
@@ -34,31 +34,4 @@ angular.module('kifi')
   }
 ])
 
-.directive('kfRelatedLibraryCard', [
-  function () {
-    return {
-      restrict: 'A',
-      replace: true,
-      scope: {
-        library: '='
-      },
-      templateUrl: 'libraries/relatedLibraryCard.tpl.html',
-      link: function (scope/*, element, attrs*/) {
-        scope.clickLibraryReco = function ($event) {
-          $event.target.href = scope.library.libraryUrl + '?o=lr';
-          scope.$emit('trackLibraryEvent', 'click', { action: 'clickedLibraryRec' });
-        };
-
-        if (scope.library.followers.length > 0 && scope.library.otherFollowersCount > 0) {
-          scope.followersToShow = _.take(scope.library.followers, 3);
-        } else {
-          scope.followersToShow = scope.library.followers;
-        }
-
-        scope.otherFollowersCount = scope.library.numFollowers - scope.followersToShow.length;
-      }
-    };
-  }
-])
-
 ;
---
+++ kifi-backend/modules/shoebox/angular/src/libraries/smallLibraryCard.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('kifi')
+
+.directive('kfSmallLibraryCard', [
+  function () {
+    return {
+      restrict: 'A',
+      replace: true,
+      scope: {
+        library: '=',
+        origin: '@',
+        action: '@'
+      },
+      templateUrl: 'libraries/smallLibraryCard.tpl.html',
+      link: function (scope/*, element, attrs*/) {
+        scope.clickLibraryReco = function ($event) {
+          $event.target.href = scope.library.libraryUrl + '?o=' + scope.origin;
+          scope.$emit('trackLibraryEvent', 'click', { action: scope.action });
+        };
+
+        if (scope.library.followers.length > 0 && scope.library.otherFollowersCount > 0) {
+          scope.followersToShow = _.take(scope.library.followers, 3);
+        } else {
+          scope.followersToShow = scope.library.followers;
+        }
+
+        scope.otherFollowersCount = scope.library.numFollowers - scope.followersToShow.length;
+      }
+    };
+  }
+])
+
+;
