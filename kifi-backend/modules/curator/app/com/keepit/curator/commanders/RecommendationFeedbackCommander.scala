987d02cdafbafbe83d48b89f2b4d8680a6a2c711
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2014-12-18 13:26:38 -0800

    revamped public feed

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/RecommendationFeedbackCommander.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/RecommendationFeedbackCommander.scala
@@ -5,18 +5,20 @@ import com.keepit.common.db.Id
 import com.keepit.common.db.slick.Database
 import com.keepit.curator.model.UriRecommendationRepo
 import com.keepit.model.{ UriRecommendationFeedback, NormalizedURI, User }
-
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import scala.concurrent.Future
 
 class RecommendationFeedbackCommander @Inject() (
     db: Database,
     uriRecRepo: UriRecommendationRepo,
-    curatorAnalytics: CuratorAnalytics) {
+    publicFeedGenerationCommander: PublicFeedGenerationCommander) {
 
   def updateUriRecommendationFeedback(userId: Id[User], uriId: Id[NormalizedURI], feedback: UriRecommendationFeedback): Future[Boolean] = {
-    db.readWriteAsync { implicit session =>
+    val future = db.readWriteAsync { implicit session =>
       uriRecRepo.updateUriRecommendationFeedback(userId, uriId, feedback)
     }
+    future.onSuccess { case true => if (feedback.clicked.isDefined) publicFeedGenerationCommander.clicked(uriId) }
+    future
   }
 
 }
