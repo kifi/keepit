30e61466ec40371be6ec49d47ebf4b03b185a973
Aaron Hsu <aaron@kifi.com>
2014-12-22 15:19:17 -0800

    whoops

--- kifi-backend/modules/common/conf/evolutions/shoebox/278.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/278.sql
@@ -2,8 +2,9 @@
 
 # --- !Ups
 
-ALTER TABLE library_membership DROP COLUMN visibility;
 DROP INDEX library_membership_i_visible ON library_membership;
+ALTER TABLE library_membership DROP COLUMN visibility;
+
 CREATE INDEX library_membership_i_listed ON library_membership (listed);
 
 insert into evolutions (name, description) values('278.sql', 'drop visibility column from library membership');
