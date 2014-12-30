18c37c18b26cb69f5a5cbf263466cffa246d4742
eishay <eishay@gmail.com>
2014-12-30 11:20:06 -0800

    fixing initiation of the fetcher

--- kifi-backend/modules/scraper/app/com/keepit/scraper/fetcher/HttpFetcherModule.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/fetcher/HttpFetcherModule.scala
@@ -21,7 +21,6 @@ case class ProdHttpFetcherModule() extends HttpFetcherModule {
       userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
       connectionTimeout = scheduler.scrapePendingFrequency * 1000,
       soTimeOut = scheduler.scrapePendingFrequency * 1000,
-      trustBlindly = true,
       schedulingProperties,
       scraperConfig.httpConfig
     )
@@ -39,7 +38,6 @@ case class DevHttpFetcherModule() extends HttpFetcherModule {
       userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
       connectionTimeout = 5 * 1000,
       soTimeOut = 5 * 1000,
-      trustBlindly = true,
       schedulingProperties,
       scraperConfig.httpConfig
     )
