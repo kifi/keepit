8387793587198a88ab9e9142c193189c6becc027
2is10 <jared@kifi.com>
2015-01-08 16:44:51 -0800

    making related library cards look just like user profile library cards

--- kifi-backend/modules/shoebox/angular/src/libraries/smallLibraryCard.js
+++ kifi-backend/modules/shoebox/angular/src/libraries/smallLibraryCard.js
@@ -18,14 +18,6 @@ angular.module('kifi')
           $event.target.href = scope.library.libraryPath + '?o=' + scope.origin;
           scope.$emit('trackLibraryEvent', 'click', { action: scope.action });
         };
-
-        if (scope.library.followers.length > 0 && scope.library.otherFollowersCount > 0) {
-          scope.followersToShow = _.take(scope.library.followers, 3);
-        } else {
-          scope.followersToShow = scope.library.followers;
-        }
-
-        scope.otherFollowersCount = scope.library.numFollowers - scope.followersToShow.length;
       }
     };
   }
