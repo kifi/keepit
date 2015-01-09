2d1686f642289ce2ec4613299fe549b0255e6481
Yasuhiro Matsuda <yasuhiro.matsuda@gmail.com>
2015-01-09 11:51:18 -0800

    removing nonlinear scoring experiment

--- kifi-backend/modules/common/app/com/keepit/model/UserExperiment.scala
+++ kifi-backend/modules/common/app/com/keepit/model/UserExperiment.scala
@@ -66,7 +66,6 @@ object ExperimentType {
   val CORTEX_NEW_MODEL = ExperimentType("cortex_new_model")
   val CURATOR_DIVERSE_TOPIC_RECOS = ExperimentType("curator_diverse_topic_recos")
   val CURATOR_LIBRARY_RECOS = ExperimentType("curator_library_recos")
-  val CURATOR_NONLINEAR_SCORING = ExperimentType("curator_nonlinear_scoring")
   val NEW_PUBLIC_FEED = ExperimentType("new_public_feed")
   val PROFILES_BETA = ExperimentType("profiles_beta")
   val TWITTER_BETA = ExperimentType("twitter_beta")
@@ -75,7 +74,7 @@ object ExperimentType {
     CAN_MESSAGE_ALL_USERS :: DEMO :: EXTENSION_LOGGING :: SHOW_HIT_SCORES :: SHOW_DISCUSSIONS ::
     MOBILE_REDIRECT :: DELIGHTED_SURVEY_PERMANENT :: SPECIAL_CURATOR :: LIBRARIES :: SEND_DIGEST_EMAIL_ON_REFRESH ::
     GRAPH_BASED_PEOPLE_TO_INVITE :: NEW_KEEP_NOTIFICATIONS :: CORTEX_NEW_MODEL :: CURATOR_DIVERSE_TOPIC_RECOS ::
-    CURATOR_LIBRARY_RECOS :: CURATOR_NONLINEAR_SCORING :: NEW_PUBLIC_FEED :: PROFILES_BETA :: TWITTER_BETA :: Nil
+    CURATOR_LIBRARY_RECOS :: NEW_PUBLIC_FEED :: PROFILES_BETA :: TWITTER_BETA :: Nil
 
   private val _ALL_MAP: Map[String, ExperimentType] = _ALL.map(e => e.value -> e).toMap
 
