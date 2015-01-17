493b535ef42d4b2dec155098bb25970f4de2d1ee
2is10 <jared@kifi.com>
2015-01-16 16:39:48 -0800

    updating follower counts in user profile lib card after follow/unfollow

--- kifi-backend/modules/shoebox/angular/src/userProfile/userProfile.js
+++ kifi-backend/modules/shoebox/angular/src/userProfile/userProfile.js
@@ -329,6 +329,7 @@ angular.module('kifi')
       var following = lib.following;
       libraryService[following ? 'leaveLibrary' : 'joinLibrary'](lib.id).then(function () {
         lib.following = !following;
+        lib.numFollowers += following ? -1 : 1;
       })['catch'](function () {
         modalService.openGenericErrorModal();
       })['finally'](function () {
