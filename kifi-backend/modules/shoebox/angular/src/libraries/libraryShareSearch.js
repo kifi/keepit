8a59035fa86f32d5d1dd7a3a809e2c95cc546a2c
Yiping Liao <yiping@kifi.com>
2015-01-20 13:34:56 -0800

    Add profile-page-view tracking to user links in the library share invitation dropdown.

--- kifi-backend/modules/shoebox/angular/src/libraries/libraryShareSearch.js
+++ kifi-backend/modules/shoebox/angular/src/libraries/libraryShareSearch.js
@@ -11,6 +11,7 @@ angular.module('kifi')
       scope: {
         library: '=',
         manageLibInvite: '=',
+        currentPageOrigin: '@',
         close: '&'
       },
       templateUrl: 'libraries/libraryShareSearch.tpl.html',
