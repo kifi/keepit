d49b4ed7d6433d663770517f33a82a32052aa251
Andrew Conner <git@andrewconner.org>
2015-01-21 10:25:10 -0800

    UriImageCommander with new async UriImageStore

--- kifi-backend/modules/scraper/app/com/keepit/scraper/store/UriImageStore.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/store/UriImageStore.scala
@@ -1,4 +1,4 @@
-package com.keepit.common.store
+package com.keepit.scraper.store
 
 import java.io.InputStream
 
@@ -6,23 +6,24 @@ import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.transfer.TransferManager
 import com.amazonaws.services.s3.transfer.model.UploadResult
-import com.google.inject.{ Singleton, ImplementedBy, Inject }
+import com.google.inject.{ ImplementedBy, Inject, Singleton }
+import com.keepit.common.store.{ S3AsyncHelper, S3ImageConfig }
 import play.api.libs.Files.TemporaryFile
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 import scala.concurrent.Future
 
-@ImplementedBy(classOf[KeepImageStoreImpl])
-trait KeepImageStore {
+@ImplementedBy(classOf[UriImageStoreImpl])
+trait UriImageStore {
   def put(key: String, is: InputStream, contentLength: Int, mimeType: String): Future[UploadResult]
   def get(key: String): Future[TemporaryFile]
 }
 
 @Singleton
-class KeepImageStoreImpl @Inject() (
+class UriImageStoreImpl @Inject() (
     s3ImageConfig: S3ImageConfig,
     val s3Client: AmazonS3,
-    implicit val transferManager: TransferManager) extends KeepImageStore with S3AsyncHelper {
+    implicit val transferManager: TransferManager) extends UriImageStore with S3AsyncHelper {
 
   def put(key: String, is: InputStream, contentLength: Int, mimeType: String): Future[UploadResult] = {
     val om = new ObjectMetadata()
