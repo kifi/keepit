ce94e5cd4e56e4ffc3bd827ab90d8cf06ce5bc3a
2is10 <jared@kifi.com>
2015-01-23 13:41:09 -0800

    making env a constant

--- kifi-backend/modules/shoebox/angular/test/unit/tags/hashtagService.spec.js
+++ kifi-backend/modules/shoebox/angular/test/unit/tags/hashtagService.spec.js
@@ -2,7 +2,7 @@
 
 describe('kifi.hashtagService', function () {
 
-  var hashtagService, $httpBackend, $q, $rootScope, keepDecoratorService;
+  var hashtagService, routeService, $httpBackend, $q, $rootScope, keepDecoratorService;
 
   var libraryId = 'l6XWQOhPGRgg',
       keepId = '42b043aa-e389-45f2-ab37-b7809990ee7e';
@@ -24,8 +24,9 @@ describe('kifi.hashtagService', function () {
 
   beforeEach(module('kifi'));
 
-  beforeEach(inject(function (_hashtagService_, _$httpBackend_, _$q_, _$rootScope_, _keepDecoratorService_) {
+  beforeEach(inject(function (_hashtagService_, _routeService_, _$httpBackend_, _$q_, _$rootScope_, _keepDecoratorService_) {
     hashtagService = _hashtagService_;
+    routeService = _routeService_;
     $httpBackend = _$httpBackend_;
     $q = _$q_;
     $rootScope = _$rootScope_;
@@ -45,10 +46,9 @@ describe('kifi.hashtagService', function () {
         expect(data).toEqual([{'tag':'Scala','matches':[[0,5]]},{'tag':'Learn Scala','matches':[[6,5]]}]);
       });
 
-      $httpBackend.expectGET('http://server/ext/libraries/l6XWQOhPGRgg/keeps/42b043aa-e389-45f2-ab37-b7809990ee7e/tags/suggest?q=scala')
+      $httpBackend.expectGET(routeService.suggestTags('l6XWQOhPGRgg', '42b043aa-e389-45f2-ab37-b7809990ee7e', 'scala'))
         .respond(200, '[{"tag":"Scala","matches":[[0,5]]},{"tag":"Learn Scala","matches":[[6,5]]}]');
       $httpBackend.flush();
-
     });
 
     it('tagKeep adds tag to keep', function () {
@@ -59,7 +59,7 @@ describe('kifi.hashtagService', function () {
         expect(data).toEqual({'tag':'Scala'});
         expect(keep.hashtags).toEqual(['foo', 'bar', 'Scala']);
       });
-      $httpBackend.expectPOST('http://server/site/libraries/'+ libraryId +'/keeps/' + keepId + '/tags/Scala').respond(200, '{"tag":"Scala"}');
+      $httpBackend.expectPOST(routeService.tagKeep(libraryId, keepId, 'Scala')).respond(200, '{"tag":"Scala"}');
       $httpBackend.flush();
     });
 
@@ -73,7 +73,7 @@ describe('kifi.hashtagService', function () {
         expect(keep1.hashtags).toEqual(['bar', 'Scala']);
         expect(keep2.hashtags).toEqual(['baz', 'Scala']);
       });
-      $httpBackend.expectPOST('http://server/site/tags/Scala', { 'keepIds':
+      $httpBackend.expectPOST(routeService.tagKeeps('Scala'), { 'keepIds':
         [keep0.id, keep1.id, keep2.id] }).respond(200, '{}');
       $httpBackend.flush();
     });
@@ -86,7 +86,7 @@ describe('kifi.hashtagService', function () {
         expect(data).toBeUndefined();
         expect(keep.hashtags).toEqual(['bar']);
       });
-      $httpBackend.expectDELETE('http://server/site/libraries/'+ libraryId +'/keeps/' + keepId + '/tags/foo').respond(204);
+      $httpBackend.expectDELETE(routeService.untagKeep(libraryId, keepId, 'foo')).respond(204);
       $httpBackend.flush();
     });
 
