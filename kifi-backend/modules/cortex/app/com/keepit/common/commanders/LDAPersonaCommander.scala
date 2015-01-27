c24da2efcf3f4b5cc3ec3b16a16b9de62b8133b2
yingjieMiao <yingjie@42go.com>
2015-01-27 14:56:06 -0800

    tests

--- kifi-backend/modules/cortex/app/com/keepit/common/commanders/LDAPersonaCommander.scala
+++ kifi-backend/modules/cortex/app/com/keepit/common/commanders/LDAPersonaCommander.scala
@@ -22,16 +22,19 @@ class LDAPersonaCommanderImpl @Inject() (
     personaLDARepo: PersonaLDAFeatureRepo,
     userLDARepo: UserLDAStatsRepo) extends LDAPersonaCommander {
 
+  // overridable in test
+  protected def getLDADimension(implicit version: ModelVersion[DenseLDA]): Int = info.getLDADimension(version)
+
   // admin operation. not optimized for performance
   def generatePersonaFeature(topicIds: Seq[LDATopic])(implicit version: ModelVersion[DenseLDA]): (UserTopicMean, Int) = {
-    val dim = info.getLDADimension(version)
+    val dim = getLDADimension
     val vecs = topicIds.distinct.flatMap { tid =>
       val topicVecs = db.readOnlyReplica { implicit s => userLDARepo.getByTopic(version, tid) }
       topicVecs.flatMap { case model => model.userTopicMean.map { _.mean } }
     }
 
     val feature: Array[Float] = if (vecs.size > 0) {
-      val weights = Array.tabulate(dim) { i => 1f / vecs.size }
+      val weights = Array.tabulate(vecs.size) { i => 1f / vecs.size }
       weightedAverage(vecs.map { toDoubleArray(_) }, weights)
     } else {
       new Array[Float](dim)
