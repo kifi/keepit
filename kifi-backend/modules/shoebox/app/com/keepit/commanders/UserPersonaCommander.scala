950907fdf1b9d62108e3042361d66360e0f9857b
Aaron Hsu <aaron@kifi.com>
2015-01-28 13:09:16 -0800

    User Persona Commander changes

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/UserPersonaCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/UserPersonaCommander.scala
@@ -1,6 +1,6 @@
 package com.keepit.commanders
 
-import com.google.inject.{ Provider, Inject }
+import com.google.inject.{ Singleton, ImplementedBy, Inject }
 import com.keepit.common.db.Id
 import com.keepit.common.db.slick.Database
 import com.keepit.common.logging.Logging
@@ -8,64 +8,76 @@ import com.keepit.common.time.Clock
 import com.keepit.heimdal.HeimdalContext
 import com.keepit.model._
 
-class UserPersonaCommander @Inject() (
+@ImplementedBy(classOf[UserPersonaCommanderImpl])
+trait UserPersonaCommander {
+  def addPersonaForUser(userId: Id[User], persona: String)(implicit context: HeimdalContext): Option[(Persona, Library)]
+  def addPersonasForUser(userId: Id[User], personas: Set[String])(implicit context: HeimdalContext): Map[Persona, Library]
+  def removePersonaForUser(userId: Id[User], persona: String): Option[Persona]
+  def removePersonasForUser(userId: Id[User], personas: Set[String]): Set[Persona]
+}
+
+@Singleton
+class UserPersonaCommanderImpl @Inject() (
     db: Database,
     userPersonaRepo: UserPersonaRepo,
     personaRepo: PersonaRepo,
     libraryCommander: LibraryCommander,
-    clock: Clock) extends Logging {
+    clock: Clock) extends UserPersonaCommander with Logging {
 
-  def getPersonasForUser(userId: Id[User]): Seq[Persona] = {
-    db.readOnlyMaster { implicit s =>
-      val personaIds = userPersonaRepo.getUserPersonaIds(userId).toSet
-      personaRepo.getPersonasByIds(personaIds)
-    }
+  def addPersonaForUser(userId: Id[User], persona: String)(implicit context: HeimdalContext): Option[(Persona, Library)] = {
+    addPersonasForUser(userId, Set(persona)).headOption
   }
 
-  def addPersonasForUser(userId: Id[User], personas: Set[String])(implicit context: HeimdalContext): (Seq[Persona], Seq[Library]) = {
-    val (newPersonas, currentPersonaIds) = db.readOnlyMaster { implicit s =>
-      val newPersonas = personaRepo.getByNames(personas)
-      val currentPersonaIds = userPersonaRepo.getUserPersonaIds(userId)
-      (newPersonas, currentPersonaIds)
+  def addPersonasForUser(userId: Id[User], personas: Set[String])(implicit context: HeimdalContext): Map[Persona, Library] = {
+    val personasToPersist = db.readOnlyMaster { implicit s =>
+      val currentPersonas = userPersonaRepo.getPersonasForUser(userId).toSet
+      val personaNamesToAdd = personas diff currentPersonas.map(_.name)
+      personaRepo.getByNames(personaNamesToAdd)
     }
-    val personasToPersist = newPersonas.filterNot(p => currentPersonaIds.contains(p.id.get))
+
     db.readWrite { implicit s =>
-      personasToPersist.map { persona =>
-        userPersonaRepo.getByUserAndPersona(userId, persona.id.get) match {
-          case None =>
-            userPersonaRepo.save(UserPersona(userId = userId, personaId = persona.id.get, state = UserPersonaStates.ACTIVE))
-          case Some(up) =>
-            userPersonaRepo.save(up.copy(state = UserPersonaStates.ACTIVE))
-        }
+      personasToPersist.map {
+        case (_, persona) =>
+          userPersonaRepo.getByUserAndPersona(userId, persona.id.get) match {
+            case None =>
+              userPersonaRepo.save(UserPersona(userId = userId, personaId = persona.id.get, state = UserPersonaStates.ACTIVE))
+            case Some(up) =>
+              userPersonaRepo.save(up.copy(state = UserPersonaStates.ACTIVE))
+          }
       }
     }
 
     // create libraries based on added personas
-    val librariesAdded = personasToPersist.map { persona =>
-      val libraryAddReq = LibraryAddRequest(persona.name, LibraryVisibility.PUBLISHED, None, persona.name)
-      libraryCommander.addLibrary(libraryAddReq, userId)
+    personasToPersist.map {
+      case (personaName, persona) =>
+        val libraryAddReq = LibraryAddRequest(personaName, LibraryVisibility.PUBLISHED, None, personaName)
+        (persona, libraryCommander.addLibrary(libraryAddReq, userId))
     }.collect {
-      case Right(lib) => lib
-    }
-    (personasToPersist, librariesAdded)
+      case (persona, Right(lib)) => (persona, lib)
+    }.toMap
   }
 
-  def removePersonasForUser(userId: Id[User], personas: Set[String]): Seq[Persona] = {
-    val (newPersonas, currentPersonaIds) = db.readOnlyMaster { implicit s =>
-      val newPersonas = personaRepo.getByNames(personas)
-      val currentPersonaIds = userPersonaRepo.getUserPersonaIds(userId)
-      (newPersonas, currentPersonaIds)
+  def removePersonaForUser(userId: Id[User], persona: String): Option[Persona] = {
+    removePersonasForUser(userId, Set(persona)).headOption
+  }
+
+  def removePersonasForUser(userId: Id[User], personas: Set[String]): Set[Persona] = {
+    val personasToRemove = db.readOnlyMaster { implicit s =>
+      val currentPersonas = userPersonaRepo.getPersonasForUser(userId).toSet
+      val personaNamesToRemove = personas intersect currentPersonas.map(_.name)
+      personaRepo.getByNames(personaNamesToRemove)
     }
-    val personasToRemove = newPersonas.filter(p => currentPersonaIds.contains(p.id.get))
+
     db.readWrite { implicit s =>
-      personasToRemove.map { persona =>
-        userPersonaRepo.getByUserAndPersona(userId, persona.id.get) match {
-          case Some(up) if up.state == UserPersonaStates.ACTIVE =>
-            userPersonaRepo.save(up.copy(state = UserPersonaStates.INACTIVE))
-          case _ =>
-        }
+      personasToRemove.map {
+        case (_, persona) =>
+          userPersonaRepo.getByUserAndPersona(userId, persona.id.get) match {
+            case Some(up) if up.state == UserPersonaStates.ACTIVE =>
+              userPersonaRepo.save(up.copy(state = UserPersonaStates.INACTIVE))
+            case _ =>
+          }
       }
     }
-    personasToRemove
+    personasToRemove.values.toSet
   }
 }
