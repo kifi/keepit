ed35aa58674a762df4d2b5ee0a8eae6299b623c7
Aaron Hsu <aaron@kifi.com>
2015-02-02 17:15:31 -0800

    finish admin controller stuff

--- kifi-backend/modules/shoebox/app/com/keepit/admin/AdminPersonaController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/admin/AdminPersonaController.scala
@@ -1,19 +1,19 @@
-package com.keepit.admin
+package com.keepit.controllers.admin
 
 import com.google.inject.Inject
 import com.keepit.commanders.UserPersonaCommander
-import com.keepit.common.controller.{AdminUserActions, UserActionsHelper}
+import com.keepit.common.controller.{ AdminUserActions, UserActionsHelper }
 import com.keepit.common.db.slick.Database
 import com.keepit.model._
-import com.keepit.common.db.Id
+import com.keepit.common.db.{ State, Id }
 import play.api.libs.json.Json
 
 class AdminPersonaController @Inject() (
-   val userActionsHelper: UserActionsHelper,
-   db: Database,
-   personaRepo: PersonaRepo,
-   userPersonaRepo: UserPersonaRepo,
-   userPersonaCommander: UserPersonaCommander) extends AdminUserActions {
+    val userActionsHelper: UserActionsHelper,
+    db: Database,
+    personaRepo: PersonaRepo,
+    userPersonaRepo: UserPersonaRepo,
+    userPersonaCommander: UserPersonaCommander) extends AdminUserActions {
 
   def getAllPersonas() = AdminUserPage { implicit request =>
     val allPersonas = db.readOnlyReplica { implicit s =>
@@ -51,38 +51,33 @@ class AdminPersonaController @Inject() (
               activeIconPath = activeIconPath,
               state = PersonaStates.ACTIVE))
         }
-
       }
     }
+
     personaOpt match {
-      case None => BadRequest(Json.obj("error" -> "unable_to_create_persona"))
-      case Some(persona) => Ok(Json.obj("persona" -> Json.toJson(persona)))
+      case None =>
+        BadRequest(Json.obj("error" -> "unable_to_create_persona"))
+      case Some(persona) =>
+        NoContent
     }
   }
 
   def editPersona(id: Id[Persona]) = AdminUserPage { implicit request =>
     request.body.asJson.map { json =>
-      val currentPersonaName = PersonaName(name)
-      val displayNameOpt = (json \ "displayName").as[Option[String]]
-      val iconPathOpt = (json \ "iconPath").as[Option[String]]
-      val activeIconPathOpt = (json \ "activeIconPath").as[Option[String]]
+      val currentNameOpt = (json \ "name").asOpt[PersonaName]
+      val displayNameOpt = (json \ "displayName").asOpt[String]
+      val iconPathOpt = (json \ "iconPath").asOpt[String]
+      val activeIconPathOpt = (json \ "activeIconPath").asOpt[String]
+      val stateOpt = (json \ "state").asOpt[State[Persona]]
 
       db.readWrite { implicit s =>
-        personaRepo.getByName(currentPersonaName).map { currentPersona =>
-          val newDisplayName = displayNameOpt.getOrElse(currentPersona.displayName)
-          val newIconPath = iconPathOpt.getOrElse(currentPersona.iconPath)
-          val newActiveIconPath = activeIconPathOpt.getOrElse(currentPersona.activeIconPath)
-          personaRepo.save(currentPersona.copy(displayName = newDisplayName, iconPath = newIconPath, activeIconPath = newActiveIconPath))
-        }
-      }
-    }
-    NoContent
-  }
-
-  def deletePersona(id: Id[Persona]) = AdminUserPage { implicit request =>
-    db.readWrite { implicit s =>
-      personaRepo.getByName(PersonaName(name)).map { persona =>
-        personaRepo.save(persona.copy(state = PersonaStates.INACTIVE))
+        val currentPersona = personaRepo.get(id)
+        val newCurrentName = currentNameOpt.getOrElse(currentPersona.name)
+        val newDisplayName = displayNameOpt.getOrElse(currentPersona.displayName)
+        val newIconPath = iconPathOpt.getOrElse(currentPersona.iconPath)
+        val newActiveIconPath = activeIconPathOpt.getOrElse(currentPersona.activeIconPath)
+        val newState = stateOpt.getOrElse(currentPersona.state)
+        personaRepo.save(currentPersona.copy(name = newCurrentName, displayName = newDisplayName, iconPath = newIconPath, activeIconPath = newActiveIconPath, state = newState))
       }
     }
     NoContent
