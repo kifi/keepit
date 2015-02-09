d9121b792d8fc6dc9f625f20a91773ee01fa89ce
Yiping Liao <yiping@kifi.com>
2015-02-09 15:45:20 -0800

    Indicate whether the new recommendations are all duplicates.

--- kifi-backend/modules/shoebox/angular/src/recos/recoStateService.js
+++ kifi-backend/modules/shoebox/angular/src/recos/recoStateService.js
@@ -11,14 +11,19 @@ angular.module('kifi')
       recosList: savedRecos,
 
       populate: function (recos) {
+        var populated = false;
+
         for (var i = 0; i < recos.length; i++) {
           var reco = recos[i];
           var url = (reco.recoLib || reco.recoKeep).url;
           if (!recoUrls[url]) {
             recoUrls[url] = true;
             savedRecos.push(reco);
+            populated = true;
           }
         }
+
+        return populated;
       },
 
       empty: function () {
