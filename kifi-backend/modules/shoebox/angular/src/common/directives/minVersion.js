844e99c18f10cb2d534748a218686402efa4e78a
Yiping Liao <yiping@kifi.com>
2015-02-10 11:27:09 -0800

    Update logic for deciding how to show/hide CTAs to trigger extension installation and Kifi guide.

--- kifi-backend/modules/shoebox/angular/src/common/directives/minVersion.js
+++ kifi-backend/modules/shoebox/angular/src/common/directives/minVersion.js
@@ -3,12 +3,12 @@
 angular.module('kifi')
 
 .directive('kfMinVersion', [
-  'installService', '$timeout',
-  function (installService, $timeout) {
+  'installService', '$rootScope',
+  function (installService, $rootScope) {
     return {
       restrict: 'A',
       link: function (scope, element, attrs) {
-        function hideNode() {
+        scope.$on('$destroy', $rootScope.$on('kifiExt', function updateNodeDisplay() {
           if (element.length && element[0].style) {
             if (!installService.hasMinimumVersion(attrs.kfMinVersion, attrs.minCanary)) {
               element[0].style.display = 'none';
@@ -16,13 +16,7 @@ angular.module('kifi')
               element[0].style.display = 'initial';
             }
           }
-        }
-        // This is because we're waiting on the extension to let us know it's version number.
-        // A better solution is a single watch on $rootScope that watches the kifi version number,
-        // and if it changes, notify everyone who cares. This is not a simple change, though, so
-        // doing this for now.
-        hideNode();
-        _.map([1, 150, 350, 1000], function (delay) { $timeout (hideNode, delay); });
+        }));
       }
     };
   }
