6d98c5d2b4c59b531263a5f85bc5ae2cc0281354
yingjieMiao <yingjie@42go.com>
2015-02-10 15:02:19 -0800

    persona reco cheat sheet

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/persona/PersonaRecommendationPool.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/persona/PersonaRecommendationPool.scala
@@ -20,3 +20,11 @@ trait FixedSetPersonaRecoPool[T, R] extends PersonaRecommendationPool[R] {
     getRecoIdsByPersona(pid).map { recoId => generateRecoItemFromId(userId, recoId) }
   }
 }
+
+private[persona] object PersonaName2Id {
+  private implicit def toPersonaId(x: Int): Id[Persona] = Id[Persona](x)
+
+  val name2Id: Map[String, Id[Persona]] = Map("dev" -> 1, "stu" -> 2, "techie" -> 3, "entre" -> 4, "art" -> 5,
+    "foodie" -> 6, "science" -> 7, "fashion" -> 8, "health" -> 9, "investor" -> 10,
+    "travel" -> 11, "gamer" -> 12, "parent" -> 13, "animal" -> 14, "thinker" -> 15)
+}
