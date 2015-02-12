0ab1fbcf3521d69dcb6176264324fb6b99270f7c
Aaron Hsu <aaron@kifi.com>
2015-02-12 12:14:15 -0800

    correct usages

--- kifi-backend/modules/shoebox/angular/src/common/directives/keepWho/keepWhoLibsAndPics.js
+++ kifi-backend/modules/shoebox/angular/src/common/directives/keepWho/keepWhoLibsAndPics.js
@@ -36,7 +36,6 @@ angular.module('kifi')
         // on the client side when the user keeps or unkeeps.
         var deregisterKeepAddedListener = $rootScope.$on('keepAdded', function (e, libSlug, keeps, library) {
           var visibleLibraryIds = _.pluck(scope.visibleKeepLibraries, 'id');
-
           _.each(keeps, function (keep) {
             if (!scope.keep.id &&                      // No scope.keep.id if the keep is not on a library page.
                 scope.keep.url === keep.url &&
