a37eaa2e3fcca4fdada403764bfc9f9050b9041e
Andrew Conner <git@andrewconner.org>
2015-02-17 12:52:12 -0600

    Formatting

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobilePageController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobilePageController.scala
@@ -29,13 +29,13 @@ class MobilePageController @Inject() (
     urlOpt match {
       case Some(url) =>
         URI.parse(url) match {
-        case Success(_) =>
-          val info = pageCommander.getPageDetails(url, request.userId, request.experiments)
-          Ok(Json.toJson(info))
-        case Failure(e) =>
-          log.error(s"Error parsing url: $url", e)
-          BadRequest(Json.obj("error" -> s"Error parsing url: $url"))
-      }
+          case Success(_) =>
+            val info = pageCommander.getPageDetails(url, request.userId, request.experiments)
+            Ok(Json.toJson(info))
+          case Failure(e) =>
+            log.error(s"Error parsing url: $url", e)
+            BadRequest(Json.obj("error" -> s"Error parsing url: $url"))
+        }
       case None =>
         // Change this back to non-optional when clients are on latest Android (2015/2/17)
         log.warn(s"[getPageDetails] No URL provided for request $request")
