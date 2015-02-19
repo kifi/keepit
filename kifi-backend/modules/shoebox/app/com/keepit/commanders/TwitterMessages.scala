eecb561b8ae8f1e54aa644defb63af4ffbfc8237
eishay <eishay@gmail.com>
2015-02-18 22:53:34 -0800

    shorter tweets

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/TwitterMessages.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/TwitterMessages.scala
@@ -6,20 +6,24 @@ class TwitterMessages {
   /**
    * todo(eishay): before openning it out, should be heavily tested.
    * 140 is twitter max msg length
-   * 20 is the urls after shortning
+   * UrlLen is the urls after shortning
    * 20 is the message overhead " kept to via @kifi"...
-   * 140 - 2 * 20 - 20 = 79
    */
+  private val UrlLen = 25
+  private val Overhead = " kept to  via @kifi".length
+  val ContextSpace = 140 - 2 * UrlLen - Overhead
+
   def keepMessage(title: String, keepUrl: String, libName: String, libUrl: String): String = {
     val contentLength = title.length + libName.length
-    val totalLength = 20 * 2 + 20 + title.length + libName.length
-    if (20 * 2 + 20 + title.length + libName.length <= 140) {
+    val totalLength = UrlLen * 2 + Overhead + title.length + libName.length
+    if (UrlLen * 2 + Overhead + title.length + libName.length <= 140) {
       s"$title $keepUrl kept to $libName $libUrl via @kifi"
     } else {
-      val overtext = 79 - (2 * 3) - contentLength //the 3 stands for the "..."
-      val maxLibName = (libName.size - overtext / 3).min(20)
+      val overtext = contentLength - ContextSpace - (2 * 3) //the 3 stands for the "..."
+      val maxLibName = (overtext / 3).min(20)
+      println("maxLibName=" + maxLibName)
       val shortLibName = libName.abbreviate(maxLibName)
-      val shortTitle = if (title.size > 79 - shortLibName.size) title.abbreviate(79 - 3 - shortLibName.size) else title
+      val shortTitle = if (title.size > ContextSpace - shortLibName.size) title.abbreviate(ContextSpace - 3 - shortLibName.size) else title
       s"$shortTitle $keepUrl kept to $shortLibName $libUrl via @kifi"
     }
   }
