956ffc11af7b806f7bafae723ef3d401ad13a029
2is10 <jared@kifi.com>
2015-02-19 16:54:57 -0800

    minor adjustments to suggested searches

--- kifi-backend/modules/shoebox/angular/src/libraries/suggestedSearches.js
+++ kifi-backend/modules/shoebox/angular/src/libraries/suggestedSearches.js
@@ -3,28 +3,14 @@
 angular.module('kifi')
 
 .directive('kfSuggestedSearches', [
-  '$location', 'platformService',
-  function ($location, platformService) {
+  function () {
     return {
       restrict: 'A',
       replace: true,
       scope: {
-        parentLibrary: '&'
+        library: '='
       },
-      templateUrl: 'libraries/suggestedSearches.tpl.html',
-      link: function (scope/*, element, attrs*/) {
-        if (!platformService.isSupportedMobilePlatform()) {
-          var parentLibrary = scope.parentLibrary();
-          scope.termsWithActions = _.map(parentLibrary.suggestedSearches, function (term) {
-            return {
-              name: term,
-              action: function () {
-                $location.url(parentLibrary.url + '/find?q=' + term);
-              }
-            };
-          });
-        }
-      }
+      templateUrl: 'libraries/suggestedSearches.tpl.html'
     };
   }
 ]);
