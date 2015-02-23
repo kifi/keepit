c80f9911e56e8bae862ce0a7092512342ce02aa6
Andrew Conner <git@andrewconner.org>
2015-02-23 12:54:29 -0800

    Dev token

--- kifi-backend/modules/shoebox/app/com/keepit/shoebox/TwilioCredentialsModule.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/shoebox/TwilioCredentialsModule.scala
@@ -22,8 +22,7 @@ case class DevTwilioCredentialsModule() extends TwilioCredentialsModule {
   @Singleton
   @Provides
   def twilioCredentials(): TwilioCredentials = {
-    TwilioCredentials("AC5f673e4da699164a4f0f50cbfed17cd0", "d3adeac93f03603d987637470ae6eed4", PhoneNumber("+17728795434"))
-    //TwilioCredentials("AC9943c9cdaab3efb186c04ac409b16131", "d2dc5b1a86126d37b8023124e41122ec", PhoneNumber("+15005550006"))
+    TwilioCredentials("AC9943c9cdaab3efb186c04ac409b16131", "d2dc5b1a86126d37b8023124e41122ec", PhoneNumber("+15005550006"))
   }
 }
 
