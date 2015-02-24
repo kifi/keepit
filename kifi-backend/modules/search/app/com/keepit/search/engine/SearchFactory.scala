0f8ab9383c2614ed0fbffa8f2ce018b4ae0943e9
Léo Grimaldi <leo@42go.com>
2015-02-24 15:45:16 -0800

    Fix typo

--- kifi-backend/modules/search/app/com/keepit/search/engine/SearchFactory.scala
+++ kifi-backend/modules/search/app/com/keepit/search/engine/SearchFactory.scala
@@ -253,7 +253,7 @@ class SearchFactory @Inject() (
   }
 
   private def addLibraryFilter(engBuilder: QueryEngineBuilder, libId: Long) = { engBuilder.addFilterQuery(new TermQuery(new Term(KeepFields.libraryField, libId.toString))) }
-  private def addUserFilter(engBuilder: QueryEngineBuilder, userId: Id[User]) = { engBuilder.addFilterQuery(new TermQuery(new Term(KeepFields.userField, userId.id.toString)) }
+  private def addUserFilter(engBuilder: QueryEngineBuilder, userId: Id[User]) = { engBuilder.addFilterQuery(new TermQuery(new Term(KeepFields.userField, userId.id.toString))) }
 
   def getLibrarySearches(
     shards: Set[Shard[NormalizedURI]],
