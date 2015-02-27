dc79b92b564ec6031f650a42a690f8dc7b320ff5
Léo Grimaldi <leo@42go.com>
2015-02-26 16:03:52 -0800

    Introduce EmbedlyArticle

--- kifi-backend/modules/common/app/com/keepit/rover/article/Article.scala
+++ kifi-backend/modules/common/app/com/keepit/rover/article/Article.scala
@@ -1,6 +1,7 @@
 package com.keepit.rover.article
 
 import com.keepit.model.PageAuthor
+import com.keepit.scraper.embedly.{ EmbedlyEntity, EmbedlyKeyword, EmbedlyImage }
 import org.joda.time.DateTime
 import play.api.libs.json._
 import com.keepit.common.reflection.CompanionTypeSystem
@@ -54,5 +55,40 @@ object ArticleKind {
   val byTypeCode: Map[String, ArticleKind[_ <: Article]] = {
     require(all.size == all.map(_.typeCode).size, "Duplicate Article type codes.")
     all.map { articleKind => articleKind.typeCode -> articleKind }.toMap
+  }
+}
+
+case class UnknownArticleVersionException[A <: Article](kind: ArticleKind[A], currentVersion: Int, unknownVersion: Int)
+  extends Throwable(s"[$kind] Unknown version: $unknownVersion (Latest version: $currentVersion)")
+
+case class EmbedlyArticle(createdAt: DateTime, json: JsValue) extends Article {
+  type A = EmbedlyArticle
+  def kind = EmbedlyArticle
+
+  def url = (json \ "original_url").as[String]
+  def destinationUrl = (json \ "url").as[String]
+
+  def title = (json \ "title").asOpt[String]
+  def description = (json \ "description").asOpt[String]
+  def rawContent = (json \ "content").asOpt[String]
+  def content = rawContent // todo(Léo): needs post-processing
+
+  def authors = (json \ "authors").asOpt[Seq[PageAuthor]] getOrElse Seq.empty[PageAuthor]
+  def publishedAt = (json \ "published").asOpt[DateTime]
+  def isSafe = (json \ "safe").asOpt[Boolean]
+  def language = (json \ "language").asOpt[String]
+  def images = (json \ "images").as[Seq[EmbedlyImage]]
+  def embedlyKeywords = (json \ "keywords").as[Seq[EmbedlyKeyword]]
+  def keywords = embedlyKeywords.map(_.name)
+  def entities = (json \ "entities").as[Seq[EmbedlyEntity]]
+  def faviconUrl = (json \ "favicon_url").asOpt[String]
+}
+
+case object EmbedlyArticle extends ArticleKind[EmbedlyArticle] {
+  val typeCode = "embedly"
+  val version = 1
+  def formatByVersion(thatVersion: Int) = thatVersion match {
+    case `version` => Json.format[EmbedlyArticle]
+    case _ => throw new UnknownArticleVersionException(this, version, thatVersion)
   }
 }
