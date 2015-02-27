b21c2378696935ccc69c06d66a366bf4f42deaf1
Stephen Kemmerling <stephen@42go.com>
2015-02-26 16:11:26 -0800

    Fake twitter waitlist length and position counts

---
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/TwitterWaitlistController.scala
@@ -0,0 +1,30 @@
+package com.keepit.controllers.website
+
+import com.google.inject.Inject
+import com.keepit.commanders.TwitterWaitlistCommander
+import com.keepit.common.controller.{ UserActionsHelper, UserActions, ShoeboxServiceController }
+import com.keepit.common.time.Clock
+import com.keepit.model._
+import play.api.libs.json.Json
+
+class TwitterWaitlistController @Inject() (
+    commander: TwitterWaitlistCommander,
+    val userActionsHelper: UserActionsHelper) extends UserActions with ShoeboxServiceController {
+
+  def getFakeWaitlistPoition(handle: String) = UserAction { request =>
+    commander.getFakeWaitlistPosition(request.userId, handle).map { pos =>
+      Ok(Json.obj(
+        "pos" -> pos
+      ))
+    } getOrElse {
+      NotFound
+    }
+  }
+
+  def getFakeWaitlistLength(handle: String) = UserAction { request =>
+    Ok(Json.obj(
+      "length" -> commander.getFakeWaitlistLength()
+    ))
+  }
+
+}
