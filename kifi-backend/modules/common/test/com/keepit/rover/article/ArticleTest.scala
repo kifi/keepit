782f275c93ef552be8c86f1257ce801aedb727f6
Léo Grimaldi <leo@42go.com>
2015-03-03 14:34:18 -0800

    Remove unused imports

--- kifi-backend/modules/common/test/com/keepit/rover/article/ArticleTest.scala
+++ kifi-backend/modules/common/test/com/keepit/rover/article/ArticleTest.scala
@@ -1,8 +1,6 @@
 package com.keepit.rover.article
 
 import org.specs2.mutable.Specification
-import com.keepit.graph.manager.GraphUpdateKind
-import com.keepit.graph.model.{ EdgeKind, EdgeDataReader, VertexKind, VertexDataReader }
 
 class ArticleTest extends Specification {
   "Article" should {
