95843862b4f6dbe36efdc9a22dec140ee0f5a62c
Léo Grimaldi <leo@42go.com>
2015-03-04 17:09:03 -0800

    Clean up ElizaStoreModule and ElizaModule

--- kifi-backend/modules/eliza/app/com/keepit/common/store/ElizaStoreModule.scala
+++ kifi-backend/modules/eliza/app/com/keepit/common/store/ElizaStoreModule.scala
@@ -1,66 +1,13 @@
 package com.keepit.common.store
 
-import com.google.inject.{ Provider, Singleton, Provides }
-import com.amazonaws.services.s3.{ AmazonS3Client, AmazonS3 }
-import com.keepit.common.logging.AccessLog
-import play.api.Play.current
-import com.amazonaws.auth.BasicAWSCredentials
-import com.keepit.search.ArticleStore
-import com.keepit.search.S3ArticleStoreImpl
-import com.keepit.search.InMemoryArticleStoreImpl
+import com.keepit.common.logging.Logging
 
-case class ElizaProdStoreModule() extends StoreModule {
-  def configure() {
+trait ElizaStoreModule extends StoreModule with Logging
 
-  }
-
-  @Singleton
-  @Provides
-  def amazonS3Client(basicAWSCredentials: BasicAWSCredentials): AmazonS3 = {
-    new AmazonS3Client(basicAWSCredentials)
-  }
-
-  @Singleton
-  @Provides
-  def articleStore(amazonS3Client: AmazonS3, accessLog: AccessLog): ArticleStore = {
-    val bucketName = S3Bucket(current.configuration.getString("amazon.s3.article.bucket").get)
-    new S3ArticleStoreImpl(bucketName, amazonS3Client, accessLog)
-  }
-
-  @Singleton
-  @Provides
-  def kifiInstallationStore(amazonS3Client: AmazonS3, accessLog: AccessLog): KifiInstallationStore = {
-    val bucketName = S3Bucket(current.configuration.getString("amazon.s3.install.bucket").get)
-    new S3KifiInstallationStoreImpl(bucketName, amazonS3Client, accessLog)
-  }
+case class ElizaProdStoreModule() extends ProdStoreModule with ElizaStoreModule {
+  def configure() {}
 }
 
-case class ElizaDevStoreModule() extends StoreModule {
-  def configure() {
-
-  }
-  protected def whenConfigured[T](parameter: String)(expression: => T): Option[T] =
-    play.api.Play.maybeApplication.map(_.configuration.getString(parameter).map(_ => expression)).flatten
-
-  @Singleton
-  @Provides
-  def amazonS3Client(basicAWSCredentials: BasicAWSCredentials): AmazonS3 = {
-    new AmazonS3Client(basicAWSCredentials)
-  }
-
-  @Singleton
-  @Provides
-  def articleStore(amazonS3ClientProvider: Provider[AmazonS3], accessLog: AccessLog): ArticleStore =
-    whenConfigured("amazon.s3.article.bucket") {
-      val bucketName = S3Bucket(current.configuration.getString("amazon.s3.install.bucket").get)
-      new S3ArticleStoreImpl(bucketName, amazonS3ClientProvider.get, accessLog)
-    }.getOrElse(new InMemoryArticleStoreImpl())
-
-  @Singleton
-  @Provides
-  def kifInstallationStore(amazonS3ClientProvider: Provider[AmazonS3], accessLog: AccessLog): KifiInstallationStore =
-    whenConfigured("amazon.s3.install.bucket") {
-      val bucketName = S3Bucket(current.configuration.getString("amazon.s3.install.bucket").get)
-      new S3KifiInstallationStoreImpl(bucketName, amazonS3ClientProvider.get, accessLog)
-    }.getOrElse(new InMemoryKifiInstallationStoreImpl())
+case class ElizaDevStoreModule() extends DevStoreModule(ElizaProdStoreModule()) with ElizaStoreModule {
+  def configure() {}
 }
--- kifi-backend/modules/eliza/app/com/keepit/eliza/ElizaProdModule.scala
+++ kifi-backend/modules/eliza/app/com/keepit/eliza/ElizaProdModule.scala
@@ -3,9 +3,7 @@ package com.keepit.eliza
 import com.keepit.abook.ProdABookServiceClientModule
 import com.keepit.common.cache.{ EhCacheCacheModule, ElizaCacheModule, MemcachedCacheModule }
 import com.keepit.common.controller.ProdRemoteUserActionsHelperModule
-import com.keepit.common.service.ServiceType
 import com.keepit.common.store.ElizaProdStoreModule
-import com.keepit.common.zookeeper.ProdDiscoveryModule
 import com.keepit.heimdal.ProdHeimdalServiceClientModule
 import com.keepit.inject.CommonProdModule
 import com.keepit.realtime.ElizaUrbanAirshipModule
@@ -13,12 +11,12 @@ import com.keepit.scraper.ProdScraperServiceClientModule
 import com.keepit.search.ProdSearchServiceClientModule
 import com.keepit.shoebox.ProdShoeboxServiceClientModule
 
-case class ElizaProdModule() extends ElizaModule(
-  userActionsModule = ProdRemoteUserActionsHelperModule(),
-  cacheModule = ElizaCacheModule(MemcachedCacheModule(), EhCacheCacheModule()),
-  urbanAirshipModule = ElizaUrbanAirshipModule(),
-  storeModule = ElizaProdStoreModule()
-) with CommonProdModule {
+case class ElizaProdModule() extends ElizaModule with CommonProdModule {
+
+  val userActionsModule = ProdRemoteUserActionsHelperModule()
+  val cacheModule = ElizaCacheModule(MemcachedCacheModule(), EhCacheCacheModule())
+  val urbanAirshipModule = ElizaUrbanAirshipModule()
+  val storeModule = ElizaProdStoreModule()
 
   // Service clients
   val searchServiceClientModule = ProdSearchServiceClientModule()
