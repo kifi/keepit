b2e81643e08d813b7ab8b476ca273f9a02bac360
2is10 <jared@kifi.com>
2015-03-05 12:46:09 -0800

    sending search friend status back with friend status

--- kifi-backend/modules/shoebox/app/com/keepit/model/BasicUserWithFriendStatus.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/model/BasicUserWithFriendStatus.scala
@@ -16,25 +16,28 @@ case class BasicUserWithFriendStatus(
   username: Username,
   isFriend: Option[Boolean],
   friendRequestSentAt: Option[DateTime],
-  friendRequestReceivedAt: Option[DateTime]) extends BasicUserFields
+  friendRequestReceivedAt: Option[DateTime],
+  unsearched: Option[Boolean] // whether viewer has opted out of seeing this person's keeps in their search results
+  ) extends BasicUserFields
 
 object BasicUserWithFriendStatus {
   implicit val format = (
     BasicUserFields.format and
     (__ \ 'isFriend).formatNullable[Boolean] and
     (__ \ 'friendRequestSentAt).formatNullable[DateTime] and
-    (__ \ 'friendRequestReceivedAt).formatNullable[DateTime]
+    (__ \ 'friendRequestReceivedAt).formatNullable[DateTime] and
+    (__ \ 'unsearched).formatNullable[Boolean]
   )(BasicUserWithFriendStatus.apply, unlift(BasicUserWithFriendStatus.unapply))
 
   def fromWithoutFriendStatus(u: User): BasicUserWithFriendStatus = fromWithoutFriendStatus(BasicUser.fromUser(u))
   def fromWithoutFriendStatus(u: BasicUser): BasicUserWithFriendStatus = from(u, None, None, None)
-  def from(u: User, isFriend: Boolean): BasicUserWithFriendStatus = from(BasicUser.fromUser(u), isFriend)
-  def from(u: BasicUser, isFriend: Boolean): BasicUserWithFriendStatus = from(u, Some(isFriend))
+  def from(u: User, isFriend: Boolean, isSearchFriend: Boolean): BasicUserWithFriendStatus = from(BasicUser.fromUser(u), isFriend, isSearchFriend)
+  def from(u: BasicUser, isFriend: Boolean, isSearchFriend: Boolean): BasicUserWithFriendStatus = from(u, Some(isFriend), unsearched = if (isFriend && !isSearchFriend) Some(true) else None)
   def fromWithRequestSentAt(u: User, at: DateTime): BasicUserWithFriendStatus = fromWithRequestSentAt(BasicUser.fromUser(u), at)
   def fromWithRequestSentAt(u: BasicUser, at: DateTime): BasicUserWithFriendStatus = from(u, Some(false), friendRequestSentAt = Some(at))
   def fromWithRequestReceivedAt(u: User, at: DateTime): BasicUserWithFriendStatus = fromWithRequestReceivedAt(BasicUser.fromUser(u), at)
   def fromWithRequestReceivedAt(u: BasicUser, at: DateTime): BasicUserWithFriendStatus = from(u, Some(false), friendRequestReceivedAt = Some(at))
-  private def from(u: BasicUser, isFriend: Option[Boolean], friendRequestSentAt: Option[DateTime] = None, friendRequestReceivedAt: Option[DateTime] = None): BasicUserWithFriendStatus = {
+  private def from(u: BasicUser, isFriend: Option[Boolean], friendRequestSentAt: Option[DateTime] = None, friendRequestReceivedAt: Option[DateTime] = None, unsearched: Option[Boolean] = None): BasicUserWithFriendStatus = {
     BasicUserWithFriendStatus(
       externalId = u.externalId,
       firstName = u.firstName,
@@ -43,6 +46,7 @@ object BasicUserWithFriendStatus {
       username = u.username,
       isFriend = isFriend,
       friendRequestSentAt = friendRequestSentAt,
-      friendRequestReceivedAt = friendRequestReceivedAt)
+      friendRequestReceivedAt = friendRequestReceivedAt,
+      unsearched = unsearched)
   }
 }
