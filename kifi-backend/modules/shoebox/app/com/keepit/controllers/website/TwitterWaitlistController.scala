cfdc4380aa96df6ccb5ca092abfde82bed9a5414
Andrew Conner <git@andrewconner.org>
2015-03-10 15:36:05 -0700

    Waitlist endpoint

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/TwitterWaitlistController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/TwitterWaitlistController.scala
@@ -3,22 +3,39 @@ package com.keepit.controllers.website
 import com.google.inject.Inject
 import com.keepit.commanders.TwitterWaitlistCommander
 import com.keepit.common.controller.{ UserActionsHelper, UserActions, ShoeboxServiceController }
+import com.keepit.common.db.slick.Database
 import com.keepit.common.time.Clock
 import com.keepit.model._
+import com.keepit.social.SocialNetworks
 import play.api.libs.json.Json
+import scala.util.Try
 
 class TwitterWaitlistController @Inject() (
     commander: TwitterWaitlistCommander,
+    socialRepo: SocialUserInfoRepo,
+    userEmailAddressRepo: UserEmailAddressRepo,
+    db: Database,
     val userActionsHelper: UserActionsHelper) extends UserActions with ShoeboxServiceController {
 
   //DO NOT USE THE WORD *FAKE* IN THE ROUTE FOR THIS!!!
-  def getFakeWaitlistPoition(handle: String) = UserAction { request =>
-    commander.getFakeWaitlistPosition(request.userId, handle).map { pos =>
-      Ok(Json.obj(
-        "pos" -> pos
-      ))
-    } getOrElse {
-      NotFound
+  def getFakeWaitlistPosition() = UserAction { request =>
+    val (handleOpt, emailOpt) = db.readOnlyReplica { implicit session =>
+      val twOpt = socialRepo.getByUser(request.userId).find(_.networkType == SocialNetworks.TWITTER).flatMap {
+        _.getProfileUrl.map(url => url.substring(url.lastIndexOf('/') + 1))
+      }
+      val emailOpt = Try(userEmailAddressRepo.getByUser(request.userId)).toOption
+      (twOpt, emailOpt)
+    }
+    (handleOpt, emailOpt) match {
+      case (Some(handle), Some(email)) =>
+        commander.getFakeWaitlistPosition(request.userId, handle).map { pos =>
+          Ok(Json.obj(
+            "email" -> email,
+            "pos" -> pos
+          ))
+        } getOrElse NotFound
+      case _ =>
+        Ok
     }
   }
 
