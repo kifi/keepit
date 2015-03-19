ab3f861182e830d2726ca07aae6389a42e01118c
Léo Grimaldi <leo@42go.com>
2015-03-19 14:32:51 -0700

    Add RoverHttpProxyRepo

--- kifi-backend/modules/rover/test/com/keepit/rover/model/RoverRepoTest.scala
+++ kifi-backend/modules/rover/test/com/keepit/rover/model/RoverRepoTest.scala
@@ -6,14 +6,23 @@ import org.specs2.mutable.Specification
 import com.keepit.common.db.Id
 import play.api.test.Helpers._
 
-class ArticleInfoTest extends Specification with RoverApplicationInjector {
-  "ArticleInfoRepo" should {
-    "save and retrieve ArticleInfo" in {
+class RoverRepoTest extends Specification with RoverApplicationInjector {
+  "Rover" should {
+    "save and retrieve models" in {
       running(new RoverApplication()) {
+
+        // ArticleRepo
         val articleInfoRepo = inject[ArticleInfoRepo]
         db.readWrite { implicit session =>
           val saved = articleInfoRepo.save(ArticleInfo(uriId = Id(1), url = "http://www.lemonde.fr", kind = DefaultArticle.typeCode))
           articleInfoRepo.get(saved.id.get).uriId === Id(1)
+        }
+
+        // HttpProxyRepo
+        val httpProxyRepo = inject[RoverHttpProxyRepo]
+        db.readWrite { implicit session =>
+          val saved = httpProxyRepo.save(RoverHttpProxy(alias = "marvin", host = "96.31.86.149", port = 3128, scheme = "http", username = None, password = None))
+          httpProxyRepo.get(saved.id.get).alias === "marvin"
         }
       }
     }
