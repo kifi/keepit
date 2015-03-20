d551c2ed0bcf8407e1dbe52b335c84d4a533e148
Léo Grimaldi <leo@42go.com>
2015-03-20 16:05:51 -0700

    Remove SystemValueCache

--- kifi-backend/modules/abook/app/com/keepit/common/cache/ABookCacheModule.scala
+++ kifi-backend/modules/abook/app/com/keepit/common/cache/ABookCacheModule.scala
@@ -125,10 +125,6 @@ case class ABookCacheModule(cachePluginModules: CachePluginModule*) extends Cach
   def userValueCache(stats: CacheStatistics, accessLog: AccessLog, outerRepo: FortyTwoCachePlugin) =
     new UserValueCache(stats, accessLog, (outerRepo, 7 days))
 
-  @Provides @Singleton
-  def systemValueCache(stats: CacheStatistics, accessLog: AccessLog, outerRepo: FortyTwoCachePlugin) =
-    new SystemValueCache(stats, accessLog, (outerRepo, 7 days))
-
   @Singleton
   @Provides
   def userSegmentCache(stats: CacheStatistics, accessLog: AccessLog, innerRepo: InMemoryCachePlugin, outerRepo: FortyTwoCachePlugin) =
