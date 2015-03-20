0eac35045fad749805d71451b29cae43dd4d4128
Aaron Hsu <aaron@kifi.com>
2015-03-20 14:33:54 -0700

    never mind just removing those indices

--- kifi-backend/modules/common/conf/evolutions/shoebox/101.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/101.sql
@@ -6,8 +6,5 @@ CREATE INDEX device_i_user_id ON device (user_id);
 
 insert into evolutions (name, description) values('101.sql', 'fixing devices table indices');
 
-DROP INDEX device_u_token ON device;
-DROP INDEX device_u_user_id ON device;
-
 # --- !Downs
 
