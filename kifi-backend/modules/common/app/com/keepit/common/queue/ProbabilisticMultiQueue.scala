3d83c8feb81e9fab1ef4015210b000387ab0e865
Léo Grimaldi <leo@42go.com>
2015-03-20 12:34:14 -0700

    Use foldLeftUntil in ProbabilisticMultiQueue

--- kifi-backend/modules/common/app/com/keepit/common/queue/ProbabilisticMultiQueue.scala
+++ kifi-backend/modules/common/app/com/keepit/common/queue/ProbabilisticMultiQueue.scala
@@ -8,6 +8,7 @@ import com.kifi.franz.{ SQSMessage, SQSQueue }
 import scala.concurrent.{ Future, ExecutionContext }
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Random
+import com.keepit.common.core._
 
 class ProbabilisticMultiQueue[T](weights: Map[SQSQueue[T], Double]) {
   private val sortedQueues = weights.keySet.toSeq.sortBy(-weights(_))
@@ -19,11 +20,11 @@ class ProbabilisticMultiQueue[T](weights: Map[SQSQueue[T], Double]) {
   def nextBatchWithLock(n: Int, lockTimeout: FiniteDuration)(implicit ec: ExecutionContext): Future[Seq[SQSMessage[T]]] = {
     val offset = offsetDensity.sample(Random.nextDouble()).get
     // not using foldLeftWhile on purpose: it doesn't return the last accumulator, which may lock tasks without returning them
-    FutureHelpers.foldLeft(sortedQueues.cycle(offset))(Seq.empty[SQSMessage[T]]) {
+    FutureHelpers.foldLeftUntil(sortedQueues.cycle(offset))(Seq.empty[SQSMessage[T]]) {
       case (tasks, nextQueue) =>
         val numberOfMissingTasks = n - tasks.length
-        if (numberOfMissingTasks <= 0) Future.successful(tasks)
-        else nextQueue.nextBatchWithLock(numberOfMissingTasks, lockTimeout).map { moreTasks => tasks ++ moreTasks }
+        if (numberOfMissingTasks <= 0) Future.successful((tasks, true))
+        else nextQueue.nextBatchWithLock(numberOfMissingTasks, lockTimeout).imap { moreTasks => (tasks ++ moreTasks, false) }
     }
   }
 }
