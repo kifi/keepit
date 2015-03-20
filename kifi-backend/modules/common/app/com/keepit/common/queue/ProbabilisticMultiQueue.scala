2a1bab20ee649d5b7baf63aad8080799173814db
Léo Grimaldi <leo@42go.com>
2015-03-20 08:25:28 -0700

    Cycling through all queues in ProbabilisticMultiQueueReader

--- kifi-backend/modules/common/app/com/keepit/common/queue/ProbabilisticMultiQueue.scala
+++ kifi-backend/modules/common/app/com/keepit/common/queue/ProbabilisticMultiQueue.scala
@@ -2,13 +2,14 @@ package com.keepit.common.queue
 
 import com.keepit.common.concurrent.FutureHelpers
 import com.keepit.common.math.ProbabilityDensityBuilder
+import com.keepit.common.CollectionHelpers._
 import com.kifi.franz.{ SQSMessage, SQSQueue }
 
 import scala.concurrent.{ Future, ExecutionContext }
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Random
 
-class SQSPriorityQueue[T](weights: Map[SQSQueue[T], Double]) {
+class ProbabilisticMultiQueue[T](weights: Map[SQSQueue[T], Double]) {
   private val sortedQueues = weights.keySet.toSeq.sortBy(-weights(_))
   private val offsetDensity = {
     val builder = new ProbabilityDensityBuilder[Int]()
@@ -18,7 +19,7 @@ class SQSPriorityQueue[T](weights: Map[SQSQueue[T], Double]) {
   def nextBatchWithLock(n: Int, lockTimeout: FiniteDuration)(implicit ec: ExecutionContext): Future[Seq[SQSMessage[T]]] = {
     val offset = offsetDensity.sample(Random.nextDouble()).get
     // not using foldLeftWhile on purpose: it doesn't return the last accumulator, which may lock tasks without returning them
-    FutureHelpers.foldLeft(sortedQueues.drop(offset))(Seq.empty[SQSMessage[T]]) {
+    FutureHelpers.foldLeft(sortedQueues.cycle(offset))(Seq.empty[SQSMessage[T]]) {
       case (tasks, nextQueue) =>
         val numberOfMissingTasks = n - tasks.length
         if (numberOfMissingTasks <= 0) Future.successful(tasks)
