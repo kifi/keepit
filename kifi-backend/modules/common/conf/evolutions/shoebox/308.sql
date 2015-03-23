7953d23b2fba40f3f46464b7c6e4216e3bb04aee
yingjieMiao <yingjie@42go.com>
2015-03-23 09:18:18 -0700

    evolution number

--- kifi-backend/modules/common/conf/evolutions/shoebox/308.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/308.sql
@@ -5,6 +5,6 @@
 alter table twitter_sync_state
     add index twitter_sync_state_i_library (library_id);
 
-insert into evolutions (name, description) values('305.sql', 'add library index to twitter_sync_table');
+insert into evolutions (name, description) values('308.sql', 'add library index to twitter_sync_table');
 
 # --- !Downs
