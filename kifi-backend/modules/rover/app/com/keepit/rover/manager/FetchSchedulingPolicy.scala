61bd0224a22b8024a85d24b163bce6d09f25652f
Léo Grimaldi <leo@42go.com>
2015-03-24 15:32:46 -0700

    Introduce FetchSchedulingPolicy

---
+++ kifi-backend/modules/rover/app/com/keepit/rover/manager/FetchSchedulingPolicy.scala
@@ -0,0 +1,33 @@
+package com.keepit.rover.manager
+
+import com.keepit.rover.article.{ ArticleKind, Article }
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
+
+case class FetchSchedulingPolicy(
+  maxRandomDelay: Duration,
+  minInterval: Duration,
+  maxInterval: Duration,
+  intervalIncrement: Duration,
+  intervalDecrement: Duration,
+  maxBackoff: Duration,
+  backoffIncrement: Duration)
+
+object FetchSchedulingPolicy {
+  def apply[A <: Article](implicit kind: ArticleKind[A]): FetchSchedulingPolicy = {
+    kind match {
+      case _ => defaultSchedulingPolicy
+    }
+  }
+
+  private val defaultSchedulingPolicy = FetchSchedulingPolicy(
+    maxRandomDelay = 6 hours,
+    minInterval = 7 days,
+    maxInterval = 120 days,
+    intervalIncrement = 2 days,
+    intervalDecrement = 2 days,
+    maxBackoff = 40 days,
+    backoffIncrement = 6 hours
+  )
+}
