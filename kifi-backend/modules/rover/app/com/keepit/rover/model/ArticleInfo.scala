654c089273790508ffcf90045efe67327bd6e791
Léo Grimaldi <leo@42go.com>
2015-03-23 18:16:09 -0700

    Add interning methods and sequencing to ArticleInfoRepo

--- kifi-backend/modules/rover/app/com/keepit/rover/model/ArticleInfo.scala
+++ kifi-backend/modules/rover/app/com/keepit/rover/model/ArticleInfo.scala
@@ -25,7 +25,16 @@ case class ArticleInfo(
     fetchInterval: Float = 24.0f) extends ModelWithState[ArticleInfo] with ModelWithSeqNumber[ArticleInfo] with ArticleKeyHolder {
   def withId(id: Id[ArticleInfo]) = this.copy(id = Some(id))
   def withUpdateTime(now: DateTime) = this.copy(updatedAt = now)
+  def isActive = (state == ArticleInfoStates.ACTIVE)
   def getFetchRequest[A <: Article](implicit kind: ArticleKind[A]) = ArticleFetchRequest(kind, url, lastFetchedAt, getLatestKey)
+  def clean: ArticleInfo = copy(
+    bestVersion = None,
+    latestVersion = None,
+    lastQueuedAt = None,
+    lastFetchedAt = None,
+    nextFetchAt = None,
+    fetchInterval = 24.0f
+  )
 }
 
 object ArticleInfo {
@@ -100,52 +109,3 @@ object ArticleInfo {
   private def articleVersionToDb(version: Option[ArticleVersion]) = (version.map(_.major), version.map(_.minor))
 }
 
-import com.google.inject.{ Singleton, ImplementedBy }
-import com.google.inject.Inject
-import com.keepit.common.time.Clock
-import com.keepit.common.healthcheck.AirbrakeNotifier
-import com.keepit.common.logging.Logging
-import com.keepit.common.db.Id
-import com.keepit.common.db.slick._
-import com.keepit.common.db.slick.DBSession.{ RWSession, RSession }
-import org.joda.time.DateTime
-
-@ImplementedBy(classOf[ArticleInfoRepoImpl])
-trait ArticleInfoRepo extends Repo[ArticleInfo] with SeqNumberFunction[ArticleInfo]
-
-@Singleton
-class ArticleInfoRepoImpl @Inject() (
-  val db: DataBaseComponent,
-  val clock: Clock,
-  airbrake: AirbrakeNotifier)
-    extends DbRepo[ArticleInfo] with ArticleInfoRepo with SeqNumberDbFunction[ArticleInfo] with Logging {
-
-  import db.Driver.simple._
-
-  type RepoImpl = ArticleInfoTable
-  class ArticleInfoTable(tag: Tag) extends RepoTable[ArticleInfo](db, tag, "article_info") with SeqNumberColumn[ArticleInfo] {
-    def uriId = column[Id[NormalizedURI]]("uri_id", O.NotNull)
-    def url = column[String]("url", O.NotNull)
-    def kind = column[String]("kind", O.NotNull)
-    def bestVersionMajor = column[VersionNumber[Article]]("best_version_major", O.Nullable)
-    def bestVersionMinor = column[VersionNumber[Article]]("best_version_minor", O.Nullable)
-    def latestVersionMajor = column[VersionNumber[Article]]("latest_version_major", O.Nullable)
-    def latestVersionMinor = column[VersionNumber[Article]]("latest_version_minor", O.Nullable)
-    def lastQueuedAt = column[DateTime]("last_queued_at", O.Nullable)
-    def lastFetchedAt = column[DateTime]("last_fetched_at", O.Nullable)
-    def nextFetchAt = column[DateTime]("next_fetch_at", O.Nullable)
-    def fetchInterval = column[Float]("fetch_interval", O.NotNull)
-    def * = (id.?, createdAt, updatedAt, state, seq, uriId, url, kind, bestVersionMajor.?, bestVersionMinor.?, latestVersionMajor.?, latestVersionMinor.?, lastQueuedAt.?, lastFetchedAt.?, nextFetchAt.?, fetchInterval) <> ((ArticleInfo.applyFromDbRow _).tupled, ArticleInfo.unapplyToDbRow _)
-  }
-
-  def table(tag: Tag) = new ArticleInfoTable(tag)
-  initTable()
-
-  override def deleteCache(model: ArticleInfo)(implicit session: RSession): Unit = {}
-
-  override def invalidateCache(model: ArticleInfo)(implicit session: RSession): Unit = {}
-
-  override def save(model: ArticleInfo)(implicit session: RWSession): ArticleInfo = {
-    super.save(model.copy(seq = deferredSeqNum()))
-  }
-}
