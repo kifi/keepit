9aa702a5f014ab75e86444f102e57cf1745466d1
2is10 <jared@kifi.com>
2015-03-25 10:56:50 -0700

    less library augmentation, doing more lazily

--- kifi-backend/modules/shoebox/angular/src/recos/recoLibraryCard.js
+++ kifi-backend/modules/shoebox/angular/src/recos/recoLibraryCard.js
@@ -29,33 +29,18 @@ angular.module('kifi')
         }
 
         // Data augmentation.
-        // TODO(yiping): make new libraryDecoratorService to do this. Then, DRY up the code that is
-        // currently in nav.js too.
         function augmentData() {
-
-          // Libraries created with the extension do not have the description field.
-          if (!scope.library.description) {
-            scope.library.description = '';
-          }
-
           var maxLength = 300, clipLength = 180;  // numbers differ significantly so that clicking More will show significantly more
-          if (scope.library.description.length > maxLength) {
+          var description = scope.library.description || '';
+          if (description.length > maxLength) {
             // Try to chop off at a word boundary, using a simple space as the delimiter. Grab the space too.
-            var clipLastIndex = scope.library.description.lastIndexOf(' ', clipLength) + 1 || clipLength;
-            scope.library.shortDescription = util.linkify(scope.library.description.substr(0, clipLastIndex));
+            var clipLastIndex = description.lastIndexOf(' ', clipLength) + 1 || clipLength;
+            scope.library.shortDescription = util.linkify(description.substr(0, clipLastIndex));
             scope.clippedDescription = true;
           }
-          scope.library.formattedDescription = '<p>' + util.linkify(scope.library.description).replace(/\n+/g, '<p>');
+          scope.library.formattedDescription = '<p>' + util.linkify(description).replace(/\n+/g, '<p>');
 
-          scope.library.shareUrl = env.origin + scope.library.url;
-          scope.library.shareFbUrl = scope.library.shareUrl +
-            '?utm_medium=vf_facebook&utm_source=library_share&utm_content=lid_' + scope.library.id +
-            '&kcid=na-vf_facebook-library_share-lid_' + scope.library.id;
-
-          scope.library.shareTwitterUrl = encodeURIComponent(scope.library.shareUrl +
-            '?utm_medium=vf_twitter&utm_source=library_share&utm_content=lid_' + scope.library.id +
-            '&kcid=na-vf_twitter-library_share-lid_' + scope.library.id);
-          scope.library.shareText = 'Discover this amazing @Kifi library about ' + scope.library.name + '!';
+          scope.library.absUrl = env.origin + scope.library.url;
         }
 
 
@@ -75,7 +60,9 @@ angular.module('kifi')
           trackShareEvent('clickedShareFacebook');
           $FB.ui({
             method: 'share',
-            href: scope.library.shareFbUrl
+            href: scope.library.absUrl +
+              '?utm_medium=vf_facebook&utm_source=library_share&utm_content=lid_' + scope.library.id +
+              '&kcid=na-vf_facebook-library_share-lid_' + scope.library.id
           });
         };
 
@@ -83,8 +70,16 @@ angular.module('kifi')
           $twitter.load();
         };
 
-        scope.shareTwitter = function () {
+        scope.shareTwitter = function (event) {
           trackShareEvent('clickedShareTwitter');
+          event.target.href = 'https://twitter.com/intent/tweet' + util.formatQueryString({
+            original_referer: scope.library.absUrl,
+            text: 'Discover this amazing @Kifi library about ' + scope.library.name + '!',
+            tw_p: 'tweetbutton',
+            url: scope.library.absUrl +
+              '?utm_medium=vf_twitter&utm_source=library_share&utm_content=lid_' + scope.library.id +
+              '&kcid=na-vf_twitter-library_share-lid_' + scope.library.id
+          });
         };
 
         scope.alreadyFollowingLibrary = function () {
