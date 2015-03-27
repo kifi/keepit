49d59559fab68fd55dbb79330bc548c4b08f0e93
Aaron Hsu <aaron@kifi.com>
2015-03-27 11:40:31 -0700

    different cta on public twitter library page

--- kifi-backend/modules/shoebox/angular/src/common/directives/pageBottomCTA/pageBottomCta.js
+++ kifi-backend/modules/shoebox/angular/src/common/directives/pageBottomCTA/pageBottomCta.js
@@ -14,9 +14,6 @@ angular.module('kifi')
       link: function (scope) {
         scope.show = false;
 
-        //scope.title = 'There\'s more to see...';
-        //scope.subtitle = 'Sign up to see the rest of what\'s here!';
-        // (todo) aaron: remove to release twitter waitlist
         scope.twitterHandle = scope.library.attr && scope.library.attr.twitter.screenName;
         scope.title = scope.twitterHandle ? 'Create a library of your tweeted links' : 'There\'s more to see...';
         scope.subtitle = scope.twitterHandle ? 'Sign up for the Twitter Deep Search Beta' : 'Sign up to see the rest of what\'s here!';
