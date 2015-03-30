695d08925751acbb091cc4eb12da8af5bc8c230a
Aaron Hsu <aaron@kifi.com>
2015-03-30 11:12:34 -0700

    allow admin library point to show inactive keeps

--- kifi-backend/modules/shoebox/app/com/keepit/model/KeepRepo.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/model/KeepRepo.scala
@@ -48,8 +48,8 @@ trait KeepRepo extends Repo[Keep] with ExternalIdColumnFunction[Keep] with SeqNu
   def getLatestKeepsURIByUser(userId: Id[User], limit: Int, includePrivate: Boolean = false)(implicit session: RSession): Seq[Id[NormalizedURI]]
   def getKeepExports(userId: Id[User])(implicit session: RSession): Seq[KeepExport]
   def getByLibrary(libraryId: Id[Library], offset: Int, limit: Int, excludeSet: Set[State[Keep]] = Set(KeepStates.INACTIVE, KeepStates.DUPLICATE))(implicit session: RSession): Seq[Keep]
-  def getCountByLibrary(libraryId: Id[Library])(implicit session: RSession): Int
-  def getCountsByLibrary(libraryIds: Set[Id[Library]])(implicit session: RSession): Map[Id[Library], Int]
+  def getCountByLibrary(libraryId: Id[Library], excludeSet: Set[State[Keep]] = Set(KeepStates.INACTIVE, KeepStates.DUPLICATE))(implicit session: RSession): Int
+  def getCountsByLibrary(libraryIds: Set[Id[Library]], excludeSet: Set[State[Keep]] = Set(KeepStates.INACTIVE, KeepStates.DUPLICATE))(implicit session: RSession): Map[Id[Library], Int]
   def getByExtIdandLibraryId(extId: ExternalId[Keep], libraryId: Id[Library], excludeSet: Set[State[Keep]] = Set(KeepStates.INACTIVE, KeepStates.DUPLICATE))(implicit session: RSession): Option[Keep]
   def getKeepsFromLibrarySince(since: DateTime, library: Id[Library], max: Int)(implicit session: RSession): Seq[Keep]
   def librariesWithMostKeepsSince(count: Int, since: DateTime)(implicit session: RSession): Seq[(Id[Library], Int)]
@@ -439,14 +439,14 @@ class KeepRepoImpl @Inject() (
     (for (b <- rows if b.libraryId === libraryId && !b.state.inSet(excludeSet)) yield b).sortBy(_.keptAt desc).drop(offset).take(limit).list
   }
 
-  def getCountByLibrary(libraryId: Id[Library])(implicit session: RSession): Int = {
-    getCountsByLibrary(Set(libraryId)).getOrElse(libraryId, 0)
+  def getCountByLibrary(libraryId: Id[Library], excludeSet: Set[State[Keep]] = Set(KeepStates.INACTIVE, KeepStates.DUPLICATE))(implicit session: RSession): Int = {
+    getCountsByLibrary(Set(libraryId), excludeSet).getOrElse(libraryId, 0)
   }
 
-  def getCountsByLibrary(libraryIds: Set[Id[Library]])(implicit session: RSession): Map[Id[Library], Int] = {
+  def getCountsByLibrary(libraryIds: Set[Id[Library]], excludeSet: Set[State[Keep]] = Set(KeepStates.INACTIVE, KeepStates.DUPLICATE))(implicit session: RSession): Map[Id[Library], Int] = {
     countByLibraryCache.bulkGetOrElse(libraryIds.map(CountByLibraryKey(_))) { missingKeys =>
       val missingLibraryIds = missingKeys.map(_.id)
-      val keepsQuery = (for (b <- rows if b.libraryId.inSet(missingLibraryIds) && b.state === KeepStates.ACTIVE) yield b)
+      val keepsQuery = (for (b <- rows if b.libraryId.inSet(missingLibraryIds) && !b.state.inSet(excludeSet)) yield b)
       val countQuery = keepsQuery.groupBy(_.libraryId).map { case (libraryId, keeps) => (libraryId, keeps.length) }
       countQuery.run.map { case (libraryIdOpt, keepCount) => (CountByLibraryKey(libraryIdOpt.get), keepCount) }.toMap
     }.map { case (CountByLibraryKey(libraryId), keepCount) => (libraryId, keepCount) }
