10845650fca6ded4201c25a73a23731b815d5751
yingjieMiao <yingjie@42go.com>
2015-03-30 13:48:13 -0700

    some admin changes

--- kifi-backend/modules/common/conf/evolutions/shoebox/310.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/310.sql
@@ -2,7 +2,7 @@
 
 # --- !Ups
 alter table url_pattern_rule
-    alter column non_sensitive tinyint(1) DEFAULT NULL;
+    alter column non_sensitive bool DEFAULT NULL;
 
 insert into evolutions (name, description) values('310.sql', 'make non_sensitive optional in url_pattern_rule');
 
