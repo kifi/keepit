d4f6abe78bb3f0cec8a73dbb5249220f45204d60
Léo Grimaldi <leo@42go.com>
2015-03-30 13:50:56 -0700

    Modify ArticlePolicy

--- kifi-backend/modules/rover/app/com/keepit/rover/manager/ArticleFetchingPolicy.scala
+++ kifi-backend/modules/rover/app/com/keepit/rover/manager/ArticleFetchingPolicy.scala
@@ -3,27 +3,29 @@ package com.keepit.rover.manager
 import com.google.inject.{ Singleton, Inject }
 import com.keepit.common.db.State
 import com.keepit.common.net.{ Host, URI }
-import com.keepit.model.{ NormalizedURIStates, NormalizedURI }
+import com.keepit.model.NormalizedURI
+import com.keepit.model.NormalizedURIStates._
 import com.keepit.rover.article._
 import com.keepit.rover.article.content.LinkedInProfile
 
 @Singleton
 class ArticleFetchingPolicy @Inject() () {
 
-  def toBeInterned(url: String, state: State[NormalizedURI]): Set[ArticleKind[_ <: Article]] = {
-    toBeFetched(url) -- toBeDeactivated(state)
-  }
-
-  def toBeDeactivated(uriState: State[NormalizedURI]): Set[ArticleKind[_ <: Article]] = {
-    import NormalizedURIStates._
+  def toBeInterned(url: String, uriState: State[NormalizedURI]): Set[ArticleKind[_ <: Article]] = {
     uriState match {
-      case INACTIVE | REDIRECTED => ArticleKind.all
-      case UNSCRAPABLE => ArticleKind.all - EmbedlyArticle
-      case _ => Set.empty
+      case ACTIVE | INACTIVE | REDIRECTED => Set.empty
+      case UNSCRAPABLE => Set(EmbedlyArticle)
+      case SCRAPED | SCRAPE_FAILED => Set(EmbedlyArticle) ++ toBeScraped(url)
     }
   }
 
-  private def toBeFetched(url: String): Set[ArticleKind[_ <: Article]] = toBeScraped(url).toSet + EmbedlyArticle
+  def toBeDeactivated(uriState: State[NormalizedURI]): Set[ArticleKind[_ <: Article]] = {
+    uriState match {
+      case INACTIVE | REDIRECTED => ArticleKind.all
+      case UNSCRAPABLE => ArticleKind.all - EmbedlyArticle
+      case ACTIVE | SCRAPED | SCRAPE_FAILED => Set.empty
+    }
+  }
 
   private def toBeScraped(url: String): Option[ArticleKind[_ <: Article]] = URI.parse(url).toOption.map {
     case URI(_, _, Some(Host("com", "youtube", _*)), _, Some(path), Some(query), _) if path.endsWith("/watch") && query.containsParam("v") => YoutubeArticle
