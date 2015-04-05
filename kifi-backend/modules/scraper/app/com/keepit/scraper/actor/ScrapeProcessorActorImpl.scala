e706c2725349f631a503db82dcde20c631d51bfa
yingjieMiao <yingjie@42go.com>
2015-04-04 22:23:42 -0700

    relax lock waiting size

--- kifi-backend/modules/scraper/app/com/keepit/scraper/actor/ScrapeProcessorActorImpl.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/actor/ScrapeProcessorActorImpl.scala
@@ -63,7 +63,7 @@ class ScrapeProcessorActorImpl @Inject() (
 
   private[this] val lock = new ReactiveLock(1, Some(100))
 
-  override def pull(): Unit = if (lock.waiting < 3) lock.withLockFuture {
+  override def pull(): Unit = if (lock.waiting < 100) lock.withLockFuture {
     val futureTask: Future[Unit] = getQueueSize() flatMap { qSize =>
       log.warn(s"[ScrapeProcessorActorImpl.pull] qSize: $qSize")
       if (qSize <= config.pullThreshold) {
