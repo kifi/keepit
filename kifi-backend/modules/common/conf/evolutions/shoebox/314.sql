9fd66b5e06ea7a27c3a6057cd0c53c67a7133b89
Léo Grimaldi <leo@42go.com>
2015-04-06 15:28:01 -0700

    Add article_info_i_last_fetched_at index to ArticleInfo

--- kifi-backend/modules/common/conf/evolutions/shoebox/314.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/314.sql
@@ -3,7 +3,8 @@
 # --- !Ups
 alter table article_info
     add column domain varchar(512) NULL;
+CREATE INDEX article_info_i_last_fetched_at ON article_info(last_fetched_at);
 
-insert into evolutions (name, description) values('314.sql', 'add domain column to article_info');
+insert into evolutions (name, description) values('314.sql', 'add domain column to article_info, article_info_i_last_fetched_at index');
 
 # --- !Downs
