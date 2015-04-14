20cc5c72ce03f2989734d4c107c5b4f68708aec3
Léo Grimaldi <leo@42go.com>
2015-04-14 11:18:07 -0700

    Do not use syntactic sugar in RandomDelay

--- kifi-backend/modules/common/app/com/keepit/common/time/RandomDelay.scala
+++ kifi-backend/modules/common/app/com/keepit/common/time/RandomDelay.scala
@@ -3,7 +3,7 @@ package com.keepit.common.time
 import scala.concurrent.duration._
 import scala.util.Random
 
-class RandomDelay(maxDelay: Duration) extends (() => Duration) {
+class RandomDelay(maxDelay: Duration) extends Function0[Duration] {
   private val maxRandomDelaySeconds: Int = maxDelay.toSeconds.toInt
   def apply = Random.nextInt(maxRandomDelaySeconds) seconds
 }
