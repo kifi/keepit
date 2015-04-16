fe055d9f76f630057deebe6632fd79c9984c3c16
yingjieMiao <yingjie@42go.com>
2015-04-15 17:12:25 -0700

    feedback tracking commander test

--- kifi-backend/modules/curator/test/com/keepit/curator/feedback/UserRecoFeedbackCommanderTest.scala
+++ kifi-backend/modules/curator/test/com/keepit/curator/feedback/UserRecoFeedbackCommanderTest.scala
@@ -1,9 +1,10 @@
 package com.keepit.curator.feedback
 
 import com.keepit.common.db.Id
+import com.keepit.cortex.models.lda.LDATopic
 import com.keepit.curator.CuratorTestInjector
-import com.keepit.curator.model.{SeedAttribution, UriScores, UriRecommendation, UserRecoFeedbackCounterRepo}
-import com.keepit.model.{User, NormalizedURI}
+import com.keepit.curator.model._
+import com.keepit.model.{ User, NormalizedURI }
 import org.specs2.mutable.Specification
 
 class UserRecoFeedbackCommanderTest extends Specification with CuratorTestInjector {
@@ -18,24 +19,39 @@ class UserRecoFeedbackCommanderTest extends Specification with CuratorTestInject
 
   "UserRecoFeedbackTrackingCommander" should {
     "work" in {
-      withDb(){ implicit injector =>
+      withDb() { implicit injector =>
         val repo = inject[UserRecoFeedbackCounterRepo]
         val commander = new UserRecoFeedbackTrackingCommander(db, repo)
-        val item = UriRecommendation(uriId = Id[NormalizedURI](1), userId = Id[User](42), masterScore = 0.15f,
-        allScores = UriScores(socialScore = 1.0f,
-          popularityScore = 1.0f,
-          overallInterestScore = 1.0f,
-          recentInterestScore = 1.0f,
-          recencyScore = 1.0f,
-          priorScore = 1.0f,
-          rekeepScore = 1.0f,
-          discoveryScore = 1.0f,
-          curationScore = None,
-          multiplier = Some(0.01f),
-          libraryInducedScore = Some(0f)),
-        delivered = 0, clicked = 0, kept = false, attribution = SeedAttribution.EMPTY,
-        topic1 = None, topic2 = None)
-        commander.trackFeedback()
+        val item = UriRecommendation(uriId = Id[NormalizedURI](1), userId = Id[User](1), masterScore = 0.15f,
+          allScores = UriScores(socialScore = 1.0f,
+            popularityScore = 1.0f,
+            overallInterestScore = 1.0f,
+            recentInterestScore = 1.0f,
+            recencyScore = 1.0f,
+            priorScore = 1.0f,
+            rekeepScore = 1.0f,
+            discoveryScore = 1.0f,
+            curationScore = None,
+            multiplier = Some(0.01f),
+            libraryInducedScore = Some(0f)),
+          delivered = 0, clicked = 0, kept = false, attribution = SeedAttribution.EMPTY,
+          topic1 = Some(LDATopic(0)), topic2 = None)
+
+        val bucketId = FeedbackBucketMapper.getBucketId(item).get
+        bucketId === 256
+
+        commander.trackFeedback(item, UriRecoFeedbackValue.CLICKED)
+        var counter = db.readOnlyReplica { implicit s => repo.getByUser(Id[User](1)) }.get
+        counter.voteUps.get(bucketId) === 1
+        counter.voteUps.toArray().sum === 1
+        counter.voteDowns.toArray().sum === 0
+
+        commander.trackFeedback(item, UriRecoFeedbackValue.DISLIKE)
+        counter = db.readOnlyReplica { implicit s => repo.getByUser(Id[User](1)) }.get
+        counter.voteDowns.get(bucketId) === 5
+        counter.voteUps.toArray().sum === 1
+        counter.voteDowns.toArray().sum === 5
+
       }
     }
   }
