53ccc70de4b06845c86678753da1f6cb66f40915
yingjieMiao <yingjie@42go.com>
2015-04-22 16:07:25 -0700

    one index was there

--- kifi-backend/modules/common/conf/evolutions/shoebox/322.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/322.sql
@@ -2,8 +2,6 @@
 
 # --- !Ups
 
-create index lda_related_library_i_version_dest on lda_related_library(version, dest_id);
-
 create index cortex_keep_i_uri on cortex_keep(uri_id);
 
 insert into evolutions (name, description) values('322.sql', 'add index on cortex_keep and lda_related_library');
