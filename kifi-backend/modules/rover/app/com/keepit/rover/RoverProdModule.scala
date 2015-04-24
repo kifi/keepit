fc8788dbfbb9ffdc89a6d279d07dbf2b2a382841
Léo Grimaldi <leo@42go.com>
2015-04-23 17:43:19 -0700

    Introduce ArticleImageProcessingTask, corresponding FastFollow and CatchUp SQS queues

--- kifi-backend/modules/rover/app/com/keepit/rover/RoverProdModule.scala
+++ kifi-backend/modules/rover/app/com/keepit/rover/RoverProdModule.scala
@@ -5,13 +5,13 @@ import com.keepit.common.controller.ProdRemoteUserActionsHelperModule
 import com.keepit.inject.CommonProdModule
 import com.keepit.rover.common.cache.RoverCacheModule
 import com.keepit.rover.fetcher.{ ProdHttpFetcherModule, HttpFetcherModule }
-import com.keepit.rover.manager.{ ProdFetchQueueModule, FetchQueueModule }
+import com.keepit.rover.manager.{ ProdRoverQueueModule, RoverQueueModule }
 import com.keepit.rover.store.RoverProdStoreModule
 
 case class RoverProdModule() extends RoverModule with CommonProdModule {
   val userActionsModule = ProdRemoteUserActionsHelperModule()
   val cacheModule = RoverCacheModule(MemcachedCacheModule(), EhCacheCacheModule())
   val storeModule = RoverProdStoreModule()
-  val fetchQueueModule: FetchQueueModule = ProdFetchQueueModule()
+  val queueModule: RoverQueueModule = ProdRoverQueueModule()
   val httpFetcherModule: HttpFetcherModule = ProdHttpFetcherModule()
 }
