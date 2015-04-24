8958e6628cf6bd321009d989565a2c81fda8a37d
Aaron Hsu <aaron@kifi.com>
2015-04-23 17:04:37 -0700

    object for reggae

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/HashtagCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/HashtagCommander.scala
@@ -4,6 +4,31 @@ import com.keepit.model.Hashtag
 
 class HashtagCommander {
 
+  def findAllHashtagNames(s: String): Set[String] = {
+    HashtagRe.hashTagRe.findAllMatchIn(s).map(_ group 1).map { escapedName =>
+      HashtagRe.backslashUnescapeRe.replaceAllIn(escapedName, "$1")
+    } toSet
+  }
+
+  // append hashtags to the end of a string, separated by spaces
+  def appendHashtagsToString(str: String, hashtags: Seq[Hashtag]): String = {
+    val hashtagNames = hashtags.map(_.tag)
+    appendHashtagNamesToString(str, hashtagNames).trim
+  }
+
+  def appendHashtagNamesToString(str: String, hashtagNames: Seq[String]): String = {
+    val tagsStr = hashtagNames.filter(_.nonEmpty).map { tag =>
+      "[#" + HashtagRe.backslashEscapeRe.replaceAllIn(tag, """\\$0""") + "]"
+    }.mkString(" ")
+    (str + " " + tagsStr).trim
+  }
+
+  def removeHashtagsFromString(str: String): String = {
+    HashtagRe.hashTagRe.replaceAllIn(str, "").trim
+  }
+}
+
+object HashtagRe {
   // matches '[#...]'.
   // A literal '[#' indicates the start of a hashtag
   // A literal ']' indicates the end of the hashtag
@@ -11,28 +36,4 @@ class HashtagCommander {
   val hashTagRe = """\[#((?:\\[\\\]]|[^\]])+)\]""".r
   val backslashEscapeRe = """[\]\\]""".r
   val backslashUnescapeRe = """\\(.)""".r
-
-  def findAllHashtagNames(s: String): Set[String] = {
-    hashTagRe.findAllMatchIn(s).map(_ group 1).map { escapedName =>
-      backslashUnescapeRe.replaceAllIn(escapedName, "$1")
-    } toSet
-  }
-
-  // append hashtags to the end of a string, separated by spaces
-  def appendHashtagsToString(str: String, hashtags: Seq[Hashtag]): String = {
-    val hashtagNames = hashtags.map(_.tag)
-    appendHashtagNamesToString(str, hashtagNames)
-  }
-
-  def appendHashtagNamesToString(str: String, hashtagNames: Seq[String]): String = {
-    val tagsStr = hashtagNames.filter(_.nonEmpty).map { tag =>
-      "[#" + backslashEscapeRe.replaceAllIn(tag, """\\$0""") + "]"
-    }.mkString(" ")
-    (str + " " + tagsStr).trim
-  }
-
-  def removeHashtagsFromString(str: String): String = {
-    hashTagRe.replaceAllIn(str, "").trim
-  }
-
 }
