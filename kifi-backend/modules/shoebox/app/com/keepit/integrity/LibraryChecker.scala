cdee331a750ec1000d46dd68982733fec5b4c572
Aaron Hsu <aaron@kifi.com>
2015-04-27 12:42:47 -0700

    data integrity plugin should be 10 minutes actually

--- kifi-backend/modules/shoebox/app/com/keepit/integrity/LibraryChecker.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/integrity/LibraryChecker.scala
@@ -138,8 +138,8 @@ class LibraryChecker @Inject() (
     val currentTime = clock.now()
     val hourInd = currentTime.hourOfDay().get
     val minuteInd = currentTime.minuteOfHour().get
-    val index = hourInd * 4 + (minuteInd / 15) // hour * 4 + minute / 15 (int div)
-    val numIntervals = 96 // 96 = 24 (hours per day) * 4 (15 minute intervals per hour)
+    val index = hourInd * 6 + (minuteInd / 10) // hour * 6 + minute / 10 (int div)
+    val numIntervals = 144 // 144 = 24 (hours per day) * 6 (10 minute intervals per hour)
     (index, numIntervals)
   }
 
