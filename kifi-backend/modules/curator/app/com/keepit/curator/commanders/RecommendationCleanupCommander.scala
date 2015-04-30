66d0573083ebc197dd7e23bd8eb37b016913d57e
yingjieMiao <yingjie@42go.com>
2015-04-29 18:23:00 -0700

    lightweight reaper, and less repetition.

--- kifi-backend/modules/curator/app/com/keepit/curator/commanders/RecommendationCleanupCommander.scala
+++ kifi-backend/modules/curator/app/com/keepit/curator/commanders/RecommendationCleanupCommander.scala
@@ -11,15 +11,20 @@ import com.keepit.common.logging.{ NamedStatsdTimer, Logging }
 
 class RecommendationCleanupCommander @Inject() (
     db: Database,
+    clock: Clock,
     uriRecoRepo: UriRecommendationRepo,
     feedsRepo: PublicFeedRepo) extends Logging {
 
   private val recosTTL = 30 // days (by updateAt)
   private val defaultLimitNumRecosForUser = 300
   def cleanup(overrideLimit: Option[Int] = None, overrideTimeCutoff: Option[DateTime] = None): Unit = {
-    val userToClean = Random.shuffle(db.readOnlyReplica { implicit session => uriRecoRepo.getUsersWithRecommendations() }.toSeq).take(75)
+    val usersWithReco = db.readOnlyReplica { implicit session => uriRecoRepo.getUsersWithRecommendations() }.toSeq
+    val (idx, ringSize) = getIndexAndTotalSize
+    val userToClean = usersWithReco.filter(_.id % ringSize == idx)
+
     val timer = new NamedStatsdTimer("RecommendationCleanupCommander.cleanup")
-    log.info(s"Running Uri Reco Cleanup for $userToClean")
+    log.info(s"Running Uri Reco Cleanup for ${userToClean.size} users. slice $idx out of $ringSize")
+
     db.readWriteBatch(userToClean) { (session, userId) =>
       uriRecoRepo.cleanupOldRecos(userId, currentDateTime.minusDays(recosTTL))(session)
       uriRecoRepo.cleanupLowMasterScoreRecos(userId, overrideLimit.getOrElse(defaultLimitNumRecosForUser), overrideTimeCutoff.getOrElse(currentDateTime.minusDays(3)))(session)
@@ -31,5 +36,15 @@ class RecommendationCleanupCommander @Inject() (
         }
     }
     timer.stopAndReport(appLog = true)
+  }
+
+  // approximately goes through everyone in 4 weeks. 4 * 7 * slicesPerDay buckets. about 8K buckets with current setting.
+  private def getIndexAndTotalSize: (Int, Int) = {
+    val t = clock.now()
+    val callFreq = 5 // scheduler calls this every 5 minutes.
+    val slicesPerDay = 60 * 24 / callFreq
+    val (wi, di, mi) = (t.weekOfWeekyear.get % 4, t.dayOfWeek.get % 7, (t.minuteOfDay.get / callFreq) % slicesPerDay)
+    val idx = wi * (7 * slicesPerDay) + di * slicesPerDay + mi
+    (idx, 4 * 7 * slicesPerDay)
   }
 }
