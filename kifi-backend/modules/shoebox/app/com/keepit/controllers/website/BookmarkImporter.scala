a54692fa2f108860347fdf143984b902ef56e9d6
Aaron Hsu <aaron@kifi.com>
2015-05-01 15:26:00 -0700

    use kept names instead of tagIds

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/BookmarkImporter.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/BookmarkImporter.scala
@@ -103,8 +103,8 @@ class BookmarkImporter @Inject() (
     }.toMap
     val taggedKeeps = parsed.map {
       case Bookmark(t, h, tagNames, createdDate, originalJson) =>
-        val keepTags = tagNames.map(tags.get).flatten.map(_.id.get)
-        BookmarkWithTagIds(t, h, keepTags, createdDate, originalJson)
+        val keepTags = tagNames.map(tags.get).flatten
+        BookmarkWithTags(t, h, keepTags, createdDate, originalJson)
     }
 
     val (importId, rawKeeps) = createRawKeeps(userId, sourceOpt, taggedKeeps, libraryId)
@@ -132,8 +132,8 @@ class BookmarkImporter @Inject() (
       }.toMap
       val taggedKeeps = parsed.map {
         case Bookmark(t, h, tagNames, createdDate, originalJson) =>
-          val keepTags = tagNames.map(tags.get).flatten.map(_.id.get) :+ importTag.id.get
-          BookmarkWithTagIds(t, h, keepTags, createdDate, originalJson)
+          val keepTags = tagNames.map(tags.get).flatten :+ importTag
+          BookmarkWithTags(t, h, keepTags, createdDate, originalJson)
       }
       log.info(s"[bmFileImport:${lf.id}] Tags extracted in ${clock.getMillis() - lf.startMillis}ms")
 
@@ -209,14 +209,16 @@ class BookmarkImporter @Inject() (
     (source, extracted)
   }
 
-  private def createRawKeeps(userId: Id[User], source: Option[KeepSource], bookmarks: Seq[BookmarkWithTagIds], libraryId: Id[Library]) = {
+  private def createRawKeeps(userId: Id[User], source: Option[KeepSource], bookmarks: Seq[BookmarkWithTags], libraryId: Id[Library]) = {
     val importId = UUID.randomUUID.toString
     val rawKeeps = bookmarks.map {
-      case BookmarkWithTagIds(title, href, tagIds, createdDate, originalJson) =>
+      case BookmarkWithTags(title, href, tags, createdDate, originalJson) =>
         val titleOpt = if (title.nonEmpty && title.exists(_.nonEmpty)) Some(title.get) else None
-        val tags = tagIds.map(_.id.toString).mkString(",") match {
-          case s if s.isEmpty => None
-          case s => Some(s)
+        val hashtagsArray = if (tags.nonEmpty) {
+          val hashtagNames = tags.map(c => Json.toJson(c.name.tag)).toSeq
+          Some(JsArray(hashtagNames))
+        } else {
+          None
         }
 
         RawKeep(userId = userId,
@@ -227,7 +229,7 @@ class BookmarkImporter @Inject() (
           source = source.getOrElse(KeepSource.bookmarkFileImport),
           originalJson = originalJson,
           installationId = None,
-          tagIds = tags,
+          keepTags = hashtagsArray,
           libraryId = Some(libraryId),
           createdDate = createdDate)
     }
@@ -237,4 +239,4 @@ class BookmarkImporter @Inject() (
 }
 
 case class Bookmark(title: Option[String], href: String, tags: List[String], createdDate: Option[DateTime], originalJson: Option[JsValue])
-case class BookmarkWithTagIds(title: Option[String], href: String, tags: List[Id[Collection]], createdAt: Option[DateTime], originalJson: Option[JsValue])
+case class BookmarkWithTags(title: Option[String], href: String, tags: List[Collection], createdAt: Option[DateTime], originalJson: Option[JsValue])
