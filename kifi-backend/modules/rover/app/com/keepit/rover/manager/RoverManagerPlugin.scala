0ceec5084858df91a369dcd513b1e981637091df
Léo Grimaldi <leo@42go.com>
2015-05-01 19:08:42 -0700

    Deactivate ArticleImageProcessing scheduling

--- kifi-backend/modules/rover/app/com/keepit/rover/manager/RoverManagerPlugin.scala
+++ kifi-backend/modules/rover/app/com/keepit/rover/manager/RoverManagerPlugin.scala
@@ -28,7 +28,7 @@ class RoverManagerPluginImpl @Inject() (
     scheduleTaskOnOneMachine(ingestionActor.system, 187 seconds, 1 minute, ingestionActor.ref, IfYouCouldJustGoAhead, "NormalizedURI Ingestion")
     scheduleTaskOnOneMachine(fetchSchedulingActor.system, 200 seconds, 1 minute, fetchSchedulingActor.ref, IfYouCouldJustGoAhead, "Fetch Scheduling")
     scheduleTaskOnAllMachines(fetchingActor.system, (30 + Random.nextInt(60)) seconds, 1 minute, fetchingActor.ref, IfYouCouldJustGoAhead)
-    scheduleTaskOnOneMachine(imageSchedulingActor.system, 200 seconds, 1 minute, imageSchedulingActor.ref, IfYouCouldJustGoAhead, "ArticleImage Scheduling")
+    // scheduleTaskOnOneMachine(imageSchedulingActor.system, 200 seconds, 1 minute, imageSchedulingActor.ref, IfYouCouldJustGoAhead, "ArticleImage Scheduling") (todo(Léo): reactivate after query has been optimized)
     scheduleTaskOnAllMachines(imageProcessingActor.system, (30 + Random.nextInt(60)) seconds, 1 minute, imageProcessingActor.ref, IfYouCouldJustGoAhead)
 
     super.onStart()
