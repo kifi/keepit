9dced7373576f40a8548b534acbcf811618d38e2
Cam <Cam@Camerons-MacBook-Pro.local>
2015-05-05 13:50:03 -0700

    added AdminUserControllerTest,,., test cases f, test cases for SuperAdmin-only adding/removing admins

---
+++ kifi-backend/modules/shoebox/test/com/keepit/controllers/admin/AdminUserControllerTest.scala
@@ -0,0 +1,93 @@
+package com.keepit.controllers.admin
+
+import com.keepit.abook.FakeABookServiceClientModule
+import com.keepit.common.actor.FakeActorSystemModule
+import com.keepit.common.concurrent.FakeExecutionContextModule
+import com.keepit.common.controller.FakeUserActionsModule
+import com.keepit.common.crypto.FakeCryptoModule
+import com.keepit.common.db.Id
+import com.keepit.common.healthcheck.FakeAirbrakeModule
+import com.keepit.common.mail.FakeMailModule
+import com.keepit.common.social.{ FakeSocialGraphModule, FakeShoeboxAppSecureSocialModule }
+import com.keepit.common.store.FakeShoeboxStoreModule
+import com.keepit.cortex.FakeCortexServiceClientModule
+import com.keepit.curator.FakeCuratorServiceClientModule
+import com.keepit.heimdal.FakeHeimdalServiceClientModule
+import com.keepit.scraper.{ FakeScraperServiceClientModule, FakeScrapeSchedulerModule }
+import com.keepit.search.FakeSearchServiceClientModule
+import com.keepit.shoebox.FakeShoeboxServiceModule
+import org.specs2.mutable.Specification
+import com.keepit.test._
+import com.keepit.model._
+import com.google.inject.Injector
+import com.keepit.common.net.FakeHttpClientModule
+import play.libs.F.Tuple
+
+class AdminUserControllerTest extends Specification with ShoeboxTestInjector {
+
+  def modules = Seq(
+    FakeShoeboxServiceModule(),
+    FakeSocialGraphModule(),
+    FakeScrapeSchedulerModule(),
+    FakeABookServiceClientModule()
+  )
+
+  def setup()(implicit injector: Injector) = {
+
+    val NON_ADMIN_ID = Id[User](87754) // cam's dad's id
+    val ADMIN_ID = Id[User](35713) // cam's id
+    val SUPERADMIN_ID = Id[User](1) // eishay's id
+
+    val userRepo = inject[UserRepo]
+    val userExperimentRepo = inject[UserExperimentRepo]
+    db.readWrite { implicit session =>
+
+      val userSuperAdminOpt: User = userRepo.save(User(
+        firstName = "Clark",
+        lastName = "Kent", username = Username("clark"), normalizedUsername = "clark"
+      ))
+      val userNonAdminOpt = userRepo.save(User(
+        firstName = "Homer",
+        lastName = "Simpson", username = Username("homer"), normalizedUsername = "homer"
+      ))
+      val userAdminOpt = userRepo.save(User(
+        firstName = "Peter",
+        lastName = "Griffin", username = Username("peter"), normalizedUsername = "peter"
+      ))
+
+      userExperimentRepo.save(UserExperiment(userId = userSuperAdminOpt.id.get, experimentType = ExperimentType.ADMIN))
+      userExperimentRepo.save(UserExperiment(userId = userAdminOpt.id.get, experimentType = ExperimentType.ADMIN))
+      userExperimentRepo.save(UserExperiment(userId = userNonAdminOpt.id.get, experimentType = ExperimentType.BYPASS_ABUSE_CHECKS))
+      (userNonAdminOpt, userAdminOpt, userSuperAdminOpt)
+    }
+  }
+
+  "AdminUserController" should {
+    "deny non-superAdmins from adding or removing admins" in {
+
+      withDb(modules: _*) { implicit injector =>
+        val (userNonAdminOpt, userAdminOpt, userSuperAdminOpt) = setup()
+        val adminUserController = inject[AdminUserController]
+        val userExperimentRepo = inject[UserExperimentRepo]
+        val userRepo = inject[UserRepo]
+
+        db.readWrite { implicit session =>
+          
+
+          adminUserController.addExperiment(requesterUserId = userNonAdminOpt.id.get, userId = userNonAdminOpt.id.get, "admin") must throwA[SuperAdminAccessDenied]
+          adminUserController.removeExperiment(requesterUserId = userNonAdminOpt.id.get, userId = userAdminOpt.id.get, "admin") must throwA[SuperAdminAccessDenied]
+
+          adminUserController.addExperiment(requesterUserId = userAdminOpt.id.get, userId = userNonAdminOpt.id.get, "admin") must throwA[SuperAdminAccessDenied]
+          adminUserController.removeExperiment(requesterUserId = userAdminOpt.id.get, userId = userNonAdminOpt.id.get, "admin") must throwA[SuperAdminAccessDenied]
+
+          adminUserController.addExperiment(requesterUserId = userSuperAdminOpt.id.get, userId = userNonAdminOpt.id.get, "admin")
+          userExperimentRepo.getUserExperiments(userNonAdminOpt.id.get).contains(ExperimentType.ADMIN) must equalTo(true)
+
+          adminUserController.removeExperiment(requesterUserId = userSuperAdminOpt.id.get, userId = userAdminOpt.id.get, "admin")
+          userExperimentRepo.getUserExperiments(userAdminOpt.id.get).contains(ExperimentType.ADMIN) must equalTo(false)
+        }
+
+      }
+    }
+  }
+}
