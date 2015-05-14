887efe8633c7302c59995ebbc85aeb2c62dee54d
Cam Hashemi <camhashemi@gmail.com>
2015-05-14 14:57:25 -0700

    rolling out new "html-plain" welcome email

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/UserCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/UserCommander.scala
@@ -377,7 +377,7 @@ class UserCommander @Inject() (
     } else Option(Future.successful(Set.empty))
   }
 
-  def sendWelcomeEmail(newUser: User, withVerification: Boolean = false, targetEmailOpt: Option[EmailAddress] = None, isPlainEmail: Boolean = false): Future[Unit] = {
+  def sendWelcomeEmail(newUser: User, withVerification: Boolean = false, targetEmailOpt: Option[EmailAddress] = None, isPlainEmail: Boolean = true): Future[Unit] = {
     if (!db.readOnlyMaster { implicit session => userValueRepo.getValue(newUser.id.get, UserValues.welcomeEmailSent) }) {
       val emailF = emailSender.welcome(newUser.id.get, targetEmailOpt, isPlainEmail)
       emailF.map { email =>
