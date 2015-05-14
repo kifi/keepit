84413c785c0976317cfcb1840a49325617f4cc20
Léo Grimaldi <leo@42go.com>
2015-05-13 17:01:05 -0700

    Index and store notes in the Keep Index

--- kifi-backend/modules/search/app/com/keepit/search/index/graph/keep/KeepRecord.scala
+++ kifi-backend/modules/search/app/com/keepit/search/index/graph/keep/KeepRecord.scala
@@ -8,21 +8,22 @@ import org.apache.lucene.store.{ InputStreamDataInput, OutputStreamDataOutput }
 import scala.collection.JavaConversions._
 import play.api.libs.json.Json
 
-case class KeepRecord(title: Option[String], url: String, createdAt: Long, libraryId: Long, externalId: ExternalId[Keep], tags: Set[Hashtag])
+case class KeepRecord(title: Option[String], url: String, createdAt: Long, libraryId: Long, externalId: ExternalId[Keep], note: Option[String], tags: Set[Hashtag])
 
 object KeepRecord {
-  def fromKeepAndTags(keep: Keep, tags: Set[Hashtag]): KeepRecord = KeepRecord(keep.title, keep.url, keep.createdAt.getMillis, keep.libraryId.map(_.id).getOrElse(-1L), keep.externalId, tags)
+  def fromKeepAndTags(keep: Keep, tags: Set[Hashtag]): KeepRecord = KeepRecord(keep.title, keep.url, keep.createdAt.getMillis, keep.libraryId.map(_.id).getOrElse(-1L), keep.externalId, keep.note.filter(_.nonEmpty), tags)
 
   implicit def toByteArray(record: KeepRecord): Array[Byte] = {
     val baos = new ByteArrayOutputStream()
     val out = new OutputStreamDataOutput(baos)
 
-    out.writeByte(1) // version
+    out.writeByte(2) // version
     out.writeString(record.title.getOrElse(""))
     out.writeString(record.url)
     out.writeLong(record.createdAt)
     out.writeLong(record.libraryId)
     out.writeString(record.externalId.id)
+    out.writeString(record.note.getOrElse(""))
     out.writeStringSet(record.tags.map(_.tag))
     out.close()
     baos.close()
@@ -35,16 +36,18 @@ object KeepRecord {
     val in = new InputStreamDataInput(new ByteArrayInputStream(bytes, offset, length))
 
     val version = in.readByte().toInt
-    if (version > 1) {
+    if (version > 2) {
       throw new Exception(s"invalid data [version=${version}]")
     }
+
     val title = Some(in.readString()).filter(_.nonEmpty)
     val url = in.readString()
     val createdAt = in.readLong()
     val libraryId = in.readLong()
     val id = ExternalId[Keep](in.readString())
+    val note = if (version < 2) None else Some(in.readString()).filter(_.nonEmpty)
     val tags = in.readStringSet().map(Hashtag(_)).toSet
-    KeepRecord(title, url, createdAt, libraryId, id, tags)
+    KeepRecord(title, url, createdAt, libraryId, id, note, tags)
   }
 
   implicit val format = Json.format[KeepRecord]
