16fda898dad96d97772b19b3534f7fd74729f56e
Léo Grimaldi <leo@42go.com>
2015-05-17 14:52:19 -0700

    Fix CortexCacheModule

--- kifi-backend/modules/cortex/app/com/keepit/common/cache/CortexCacheModule.scala
+++ kifi-backend/modules/cortex/app/com/keepit/common/cache/CortexCacheModule.scala
@@ -47,11 +47,6 @@ case class CortexCacheModule(cachePluginModules: CachePluginModule*) extends Cac
 
   @Singleton
   @Provides
-  def uriSummaryCache(stats: CacheStatistics, accessLog: AccessLog, innerRepo: InMemoryCachePlugin, outerRepo: FortyTwoCachePlugin) =
-    new URISummaryCache(stats, accessLog, (innerRepo, 10 minutes), (outerRepo, 30 days))
-
-  @Singleton
-  @Provides
   def socialUserInfoUserCache(stats: CacheStatistics, accessLog: AccessLog, outerRepo: FortyTwoCachePlugin) =
     new SocialUserInfoUserCache(stats, accessLog, (outerRepo, 30 days))
 
