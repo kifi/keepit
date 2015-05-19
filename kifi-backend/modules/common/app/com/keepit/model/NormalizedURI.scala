69ec0a0f991ec74bc6106edaf597350f8213ab41
Léo Grimaldi <leo@42go.com>
2015-05-19 15:13:26 -0700

    Introduce RoverArticleInfo.urlHash

--- kifi-backend/modules/common/app/com/keepit/model/NormalizedURI.scala
+++ kifi-backend/modules/common/app/com/keepit/model/NormalizedURI.scala
@@ -69,7 +69,7 @@ object NormalizedURI {
     (__ \ 'externalId).format(ExternalId.format[NormalizedURI]) and
     (__ \ 'title).formatNullable[String] and
     (__ \ 'url).format[String] and
-    (__ \ 'urlHash).format[String].inmap(UrlHash.apply, unlift(UrlHash.unapply)) and
+    (__ \ 'urlHash).format[UrlHash] and
     (__ \ 'state).format(State.format[NormalizedURI]).inmap(handleDeprecatedScrapeWantedState, identity[State[NormalizedURI]]) and
     (__ \ 'seq).format(SequenceNumber.format[NormalizedURI]) and
     (__ \ 'restriction).formatNullable[Restriction] and
@@ -79,23 +79,30 @@ object NormalizedURI {
     (__ \ 'shouldHaveContent).format[Boolean]
   )(NormalizedURI.apply, unlift(NormalizedURI.unapply))
 
-  def hashUrl(normalizedUrl: String): UrlHash = {
-    val binaryHash = MessageDigest.getInstance("MD5").digest(normalizedUrl)
-    UrlHash(new String(new Base64().encode(binaryHash), UTF8))
-  }
-
   def withHash(
     normalizedUrl: String,
     title: Option[String] = None,
     state: State[NormalizedURI] = NormalizedURIStates.ACTIVE,
     normalization: Option[Normalization] = None): NormalizedURI = {
     if (normalizedUrl.size > URLFactory.MAX_URL_SIZE) throw new Exception(s"url size is ${normalizedUrl.size} which exceeds ${URLFactory.MAX_URL_SIZE}: $normalizedUrl")
-    NormalizedURI(title = title, url = normalizedUrl, urlHash = hashUrl(normalizedUrl), state = state, normalization = normalization)
+    NormalizedURI(title = title, url = normalizedUrl, urlHash = UrlHash.hash(normalizedUrl), state = state, normalization = normalization)
   }
 }
 
 case class UrlHash(hash: String) extends AnyVal {
   override def toString: String = hash
+}
+
+object UrlHash {
+  def hash(url: String): UrlHash = {
+    val binaryHash = MessageDigest.getInstance("MD5").digest(url)
+    UrlHash(new String(new Base64().encode(binaryHash), UTF8))
+  }
+
+  implicit val format: Format[UrlHash] = new Format[UrlHash] {
+    def reads(json: JsValue): JsResult[UrlHash] = json.validate[String].map(UrlHash.apply)
+    def writes(o: UrlHash): JsValue = JsString(o.hash)
+  }
 }
 
 case class NormalizedURIKey(id: Id[NormalizedURI]) extends Key[NormalizedURI] {
