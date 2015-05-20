5bd29026672d7e9f2bbc967a776c7923bfd89246
2is10 <jared@kifi.com>
2015-05-20 15:10:52 -0700

    deprecating routeService, introducing net

---
+++ kifi-backend/modules/shoebox/angular/src/common/services/net.js
@@ -0,0 +1,60 @@
+'use strict';
+
+angular.module('kifi')
+
+.factory('net', [
+  'env', '$http',
+  function (env, $http) {
+    var shoebox = env.xhrBase;
+    var search = env.xhrBaseSearch;
+    var pathParamRe = /(:\w+)/;
+
+    var get = angular.bind(null, http, 'GET');    // caller should pass any path params and then, optionally, a query params object
+    var post = angular.bind(null, http, 'POST');  // caller should pass any path params, optional post data (JSON), and an optional query params object
+    var del = angular.bind(null, http, 'DELETE'); // caller should pass any path params and then, optionally, a query params object
+
+    return {
+      event: post(shoebox, '/events'),
+
+      getKeep: get(shoebox, '/keeps/:id'),
+      addKeepsToLibrary: post(shoebox, '/libraries/:id/keeps'),
+      copyKeepsToLibrary: post(shoebox, '/libraries/copy'),
+      moveKeepsToLibrary: post(shoebox, '/libraries/move'),
+      removeKeepFromLibrary: del(shoebox, '/libraries/:id/keeps/:keepId'),
+      removeManyKeepsFromLibrary: post(shoebox, '/libraries/:id/keeps/delete'),
+
+      search: {
+        search: get(search, '/search'),
+        searched: post(search, '/search/events/searched'),
+        resultClicked: post(search, '/search/events/resultClicked')
+      }
+    };
+
+    function http(method, base, pathSpec) {
+      var pathParts = pathSpec.split(pathParamRe);
+      return function () {
+        var path = constructPath(pathParts, arguments);
+        var i = (pathParts.length - 1) / 2;
+        var data = method === 'POST' ? arguments[i++] : undefined;
+        var params = arguments[i++];
+        return $http({
+          method: method,
+          url: base + path,
+          params: params,
+          data: data
+        });
+      };
+    }
+
+    function constructPath(pathParts, args) {
+      if (pathParts.length === 1) {
+        return pathParts[0];
+      }
+      var parts = pathParts.slice();
+      for (var i = 1; i < pathParts.length; i += 2) {
+        parts[i] = args[(i - 1) / 2];
+      }
+      return parts.join('');
+    }
+  }
+]);
---
+++ kifi-backend/modules/shoebox/angular/test/unit/common/services/net.spec.js
@@ -0,0 +1,54 @@
+'use strict';
+
+describe('net', function () {
+
+  beforeEach(module('kifi'));
+  beforeEach(module(function ($provide) {
+    $provide.value('$http', function (o) { return o; });
+  }));
+
+  var net, env;
+  beforeEach(inject(function (_net_, _env_) {
+    net = _net_;
+    env = _env_;
+  }));
+
+  describe('net.event', function () {
+    it('issues a POST request', function () {
+      var eventData = {type: 'foo', a: 0, b: true};
+      expect(net.event(eventData)).toEqual({
+        method: 'POST',
+        url: env.xhrBase + '/events',
+        params: undefined,
+        data: eventData
+      });
+    });
+  });
+
+  describe('net.removeKeepFromLibrary', function () {
+    it('issues a DELETE request', function () {
+      var libraryId = 'l5B4wZ0GoRWb';
+      var keepId = '8bcca8c5-d1a7-47d0-9f69-f8f58305779f';
+      expect(net.removeKeepFromLibrary(libraryId, keepId)).toEqual({
+        method: 'DELETE',
+        url: env.xhrBase + '/libraries/' + libraryId + '/keeps/' + keepId,
+        params: undefined,
+        data: undefined
+      });
+    });
+  });
+
+  describe('net.search.search', function () {
+    it('issues a GET request', function () {
+      // Note: Angular omits a parameter from the query string if its value is [] (an empty array)
+      var params = {q: 'dogfood', l: [], maxUsers: 3, maxLibraries: 3, maxUris: 3, is: '88x72'};
+      expect(net.search.search(params)).toEqual({
+        method: 'GET',
+        url: env.xhrBaseSearch + '/search',
+        params: params,
+        data: undefined
+      });
+    });
+  });
+
+});
