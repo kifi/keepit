2c55e9de8bf695e50920c845a44c65bf3cab0e21
Andrew Conner <github@andrewconner.org>
2015-05-20 10:09:27 -0700

    Revert "Removing pass phrase UI (todo: business logic). Adding index to email_opt_out table."

--- kifi-backend/modules/shoebox/angular/src/libraries/libraryPassPhrase.js
+++ kifi-backend/modules/shoebox/angular/src/libraries/libraryPassPhrase.js
@@ -3,11 +3,53 @@
 angular.module('kifi')
 
 .directive('kfLibraryPassPhrase', [
-  function () {
+  '$rootScope', '$state', '$timeout', 'libraryService',
+  function ($rootScope, $state, $timeout, libraryService) {
     return {
       restrict: 'A',
       templateUrl: 'libraries/libraryPassPhrase.tpl.html',
-      link: function () { }
+      scope: {
+        errorParams: '='
+      },
+      link: function (scope, element) {
+
+        function reloadThisLibrary() {
+          $state.transitionTo('library.keeps', scope.errorParams, {reload: true, inherit: false, notify: true});
+        }
+
+        //
+        // Scope methods.
+        //
+        scope.submitPassPhrase = function () {
+          var params = scope.errorParams;
+          libraryService.authIntoLibrary(params.username, params.librarySlug, params.authToken, scope.passphrase.value.toLowerCase())
+          .then(reloadThisLibrary)
+          ['catch'](function () {
+            scope.showErrorMessage = true;
+            $timeout(function () {
+              scope.showErrorMessage = false;
+            }, 4200);
+          });
+        };
+
+
+        //
+        // Watches and listeners.
+        //
+        var deregister = $rootScope.$on('userLoggedInStateChange', function (e, loggedIn) {
+          if (!loggedIn) {
+            reloadThisLibrary();
+          }
+        });
+        scope.$on('$destroy', deregister);
+
+
+        //
+        // Initialize.
+        //
+
+        element.find('input').first().focus();
+      }
     };
   }
 ]);
