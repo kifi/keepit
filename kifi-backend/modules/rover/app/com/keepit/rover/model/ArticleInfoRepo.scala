d076f84346c0478a9cb0dc29d1ea938bdb5d7774
Léo Grimaldi <leo@42go.com>
2015-05-22 08:22:32 -0700

    Do not fix bad hashes

--- kifi-backend/modules/rover/app/com/keepit/rover/model/ArticleInfoRepo.scala
+++ kifi-backend/modules/rover/app/com/keepit/rover/model/ArticleInfoRepo.scala
@@ -168,17 +168,14 @@ class ArticleInfoRepoImpl @Inject() (
       val existingByKind: Map[ArticleKind[_ <: Article], RoverArticleInfo] = getByUri(uriId, excludeState = None).map { info => (info.articleKind -> info) }.toMap
       kinds.map { kind =>
         val savedInfo = existingByKind.get(kind) match {
-          case Some(articleInfo) if articleInfo.isActive && articleInfo.url == url => {
-            val validHash = UrlHash.hashUrl(url)
-            if (articleInfo.urlHash != validHash) save(articleInfo.copy(urlHash = validHash)) else articleInfo // todo(Léo): remove when hashes are all valid
-          }
-          case Some(inactiveArticleInfo) if !inactiveArticleInfo.isActive => {
-            val reactivatedInfo = inactiveArticleInfo.clean.copy(url = url, urlHash = UrlHash.hashUrl(url), state = ArticleInfoStates.ACTIVE).initializeSchedulingPolicy
+          case Some(articleInfo) if articleInfo.isActive && articleInfo.url == url && articleInfo.urlHash == UrlHash.hashUrl(url) => articleInfo
+          case Some(inactiveArticleInfo) if !inactiveArticleInfo.isActive && inactiveArticleInfo.url == url && inactiveArticleInfo.urlHash == UrlHash.hashUrl(url) => {
+            val reactivatedInfo = inactiveArticleInfo.clean.copy(state = ArticleInfoStates.ACTIVE).initializeSchedulingPolicy
             save(reactivatedInfo)
           }
           case invalidArticleInfoOpt => {
             invalidArticleInfoOpt.foreach { invalidArticleInfo =>
-              airbrake.notify(s"Found ArticleInfo $kind for uri $uriId with inconsistent url: expected $url, has ${invalidArticleInfo.url}")
+              airbrake.notify(s"Found ArticleInfo $kind for uri $uriId with inconsistent url: expected $url, has ${invalidArticleInfo.url} with hash ${invalidArticleInfo.urlHash}")
               // to be deleted, we got a bad uriId from Shoebox, wipe out the urlHash not to break the table unique constraints
               save(invalidArticleInfo.copy(uriId = Id(-invalidArticleInfo.uriId.id), urlHash = UrlHash.hashUrl(ExternalId().id), state = ArticleInfoStates.INACTIVE))
             }
