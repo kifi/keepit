f6bd84ba3b2a0d8492a7ca66abc695578310685c
Cam Hashemi <camhashemi@gmail.com>
2015-05-29 15:14:35 -0700

    grat emails still not sending, increasing frequency

--- kifi-backend/modules/shoebox/app/com/keepit/shoebox/cron/GratificationEmailCron.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/shoebox/cron/GratificationEmailCron.scala
@@ -28,7 +28,7 @@ class GratificationEmailCronPluginImpl @Inject() (
     val utcHourForNoonEasternTime = 12 + -offsetHoursToUtc
     val utcHourFor8pmEasternTime = 8 + -offsetHoursToUtc
 
-    val cronTimeEveryday = s"0 0 $utcHourForNoonEasternTime-$utcHourFor8pmEasternTime ? * *" // scheduled to send to QA
+    val cronTimeEveryday = s"0 0/15 $utcHourForNoonEasternTime-$utcHourFor8pmEasternTime ? * *" // scheduled to send to QA
     cronTaskOnLeader(quartz, actor.ref, cronTimeEveryday, GratificationEmailMessage.SendEmails)
 
     //    val cronTimeFriday = s"0 0 $utcHourForNoonEasternTime ? * FRI"
