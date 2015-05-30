9d997169ef84d61781e4a91ce4799f322ae55624
Colin Lane <Colin-Lane@comcast.net>
2015-05-29 17:26:52 -0700

    Organization Member Repo
    - With A Passing Test

---
+++ kifi-backend/modules/shoebox/test/com/keepit/model/OrganizationMembershipRepoTest.scala
@@ -0,0 +1,23 @@
+package com.keepit.model
+
+import com.keepit.common.db.Id
+import com.keepit.test.ShoeboxTestInjector
+import org.specs2.mutable.Specification
+
+class OrganizationMembershipRepoTest extends Specification with ShoeboxTestInjector {
+
+  "Organization Repo" should {
+    "save organizations and get them by id" in {
+      withDb() { implicit injector =>
+        val orgMemberRepo = inject[OrganizationMembershipRepo]
+        val org = db.readWrite { implicit s =>
+          orgMemberRepo.save(OrganizationMembership(organizationId = Id[Organization](1), userId = Id[User](1), access = OrganizationAccess.OWNER))
+        }
+
+        db.readOnlyMaster { implicit s =>
+          orgMemberRepo.get(org.id.get) === org
+        }
+      }
+    }
+  }
+}
