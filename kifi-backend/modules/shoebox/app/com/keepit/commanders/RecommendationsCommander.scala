de42da1e993739092caa59a861224212410f63c1
Ryan Brewster <ryan@kifi.com>
2015-06-03 16:43:24 -0700

    Changed the way that updated libraries are recommeded to the user. Previously we simply presented the most recent X keeps from libraries that the user follows. This meant that one library with many keeps could drown out other libraries. Now we impose a limit on the number of keeps recommended from any single library (5, for now).

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/RecommendationsCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/RecommendationsCommander.scala
@@ -138,12 +138,21 @@ class RecommendationsCommander @Inject() (
     }
   }
 
+  def sampleFairly[T](seqOfSeqs: Seq[Seq[T]], maxPerSeq: Int): Seq[T] = {
+    seqOfSeqs.flatMap(_.take(maxPerSeq))
+  }
   def maybeUpdatesFromFollowedLibraries(userId: Id[User]): Future[Option[FullLibUpdatesRecoInfo]] = {
+    val maxUpdates = 20
+    val maxPerLibrary = 5
     val keepsOpt: Option[Seq[Keep]] = db.readWrite { implicit session =>
       val lastSeen = userValueRepo.getValue(userId, UserValues.libraryUpdatesLastSeen)
       if (lastSeen.isBefore(currentDateTime.minusHours(12))) {
         userValueRepo.setValue(userId, UserValueName.UPDATED_LIBRARIES_LAST_SEEN, currentDateTime)
-        Some(keepRepo.getRecentKeepsFromFollowedLibraries(userId, 20))
+        val recentlyUpdatedKeeps = keepRepo.getRecentKeepsFromFollowedLibraries(userId)
+        val keepsByLibrary = recentlyUpdatedKeeps.groupBy(_.libraryId).values.toSeq
+        val fairlySampledKeeps = sampleFairly(keepsByLibrary, maxPerLibrary)
+        val result = fairlySampledKeeps.sortBy(_.keptAt).reverse.take(maxUpdates)
+        Some(result)
       } else {
         None
       }
--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/RecommendationsController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/RecommendationsController.scala
@@ -73,11 +73,26 @@ trait RecommendationControllerHelper extends Logging {
   // hack, make sure Danny is happy.
   private val specialBoostSet = Set(46862, 36680, 49078, 24103, 47498, 51798, 47889, 47191, 47494, 48661, 49090, 50874, 49135, 26460, 27238, 25168, 50812, 47488, 42651, 27760, 25368, 44475, 24203, 50862, 47284, 25000, 27545, 51496, 27049, 26465).map { Id[Library](_) }
 
+  /**
+   * Takes in a list of URI recommendations and a list of Library recommendations
+   * "Mixes" them together (i.e. a random shuffle), with one slight complexity.
+   *
+   * Partitions the libraries based on whether or not they're in the specialBoostSet
+   * Those that are ARE in the specialBoostSet are placed at the front of the list
+   * The rest of the libraries are mixed together with the URIs and placed at the end
+   */
   def mix(uris: Seq[FullUriRecoInfo], libs: Seq[(Id[Library], FullLibRecoInfo)]): Seq[FullRecoInfo] = {
     val (lucky, rest) = libs.partition { case (id, _) => specialBoostSet.contains(id) }
     util.Random.shuffle(lucky.map { _._2 }) ++ util.Random.shuffle(uris ++ rest.map(_._2))
   }
 
+  /**
+   * Takes in a user and figures out how many libraries we should be recommending to them.
+   *
+   * Accounts for the amount of time the user has been around, and how many libraries they
+   * have started/followed. If the user is "experienced", we recommend fewer libraries. If
+   * the user is "new", we recommend a lot of libraries.
+   */
   def libraryRecoCount(userId: Id[User]): Int = {
     val alpha = {
       val user = db.readOnlyMaster { implicit s => userRepo.get(userId) }
@@ -86,14 +101,16 @@ trait RecommendationControllerHelper extends Logging {
       ratio min 1f
     }
     val timeBased = ((1 - alpha) * 10 + alpha * 2).toInt
+    // 10 - 8*min(1, days/14)
 
     val beta = {
       val cnts = db.readOnlyMaster { implicit s => libMemRepo.countsWithUserIdAndAccesses(userId, Set(LibraryAccess.OWNER, LibraryAccess.READ_ONLY)) }
-      val cntsSum = cnts.values.foldLeft(0)(_ + _)
+      val cntsSum = cnts.values.sum
       val ratio = cntsSum * 1f / 50
       ratio min 1f
     }
     val libBased = ((1 - beta) * 10 + beta * 2).toInt
+    // 10 - 8*min(1, libs/50)
 
     log.info(s"smart lib reco number: timeBased = $timeBased, libBased = $libBased")
 
