66b8ba8d9836aaabaa5e2b5d2e406edaf7365fc2
Ryan Brewster <ryan@kifi.com>
2015-06-04 11:46:37 -0700

    Added a test for RecommendationsCommander.maybeUpdatesFromFollowedLibraries and fixed a test that had broken because of the change to KeepRepo.getRecentKeepsFromFollowedLibraries

--- kifi-backend/modules/shoebox/test/com/keepit/model/KeepRepoTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/model/KeepRepoTest.scala
@@ -59,7 +59,7 @@ class KeepRepoTest extends Specification with ShoeboxTestInjector {
         db.readWrite { implicit s =>
           val user1 = user().saved
           inject[LibraryMembershipRepo]
-          inject[KeepRepo].getRecentKeepsFromFollowedLibraries(user1.id.get, 10)
+          inject[KeepRepo].getRecentKeepsFromFollowedLibraries(user1.id.get)
           1 === 1
         }
       }
