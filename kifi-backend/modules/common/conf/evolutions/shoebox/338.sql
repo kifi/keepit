b891aeb4ebdf9353ebba82e0aeff848f9ab3d421
Léo Grimaldi <leo@42go.com>
2015-06-05 16:33:01 -0700

    Fix evolutions

--- kifi-backend/modules/common/conf/evolutions/shoebox/338.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/338.sql
@@ -3,13 +3,13 @@
 # --- !Ups
 
 ALTER TABLE handle_ownership
-	ADD CONSTRAINT `handle_ownership_f_user` FOREIGN KEY (`user_id`) REFERENCES user(`id`)
+	ADD CONSTRAINT `handle_ownership_f_user` FOREIGN KEY (`user_id`) REFERENCES user(`id`);
 
 ALTER TABLE user
-   ADD CONSTRAINT user_f_handle_ownership FOREIGN KEY (normalized_username, id) REFERENCES handle_ownership(handle, user_id);
+   ADD CONSTRAINT `user_f_handle_ownership` FOREIGN KEY (`normalized_username`, `id`) REFERENCES handle_ownership(`handle`, `user_id`);
 
 ALTER TABLE handle_ownership
-	ADD CONSTRAINT `handle_ownership_f_organization` FOREIGN KEY (`organization_id`) REFERENCES organization(`id`)
+	ADD CONSTRAINT `handle_ownership_f_organization` FOREIGN KEY (`organization_id`) REFERENCES organization(`id`);
 
 ALTER TABLE organization
    ADD CONSTRAINT organization_f_handle_ownership FOREIGN KEY (normalized_handle, id) REFERENCES handle_ownership(handle, organization_id);
