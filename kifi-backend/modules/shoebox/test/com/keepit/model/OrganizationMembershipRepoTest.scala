7b569c744120d1515904938fbfeafa643892f0fa
Colin Lane <Colin-Lane@comcast.net>
2015-06-15 13:03:16 -0700

    OrganizationInviteRepo
    - invite repo methods.

--- kifi-backend/modules/shoebox/test/com/keepit/model/OrganizationMembershipRepoTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/model/OrganizationMembershipRepoTest.scala
@@ -4,6 +4,8 @@ import com.keepit.common.db.Id
 import com.keepit.test.ShoeboxTestInjector
 import org.specs2.mutable.Specification
 
+import scala.collection.immutable.IndexedSeq
+
 class OrganizationMembershipRepoTest extends Specification with ShoeboxTestInjector {
 
   "Organization Member Repo" should {
@@ -38,6 +40,59 @@ class OrganizationMembershipRepoTest extends Specification with ShoeboxTestInjec
           orgMemberRepo.getByOrgIdAndUserId(organizationId, userId) must equalTo(Some(activeMember))
           orgMemberRepo.getByOrgIdAndUserId(organizationId, userId, excludeState = Some(OrganizationMembershipStates.ACTIVE)) must beSome(inactiveMember)
         }
+      }
+    }
+
+    "get by user id" in {
+      withDb() { implicit injector =>
+        val orgMemberRepo = inject[OrganizationMembershipRepo]
+        val userId = Id[User](1)
+        val orgIds: IndexedSeq[Id[Organization]] = 5 to 15 map (Id[Organization](_))
+        db.readWrite { implicit session =>
+          orgMemberRepo.save(OrganizationMembership(organizationId = Id(1), access = OrganizationAccess.READ_WRITE, userId = Id[User](2)))
+          for { orgId <- orgIds } {
+            orgMemberRepo.save(OrganizationMembership(organizationId = orgId, access = OrganizationAccess.READ_WRITE, userId = userId))
+          }
+        }
+
+        val memberships = db.readOnlyMaster { implicit session => orgMemberRepo.getByUserId(userId) }
+        memberships.length === orgIds.length
+        memberships.map(_.organizationId).diff(orgIds) === List.empty[Id[Organization]]
+      }
+    }
+
+    "get by org id and userIds" in {
+      withDb() { implicit injector =>
+        val orgMemberRepo = inject[OrganizationMembershipRepo]
+        val orgId = Id[Organization](1)
+        val otherOrgId = Id[Organization](5)
+        val userIds: IndexedSeq[Id[User]] = 5 to 10 map (Id[User](_))
+        db.readWrite { implicit session =>
+          for (userId <- userIds) {
+            orgMemberRepo.save(OrganizationMembership(organizationId = otherOrgId, access = OrganizationAccess.READ_WRITE, userId = userId))
+            orgMemberRepo.save(OrganizationMembership(organizationId = orgId, access = OrganizationAccess.READ_WRITE, userId = userId))
+          }
+        }
+
+        val getUserIds = Set(Id[User](5), Id[User](6), Id[User](7))
+        val memberships = db.readOnlyMaster { implicit session => orgMemberRepo.getByOrgIdAndUserIds(orgId, getUserIds) }
+        memberships.length === getUserIds.size
+        memberships.map(_.userId).diff(getUserIds.toSeq) === List.empty[Id[Organization]]
+      }
+    }
+
+    "deactivate" in {
+      withDb() { implicit injector =>
+        val orgMemberRepo = inject[OrganizationMembershipRepo]
+        val orgId = Id[Organization](1)
+        val userId = Id[User](1)
+        val membership = db.readWrite { implicit session =>
+          orgMemberRepo.save(OrganizationMembership(organizationId = orgId, access = OrganizationAccess.READ_WRITE, userId = userId))
+        }
+
+        val deactivate = db.readWrite { implicit session => orgMemberRepo.deactivate(membership.id.get) }
+        membership.state === OrganizationMembershipStates.ACTIVE
+        deactivate.state === OrganizationMembershipStates.INACTIVE
       }
     }
   }
