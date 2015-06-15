f8bf36480a627669a5de28fc4140d378b7557e3f
Ryan Brewster <ryan@kifi.com>
2015-06-15 13:32:25 -0700

    Fixed OrganizationInviteCommander incompatibility

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/OrganizationInviteCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/OrganizationInviteCommander.scala
@@ -11,12 +11,12 @@ import scala.concurrent.Future
 
 class OrganizationInviteCommander @Inject() () {
 
-  def inviteUsersToOrganization(orgId: Id[Organization], inviterId: Id[User], invitees: Seq[(Either[Id[User], EmailAddress], LibraryAccess, Option[String])]): Future[Either[LibraryFail, Seq[(Either[BasicUser, RichContact], OrganizationAccess)]]] = ???
+  def inviteUsersToOrganization(orgId: Id[Organization], inviterId: Id[User], invitees: Seq[(Either[Id[User], EmailAddress], OrganizationRole, Option[String])]): Future[Either[OrganizationFail, Seq[(Either[BasicUser, RichContact], OrganizationRole)]]] = ???
 
   def acceptInvitation(orgId: Id[Organization], userId: Id[User], authToken: Option[String] = None): Either[OrganizationFail, (Organization, OrganizationMembership)] = ???
 
   def declineInvitation(orgId: Id[Organization], userId: Id[User]) = ???
 
   // Creates a Universal Invite Link for an organization and inviter. Anyone with the link can join the Organization
-  def universalInviteLink(orgId: Id[Organization], inviterId: Id[User], access: OrganizationAccess = OrganizationAccess.READ_WRITE, authToken: Option[String] = None) = ???
+  def universalInviteLink(orgId: Id[Organization], inviterId: Id[User], role: OrganizationRole = OrganizationRole.MEMBER, authToken: Option[String] = None) = ???
 }
