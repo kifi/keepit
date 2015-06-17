34db644455f302fe0851b7a21d2cd13a5b9161e0
Colin Lane <Colin-Lane@comcast.net>
2015-06-17 13:05:15 -0700

    using orgMemberCommander for modifying membership

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/OrganizationInviteCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/OrganizationInviteCommander.scala
@@ -34,6 +34,7 @@ trait OrganizationInviteCommander {
 class OrganizationInviteCommanderImpl @Inject() (db: Database,
     airbrake: AirbrakeNotifier,
     organizationRepo: OrganizationRepo,
+    organizationMembershipCommander: OrganizationMembershipCommander,
     organizationMembershipRepo: OrganizationMembershipRepo,
     organizationInviteRepo: OrganizationInviteRepo,
     basicUserRepo: BasicUserRepo,
@@ -79,9 +80,9 @@ class OrganizationInviteCommanderImpl @Inject() (db: Database,
               case OrganizationMemberInvitation(Left(inviteeId), inviteRole, msgOpt) =>
                 organizationMembersMap.get(inviteeId) match {
                   case Some(inviteeMember) if (inviteeMember.role < inviteRole && inviteRole != OrganizationRole.OWNER) => // member needs upgrade
-                    db.readWrite { implicit session =>
-                      organizationMembershipRepo.save(inviteeMember.withRole(inviteRole))
-                    }
+                    val modifyRequest = OrganizationMembershipModifyRequest(orgId = orgId, requesterId = inviterId, targetId = inviteeId, newRole = inviteRole)
+                    organizationMembershipCommander.modifyMembership(modifyRequest)
+
                     val orgInvite = OrganizationInvite(organizationId = orgId, inviterId = inviterId, userId = Some(inviteeId), role = inviteRole, message = msgOpt)
                     val inviteeInfo = (Left(contactsByUserId(inviteeId)), inviteRole)
                     Some((orgInvite, inviteeInfo))
