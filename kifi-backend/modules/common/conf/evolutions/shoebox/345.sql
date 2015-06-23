3f58ec2d4792244b4d0059fece3bcc09c0a05834
Cam Hashemi <camhashemi@gmail.com>
2015-06-22 19:39:54 -0700

    fixed evolutions typo

--- kifi-backend/modules/common/conf/evolutions/shoebox/345.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/345.sql
@@ -13,6 +13,6 @@ CREATE SEQUENCE ip_address_sequence;
 ALTER TABLE user_ip_addresses ADD seq INT NOT NULL DEFAULT 0;
 CREATE INDEX ip_address_seq_index ON user_ip_addresses(seq);
 
-insert into evolutions (name, description) values('344.sql', 'add sequence number to user_ip_addresses');
+insert into evolutions (name, description) values('345.sql', 'add sequence number to user_ip_addresses');
 
 # --- !Downs
