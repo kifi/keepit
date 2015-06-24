afc37e12470bfc1010fe50856c4919285d148bbf
Adam Kochanowicz <ajkochanowicz@gmail.com>
2015-06-24 14:21:24 -0400

    Implementing profile widget in right rail

--- kifi-backend/modules/shoebox/angular/src/profile/profileWidget.js
+++ kifi-backend/modules/shoebox/angular/src/profile/profileWidget.js
@@ -2,16 +2,38 @@
 
 angular.module('kifi')
 
-.factory('profileWidget', [
-  // '$analytics', '$http', '$location', '$q', '$timeout', 'env', 'Clutch', 'routeService',
-  'profileService',
-  function (profileService) {
+.directive('kfProfileWidget', [
+  '$analytics', 'profileService', 'modalService',
+  function ($analytics, profileService, modalService) {
     return {
       replace: true,
       restrict: 'A',
       templateUrl: 'profile/profileWidget.tpl.html',
       link: function (scope) {
         scope.me = profileService.me;
+
+        scope.registerEvent = function(action) {
+          $analytics.eventTrack('user_clicked_page', {
+            'action': 'clickedProfile' + action,
+            'type': 'yourKeeps'
+          });
+        };
+
+        scope.bioClick = function() {
+          if (typeof(scope.me.biography) === 'undefined') {
+            this.registerEvent('AddBio');
+
+            modalService.open({
+              template: 'profile/editUserBiographyModal.tpl.html',
+              modalData: {
+                biography: '',
+                onClose: function (newBiography) {
+                  scope.me.biography = newBiography;
+                }
+              }
+            });
+          }
+        };
       }
     };
   }
