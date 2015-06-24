f40c1cfb40d0f33bb2d311d43da3eac9f9266755
Cam Hashemi <camhashemi@gmail.com>
2015-06-24 15:46:50 -0700

    add evolutions for org/orgmembership sequence

---
+++ kifi-backend/modules/common/conf/evolutions/shoebox/346.sql
@@ -0,0 +1,21 @@
+# SHOEBOX
+
+# --- !Ups
+
+-- MySQL:
+-- CREATE TABLE organization_sequence (id BIGINT NOT NULL);
+-- INSERT organization_sequence SET id = 0;
+-- CREATE TABLE organization_membership_sequence (id BIGINT NOT NULL);
+-- INSERT organization_membership_sequence SET id = 0;
+-- H2:
+CREATE SEQUENCE organization_sequence;
+
+CREATE INDEX organization_seq_index ON organization(seq);
+
+CREATE SEQUENCE organization_membership_sequence;
+
+CREATE INDEX organization_membership_seq_index ON organization_membership(seq);
+
+insert into evolutions (name, description) values('346.sql', 'add organization_sequence and organization_membership_sequence');
+
+# --- !Downs
