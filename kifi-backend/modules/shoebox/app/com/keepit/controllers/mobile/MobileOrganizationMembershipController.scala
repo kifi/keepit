ab604ae192812a6ad199fac85705ca9abbb1dd2c
Ryan Brewster <ryan@kifi.com>
2015-06-24 17:09:22 -0700

    Modify/Remove org memberships on the controller
    Also some testing for the controller. Caught one bug with
    getMembersandInvitees in orgMembershipCommander.

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobileOrganizationMembershipController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobileOrganizationMembershipController.scala
@@ -20,23 +20,63 @@ class MobileOrganizationMembershipController @Inject() (
     val userActionsHelper: UserActionsHelper,
     implicit val publicIdConfig: PublicIdConfiguration) extends UserActions with OrganizationAccessActions with ShoeboxServiceController {
 
-  private def sendFailResponse(fail: OrganizationFail) = Status(fail.status)(Json.obj("error" -> fail.message))
-
   // If userIdOpt is provided AND the user can invite members, return invited users as well as members
-  def getMembers(pubId: PublicId[Organization], offset: Int, limit: Int, userIdOpt: Option[Id[User]]) = OrganizationAction(pubId) { request =>
+  def getMembers(pubId: PublicId[Organization], offset: Int, limit: Int) = OrganizationAction(pubId, OrganizationPermission.VIEW_ORGANIZATION) { request =>
     if (limit > 30) {
       BadRequest(Json.obj("error" -> "invalid_limit"))
     } else Organization.decodePublicId(pubId) match {
       case Failure(ex) => BadRequest(Json.obj("error" -> "invalid_organization_id"))
       case Success(orgId) =>
-        val permissions = orgMembershipCommander.getPermissions(orgId, userIdOpt)
-        val showInvitees = permissions.contains(OrganizationPermission.INVITE_MEMBERS)
+        val showInvitees = request.permissions.contains(OrganizationPermission.INVITE_MEMBERS)
+        println("[RPB] user can see invitees? " + showInvitees)
         val membersAndMaybeInvitees = orgMembershipCommander.getMembersAndInvitees(orgId, Limit(limit), Offset(offset), includeInvitees = showInvitees)
         Ok(Json.obj("members" -> membersAndMaybeInvitees))
     }
   }
 
-  def modifyMembers(pubId: PublicId[Organization]) = ???
-  def removeMembers(pubId: PublicId[Organization]) = ???
+  def modifyMembers(pubId: PublicId[Organization]) = UserAction(parse.tolerantJson) { request =>
+    Organization.decodePublicId(pubId) match {
+      case Failure(ex) => BadRequest(Json.obj("error" -> "invalid_organization_id"))
+      case Success(orgId) =>
+        val membersToBeModified = (request.body \ "members").as[JsArray].value
+        val modifyRequests = membersToBeModified map { memberMod =>
+          val targetId = (memberMod \ "userId").as[Id[User]]
+          val newRole = (memberMod \ "role").as[OrganizationRole]
+          OrganizationMembershipModifyRequest(orgId, requesterId = request.userId, targetId = targetId, newRole = newRole)
+        }
+
+        orgMembershipCommander.modifyMemberships(modifyRequests) match {
+          case Left(failure) => failure.asErrorResponse
+          case Right(responses) =>
+            val modifications = responses.keys.map(r => Json.obj("userId" -> r.targetId, "newRole" -> r.newRole))
+            Ok(Json.obj("modifications" -> modifications))
+        }
+    }
+  }
+  def removeMembers(pubId: PublicId[Organization]) = UserAction(parse.tolerantJson) { request =>
+    Organization.decodePublicId(pubId) match {
+      case Failure(ex) => BadRequest(Json.obj("error" -> "invalid_organization_id"))
+      case Success(orgId) =>
+        val membersToBeRemoved = (request.body \ "members").as[Seq[Id[User]]]
+        val removeRequests = for (targetId <- membersToBeRemoved) yield OrganizationMembershipRemoveRequest(orgId, request.userId, targetId)
+
+        orgMembershipCommander.removeMemberships(removeRequests) match {
+          case Left(failure) => failure.asErrorResponse
+          case Right(responses) => Ok(Json.obj("removals" -> responses.keys.map(_.targetId)))
+        }
+    }
+  }
+
+  def leaveOrganization(pubId: PublicId[Organization]) = UserAction { request =>
+    Organization.decodePublicId(pubId) match {
+      case Failure(ex) => BadRequest(Json.obj("error" -> "invalid_id"))
+      case Success(orgId) =>
+        val leaveRequest = OrganizationMembershipRemoveRequest(orgId, request.userId, request.userId)
+        orgMembershipCommander.removeMembership(leaveRequest) match {
+          case Left(fail) => fail.asErrorResponse
+          case Right(response) => Ok(JsString("success"))
+        }
+    }
+  }
 
 }
