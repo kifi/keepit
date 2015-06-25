683a70c4dc311ecb77eb37439eb2b5f8ce196e61
Andrew Conner <git@andrewconner.org>
2015-06-25 07:33:38 -0700

    Fixing laziness, finding more?!

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/KeepDecorator.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/KeepDecorator.scala
@@ -36,7 +36,13 @@ class KeepDecorator @Inject() (
     implicit val publicIdConfig: PublicIdConfiguration) {
 
   def decorateKeepsIntoKeepInfos(perspectiveUserIdOpt: Option[Id[User]], showPublishedLibraries: Boolean, keepsSeq: Seq[Keep], idealImageSize: ImageSize, withKeepTime: Boolean): Future[Seq[KeepInfo]] = {
-    val keeps = keepsSeq.toList // Make sure we're not dealing with a lazy structure here, which doesn't play nice with a database session...
+    val keeps = keepsSeq match {
+      case k: List[Keep] => k
+      case other =>
+        // Make sure we're not dealing with a lazy structure here, which doesn't play nice with a database session...
+        airbrake.notify("[decorateKeepsIntoKeepInfos] Found it! Grab Léo, Yingjie, and Andrew", new Exception())
+        other.toList
+    }
     if (keeps.isEmpty) Future.successful(Seq.empty[KeepInfo])
     else {
       val augmentationFuture = {
