0302faaa515b423a48c333ff30a4cb46364fffdc
Colin Lane <Colin-Lane@comcast.net>
2015-06-26 16:03:59 -0700

    LibraryMembership Modify Request

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryMembershipCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryMembershipCommander.scala
@@ -1,6 +1,6 @@
 package com.keepit.commanders
 
-import com.google.inject.{ImplementedBy, Inject}
+import com.google.inject.{ ImplementedBy, Inject, Singleton }
 import com.keepit.common.db.Id
 import com.keepit.common.db.slick.Database
 import com.keepit.common.logging.Logging
@@ -9,51 +9,69 @@ import play.api.http.Status._
 
 @ImplementedBy(classOf[LibraryMembershipCommanderImpl])
 trait LibraryMembershipCommander {
-  def updateMembership(userId: Id[User], libraryId: Id[Library], request: ModifyLibraryMembershipRequest): Either[LibraryFail, LibraryMembership]
+  def updateMembership(requestorId: Id[User], request: ModifyLibraryMembershipRequest): Either[LibraryFail, LibraryMembership]
 }
 
-case class ModifyLibraryMembershipRequest(subscribedToUpdatesNew: Option[Boolean] = None, isStarred: Option[Boolean] = None)
+case class ModifyLibraryMembershipRequest(userId: Id[User], libraryId: Id[Library],
+  subscription: Option[Boolean] = None,
+  isStarred: Option[Boolean] = None,
+  listed: Option[Boolean] = None,
+  access: Option[LibraryAccess] = None)
 
+@Singleton
 class LibraryMembershipCommanderImpl @Inject() (
     db: Database,
     libraryMembershipRepo: LibraryMembershipRepo) extends LibraryMembershipCommander with Logging {
 
-  type LibraryModification = (LibraryMembership) => LibraryMembership
-
-  def updateMembership(userId: Id[User], libraryId: Id[Library], request: ModifyLibraryMembershipRequest): Either[LibraryFail, LibraryMembership] = {
-    val libMembership = db.readOnlyMaster { implicit s =>
-      libraryMembershipRepo.getWithLibraryIdAndUserId(libraryId, userId)
+  def updateMembership(requestorId: Id[User], request: ModifyLibraryMembershipRequest): Either[LibraryFail, LibraryMembership] = {
+    val (requestorMembership, targetMembershipOpt) = db.readOnlyMaster { implicit s =>
+      val requestorMembership = libraryMembershipRepo.getWithLibraryIdAndUserId(request.libraryId, requestorId)
+      val targetMembershipOpt = libraryMembershipRepo.getWithLibraryIdAndUserId(request.libraryId, request.userId)
+      (requestorMembership, targetMembershipOpt)
     }
-    val modifications: LibraryModification = request.isStarred match {
-      case Some(starred) => (m) => m.copy(starred = starred)
-      case None => (m) => m
-    }
-    libMembership match {
-      case None => Left(LibraryFail(NOT_FOUND, "not_a_member"))
-      case Some(mem) =>
-        val modifiedLib = modifications(mem)
-        if (mem != modifiedLib) {
-          Right(db.readWrite { implicit session =>
-            libraryMembershipRepo.save(modifiedLib)
-          })
-        } else {
-          Right(mem)
+    (for {
+      requester <- requestorMembership
+      targetMembership <- targetMembershipOpt
+    } yield {
+      def modifySelfCheck[T](valueOpt: Option[T], fallback: => T): Either[LibraryFail, T] = {
+        valueOpt match {
+          case Some(value) =>
+            if (requestorId == targetMembership.userId) {
+              Right(value)
+            } else {
+              Left(LibraryFail(BAD_REQUEST, "permission_denied"))
+            }
+          case None => Right(fallback)
         }
-    }
-  }
-
-  private def updateStarredLibrary(userId: Id[User], libraryId: Id[Library], isStarred: Boolean): Either[LibraryFail, LibraryMembership] = {
-    db.readOnlyMaster { implicit s =>
-      libraryMembershipRepo.getWithLibraryIdAndUserId(libraryId, userId)
-    } match {
-      case None => Left(LibraryFail(NOT_FOUND, "need_to_follow_to_star"))
-      case Some(mem) if mem.starred == isStarred => Right(mem)
-      case Some(mem) => {
-        val updatedMembership = db.readWrite { implicit s =>
-          libraryMembershipRepo.save(mem.copy(starred = isStarred))
-        }
-        Right(updatedMembership)
       }
-    }
+
+      def canChangeAccess(accessOpt: Option[LibraryAccess]) = {
+        accessOpt match {
+          case Some(access) =>
+            access match {
+              case LibraryAccess.READ_WRITE | LibraryAccess.READ_ONLY if (requester.isOwner && targetMembership.access != LibraryAccess.OWNER) =>
+                Right(access)
+              case _ => Left(LibraryFail(BAD_REQUEST, "permission_denied"))
+            }
+          case None => Right(targetMembership.access)
+        }
+      }
+
+      for {
+        starred <- modifySelfCheck(request.isStarred, targetMembership.starred).right
+        subscribed <- modifySelfCheck(request.subscription, targetMembership.subscribedToUpdates).right
+        isListed <- modifySelfCheck(request.listed, targetMembership.listed).right
+        access <- canChangeAccess(request.access).right
+      } yield {
+        val modifiedLib = targetMembership.copy(starred = starred, subscribedToUpdates = subscribed, listed = isListed, access = access)
+        if (targetMembership != modifiedLib) {
+          db.readWrite { implicit session =>
+            libraryMembershipRepo.save(modifiedLib)
+          }
+        } else {
+          targetMembership
+        }
+      }
+    }).getOrElse(Left(LibraryFail(BAD_REQUEST, "permission_denied")))
   }
 }
