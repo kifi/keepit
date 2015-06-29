1815a55e9ca4895dae5f86e50085abe67388d23a
Colin Lane <Colin-Lane@comcast.net>
2015-06-29 10:04:40 -0700

    evolution update
    - using a archer instead of boolean

--- kifi-backend/modules/common/conf/evolutions/shoebox/349.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/349.sql
@@ -2,8 +2,8 @@
 
 # --- !Ups
 
-ALTER TABLE `library_membership` ADD COLUMN starred TINYINT default 0;
+ALTER TABLE `library_membership` ADD COLUMN starred varchar(16) default 0;
 
-insert into evolutions (name, description) values('347.sql', 'add starred to library_membership');
+insert into evolutions (name, description) values('349.sql', 'add starred to library_membership');
 
 # --- !Downs
