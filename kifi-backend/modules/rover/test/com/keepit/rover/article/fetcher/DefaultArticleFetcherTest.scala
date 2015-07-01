100eb04f51774248bd62f47e247cec33b25858ce
gratimax <max@ovsankin.com>
2015-07-01 16:55:02 -0700

    Transfer url decoding test to NormalizationCandidateSanitizerTest

--- kifi-backend/modules/rover/test/com/keepit/rover/article/fetcher/DefaultArticleFetcherTest.scala
+++ kifi-backend/modules/rover/test/com/keepit/rover/article/fetcher/DefaultArticleFetcherTest.scala
@@ -55,13 +55,6 @@ class DefaultArticleFetcherTest extends Specification with ArticleFetcherTest[De
       normalization.canonicalUrl === None
     }
 
-    "ignore canonical URLs that are the page URL with parameter values URL-encoded twice" in {
-      val url = "https://soundcloud.com/search?q=%D8%AF%DB%8C%D8%A7%D9%84%D9%88%DA%AF"
-      val scraped = fetch("double-encode.txt")
-      val normalization = scraped.content.normalization
-      normalization.canonicalUrl === None
-    }
-
     "extract keywords" in {
       val scraped = fetch("www.cnn.com.health.txt")
       val normalization = scraped.content.normalization
