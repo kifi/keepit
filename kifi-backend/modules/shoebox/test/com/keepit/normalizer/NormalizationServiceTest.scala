5d2c072cd46a4131ffafb982ff96619bc9a90888
gratimax <max@ovsankin.com>
2015-07-01 23:48:02 -0700

    Remove remaining scraper classes in common

--- kifi-backend/modules/shoebox/test/com/keepit/normalizer/NormalizationServiceTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/normalizer/NormalizationServiceTest.scala
@@ -9,8 +9,7 @@ import com.keepit.inject.FakeFortyTwoModule
 import com.keepit.integrity.UriIntegrityPlugin
 import com.keepit.model._
 import com.keepit.rover.document.utils.Signature
-import com.keepit.scraper.{ BasicArticle, FakeSignatureBuilder }
-import com.keepit.scraper.extractor.ExtractorProviderType
+import com.keepit.scraper.FakeSignatureBuilder
 import com.keepit.shoebox.FakeKeepImportsModule
 import com.keepit.test.ShoeboxTestInjector
 import net.codingwell.scalaguice.ScalaModule
@@ -20,14 +19,6 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 
 class NormalizationServiceTest extends TestKitSupport with SpecificationLike with ShoeboxTestInjector {
-
-  val fakeArticles: PartialFunction[(String, Option[ExtractorProviderType]), BasicArticle] = {
-    case (url @ "http://www.linkedin.com/pub/leonard\u002dgrimaldi/12/42/2b3", Some(_)) => BasicArticle("leonard grimaldi", "whatever", signature = fakeSignature("whatever"), destinationUrl = url)
-    case (url @ "http://www.linkedin.com/pub/leo\u002dgrimaldi/12/42/2b3", Some(_)) => BasicArticle("leo grimaldi", "17558679", signature = fakeSignature("17558679"), destinationUrl = url)
-    case (url @ "http://www.linkedin.com/pub/leo\u002dgrimaldi/12/42/2b3", None) => BasicArticle("leo", "some content", signature = fakeSignature("some content"), destinationUrl = url)
-    case (url @ "http://www.linkedin.com/in/leo", None) => BasicArticle("leo", "some content", signature = fakeSignature("some content"), destinationUrl = url)
-    case (url @ "http://www.linkedin.com/in/viviensaulue", Some(_)) => BasicArticle("vivien", "136123062", signature = fakeSignature("136123062"), destinationUrl = url)
-  }
 
   private def fakeSignature(text: String): Signature = {
     new FakeSignatureBuilder().add(text).build
