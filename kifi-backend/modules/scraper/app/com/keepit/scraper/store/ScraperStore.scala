34f2a5116f238ea0bc35d954adde3871dfd92b71
gratimax <max@ovsankin.com>
2015-07-01 18:10:30 -0700

    Remove UriImageStore and associated classes

--- kifi-backend/modules/scraper/app/com/keepit/scraper/store/ScraperStore.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/store/ScraperStore.scala
@@ -1,7 +1,6 @@
 package com.keepit.common.store
 
 import com.google.inject.{ Provides, Singleton }
-import com.keepit.scraper.store.UriImageStoreInbox
 import org.apache.commons.io.FileUtils
 import play.api.Play._
 
@@ -9,19 +8,10 @@ case class ScraperProdStoreModule() extends ProdStoreModule {
   def configure() {
   }
 
-  @Provides @Singleton
-  def uriImageStoreInbox: UriImageStoreInbox = {
-    val inboxDir = forceMakeTemporaryDirectory(current.configuration.getString("scraper.temporary.directory").get, "uri_images")
-    UriImageStoreInbox(inboxDir)
-  }
 }
 
 case class ScraperDevStoreModule() extends DevStoreModule(ScraperProdStoreModule()) {
   def configure() {
   }
 
-  @Provides @Singleton
-  def uriImageStoreInbox: UriImageStoreInbox = whenConfigured("scraper.temporary.directory")(prodStoreModule.uriImageStoreInbox) getOrElse {
-    UriImageStoreInbox(FileUtils.getTempDirectory)
-  }
 }
