c329cd85783bb000434bced50e72844179c9c12f
gratimax <max@ovsankin.com>
2015-07-01 17:55:58 -0700

    Remove embedly store

--- kifi-backend/modules/scraper/app/com/keepit/scraper/store/ScraperStore.scala
+++ kifi-backend/modules/scraper/app/com/keepit/scraper/store/ScraperStore.scala
@@ -1,13 +1,9 @@
 package com.keepit.common.store
 
 import com.google.inject.{ Provides, Singleton }
-import com.amazonaws.services.s3.{ AmazonS3 }
 import com.keepit.scraper.store.UriImageStoreInbox
 import org.apache.commons.io.FileUtils
 import play.api.Play._
-import com.keepit.common.logging.AccessLog
-import com.google.inject.Provider
-import com.keepit.scraper.embedly._
 
 case class ScraperProdStoreModule() extends ProdStoreModule {
   def configure() {
@@ -18,13 +14,6 @@ case class ScraperProdStoreModule() extends ProdStoreModule {
     val inboxDir = forceMakeTemporaryDirectory(current.configuration.getString("scraper.temporary.directory").get, "uri_images")
     UriImageStoreInbox(inboxDir)
   }
-
-  @Singleton
-  @Provides
-  def embedlyStore(amazonS3Client: AmazonS3, accessLog: AccessLog): EmbedlyStore = {
-    val bucketName = S3Bucket(current.configuration.getString("amazon.s3.embedly.bucket").get)
-    new S3EmbedlyStoreImpl(bucketName, amazonS3Client, accessLog)
-  }
 }
 
 case class ScraperDevStoreModule() extends DevStoreModule(ScraperProdStoreModule()) {
@@ -34,13 +23,5 @@ case class ScraperDevStoreModule() extends DevStoreModule(ScraperProdStoreModule
   @Provides @Singleton
   def uriImageStoreInbox: UriImageStoreInbox = whenConfigured("scraper.temporary.directory")(prodStoreModule.uriImageStoreInbox) getOrElse {
     UriImageStoreInbox(FileUtils.getTempDirectory)
-  }
-
-  @Singleton
-  @Provides
-  def embedlyStore(amazonS3ClientProvider: Provider[AmazonS3], accessLog: AccessLog): EmbedlyStore = {
-    whenConfigured("amazon.s3.embedly.bucket")(
-      prodStoreModule.embedlyStore(amazonS3ClientProvider.get, accessLog)
-    ).getOrElse(new InMemoryEmbedlyStoreImpl())
   }
 }
