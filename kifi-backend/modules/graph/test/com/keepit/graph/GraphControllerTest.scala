95e81f5c6cb97dd0328009a39ee82f5a14ae665a
Cam Hashemi <camhashemi@gmail.com>
2015-07-06 16:26:28 -0700

    added test for GraphController.getSociallyRelatedEntitiesForOrg

--- kifi-backend/modules/graph/test/com/keepit/graph/GraphControllerTest.scala
+++ kifi-backend/modules/graph/test/com/keepit/graph/GraphControllerTest.scala
@@ -2,7 +2,7 @@ package com.keepit.graph
 
 import com.keepit.graph.controllers.internal.GraphController
 import com.keepit.graph.manager.GraphManager
-import com.keepit.graph.model.{ SociallyRelatedEntitiesForUser }
+import com.keepit.graph.model.{ SociallyRelatedEntitiesForOrg, SociallyRelatedEntitiesForUser }
 import com.keepit.graph.simple.SimpleGraphTestModule
 import com.keepit.graph.test.{ GraphTestInjector }
 import com.keepit.model.User
@@ -46,7 +46,7 @@ class GraphControllerTest extends Specification with GraphTestInjector with Grap
         content !== null
       }
 
-      "get socially related entities" in {
+      "get socially related entities for users" in {
         withInjector(modules: _*) { implicit injector =>
           val route = com.keepit.graph.controllers.internal.routes.GraphController.getSociallyRelatedEntitiesForUser(u42).url
           route === "/internal/graph/getSociallyRelatedEntitiesForUser?userId=42"
@@ -63,6 +63,27 @@ class GraphControllerTest extends Specification with GraphTestInjector with Grap
           jsResult.get.linkedInAccounts.id == u42
           jsResult.get.emailAccounts.id == u42
           jsResult.get.users.id == u42
+          jsResult.get.users.related.size === 5
+        }
+      }
+
+      "get socially related entities for orgs" in {
+        withInjector(modules: _*) { implicit injector =>
+          val route = com.keepit.graph.controllers.internal.routes.GraphController.getSociallyRelatedEntitiesForOrg(orgId1).url
+          route === "/internal/graph/getSociallyRelatedEntitiesForOrg?orgId=1"
+          val controller = inject[GraphController] //setup
+          val manager = inject[GraphManager]
+          manager.update(allUpdates: _*)
+          val result = controller.getSociallyRelatedEntitiesForOrg(orgId1)(FakeRequest())
+          status(result) must equalTo(OK)
+          val content = contentAsString(result)
+          content !== null
+
+          println(content)
+
+          val jsResult = Json.fromJson[SociallyRelatedEntitiesForOrg](Json.parse(content))
+          jsResult.get.emailAccounts.id == orgId1
+          jsResult.get.users.id == orgId1
           jsResult.get.users.related.size === 5
         }
       }
