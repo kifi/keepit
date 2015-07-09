e031b6405a303e7851cc6cdf786c5b438d8434f6
gratimax <max@ovsankin.com>
2015-07-09 11:18:24 -0700

    Add test for locating proxies

--- kifi-backend/modules/rover/test/com/keepit/rover/model/RoverUrlRuleTest.scala
+++ kifi-backend/modules/rover/test/com/keepit/rover/model/RoverUrlRuleTest.scala
@@ -1,13 +1,16 @@
 package com.keepit.rover.model
 
+import com.keepit.common.db.Id
 import com.keepit.common.db.slick.Database
-import com.keepit.rover.rule.UrlRuleAction
+import com.keepit.rover.rule.{ RoverHttpProxyCommander, RoverUrlRuleCommander, UrlRuleAction }
 import com.keepit.rover.test.{ RoverTestInjector }
 import org.specs2.mutable.Specification
 
 class RoverUrlRuleTest extends Specification with RoverTestInjector {
 
   "RoverUrlRule" should {
+
+    val proxy1 = RoverHttpProxy(alias = "proxy1", host = "localhost", port = 8080, scheme = ProxyScheme.Http, username = None, password = None)
 
     "persist & use patterns" in {
       withDb() { implicit injector =>
@@ -19,18 +22,41 @@ class RoverUrlRuleTest extends Specification with RoverTestInjector {
 
         val proxyId =
           db.readWrite { implicit session =>
-            httpProxyRepo.save(RoverHttpProxy(alias = "proxy1", host = "localhost", port = 8080, scheme = ProxyScheme.Http, username = None, password = None)).id
+            httpProxyRepo.save(proxy1).id
           }
 
         db.readWrite { implicit session =>
           urlRuleRepo.save(RoverUrlRule(pattern = "^https*://www\\.facebook\\.com/login.*$", proxy = proxyId))
           urlRuleRepo.save(RoverUrlRule(pattern = "^https*://.*.google.com.*/ServiceLogin.*$", proxy = None))
-          urlRuleRepo.save(RoverUrlRule(pattern = "^https*://app.asana.com.*$", proxy = None))
+          urlRuleRepo.save(RoverUrlRule(pattern = "^https*://app.asana.com.*$", proxy = proxyId))
         }
 
         db.readOnlyMaster { implicit session =>
           urlRuleRepo.actionsFor("http://www.google.com/") === List()
           urlRuleRepo.actionsFor("https://www.facebook.com/login.php?bb") === List(UrlRuleAction.UseProxy(proxyId.get))
+        }
+      }
+    }
+
+    "locate proxies for usage" in {
+      withDb() { implicit injector =>
+
+        val httpProxyCommander = inject[RoverHttpProxyCommander]
+        val httpProxyRepo = inject[RoverHttpProxyRepo]
+        val urlRuleCommander = inject[RoverUrlRuleCommander]
+        val urlRuleRepo = inject[RoverUrlRuleRepo]
+
+        val proxy = db.readWrite { implicit session =>
+          val proxy = httpProxyRepo.save(proxy1)
+          urlRuleRepo.save(RoverUrlRule(pattern = "^https*://app.asana.com.*$", proxy = proxy.id))
+          proxy
+        }
+
+        val lightweightProxy = httpProxyCommander.roverHttpProxyToHttpProxy(proxy)
+
+        db.readOnlyMaster { implicit session =>
+          urlRuleCommander.lightweightProxyFor("http://www.google.com/") === None
+          urlRuleCommander.lightweightProxyFor("https://app.asana.com/") === Some(lightweightProxy)
         }
       }
     }
