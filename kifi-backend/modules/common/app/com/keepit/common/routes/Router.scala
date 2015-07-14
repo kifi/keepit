69095d77b1f5949fcf8824729bfc3c5e2bdff7b0
yingjieMiao <yingjie.miao@outlook.com>
2015-07-14 05:28:35 -0700

    Revert "Revert "heimdal cleanup""

--- kifi-backend/modules/common/app/com/keepit/common/routes/Router.scala
+++ kifi-backend/modules/common/app/com/keepit/common/routes/Router.scala
@@ -238,9 +238,6 @@ object Eliza extends Service {
 
 object Heimdal extends Service {
   object internal {
-    def getMetricData(repo: String, name: String) = ServiceRoute(GET, s"/internal/heimdal/$repo/getMetricData", Param("name", name))
-    def updateMetrics() = ServiceRoute(GET, "/internal/heimdal/updateMetrics")
-    def getRawEvents(repo: String, eventTypes: Seq[String], limit: Int, window: Int) = ServiceRoute(GET, s"/internal/heimdal/$repo/rawEvents", Param("events", eventTypes.mkString(",")), Param("limit", limit), Param("window", window))
     def deleteUser(userId: Id[User]) = ServiceRoute(GET, s"/internal/heimdal/user/delete", Param("userId", userId))
     def incrementUserProperties(userId: Id[User]) = ServiceRoute(POST, s"/internal/heimdal/user/increment", Param("userId", userId))
     def setUserProperties(userId: Id[User]) = ServiceRoute(POST, s"/internal/heimdal/user/set", Param("userId", userId))
