bd9c67ba53c28f8752bdd6244993a65ce224de81
gratimax <max@ovsankin.com>
2015-07-14 17:04:26 -0700

    Refactor potential orgs query

--- kifi-backend/modules/shoebox/app/com/keepit/model/OrganizationRepo.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/model/OrganizationRepo.scala
@@ -51,25 +51,6 @@ class OrganizationRepoImpl @Inject() (
 
   initTable()
 
-  implicit val getHandleResult = getResultOptionFromMapper[OrganizationHandle]
-  implicit val getBasePermissionsResult = getResultFromMapper[BasePermissions]
-
-  implicit val tableResult: GetResult[Organization] = GetResult { r: PositionedResult =>
-    Organization.applyFromDbRow(
-      id = r.<<[Option[Id[Organization]]],
-      createdAt = r.<<[DateTime],
-      updatedAt = r.<<[DateTime],
-      state = r.<<[State[Organization]],
-      seq = r.<<[SequenceNumber[Organization]],
-      name = r.<<[String],
-      description = r.<<[Option[String]],
-      ownerId = r.<<[Id[User]],
-      organizationHandle = r.<<[Option[OrganizationHandle]],
-      normalizedOrganizationHandle = r.<<[Option[OrganizationHandle]],
-      basePermissions = r.<<[BasePermissions]
-    )
-  }
-
   override def deleteCache(org: Organization)(implicit session: RSession) {
     orgCache.remove(org.id.get)
   }
@@ -95,21 +76,30 @@ class OrganizationRepoImpl @Inject() (
   def getPotentialOrganizationsForUser(userId: Id[User])(implicit session: RSession): Seq[Organization] = {
     import com.keepit.common.db.slick.StaticQueryFixed.interpolation
 
-    sql"""
-          select organization.* from (
-            select user_ip_addresses.user_id as id from (
-              select distinct ip_address from user_ip_addresses where user_id = $userId
-            ) as ip
-              inner join user_ip_addresses on user_ip_addresses.ip_address = ip.ip_address where user_ip_addresses.user_id != $userId
+    val orgIds = sql"""
+      select distinct organization.id from (
+        select membership.organization_id from (
+          select user_ip_addresses.user_id as id from (
+            select distinct ip_address from user_ip_addresses where user_id = $userId
+          ) as ip
+            inner join user_ip_addresses on user_ip_addresses.ip_address = ip.ip_address
+            where user_ip_addresses.user_id != $userId
+          union
+            select user_2 as id from user_connection where user_connection.user_1 = $userId
+          union
+            select user_1 as id from user_connection where user_connection.user_2 = $userId
+        ) as user
+          inner join (
+            select user_id, organization_id from organization_membership
             union
-              select user_2 as id from user_connection where user_connection.user_1 = $userId
-            union
-              select user_1 as id from user_connection where user_connection.user_2 = $userId
-          ) as user
-            inner join organization_membership on organization_membership.user_id = user.id
-            inner join organization on organization.id = organization_membership.organization_id
-            group by organization.id;
-      """.as[Organization].list
+              select user_id, organization_id from organization_membership_candidate
+          ) as membership
+          on user.id = membership.user_id
+      ) as membership
+        inner join organization on organization.id = membership.organization_id;
+      """.as[Id[Organization]].list.toSet
+
+    getByIds(orgIds).values.toSeq
   }
 
 }
