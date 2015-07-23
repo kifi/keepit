5c1e11f7a38565e3d8c1ad98d133e85bbfea731f
Andrew Conner <git@andrewconner.org>
2015-07-23 11:53:30 -0700

    Allowing version 7 to exist

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/MarketingSiteRouter.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/MarketingSiteRouter.scala
@@ -61,9 +61,10 @@ object MarketingSiteRouter extends AssetsBuilder with Controller with Logging {
   private object Version4 extends LandingVersion { val version = 4 }
   private object Version5 extends LandingVersion { val version = 5 }
   private object Version6 extends LandingVersion { val version = 6 }
+  private object Version7 extends LandingVersion { val version = 7 }
 
-  private val versions = Seq(Version1, Version2, Version3, Version4, Version5, Version6)
-  private val defaultVersion = Version5
+  private val versions = Seq(Version1, Version2, Version3, Version4, Version5, Version6, Version7)
+  private val defaultVersion = Version6
 
   def landing(implicit request: Request[_]): String = {
     val possiblyBot = request.userAgentOpt.map(_.possiblyBot).getOrElse(true)
@@ -73,9 +74,10 @@ object MarketingSiteRouter extends AssetsBuilder with Controller with Logging {
       val pickOpt = Try(request.getQueryString("v").map(_.toInt)).toOption.flatten
 
       pickOpt.flatMap(v => versions.find(_.version == v)).getOrElse {
-        val ip = request.headers.get("X-Forwarded-For").getOrElse(request.remoteAddress)
-        val hash = Math.abs(ip.hashCode) % 100
-        (if (hash < 50) Version5 else Version6) tap { w => log.info(s"[landing] remoteAddr=${request.remoteAddress} ip=$ip winner=$w") }
+//        val ip = request.headers.get("X-Forwarded-For").getOrElse(request.remoteAddress)
+//        val hash = Math.abs(ip.hashCode) % 100
+//        (if (hash < 50) Version5 else Version6) tap { w => log.info(s"[landing] remoteAddr=${request.remoteAddress} ip=$ip winner=$w") }
+        Version6
       }
     }
     s"index.${version.version}"
