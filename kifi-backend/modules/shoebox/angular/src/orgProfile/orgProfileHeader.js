025c710889d7aea25ec4151296f145a45690e06e
Adam Kochanowicz <ajkochanowicz@gmail.com>
2015-07-27 16:39:08 -0700

    Editing attempts show error banner for connectivity problems

--- kifi-backend/modules/shoebox/angular/src/orgProfile/orgProfileHeader.js
+++ kifi-backend/modules/shoebox/angular/src/orgProfile/orgProfileHeader.js
@@ -15,25 +15,20 @@ angular.module('kifi')
     templateUrl: 'orgProfile/orgProfileHeader.tpl.html',
     link: function (scope) {
 
-      scope.editing = false;
-      scope.notification = null;
       var lastSavedInfo = {};
+      scope.notification = null;
+
       scope.readonly = scope.membership.permissions.indexOf('edit_organization') === -1;
       scope.myTextValue = 'Hello';
       scope.acknowledgedInvite = false;
 
-      scope.toggleEditing = function () {
-        scope.editing = !scope.editing;
-        lastSavedInfo = angular.extend(lastSavedInfo, scope.profile);
-      };
-
       scope.undo = function () {
         scope.profile = angular.extend(scope.profile, lastSavedInfo);
-        scope.toggleEditing();
       };
 
       var updateMe = function(data) {
-        angular.extend(scope.profile, data);
+        scope.profile = angular.extend(scope.profile, data);
+        lastSavedInfo = angular.extend(lastSavedInfo, scope.profile);
       };
 
       scope.save = function () {
@@ -58,6 +53,12 @@ angular.module('kifi')
               scope.notification = null;
             }, 1500);
             return updateMe(res.data);
+          })['catch'](function() {
+            scope.notification = 'error';
+            scope.undo();
+            $timeout(function() {
+              scope.notification = null;
+            }, 1500);
           });
       };
 
