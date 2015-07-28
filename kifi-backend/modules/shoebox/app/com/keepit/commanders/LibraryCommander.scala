6c49217a23708ba26f397ac9efa2e96e681f0806
Cam Hashemi <camhashemi@gmail.com>
2015-07-28 14:25:42 -0700

    refactor

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryCommander.scala
@@ -1463,12 +1463,10 @@ class LibraryCommanderImpl @Inject() (
   def getLibraryWithHandleAndSlug(handle: Handle, slug: LibrarySlug, viewerId: Option[Id[User]])(implicit context: HeimdalContext): Either[LibraryFail, Library] = {
     val ownerOpt = db.readOnlyReplica { implicit session => handleCommander.getByHandle(handle) }
 
-    val librarySpaceOpt = ownerOpt match {
-      case None => None // "invalid_handle"
-      case Some((Left(org), _)) => Some(LibrarySpace.fromOrganizationId(org.id.get))
-      case Some((Right(user), _)) => Some(LibrarySpace.fromUserId(user.id.get))
-    }
-    librarySpaceOpt.map { librarySpace =>
+    ownerOpt.map {
+      case (Left(org), _) => LibrarySpace.fromOrganizationId(org.id.get)
+      case (Right(user), _) => LibrarySpace.fromUserId(user.id.get)
+    }.map { librarySpace =>
       getLibraryBySlugOrAlias(librarySpace, slug) match {
         case None => Left(LibraryFail(NOT_FOUND, "no_library_found"))
         case Some((library, isLibraryAlias)) =>
