de3d5490f16210de637420a6f878844ecfdbf79f
Carlos Vializ <cvializ@vt.edu>
2015-07-29 11:01:59 -0700

    Consume the org's avatarPath

--- kifi-backend/modules/shoebox/angular/src/common/filter.js
+++ kifi-backend/modules/shoebox/angular/src/common/filter.js
@@ -18,9 +18,15 @@ angular.module('kifi')
   'routeService',
   function (routeService) {
     return function (entity, width) { // entity is a catchall for users and orgs
-      return entity && entity.pictureName ?
-        routeService.formatPicUrl(entity.id, entity.pictureName, width > 100 ? 200 : 100) :
-        '//www.kifi.com/assets/img/ghost.200.png';
+      if (entity) {
+        if (entity.pictureName) {
+          return routeService.formatUserPicUrl(entity.id, entity.pictureName, width > 100 ? 200 : 100);
+        } else if (entity.avatarPath) {
+          return routeService.formatOrgPicUrl(entity.avatarPath);
+        } else {
+          return '//www.kifi.com/assets/img/ghost.200.png';
+        }
+      }
     };
   }
 ])
