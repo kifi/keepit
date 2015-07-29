d10933cd3b5759fcf0e762d50547848dad20fe60
Cam Hashemi <camhashemi@gmail.com>
2015-07-28 17:00:12 -0700

    add indices on seq numbers

---
+++ kifi-backend/modules/common/conf/evolutions/shoebox/361.sql
@@ -0,0 +1,11 @@
+# SHOEBOX
+
+# --- !Ups
+
+CREATE INDEX user_ip_addresses_i_seq ON user_ip_addresses(seq);
+
+CREATE INDEX organization_membership_i_seq ON organization_membership(seq);
+
+insert into evolutions (name, description) values('361.sql', 'indices on seq for user_ip_addresses and org_membership');
+
+# --- !Downs
