e796f764a06efee45474d19aac8e76f4c21e0126
Adam Grant <ajkochanowicz@gmail.com>
2015-07-29 16:08:40 -0700

    Update profileWidget.js

--- kifi-backend/modules/shoebox/angular/src/profile/profileWidget.js
+++ kifi-backend/modules/shoebox/angular/src/profile/profileWidget.js
@@ -11,7 +11,7 @@ angular.module('kifi')
       templateUrl: 'profile/profileWidget.tpl.html',
       link: function (scope) {
         scope.me = profileService.me;
-        scope.organizations = profileService.me.organizations;
+        scope.organizations = profileService.me.orgs;
 
         scope.registerEvent = function(action) {
           $analytics.eventTrack('user_clicked_page', {
