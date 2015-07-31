740d624afbc804d517d125160db28f7c40d46bea
Ryan P Brewster <ryan@kifi.com>
2015-07-31 12:48:19 -0700

    Dropped the bookmark constraint on in_disjoint_lib

--- kifi-backend/modules/common/conf/evolutions/shoebox/363.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/363.sql
@@ -3,7 +3,7 @@
 # --- !Ups
 
 ALTER TABLE bookmark ALTER COLUMN in_disjoint_lib tinyint(1) DEFAULT NULL;
--- DROP INDEX `user_disjoint_primary_uri` ON bookmark;
+ALTER TABLE bookmark DROP CONSTRAINT user_disjoint_primary_uri;
 
 insert into evolutions(name, description) values('363.sql', 'drop the in_disjoin_lib column');
 
