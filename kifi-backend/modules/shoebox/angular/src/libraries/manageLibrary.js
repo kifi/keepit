74d8dce1197df84685f55ea51237089803066ec0
Carlos Vializ <cvializ@vt.edu>
2015-07-31 10:45:57 -0700

    Redirect to the departed library's space page after deletion

--- kifi-backend/modules/shoebox/angular/src/libraries/manageLibrary.js
+++ kifi-backend/modules/shoebox/angular/src/libraries/manageLibrary.js
@@ -252,10 +252,20 @@ angular.module('kifi')
 
           submitting = true;
           libraryService.deleteLibrary(scope.library.id).then(function () {
-            // If we were on the deleted library's page, return to the homepage.
+            // If we were on the deleted library's page,
+            // return to the space it was in.
             if ($state.is('library.keeps') &&
-                ($state.href('library.keeps') === scope.library.path)) {
-              $location.url('/');
+                $state.href('library.keeps') === scope.library.path) {
+
+              var redirectToSpaceParams = {
+                handle: (
+                  scope.library.org ? scope.library.org.handle : (
+                    scope.library.owner ? scope.library.owner.username : null
+                  )
+                )
+              };
+
+              $state.go('userOrOrg', redirectToSpaceParams);
             }
           })['catch'](modalService.openGenericErrorModal)['finally'](function () {
             submitting = false;
