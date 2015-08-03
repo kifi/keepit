3ec577b4640af8b173e7646b664a767e77977401
gratimax <max@ovsankin.com>
2015-08-03 14:08:56 -0700

    Encapsulation

--- kifi-backend/modules/eliza/test/com/keepit/eliza/ws/MockWebSocket.scala
+++ kifi-backend/modules/eliza/test/com/keepit/eliza/ws/MockWebSocket.scala
@@ -12,22 +12,24 @@ import scala.concurrent.ExecutionContext.global
 
 case class MockWebSocket[A](implicit ws: WebSocket[A, A]) {
 
-  val socketOut: Stream[Promise[A]] = Stream.continually(Promise())
+  private val socketOut: Stream[Promise[A]] = Stream.continually(Promise())
 
-  val itOut = Iteratee.fold[A, Int](0) { (index, output) =>
+  private val itOut = Iteratee.fold[A, Int](0) { (index, output) =>
     socketOut(index).success(output)
     index + 1
   }(global).map { _ => () }(global)
 
-  val (enumOut, channel) = Concurrent.broadcast[A]
+  private val (enumOut, channel) = Concurrent.broadcast[A]
 
-  val wsResult = Await.result(ws.f(FakeRequest("GET", "/ws?sid=" + FakeSecureSocial.FAKE_SID)), 10 seconds)
-  val wsFunction = wsResult.right.get
-  wsFunction(enumOut, itOut)
+  {
+    val wsResult = Await.result(ws.f(FakeRequest("GET", "/ws?sid=" + FakeSecureSocial.FAKE_SID)), 10 seconds)
+    val wsFunction = wsResult.right.get
+    wsFunction(enumOut, itOut)
+  }
 
   def in(block: => A): Unit = channel.push(block)
 
-  var outIndex = 0
+  private var outIndex = 0
 
   def out: A = {
     val result = Await.result(socketOut(outIndex).future, 10 seconds)
