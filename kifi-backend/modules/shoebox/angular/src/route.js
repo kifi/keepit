a932cf214c796dc196bf0af23e4a1fa7d8ae480e
Andrew Conner <git@andrewconner.org>
2015-08-03 12:59:54 -0700

    Removing ng dependency on loading org profile to route library

--- kifi-backend/modules/shoebox/angular/src/route.js
+++ kifi-backend/modules/shoebox/angular/src/route.js
@@ -169,13 +169,6 @@ angular.module('kifi')
         templateUrl: 'libraries/library.tpl.html',
         controller: 'LibraryCtrl',
         resolve: {
-          profile: ['$stateParams', 'orgProfileService', function ($stateParams, orgProfileService) {
-            return orgProfileService
-              .userOrOrg($stateParams.handle)
-              .then(function (userOrOrgData) {
-                return userOrOrgData;
-              });
-          }],
           libraryService: 'libraryService',
           library: ['libraryService', '$stateParams', function (libraryService, $stateParams) {
             return libraryService.getLibraryByHandleAndSlug($stateParams.handle, $stateParams.librarySlug, $stateParams.authToken);
