c721111ca1b5b451e7ddbf69f18dd10600e71c4e
Léo Grimaldi <leo@42go.com>
2015-08-03 11:40:26 +0200

    Fix evolution for MYSQL

--- kifi-backend/modules/common/conf/evolutions/shoebox/364.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/364.sql
@@ -2,7 +2,7 @@
 
 # --- !Ups
 
-ALTER TABLE password_reset ALTER COLUMN sent_to varchar(512) NOT NULL;
+ALTER TABLE password_reset MODIFY COLUMN sent_to varchar(512) NOT NULL;
 
 insert into evolutions(name, description) values('364.sql', 'modify password_reset.sent_to to varchar(512) NOT NULL');
 
