14cec17707d462aee87e9f93ace5e1c3c8b9f3be
gratimax <max@ovsankin.com>
2015-08-04 15:49:48 -0700

    Add message notification test

--- kifi-backend/modules/eliza/test/com/keepit/eliza/ws/ElizaWebSocketTest.scala
+++ kifi-backend/modules/eliza/test/com/keepit/eliza/ws/ElizaWebSocketTest.scala
@@ -8,19 +8,21 @@ import com.keepit.common.concurrent.FakeExecutionContextModule
 import com.keepit.common.controller.FakeUserActionsModule
 import com.keepit.common.db.{ ExternalId, Id }
 import com.keepit.common.store.FakeElizaStoreModule
+import com.keepit.eliza.commanders.MessagingCommander
 import com.keepit.eliza.controllers.shared.SharedWsMessagingController
-import com.keepit.eliza.model.{ MessageThreadParticipants, MessageThread, MessageThreadRepo }
+import com.keepit.eliza.model._
 import com.keepit.eliza.social.{ FakeSecureSocial, FakeSecureSocialUserPluginModule, FakeSecureSocialAuthenticatorPluginModule }
-import com.keepit.heimdal.FakeHeimdalServiceClientModule
-import com.keepit.model.SocialUserInfo
+import com.keepit.heimdal.{ HeimdalContext, FakeHeimdalServiceClientModule }
+import com.keepit.model.{ User, SocialUserInfo }
 import com.keepit.rover.FakeRoverServiceClientModule
 import com.keepit.shoebox.{ FakeShoeboxServiceClientImpl, ShoeboxServiceClient, FakeShoeboxServiceModule }
 import com.keepit.test.{ ElizaApplication, ElizaApplicationInjector }
 import org.specs2.mutable.Specification
 import org.specs2.time.NoTimeConversions
-import play.api.libs.json.{ JsArray, Json }
+import play.api.libs.json.{ JsNull, JsArray, Json }
 import play.api.mvc.WebSocket
 import play.api.test.Helpers._
+import com.keepit.common.time._
 
 class ElizaWebSocketTest extends Specification with ElizaApplicationInjector with NoTimeConversions {
 
@@ -127,6 +129,60 @@ class ElizaWebSocketTest extends Specification with ElizaApplicationInjector wit
 
         socket.close
         socket.out === Json.arr("bye", "session")
+      }
+    }
+
+    "notify of new messages" in {
+      running(new ElizaApplication(modules: _*)) {
+
+        setupSocialUser
+        val messagingCommander = inject[MessagingCommander]
+        val socket = MockWebSocket()
+        socket.out
+
+        val messageThreadRepo = inject[MessageThreadRepo]
+        val userThreadRepo = inject[UserThreadRepo]
+        val uuid = UUID.randomUUID().toString
+
+        val (messageThread, userThread) = db.readWrite { implicit session =>
+
+          val messageThread = messageThreadRepo.save(MessageThread(
+            externalId = ExternalId(uuid),
+            uriId = Some(Id(1)),
+            url = None,
+            nUrl = None,
+            pageTitle = None,
+            participants = Some(MessageThreadParticipants(Set(Id(1), Id(2)))),
+            participantsHash = None,
+            replyable = true))
+
+          val userThread = userThreadRepo.save(UserThread(
+            user = Id(1),
+            threadId = messageThread.id.get,
+            uriId = Some(Id(1)),
+            lastSeen = None,
+            lastMsgFromOther = None,
+            lastNotification = JsNull,
+            lastActive = Some(currentDateTime),
+            started = true
+          ))
+
+          (messageThread, userThread)
+        }
+
+        implicit val context = new HeimdalContext(Map())
+
+        messagingCommander.sendMessage(Id[User](2), messageThread.id.get, "So long and thanks for all the fish", None, None)
+
+        val message = socket.out
+        message(0).as[String] === "message"
+        message(1).as[String] === uuid
+        val messageContent = message(2)
+        (messageContent \ "text").as[String] === "So long and thanks for all the fish"
+        (messageContent \ "participants").asInstanceOf[JsArray].value.length === 2
+        socket.close
+        socket.out === Json.arr("bye", "session")
+
       }
     }
 
