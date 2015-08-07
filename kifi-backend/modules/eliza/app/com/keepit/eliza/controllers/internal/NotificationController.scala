fc8d20fe9c9c11c920071c19d65208ac066f8333
gratimax <max@ovsankin.com>
2015-08-07 14:45:23 -0700

    NotificationController should extend ServiceController

--- kifi-backend/modules/eliza/app/com/keepit/eliza/controllers/internal/NotificationController.scala
+++ kifi-backend/modules/eliza/app/com/keepit/eliza/controllers/internal/NotificationController.scala
@@ -1,6 +1,8 @@
 package com.keepit.eliza.controllers.internal
 
 import com.google.inject.{ Inject, Singleton }
+import com.keepit.common.controller.{ ElizaServiceController, ServiceController }
+import com.keepit.common.service.ServiceType
 import com.keepit.eliza.commanders.NotificationCommander
 import com.keepit.notify.model.NotificationEvent
 import play.api.libs.json.Json
@@ -8,7 +10,7 @@ import play.api.mvc.{ Action, Controller }
 
 @Singleton
 class NotificationController @Inject() (
-    val notificationCommander: NotificationCommander) extends Controller {
+    val notificationCommander: NotificationCommander) extends ElizaServiceController {
 
   def postEvent = Action(parse.json) { implicit request =>
     val event = request.body.as[NotificationEvent]
