f72cd522b469bea2ee9b770899182b149ef6c782
Ryan P Brewster <ryan@kifi.com>
2015-08-07 16:03:26 -0700

    Better admin test to ensure safety

--- kifi-backend/modules/shoebox/test/com/keepit/controllers/admin/AdminLibraryControllerTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/controllers/admin/AdminLibraryControllerTest.scala
@@ -11,7 +11,7 @@ import com.keepit.model.UserFactoryHelper._
 import com.keepit.model._
 import com.keepit.test.ShoeboxTestInjector
 import org.specs2.mutable.Specification
-import play.api.libs.json.{ JsValue, Json }
+import play.api.libs.json.{ JsNull, JsValue, Json }
 import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
@@ -31,25 +31,38 @@ class AdminLibraryControllerTest extends Specification with ShoeboxTestInjector 
         inject[AdminLibraryController] !== null
       }
     }
-    "force a user into a library" in {
-      withDb(modules: _*) { implicit injector =>
-        val (admin, lib, rando) = db.readWrite { implicit session =>
-          val admin = UserFactory.user().saved
-          val owner = UserFactory.user().saved
-          val rando = UserFactory.user().saved
-          val lib = LibraryFactory.library().withOwner(owner).saved
-          (admin, lib, rando)
+    "let admins put people in libraries" in {
+      "never allow a non-admin to do anything" in {
+        withDb(modules: _*) { implicit injector =>
+          val rando = db.readWrite { implicit session => UserFactory.user().saved }
+
+          inject[FakeUserActionsHelper].setUser(rando)
+          val payload: JsValue = JsNull
+          val request = route.unsafeAddMember().withBody(payload)
+          val result = controller.unsafeAddMember(request)
+          status(result) === FORBIDDEN
         }
+      }
+      "force a user into a library" in {
+        withDb(modules: _*) { implicit injector =>
+          val (admin, lib, rando) = db.readWrite { implicit session =>
+            val admin = UserFactory.user().saved
+            val owner = UserFactory.user().saved
+            val rando = UserFactory.user().saved
+            val lib = LibraryFactory.library().withOwner(owner).saved
+            (admin, lib, rando)
+          }
 
-        inject[FakeUserActionsHelper].setUser(admin, Set(UserExperimentType.ADMIN))
-        val payload: JsValue = Json.obj("userId" -> rando.id.get, "libraryId" -> lib.id.get, "access" -> LibraryAccess.READ_WRITE)
-        val request = route.unsafeAddMember().withBody(payload)
-        val result = controller.unsafeAddMember(request)
-        status(result) === OK
+          inject[FakeUserActionsHelper].setUser(admin, Set(UserExperimentType.ADMIN))
+          val payload: JsValue = Json.obj("userId" -> rando.id.get, "libraryId" -> lib.id.get, "access" -> LibraryAccess.READ_WRITE)
+          val request = route.unsafeAddMember().withBody(payload)
+          val result = controller.unsafeAddMember(request)
+          status(result) === OK
 
-        val membership = contentAsJson(result).as[LibraryMembership]
-        membership.userId === rando.id.get
-        membership.libraryId === lib.id.get
+          val membership = contentAsJson(result).as[LibraryMembership]
+          membership.userId === rando.id.get
+          membership.libraryId === lib.id.get
+        }
       }
     }
   }
