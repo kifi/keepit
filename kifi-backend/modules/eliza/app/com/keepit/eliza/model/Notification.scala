46268ff820410e987a283c85846f961596482a09
gratimax <max@ovsankin.com>
2015-08-06 18:53:30 -0700

    Add some docs explaining notification architecture

--- kifi-backend/modules/eliza/app/com/keepit/eliza/model/Notification.scala
+++ kifi-backend/modules/eliza/app/com/keepit/eliza/model/Notification.scala
@@ -8,6 +8,22 @@ import org.joda.time.DateTime
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
+/**
+ * The architecture of delivering and handling what is conceptually a "notification to the user" is split between
+ * several classes.
+ *
+ * [[com.keepit.notify.model.NotificationEvent]] represents something that happened in the Kifi application. At this point
+ * we're not sure anything will even be delivered, but we'll hand it over to Eliza for processing. Each event has a distinct
+ * [[NotificationKind]] describing its grouping behavior.
+ *
+ * [[NotificationItem]] is essentially a persisted [[com.keepit.notify.model.NotificationEvent]] that is attached to a notification.
+ * When Eliza receives a new [[com.keepit.notify.model.NotificationEvent]], it looks at previous similar events (specifically, those
+ * to the same user and of the same kind) and decides whether or not to group the new event with previous events.
+ *
+ * [[Notification]] represents a group of [[NotificationItem]]s and is what is finally displayed to the user as a clickable, tappable,
+ * or otherwise actionable UI element. When new events flow in, it is possible for a notification to be updated with new [[NotificationItem]]s,
+ * or a new one to be created.
+ */
 case class Notification(
     id: Option[Id[Notification]] = None,
     createdAt: DateTime = currentDateTime,
