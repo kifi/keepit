8c31132770f8ec7dacc356945916b4dd6c83965a
Léo Grimaldi <leo@42go.com>
2015-08-12 17:21:37 +0200

    Actually, fix evolution

--- kifi-backend/modules/common/conf/evolutions/shoebox/373.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/373.sql
@@ -2,8 +2,8 @@
 
 # --- !Ups
 
-ALTER TABLE `email_address` DROP COLUMN if exists `primary_email`;
+ALTER TABLE `user` DROP COLUMN `primary_email`;
 
-insert into evolutions (name, description) values('373.sql', 'drop column email_address.primary_email');
+insert into evolutions (name, description) values('373.sql', 'drop column user.primary_email');
 
 # --- !Downs
