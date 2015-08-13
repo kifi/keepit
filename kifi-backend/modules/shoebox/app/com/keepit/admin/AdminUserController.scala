0128a3a230302504c3924c6ae0b6cf0eb9137266
Cam Hashemi <camhashemi@gmail.com>
2015-08-13 14:58:38 -0700

    don't let admin delete users who own libraries with collaborators or organizations

--- kifi-backend/modules/shoebox/app/com/keepit/admin/AdminUserController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/admin/AdminUserController.scala
@@ -948,7 +948,9 @@ class AdminUserController @Inject() (
     // Libraries Data
     libraryInviteRepo.getByUser(userId, Set(LibraryInviteStates.INACTIVE)).foreach { case (invite, _) => libraryInviteRepo.save(invite.withState(LibraryInviteStates.INACTIVE)) } // Library Invites
     libraryMembershipRepo.getWithUserId(userId).foreach { membership => libraryMembershipRepo.save(membership.withState(LibraryMembershipStates.INACTIVE)) } // Library Memberships
-    libraryRepo.getAllByOwner(userId).foreach { library => libraryRepo.save(library.withState(LibraryStates.INACTIVE)) } // Libraries
+    val ownedLibraries = libraryRepo.getAllByOwner(userId).map(_.id.get)
+    val ownsCollaborativeLibs = libraryMembershipRepo.getCollaboratorsByLibrary(ownedLibraries.toSet).exists { case (_, collaborators) => collaborators.size > 1 }
+    assert(!ownsCollaborativeLibs, "cannot deactivate a user if they own a library with collaborators: either delete the library or transfer its ownership")
 
     // Personal Info
     userSessionRepo.invalidateByUser(userId) // User Session
@@ -960,6 +962,7 @@ class AdminUserController @Inject() (
     orgMembershipRepo.getAllByUserId(userId).foreach { membership => orgMembershipRepo.save(membership.withState(OrganizationMembershipStates.INACTIVE)) }
     orgInviteRepo.getAllByUserId(userId).foreach { invite => orgInviteRepo.save(invite.withState(OrganizationInviteStates.INACTIVE)) }
     orgMembershipCandidateRepo.getAllByUserId(userId).foreach { candidacy => orgMembershipCandidateRepo.save(candidacy.withState(OrganizationMembershipCandidateStates.INACTIVE)) }
+    assert(orgRepo.getAllByOwnerId(userId).isEmpty, "cannot deactivate a user if they own an org: either delete the org or transfer its ownership")
 
     val user = userRepo.get(userId)
 
