8e7b842c1aff0d3703937041cc4f514ea9703971
gratimax <max@ovsankin.com>
2015-08-13 12:54:20 -0700

    Fix compile errors

--- kifi-backend/modules/shoebox/app/com/keepit/notify/info/ShoeboxNotificationInfoSourceImpl.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/notify/info/ShoeboxNotificationInfoSourceImpl.scala
@@ -6,18 +6,18 @@ import com.keepit.model.{ Library, User }
 import com.keepit.notify.model.NotificationEvent
 
 import scala.concurrent.Future
-import scala.util.{Failure, Try}
+import scala.util.{ Failure, Try }
 
 class ShoeboxNotificationInfoSourceImpl extends NotificationInfoSource {
 
   override def pickOne[E <: NotificationEvent](events: Set[E]): Future[E] = Future.fromTry(Try { events.head })
 
-  override def userImage(id: Id[User], name: Option[String]): Future[String] = ???
+  override def userImage(id: Id[User]): Future[String] = ???
 
-  override def libraryPath(id: Id[Library], name: Option[String]): Future[EncodedPath] = ???
+  override def libraryPath(id: Id[Library]): Future[EncodedPath] = ???
 
-  override def user(id: Id[User], name: Option[String]): Future[User] = ???
+  override def user(id: Id[User]): Future[User] = ???
 
-  override def library(id: Id[Library], name: Option[String]): Future[Library] = ???
+  override def library(id: Id[Library]): Future[Library] = ???
 
 }
