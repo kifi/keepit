cbb5ae93facf52f5e2d92f220863d600c27d2d3c
Léo Grimaldi <leo@42go.com>
2015-08-16 17:38:16 -0700

    Override org handle validity check in admin

--- kifi-backend/modules/shoebox/app/com/keepit/admin/AdminOrganizationController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/admin/AdminOrganizationController.scala
@@ -337,7 +337,7 @@ class AdminOrganizationController @Inject() (
   def setHandle(orgId: Id[Organization]) = AdminUserPage { implicit request =>
     val handle = OrganizationHandle(request.body.asFormUrlEncoded.flatMap(_.get("handle").flatMap(_.headOption)).filter(_.length > 0).get)
     db.readWrite { implicit session =>
-      handleCommander.setOrganizationHandle(orgRepo.get(orgId), handle)
+      handleCommander.setOrganizationHandle(orgRepo.get(orgId), handle, overrideValidityCheck = true)
     }
     Redirect(com.keepit.controllers.admin.routes.AdminOrganizationController.organizationViewById(orgId))
   }
