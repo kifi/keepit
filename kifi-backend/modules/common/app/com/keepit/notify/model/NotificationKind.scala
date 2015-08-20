f7289baffbd1e6b6955fa33730e67735df6c7b96
gratimax <max@ovsankin.com>
2015-08-20 11:37:47 -0700

    Add NonGroupingNotificationEvent, makes grouping very explicit

--- kifi-backend/modules/common/app/com/keepit/notify/model/NotificationKind.scala
+++ kifi-backend/modules/common/app/com/keepit/notify/model/NotificationKind.scala
@@ -40,7 +40,32 @@ trait NotificationKind[N <: NotificationEvent] {
    */
   def shouldGroupWith(newEvent: N, existingEvents: Set[N]): Boolean
 
+  /**
+   * Generates notification info for a set of events. This essentially returns a function wrapped in
+   * [[UsingDbSubset]] in order to batch potential calls from Eliza to shoebox.
+   */
   def info(events: Set[N]): UsingDbSubset[NotificationInfo]
+}
+
+/**
+ * Defines a kind of notification that guarantees that it does not group events.
+ */
+trait NonGroupingNotificationKind[N <: NotificationEvent] extends NotificationKind[N] {
+
+  override final def groupIdentifier(event: N): Option[String] = None
+
+  override final def shouldGroupWith(newEvent: N, existingEvents: Set[N]): Boolean = false
+
+  override final def info(events: Set[N]): UsingDbSubset[NotificationInfo] = {
+    require(events.size == 1,
+      "Subtypes of NonGroupingNotificationKind are supposed to guarantee that no events ever group, yet a group of events was received.")
+    info(events.head)
+  }
+
+  /**
+   * Because it is guaranteed that notifications aren't ever grouped, info can be written taking only one event.
+   */
+  def info(event: N): UsingDbSubset[NotificationInfo]
 
 }
 
--- kifi-backend/modules/common/app/com/keepit/notify/model/event/ConnectionEvent.scala
+++ kifi-backend/modules/common/app/com/keepit/notify/model/event/ConnectionEvent.scala
@@ -5,7 +5,7 @@ import com.keepit.common.path.Path
 import com.keepit.model.User
 import com.keepit.notify.info.NeedInfo.Using
 import com.keepit.notify.info._
-import com.keepit.notify.model.{ NotificationKind, Recipient, NotificationEvent }
+import com.keepit.notify.model.{NonGroupingNotificationKind, NotificationKind, Recipient, NotificationEvent}
 import com.keepit.social.BasicUser
 import org.joda.time.DateTime
 import play.api.libs.functional.syntax._
@@ -24,7 +24,7 @@ case class NewConnectionInvite(
 
 }
 
-object NewConnectionInvite extends NotificationKind[NewConnectionInvite] {
+object NewConnectionInvite extends NonGroupingNotificationKind[NewConnectionInvite] {
 
   override val name: String = "new_connection_invite"
 
@@ -34,14 +34,12 @@ object NewConnectionInvite extends NotificationKind[NewConnectionInvite] {
     (__ \ "inviterId").format[Id[User]]
   )(NewConnectionInvite.apply, unlift(NewConnectionInvite.unapply))
 
-  override def shouldGroupWith(newEvent: NewConnectionInvite, existingEvents: Set[NewConnectionInvite]): Boolean = false
 
-  override def info(events: Set[NewConnectionInvite]): UsingDbSubset[NotificationInfo] = {
+  override def info(event: NewConnectionInvite): UsingDbSubset[NotificationInfo] = {
     import NeedInfo._
-    val oneEvent = events.head
-    UsingDbSubset(user(oneEvent.inviterId), userImageUrl(oneEvent.inviterId)) { subset =>
-      val inviter = subset.user(oneEvent.inviterId)
-      val inviterImage = subset.userImageUrl(oneEvent.inviterId)
+    UsingDbSubset(user(event.inviterId), userImageUrl(event.inviterId)) { subset =>
+      val inviter = subset.user(event.inviterId)
+      val inviterImage = subset.userImageUrl(event.inviterId)
       NotificationInfo(
         url = Path(inviter.username.value).encode.absolute,
         title = s"${inviter.firstName} ${inviter.lastName} wants to connect with you on Kifi",
@@ -66,7 +64,7 @@ case class ConnectionInviteAccepted(
 
 }
 
-object ConnectionInviteAccepted extends NotificationKind[ConnectionInviteAccepted] {
+object ConnectionInviteAccepted extends NonGroupingNotificationKind[ConnectionInviteAccepted] {
 
   override val name: String = "connection_invite_accepted"
 
@@ -76,14 +74,11 @@ object ConnectionInviteAccepted extends NotificationKind[ConnectionInviteAccepte
     (__ \ "accepterId").format[Id[User]]
   )(ConnectionInviteAccepted.apply, unlift(ConnectionInviteAccepted.unapply))
 
-  override def shouldGroupWith(newEvent: ConnectionInviteAccepted, existingEvents: Set[ConnectionInviteAccepted]): Boolean = false
-
-  override def info(events: Set[ConnectionInviteAccepted]): UsingDbSubset[NotificationInfo] = {
+  override def info(event: ConnectionInviteAccepted): UsingDbSubset[NotificationInfo] = {
     import NeedInfo._
-    val oneEvent = events.head
-    UsingDbSubset(user(oneEvent.accepterId), userImageUrl(oneEvent.accepterId)) { subset =>
-        val accepter = subset.user(oneEvent.accepterId)
-        val accepterImage = subset.userImageUrl(oneEvent.accepterId)
+    UsingDbSubset(user(event.accepterId), userImageUrl(event.accepterId)) { subset =>
+        val accepter = subset.user(event.accepterId)
+        val accepterImage = subset.userImageUrl(event.accepterId)
         NotificationInfo(
           url = Path(accepter.username.value).encode.absolute,
           title = s"${accepter.firstName} ${accepter.lastName} accepted your invitation to connect!",
