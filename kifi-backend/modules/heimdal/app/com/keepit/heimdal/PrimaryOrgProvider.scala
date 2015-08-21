4fda814a871d3a374da69e3f3d460b9b174fcb70
Cam Hashemi <camhashemi@gmail.com>
2015-08-20 18:27:29 -0700

    rename keeper with most clicks

--- kifi-backend/modules/heimdal/app/com/keepit/heimdal/PrimaryOrgProvider.scala
+++ kifi-backend/modules/heimdal/app/com/keepit/heimdal/PrimaryOrgProvider.scala
@@ -54,7 +54,7 @@ class PrimaryOrgProviderImpl @Inject() (
         "inviteCount" -> ContextDoubleData(shoeboxValues.inviteCount),
         "collabLibCount" -> ContextDoubleData(shoeboxValues.collabLibCount),
         "messageCount" -> ContextDoubleData(messageCount),
-        "keeperWithMostClicks" -> ContextStringData(keeperWithMostClicks.toString)
+        "overallKeepViews" -> ContextStringData(keeperWithMostClicks.toString)
       )
     }
   }
