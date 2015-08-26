5d9fad42bb91e14ebf6f9cf77a313d360c62b0be
Cam Hashemi <camhashemi@gmail.com>
2015-08-26 15:06:48 -0700

    add create library mobile endpoint that includes space

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobileLibraryController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/mobile/MobileLibraryController.scala
@@ -7,6 +7,7 @@ import com.keepit.common.controller._
 import com.keepit.common.crypto.{ PublicId, PublicIdConfiguration }
 import com.keepit.common.db.{ ExternalId, Id }
 import com.keepit.common.db.slick.Database
+import com.keepit.common.healthcheck.AirbrakeNotifier
 import com.keepit.common.json
 import com.keepit.common.json.TupleFormat
 import com.keepit.common.mail.EmailAddress
@@ -17,6 +18,7 @@ import com.keepit.common.util.Paginator
 import com.keepit.controllers.mobile.ImplicitHelper._
 import com.keepit.heimdal.{ HeimdalContext, HeimdalContextBuilderFactory }
 import com.keepit.inject.FortyTwoConfig
+import com.keepit.model.ExternalLibrarySpace.{ ExternalOrganizationSpace, ExternalUserSpace }
 import com.keepit.model._
 import com.keepit.normalizer.NormalizedURIInterner
 import com.keepit.shoebox.controllers.LibraryAccessActions
@@ -34,6 +36,7 @@ class MobileLibraryController @Inject() (
   db: Database,
   fortyTwoConfig: FortyTwoConfig,
   libraryRepo: LibraryRepo,
+  libraryMembershipRepo: LibraryMembershipRepo,
   keepRepo: KeepRepo,
   keepToCollectionRepo: KeepToCollectionRepo,
   collectionRepo: CollectionRepo,
@@ -51,6 +54,7 @@ class MobileLibraryController @Inject() (
   normalizedUriInterner: NormalizedURIInterner,
   heimdalContextBuilder: HeimdalContextBuilderFactory,
   clock: Clock,
+  airbrake: AirbrakeNotifier,
   val libraryCommander: LibraryCommander,
   val libraryInviteCommander: LibraryInviteCommander,
   val userActionsHelper: UserActionsHelper,
@@ -78,6 +82,43 @@ class MobileLibraryController @Inject() (
     libraryCommander.addLibrary(addRequest, request.userId) match {
       case Left(fail) => sendFailResponse(fail)
       case Right(lib) => Ok(Json.toJson(constructLibraryInfo(lib)))
+    }
+  }
+
+  def createLibraryV2() = UserAction(parse.tolerantJson) { request =>
+    val externalAddRequestValidated = request.body.validate[ExternalLibraryAddRequest]
+
+    externalAddRequestValidated match {
+      case JsError(errs) =>
+        airbrake.notify(s"Could not json-validate addLibRequest from ${request.userId}: ${request.body}", new JsResultException(errs))
+        BadRequest(Json.obj("error" -> "badly_formatted_request"))
+      case JsSuccess(externalAddRequest, _) =>
+        val libAddRequest = db.readOnlyReplica { implicit session =>
+          val slug = externalAddRequest.slug.getOrElse(LibrarySlug.generateFromName(externalAddRequest.name))
+          val space = externalAddRequest.space map {
+            case ExternalUserSpace(extId) => LibrarySpace.fromUserId(userRepo.getByExternalId(extId).id.get)
+            case ExternalOrganizationSpace(pubId) => LibrarySpace.fromOrganizationId(Organization.decodePublicId(pubId).get)
+          }
+          LibraryAddRequest(
+            name = externalAddRequest.name,
+            slug = slug,
+            visibility = externalAddRequest.visibility,
+            description = externalAddRequest.description,
+            color = externalAddRequest.color,
+            listed = externalAddRequest.listed,
+            whoCanInvite = externalAddRequest.whoCanInvite,
+            subscriptions = externalAddRequest.subscriptions,
+            space = space
+          )
+        }
+
+        implicit val context = heimdalContextBuilder.withRequestInfoAndSource(request, KeepSource.mobile).build
+        libraryCommander.addLibrary(libAddRequest, request.userId) match {
+          case Left(fail) =>
+            Status(fail.status)(Json.obj("error" -> fail.message))
+          case Right(newLibrary) =>
+            Ok(Json.toJson(constructLibraryInfo(newLibrary)))
+        }
     }
   }
 
