f5cc7c6bb2579532864e4427dbbe07b02ccac81f
Stephen Kemmerling <stephen@42go.com>
2015-09-01 19:47:03 -0400

    so if you quote a column name you'll get a column with actual quotes in the name in H2

--- kifi-backend/modules/common/conf/evolutions/shoebox/388.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/388.sql
@@ -2,7 +2,7 @@
 
 # --- !Ups
 
-ALTER TABLE paid_account ADD COLUMN "frozen" boolean NOT NULL DEFAULT false;
+ALTER TABLE paid_account ADD COLUMN frozen boolean NOT NULL DEFAULT false;
 
 insert into evolutions (name, description) values('388.sql', 'adding frozen column to paid_account');
 
