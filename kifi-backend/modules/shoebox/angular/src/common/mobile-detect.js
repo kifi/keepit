114b37cec0307d3b211a30bb5b6aef833dbe4c71
Adam Grant <hello@adamgrant.me>
2015-09-01 16:08:39 -0700

    Adding function to detect mobile operating system

---
+++ kifi-backend/modules/shoebox/angular/src/common/mobile-detect.js
@@ -0,0 +1,18 @@
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+  {
+    return 'iOS';
+
+  }
+  else if( userAgent.match( /Android/i ) )
+  {
+
+    return 'Android';
+  }
+  else
+  {
+    return 'unknown';
+  }
+}
