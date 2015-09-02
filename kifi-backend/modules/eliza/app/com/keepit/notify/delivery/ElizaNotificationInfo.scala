74c74b1628de6a81bbf03ce284d4fae026a7baaa
gratimax <max@ovsankin.com>
2015-09-02 11:28:44 -0700

    Use new external id image store to resolve user images

--- kifi-backend/modules/eliza/app/com/keepit/notify/delivery/ElizaNotificationInfo.scala
+++ kifi-backend/modules/eliza/app/com/keepit/notify/delivery/ElizaNotificationInfo.scala
@@ -4,6 +4,7 @@ import com.google.inject.Inject
 import com.keepit.eliza.model.{ Notification, NotificationItem }
 import com.keepit.notify.info.{ PublicImage, UserImage, NotificationImage, NotificationInfo }
 import com.keepit.shoebox.ShoeboxServiceClient
+import com.keepit.store.ElizaS3ExternalIdImageStore
 import play.api.libs.json.{ Json, JsValue }
 import com.keepit.common.time._
 
@@ -11,36 +12,34 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class ElizaNotificationInfo @Inject() (
     shoeboxServiceClient: ShoeboxServiceClient,
+    elizaS3ExternalIdImageStore: ElizaS3ExternalIdImageStore,
     implicit val executionContext: ExecutionContext) {
 
   import Ordering.Implicits._
 
-  def mkJson(notif: Notification, items: Set[NotificationItem], info: NotificationInfo): Future[JsValue] = {
+  def mkJson(notif: Notification, items: Set[NotificationItem], info: NotificationInfo): JsValue = {
     val maxByTime = items.maxBy(_.event.time)
     val maxTime = maxByTime.event.time
-    resolveImage(info.image).map { image =>
-      Json.obj(
-        "id" -> maxByTime.externalId,
-        "time" -> maxTime,
-        "thread" -> notif.externalId,
-        "unread" -> Json.toJson(!notif.disabled && notif.lastEvent > notif.lastChecked),
-        "category" -> "triggered",
-        "fullCategory" -> "replace me",
-        "title" -> info.title,
-        "bodyHtml" -> info.body,
-        "linkText" -> info.linkText,
-        "url" -> info.url,
-        "isSticky" -> false,
-        "image" -> image,
-        "extra" -> info.extraJson
-      )
-    }
-
+    Json.obj(
+      "id" -> maxByTime.externalId,
+      "time" -> maxTime,
+      "thread" -> notif.externalId,
+      "unread" -> Json.toJson(!notif.disabled && notif.lastEvent > notif.lastChecked),
+      "category" -> "triggered",
+      "fullCategory" -> "replace me",
+      "title" -> info.title,
+      "bodyHtml" -> info.body,
+      "linkText" -> info.linkText,
+      "url" -> info.url,
+      "isSticky" -> false,
+      "image" -> resolveImage(info.image),
+      "extra" -> info.extraJson
+    )
   }
 
-  def resolveImage(image: NotificationImage): Future[String] = image match {
-    case UserImage(userId) => shoeboxServiceClient.getUserImageUrl(userId, 200)
-    case PublicImage(url) => Future.successful(url)
+  def resolveImage(image: NotificationImage): String = image match {
+    case UserImage(user) => elizaS3ExternalIdImageStore.avatarUrlByUser(user)
+    case PublicImage(url) => url
   }
 
 }
