943f9128cdca9cf6ac7d278bb9e68af7b10c94dd
gratimax <max@ovsankin.com>
2015-09-02 11:05:55 -0700

    Add endpoint to batch user images by external ids

--- kifi-backend/modules/common/app/com/keepit/common/routes/Router.scala
+++ kifi-backend/modules/common/app/com/keepit/common/routes/Router.scala
@@ -132,6 +132,7 @@ object Shoebox extends Service {
     def getSearchFriendsChanged(seqNum: SequenceNumber[SearchFriend], fetchSize: Int) = ServiceRoute(GET, "/internal/shoebox/database/getSearchFriendsChanged", Param("seqNum", seqNum), Param("fetchSize", fetchSize))
     def allURLPatternRules() = ServiceRoute(GET, "/internal/shoebox/database/urlPatternRules")
     def getUserImageUrl(id: Id[User], width: Int) = ServiceRoute(GET, "/internal/shoebox/image/getUserImageUrl", Param("id", id), Param("width", width))
+    def getUserImageUrlsByExternalIds() = ServiceRoute(POST, "/internal/shoebox/image/getUserImageUrlsByExternalIds")
     def getCandidateURIs() = ServiceRoute(POST, "/internal/shoebox/database/getCandidateURIs")
     def getUnsubscribeUrlForEmail(email: EmailAddress) = ServiceRoute(GET, "/internal/shoebox/email/getUnsubscribeUrlForEmail", Param("email", email))
     def getIndexableSocialConnections(seqNum: SequenceNumber[SocialConnection], fetchSize: Int) = ServiceRoute(GET, "/internal/shoebox/database/getIndexableSocialConnections", Param("seqNum", seqNum), Param("fetchSize", fetchSize))
