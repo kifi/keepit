d0ef9114f10d86864ce6c5f8bf35fcb74b83e5e6
Adam Grant <hello@adamgrant.me>
2015-09-03 14:05:06 -0700

    Adding links to app stores

--- kifi-backend/modules/shoebox/angular/src/common/directives/mobileInterstitial/mobileInterstitial.js
+++ kifi-backend/modules/shoebox/angular/src/common/directives/mobileInterstitial/mobileInterstitial.js
@@ -2,22 +2,22 @@
 
 angular.module('kifi')
 
-.directive('kfMobileInterstitial',
-  function() {
+.directive('kfMobileInterstitial', ['mobileOS',
+  function(mobileOS) {
     return {
       restrict: 'A',
       templateUrl: 'common/directives/mobileInterstitial/mobileInterstitial.tpl.html',
-      scope: { show: '=' },
+      scope: { },
       link: function($scope) {
-        if (!!document.cookie.replace(/(?:(?:^|.*;\s*)kfShowMobileInterstitial\s*\=\s*([^;]*).*$)|^.*$/, "$1")) {
-          $scope.show = false;
-        }
+        $scope.show = !document.cookie.replace(/(?:(?:^|.*;\s*)kfHideMobileInterstitial\s*\=\s*([^;]*).*$)|^.*$/, "$1") && (mobileOS === "iOS" || mobileOS === "Android")
+
+        $scope.app_link = (mobileOS === 'iOS' ? 'https://itunes.apple.com/us/app/kifi-new-way-to-build-your/id740232575?mt=8' : 'https://play.google.com/store/apps/details?id=com.kifi');
 
         $scope.permanentlyHide = function() {
           $scope.show = false;
-          document.cookie="kfShowMobileInterstitial=true";
+          document.cookie="kfHideMobileInterstitial=true";
         };
       }
     };
-  }
+  }]
 );
