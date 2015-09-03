9b6bfd11a66b6d7c34d9e1ee97309992aaadad3f
Léo Grimaldi <leo@42go.com>
2015-09-02 17:45:15 -0700

    Rename 388.sql => 389.sql

--- kifi-backend/modules/common/conf/evolutions/shoebox/388.sql
+++
@@ -1,9 +0,0 @@
-# SHOEBOX
-
-# --- !Ups
-
-ALTER TABLE library ADD COLUMN organization_member_access varchar(20);
-
-insert into evolutions (name, description) values('388.sql', 'adding organization_member_access column to library');
-
-# --- !Downs
