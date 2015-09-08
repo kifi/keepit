02c62b4049448f5325b854047a068e9924c1dfc2
Cam Hashemi <camhashemi@gmail.com>
2015-09-08 16:47:04 -0700

    create a library from scratch, even if it was once inactive

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryCommander.scala
@@ -144,7 +144,9 @@ class LibraryCommanderImpl @Inject() (
                   }
                   lib
                 case Some(lib) =>
-                  val newLib = libraryRepo.save(lib.copy(state = LibraryStates.ACTIVE))
+                  val newLib = libraryRepo.save(Library(id = lib.id, ownerId = ownerId,
+                    name = libCreateReq.name, description = libCreateReq.description, visibility = libCreateReq.visibility, slug = validSlug, color = newColor, kind = newKind,
+                    memberCount = 1, keepCount = 0, whoCanInvite = newInviteToCollab, organizationId = orgIdOpt, organizationMemberAccess = newOrgMemberAccessOpt))
                   libraryMembershipRepo.getWithLibraryIdAndUserId(libraryId = lib.id.get, userId = ownerId, None) match {
                     case None => libraryMembershipRepo.save(LibraryMembership(libraryId = lib.id.get, userId = ownerId, access = LibraryAccess.OWNER))
                     case Some(mem) => libraryMembershipRepo.save(mem.copy(state = LibraryMembershipStates.ACTIVE, listed = newListed))
