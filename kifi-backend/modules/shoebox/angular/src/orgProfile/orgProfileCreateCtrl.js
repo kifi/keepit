6d4fd6f039b40e0023f65216c86cf8f0c93fdfe0
Adam Grant <hello@adamgrant.me>
2015-09-14 10:31:40 -0700

    Org creation takes user to newly-created org page

--- kifi-backend/modules/shoebox/angular/src/orgProfile/orgProfileCreateCtrl.js
+++ kifi-backend/modules/shoebox/angular/src/orgProfile/orgProfileCreateCtrl.js
@@ -3,13 +3,19 @@
 angular.module('kifi')
 
 .controller('OrgProfileCreateCtrl', [
-  '$scope', 'orgProfileService', 'net',
-  function($scope, orgProfileService, net) {
+  '$scope', 'orgProfileService', 'net', '$location', 'ml',
+  function($scope, orgProfileService, net, $location, ml) {
     $scope.orgName = '';
     $scope.createOrg = function() {
-      net.createOrg({name: $scope.orgName}).then(function(data) {
-        debugger;
-        alert('org created');
+      ml.specs.createOrg = new ml.Spec([
+        new ml.Expect('Org was assigned a handle', function(handle) { return (handle !== undefined); }),
+        new ml.Assert('Created org was returned in 3 seconds or less', 3000)
+      ]);
+
+      net.createOrg({name: $scope.orgName}).then(function(response) {
+        var handle = response.data.organization.handle;
+        $location.url('/' + handle);
+        ml.specs.createOrg.respond(handle);
       });
     };
   }
