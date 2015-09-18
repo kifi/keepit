19a34d4c471b85332f180305a6c12e34c012369a
Cam Hashemi <camhashemi@gmail.com>
2015-09-17 19:38:38 -0700

    avoid session in sessions

--- kifi-backend/modules/shoebox/app/com/keepit/payments/PlanManagementCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/payments/PlanManagementCommander.scala
@@ -30,7 +30,7 @@ trait PlanManagementCommander {
   def createAndInitializePaidAccountForOrganization(orgId: Id[Organization], planId: Id[PaidPlan], creator: Id[User], session: RWSession): Try[AccountEvent]
   def deactivatePaidAccountForOrganziation(orgId: Id[Organization], session: RWSession): Try[Unit]
 
-  def registerNewUser(orgId: Id[Organization], userId: Id[User], attribution: ActionAttribution): AccountEvent
+  def registerNewUser(orgId: Id[Organization], userId: Id[User], attribution: ActionAttribution)(implicit session: RWSession): AccountEvent
   def registerRemovedUser(orgId: Id[Organization], userId: Id[User], attribution: ActionAttribution): AccountEvent
   def registerNewAdmin(orgId: Id[Organization], userId: Id[User], attribution: ActionAttribution): AccountEvent
   def registerRemovedAdmin(orgId: Id[Organization], userId: Id[User], attribution: ActionAttribution): AccountEvent
@@ -179,7 +179,7 @@ class PlanManagementCommanderImpl @Inject() (
 
   }
 
-  def registerNewUser(orgId: Id[Organization], userId: Id[User], attribution: ActionAttribution): AccountEvent = db.readWrite { implicit session =>
+  def registerNewUser(orgId: Id[Organization], userId: Id[User], attribution: ActionAttribution)(implicit session: RWSession): AccountEvent = {
     accountEventRepo.save(AccountEvent.simpleNonBillingEvent(
       eventTime = clock.now,
       accountId = orgId2AccountId(orgId),
