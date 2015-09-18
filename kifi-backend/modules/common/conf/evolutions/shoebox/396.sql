857c4d92e3b0da7501a06c7e333f939e61100c2f
Stephen Kemmerling <stephen@42go.com>
2015-09-17 20:03:38 -0400

    oops

--- kifi-backend/modules/common/conf/evolutions/shoebox/396.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/396.sql
@@ -7,6 +7,6 @@ ALTER TABLE account_event ADD COLUMN charge_id text DEFAULT NULL;
 
 ALTER TABLE paid_account ADD COLUMN billing_cycle_start DATETIME;
 
-insert into evolutions (name, description) values('394.sql', 'dropping proccessing_stage from account_event and adding charge_id, adding billing_cycle_start to paid_account');
+insert into evolutions (name, description) values('396.sql', 'dropping proccessing_stage from account_event and adding charge_id, adding billing_cycle_start to paid_account');
 
 # --- !Downs
