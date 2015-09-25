c0124de8ac85b93710d3b9118ca14ca8d1481eec
gratimax <max@ovsankin.com>
2015-09-24 18:26:08 -0700

    Take out endpoints, fix tests

--- kifi-backend/modules/shoebox/test/com/keepit/commanders/LibraryCommanderTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/commanders/LibraryCommanderTest.scala
@@ -1559,9 +1559,6 @@ class LibraryCommanderTest extends TestKitSupport with SpecificationLike with Sh
         Await.result(libraryInviteCommander.persistInvitesAndNotify(newInvites), Duration(10, "seconds"))
         eliza.inbox.size === 4
 
-        eliza.inbox.count(t => t._2 == NotificationCategory.User.LIBRARY_FOLLOWED && t._4.endsWith("/0.jpg")) === 0
-        eliza.inbox.count(t => t._2 == NotificationCategory.User.LIBRARY_INVITATION && t._4.endsWith("/0.jpg")) === 4
-        eliza.inbox.count(t => t._3 == "https://www.kifi.com/captainamerica/murica") === 3
         db.readOnlyMaster { implicit s => emailRepo.count === 4 }
 
         val t2 = t1.plusMinutes(3) // send another set of invites 3 minutes later - too spammy, should not persist!
