3a210923a593ae0d04f0244fd02d60a0806d55a9
Léo Grimaldi <leo@42go.com>
2015-09-25 10:00:46 -0700

    Fix FriendRecommendationTipTest

--- kifi-backend/modules/shoebox/test/com/keepit/common/store/FakeShoeboxStoreModule.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/common/store/FakeShoeboxStoreModule.scala
@@ -1,13 +1,15 @@
 package com.keepit.common.store
 
 import com.google.inject.{ Singleton, Provides }
+import com.keepit.common.db.slick.Database
+import com.keepit.model.UserRepo
 import com.keepit.social.{ SocialUserRawInfoStore, InMemorySocialUserRawInfoStoreImpl }
 import com.keepit.typeahead.{ InMemoryKifiUserTypeaheadStoreImpl, KifiUserTypeaheadStore, InMemorySocialUserTypeaheadStoreImpl, SocialUserTypeaheadStore }
 
 case class FakeShoeboxStoreModule() extends FakeStoreModule {
 
   @Provides @Singleton
-  def s3ImageStore(s3ImageConfig: S3ImageConfig): S3ImageStore = FakeS3ImageStore(s3ImageConfig)
+  def s3ImageStore(s3ImageConfig: S3ImageConfig, db: Database, userRepo: UserRepo): S3ImageStore = FakeS3ImageStore(s3ImageConfig, db, userRepo)
 
   @Provides @Singleton
   def socialUserRawInfoStore(): SocialUserRawInfoStore = new InMemorySocialUserRawInfoStoreImpl()
