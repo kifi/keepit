644a75bbab063689f41ecfdfc1728de332201803
Andrew Conner <git@andrewconner.org>
2015-09-28 13:06:11 -0700

    Throwing visibility into KeepData, so the extension can be smarter.

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/KeeperInfo.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/KeeperInfo.scala
@@ -62,7 +62,8 @@ case class KeepData(
   id: ExternalId[Keep],
   mine: Boolean,
   removable: Boolean,
-  secret: Boolean, // i.e. library.visibility == SECRET
+  secret: Boolean, // i.e. library.visibility == SECRET; please use `visibility` below instead
+  visibility: LibraryVisibility,
   libraryId: PublicId[Library])
 object KeepData {
   implicit val writes: Writes[KeepData] = (
@@ -70,11 +71,12 @@ object KeepData {
     (__ \ 'mine).write[Boolean] and
     (__ \ 'removable).write[Boolean] and
     (__ \ 'secret).writeNullable[Boolean].contramap[Boolean](Some(_).filter(identity)) and
+    (__ \ 'visibility).write[LibraryVisibility] and
     (__ \ 'libraryId).write[PublicId[Library]]
   )(unlift(KeepData.unapply))
 
   def apply(personalKeep: PersonalKeep): KeepData =
-    KeepData(personalKeep.id, personalKeep.mine, personalKeep.removable, personalKeep.visibility == LibraryVisibility.SECRET, personalKeep.libraryId)
+    KeepData(personalKeep.id, personalKeep.mine, personalKeep.removable, personalKeep.visibility == LibraryVisibility.SECRET, personalKeep.visibility, personalKeep.libraryId)
 }
 
 // The extension uses this object to augment `KeepData` only when needed. It's useless by itself.
