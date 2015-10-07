89f98b5ab22af20a3b8255894056f90128499f74
Andrew Conner <git@andrewconner.org>
2015-10-07 13:13:43 -0700

    Matching styles

--- kifi-backend/modules/shoebox/angular/src/common/mobile-detect.js
+++ kifi-backend/modules/shoebox/angular/src/common/mobile-detect.js
@@ -5,7 +5,7 @@ angular.module('kifi')
 .factory('mobileOS', function () {
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-  if (userAgent.match(/Kifi/i)) {
+  if (userAgent.match( /Kifi/i )) {
     return 'Kifi';
   } if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) ) {
     return 'iOS';
