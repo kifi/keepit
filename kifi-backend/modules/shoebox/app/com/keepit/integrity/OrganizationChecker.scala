09234e0b8658fec900802b3ae0ac74ebd62fd012
Ryan P Brewster <ryan@kifi.com>
2015-10-08 17:34:18 -0700

    Fix org checker (session-in-session issues)
    Library modify/delete is asynchronous, so it opens its own session. This is
    an issue because the org checker was doing all of its modification in a single
    monster read/write session. The fix involved splitting the single monster session
    into multiple sessions.
    
    This would be super expensive, so we first check all the orgs in a single
    read session, pick out just the ones that are broken, then fix each broken
    one independently. We block the thread to wait for the async library fixes
    to complete.

--- kifi-backend/modules/shoebox/app/com/keepit/integrity/OrganizationChecker.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/integrity/OrganizationChecker.scala
@@ -1,19 +1,19 @@
 package com.keepit.integrity
 
-import com.google.inject.{ Inject, Singleton }
+import com.google.inject.{Inject, Singleton}
 import com.keepit.commanders.LibraryCommander
-import com.keepit.common.db.slick.DBSession.RWSession
+import com.keepit.common.db.slick.DBSession.RSession
 import com.keepit.common.db.slick.Database
-import com.keepit.common.db.{ Id, SequenceNumber }
+import com.keepit.common.db.{Id, SequenceNumber}
 import com.keepit.common.healthcheck.AirbrakeNotifier
 import com.keepit.common.logging.Logging
-import com.keepit.common.performance.{ AlertingTimer, StatsdTiming }
-import com.keepit.common.time.{ Clock, _ }
+import com.keepit.common.performance.{AlertingTimer, StatsdTiming}
+import com.keepit.common.time.{Clock, _}
 import com.keepit.model._
-import com.keepit.payments.{ PlanManagementCommander, PaidAccountRepo, PaidAccountStates }
+import com.keepit.payments.{PaidAccountRepo, PaidAccountStates, PlanManagementCommander}
 
-import scala.concurrent.{ Future, ExecutionContext }
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.{Duration, SECONDS}
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 @Singleton
 class OrganizationChecker @Inject() (
@@ -38,61 +38,105 @@ class OrganizationChecker @Inject() (
 
   private[integrity] val ORGANIZATION_INTEGRITY_SEQ = Name[SequenceNumber[Organization]]("organization_integrity_plugin")
   private val ORG_FETCH_SIZE = 20
+  private val MAX_CHECK_TIME = Duration(10, SECONDS)
 
   @AlertingTimer(10 seconds)
   @StatsdTiming("OrganizationChecker.check")
   def check(): Unit = lock.synchronized {
-    db.readWrite { implicit s =>
+    val (allOrgs, brokenOrgs) = db.readOnlyReplica { implicit s =>
       val lastSeq = systemValueRepo.getSequenceNumber(ORGANIZATION_INTEGRITY_SEQ).getOrElse(SequenceNumber.ZERO[Organization])
-      val orgs = orgRepo.getBySequenceNumber(lastSeq, ORG_FETCH_SIZE)
-      if (orgs.nonEmpty) {
-        orgs.foreach { org =>
-          ensureStateIntegrity(org.id.get)
-          ensureOrgSystemLibraryIntegrity(org.id.get)
-        }
-        systemValueRepo.setSequenceNumber(ORGANIZATION_INTEGRITY_SEQ, orgs.map(_.seq).max)
+      val orgsToCheck = orgRepo.getBySequenceNumber(lastSeq, ORG_FETCH_SIZE).toSet
+      val brokenOrgs = orgsToCheck.filter { org => orgHasBrokenState(org.id.get) || orgHasBrokenSystemLibraries(org.id.get) }
+      (orgsToCheck, brokenOrgs)
+    }
+
+    if (allOrgs.nonEmpty) {
+      val maxSeq = allOrgs.map(_.seq).max
+      val tryToFixAllOrgsFut = Future.sequence { brokenOrgs.map(org => fixOrg(org.id.get)) }.map { _ => () }
+      Await.result(tryToFixAllOrgsFut, MAX_CHECK_TIME)
+      db.readWrite { implicit session =>
+        systemValueRepo.setSequenceNumber(ORGANIZATION_INTEGRITY_SEQ, maxSeq)
       }
     }
   }
 
-  private def ensureStateIntegrity(orgId: Id[Organization])(implicit session: RWSession) = {
+  def fixOrg(orgId: Id[Organization]): Future[Unit] = {
+    val fixes = Set(
+      ensureStateIntegrity(orgId),
+      ensureOrgSystemLibraryIntegrity(orgId)
+    )
+    Future.sequence(fixes).map { _ => () }
+  }
+
+  private def orgHasBrokenState(orgId: Id[Organization])(implicit session: RSession): Boolean = {
     val org = orgRepo.get(orgId)
-    if (org.isInactive) {
+    if (org.isActive) true
+    else {
       val zombieMemberships = orgMembershipRepo.getAllByOrgId(org.id.get, excludeState = Some(OrganizationMembershipStates.INACTIVE))
-      if (zombieMemberships.nonEmpty) {
-        airbrake.notify(s"[ORG-STATE-MATCH] Dead org $orgId has zombie memberships for these users: ${zombieMemberships.map(_.userId)}")
-        zombieMemberships.foreach(orgMembershipRepo.deactivate)
-      }
-
       val zombieCandidates = orgMembershipCandidateRepo.getAllByOrgId(org.id.get, states = Set(OrganizationMembershipCandidateStates.ACTIVE))
-      if (zombieCandidates.nonEmpty) {
-        airbrake.notify(s"[ORG-STATE-MATCH] Dead org $orgId has zombie candidates for these users: ${zombieCandidates.map(_.userId)}")
-        zombieCandidates.foreach(orgMembershipCandidateRepo.deactivate)
-      }
-
       val zombieInvites = orgInviteRepo.getAllByOrgId(org.id.get, state = OrganizationInviteStates.ACTIVE)
-      if (zombieInvites.nonEmpty) {
-        airbrake.notify(s"[ORG-STATE-MATCH] Dead org $orgId has zombie invites: ${zombieInvites.map(_.id.get)}")
-        zombieInvites.foreach(orgInviteRepo.deactivate)
-      }
-
       val zombiePaidAccount = paidAccountRepo.maybeGetByOrgId(org.id.get, excludeStates = Set(PaidAccountStates.INACTIVE))
-      if (zombiePaidAccount.isDefined) {
-        airbrake.notify(s"[ORG-STATE-MATCH] Dead org $orgId has zombie paid account: ${zombiePaidAccount.map(_.id.get)}")
-        planManagementCommander.deactivatePaidAccountForOrganization(org.id.get, session)
+      val zombieLibs = libraryRepo.getBySpace(org.id.get, excludeState = Some(LibraryStates.INACTIVE))
+
+      zombieMemberships.nonEmpty || zombieCandidates.nonEmpty || zombieInvites.nonEmpty || zombiePaidAccount.isDefined || zombieLibs.nonEmpty
+    }
+  }
+  private def ensureStateIntegrity(orgId: Id[Organization]): Future[Unit] = {
+    val org = db.readOnlyReplica { implicit session => orgRepo.get(orgId) }
+    if (org.isActive) Future.successful(())
+    else {
+      // There is some easy stuff that can be done synchronously
+      db.readWrite { implicit session =>
+        val zombieMemberships = orgMembershipRepo.getAllByOrgId(org.id.get, excludeState = Some(OrganizationMembershipStates.INACTIVE))
+        if (zombieMemberships.nonEmpty) {
+          airbrake.notify(s"[ORG-STATE-MATCH] Dead org $orgId has zombie memberships for these users: ${zombieMemberships.map(_.userId)}")
+          zombieMemberships.foreach(orgMembershipRepo.deactivate)
+        }
+
+        val zombieCandidates = orgMembershipCandidateRepo.getAllByOrgId(org.id.get, states = Set(OrganizationMembershipCandidateStates.ACTIVE))
+        if (zombieCandidates.nonEmpty) {
+          airbrake.notify(s"[ORG-STATE-MATCH] Dead org $orgId has zombie candidates for these users: ${zombieCandidates.map(_.userId)}")
+          zombieCandidates.foreach(orgMembershipCandidateRepo.deactivate)
+        }
+
+        val zombieInvites = orgInviteRepo.getAllByOrgId(org.id.get, state = OrganizationInviteStates.ACTIVE)
+        if (zombieInvites.nonEmpty) {
+          airbrake.notify(s"[ORG-STATE-MATCH] Dead org $orgId has zombie invites: ${zombieInvites.map(_.id.get)}")
+          zombieInvites.foreach(orgInviteRepo.deactivate)
+        }
+
+        val zombiePaidAccount = paidAccountRepo.maybeGetByOrgId(org.id.get, excludeStates = Set(PaidAccountStates.INACTIVE))
+        if (zombiePaidAccount.isDefined) {
+          airbrake.notify(s"[ORG-STATE-MATCH] Dead org $orgId has zombie paid account: ${zombiePaidAccount.map(_.id.get)}")
+          planManagementCommander.deactivatePaidAccountForOrganization(org.id.get, session)
+        }
       }
 
-      val zombieLibs = libraryRepo.getBySpace(org.id.get, excludeState = Some(LibraryStates.INACTIVE))
-      if (zombieLibs.nonEmpty) {
+      val zombieLibs = db.readOnlyReplica { implicit session =>
+        libraryRepo.getBySpace(org.id.get, excludeState = Some(LibraryStates.INACTIVE))
+      }
+      val libIntegrityFuts = if (zombieLibs.isEmpty) {
+        Set.empty[Future[Unit]]
+      } else {
         airbrake.notify(s"[ORG-STATE-MATCH] Dead org $orgId has zombie libraries: ${zombieLibs.map(_.id.get)}")
         zombieLibs.collect {
-          case lib if lib.canBeModified => libraryCommander.unsafeModifyLibrary(lib, LibraryModifications(space = Some(lib.ownerId)))
+          case lib if lib.canBeModified => libraryCommander.unsafeModifyLibrary(lib, LibraryModifications(space = Some(lib.ownerId))).keepChanges
           case lib if lib.isSystemLibrary => libraryCommander.unsafeAsyncDeleteLibrary(lib.id.get)
         }
       }
+      Future.sequence(libIntegrityFuts).map { _ => () }
     }
   }
-  private def ensureOrgSystemLibraryIntegrity(orgId: Id[Organization])(implicit session: RWSession) = {
+
+  private def orgHasBrokenSystemLibraries(orgId: Id[Organization])(implicit session: RSession): Boolean = {
+    val org = orgRepo.get(orgId)
+    if (org.isInactive) true
+    else {
+      val orgGeneralLibrary = libraryRepo.getBySpaceAndKind(org.id.get, LibraryKind.SYSTEM_ORG_GENERAL)
+      orgGeneralLibrary.size != 1
+    }
+  }
+  private def ensureOrgSystemLibraryIntegrity(orgId: Id[Organization]): Future[Unit] = db.readWriteAsync { implicit session =>
     val org = orgRepo.get(orgId)
     if (org.isActive) {
       val orgGeneralLibrary = libraryRepo.getBySpaceAndKind(org.id.get, LibraryKind.SYSTEM_ORG_GENERAL)
