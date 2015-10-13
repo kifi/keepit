b8a0a6ca7b84a1d1a35989f281ebac6715fc705b
Cam Hashemi <camhashemi@gmail.com>
2015-10-12 17:34:00 -0700

    add Feature.ViewSettings

--- kifi-backend/modules/common/conf/evolutions/shoebox/398.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/398.sql
@@ -27,7 +27,7 @@ INSERT INTO `paid_plan` (`id`, `created_at`, `updated_at`, `state`, `name`, `bil
 VALUES
   (1, '2015-08-19 21:50:48', '2015-08-19 21:50:48', 'active', 'Test', 1, 0, 'normal',
   '["force_edit_libraries","invite_members","group_messaging","edit_organization","view_organization","remove_libraries","create_slack_integration","export_keeps","publish_libraries","view_members"]',
-  '{"publish_libraries":"members","group_messaging":"members","force_edit_libraries":"disabled","export_keeps":"admins","view_members":"anyone","create_slack_integration":"disabled","edit_organization":"admins","remove_libraries":"members","invite_members":"members","view_organization":"anyone"}'
+  '{"publish_libraries":"members","group_messaging":"members","force_edit_libraries":"disabled","export_keeps":"admins","view_members":"anyone","create_slack_integration":"disabled","edit_organization":"admins","remove_libraries":"members","invite_members":"members","view_organization":"anyone", "view_settings":"members"}'
   );
 
 insert into evolutions(name, description) values('398.sql', 'refactor org permissions: drop org_membership.permissions, drop org.base_permissions, paid_plan.features -> {paid_plan.editable_features, paid_plan.default_settings}, paid_account.feature_settings -> org_config.settings');
