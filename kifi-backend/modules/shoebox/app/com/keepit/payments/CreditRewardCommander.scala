0d5c8917d3ae1b55ec3f7d76834cc57bc8e49ddb
Ryan P Brewster <ryan@kifi.com>
2015-10-22 16:06:08 -0700

    Started implementing methods in CreditRewardCmndr

--- kifi-backend/modules/shoebox/app/com/keepit/payments/CreditRewardCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/payments/CreditRewardCommander.scala
@@ -6,14 +6,16 @@ import com.keepit.common.db.slick.Database
 import com.keepit.common.logging.Logging
 import com.keepit.common.time._
 import com.keepit.model.{ OrganizationRepo, Organization }
+import com.keepit.payments.CreditCodeFail.{ CreditCodeNotApplicable, NoPaidAccountException, CreditCodeNotFoundException }
 import org.apache.commons.lang3.RandomStringUtils
 
 import scala.concurrent.ExecutionContext
+import scala.util.{ Success, Failure, Try }
 
 @ImplementedBy(classOf[CreditRewardCommanderImpl])
 trait CreditRewardCommander {
   def getOrCreateReferralCode(orgId: Id[Organization]): CreditCode
-  def createCreditCodeRewards(info: CreditCodeInfo): CreditCodeRewards
+  def applyCreditCode(req: CreditCodeApplyRequest): Try[CreditCodeRewards]
 }
 
 @Singleton
@@ -21,12 +23,12 @@ class CreditRewardCommanderImpl @Inject() (
   db: Database,
   orgRepo: OrganizationRepo,
   creditCodeInfoRepo: CreditCodeInfoRepo,
+  creditRewardRepo: CreditRewardRepo,
+  accountRepo: PaidAccountRepo,
   clock: Clock,
   eventCommander: AccountEventTrackingCommander,
   implicit val defaultContext: ExecutionContext)
     extends CreditRewardCommander with Logging {
-
-  private val orgReferralCredit = DollarAmount.dollars(420)
 
   def getOrCreateReferralCode(orgId: Id[Organization]): CreditCode = {
     db.readWrite { implicit session =>
@@ -43,5 +45,36 @@ class CreditRewardCommanderImpl @Inject() (
       creditCodeInfo.code
     }
   }
-  def createCreditCodeRewards(info: CreditCodeInfo): CreditCodeRewards = ???
+  def applyCreditCode(req: CreditCodeApplyRequest): Try[CreditCodeRewards] = db.readWrite { implicit session =>
+    for {
+      creditCodeInfo <- creditCodeInfoRepo.getByCode(req.code).map(Success(_)).getOrElse(Failure(CreditCodeNotFoundException(req.code)))
+      accountId <- req.orgId.map(accountRepo.getAccountId(_)).map(Success(_)).getOrElse(Failure(NoPaidAccountException(req.applierId, req.orgId)))
+      appliable <- if (creditCodeInfo.referrer.exists(r => r.organizationId.isDefined && r.organizationId == req.orgId)) Failure(CreditCodeNotApplicable(req)) else Success(true)
+    } yield {
+      creditCodeInfo.kind match {
+        case CreditCodeKind.Coupon => ???
+        case CreditCodeKind.OrganizationReferral =>
+          val targetReward = Reward(RewardKind.OrganizationCreation)(RewardKind.OrganizationCreation.Created)(req.orgId.get)
+          val targetCreditReward = creditRewardRepo.save(CreditReward(
+            accountId = accountId,
+            credit = creditCodeInfo.credit,
+            applied = None,
+            reward = targetReward,
+            unrepeatable = req.orgId.map(UnrepeatableRewardKey.ForOrganization),
+            code = Some(UsedCreditCode(creditCodeInfo, req.applierId))
+          ))
+
+          val referrerReward = Reward(RewardKind.OrganizationReferral)(RewardKind.OrganizationReferral.Created)(req.orgId.get)
+          val referrerCreditReward = Some(creditRewardRepo.save(CreditReward(
+            accountId = accountId,
+            credit = creditCodeInfo.referrerCredit.get,
+            applied = None,
+            reward = referrerReward,
+            unrepeatable = req.orgId.map(UnrepeatableRewardKey.ForOrganization),
+            code = Some(UsedCreditCode(creditCodeInfo, req.applierId))
+          )))
+          CreditCodeRewards(target = targetCreditReward, referrer = referrerCreditReward)
+      }
+    }
+  }
 }
