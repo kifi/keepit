6b7d1be07718644389ecbf5955daedc9fde37f36
Léo Grimaldi <leo@42go.com>
2015-10-22 12:27:01 -0700

    Rename 403.sql to 405.sql

--- kifi-backend/modules/common/conf/evolutions/shoebox/405.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/405.sql
@@ -4,6 +4,6 @@
 
 ALTER TABLE paid_account ADD COLUMN payment_due_at datetime DEFAULT NULL AFTER credit;
 
-insert into evolutions(name, description) values('403.sql', 'add column paid_account.payment_due_at');
+insert into evolutions(name, description) values('405.sql', 'add column paid_account.payment_due_at');
 
 # --- !Downs
