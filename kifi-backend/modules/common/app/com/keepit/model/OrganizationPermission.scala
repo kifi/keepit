3a83254442c6d050a01f5a8a7a6c64af505140a3
Ryan P Brewster <ryan@kifi.com>
2015-10-26 19:05:11 -0700

    Safereads for Sets of {Feature, OrgPermission}
    The only way to deserialize (easily) a Set[Feature] or
    Set[OrganizationPermission] is safely

--- kifi-backend/modules/common/app/com/keepit/model/OrganizationPermission.scala
+++ kifi-backend/modules/common/app/com/keepit/model/OrganizationPermission.scala
@@ -1,5 +1,6 @@
 package com.keepit.model
 
+import com.keepit.common.json.TraversableFormat
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
@@ -42,10 +43,14 @@ object OrganizationPermission {
     VIEW_SETTINGS
   )
 
-  implicit val format: Format[OrganizationPermission] =
+  val format: Format[OrganizationPermission] =
     Format(__.read[String].map(OrganizationPermission(_)), new Writes[OrganizationPermission] {
       def writes(o: OrganizationPermission) = JsString(o.value)
     })
+
+  implicit val writes = Writes(format.writes)
+  val reads = Reads(format.reads)
+  implicit val safeSetReads = TraversableFormat.safeSetReads[OrganizationPermission](reads)
 
   def apply(str: String): OrganizationPermission = {
     str match {
@@ -67,100 +72,6 @@ object OrganizationPermission {
       case VIEW_MEMBERS.value => VIEW_MEMBERS
       case VIEW_ORGANIZATION.value => VIEW_ORGANIZATION
       case VIEW_SETTINGS.value => VIEW_SETTINGS
-    }
-  }
-
-}
-case class BasePermissions(permissionsMap: PermissionsMap) {
-  def forRole(role: OrganizationRole): Set[OrganizationPermission] = permissionsMap(Some(role))
-  def forNonmember: Set[OrganizationPermission] = permissionsMap(None)
-
-  def withPermissions(roleAndPermissions: (Option[OrganizationRole], Set[OrganizationPermission])*): BasePermissions =
-    this.copy(permissionsMap overwriteWith PermissionsMap(roleAndPermissions.toMap))
-
-  // Return a BasePermissions where "role" has added and removed permissions
-  def modified(roleOpt: Option[OrganizationRole], added: Set[OrganizationPermission], removed: Set[OrganizationPermission]): BasePermissions =
-    this.copy(permissionsMap ++ PermissionsMap.just(roleOpt -> added) -- PermissionsMap.just(roleOpt -> removed))
-
-  def applyPermissionsDiff(pdiff: PermissionsDiff): BasePermissions =
-    this.copy(permissionsMap ++ pdiff.added -- pdiff.removed)
-
-  def addPermission(kv: (Option[OrganizationRole], OrganizationPermission)) =
-    this.applyPermissionsDiff(PermissionsDiff(added = PermissionsMap.just(kv._1 -> Set(kv._2))))
-
-  def removePermission(kv: (Option[OrganizationRole], OrganizationPermission)) =
-    this.applyPermissionsDiff(PermissionsDiff(removed = PermissionsMap.just(kv._1 -> Set(kv._2))))
-
-  def diff(that: BasePermissions): PermissionsDiff = PermissionsDiff(added = this.permissionsMap -- that.permissionsMap, that.permissionsMap -- this.permissionsMap)
-}
-object BasePermissions {
-  def apply(pm: Map[Option[OrganizationRole], Set[OrganizationPermission]]): BasePermissions = BasePermissions(PermissionsMap(pm))
-  def apply(kvs: (Option[OrganizationRole], Set[OrganizationPermission])*): BasePermissions = BasePermissions(PermissionsMap(kvs.toMap))
-  implicit val format: Format[BasePermissions] =
-    Format(__.read[PermissionsMap].map(BasePermissions(_)), new Writes[BasePermissions] {
-      def writes(bp: BasePermissions) = Json.toJson(bp.permissionsMap)
-    })
-}
-
-case class PermissionsDiff(added: PermissionsMap = PermissionsMap.empty, removed: PermissionsMap = PermissionsMap.empty)
-object PermissionsDiff {
-  val empty = PermissionsDiff()
-  implicit val format: Format[PermissionsDiff] = (
-    (__ \ 'add).formatNullable[PermissionsMap] and
-    (__ \ 'remove).formatNullable[PermissionsMap]
-  )(PermissionsDiff.fromOptions, PermissionsDiff.toOptions)
-
-  def justAdd(kvs: (Option[OrganizationRole], Set[OrganizationPermission])*): PermissionsDiff = PermissionsDiff(added = PermissionsMap(kvs.toMap))
-  def justRemove(kvs: (Option[OrganizationRole], Set[OrganizationPermission])*): PermissionsDiff = PermissionsDiff(removed = PermissionsMap(kvs.toMap))
-
-  def fromOptions(addedOpt: Option[PermissionsMap], removedOpt: Option[PermissionsMap]): PermissionsDiff =
-    PermissionsDiff(addedOpt.getOrElse(PermissionsMap.empty), removedOpt.getOrElse(PermissionsMap.empty))
-
-  def toOptions(pdiff: PermissionsDiff): (Option[PermissionsMap], Option[PermissionsMap]) =
-    (if (pdiff.added.isEmpty) None else Some(pdiff.added),
-      if (pdiff.removed.isEmpty) None else Some(pdiff.removed))
-}
-
-case class PermissionsMap(pm: Map[Option[OrganizationRole], Set[OrganizationPermission]]) {
-  def ++(that: PermissionsMap) = {
-    val allKeys = pm.keySet ++ that.pm.keySet
-    val newPM = allKeys.map { k => k -> (find(k) ++ that.find(k)) }.toMap
-    PermissionsMap(newPM)
-  }
-  def --(that: PermissionsMap) = {
-    val allKeys = pm.keySet ++ that.pm.keySet
-    val newPM = allKeys.map { k => k -> (find(k) -- that.find(k)) }.toMap
-    PermissionsMap(newPM)
-  }
-  def overwriteWith(that: PermissionsMap) = {
-    PermissionsMap(pm ++ that.pm)
-  }
-
-  def find(roleOpt: Option[OrganizationRole]): Set[OrganizationPermission] = pm.getOrElse(roleOpt, Set.empty)
-  def apply(roleOpt: Option[OrganizationRole]) = find(roleOpt)
-  def isEmpty: Boolean = pm.isEmpty
-}
-object PermissionsMap {
-  val empty: PermissionsMap = PermissionsMap(Map.empty[Option[OrganizationRole], Set[OrganizationPermission]])
-  def just(kvs: (Option[OrganizationRole], Set[OrganizationPermission])*): PermissionsMap = PermissionsMap(kvs.toMap)
-  implicit val format: Format[PermissionsMap] = new Format[PermissionsMap] {
-    def reads(json: JsValue): JsResult[PermissionsMap] = {
-      json.validate[JsObject].map { obj =>
-        val pm = (for ((k, v) <- obj.value) yield {
-          val roleOpt = if (k == "none") None else Some(OrganizationRole(k))
-          val permissions = v.as[Set[OrganizationPermission]]
-          roleOpt -> permissions
-        }).toMap
-        PermissionsMap(pm)
-      }
-    }
-    def writes(permissionsMap: PermissionsMap): JsValue = {
-      val jsonMap = for ((roleOpt, permissions) <- permissionsMap.pm) yield {
-        val k = roleOpt.map(_.value).getOrElse("none")
-        val v = Json.toJson(permissions)
-        k -> v
-      }
-      JsObject(jsonMap.toSeq)
     }
   }
 }
