157ff0079be101a47baf921765afe72528b5d758
Ryan P Brewster <ryan@kifi.com>
2015-10-30 14:44:27 -0700

    Made Dashboard commander injectable, fixed bug regarding new orgs

--- kifi-backend/modules/shoebox/app/com/keepit/payments/PaymentsDashboardCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/payments/PaymentsDashboardCommander.scala
@@ -1,5 +1,6 @@
 package com.keepit.payments
 
+import com.google.inject.{ Inject, ImplementedBy, Singleton }
 import com.keepit.common.db.slick.DBSession.RSession
 import com.keepit.common.db.slick.Database
 import com.keepit.common.time.{ Clock, DEFAULT_DATE_TIME_ZONE }
@@ -8,13 +9,19 @@ import com.keepit.model.OrganizationRepo
 
 import scala.collection.mutable
 
-class PaymentsDashboardCommander(
+@ImplementedBy(classOf[PaymentsDashboardCommanderImpl])
+trait PaymentsDashboardCommander {
+  def generateDashboard(): AdminPaymentsDashboard
+}
+
+@Singleton
+class PaymentsDashboardCommanderImpl @Inject() (
     db: Database,
     orgRepo: OrganizationRepo,
     paidPlanRepo: PaidPlanRepo,
     paidAccountRepo: PaidAccountRepo,
     accountEventRepo: AccountEventRepo,
-    clock: Clock) {
+    clock: Clock) extends PaymentsDashboardCommander {
   private def createAdminAccountView(account: PaidAccount)(implicit session: RSession): AdminAccountView = {
     AdminAccountView(
       organization = orgRepo.get(account.orgId)
@@ -32,21 +39,23 @@ class PaymentsDashboardCommander(
     }.toMap
     val oldPlanEnrollments = {
       // mutable map logic ahoy
-      val m_planByAccount = mutable.Map(paidAccountRepo.all.map(a => a.id.get -> a.planId): _*)
+      val m_planByAccount = mutable.Map(paidAccountRepo.all.map(a => a.id.get -> Option(a.planId)): _*)
       val m_usersByAccount = mutable.Map(paidAccountRepo.all.map(a => a.id.get -> a.activeUsers): _*)
       accountEventRepo.adminGetByKindSince(AccountEventKind.all, now.minusWeeks(1)).map(e => (e.accountId, e.action)).foreach {
+        case (accountId, AccountEventAction.OrganizationCreated(initialPlan, _)) =>
+          m_planByAccount(accountId) = None
         case (accountId, AccountEventAction.PlanChanged(oldPlan, newPlan, _)) =>
-          m_planByAccount(accountId) = oldPlan
+          m_planByAccount(accountId) = Some(oldPlan)
         case (accountId, AccountEventAction.UserJoinedOrganization(_, _)) =>
-          m_usersByAccount(accountId) = m_usersByAccount(accountId) - 1
+          m_usersByAccount(accountId) -= 1
         case (accountId, AccountEventAction.UserLeftOrganization(_, _)) =>
-          m_usersByAccount(accountId) = m_usersByAccount(accountId) + 1
+          m_usersByAccount(accountId) += 1
         case _ =>
       }
       plans.map { p =>
         p -> PlanEnrollment(
-          numAccounts = m_planByAccount.collect { case (accountId, planId) if planId == p.id.get => 1 }.sum,
-          numActiveUsers = m_usersByAccount.collect { case (accountId, numUsers) if m_planByAccount(accountId) == p.id.get => numUsers }.sum
+          numAccounts = m_planByAccount.collect { case (accountId, planId) if planId.contains(p.id.get) => 1 }.sum,
+          numActiveUsers = m_usersByAccount.collect { case (accountId, numUsers) if m_planByAccount(accountId).contains(p.id.get) => numUsers }.sum
         )
       }.toMap
     }
