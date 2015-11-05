9a77c1c0f21eeeba6e548257605192364d8945c6
Ryan P Brewster <ryan@kifi.com>
2015-11-05 08:30:21 -0800

    Finally got things working.

--- kifi-backend/modules/common/test/com/keepit/common/reflection/EnumeratorTest.scala
+++ kifi-backend/modules/common/test/com/keepit/common/reflection/EnumeratorTest.scala
@@ -5,16 +5,26 @@ import org.specs2.mutable.Specification
 sealed abstract class Foo(val value: String)
 object Foo extends Enumerator[Foo] {
   case object A extends Foo("a")
-  case object B extends Foo("b")
-  case object C extends Foo("c")
-  def all = _all.toSet
+  object Bar extends Enumerator[Foo] {
+    case object B extends Foo("b")
+    object Baz {
+      case object C extends Foo("c")
+    }
+    case class Obstacle(y: String)
+    def all: Set[Foo] = _all.toSet
+  }
+  case class Rando(x: Int)
+  def all: Set[Foo] = _all.toSet
 }
 
 class EnumeratorTest extends Specification {
   "EnumeratorTest" should {
-    "retrieve all of a sealed trait's subclasses" in {
+    "retrieve all of an trait's subclasses enclosed within a given object" in {
       Foo.all must haveSize(3)
       Foo.all.map(_.value) === Set("a", "b", "c")
+
+      Foo.Bar.all must haveSize(2)
+      Foo.Bar.all.map(_.value) === Set("b", "c")
     }
   }
 }
