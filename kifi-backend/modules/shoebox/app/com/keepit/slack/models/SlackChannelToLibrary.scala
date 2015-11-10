bf08eb218ed5183ebeb6f5156a23f8e8607a8a64
Léo Grimaldi <leo@42go.com>
2015-11-10 11:20:00 -0800

    Introduce SlackChannelToLibraryRepo

--- kifi-backend/modules/shoebox/app/com/keepit/slack/models/SlackChannelToLibrary.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/slack/models/SlackChannelToLibrary.scala
@@ -1,20 +1,96 @@
 package com.keepit.slack.models
 
-import com.keepit.common.db.{ Id, State, States }
+import com.google.inject.{ Inject, Singleton, ImplementedBy }
+import com.keepit.common.db.slick.{ DbRepo, DataBaseComponent, Repo }
+import com.keepit.common.db.{ ModelWithState, Id, State, States }
 import com.keepit.common.time._
-import com.keepit.model.Library
+import com.keepit.model.{ Library }
 import org.joda.time.DateTime
 
 case class SlackChannelToLibrary(
-  id: Option[Id[SlackChannelToLibrary]] = None,
-  createdAt: DateTime = currentDateTime,
-  updatedAt: DateTime = currentDateTime,
-  state: State[SlackChannelToLibrary] = SlackChannelToLibraryStates.ACTIVE,
-  membershipId: Id[SlackTeamMembership],
-  channel: SlackChannel,
-  library: Id[Library],
-  status: SlackIntegrationStatus,
-  lastProcessedAt: Option[DateTime],
-  lastMessageAt: Option[DateTime])
+    id: Option[Id[SlackChannelToLibrary]] = None,
+    createdAt: DateTime = currentDateTime,
+    updatedAt: DateTime = currentDateTime,
+    state: State[SlackChannelToLibrary] = SlackChannelToLibraryStates.ACTIVE,
+    membershipId: Id[SlackTeamMembership],
+    channel: SlackChannel,
+    libraryId: Id[Library],
+    status: SlackIntegrationStatus,
+    lastProcessedAt: Option[DateTime],
+    lastMessageAt: Option[DateTime]) extends ModelWithState[SlackChannelToLibrary] {
+  def withId(id: Id[SlackChannelToLibrary]) = this.copy(id = Some(id))
+  def withUpdateTime(now: DateTime) = this.copy(updatedAt = now)
+}
 
 object SlackChannelToLibraryStates extends States[SlackChannelToLibrary]
+
+@ImplementedBy(classOf[SlackChannelToLibraryRepoImpl])
+trait SlackChannelToLibraryRepo extends Repo[SlackChannelToLibrary]
+
+@Singleton
+class SlackChannelToLibraryRepoImpl @Inject() (
+    val db: DataBaseComponent,
+    val clock: Clock) extends DbRepo[SlackChannelToLibrary] with SlackChannelToLibraryRepo {
+
+  import com.keepit.common.db.slick.DBSession._
+  import db.Driver.simple._
+
+  implicit val channelColumnType = SlackDbColumnTypes.channel(db)
+  implicit val statusColumnType = SlackIntegrationStatus.columnType(db)
+
+  private def stlFromDbRow(
+    id: Option[Id[SlackChannelToLibrary]],
+    createdAt: DateTime,
+    updatedAt: DateTime,
+    state: State[SlackChannelToLibrary],
+    membershipId: Id[SlackTeamMembership],
+    channel: SlackChannel,
+    libraryId: Id[Library],
+    status: SlackIntegrationStatus,
+    lastProcessedAt: Option[DateTime],
+    lastMessageAt: Option[DateTime]) = {
+    SlackChannelToLibrary(
+      id,
+      createdAt,
+      updatedAt,
+      state,
+      membershipId,
+      channel,
+      libraryId,
+      status,
+      lastProcessedAt,
+      lastMessageAt
+    )
+  }
+
+  private def stlToDbRow(stl: SlackChannelToLibrary) = Some((
+    stl.id,
+    stl.createdAt,
+    stl.updatedAt,
+    stl.state,
+    stl.membershipId,
+    stl.channel,
+    stl.libraryId,
+    stl.status,
+    stl.lastProcessedAt,
+    stl.lastMessageAt
+  ))
+
+  type RepoImpl = SlackChannelToLibraryTable
+
+  class SlackChannelToLibraryTable(tag: Tag) extends RepoTable[SlackChannelToLibrary](db, tag, "slack_channel_to_library") {
+    def membershipId = column[Id[SlackTeamMembership]]("membership_id", O.NotNull)
+    def channel = column[SlackChannel]("channel", O.NotNull)
+    def libraryId = column[Id[Library]]("library_id", O.NotNull)
+    def status = column[SlackIntegrationStatus]("status", O.NotNull)
+    def lastProcessedAt = column[Option[DateTime]]("last_processed_at", O.Nullable)
+    def lastMessageAt = column[Option[DateTime]]("last_message_at", O.Nullable)
+    def * = (id.?, createdAt, updatedAt, state, membershipId, channel, libraryId, status, lastProcessedAt, lastMessageAt) <> ((stlFromDbRow _).tupled, stlToDbRow _)
+  }
+
+  private def activeRows = rows.filter(row => row.state === SlackChannelToLibraryStates.ACTIVE)
+  def table(tag: Tag) = new SlackChannelToLibraryTable(tag)
+  initTable()
+  override def deleteCache(info: SlackChannelToLibrary)(implicit session: RSession): Unit = {}
+  override def invalidateCache(info: SlackChannelToLibrary)(implicit session: RSession): Unit = {}
+}
