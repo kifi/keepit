0de7a46069430ad6d59f4baab225644bd21ba72d
Ryan P Brewster <ryan@kifi.com>
2015-11-10 13:55:18 -0800

    I totally didn't compile. I am bad and I feel bad.

--- kifi-backend/modules/shoebox/app/com/keepit/slack/FakeSlackClientImpl.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/slack/FakeSlackClientImpl.scala
@@ -7,6 +7,6 @@ import scala.util.Try
 
 class FakeSlackClientImpl extends SlackClient {
   def sendToSlack(url: String, msg: SlackMessage): Future[Try[Unit]] = ???
-  def generateAuthorizationRequest(scopes: Set[SlackAuthScope], state: JsObject, redirectUri: Option[String] = None): String = ???
+  def generateAuthorizationRequest(scopes: Set[SlackAuthScope], state: JsObject): String = ???
   def processAuthorizationResponse(code: SlackAuthorizationCode, state: String): Future[Try[(SlackAuthorizationResponse, JsObject)]] = ???
 }
