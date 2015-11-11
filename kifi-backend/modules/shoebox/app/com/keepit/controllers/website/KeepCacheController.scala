6137964ebfb8b14caaf1092e03959716a5cc9a0d
Andrew Conner <git@andrewconner.org>
2015-11-11 11:52:50 -0800

    Switching to using normalized uris, but things aren't working!? Added better response

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/KeepCacheController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/KeepCacheController.scala
@@ -4,7 +4,7 @@ import com.google.inject.Inject
 import com.keepit.common.controller.{ UserActionsHelper, ShoeboxServiceController, UserActions }
 import com.keepit.common.db.ExternalId
 import com.keepit.common.db.slick.Database
-import com.keepit.model.{ KeepRepo, Keep }
+import com.keepit.model.{ KeepRepo, Keep, NormalizedURIRepo }
 import com.keepit.rover.RoverServiceClient
 import com.keepit.rover.article.EmbedlyArticle
 import play.twirl.api.Html
@@ -16,14 +16,19 @@ class KeepCacheController @Inject() (
     roverServiceClient: RoverServiceClient,
     db: Database,
     keepRepo: KeepRepo,
+    normalizedUriRepo: NormalizedURIRepo,
     val userActionsHelper: UserActionsHelper,
     implicit val ec: ExecutionContext) extends UserActions with ShoeboxServiceController {
 
   def getCachedKeep(id: ExternalId[Keep]) = MaybeUserPage.async { request =>
-    val keepOpt = db.readOnlyReplica(keepRepo.getOpt(id)(_))
-    keepOpt.map { keep =>
-      roverServiceClient.getOrElseFetchRecentArticle(keep.url, 182.days)(EmbedlyArticle).map {
-        case Some(article) => article.content.content
+    val nUriOpt = db.readOnlyReplica { implicit session =>
+      keepRepo.getOpt(id).map { keep =>
+        normalizedUriRepo.get(keep.uriId)
+      }
+    }
+    nUriOpt.map { nUri =>
+      roverServiceClient.getOrElseFetchRecentArticle(nUri.url, 182.days)(EmbedlyArticle).map {
+        case Some(article) => Some(article.content.description.getOrElse("no desc") + article.content.content.getOrElse("no content"))
         case None => None
       }
     }.getOrElse(Future.successful(None)).map {
