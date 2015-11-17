0acf418202197cd4d50c64d2b4fd2d8ee425c000
Jared Petker <jp@kifi.com>
2015-11-16 16:52:16 -0800

    more fixing slack integration styling

--- kifi-backend/modules/shoebox/angular/src/libraries/library.js
+++ kifi-backend/modules/shoebox/angular/src/libraries/library.js
@@ -144,7 +144,7 @@ angular.module('kifi')
     $scope.$error = $scope.$error || {};
     $scope.userIsOwner = $rootScope.userLoggedIn && library.owner.id === profileService.me.id;
     $scope.isAdminExperiment = (profileService.me.experiments || []).indexOf('admin') !== -1;
-    $scope.canCreateSlackIntegration = (library.permissions || []).indexOf('create_slack_integration') !== -1;
+    $scope.canCreateSlackIntegration = (profileService.me.experiments || []).indexOf('slack') !== -1;
 
     // slack stuff
     $scope.slackIntegrations = [];
