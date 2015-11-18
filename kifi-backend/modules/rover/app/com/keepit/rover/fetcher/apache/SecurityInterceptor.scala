168950de95f4a958017a802c804e8ecb64d82de8
Andrew Conner <git@andrewconner.org>
2015-11-18 14:25:44 -0800

    What a bad API

--- kifi-backend/modules/rover/app/com/keepit/rover/fetcher/apache/SecurityInterceptor.scala
+++ kifi-backend/modules/rover/app/com/keepit/rover/fetcher/apache/SecurityInterceptor.scala
@@ -3,44 +3,29 @@ package com.keepit.rover.fetcher.apache
 import com.keepit.common.logging.Logging
 import com.keepit.common.net.URI
 import org.apache.http.HttpHeaders._
-import org.apache.http.client.methods.HttpUriRequest
+import org.apache.http.client.methods.{ HttpRequestWrapper, HttpUriRequest }
 import org.apache.http.protocol.HttpContext
 import org.apache.http._
 
 trait SecurityInterceptor extends Logging {
-  protected def process(host: String): Boolean = {
+  def throwIfBannedHost(host: String): Boolean = {
     val trimmed = host.trim
     if (SecurityInterceptor.bannedHosts.exists(trimmed.startsWith)) {
       //throw new HttpException(s"Tried to scrape banned domain ${host.toString}")
       log.error(s"[SecurityInterceptor] Blocking $trimmed")
       true
     } else {
-      log.info(s"[SecurityInterceptr] Allowing $trimmed")
+      log.info(s"[SecurityInterceptor] Allowing $trimmed")
       false
-    }
-  }
-}
-
-class RequestSecurityInterceptor extends HttpRequestInterceptor with SecurityInterceptor {
-  def process(request: HttpRequest, context: HttpContext): Unit = {
-    request match {
-      case uriRequest: HttpUriRequest =>
-        Option(uriRequest.getURI).flatMap(u => Option(u.getHost)).map(process).getOrElse {
-          val deets = {
-            uriRequest.getClass.getCanonicalName + "  " + uriRequest.getURI.toString + " " + uriRequest.getAllHeaders.map(h => h.getName -> h.getValue).toList.mkString(", ")
-          }
-          log.warn(s"[SecurityInterceptor-2] Couldn't parse ${request.toString} Allowing. $deets")
-        }
-      case _ =>
     }
   }
 }
 
 class ResponseSecurityInterceptor extends HttpResponseInterceptor with SecurityInterceptor {
   def process(response: HttpResponse, context: HttpContext): Unit = {
-    val loc = Option(response.getFirstHeader(LOCATION)).map(_.getValue)
+    val loc = Option(response.getFirstHeader(LOCATION)).flatMap(l => URI.parse(l.getValue).toOption).flatMap(_.host).map(_.name)
     val host = Option(response.getFirstHeader(HOST)).map(_.getValue)
-    loc.orElse(host).map(process).getOrElse {
+    loc.orElse(host).map(throwIfBannedHost).getOrElse {
       val deets = {
         response.getClass.getCanonicalName + "  " + response.getAllHeaders.map(h => h.getName -> h.getValue).toList.mkString(", ")
       }
@@ -50,7 +35,7 @@ class ResponseSecurityInterceptor extends HttpResponseInterceptor with SecurityI
   }
 }
 
-object SecurityInterceptor {
+object SecurityInterceptor extends SecurityInterceptor {
   val bannedHosts = Set(
     "10.",
     "169.254.169.254",
