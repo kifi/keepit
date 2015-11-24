ec9d8ab1473f45183db039fc10f055ee6abb5a97
Carlos Vializ <cvializ@vt.edu>
2015-11-24 10:26:49 -0800

    If symbols are not ready, wait for them to be ready.

--- kifi-backend/modules/shoebox/angular/src/common/directives/symbol-sprite.js
+++ kifi-backend/modules/shoebox/angular/src/common/directives/symbol-sprite.js
@@ -5,27 +5,42 @@
 'use strict';
 angular.module('kifi')
 
-.directive('kfSymbolSprite', [function () {
-  return {
-    restrict: 'A',
-    compile: function () {
-      return function (scope, element, attrs) {
-        var paths = angular.element('symbol#' + attrs.icon);
-        var path = paths.children('g').clone()[0];
-        var svgElement;
-        var className;
+.directive('kfSymbolSprite', [
+  '$window',
+  function ($window) {
+    var MutationObserver = $window.MutationObserver;
 
-        if (path) {
-          svgElement = element[0];
-          className = svgElement.getAttribute('class') || '';
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        function renderSymbolSprite() {
+          var paths = angular.element('symbol#' + attrs.icon);
+          var path = paths.children('g').clone()[0];
 
-          svgElement.appendChild(path);
-          svgElement.setAttribute('class', className + (className ? ' ' : '') + 'symbol-sprite');
-          svgElement.setAttribute('viewBox', '0 0 512 512');
-        } else {
-          element.remove();
+          var svgElement;
+          var className;
+
+          if (path) {
+            svgElement = element[0];
+            className = svgElement.getAttribute('class') || '';
+
+            svgElement.appendChild(path);
+            svgElement.setAttribute('class', className + (className ? ' ' : '') + 'symbol-sprite');
+            svgElement.setAttribute('viewBox', '0 0 512 512');
+          } else {
+            element.remove();
+          }
         }
-      };
-    }
-  };
-}]);
+
+        var symbolSpriteContainer = angular.element('.symbol-sprite-container')[0];
+        var observer;
+        if (symbolSpriteContainer.children.length === 0) {
+          observer = new MutationObserver(renderSymbolSprite);
+          observer.observe(symbolSpriteContainer, { childList: true });
+        } else {
+          renderSymbolSprite();
+        }
+      }
+    };
+  }
+]);
