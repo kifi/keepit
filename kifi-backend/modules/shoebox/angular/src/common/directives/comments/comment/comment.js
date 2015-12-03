b8d26017160dd14f65e35673f9ebe15aae22d3b9
Jared Petker <jp@kifi.com>
2015-12-02 17:40:39 -0800

    (Ground / leg) work for getting look here links / images and such working with comments

--- kifi-backend/modules/shoebox/angular/src/common/directives/comments/comment/comment.js
+++ kifi-backend/modules/shoebox/angular/src/common/directives/comments/comment/comment.js
@@ -2,15 +2,19 @@
 
 angular.module('kifi')
 
-  .directive('kfKeepComment', [
-    function () {
+  .directive('kfKeepComment', ['messageFormattingService',
+    function (messageFormattingService) {
       return {
         restrict: 'A',
         scope: {
           comment: '='
         },
         templateUrl: 'common/directives/comments/comment/comment.tpl.html',
-        link: function () {
+        link: function (scope, element) {
+          //var queryResult = element[0].querySelector('.kf-keep-comment-commenter-text');
+          //var wrappedQueryResult = angular.element(queryResult);
+          scope.commentParts = messageFormattingService.full(scope.comment.text);
+          //wrappedQueryResult.append(messageFormattingService.full(scope.comment.text));
 
         }
       };
