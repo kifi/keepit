76d4512e139aff7627edb46d499cb084cf001106
Carlos Vializ <cvializ@vt.edu>
2015-12-15 18:11:28 -0800

    Use actual values

--- kifi-backend/modules/shoebox/angular/src/orgProfile/orgProfileLibrariesCtrl.js
+++ kifi-backend/modules/shoebox/angular/src/orgProfile/orgProfileLibrariesCtrl.js
@@ -144,7 +144,8 @@ angular.module('kifi')
     };
     $rootScope.$emit('trackOrgProfileEvent', 'view', { type: 'org_profile:libraries'});
 
-    if (true) { // if me.prefs.show_slack_upsell
+    if (profileService.prefs.slack_int_promo) { // if me.prefs.show_slack_upsell
+      profileService.savePrefs({ slack_int_promo: false });
       libraryService
       .getLibraryByHandleAndSlug(organization.handle, 'general')
       .then(function (library) {
