0994e6801501d6627516386d7152d4d7add5949c
Cam Hashemi <camhashemi@gmail.com>
2015-12-16 10:45:17 -0800

    clean up

--- kifi-backend/modules/eliza/app/com/keepit/eliza/controllers/site/WebsiteMessagingController.scala
+++ kifi-backend/modules/eliza/app/com/keepit/eliza/controllers/site/WebsiteMessagingController.scala
@@ -1,34 +1,15 @@
 package com.keepit.eliza.controllers.site
 
-import com.keepit.common.crypto.{ PublicIdConfiguration, PublicId }
-import com.keepit.common.db.Id
-import com.keepit.common.json.{ TraversableFormat, KeyFormat, TupleFormat }
-import com.keepit.discussion.Message
-import com.keepit.eliza.commanders.{ MessageFetchingCommander, ElizaDiscussionCommander, NotificationDeliveryCommander, MessagingCommander }
-import com.keepit.common.controller.{ ElizaServiceController, UserActions, UserActionsHelper }
-import com.keepit.common.time._
-import com.keepit.eliza.model.{ ElizaMessage, MessageSource }
-import com.keepit.eliza.model.ElizaMessage._
-import com.keepit.heimdal._
-import com.keepit.model.Keep
-import com.keepit.shoebox.ShoeboxServiceClient
-
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json._
-
 import com.google.inject.Inject
-
-import scala.concurrent.Future
-import scala.util.{ Try, Success, Failure }
+import com.keepit.common.controller.{ ElizaServiceController, UserActions, UserActionsHelper }
+import com.keepit.eliza.commanders.NotificationDeliveryCommander
+import play.api.libs.json._
+import com.keepit.common.time._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 class WebsiteMessagingController @Inject() (
     notificationCommander: NotificationDeliveryCommander,
-    discussionCommander: ElizaDiscussionCommander,
-    messageFetchingCommander: MessageFetchingCommander,
-    shoebox: ShoeboxServiceClient,
-    val userActionsHelper: UserActionsHelper,
-    implicit val publicIdConfig: PublicIdConfiguration,
-    heimdalContextBuilder: HeimdalContextBuilderFactory) extends UserActions with ElizaServiceController {
+    val userActionsHelper: UserActionsHelper) extends UserActions with ElizaServiceController {
 
   def getNotifications(howMany: Int, before: Option[String]) = UserAction.async { request =>
     val noticesFuture = before match {
