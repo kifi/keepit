b6208aeeeabe3a782b1bb72a5ae9cab48ebcbb53
Ryan P Brewster <ryan@kifi.com>
2016-01-06 14:56:21 -0800

    Fix an issue with dead keeps

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/KeepCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/KeepCommander.scala
@@ -161,8 +161,8 @@ class KeepCommanderImpl @Inject() (
   def getCrossServiceKeeps(ids: Set[Id[Keep]]): Map[Id[Keep], CrossServiceKeep] = {
     val (keeps, users, libs) = db.readOnlyMaster { implicit s =>
       val keepsById = keepRepo.getByIds(ids)
-      val usersById = ktuRepo.getAllByKeepIds(ids).map { case (keepId, ktus) => keepId -> ktus.map(_.userId).toSet }
-      val libsById = ktlRepo.getAllByKeepIds(ids).map { case (keepId, ktls) => keepId -> ktls.map(_.libraryId).toSet }
+      val usersById = ktuRepo.getAllByKeepIds(keepsById.keySet).map { case (keepId, ktus) => keepId -> ktus.map(_.userId).toSet }
+      val libsById = ktlRepo.getAllByKeepIds(keepsById.keySet).map { case (keepId, ktls) => keepId -> ktls.map(_.libraryId).toSet }
       (keepsById, usersById, libsById)
     }
     keeps.map {
