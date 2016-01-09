40ffa12e91d1e3cb9e33e0466520a43e288bf453
Ryan P Brewster <ryan@kifi.com>
2016-01-08 21:59:09 -0800

    Example of safer db sessions

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/RPBExampleCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/RPBExampleCommander.scala
@@ -2,49 +2,45 @@ package com.keepit.commanders
 
 import com.google.inject.{ Inject, Singleton }
 import com.keepit.common.db.Id
-import com.keepit.common.db.slick.DBSession.RSession
 import com.keepit.common.db.slick._
 import com.keepit.model._
 
-sealed trait DBContext
-object DBContext {
-  case class NoSession(dummy: Int) extends DBContext
-  case class YesSession(session: RSession) extends DBContext
-}
-
 @Singleton
 class RPBExampleCommander @Inject() (
-    db: Database,
+    db: SafeDatabase,
     keepRepo: KeepRepo) {
 
   // For the purposes of this example, pretend that our repos used this convention
-  def safeDb[T](f: DBContext.YesSession => T)(implicit ctx: DBContext.NoSession) = db.readOnlyMaster { s => f(DBContext.YesSession(s)) }
-  def getKeep(keepId: Id[Keep])(implicit ctx: DBContext.YesSession) = keepRepo.get(keepId)(ctx.session)
-  def getCount()(implicit ctx: DBContext.YesSession) = keepRepo.count(ctx.session)
+  def getKeep(keepId: Id[Keep])(implicit ctx: DBContext.Read) = keepRepo.get(keepId)(ctx.session)
+  def getCount()(implicit ctx: DBContext.Read) = keepRepo.count(ctx.session)
 
   // If a method opens a session, you must declare it as such
-  def openSession(keepId: Id[Keep])(implicit ctx: DBContext.NoSession): Keep = {
-    safeDb { implicit s =>
+  def openSession(keepId: Id[Keep])(implicit ctx: DBContext.Clean): Keep = {
+    db.read { implicit s =>
       getKeep(keepId)
     }
   }
 
-  def deferSession(keepId: Id[Keep])(implicit ctx: DBContext.NoSession): Keep = {
-    safeDb { implicit s =>
+  def deferSession(keepId: Id[Keep])(implicit ctx: DBContext.Clean): Keep = {
+    db.read { implicit s =>
       useDeferredSession(keepId)
     }
   }
-  def useDeferredSession(keepId: Id[Keep])(implicit ctx: DBContext.YesSession): Keep = {
+  def useDeferredSession(keepId: Id[Keep])(implicit ctx: DBContext.Read): Keep = {
+    doSomethingThatIsActuallySafe()
     doSomethingThatSeemsSafe()
     getKeep(keepId)
   }
-  def doSomethingThatSeemsSafe()(implicit ctx: DBContext.YesSession): Unit = {
-    println("i am totally safe")
+  def doSomethingThatIsActuallySafe(): Unit = {
+    println("i am really safe")
+  }
+  def doSomethingThatSeemsSafe()(implicit ctx: DBContext.Read): Unit = {
+    println("i am definitely unsafe")
     println("when I was written, no one assumed I would misbehave ever")
     println("but now I am angsty and want to do my own thing")
     // Try to uncomment these lines
     /*
-    safeDb { implicit s =>
+    db.write { implicit s =>
       println("ha, take that dad")
       println(getCount)
     }
