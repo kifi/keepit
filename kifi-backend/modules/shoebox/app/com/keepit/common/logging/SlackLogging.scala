b06134f6cf261acc7b3f57e320feb279bb25078a
Ryan P Brewster <ryan@kifi.com>
2016-01-12 15:13:43 -0800

    Rate-limit slack logging

--- kifi-backend/modules/shoebox/app/com/keepit/common/logging/SlackLogging.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/common/logging/SlackLogging.scala
@@ -1,11 +1,12 @@
 package com.keepit.common.logging
 
-import com.keepit.common.util.DescriptionElements
+import com.keepit.common.util.{ Debouncing, DescriptionElements }
 import com.keepit.macros.Location
 import com.keepit.slack.models.{ SlackAttachment, SlackMessageRequest }
 import com.keepit.slack.{ InhouseSlackChannel, InhouseSlackClient }
+import org.joda.time.Period
 
-trait SlackLogging {
+trait SlackLogging extends Debouncing {
   val inhouseSlackClient: InhouseSlackClient
   val loggingDestination: InhouseSlackChannel
   object slackLog {
@@ -14,11 +15,13 @@ trait SlackLogging {
     def error(elements: DescriptionElements*)(implicit sourceCodeLocation: Location): Unit = sendLog(sourceCodeLocation, elements, "danger")
 
     private def sendLog(fromLine: Location, text: DescriptionElements, color: String): Unit = {
-      val msg = SlackMessageRequest.inhouse(
-        DescriptionElements.fromLocation(fromLine),
-        attachments = Seq(SlackAttachment(color = Some(color), text = Some(DescriptionElements.formatForSlack(text))))
-      )
-      inhouseSlackClient.sendToSlack(loggingDestination, msg)
+      debounce(fromLine.location, Period.millis(100)) {
+        val msg = SlackMessageRequest.inhouse(
+          DescriptionElements.fromLocation(fromLine),
+          attachments = Seq(SlackAttachment(color = Some(color), text = Some(DescriptionElements.formatForSlack(text))))
+        )
+        inhouseSlackClient.sendToSlack(loggingDestination, msg)
+      }
     }
   }
 }
