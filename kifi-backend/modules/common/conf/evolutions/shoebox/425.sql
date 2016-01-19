947bf0812898a3f9bd9582d2b9c8ceaa770404d7
Andrew Conner <git@andrewconner.org>
2016-01-19 11:15:24 -0800

    Sigh

--- kifi-backend/modules/common/conf/evolutions/shoebox/425.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/425.sql
@@ -4,6 +4,6 @@
 
 alter TABLE twitter_sync_state add column sync_target varchar(32) NULL;
 
-insert into evolutions(name, description) values('423.sql', 'add sync_target to twitter_sync_state');
+insert into evolutions(name, description) values('425.sql', 'add sync_target to twitter_sync_state');
 
 # --- !Downs
