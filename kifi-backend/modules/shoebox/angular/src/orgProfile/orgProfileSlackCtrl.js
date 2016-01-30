8a468896f7c16dd5f9bec017ab1c63733bedc2de
Carlos Vializ <cvializ@vt.edu>
2016-01-29 18:27:30 -0800

    Remove unused dependencies

--- kifi-backend/modules/shoebox/angular/src/orgProfile/orgProfileSlackCtrl.js
+++ kifi-backend/modules/shoebox/angular/src/orgProfile/orgProfileSlackCtrl.js
@@ -3,10 +3,8 @@
 angular.module('kifi')
 
 .controller('OrgProfileSlackCtrl', [
-  '$window', '$location', '$rootScope', '$analytics', '$scope', '$stateParams',
-  'profile', 'orgProfileService', 'originTrackingService', '$timeout',
-  function ($window, $location, $rootScope, $analytics, $scope, $stateParams,
-            profile, orgProfileService, originTrackingService, $timeout) {
+  '$window', '$rootScope', '$scope', '$stateParams', 'profile', '$timeout',
+  function ($window, $rootScope, $scope, $stateParams, profile, $timeout) {
     $window.document.title = profile.organization.name + ' • Kifi <3 Slack';
     $scope.userLoggedIn = $rootScope.userLoggedIn;
     $scope.slackTeamId = $stateParams.slackTeamId;
