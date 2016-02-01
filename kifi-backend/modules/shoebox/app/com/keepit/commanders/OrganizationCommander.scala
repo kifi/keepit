5c647f19daeba3e1136a31979f0ce7e4e32cccfb
Cameron Hashemi <camhashemi@gmail.com>
2016-01-31 20:25:07 -0800

    Revert "don't trip over site url 'correctness'"

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/OrganizationCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/OrganizationCommander.scala
@@ -91,7 +91,17 @@ class OrganizationCommanderImpl @Inject() (
   }
 
   private def validateModifications(modifications: OrganizationModifications): Option[OrganizationFail] = {
-    Some(OrganizationFail.INVALID_MODIFY_NAME).filter(_ => modifications.name.exists(_.isEmpty))
+    val badName = modifications.name.exists(_.isEmpty)
+    val normalizedSiteUrl = modifications.site.map { url =>
+      if (url.startsWith("http://") || url.startsWith("https://")) url
+      else "https://" + url
+    }
+    val badSiteUrl = normalizedSiteUrl.exists(URI.parse(_).isFailure)
+
+    Stream(
+      badName -> OrganizationFail.INVALID_MODIFY_NAME,
+      badSiteUrl -> OrganizationFail.INVALID_MODIFY_SITEURL
+    ).collect { case (true, fail) => fail }.headOption
   }
 
   private def validateOrganizationSettings(orgId: Id[Organization], newSettings: OrganizationSettings)(implicit session: RSession): Option[OrganizationFail] = {
