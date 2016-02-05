d8648111dcdf40843922b98b5195f671683dc605
Léo Grimaldi <leo@kifi.com>
2016-02-05 12:04:21 -0800

    Whoops

--- kifi-backend/modules/common/conf/evolutions/shoebox/409.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/409.sql
@@ -19,7 +19,7 @@ CREATE TABLE slack_team_membership (
   PRIMARY KEY(id),
   UNIQUE KEY slack_team_membership_u_slack_team_id_slack_user_id (slack_team_id, slack_user_id),
   INDEX slack_team_membership_i_user_id_slack_team_id_slack_user_id (user_id, slack_user_id, slack_team_id),
-  INDEX slack_team_membership_i_slack_user_id_slack_team_id (slack_team_id, slack_user_id)
+  INDEX slack_team_membership_i_slack_user_id_slack_team_id (slack_team_id, slack_user_id),
   CONSTRAINT slack_team_membership_f_slack_team FOREIGN KEY slack_team_id REFERENCES slack_team(slack_team_id)
 );
 
