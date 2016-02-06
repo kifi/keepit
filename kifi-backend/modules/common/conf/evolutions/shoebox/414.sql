aff63325609690749b9f2757f2e41cc58029a5b0
Ryan P Brewster <ryan@kifi.com>
2016-02-05 18:31:57 -0800

    Add an author onto KeepSourceAttribution

--- kifi-backend/modules/common/conf/evolutions/shoebox/414.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/414.sql
@@ -3,7 +3,9 @@
 # --- !Ups
 
 ALTER TABLE keep_source_attribution ADD COLUMN keep_id BIGINT(20) NOT NULL AFTER updated_at;
+ALTER TABLE keep_source_attribution ADD COLUMN author VARCHAR(64) DEFAULT NULL AFTER keep_id;
 CREATE UNIQUE INDEX keep_source_attribution_u_keep_id ON keep_source_attribution (keep_id);
+CREATE INDEX keep_source_attribution_u_author ON keep_source_attribution(author);
 -- ALTER TABLE bookmark DROP FOREIGN KEY bookmark_f_source_attribution;
 ALTER TABLE bookmark DROP COLUMN source_attribution_id;
 insert into evolutions(name, description) values('414.sql', 'add keep_source_attribution.keep_id, remove bookmark.source_attribution_id');
