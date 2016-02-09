47eb9d6f8cad572c03b5821caa96f7b7ecb3d31f
Carlos Vializ <cvializ@vt.edu>
2016-02-09 09:50:11 -0800

    Add retry to make it more robust. Failure won't break the form

--- kifi-backend/modules/shoebox/angular/src/teamSettings/paymentPlan.js
+++ kifi-backend/modules/shoebox/angular/src/teamSettings/paymentPlan.js
@@ -96,25 +96,49 @@ angular.module('kifi')
       });
     }
 
-    $scope.changePlanToFree = function () {
-      var freeTierPlans = plansByTier[Object.keys(plansByTier)[0]];
-      var firstFreeTierPlan = freeTierPlans[0];
-      $scope.plan.name = firstFreeTierPlan.name;
-      $scope.plan.cycle = firstFreeTierPlan.cycle;
+    $scope.changePlanToFree = function (retried) {
+      retried = (typeof retried === 'boolean' ? retried : false);
+
+      // Sometimes the controller can run before ng-form assigns the form object
+      // to the scope. If that happens, try one more time after a timeout
       if ($scope.planSelectsForm) {
+        var freeTierPlans = plansByTier[Object.keys(plansByTier)[0]];
+        var firstFreeTierPlan = freeTierPlans[0];
+        $scope.plan.name = firstFreeTierPlan.name;
+        $scope.plan.cycle = firstFreeTierPlan.cycle;
         $scope.planSelectsForm.$setDirty();
+        $scope.trackClick('free_card:downgrade');
+      } else {
+        if (!retried) {
+          $timeout(function () {
+            $scope.changePlanToFree(true);
+          });
+        } else {
+          $scope.trackClick('free_card:downgrade:fail');
+        }
       }
-      $scope.trackClick('free_card:downgrade');
     };
 
-    $scope.changePlanToStandard = function () {
-      var standardTierPlans = plansByTier[Object.keys(plansByTier)[1]];
-      var firstStandardTierPlan = standardTierPlans[0];
-      $scope.plan.name = firstStandardTierPlan.name;
+    $scope.changePlanToStandard = function (retried) {
+      retried = (typeof retried === 'boolean' ? retried : false);
+
+      // Sometimes the controller can run before ng-form assigns the form object
+      // to the scope. If that happens, try one more time after a timeout.
       if ($scope.planSelectsForm) {
+        var standardTierPlans = plansByTier[Object.keys(plansByTier)[1]];
+        var firstStandardTierPlan = standardTierPlans[0];
+        $scope.plan.name = firstStandardTierPlan.name;
         $scope.planSelectsForm.$setDirty();
+        $scope.trackClick('standard_card:upgrade');
+      } else {
+        if (!retried) {
+          $timeout(function () {
+            $scope.changePlanToStandard(true);
+          });
+        } else {
+          $scope.trackClick('standard_card:upgrade:fail');
+        }
       }
-      $scope.trackClick('standard_card:upgrade');
     };
 
     var plansByTier = paymentPlans.plans;
