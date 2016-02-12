6f71354f2613d7d53d2b8c7744e880d567de6d7a
Ryan P Brewster <ryan@kifi.com>
2016-02-11 17:49:57 -0800

    Add index

--- kifi-backend/modules/common/conf/evolutions/shoebox/429.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/429.sql
@@ -3,6 +3,7 @@
 # --- !Ups
 
 ALTER TABLE bookmark ADD COLUMN hseq BIGINT(20) DEFAULT NULL AFTER seq;
+CREATE INDEX bookmark_i_hseq ON bookmark(hseq, seq);
 
 insert into evolutions (name, description) values('429.sql', 'track the human-initiated subsequence number (hseq)');
 
