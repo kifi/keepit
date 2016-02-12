f3547d9506c1d94e21141a206c115a40ec8e23cf
Cam Hashemi <camhashemi@gmail.com>
2016-02-12 11:09:00 -0800

    sort left hand rail libraries by most kept in past 2 weeks

--- kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryQueryCommander.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/commanders/LibraryQueryCommander.scala
@@ -1,10 +1,14 @@
 package com.keepit.commanders
 
 import com.google.inject.{ Inject, ImplementedBy, Singleton }
+import com.keepit.commanders.LibraryQuery.Arrangement
+import com.keepit.common.crypto.PublicIdConfiguration
 import com.keepit.common.db.Id
 import com.keepit.common.db.slick.DBSession.{ RWSession, RSession }
 import com.keepit.common.db.slick.Database
 import com.keepit.common.healthcheck.AirbrakeNotifier
+import com.keepit.common.social.BasicUserRepo
+import com.keepit.common.time._
 import com.keepit.model._
 import com.keepit.common.core.tryExtensionOps
 import play.api.libs.json.{ Format, Json }
@@ -15,8 +19,8 @@ case class LibraryQuery(
     target: LibraryQuery.Target,
     arrangement: Option[LibraryQuery.Arrangement] = None,
     fromId: Option[Id[Library]],
-    offset: Int,
-    limit: Int) {
+    offset: Offset,
+    limit: Limit) {
   def withArrangement(newArrangement: LibraryQuery.Arrangement) = this.copy(arrangement = Some(newArrangement))
 }
 
@@ -37,6 +41,8 @@ object LibraryQuery {
 trait LibraryQueryCommander {
   def setPreferredArrangement(userId: Id[User], arrangement: LibraryQuery.Arrangement): Unit
   def getLibraries(requester: Option[Id[User]], query: LibraryQuery)(implicit session: RSession): Seq[Id[Library]]
+  def getLHRLibrariesForUser(userId: Id[User], arrangement: Option[Arrangement] = None, fromIdOpt: Option[Id[Library]], offset: Offset, limit: Limit, windowSize: Option[Int])(implicit session: RSession): Seq[BasicLibrary]
+  def getLHRLibrariesForOrg(userId: Id[User], orgId: Id[Organization], arrangementOpt: Option[Arrangement], fromIdOpt: Option[Id[Library]], offset: Offset, limit: Limit, windowSize: Option[Int])(implicit session: RSession): Seq[BasicLibrary]
 }
 
 @Singleton
@@ -46,6 +52,10 @@ class LibraryQueryCommanderImpl @Inject() (
   libMembershipRepo: LibraryMembershipRepo,
   orgMembershipRepo: OrganizationMembershipRepo,
   libRepo: LibraryRepo,
+  orgRepo: OrganizationRepo,
+  basicUserRepo: BasicUserRepo,
+  ktlRepo: KeepToLibraryRepo,
+  implicit val publicIdConfig: PublicIdConfiguration,
   implicit val airbrake: AirbrakeNotifier)
     extends LibraryQueryCommander {
 
@@ -69,6 +79,34 @@ class LibraryQueryCommanderImpl @Inject() (
       orgsWithVisibleLibraries = requester.map(userId => orgMembershipRepo.getAllByUserId(userId).map(_.organizationId).toSet).getOrElse(Set.empty)
     )
     libRepo.getLibraryIdsForQuery(customizedQuery, extraInfo)
+  }
+
+  def getLHRLibrariesForUser(userId: Id[User], arrangementOpt: Option[Arrangement] = None, fromIdOpt: Option[Id[Library]], offset: Offset, limit: Limit, windowSize: Option[Int])(implicit session: RSession): Seq[BasicLibrary] = {
+    import LibraryQuery._
+    val arrangement = arrangementOpt.getOrElse(Arrangement(LibraryOrdering.ALPHABETICAL, SortDirection.ASCENDING))
+    val sortedLibIds = arrangement.ordering match {
+      case LibraryOrdering.MOST_RECENT_KEEPS_BY_USER => ktlRepo.getSortedByKeepCountSince(userId, orgIdOpt = None, currentDateTime.minusDays(windowSize.getOrElse(14)), offset, limit)
+      case _ => getLibraries(Some(userId), LibraryQuery(ForUser(userId, roles = Set(LibraryAccess.OWNER)), Some(arrangement), fromIdOpt, offset, limit))
+    }
+    val libsById = libRepo.getActiveByIds(sortedLibIds.toSet)
+    val basicUser = basicUserRepo.load(userId)
+    sortedLibIds.flatMap(libsById.get).map(lib => BasicLibrary(lib, basicUser, None))
+  }
+
+  def getLHRLibrariesForOrg(userId: Id[User], orgId: Id[Organization], arrangementOpt: Option[Arrangement], fromIdOpt: Option[Id[Library]], offset: Offset, limit: Limit, windowSize: Option[Int])(implicit session: RSession): Seq[BasicLibrary] = {
+    import LibraryQuery._
+
+    val arrangement = arrangementOpt.getOrElse(Arrangement(LibraryOrdering.ALPHABETICAL, SortDirection.ASCENDING))
+    val sortedLibIds = arrangement.ordering match {
+      case LibraryOrdering.MOST_RECENT_KEEPS_BY_USER => ktlRepo.getSortedByKeepCountSince(userId, Some(orgId), currentDateTime.minusDays(windowSize.getOrElse(14)), offset, limit)
+      case _ => getLibraries(Some(userId), LibraryQuery(ForOrg(orgId), Some(arrangement), fromIdOpt, offset, limit))
+    }
+
+    val libsById = libRepo.getActiveByIds(sortedLibIds.toSet)
+    val libs = sortedLibIds.flatMap(libsById.get)
+    val libOwners = basicUserRepo.loadAll(libs.map(_.ownerId).toSet)
+    val orgHandle = orgRepo.get(orgId).primaryHandle.get.normalized
+    libs.map(lib => BasicLibrary(lib, libOwners(lib.ownerId), Some(orgHandle)))
   }
 
 }
