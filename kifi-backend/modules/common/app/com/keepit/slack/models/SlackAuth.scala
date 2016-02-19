b42d96d32e7c87262a2f2b0f36a80705dca71f13
eishay <eishay@gmail.com>
2016-02-18 20:41:19 -0800

    fix slack json formatting and add tests

--- kifi-backend/modules/common/app/com/keepit/slack/models/SlackAuth.scala
+++ kifi-backend/modules/common/app/com/keepit/slack/models/SlackAuth.scala
@@ -188,7 +188,7 @@ object SlackUserInfo {
     (__ \ 'deleted).readNullable[Boolean].map(_.contains(true)) and
     (__ \ 'is_admin).readNullable[Boolean].map(_.contains(true)) and
     (__ \ 'is_owner).readNullable[Boolean].map(_.contains(true)) and
-    (__ \ 'bot).readNullable[Boolean].map(_.contains(true)) and
+    (__ \ 'is_bot).readNullable[Boolean].map(_.contains(true)) and
     (__ \ 'is_restricted).readNullable[Boolean].map(_.contains(true)) and
     Reads(JsSuccess(_))
   )(SlackUserInfo.apply _)
--- kifi-backend/modules/shoebox/test/com/keepit/slack/models/SlackModelsTest.scala
+++ kifi-backend/modules/shoebox/test/com/keepit/slack/models/SlackModelsTest.scala
@@ -1,5 +1,8 @@
 package com.keepit.slack.models
 
+import java.io.File
+
+import com.keepit.common.strings._
 import com.keepit.model.OrganizationSettings
 import org.specs2.mutable.Specification
 import play.api.libs.json.{ JsError, JsSuccess, Json }
@@ -22,11 +25,14 @@ class SlackModelsTest extends Specification {
           """.stripMargin).validate[SlackIdentifyResponse] must haveClass[JsSuccess[_]]
       }
       "users.list" in {
-        val raw = """{"ok":true,"members":[{"id":"U0JT6K0KV","team_id":"T0JQ45PPS","name":"jarod","deleted":false,"status":null,"color":"4bbe2e","real_name":"","tz":"America/Los_Angeles","tz_label":"Pacific Standard Time","tz_offset":-28800,"profile":{"avatar_hash":"gb8cb5a6ee82","real_name":"","real_name_normalized":"","email":"jarod@lvsweet.com","image_24":"https://secure.gravatar.com/avatar/b8cb5a6ee827f195f7495832dcabb37c.jpg?s=24&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F0180%2Fimg%2Favatars%2Fava_0025-24.png","image_32":"https://secure.gravatar.com/avatar/b8cb5a6ee827f195f7495832dcabb37c.jpg?s=32&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0025-32.png","image_48":"https://secure.gravatar.com/avatar/b8cb5a6ee827f195f7495832dcabb37c.jpg?s=48&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0025-48.png","image_72":"https://secure.gravatar.com/avatar/b8cb5a6ee827f195f7495832dcabb37c.jpg?s=72&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0025-72.png","image_192":"https://secure.gravatar.com/avatar/b8cb5a6ee827f195f7495832dcabb37c.jpg?s=192&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F7fa9%2Fimg%2Favatars%2Fava_0025-192.png","image_512":"https://secure.gravatar.com/avatar/b8cb5a6ee827f195f7495832dcabb37c.jpg?s=512&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F7fa9%2Fimg%2Favatars%2Fava_0025-512.png"},"is_admin":false,"is_owner":false,"is_primary_owner":false,"is_restricted":false,"is_ultra_restricted":false,"is_bot":false,"has_2fa":false},{"id":"U0JQ3ETKR","team_id":"T0JQ45PPS","name":"mgreetzelle","deleted":false,"status":null,"color":"9f69e7","real_name":"","tz":"America/Los_Angeles","tz_label":"Pacific Standard Time","tz_offset":-28800,"profile":{"avatar_hash":"g7e026919f4f","real_name":"","real_name_normalized":"","email":"immollybloom@gmail.com","image_24":"https://secure.gravatar.com/avatar/7e026919f4fc91bcc9e38be7af8bebcd.jpg?s=24&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0013-24.png","image_32":"https://secure.gravatar.com/avatar/7e026919f4fc91bcc9e38be7af8bebcd.jpg?s=32&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F0180%2Fimg%2Favatars%2Fava_0013-32.png","image_48":"https://secure.gravatar.com/avatar/7e026919f4fc91bcc9e38be7af8bebcd.jpg?s=48&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0013-48.png","image_72":"https://secure.gravatar.com/avatar/7e026919f4fc91bcc9e38be7af8bebcd.jpg?s=72&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F66f9%2Fimg%2Favatars%2Fava_0013-72.png","image_192":"https://secure.gravatar.com/avatar/7e026919f4fc91bcc9e38be7af8bebcd.jpg?s=192&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F7fa9%2Fimg%2Favatars%2Fava_0013-192.png","image_512":"https://secure.gravatar.com/avatar/7e026919f4fc91bcc9e38be7af8bebcd.jpg?s=512&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F7fa9%2Fimg%2Favatars%2Fava_0013-512.png"},"is_admin":true,"is_owner":true,"is_primary_owner":true,"is_restricted":false,"is_ultra_restricted":false,"is_bot":false},{"id":"USLACKBOT","team_id":"T0JQ45PPS","name":"slackbot","deleted":false,"status":null,"color":"757575","real_name":"slackbot","tz":null,"tz_label":"Pacific Standard Time","tz_offset":-28800,"profile":{"first_name":"slackbot","last_name":"","image_24":"https://slack.global.ssl.fastly.net/0180/img/slackbot_24.png","image_32":"https://slack.global.ssl.fastly.net/66f9/img/slackbot_32.png","image_48":"https://slack.global.ssl.fastly.net/66f9/img/slackbot_48.png","image_72":"https://slack.global.ssl.fastly.net/0180/img/slackbot_72.png","image_192":"https://slack.global.ssl.fastly.net/66f9/img/slackbot_192.png","image_512":"https://slack.global.ssl.fastly.net/7fa9/img/slackbot_512.png","avatar_hash":"sv1444671949","real_name":"slackbot","real_name_normalized":"slackbot","email":null,"fields":null},"is_admin":false,"is_owner":false,"is_primary_owner":false,"is_restricted":false,"is_ultra_restricted":false,"is_bot":false}],"cache_ts":1455754717}"""
+        val raw = io.Source.fromFile(new File("test/data/slack_kifi.json"), UTF8).mkString
         val res = (Json.parse(raw) \ "members").validate[Seq[SlackUserInfo]]
         res match {
           case e: JsError => throw new Exception(e.errors.mkString)
-          case _ =>
+          case JsSuccess(users, _) =>
+            users.size === 69
+            users.filter(_.bot).size === 8
+            users.filter(_.deleted).size === 44
         }
         res must haveClass[JsSuccess[_]]
       }
