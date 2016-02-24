003481fca44ac8afeb092ef3477edeae1b7d3e9b
Ryan P Brewster <ryan@kifi.com>
2016-02-23 16:42:20 -0800

    Strip look-heres from messages pushed to Slack

--- kifi-backend/modules/shoebox/app/com/keepit/slack/SlackPushingActor.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/slack/SlackPushingActor.scala
@@ -420,7 +420,7 @@ class SlackPushingActor @Inject() (
     val keepElement = keep.title.getOrElse(keep.url.abbreviate(KEEP_URL_MAX_DISPLAY_LENGTH)) --> keepLink
     DescriptionElements(
       SlackEmoji.speechBalloon,
-      if (msg.isDeleted) DescriptionElements("[deleted]") else DescriptionElements(userElement getOrElse DescriptionElements("Someone"), "said:", msg.text),
+      if (msg.isDeleted) DescriptionElements("[deleted]") else DescriptionElements(userElement getOrElse DescriptionElements("Someone"), "said:", CrossServiceMessage.stripLookHeres(msg.text)),
       "---", keepElement
     )
   }
