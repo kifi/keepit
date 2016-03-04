42455e5daa23518e0eaf2b1bce08ea84792b4764
Cam Hashemi <camhashemi@gmail.com>
2016-03-04 14:35:29 -0800

    improvements to slack public sync widget

--- kifi-backend/modules/shoebox/angular/src/home/featureUpsell.js
+++ kifi-backend/modules/shoebox/angular/src/home/featureUpsell.js
@@ -15,23 +15,24 @@ angular.module('kifi')
       link: function (scope) {
         scope.me = profileService.me;
         var hasFeatureUpsellExp = (scope.me.experiments || []).indexOf('slack_upsell_widget') !== -1;
-        var orgToSync = scope.me.orgs.filter(function (org) {
+        scope.orgToSync = scope.me.orgs.filter(function (org) {
           return !org.slackTeam || !org.slackTeam.publicChannelsLastSyncedAt;
         })[0];
         scope.userLoggedIn = $rootScope.userLoggedIn;
 
         (Object.keys(profileService.prefs).length === 0 ? profileService.fetchPrefs() : $q.when(profileService.prefs)).then(function (prefs) {
-          scope.showFeatureUpsell = orgToSync && hasFeatureUpsellExp && prefs.slack_upsell_widget;
+          scope.showFeatureUpsell = scope.orgToSync && hasFeatureUpsellExp && prefs.slack_upsell_widget;
         });
 
         scope.hide = function () {
           scope.showFeatureUpsell = false;
           profileService.savePrefs({ slack_upsell_widget: false });
+          $analytics.eventTrack('user_clicked_page', { type: 'homeFeed', action: 'slackHideUpsellWidget' });
         };
 
         scope.clickedConnectSlack = function() {
           $analytics.eventTrack('user_clicked_page', { type: 'homeFeed', action: 'slackSyncAllChannels' });
-          slackService.publicSync(orgToSync.id).then(function (resp) {
+          slackService.publicSync(scope.orgToSync.id).then(function (resp) {
             if (resp.redirect) {
               $window.location = resp.redirect;
             } else {
