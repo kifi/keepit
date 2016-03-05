c85bd8781d0ab11ad673c66b557575657ce72805
Léo Grimaldi <leo@kifi.com>
2016-03-04 16:09:03 -0800

    Get back to ConnectSlackTeam

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/website/SlackController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/website/SlackController.scala
@@ -117,8 +117,7 @@ class SlackController @Inject() (
       val useKifiBot = orgExperimentRepo.hasExperiment(request.orgId, OrganizationExperimentType.SLACK_COMMENT_MIRRORING)
       (slackTeamIdOpt, useKifiBot)
     }
-    val syncAction = SyncPublicChannels(useKifiBot)
-    val action = if (slackTeamIdOpt.isDefined) syncAction else ConnectSlackTeam(request.orgId, andThen = Some(syncAction))
+    val action = ConnectSlackTeam(request.orgId, andThen = Some(SyncPublicChannels(useKifiBot)))
     val res = slackAuthCommander.processActionOrElseAuthenticate(request.request.userId, slackTeamIdOpt, action)
     handleAsAPIRequest(res)(request.request)
   }
