87220778760fd2ca0a7e5301cddd691419a0fa0b
Cam Hashemi <camhashemi@gmail.com>
2016-03-08 15:33:38 -0800

    handle system notifcations using mock user

--- packrat/scripts/notices.js
+++ packrat/scripts/notices.js
@@ -204,6 +204,7 @@ k.panes.notices = k.panes.notices || function () {
       notice.title = notice.title || formatTitleFromUrl(notice.url);
       var participants = notice.participants;
       var nParticipants = participants.length;
+      var isSystem = !notice.author || notice.author.id === '42424242-4242-4242-4242-000000000001';
       if (notice.authors === 1) {
         notice[nParticipants === 1 ? 'isSelf' : (notice.author && notice.author.id) === k.me.id ? 'isSent' : 'isReceived'] = true;
       } else if (notice.firstAuthor > 1) {
@@ -250,6 +251,8 @@ k.panes.notices = k.panes.notices || function () {
           notice.multiple = notice.messages > 1;
         }
         notice.authorShortName = 'Me';
+      } else if (isSystem) {
+        notice.authorShortName = '';
       } else if (notice.author && nParticipants > 2) {
         notice.authorShortName = notice.author.firstName;
       }
