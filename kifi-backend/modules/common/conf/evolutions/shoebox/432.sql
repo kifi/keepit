cf9a74d3a6889a3577036dea69da461716c02108
Ryan P Brewster <ryan@kifi.com>
2016-03-14 18:10:40 -0700

    Drop visibility in the evolution

--- kifi-backend/modules/common/conf/evolutions/shoebox/432.sql
+++ kifi-backend/modules/common/conf/evolutions/shoebox/432.sql
@@ -3,6 +3,7 @@
 # --- !Ups
 
 ALTER TABLE bookmark DROP COLUMN organization_id;
+ALTER TABLE bookmark DROP COLUMN visibility;
 
 insert into evolutions(name, description) values('432.sql', 'BEGIN THE REAPING (i.e., drop organization_id from bookmark)');
 
