5e3803fe9e834c8be0f2d3c63dc450f857f07993
Derek Burch <derekaburch@gmail.com>
2016-03-17 15:48:23 -0700

    show keep title as the page title on keep page

--- kifi-backend/modules/shoebox/angular/src/keep/keepPage.js
+++ kifi-backend/modules/shoebox/angular/src/keep/keepPage.js
@@ -3,8 +3,8 @@
 angular.module('kifi')
 
 .controller('KeepPageCtrl', [ '$rootScope', '$location', '$scope', '$state',
-    '$stateParams', '$timeout', '$analytics', 'keepActionService', 'modalService',
-  function ($rootScope, $location, $scope, $state, $stateParams, $timeout, $analytics, keepActionService, modalService) {
+    '$stateParams', '$timeout', '$window', '$analytics', 'keepActionService', 'modalService', 'util',
+  function ($rootScope, $location, $scope, $state, $stateParams, $timeout, $window, $analytics, keepActionService, modalService, util) {
 
     $scope.unkeepFromLibrary = function (event, keep) {
       if (keep.libraryId && keep.id) {
@@ -36,9 +36,17 @@ angular.module('kifi')
     keepActionService.getFullKeepInfo($stateParams.pubId, $stateParams.authToken, $scope.maxInitialComments * 2).then(function (result) {
       $scope.loaded = true;
       $scope.keep = result;
+      var displayTitle = result.title || result.summary && result.summary.title || util.formatTitleFromUrl(result.url);
+      if (displayTitle) {
+        $window.document.title = 'Kifi • ' + displayTitle;
+      } else {
+        $window.document.title = 'Kifi';
+      }
+
       $timeout(trackPageView);
     })['catch'](function(reason){
       $scope.loaded = true;
+      $window.document.title = 'Kifi';
       $rootScope.$emit('errorImmediately', reason);
     });
   }
