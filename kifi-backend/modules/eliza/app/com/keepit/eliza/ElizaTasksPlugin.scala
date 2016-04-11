ffe3861d7a9d3c66e692c503a552a58700a04653
Ryan P Brewster <ryan@kifi.com>
2016-04-11 15:59:42 -0700

    DRY up the task plugin

--- kifi-backend/modules/eliza/app/com/keepit/eliza/ElizaTasksPlugin.scala
+++ kifi-backend/modules/eliza/app/com/keepit/eliza/ElizaTasksPlugin.scala
@@ -1,33 +1,36 @@
 package com.keepit.eliza
 
-import com.google.inject.{ Inject, Singleton }
+import com.google.inject.{Inject, Singleton}
 import com.keepit.common.actor.ActorInstance
-import com.keepit.common.plugin.{ SchedulerPlugin, SchedulingProperties }
-import com.keepit.eliza.integrity.ElizaDataIntegrityActors
+import com.keepit.common.plugin.{SchedulerPlugin, SchedulingProperties}
+import com.keepit.eliza.integrity.ElizaMessageThreadByMessageIntegrityActor
 import com.keepit.eliza.shoebox.ElizaKeepIngestingActor
-import com.kifi.juggle.ConcurrentTaskProcessingActor.{ Close, IfYouCouldJustGoAhead }
+import com.kifi.juggle.ConcurrentTaskProcessingActor.{Close, IfYouCouldJustGoAhead}
 
 import scala.concurrent.duration._
 
 @Singleton
 class ElizaTasksPlugin @Inject() (
-  dataIntegrity: ElizaDataIntegrityActors,
+  messageThreadByMessageIntegrityActor: ActorInstance[ElizaMessageThreadByMessageIntegrityActor],
   keepIngestingActor: ActorInstance[ElizaKeepIngestingActor],
   val scheduling: SchedulingProperties)
     extends SchedulerPlugin {
 
+  private def taskActors = Seq(
+    keepIngestingActor -> 1.minute,
+    messageThreadByMessageIntegrityActor -> 3.minutes
+  )
+
   override def onStart() {
     log.info("ElizaTasksPlugin onStart")
-    scheduleTaskOnLeader(keepIngestingActor.system, 3 minutes, 1 minute, keepIngestingActor.ref, IfYouCouldJustGoAhead)
-
-    scheduleTaskOnLeader(keepIngestingActor.system, 3 minutes, 1 minute, dataIntegrity.messageThreadByMessage.ref, IfYouCouldJustGoAhead)
+    taskActors.zipWithIndex.foreach {
+      case ((actor, freq), idx) =>
+        scheduleTaskOnLeader(actor.system, (180 + 10 * idx) seconds, freq, actor.ref, IfYouCouldJustGoAhead)
+    }
   }
 
   override def onStop() {
-    Seq(
-      keepIngestingActor,
-      dataIntegrity.messageThreadByMessage
-    ).foreach(_.ref ! Close)
+    taskActors.foreach { case (actor, _) => actor.ref ! Close }
     super.onStop()
   }
 
--- kifi-backend/modules/eliza/app/com/keepit/eliza/integrity/ElizaDataIntegrityActors.scala
+++
@@ -1,8 +0,0 @@
-package com.keepit.eliza.integrity
-
-import com.google.inject.Inject
-import com.keepit.common.actor.ActorInstance
-
-class ElizaDataIntegrityActors @Inject() (
-  val messageThreadByMessage: ActorInstance[ElizaMessageThreadByMessageIntegrityActor])
-
