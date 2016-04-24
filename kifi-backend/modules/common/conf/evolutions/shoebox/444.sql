c890d1d1bab9039c716326cd7aec4dff6b5ce84d
Léo Grimaldi <leo@kifi.com>
2016-04-23 20:52:18 -0700

    Drop UriRenormalizationTracking

---
+++ kifi-backend/modules/common/conf/evolutions/shoebox/444.sql
@@ -0,0 +1,9 @@
+# ELIZA
+
+# --- !Ups
+
+DROP TABLE uri_renormalization_event;
+
+insert into evolutions (name, description) values('444.sql', 'drop table uri_renormalization_event');
+
+# --- !Downs
