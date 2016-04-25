509368b02b05c5a1ffa645552cc52048b2315442
Léo Grimaldi <leo@kifi.com>
2016-04-25 11:39:08 -0700

    Missed the trait

--- kifi-backend/modules/eliza/app/com/keepit/eliza/commanders/MessagingCommander.scala
+++ kifi-backend/modules/eliza/app/com/keepit/eliza/commanders/MessagingCommander.scala
@@ -55,7 +55,6 @@ trait MessagingCommander {
   // todo: For each method here, remove if no one's calling it externally, and set as private in the implementation
   def getThreadInfos(userId: Id[User], url: String): Future[(String, Seq[ElizaThreadInfo])]
   def keepAttribution(userId: Id[User], uriId: Id[NormalizedURI]): Seq[Id[User]]
-  def hasThreads(userId: Id[User], url: String): Future[Boolean]
   def checkUrisDiscussed(userId: Id[User], uriIds: Seq[Id[NormalizedURI]]): Future[Seq[Boolean]]
   def sendMessageWithNonUserThread(nut: NonUserThread, messageText: String, source: Option[MessageSource], urlOpt: Option[URI])(implicit context: HeimdalContext): (MessageThread, ElizaMessage)
   def sendMessageWithUserThread(userThread: UserThread, messageText: String, source: Option[MessageSource], urlOpt: Option[URI])(implicit context: HeimdalContext): (MessageThread, ElizaMessage)
