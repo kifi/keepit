0d684e2223350987d52aea31254620ca1ae631f6
eishay <eishay@gmail.com>
2016-06-01 11:16:11 -0700

    more twitter filters

--- kifi-backend/modules/shoebox/app/com/keepit/common/util/UrlClassifier.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/common/util/UrlClassifier.scala
@@ -14,7 +14,9 @@ object UrlClassifier {
       """https?://instagr\.am/.*""",
       """https?://*riffiti\.com/.*""", //download sites
       """https?://ift\.tt/.*""", //IFTTT has lots of bad dead urls
+      """https?://dsh\.re/.*""", //screenshot sharing
       """https?://shar\.es/.*""",
+      """https?://blab\.im/.*""",//some real time chat like periscope 
       """https?://twitter\.com//.*""", //this one is a bit strange and may take out legit twitter articles. the core issue is people linking to other tweets that have nothing but the tweet and it looks bad
       """https?://runkeeper\.com/user/.*/activity/.*""",
       """https?://techmeme\.com/.*""",
