28db711ae9a17a5ff58529d24d4117d31875bca1
Ryan P Brewster <ryan@kifi.com>
2016-06-03 13:02:47 -0700

    Maybe it is important to have ModelWithPublicId

--- kifi-backend/modules/shoebox/app/com/keepit/shoebox/path/ShortenedPath.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/shoebox/path/ShortenedPath.scala
@@ -2,7 +2,7 @@ package com.keepit.shoebox.path
 
 import javax.crypto.spec.IvParameterSpec
 
-import com.keepit.common.crypto.PublicIdGenerator
+import com.keepit.common.crypto.{ ModelWithPublicId, PublicIdGenerator }
 import com.keepit.common.db.{ States, State, ModelWithState, Id }
 import com.keepit.common.path.Path
 import org.joda.time.DateTime
@@ -14,7 +14,7 @@ final case class ShortenedPath(
   updatedAt: DateTime = currentDateTime,
   state: State[ShortenedPath] = ShortenedPathStates.ACTIVE,
   path: Path)
-    extends ModelWithState[ShortenedPath] {
+    extends ModelWithState[ShortenedPath] with ModelWithPublicId[ShortenedPath] {
   def withId(id: Id[ShortenedPath]): ShortenedPath = this.copy(id = Some(id))
   def withUpdateTime(now: DateTime): ShortenedPath = this.copy(updatedAt = now)
   def withState(newState: State[ShortenedPath]): ShortenedPath = this.copy(state = newState)
@@ -28,6 +28,6 @@ final case class ShortenedPath(
 object ShortenedPathStates extends States[ShortenedPath]
 
 object ShortenedPath extends PublicIdGenerator[ShortenedPath] {
-  val publicIdIvSpec: IvParameterSpec = new IvParameterSpec(Array(-67, -41, -89, -42, 13, -18, -68, 35, 110, -24, -83, -47, 124, -32, -20, 37))
-  val publicIdPrefix = "sp"
+  override protected val publicIdIvSpec: IvParameterSpec = new IvParameterSpec(Array(-67, -41, -89, -42, 13, -18, -68, 35, 110, -24, -83, -47, 124, -32, -20, 37))
+  override protected val publicIdPrefix = "sp"
 }
