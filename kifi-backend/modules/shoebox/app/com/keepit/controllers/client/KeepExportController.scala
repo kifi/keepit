3dea6e15f3938317dcbd72e3cd25003418b14857
Ryan P Brewster <ryan@kifi.com>
2016-06-15 19:24:30 -0700

    Further simplify export logic

--- kifi-backend/modules/shoebox/app/com/keepit/controllers/client/KeepExportController.scala
+++ kifi-backend/modules/shoebox/app/com/keepit/controllers/client/KeepExportController.scala
@@ -10,7 +10,7 @@ import com.keepit.common.db.slick.Database
 import com.keepit.common.healthcheck.AirbrakeNotifier
 import com.keepit.common.json.EitherFormat
 import com.keepit.common.time._
-import com.keepit.export.FullExportCommander
+import com.keepit.export.{ FullStreamingExport, FullExportCommander }
 import com.keepit.model._
 import com.keepit.social.BasicUser
 import play.api.libs.iteratee.{ Iteratee, Enumeratee, Enumerator }
@@ -74,78 +74,79 @@ class KeepExportController @Inject() (
   }
 
   def fullKifiExport() = UserAction { request =>
-    log.info(s"[RPB] Full kifi export for ${request.userId}")
     val export = fullExportCommander.fullExport(request.userId)
     val fileEnum = export.spaces.flatMap { space =>
       space.libraries.flatMap { library =>
-        library.keeps.flatMap { keeps =>
-          Enumerator.empty[(String, JsValue)]
-        } andThen library.keeps.map(_.keep).through(Enumeratee.grouped(Iteratee.getChunks)).through(Enumeratee.map { keeps =>
-          fullLibraryPage(library.library, keeps)
-        })
-      } andThen space.libraries.map(_.library).through(Enumeratee.grouped(Iteratee.getChunks)).through(Enumeratee.map { libs =>
-        fullSpacePage(space.space, libs)
-      })
-    } andThen export.spaces.map(_.space).through(Enumeratee.grouped(Iteratee.getChunks)).through(Enumeratee.map { spaces =>
-      fullIndexPage(export.user, spaces)
-    })
+        library.keeps.flatMap { keep =>
+          fullKeepPage(keep)
+        } andThen fullLibraryPage(library)
+      } andThen fullSpacePage(space)
+    } andThen fullIndexPage(export)
 
+    val exportBase = s"${request.user.primaryUsername.get.normalized.value}-kifi-export"
     val enum = Enumerator.outputStream { os =>
       val zip = new ZipOutputStream(os)
       fileEnum.run(Iteratee.foreach {
         case (filename, value) =>
-          zip.putNextEntry(new ZipEntry("kifi/" + filename + ".json"))
+          zip.putNextEntry(new ZipEntry(s"$exportBase/$filename.json"))
           zip.write(Json.prettyPrint(value).map(_.toByte).toArray)
           zip.closeEntry()
       }).andThen {
         case res => zip.close()
       }
     }
-    val exportFileName = s"${request.user.primaryUsername.get.normalized.value}-kifi-export.zip"
-    Ok.chunked(enum >>> Enumerator.eof).withHeaders(
+    Ok.chunked(enum andThen Enumerator.eof).withHeaders(
       "Content-Type" -> "application/zip",
-      "Content-Disposition" -> s"attachment; filename=$exportFileName"
+      "Content-Disposition" -> s"attachment; filename=$exportBase.zip"
     )
   }
 
-  private def fullIndexPage(me: BasicUser, spaces: Seq[Either[BasicUser, BasicOrganization]]): (String, JsValue) = {
+  private def fullIndexPage(export: FullStreamingExport.Root): Enumerator[(String, JsValue)] = {
     implicit val spaceWrites = EitherFormat.keyedWrites[BasicUser, BasicOrganization]("user", "org")
-    "index" -> Json.obj(
-      "me" -> me,
-      "spaces" -> JsArray(spaces.map { space =>
-        val partialSpace = spaceWrites.writes(space)
-        partialSpace
-      })
-    )
+    export.spaces.map(_.space).through(Enumeratee.grouped(Iteratee.getChunks)).through(Enumeratee.map { spaces =>
+      "index" -> Json.obj(
+        "me" -> export.user,
+        "spaces" -> JsArray(spaces.map { space =>
+          val partialSpace = spaceWrites.writes(space)
+          partialSpace
+        })
+      )
+    })
   }
-  private def fullSpacePage(space: Either[BasicUser, BasicOrganization], libs: Seq[Library]): (String, JsValue) = {
+  private def fullSpacePage(space: FullStreamingExport.SpaceExport): Enumerator[(String, JsValue)] = {
     implicit val spaceWrites = EitherFormat.keyedWrites[BasicUser, BasicOrganization]("user", "org")
-    val fullSpace = spaceWrites.writes(space)
-    space.fold(_.externalId.id, _.orgId.id) -> Json.obj(
-      "space" -> fullSpace,
-      "libraries" -> JsArray(libs.map { lib =>
-        val partialLibrary = Json.obj(
-          "id" -> Library.publicId(lib.id.get),
-          "name" -> lib.name,
-          "description" -> lib.description
-        )
-        partialLibrary
-      })
-    )
+    space.libraries.map(_.library).through(Enumeratee.grouped(Iteratee.getChunks)).through(Enumeratee.map { libs =>
+      val fullSpace = spaceWrites.writes(space.space)
+      space.space.fold(_.externalId.id, _.orgId.id) -> Json.obj(
+        "space" -> fullSpace,
+        "libraries" -> JsArray(libs.map { lib =>
+          val partialLibrary = Json.obj(
+            "id" -> Library.publicId(lib.id.get),
+            "name" -> lib.name,
+            "description" -> lib.description
+          )
+          partialLibrary
+        })
+      )
+    })
   }
-  def fullLibraryPage(library: Library, keeps: Seq[Keep]): (String, JsValue) = {
-    val fullLibrary = Json.obj("name" -> library.name)
-    Library.publicId(library.id.get).id -> Json.obj(
-      "library" -> fullLibrary,
-      "keeps" -> JsArray(keeps.map { keep =>
-        val partialKeep = Json.obj(
-          "id" -> Keep.publicId(keep.id.get),
-          "title" -> keep.title,
-          "url" -> keep.url,
-          "keptAt" -> keep.keptAt
-        )
-        partialKeep
-      })
-    )
+  def fullLibraryPage(library: FullStreamingExport.LibraryExport): Enumerator[(String, JsValue)] = {
+    val fullLibrary = Json.obj("name" -> library.library.name)
+    library.keeps.map(_.keep).through(Enumeratee.grouped(Iteratee.getChunks)).through(Enumeratee.map { keeps =>
+      Library.publicId(library.library.id.get).id -> Json.obj(
+        "library" -> fullLibrary,
+        "keeps" -> JsArray(keeps.map { keep =>
+          val partialKeep = Json.obj(
+            "id" -> Keep.publicId(keep.id.get),
+            "title" -> keep.title,
+            "url" -> keep.url,
+            "keptAt" -> keep.keptAt
+          )
+          partialKeep
+        })
+      )
+    })
   }
+
+  def fullKeepPage(keep: FullStreamingExport.KeepExport): Enumerator[(String, JsValue)] = Enumerator.empty
 }
